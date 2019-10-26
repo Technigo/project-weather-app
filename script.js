c13ad246506bcfad15ca007685444feac287f55a
Anna Binnquist <Binnquist@annas-mbp-3.mynet>
2019-10-26 23:35:33 +0200

    started styling and fixed the temp

--- script.js
+++ script.js
@@ -8,11 +8,14 @@ fetch('https://api.openweathermap.org/data/2.5/weather?q=Stockholm&APPID=0873dd3
 .then((json) => {
 
 
-    console.log(json)
+    //console.log(json)
 
-    json.weather.forEach((test) => {
 
-        myWeather.innerHTML += `My weather right now is ${test.main} `
+    myWeather.innerHTML += `<h1>My weather right now in ${json.name} </h1>`
 
-    })
+    myWeather.innerHTML += `<h2>Tempurature is: ${Math.round(json.main.temp - 273.15)} degrees </h2>`
+
+    myWeather.innerHTML += `<h2>Type of weather: ${json.weather[0].description} </h2>`
+
+
 })
