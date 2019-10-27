fb765b193ad870390cf08fa8d1e6581e26c8c3b0
Anna Binnquist <Binnquist@annas-mbp-3.mynet>
2019-10-27 14:20:06 +0100

    trying to add forecast, but Id say the readme is lacking

--- script.js
+++ script.js
@@ -1,4 +1,5 @@
 const myWeather = document.getElementById('container')
+const myWeather2 = document.getElementById('container2')
 
 fetch('https://api.openweathermap.org/data/2.5/weather?q=Sóller&APPID=0873dd387b81dc473ae107f675063248')
     .then((response) => {
@@ -6,9 +7,6 @@ fetch('https://api.openweathermap.org/data/2.5/weather?q=Sóller&APPID=0873dd387
     })
 
 .then((json) => {
-
-
-            //console.log(json)
 
 
             myWeather.innerHTML += `<h1>My weather right now in ${json.name} </h1>`
@@ -46,20 +44,13 @@ fetch('https://api.openweathermap.org/data/2.5/forecast?q=Stockholm,Sweden&units
     .then((json) => {
 
 
-        //console.log(json)
+        myWeather2.innerHTML += `<h2>Temp min ${json.list[3].main.temp_min}</h2>`
 
-        const unixTimestampD = 1572188400
+        myWeather2.innerHTML += `<h2>Temp max ${json.list[3].main.temp_max}</h2>`
 
-        let myDate = new Date(unixTimestampD * 1000)
+        //const unixTimestampMon = 1572220800
+
+        //let myDate = new Date(unixTimestampD * 1000)
 
         
-
-//console.log(`${sunrise.getHours()}:${sunrise.getMinutes()}`)
-
-
-        /* myWeather.innerHTML += `<h2>My weather right now in ${myDate.getHours()}  </h2>` */
-
-    
-
-
-    })
+})
