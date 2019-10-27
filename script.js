28b1da38434f48a1df17c25b5706664726ed9b56
AleksandraMuller <aleksandra.e.mueller@gmail.com>
2019-10-27 22:08:31 +0100

    nr3

--- script.js
+++ script.js
@@ -9,7 +9,27 @@ fetch('http://api.openweathermap.org/data/2.5/weather?q=Stockholm,Sweden&units=m
         const temperature = Math.round(json.main.temp * 10 ) / 10
         json.weather.forEach((el) => {
             const desc = el.description
-            container.innerHTML = `<h1>In ${json.name} the temperature is ${temperature} and we have ${desc} today.</h1>`
+            container.innerHTML = `<h1>The weather for ${json.name}:</h1>`
+            container.innerHTML += `<p>The temerature today is: ${temperature}</p>`
+            container.innerHTML += `<p>Type of weather: ${desc}</p>`
         })
-    
+
+        const sunrise = json.sys.sunrise
+        const sunset = json.sys.sunset
+        const sunriseConverted = new Date(sunrise * 1000);
+        const sunsetConverted = new Date(sunset * 1000);
+        let sunriseMin = sunriseConverted.getMinutes()
+        let sunsetMin = sunsetConverted.getMinutes()
+        if (sunriseMin < 10 ) { 
+            sunriseMin = '0' + sunriseMin;
+            } else {
+            sunriseMin = sunriseMin + '';
+            }
+        if (sunsetMin < 10) {
+            sunsetMin = '0' + sunsetMin;
+        }   else {
+            sunsetMin = sunsetMin + '';
+        }
+        container.innerHTML += `<p>Sunrise at: ${sunriseConverted.getHours()}:${sunriseMin}</p>`
+        container.innerHTML += `<p>Sunset at: ${sunsetConverted.getHours()}:${sunsetMin}</p>` 
     })
