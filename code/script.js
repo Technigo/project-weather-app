3f2c8d8eca76012b2e27557e4983ca486960d9f2
nyblad <nyblad@hotmail.com>
2019-10-27 22:39:26 +0100

    Updated to icon in todays weather

--- code/script.js
+++ code/script.js
@@ -21,11 +21,31 @@ fetch("http://api.openweathermap.org/data/2.5/weather?q=Bollnas&units=metric&APP
 
   //Create HTML content with the json content
   .then((json) => {
-    theCity.innerHTML = `${json.name}`
-    theTemp.innerHTML = `${Math.round((json.main.temp) * 10) / 10} &deg;C`
+    theCity.innerHTML = `<h2>${json.name}</h2>`
+    theTemp.innerHTML = `<h1>${json.main.temp.toFixed(1)} &deg;C</h1>`
     theWeather.innerHTML = `${json.weather[0].description}`
     theSunrise.innerHTML = `<img src=\"assets/sunrise.png\" width=\"50px\""></br>${sunriseTime}`
     theSunset.innerHTML = `<img src=\"assets/sunset.png\" width=\"50px\""></br>${sunsetTime}`
+
+
+    if (json.weather[0].description === "clear sky") {
+      theWeather.innerHTML = `<img src=\"assets/clear-sky-day.svg\" width=\"70px\""></br>`
+    } else if (json.weather[0].description === "few clouds") {
+      theWeather.innerHTML = `<img src=\"assets/few-clouds-day.svg\" width=\"70px\""></br>`
+    } else if (json.weather[0].description === "scattered clouds") {
+      theWeather.innerHTML = `<img src=\"assets/scattered-clouds.svg\" width=\"70px\""></br>`
+    } else if (json.weather[0].description === "broken clouds") {
+      theWeather.innerHTML = `<img src=\"assets/broken-clouds.svg\" width=\"70px\""></br>`
+    } else if (json.weather[0].description === "shower rain" || json.weather[0].description === "rain") {
+      theWeather.innerHTML = `<img src=\"assets/broken-clouds.svg\" width=\"70px\""></br>`
+    } else if (json.weather[0].description === "thunderstorm") {
+      theWeather.innerHTML = `<img src=\"assets/thunderstorm.svg\" width=\"70px\""></br>`
+    } else if (json.weather[0].description === "snow") {
+      theWeather.innerHTML = `<img src=\"assets/snow.svg\" width=\"70px\""></br>`
+    } else if (json.weather[0].description === "mist") {
+      theWeather.innerHTML = `<img src=\"assets/mist.svg\" width=\"70px\""></br>`
+    }
+
   })
 
 //Fetch API for Bollnäs forecast every 3 hours
@@ -42,11 +62,21 @@ fetch("http://api.openweathermap.org/data/2.5/forecast?q=Bollnas&units=metric&cn
     //Loop to go through the 5 forecasts in API
     json.list.forEach(forecast => {
 
-      theDayDate.innerHTML += `${forecast.dt_txt}</br>`
+      theDayDate.innerHTML += `${forecast.dt}</br>`
       theDayWeather.innerHTML += `${forecast.weather[0].description}</br>`
-      theDayTemp.innerHTML += `${Math.round((forecast.main.temp_max) * 10) / 10} &deg;C / ${Math.round((forecast.main.temp_min) * 10) / 10} &deg;C</br>`
+      // theDayTemp.innerHTML += `${Math.round((forecast.main.temp_max) * 10) / 10} &deg;C / ${Math.round((forecast.main.temp_min) * 10) / 10} &deg;C</br>`
+      theDayTemp.innerHTML += `${forecast.main.temp_max.toFixed(1)} &deg;C / ${forecast.main.temp_min.toFixed(1)} &deg;C</br>`
 
     })
+
+    // if (theDayTemp > '15') {
+    //   document.body.style.backgroundColor = '#B0B7c6';
+    // } else {
+    //   document.body.style.backgroundColor = "#ff8d88";
+    // }
+
+
+
 
   })
 
@@ -66,9 +96,6 @@ let sunsetTime = sunset.toLocaleTimeString([], { timeStyle: 'short' });
 
 const getIcons = () => {
 
-  if (forecast.weather[0].description === "clear sky") {
-    theDayWeather.innerHTML = `<img src=\"assets/clear-sky-day.svg\" width=\"20px\""></br>`
-  }
 
 }
 
