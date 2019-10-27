ba8cfcd02cb0951271a7abd7c6ab77eae7c7a730
AleksandraMuller <aleksandra.e.mueller@gmail.com>
2019-10-27 15:44:40 +0100

    nr2

--- script.js
+++ script.js
@@ -1,4 +1,4 @@
-const weather = document.getElementById('weather')
+const container = document.getElementById('weather')
 
 fetch('http://api.openweathermap.org/data/2.5/weather?q=Stockholm,Sweden&units=metric&APPID=9c5547207014dca2db40f4f51bbb601a')
     .then((response) => {
@@ -6,4 +6,10 @@ fetch('http://api.openweathermap.org/data/2.5/weather?q=Stockholm,Sweden&units=m
     })
     .then((json) => {
         console.log(json)
+        const temperature = Math.round(json.main.temp * 10 ) / 10
+        json.weather.forEach((el) => {
+            const desc = el.description
+            container.innerHTML = `<h1>In ${json.name} the temperature is ${temperature} and we have ${desc} today.</h1>`
+        })
+    
     })
