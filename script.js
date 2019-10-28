13714ab26d1b4776d0a81b3c10856d5af35f0855
Tiago Ferreira <tiagoferreira@tuta.io>
2019-10-28 23:14:58 +0100

    Geolocation refinement. Background color change on weather condition.

--- script.js
+++ script.js
@@ -1,125 +1,232 @@
-const apiKey = '79a5016dc063fba5a823f15d23b3fb1f'
+const apiKey = "79a5016dc063fba5a823f15d23b3fb1f";
 
-const monthNames = ["January", "February", "March", "April", "May", "June",
-    "July", "August", "September", "October", "November", "December"
+const monthNames = [
+  "January",
+  "February",
+  "March",
+  "April",
+  "May",
+  "June",
+  "July",
+  "August",
+  "September",
+  "October",
+  "November",
+  "December"
 ];
 
-const monthShortNames = ["Jan", "Feb", "Mar", "Apr", "May", "Jun",
-    "Jul", "Aug", "Sep", "Oct", "Nov", "Dec"
+const monthShortNames = [
+  "Jan",
+  "Feb",
+  "Mar",
+  "Apr",
+  "May",
+  "Jun",
+  "Jul",
+  "Aug",
+  "Sep",
+  "Oct",
+  "Nov",
+  "Dec"
 ];
 
-let city = ''
+let city = "";
 
 function locationRetrieved(pos) {
-    let crd = pos.coords
-    city = `lat=${crd.latitude}&lon=${crd.longitude}`
-    return city
+  let crd = pos.coords;
+  city = `lat=${crd.latitude}&lon=${crd.longitude}`;
+  return city;
 }
 
 function locationError(err) {
-    console.warn(`ERROR(${err.code}): ${err.message}`);
+  console.warn(`ERROR(${err.code}): ${err.message}`);
 }
 
 function toggle() {
-    this.classList.toggle("active")
+  this.classList.toggle("active");
 }
 
 const clearData = () => {
-    document.getElementById("city").innerHTML = ''
-    document.getElementById("summary").innerHTML = ''
-    document.getElementById("icon").style.display = 'none'
-    document.getElementById("forecast").innerHTML = ''
-    document.getElementById('sunrise-sunset').innerHTML = ''
-}
+  document.getElementById("city").innerHTML = "";
+  document.getElementById("summary").innerHTML = "";
+  document.getElementById("icon").style.display = "none";
+  document.getElementById("forecast").innerHTML = "";
+  document.getElementById("sunrise-sunset").innerHTML = "";
+};
 
 function showPosition(position) {
-    console.log("before: " + city)
-    city = `lat=${position.coords.latitude}&lon=${position.coords.longitude}`
-    console.log("after: " + city)
+  console.log("before: " + city);
+  city = `lat=${position.coords.latitude}&lon=${position.coords.longitude}`;
+  console.log("after: " + city);
+}
+
+const getWindDirection = degrees => {
+  if (degrees < 45) {
+    return "N";
+  } else if (degrees < 90) {
+    return "NE";
+  } else if (degrees < 135) {
+    return "E";
+  } else if (degrees < 180) {
+    return "SE";
+  } else if (degrees < 225) {
+    return "S";
+  } else if (degrees < 270) {
+    return "SW";
+  } else if (degrees < 315) {
+    return "W";
+  } else if (degrees <= 360) {
+    return "NW";
+  }
+};
+
+const getBackgroundColor = cod => {
+  if (cod >= 200 && cod < 300) {
+    return "#717a7d";
+  } else if (cod >= 300 && cod < 400) {
+    return "#8caeab";
+  } else if (cod >= 500 && cod < 600) {
+    return "#9BBB99";
+  } else if (cod >= 600 && cod < 700) {
+    return "#cdc9c9";
+  } else if (cod >= 700 && cod < 800) {
+    return "#dcdcdc";
+  } else if (cod === 800) {
+    return "#7ec0ee";
+  } else if (cod > 800) {
+    return "#ABA39A";
+  }
+};
+
+function inCaseOfError(error) {
+  switch (error.code) {
+    case error.PERMISSION_DENIED:
+      alert(
+        "User denied the request for Geolocation. Please type a city on the search bar."
+      );
+      break;
+    case error.POSITION_UNAVAILABLE:
+      alert(
+        "Location information is unavailable. Please type a city on the search bar."
+      );
+      break;
+    case error.TIMEOUT:
+      alert(
+        "The request to get user location timed out. Please type a city on the search bar."
+      );
+      break;
+    case error.UNKNOWN_ERROR:
+      alert("An unknown error occurred. Please type a city on the search bar.");
+      break;
+  }
 }
 
 const getWeather = () => {
-    if (document.getElementById('location').value === '') {
-        alert('Please type in a city.')
-    } else {
-        let position = `q=${document.getElementById('location').value}`
-        refreshWeather(position)
-        document.getElementById('search-bar').reset()
-    }
-}
+  if (document.getElementById("location").value === "") {
+    alert("Please type in a city.");
+  } else {
+    let position = `q=${document.getElementById("location").value}`;
+    refreshWeather(position);
+    document.getElementById("search-bar").reset();
+  }
+};
 
-const refreshWeather = (position) => {
-    console.log(typeof position)
-    if (typeof position === 'object') {
-        city = `lat=${position.coords.latitude}&lon=${position.coords.longitude}`
-    } else if (typeof position === 'string') {
-        city = position
-    }
-    console.log(city)
-    fetch(`https://api.openweathermap.org/data/2.5/weather?${city}&units=metric&APPID=${apiKey}`)
-        .then((response) => {
-            return response.json()
-        })
-        .then((json) => {
-            if (json.cod === 200) {
-                clearData()
-                let sunrise = new Date(0)
-                let sunset = new Date(0)
-                sunrise.setUTCSeconds(json.sys.sunrise)
-                sunset.setUTCSeconds(json.sys.sunset)
-                document.getElementById("city").innerHTML = `${json.name}, ${json.sys.country}`
-                document.getElementById("summary").innerHTML += `<p class="temperature">${json.main.temp.toFixed(1)}°</p>${json.weather[0].main.toLowerCase()}`
-                document.getElementById("sunrise-sunset").innerHTML = `sunrise ${("0" + sunrise.getHours()).slice(-2)}:${("0" + sunrise.getMinutes()).slice(-2)} | sunset: ${("0" + sunset.getHours()).slice(-2)}:${("0" + sunset.getMinutes()).slice(-2)}`
-                document.getElementById("icon").style.display = 'block'
-                document.getElementById("icon").src = `https://openweathermap.org/img/wn/${json.weather[0].icon}@2x.png`
-            } else if (json.cod === 404) {
-                console.log(city)
-                navigator.geolocation.getCurrentPosition(showPosition)
-                console.log(city)
-            }
-            else {
-                alert(`City not found.`)
-            }
+const refreshWeather = position => {
+  if (typeof position === "object") {
+    city = `lat=${position.coords.latitude}&lon=${position.coords.longitude}`;
+  } else if (typeof position === "string") {
+    city = position;
+  }
 
-        })
-        .catch((err) => {
-            console.log('caught error', err)
-            return err
-        })
+  fetch(
+    `https://api.openweathermap.org/data/2.5/weather?${city}&units=metric&APPID=${apiKey}`
+  )
+    .then(response => {
+      return response.json();
+    })
+    .then(json => {
+      if (json.cod === 200) {
+        clearData();
+        let sunrise = new Date(json.sys.sunrise * 1000);
+        let sunset = new Date(json.sys.sunset * 1000);
 
-    fetch(`https://api.openweathermap.org/data/2.5/forecast?${city}&cnt=5&units=metric&APPID=${apiKey}`)
-        .then((response) => {
-            return response.json()
-        })
-        .then((json => {
-            console.log(json)
-            index = 1
-            json.list.forEach(weather => {
-                let dt = new Date(0)
-                dt.setUTCSeconds(weather.dt)
-                document.getElementById("forecast").innerHTML += `<div class="main-forecast" id="section${index}"><div>${dt.getDate()} ${monthShortNames[dt.getMonth()]} ${("0" + dt.getHours()).slice(-2)}:${("0" + dt.getMinutes()).slice(-2)}</div><div class="forecast-info">${weather.main.temp.toFixed(0)}°<img src="https://openweathermap.org/img/wn/${weather.weather[0].icon}.png" alt="Weather representation"> <span id="arrow${index}">&#x25B6;</span></div></div><div class="detail"><p>This</p></div>`
-                index++;
-            })
+        document.getElementById("main").style.background = getBackgroundColor(
+          json.weather[0].id
+        );
+        console.log(json.weather[0].id);
 
-            document.getElementById("section1").onclick = toggle
-            document.getElementById("section2").onclick = toggle
-            document.getElementById("section3").onclick = toggle
-            document.getElementById("section4").onclick = toggle
-            document.getElementById("section5").onclick = toggle
-        }))
-        .catch((err) => {
-            console.log('caught error', err)
-        })
-}
+        document.getElementById(
+          "city"
+        ).innerHTML = `${json.name}, ${json.sys.country}`;
+        document.getElementById(
+          "summary"
+        ).innerHTML += `<p class="temperature">${json.main.temp.toFixed(
+          1
+        )}°</p>${json.weather[0].main.toLowerCase()}`;
+        document.getElementById("sunrise-sunset").innerHTML = `sunrise ${(
+          "0" + sunrise.getHours()
+        ).slice(-2)}:${("0" + sunrise.getMinutes()).slice(-2)} | sunset: ${(
+          "0" + sunset.getHours()
+        ).slice(-2)}:${("0" + sunset.getMinutes()).slice(-2)}`;
+        document.getElementById("icon").style.display = "block";
+        document.getElementById(
+          "icon"
+        ).src = `https://openweathermap.org/img/wn/${json.weather[0].icon}@2x.png`;
+      } else if (json.cod === 404) {
+        navigator.geolocation.getCurrentPosition(showPosition);
+      } else {
+        alert(`City not found.`);
+      }
+    })
+    .catch(err => {
+      return err;
+    });
+
+  fetch(
+    `https://api.openweathermap.org/data/2.5/forecast?${city}&cnt=5&units=metric&APPID=${apiKey}`
+  )
+    .then(response => {
+      return response.json();
+    })
+    .then(json => {
+      index = 1;
+      json.list.forEach(weather => {
+        let dt = new Date(0);
+        dt.setUTCSeconds(weather.dt);
+        document.getElementById(
+          "forecast"
+        ).innerHTML += `<div class="main-forecast" id="section${index}"><div>${dt.getDate()} ${
+          monthShortNames[dt.getMonth()]
+        } ${("0" + dt.getHours()).slice(-2)}:${("0" + dt.getMinutes()).slice(
+          -2
+        )}</div><div class="forecast-info">${weather.main.temp.toFixed(
+          1
+        )}°<img src="https://openweathermap.org/img/wn/${
+          weather.weather[0].icon
+        }.png" alt="Weather representation"> <span id="arrow${index}">&#x25B6;</span></div></div>
+        <div class="detail">
+        <p><b>Weather description:</b> ${weather.weather[0].description}</p>
+        <p><b>Wind speed (m/s):</b> ${weather.wind.speed.toFixed(0)}</p>
+        <p><b>Wind direction:</b> ${getWindDirection(weather.wind.deg)}</p>
+        <p><b>Atmospheric pressure (hPa):</b> ${weather.main.pressure}</p>
+        <p><b>Humidity (%):</b> ${weather.main.humidity}</p></div>`;
+
+        index++;
+      });
+
+      document.getElementById("section1").onclick = toggle;
+      document.getElementById("section2").onclick = toggle;
+      document.getElementById("section3").onclick = toggle;
+      document.getElementById("section4").onclick = toggle;
+      document.getElementById("section5").onclick = toggle;
+    })
+    .catch(err => {
+      console.log("caught error", err);
+    });
+};
 
 if (navigator.geolocation) {
-    try {
-        navigator.geolocation.getCurrentPosition(refreshWeather)
-    }
-    catch (err) {
-        console.log(err)
-    }
+  navigator.geolocation.getCurrentPosition(refreshWeather, inCaseOfError);
 } else {
-    refreshWeather('Stockholm, SE')
+  refreshWeather("Stockholm, SE");
 }
-
