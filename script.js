7d377eedc6b0cc9a40f8fd7766ce358a1c6a8b88
root <root@LAPTOP-H24D07TP.localdomain>
2019-10-28 18:08:51 +0100

    added temp and description

--- script.js
+++ script.js
@@ -1,9 +1,17 @@
 const container = document.getElementById("weather")
-
+const tempContainer = document.getElementById("temp")
+const descriptionContainer = document.getElementById("weatherDescription")
 fetch("http://api.openweathermap.org/data/2.5/weather?q=Stockholm,Sweden&units=metric&APPID=f4893e301384d5deeafa555ccaa61aaa")
 .then((response) => {
 return response.json()
 })
 .then((json) => {
-  console.log(json)  
+    console.log(json)
+  container.innerHTML = `<h1>Today's weather in ${json.name} </h1>` 
+  tempContainer.innerHTML = `<h2> ${json.main.temp}   ${json.weather[0].description}</h2>`
+  descriptionContainer.innerHTML = `<h2>  </h2>`
+
+  
 })
+
+
