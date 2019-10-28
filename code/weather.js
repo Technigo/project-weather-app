30d446a20a37c7deb6c106900a120a4effb2523c
Theresa Ulwahn <theresaulwahn@gmail.com>
2019-10-28 14:33:44 +0100

    First log of info is up on the site

---
+++ code/weather.js
@@ -0,0 +1,15 @@
+const container = document.getElementById('weathers')
+
+fetch('http://api.openweathermap.org/data/2.5/weather?q=Miami,%20USA&units=metric&APPID=804400298ac4f45795306a8ea7ab8f5e')
+.then((response) => {
+  return response.json()
+})
+.then((json) => {
+  container.innerHTML = `<h1>It is ${json.main.temp} in ${json.name} today :-)</h1>`
+  console.log(json)
+
+/*json.base((status) => {
+  container.innerHTML += `<p>${status.main}</p>`
+});*/
+
+})
