e243d712bf7b92ab523442d8f793648e36bdea85
AceTircevski <47758694+AceTircevski@users.noreply.github.com>
2019-10-28 21:56:23 +0100

    started and finished weather app step 1 and 2

---
+++ script.js
@@ -0,0 +1,19 @@
+const container = document.getElementById('weather')
+
+fetch ('http://api.openweathermap.org/data/2.5/weather?q=Stockholm,Sweden&units=metric&APPID=3927ba6963ab68cfceebff54c1ee693f')
+
+.then((response) => {
+
+    return response.json()
+})
+.then((json) => {
+
+ console.log(json)
+   container.innerHTML = `<h1>The daily temperature in stockholm is ${json.main.temp} degrees with a daily min of ${json.main.temp_min} and daily max of
+   ${json.main.temp_max} </h1>`
+
+   
+
+})
+    
+
