8ac90e4b8ccca94e9c533fc27587d971fa9c9f0b
Theresa Ulwahn <theresaulwahn@gmail.com>
2019-10-28 16:31:59 +0100

    trying out the sunset and the sunrise / not working at all

--- code/weather.js
+++ code/weather.js
@@ -5,12 +5,31 @@ fetch('http://api.openweathermap.org/data/2.5/weather?q=Miami,%20USA&units=metri
   return response.json()
 })
 .then((json) => {
-  container.innerHTML = `<h1>It is ${json.main.temp} ${json.weather[0].description} ${Math.round(json.main.temp)} in ${json.name} today :-)</h1>`
+  container.innerHTML = `<h1>It is ${Math.round(json.main.temp)} ${json.weather[0].description} in ${json.name} today :-)</h1>`
+
   console.log(json)
 
+  /*const container = document.getElementById('suntimes')
 
-/*json.base((status) => {
-  container.innerHTML += `<p>${status.main}</p>`
-});*/
+  fetch('http://api.openweathermap.org/data/2.5/weather?q=Miami,%20USA&units=metric&APPID=804400298ac4f45795306a8ea7ab8f5e')
+  .then((response) => {
+    return response.json()
+  })
+  .then((json) => {
+  container.innerHTML = `<h2>Sunrise at: ${`${json.sys.sunrise}:${json.sys.sunrise}`}</h2>`
+
+  /*container.innerHTML = `<h2>Sunset at: ${`${sunset.getHours()}:${sunset.getMinutes()}`}</h2>`*/
+
+
+const unixTimestamp = 1572261986
+
+const unixTimestamp2 = 1572302548
+
+let sunrise = new Date(unixTimestamp * 1000)
+
+let sunset = new Date(unixTimestamp2 * 1000)
+
+/*console.log(`${sunrise.getHours()}:${sunrise.getMinutes()}`)*/
+
 
 })
