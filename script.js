b232dbb49885fa91304568b41c86732f36c49f6d
Clara Gyökeres <claragyokeres@Claras-Air.lan>
2019-10-28 21:15:17 +0100

    updated version with consts

--- script.js
+++ script.js
@@ -1,10 +1,25 @@
 const container = document.getElementById('myWeather')
+const theTemperature = document.getElementById('temperature')
+const theCity = document.getElementById('city')
+const theImage = document.getElementById('weatherImage')
+const sunriseAndSunset = document.getElementById('sunUpSunDown')
 
 fetch('http://api.openweathermap.org/data/2.5/weather?q=Stockholm,Sweden&units=metric&APPID=82792eb4459b56038cc8a4b53d2f5c3d')
     .then((response) => {
         return response.json()
     })
     .then((json) => {
-        container.innerHTML = `<h1>The weather in ${json.name} today is: ${json.weather[0].main} Temp: ${Math.round(json.main.temp)} degrees with a humidity of:${json.main.humidity} percent Sunrise</h1>`
-
+        const unixTimestampSunrise = json.sys.sunrise
+        const unixTimestampSunset = json.sys.sunset
+        const sunrise = new Date(unixTimestampSunrise * 1000)
+        const sunset = new Date(unixTimestampSunset * 1000)
+        const sunriseTime = sunrise.toLocaleTimeString([], {
+            timeStyle: 'short'
+        })
+        const sunsetTime = sunset.toLocaleTimeString([], {
+            timeStyle: 'short'
+        })
+        theTemperature.innerHTML += `${json.main.temp}° | ${json.weather[0].main}`
+        theCity.innerHTML += `${json.name}`
+        container.innerHTML += `Sunrise at ${sunriseTime} | Sunset at ${sunsetTime}`
     })
