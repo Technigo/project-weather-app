8d152639a7beaf2ad3909ed372dd5c7b34664cb5
codebyfredrik <50440682+codebyfredrik@users.noreply.github.com>
2019-10-28 21:44:04 +0100

    Code refactoring

--- weather.js
+++ weather.js
@@ -1,7 +1,7 @@
 const APIKEY = '3d9e5059943c355e05df6f5850ac47d3';
 
 const getWeather = async city => {
-	const base = 'http://api.openweathermap.org/data/2.5/weather';
+	const base = 'https://api.openweathermap.org/data/2.5/weather';
 	const query = `?q=${city}&units=metric&APPID=${APIKEY}`;
 
 	const response = await fetch(base + query);
@@ -11,7 +11,7 @@ const getWeather = async city => {
 };
 
 const getForecast = async city => {
-	const base = 'http://api.openweathermap.org/data/2.5/forecast';
+	const base = 'https://api.openweathermap.org/data/2.5/forecast';
 	const query = `?q=${city}&units=metric&cnt=${40}&APPID=${APIKEY}`;
 
 	const response = await fetch(base + query);
