f8817bd2d3f313e564c28ba67cfd440e6213499b
Michel Elmoznino Laufer <mitchel_85@hotmail.com>
2019-10-28 10:32:40 +0100

    Step 1, Getting started with the weather API

---
+++ script.js
@@ -0,0 +1,11 @@
+const container = document.getElementById("weather")
+
+fetch('http://api.openweathermap.org/data/2.5/weather?q=Stockholm,Sweden&units=metric&APPID=60a9f45b45a7b39b290afa28477d7241')
+  .then((response) => {
+    return response.json()
+  })
+  .then((json) => {
+    console.log(json)
+    container.innerHTML = `The temperature in Stockholm is ${json.main.temp} degrees with a daily min 
+    of ${json.main.temp_min} and a daily max of ${json.main.temp_max}`
+  })
