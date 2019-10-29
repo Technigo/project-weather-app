5ebc78d45652e4dda0f26a11d89a97eb5b73e43b
Johanna Luks Wærn <johanna@misswaern.se>
2019-10-29 09:30:05 +0100

    style, images
    initial styling mobile
    adding/deletin images
    js - pick image depending on weather

--- weather.js
+++ weather.js
@@ -1,5 +1,6 @@
 const container1 = document.getElementById("todaysWeather")
 const theTemperature = document.getElementById("temperature")
+const theWeatherSummary = document.getElementById("weatherSummary")
 const theCity = document.getElementById("city")
 const theImage = document.getElementById("weatherImage")
 const sunriseAndSunset = document.getElementById("sunUpSunDown")
@@ -12,19 +13,39 @@ fetch(
   })
 
   .then(json => {
-    //Declare variable for the time of sunrise/sunset
     const unixTimestampSunrise = json.sys.sunrise
     const unixTimestampSunset = json.sys.sunset
 
-    //To get sunrise/sunset time in hours:minutes:seconds
-    let sunrise = new Date(unixTimestampSunrise * 1000)
-    let sunset = new Date(unixTimestampSunset * 1000)
+    const sunrise = new Date(unixTimestampSunrise * 1000)
+    const sunset = new Date(unixTimestampSunset * 1000)
 
-    //Declare new variable to show only hh:mm
-    let sunriseTime = sunrise.toLocaleTimeString([], { timeStyle: "short" })
-    let sunsetTime = sunset.toLocaleTimeString([], { timeStyle: "short" })
+    const sunriseTime = sunrise.toLocaleTimeString([], { timeStyle: "short" })
+    const sunsetTime = sunset.toLocaleTimeString([], { timeStyle: "short" })
 
-    theTemperature.innerHTML += `${json.main.temp}° | ${json.weather[0].main}`
+    theTemperature.innerHTML += `${Math.floor(json.main.temp)}°C`
+    //theWeatherSummary.innerHTML += `${json.weather[0].main}`
     theCity.innerHTML += `${json.name}`
-    container1.innerHTML += `<p>Sunrise at ${sunriseTime} | Sunset at ${sunsetTime}</p>`
+
+    sunriseAndSunset.innerHTML += `Sunrise at ${sunriseTime} | Sunset at ${sunsetTime}`
+
+    const id = json.weather[0].id
+    if (id >= 200 && id <= 232) {
+      theImage.src = "images/lightning.png"
+    } else if (id >= 300 && id <= 531) {
+      theImage.src = "images/rain.png"
+    } else if (id >= 600 && id <= 622) {
+      theImage.src = "images/snow.png"
+    } else if (id >= 701 && id <= 781) {
+      theImage.src = "images/fog.png"
+    } else if (id === 800) {
+      theImage.src = "images/sun.png"
+    } else if (id === 801) {
+      theImage.src = "images/clouds-sun.png"
+    } else if (id === 802) {
+      theImage.src = "images/clouds-little sun.png"
+    } else if (id === 803) {
+      theImage.src = "images/clouds.png"
+    } else if (id === 804) {
+      theImage.src = "images/clouds-many.png"
+    }
   })
