3f7e7e98b8ac3bc976def7d66a304e62251543fc
Johanna Luks Wærn <johanna@misswaern.se>
2019-10-29 10:59:55 +0100

    forecast
    added new api for 5day forecast
    added code and styling for that forecast div

--- weather.js
+++ weather.js
@@ -1,9 +1,11 @@
-const container1 = document.getElementById("todaysWeather")
+//const container1 = document.getElementById("todaysWeather")
 const theTemperature = document.getElementById("temperature")
 const theWeatherSummary = document.getElementById("weatherSummary")
 const theCity = document.getElementById("city")
 const theImage = document.getElementById("weatherImage")
 const sunriseAndSunset = document.getElementById("sunUpSunDown")
+const theWeekdays = document.getElementById("weekdayForecast")
+const theTemperatureForecast = document.getElementById("temperatureForecast")
 
 fetch(
   "http://api.openweathermap.org/data/2.5/weather?q=Stockholm,Sweden&units=metric&APPID=8c93692e6dae295a7a97fe22cff8e23c"
@@ -23,12 +25,13 @@ fetch(
     const sunsetTime = sunset.toLocaleTimeString([], { timeStyle: "short" })
 
     theTemperature.innerHTML += `${Math.floor(json.main.temp)}°C`
-    //theWeatherSummary.innerHTML += `${json.weather[0].main}`
+    theWeatherSummary.innerHTML += `${json.weather[0].main}`
     theCity.innerHTML += `${json.name}`
 
     sunriseAndSunset.innerHTML += `Sunrise at ${sunriseTime} | Sunset at ${sunsetTime}`
 
     const id = json.weather[0].id
+    //const id = 200
     if (id >= 200 && id <= 232) {
       theImage.src = "images/lightning.png"
     } else if (id >= 300 && id <= 531) {
@@ -48,4 +51,21 @@ fetch(
     } else if (id === 804) {
       theImage.src = "images/clouds-many.png"
     }
+  })
+
+fetch(
+  "http://api.openweathermap.org/data/2.5/forecast?q=Stockholm,Sweden&units=metric&APPID=8c93692e6dae295a7a97fe22cff8e23c"
+)
+  .then(response => {
+    return response.json()
+  })
+
+  .then(json => {
+    const unixTimestampToday = json.list[0].dt
+    var today = new Date(unixTimestampToday * 1000)
+
+    var options = { weekday: "long" }
+    const weekday = new Intl.DateTimeFormat("en-US", options).format(today)
+
+    theWeekdays.innerHTML += `${weekday}`
   })
