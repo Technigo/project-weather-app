1f0e946f17569334b31680be117b82e115186b9f
SanSjo <susanna.sjoberg@gmail.com>
2019-10-29 10:33:26 +0100

    done with step 2

--- Designs/script.js
+++ Designs/script.js
@@ -11,5 +11,11 @@ fetch('http://api.openweathermap.org/data/2.5/weather?q=Stockholm,Sweden&units=m
     const tempOneDec = temp.toFixed(0.1)
     container.innerHTML += `<p>Temperature: ${tempOneDec} °C</p>`
     container.innerHTML += `Today it's: ${json.weather[0].description}`
+    const sunriseUTC = json.sys.sunrise
+    const newSunriseDate = new Date(sunriseUTC * 1000).toLocaleString()
+    container.innerHTML += `<p>Sunrise: ${newSunriseDate}</p>`
+    const sunsetUTC = json.sys.sunset
+    const newSunsetDate = new Date(sunsetUTC * 1000).toLocaleString()
+    container.innerHTML += `<p>Sunset: ${newSunsetDate}</p>`
   })
   
