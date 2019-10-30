6e262f33baf5b33b651a7a54ca6bb32258b6b514
AceTircevski <47758694+AceTircevski@users.noreply.github.com>
2019-10-30 21:31:28 +0100

    step 4

--- script.js
+++ script.js
@@ -1,25 +1,66 @@
-const container = document.getElementById('weather')
+"use strict";
 
-fetch ('http://api.openweathermap.org/data/2.5/weather?q=Stockholm,Sweden&units=metric&APPID=3927ba6963ab68cfceebff54c1ee693f')
+const api_key = "3927ba6963ab68cfceebff54c1ee693f";
+const container = document.getElementById("weather");
+const days = ["Sun", "Mon", "Tues", "Wed", "Thurs", "Fri", "Sat"];
 
-.then((response) => {
+fetch(
+  `http://api.openweathermap.org/data/2.5/weather?q=Stockholm,Sweden&units=metric&APPID=${api_key}`
+)
+  .then(response => {
+    return response.json();
+  })
+  .then(json => {
+    console.log(json);
 
-    return response.json()
-})
-.then((json) => {
-  
- 
- console.log(json)
-   
- container.innerHTML = `<h2>The daily temperature in stockholm is ${json.main.temp} degrees with a daily min of ${json.main.temp_min} and daily max of
-   ${json.main.temp_max} </h2>`
+    let sunriseHour = new Date(json.sys.sunrise * 1000).toLocaleTimeString([], {
+      hour: "2-digit",
+      minute: "2-digit"
+    });
+    let sunsetHour = new Date(json.sys.sunset * 1000).toLocaleTimeString([], {
+      hour: "2-digit",
+      minute: "2-digit"
+    });
 
-   
+    console.log(`Sunrise: ${sunriseHour}, Sunset: ${sunsetHour}`);
 
-})
-    
+    container.innerHTML = `<h2>Todays weather in Stockholm  ${json.main.temp} degrees with a daily min of ${json.main.temp_min} and daily max of
+   ${json.main.temp_max} ,  ${sunriseHour} ${sunsetHour}</h2>`;
+  });
 
-//  let sunset
+const handle5DayForecast = json => {
+  const forecastDiv = document.getElementById("forecast");
+  const dates = {};
 
-// let sunrise = new Date();
-// document.getElementById('weather')
+  json.list.forEach(weather => {
+    const date = weather.dt_txt.split(" ")[0];
+    if (dates[date]) {
+      dates[date].push(weather);
+    } else {
+      dates[date] = [weather];
+    }
+  });
+
+  Object.entries(dates).forEach((item, index) => {
+    if (index === 0) {
+      return;
+    }
+
+    const date = days[new Date(item[0]).getDay()];
+    const weatherValues = item[1];
+
+    const temps = weatherValues.map(value => value.main.temp);
+    const minTemp = Math.min(...temps);
+    const maxTemp = Math.max(...temps);
+
+    forecastDiv.innerHTML += `<li>${date} - min: ${minTemp.toFixed(
+      1
+    )}, max: ${maxTemp.toFixed(1)}</li>`;
+  });
+};
+
+fetch(
+  `https://api.openweathermap.org/data/2.5/forecast?q=Stockholm,Sweden&appid=${api_key}&units=metric`
+)
+  .then(res => res.json())
+  .then(handle5DayForecast);
