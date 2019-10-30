8b63df431e3548b089c47f21ec055fcb1ce21f43
Theresa Ulwahn <theresaulwahn@gmail.com>
2019-10-30 11:45:21 +0100

    Last details working with the forecast, min and max temp and description

--- code/weather.js
+++ code/weather.js
@@ -19,41 +19,42 @@ fetch('http://api.openweathermap.org/data/2.5/weather?q=Miami,%20USA,3166-2US-FL
   console.log(json)
 
 /*************SUNRISE & SUNSET **********/
-  //Declare variable for the time of sunrise/sunset
-  const unixTimestampSunrise = json.sys.sunrise
-  const unixTimestampSunset = json.sys.sunset
 
-  //To get sunrise/sunset time in hours:minutes:seconds
-  let sunrise = new Date(unixTimestampSunrise * 1000)
-  let sunset = new Date(unixTimestampSunset * 1000)
+//Declare variable for the time of sunrise/sunset
+const unixTimestampSunrise = json.sys.sunrise
+const unixTimestampSunset = json.sys.sunset
 
-  //Declare new variable to show only hh:mm
-  let sunriseTime = sunrise.toLocaleTimeString([], { timeStyle: 'short' })
-  let sunsetTime = sunset.toLocaleTimeString([], { timeStyle: 'short' })
+//To get sunrise/sunset time in hours:minutes:seconds
+let sunrise = new Date(unixTimestampSunrise * 1000)
+let sunset = new Date(unixTimestampSunset * 1000)
 
-  sunsetContainer.innerHTML = `<h1> Sunset: ${sunsetTime} </h1>`
-  sunriseContainer.innerHTML = `<h1> Sunrise: ${sunriseTime} </h1>`
+//Declare new variable to show only hh:mm
+let sunriseTime = sunrise.toLocaleTimeString([], { timeStyle: 'short' })
+let sunsetTime = sunset.toLocaleTimeString([], { timeStyle: 'short' })
+
+sunsetContainer.innerHTML = `<h1> Sunset: ${sunsetTime} </h1>`
+sunriseContainer.innerHTML = `<h1> Sunrise: ${sunriseTime} </h1>`
 })
+
+
+/*******FORECAST 4 DAYS ********/
 
 const wednesdayBox = document.getElementById('wednesdayContainer')
 const thursdayBox = document.getElementById('thursdayContainer')
 const fridayBox = document.getElementById('fridayContainer')
 const saturdayBox = document.getElementById('saturdayContainer')
 
-/*******FORECAST 4 DAYS ********/
-
 fetch('http://api.openweathermap.org/data/2.5/forecast?q=Miami,%20USA&units=metric&APPID=c4ff459e6977fcdff3cc5fa6319866b6')
 .then((response) => {
   return response.json()
 })
 .then((json) => {
-  wednesdayBox.innerHTML= `<p>Wednesday:${json.list[15].main.temp.toFixed(1)}</p>`
-  thursdayBox.innerHTML=`<p>Thursday:${json.list[23].main.temp.toFixed(1)}</p>`
-  fridayBox.innerHTML= `<p>Friday:${json.list[31].main.temp.toFixed(1)}</p>`
-  saturdayBox.innerHTML= `<p>Saturday:${json.list[39].main.temp.toFixed(1)}</p>`
+  wednesdayBox.innerHTML= `<p>Wednesday min:${json.list[15].main.temp_min.toFixed(1)}° - max:${json.list[18].main.temp_max.toFixed(1)}° with ${json.list[18].weather[0].description}</p>`
+  thursdayBox.innerHTML=`<p>Thursday min:${json.list[23].main.temp_min.toFixed(1)}° - max:${json.list[26].main.temp_max.toFixed(1)}° with ${json.list[26].weather[0].description}</p>`
+  fridayBox.innerHTML= `<p>Friday min:${json.list[31].main.temp_min.toFixed(1)}° - max:${json.list[34].main.temp_max.toFixed(1)}° with ${json.list[34].weather[0].description}</p>`
+  saturdayBox.innerHTML= `<p>Saturday min:${json.list[39].main.temp_min.toFixed(1)}° - max:${json.list[38].main.temp_max.toFixed(1)}° with ${json.list[38].weather[0].description}</p>`
   
 
    console.log(json)
-
 
 })
