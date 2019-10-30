bcfb58b7ae894abbacf437bc62ee026c951ac76e
ucha3i <jchabik@gmail.com>
2019-10-30 16:23:04 +0100

    step 4 - more or less

--- weather.js
+++ weather.js
@@ -1,16 +1,16 @@
 const container = document.getElementById('weather')
 
-fetch('http://api.openweathermap.org/data/2.5/weather?q=Stockholm,Sweden&units=metric&APPID=ead60d2e1c3fff29cccec12bd6a43922')
+/* fetch('http://api.openweathermap.org/data/2.5/weather?q=Stockholm,Sweden&units=metric&APPID=ead60d2e1c3fff29cccec12bd6a43922')
     .then((response) => {
         return response.json()
     })
     .then((json) => {
         console.log(json)
-        container.innerHTML = `<h1>Current weater in ${json.name}</h2>`
+        container.innerHTML = `<h1>Current weather in ${json.name}</h2>`
 
         const temp = Math.round(+json.main.temp)
 
-        container.innerHTML += `<p> The temperature is ${temp}. </p>`
+        container.innerHTML += `<p> The temperature is ${temp} °C.</p>`
 
         json.weather.forEach((element) => {
             container.innerHTML += `<p> The overal weather: ${element.description}. </p>`
@@ -24,42 +24,54 @@ fetch('http://api.openweathermap.org/data/2.5/weather?q=Stockholm,Sweden&units=m
         const sunriseShort = sunrise.toLocaleTimeString([], { timeStyle: 'short' })
         const sunsetShort = sunset.toLocaleTimeString([], { timeStyle: 'short' })
 
-        container.innerHTML += `<p> Sunrise ${sunriseShort} </p>`
-        container.innerHTML += `<p> Sunset ${sunsetShort} </p>`
+        container.innerHTML += `<p> Sunrise ${sunriseShort} AM.</p>`
+        container.innerHTML += `<p> Sunset ${sunsetShort} PM.</p>`
     })
     .catch((err) => {
         console.log('caught errors', err)
-    })
+    }) */
 
-/* fetch('http://api.openweathermap.org/data/2.5/forecast?q=Wanaka,NewZealand&units=metric&APPID=ead60d2e1c3fff29cccec12bd6a43922')
+fetch('http://api.openweathermap.org/data/2.5/forecast?q=Lisbon,Portugal&units=metric&APPID=ead60d2e1c3fff29cccec12bd6a43922')
     .then((response) => {
         return response.json()
     })
     .then((json) => {
         console.log(json)
-        container.innerHTML = `<h1>Current weater in ${json.city.name}</h2>`
+        container.innerHTML = `<h1>Current weather in ${json.city.name}</h2>`
 
-        json.weather.forEach((element) => {
-            container.innerHTML += `<p> The overal weather: ${element.description}. </p>`
+        // // TO DZIAŁA DLA POJEDYNCZEGO DNIA, ALE NIE DLA PROGNOZY
+
+        /*  const temp = Math.round(+json.main.temp)
+         container.innerHTML += `<p> The temperature is ${temp} °C.</p>` */
+
+        // TU WYWALA MI BUGA, BO NIE MOZE ZNALEZC ELEMENTU WEATHER W PIERWSZEJ ARRAY
+
+        /*  json.list[""].weather.forEach((element) => {
+             container.innerHTML += `<p> The overal weather: ${element.description}. </p>`
+         }) */
+
+        json.list.forEach((day) => {
+            container.innerHTML += `<p> The overal weather: ${day.weather[0].description}. </p>`
         })
 
-        container.innerHTML += `<p> The temperature is between ${json.main.temp_max} and ${json.main.temp_min}. </p>`
+        // TO DZIAŁA DLA POJEDYNCZEGO DNIA, ALE NIE DLA PROGNOZY
 
-        //Declare variable for the time of sunrise/sunset
-        const unixTimestampSunrise = json.sys.sunrise
-        const unixTimestampSunset = json.sys.sunset
+        /* json.weather.forEach((element) => {
+            container.innerHTML += `<p> The overal weather: ${element.description}. </p>`
+        }) */
 
-        //To get sunrise/sunset time in hours:minutes:seconds
-        const sunrise = new Date(unixTimestampSunrise * 1000)
-        const sunset = new Date(unixTimestampSunset * 1000)
+
+        //Declare variable for the time of sunrise/sunset and get them in hours:minutes:seconds GMT
+        const sunrise = new Date(json.city.sunrise * 1000);
+        const sunset = new Date(json.city.sunset * 1000)
 
         //Declare new variable to show only hh:mm
-        const sunriseTime = sunrise.toLocaleTimeString([], { timeStyle: 'short' })
-        const sunsetTime = sunset.toLocaleTimeString([], { timeStyle: 'short' })
+        const sunriseShort = sunrise.toLocaleTimeString([], { timeStyle: 'short' })
+        const sunsetShort = sunset.toLocaleTimeString([], { timeStyle: 'short' })
 
-        container.innerHTML += `<p> Sunrise ${sunriseTime} </p>`
-        container.innerHTML += `<p> Sunset ${sunriseTime} </p>`
+        container.innerHTML += `<p> Sunrise ${sunriseShort} AM.</p>`
+        container.innerHTML += `<p> Sunset ${sunsetShort} PM.</p>`
     })
     .catch((err) => {
         console.log('caught errors', err)
-    }) */
+    })
