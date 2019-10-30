47a4f6942fd76da6d6765732fec36868cc674b89
Nina Månsson <ninamansster@gmail.com>
2019-10-30 10:06:20 +0100

    functionality in Weather now fetch

--- Code/weather.js
+++ Code/weather.js
@@ -1,8 +1,11 @@
-fetch('http://api.openweathermap.org/data/2.5/forecast?q=Kalmar,Sweden&units=metric&cnt=3&APPID=996158b88361cd2c1991a7aee0bf6883')
+const containerWeather = document.getElementById('weatherNow')
+
+fetch('http://api.openweathermap.org/data/2.5/weather?q=Kalmar,se&units=metric&APPID=996158b88361cd2c1991a7aee0bf6883')
     .then((response) => {
         return response.json()
     })
     .then((json) => {
+        containerWeather.innerHTML = `<h1>The weather in ${json.name} is ${json.weather[0].description} and it is ${json.main.temp} degrees.</h1>`
         console.log(json)
     })
     .catch((err) => {
@@ -10,9 +13,6 @@ fetch('http://api.openweathermap.org/data/2.5/forecast?q=Kalmar,Sweden&units=met
     })
 
 /*
-.then((json) => {
-return request.json()
-})
-.then((json) => {
-console.log(json)
-})*/
+const container = document.getElementById('forecast')
+'http://api.openweathermap.org/data/2.5/forecast?q=Kalmar,Sweden&units=metric&cnt=3&APPID=996158b88361cd2c1991a7aee0bf6883
+*/
