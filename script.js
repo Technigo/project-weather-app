72b8b05df1455e13b02f1e3bf54388879b0e7691
anna-ip <anna.almestal@gmail.com>
2019-10-30 10:05:18 +0100

    bsfkjads

--- script.js
+++ script.js
@@ -15,7 +15,8 @@ fetch(
     container.innerHTML = `<h1>Weather right now in ${json.name}.</h1>`;
     //return json.toFixed(1);
     console.log(json); //first check
-    tempContainer.innerHTML += `<h1>${json.main.temp.toFixed(1)} degrees</h1>`;
+    tempContainer.innerHTML += `<h1>${json.main.temp.toFixed(1)} &#8451 
+    </h1>`;
     //return json.toFixed(1);
     descriptionContainer.innerHTML += `<h1>${json.weather[0].description}</h1>`;
 
@@ -25,16 +26,16 @@ fetch(
     let sunrise = new Date(unixTimestampSunrise * 1000);
     //Declare new variable to show only hh:mm
     let sunriseTime = sunrise.toLocaleTimeString([], { timeStyle: "short" });
-    sunriseContainer.innerHTML += `<p>Sunrise today ${sunriseTime}</p>`;
+    sunriseContainer.innerHTML += `<p>Sunrise today at ${sunriseTime}</p>`;
 
     const unixTimestampSunset = json.sys.sunset;
     let sunset = new Date(unixTimestampSunset * 1000);
     let sunsetTime = sunset.toLocaleTimeString([], { timeStyle: "short" });
-    sunsetContainer.innerHTML += `<p>Sunset today ${sunsetTime}</p>`;
+    sunsetContainer.innerHTML += `<p>Sunset today at ${sunsetTime}</p>`;
   });
 
 const forecastContainer = document.getElementById("forecast");
-const monContainer = document.getElementById("mon");
+const todayContainer = document.getElementById("today");
 const tueContainer = document.getElementById("tue");
 const wedContainer = document.getElementById("wed");
 const thuContainer = document.getElementById("thu");
@@ -50,16 +51,16 @@ fetch(
 
   .then(json => {
     //forecastContainer.innerHTML = `<h1>Weather right now in ${json.list}.</h1>`;
-    monContainer.innerHTML = `<p>Monday ${json.list[0].main.temp.toFixed(1)}
-    </p>`;
+    todayContainer.innerHTML = `<p>Today ${json.list[0].main.temp.toFixed(1)}
+    &#8451</p>`;
     tueContainer.innerHTML = `<p>Tuesday ${json.list[8].main.temp.toFixed(1)}
-    </p>`;
+    &#8451</p>`;
     wedContainer.innerHTML = `<p>Wednsday ${json.list[16].main.temp.toFixed(1)}
-    </p>`;
+    &#8451</p>`;
     thuContainer.innerHTML = `<p>Thursday ${json.list[24].main.temp.toFixed(1)}
-    </p>`;
+    &#8451</p>`;
     friContainer.innerHTML = `<p>Friday ${json.list[32].main.temp.toFixed(1)}
-    </p>`;
+    &#8451</p>`;
 
     console.log(json); //first check
   });
