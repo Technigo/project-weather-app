afe3061a1e9e0aebb7d4762e732ec65a21b20f09
sarah wolfson <swolfson23@gmail.com>
2019-10-30 13:22:17 +0100

    step 4 completed

--- code/weather.js
+++ code/weather.js
@@ -4,7 +4,6 @@ fetch('http://api.openweathermap.org/data/2.5/weather?q=Stockholm,Sweden&units=m
     .then((Response) => {
         return Response.json()
     })
-console.log(json)
     .then((json) => {
         header.innerHTML = `<h1> ${json.name} weather right now is </h1>`
         const temp = json.main.temp
@@ -17,7 +16,7 @@ console.log(json)
         const time2 = json.sys.sunset
         let sunrise = new Date(time1 * 1000)
         let sunset = new Date(time2 * 1000)
-        sun.innerHTML += `<p>Sunrise: ${`${sunrise.getHours()}:${sunrise.getMinutes()}`}</p>`
+        sun.innerHTML += `<p>Sunrise: ${`${sunrise.getHours()}.00`}</p>`
         sun.innerHTML += `<p>Sunset: ${`${sunset.getHours()}:${sunset.getMinutes()}`}</p>`
     });
 
@@ -30,20 +29,41 @@ fetch('http://api.openweathermap.org/data/2.5/forecast?q=Stockholm,Sweden&units=
     })
 
     .then((json) => {
-        forcast.innerHTML = `<h2> 3 day forcast</h2>`
+        forcast.innerHTML = `<h2> Hourly forcast</h2>`
 
         //day 1 
-        const weekday = json.list[1].dt
-        let newWeekday = new Date(weekday)
-        let day = newWeekday.getDay()
+        // Time interval One
+        const hour1 = json.list[0].dt
+        const time1 = new Date(hour1 * 1000).getHours()
+        document.getElementById("hour1").innerHTML += `<p>${time1}.00</P>`
+        document.getElementById("hour1").innerHTML += `<p>${json.list[0].weather[0].description}</p>`
+        const minTempOne = json.list[0].main.temp_min
+        const maxTempOne = json.list[0].main.temp_max
+        const minTemp = minTempOne.toFixed(0.1)
+        const maxTemp = maxTempOne.toFixed(0.1)
+        document.getElementById("hour1").innerHTML += `<p>${minTemp} ° / ${maxTemp} °C</p>`
 
-        day1.innerHTML += `<h2>${day}</h2>`
-        day1.innerHTML += `<h3>${json.list[1].weather.description}</h3>`
-        day1.innerHTML += `<h3>Tempature min: ${json.list[6].main.temp_min} <br> Temp max: ${json.list[7].main.temp_max} </h3`
+
         //day 2
-        day2.innerHTML += `<h3></h3>`
-        day2.innerHTML += `<h3></h3>`
+        const hour2 = json.list[1].dt
+        const time2 = new Date(hour2 * 1000).getHours()
+        document.getElementById("hour2").innerHTML += `<p>${time2}.00</p>`
+        document.getElementById("hour2").innerHTML += `<p>${json.list[1].weather[0].description}</p>`
+        const minTempTwo = json.list[1].main.temp_min
+        const maxTempTwo = json.list[1].main.temp_max
+        const minTemp2 = minTempTwo.toFixed(0.1)
+        const maxTemp2 = maxTempTwo.toFixed(0.1)
+        document.getElementById("hour2").innerHTML += `<p>${minTemp2} ° / ${maxTemp2} °C </p>`
 
-        //day 3
 
+        // //day 3
+        const hour3 = json.list[2].dt
+        const time3 = new Date(hour3 * 1000).getHours()
+        document.getElementById("hour3").innerHTML += `<p>${time3}.00 </p>`
+        document.getElementById("hour3").innerHTML += `<p>${json.list[2].weather[0].description} </p>`
+        const minTempThree = json.list[2].main.temp_min
+        const maxTempThree = json.list[2].main.temp_max
+        const minTemp3 = minTempThree.toFixed(0.1)
+        const maxTemp3 = maxTempThree.toFixed(0.1)
+        document.getElementById("hour3").innerHTML += `<p>${minTemp3} ° / ${maxTemp3} °C </p>`
     })
