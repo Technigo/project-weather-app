1c506f956b7eb975bd7ef6c644800869413d847f
ucha3i <jchabik@gmail.com>
2019-10-30 17:34:17 +0100

    back to step 3 and a lot of comments about step 4

--- weather.js
+++ weather.js
@@ -1,26 +1,32 @@
 const container = document.getElementById('weather')
 
-/* fetch('http://api.openweathermap.org/data/2.5/weather?q=Stockholm,Sweden&units=metric&APPID=ead60d2e1c3fff29cccec12bd6a43922')
+// WEATHER FOR TODAY IN STOCKHOLM
+
+fetch('http://api.openweathermap.org/data/2.5/weather?q=Stockholm,Sweden&units=metric&APPID=ead60d2e1c3fff29cccec12bd6a43922')
     .then((response) => {
         return response.json()
     })
     .then((json) => {
+
+        // city
         console.log(json)
         container.innerHTML = `<h1>Current weather in ${json.name}</h2>`
 
+        // temperature
         const temp = Math.round(+json.main.temp)
-
         container.innerHTML += `<p> The temperature is ${temp} °C.</p>`
 
+
+        // description
         json.weather.forEach((element) => {
             container.innerHTML += `<p> The overal weather: ${element.description}. </p>`
         })
 
-        //Declare variable for the time of sunrise/sunset and get them in hours:minutes:seconds GMT
+        //time of sunrise/sunset in hours:minutes:seconds GMT
         const sunrise = new Date(json.sys.sunrise * 1000);
         const sunset = new Date(json.sys.sunset * 1000)
 
-        //Declare new variable to show only hh:mm
+        //time and sunrise as hh:mm
         const sunriseShort = sunrise.toLocaleTimeString([], { timeStyle: 'short' })
         const sunsetShort = sunset.toLocaleTimeString([], { timeStyle: 'short' })
 
@@ -29,49 +35,76 @@ const container = document.getElementById('weather')
     })
     .catch((err) => {
         console.log('caught errors', err)
-    }) */
+    })
+
+// WEATHER FORECAST IN LISBON - TRIAL AND ERROR, BUT MOSTLY ERRORS
+
+/* const handle5daysforecast = (json) => {
+    const dates = {}
+
+    json.list.forEach((weather) => {
+        const date = weather.dt_txt.split(' ')[0]
+        if (dates[date]) {
+            dates[date].push(weather)
+        } else {
+            dates[date] = [weather]
+        }
+    })
+}
+
+console.log('dates')
+
+AND IT DOESN'T PRINT OUT DATES, UNFORTUNATELY
+
+Object.entries('dates').forEach((item) => {
+    const date = item[0]
+    const weatherValues = item[1]
+
+    const temps = weatherValues.map((value) => {
+        return value.main.temp
+    })
+    const minTemp = Math.min(...temps)
+})
 
 fetch('http://api.openweathermap.org/data/2.5/forecast?q=Lisbon,Portugal&units=metric&APPID=ead60d2e1c3fff29cccec12bd6a43922')
+    .then((res) => res.json())
+    .then(handle5daysforecast)
+    .catch((err) => {
+        console.log('caught errors', err)
+    })
+
+ */
+/* fetch('http://api.openweathermap.org/data/2.5/forecast?q=Lisbon,Portugal&units=metric&APPID=ead60d2e1c3fff29cccec12bd6a43922')
     .then((response) => {
         return response.json()
     })
     .then((json) => {
         console.log(json)
-        container.innerHTML = `<h1>Current weather in ${json.city.name}</h2>`
+        container.innerHTML = `<h1>Current weather in ${json.city.name}</h2>` */
 
-        // // TO DZIAŁA DLA POJEDYNCZEGO DNIA, ALE NIE DLA PROGNOZY
+//Declare variable for the time of sunrise/sunset and get them in hours:minutes:seconds GMT
+/*        const sunrise = new Date(json.city.sunrise * 1000);
+       const sunset = new Date(json.city.sunset * 1000) */
 
-        /*  const temp = Math.round(+json.main.temp)
-         container.innerHTML += `<p> The temperature is ${temp} °C.</p>` */
+//Declare new variable to show only hh:mm
+/*        const sunriseShort = sunrise.toLocaleTimeString([], { timeStyle: 'short' })
+       const sunsetShort = sunset.toLocaleTimeString([], { timeStyle: 'short' })
 
-        // TU WYWALA MI BUGA, BO NIE MOZE ZNALEZC ELEMENTU WEATHER W PIERWSZEJ ARRAY
+       container.innerHTML += `<p> Sunrise ${sunriseShort} AM.</p>`
+       container.innerHTML += `<p> Sunset ${sunsetShort} PM.</p>` */
 
-        /*  json.list[""].weather.forEach((element) => {
-             container.innerHTML += `<p> The overal weather: ${element.description}. </p>`
-         }) */
+// MATH.ROUND WORKS ONLY FOR SINGLE DAY
 
-        json.list.forEach((day) => {
-            container.innerHTML += `<p> The overal weather: ${day.weather[0].description}. </p>`
-        })
+/*  const temp = Math.round(+json.main.temp)
+ container.innerHTML += `<p> The temperature is ${temp} °C.</p>` */
 
-        // TO DZIAŁA DLA POJEDYNCZEGO DNIA, ALE NIE DLA PROGNOZY
+// DESCRIPTION FROM ALL ARRAYS
 
-        /* json.weather.forEach((element) => {
-            container.innerHTML += `<p> The overal weather: ${element.description}. </p>`
-        }) */
-
-
-        //Declare variable for the time of sunrise/sunset and get them in hours:minutes:seconds GMT
-        const sunrise = new Date(json.city.sunrise * 1000);
-        const sunset = new Date(json.city.sunset * 1000)
-
-        //Declare new variable to show only hh:mm
-        const sunriseShort = sunrise.toLocaleTimeString([], { timeStyle: 'short' })
-        const sunsetShort = sunset.toLocaleTimeString([], { timeStyle: 'short' })
-
-        container.innerHTML += `<p> Sunrise ${sunriseShort} AM.</p>`
-        container.innerHTML += `<p> Sunset ${sunsetShort} PM.</p>`
+/*     json.list.forEach((day) => {
+        container.innerHTML += `<p> The overall weather: ${day.weather[0].description}. </p>`
     })
-    .catch((err) => {
-        console.log('caught errors', err)
-    })
+
+}) */
+/* .catch((err) => {
+    console.log('caught errors', err)
+}) */
