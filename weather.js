47972ced619e46dc58f0e12da049fece5c1a75a4
ucha3i <jchabik@gmail.com>
2019-10-30 11:45:48 +0100

    Since I broke the last version, this is new one, also I fixed sunrise and sunset, and add Wanaka in comment for future app.

--- weather.js
+++ weather.js
@@ -7,12 +7,40 @@ fetch('http://api.openweathermap.org/data/2.5/weather?q=Stockholm,Sweden&units=m
     .then((json) => {
         console.log(json)
         container.innerHTML = `<h1>Current weater in ${json.name}</h2>`
+        container.innerHTML += `<p> The temperature is ${json.main.temp}. </p>`
 
         json.weather.forEach((element) => {
-            container.innerHTML += `<p>The weather in Stockholm is ${element.description}.</p>`
+            container.innerHTML += `<p> The overal weather: ${element.description}. </p>`
         })
 
-        container.innerHTML += `<p>The temperature is between ${json.main.temp_max} and ${json.main.temp_min}.</p>`
+        //Declare variable for the time of sunrise/sunset and get them in hours:minutes:seconds GMT
+        const sunrise = new Date(json.sys.sunrise * 1000);
+        const sunset = new Date(json.sys.sunset * 1000)
+
+        //Declare new variable to show only hh:mm
+        const sunriseShort = sunrise.toLocaleTimeString([], { timeStyle: 'short' })
+        const sunsetShort = sunset.toLocaleTimeString([], { timeStyle: 'short' })
+
+        container.innerHTML += `<p> Sunrise ${sunriseShort} </p>`
+        container.innerHTML += `<p> Sunset ${sunsetShort} </p>`
+    })
+    .catch((err) => {
+        console.log('caught errors', err)
+    })
+
+/* fetch('http://api.openweathermap.org/data/2.5/forecast?q=Wanaka,NewZealand&units=metric&APPID=ead60d2e1c3fff29cccec12bd6a43922')
+    .then((response) => {
+        return response.json()
+    })
+    .then((json) => {
+        console.log(json)
+        container.innerHTML = `<h1>Current weater in ${json.city.name}</h2>`
+
+        json.weather.forEach((element) => {
+            container.innerHTML += `<p> The overal weather: ${element.description}. </p>`
+        })
+
+        container.innerHTML += `<p> The temperature is between ${json.main.temp_max} and ${json.main.temp_min}. </p>`
 
         //Declare variable for the time of sunrise/sunset
         const unixTimestampSunrise = json.sys.sunrise
@@ -31,4 +59,4 @@ fetch('http://api.openweathermap.org/data/2.5/weather?q=Stockholm,Sweden&units=m
     })
     .catch((err) => {
         console.log('caught errors', err)
-    })
+    }) */
