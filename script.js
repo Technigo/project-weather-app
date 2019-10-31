b9649646f864a790f1438695d51fb3cd799f3d3b
Frida Jonsson <annafridamariajonsson@gmail.com>
2019-10-31 11:13:36 +0100

    Step 3 Sunrise & sunset

--- script.js
+++ script.js
@@ -2,6 +2,8 @@ const container = document.getElementById('stockholm')
 const place = document.getElementById('location')
 const weather = document.getElementById('weather')
 const temp = document.getElementById('temp')
+const sunrise = document.getElementById('sunrise')
+const sunset = document.getElementById('sunset')
 
 
 fetch('http://api.openweathermap.org/data/2.5/weather?q=Stockholm,Sweden&units=metric&APPID=136131579df2be1e6059803ccc6e55b2')
@@ -13,8 +15,19 @@ fetch('http://api.openweathermap.org/data/2.5/weather?q=Stockholm,Sweden&units=m
     console.log(json)
 
     place.innerHTML = (json.name)
-    weather.innerHTML = (json.weather[0].description)
     temp.innerHTML = (json.main.temp)
+    weather.innerHTML = (json.weather[0].description)
 
-    
+    //Declare variable for the time of sunrise/sunset and get them in hours:minutes:seconds GMT
+    const sunriseCalc = new Date(json.sys.sunrise * 1000);
+    const sunsetCalc = new Date(json.sys.sunset * 1000)
+
+    //Declare new variable to show only hh:mm
+    const sunriseTime = sunriseCalc.toLocaleTimeString([], { timeStyle: 'short' })
+    const sunsetTime = sunsetCalc.toLocaleTimeString([], { timeStyle: 'short' })
+
+    sunrise.innerHTML = (sunriseTime)
+    sunset.innerHTML = (sunsetTime)
+        
     })
+    
