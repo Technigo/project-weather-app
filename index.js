de1685bb68e89e31bcc6ae4a98874ccc7a6caeaf
nasimmhn <nasim.mahzoun@gmail.com>
2019-10-31 22:47:00 +0100

    added icons and styling

--- index.js
+++ index.js
@@ -1,17 +1,18 @@
 const city = document.getElementById("city");
-const weather = document.getElementById("weather");
+const weather_icon = document.getElementById("weather_icon");
 const temp = document.getElementById("temp");
 const sunrise = document.getElementById("sunrise");
 const sunset = document.getElementById("sunset");
+const weekdaysDiv = document.getElementById("weekdays")
 
 const weekdays = [
-  "Sunday",
-  "Monday",
-  "Tuesday",
-  "Wednesday",
-  "Thursday",
-  "Friday",
-  "Saturday"
+  "Sun",
+  "Mon",
+  "Tue",
+  "Wed",
+  "Thu",
+  "Fri",
+  "Sat"
 ];
 
 // http://api.openweathermap.org/data/2.5/find?q=New York,US&units=metric&APPID=da417ece928b0291f06b75fca4777157
@@ -28,8 +29,9 @@ fetch(
     console.log(json);
 
     city.innerHTML = json.city.name;
-    weather.innerHTML = json.list[0].weather[0].main;
-    temp.innerHTML = `${Math.round(json.list[0].main.temp)}&deg;C`;
+    weather_icon.src = `http://openweathermap.org/img/wn/${json.list[0].weather[0].icon}@2x.png`
+    //json.list[0].weather[0].main;
+    temp.innerHTML = `${getNumber(Math.round(json.list[0].main.temp))}&deg;C`;
 
     sunrise.innerHTML = `${("0" + sunRise.getHours()).slice(-2)}:${("0" + sunRise.getMinutes()).slice(-2)}`;
     sunset.innerHTML = `${("0" + sunSet.getHours()).slice(-2)}:${("0" + sunSet.getMinutes()).slice(-2)}`;
@@ -38,8 +40,21 @@ fetch(
       let date = new Date(day.dt_txt);
       if (date.getHours() != "12") return;
 
-      document.getElementById("weekdays").innerHTML += `<div class="week_days"> ${weekdays[date.getDay()]} ${Math.round(day.main.temp)}&deg;C </div>`;
+      weekdaysDiv.innerHTML += `<div class="week_days"> 
+                                  <div>${weekdays[date.getDay()]}</div>
+                                  <img src="http://openweathermap.org/img/wn/${day.weather[0].icon}.png">
+                                  <div>${getNumber(Math.round(day.main.temp))}&deg;C </div>
+                                </div>`;
 
       //container.innerHTML += `<p>${person.name} is on the ${person.craft} </p>`
     });
   });
+
+//  Function to add a '+' in front of positive numbers
+const getNumber = (theNumber) => {
+  if (theNumber > 0) {
+    return "+" + theNumber;
+  } else {
+    return theNumber.toString();
+  }
+}
