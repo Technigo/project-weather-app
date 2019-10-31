f2ee6e481356b8535a427c5177f8ca98a61764fc
Frida Jonsson <annafridamariajonsson@gmail.com>
2019-10-31 16:00:16 +0100

    Added forecast, group items in arrays per day, calculate max & min temps

--- script.js
+++ script.js
@@ -4,9 +4,57 @@ const weather = document.getElementById('weather')
 const temp = document.getElementById('temp')
 const sunrise = document.getElementById('sunrise')
 const sunset = document.getElementById('sunset')
+const forecast = document.getElementById('forecast')
+
+// FORECAST functions
+
+//group items in list
+const handle5DayForecast = (json) => {
+    console.log(json)
+    const dates = {}
+    //iterate over items in list
+    json.list.forEach((weather) => {
+        // remove time stamp from date
+        const date = weather.dt_txt.split(' ')[0]
+        //if date is in the array, add weather
+        if (dates[date]) {
+            dates[date].push(weather)
+
+        } //if date is not in array, add object
+        else {
+            dates[date] = [weather]
+        }
+    }) 
+    
+    //iterate over each day / objects (turning object into array) (dates is the object?)
+    Object.entries(dates).forEach((item, index) => {
+        if (index === 0) {
+            return
+        }
+        const date = item[0]
+        const weatherValues = item[1]
+
+        //calculate (temp) values        
+        const temps = weatherValues.map((value) => value.main.temp)
+
+        //use spread ... to turn array into numbers
+        const minTemp = Math.min(...temps)
+        const maxTemp = Math.max(...temps)
+
+        forecast.innerHTML += `<li>${date} -  ${minTemp.toFixed(0)} to ${maxTemp.toFixed(0)}°</li>`
+    })
+}
+
+//create day from date
+const date = new Date()
+const dayNames = ['Sunday', 'Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday']
+console.log(dayNames[date.getDay()])
 
 
-fetch('http://api.openweathermap.org/data/2.5/weather?q=Stockholm,Sweden&units=metric&APPID=136131579df2be1e6059803ccc6e55b2')
+        
+
+    // TODAYS weather
+fetch('http://api.openweathermap.org/data/2.5/weather?q=Stockholm,Sweden&units=metric&APPID=3234696c6c8945542b6d5204a0d55386')
 .then ((response) => {
     return response.json()
 })
@@ -26,8 +74,18 @@ fetch('http://api.openweathermap.org/data/2.5/weather?q=Stockholm,Sweden&units=m
     const sunriseTime = sunriseCalc.toLocaleTimeString([], { timeStyle: 'short' })
     const sunsetTime = sunsetCalc.toLocaleTimeString([], { timeStyle: 'short' })
 
-    sunrise.innerHTML = (sunriseTime)
+    sunrise.innerHTML += (sunriseTime)
     sunset.innerHTML = (sunsetTime)
         
     })
+
+    //FORECAST
+
+fetch('http://api.openweathermap.org/data/2.5/forecast?q=Stockholm,Sweden&units=metric&cnt=40&APPID=3234696c6c8945542b6d5204a0d55386')
     
+.then ((response) => {
+    return response.json()
+
+})
+
+.then (handle5DayForecast)
