71ccc4db940521a73386296114e325db3e4a2f84
Michel Elmoznino Laufer <mitchel_85@hotmail.com>
2019-11-01 11:59:52 +0100

    Stretch goals, Design: Changing the colors

--- script.js
+++ script.js
@@ -2,18 +2,26 @@ const container = document.getElementById("weatherToday")
 const containerCity = document.getElementById("weatherCity")
 const containerWeekly = document.getElementById("weatherWeekly")
 
-fetch('http://api.openweathermap.org/data/2.5/weather?q=Stockholm,Sweden&units=metric&APPID=60a9f45b45a7b39b290afa28477d7241')
+
+fetch('https://api.openweathermap.org/data/2.5/weather?q=Stockholm,Sweden&units=metric&APPID=60a9f45b45a7b39b290afa28477d7241')
   .then((response) => {
     return response.json()
   })
   .then((json) => {
     console.log(json)
+    let mainTemp = json.main.temp.toFixed(1);
     let sunriseHour = new Date((json.sys.sunrise)*1000).toLocaleTimeString([], {hour: '2-digit', minute: '2-digit'})
     let sunsetHour = new Date((json.sys.sunset)*1000).toLocaleTimeString([], {hour: '2-digit', minute: '2-digit'})
-    console.log(`Sunrise ${sunriseHour}, Sunset ${sunsetHour}`)
+    console.log(`Main temp ${mainTemp} Sunrise ${sunriseHour}, Sunset ${sunsetHour}`)
 
-    container.innerHTML = `${json.weather[0].description} | ${json.main.temp.toFixed(1)} &#8451 
-    <p>sunrise ${sunriseHour}</p> <p>sunset ${sunsetHour}</p>`
+    if(mainTemp < 10) {
+      document.body.style.background = 'linear-gradient(to right, rgba(97, 153, 153, 0), rgb(50, 162, 226))' 
+    } else {
+      document.body.style.background = 'linear-gradient(to right, rgba(228, 200, 45, 0), rgb(226, 200, 50))'
+    }
+
+    container.innerHTML = `${json.weather[0].description} | ${mainTemp} &#8451 
+    <p id="infoTop">sunrise ${sunriseHour}</p> <p id="infoTop">sunset ${sunsetHour}</p>`
     
     containerCity.innerHTML = `<h2>${json.name}</h2>`
   })
@@ -25,7 +33,7 @@ fetch('http://api.openweathermap.org/data/2.5/weather?q=Stockholm,Sweden&units=m
   console.log(json)
 
   json.list.forEach((weather) => {
-    let weekDay = new Date((weather.dt)*1000).toLocaleDateString([], {weekday: 'long'})
+    let weekDay = new Date((weather.dt)*1000).toLocaleDateString("en-US", {weekday: 'short'})
     if (weekDays[weekDay]) {
       weekDays[weekDay].push(weather)
     } else {
@@ -37,7 +45,7 @@ fetch('http://api.openweathermap.org/data/2.5/weather?q=Stockholm,Sweden&units=m
     if (index === 0) {
       return 
     }
-
+    
     let weekDay = item[0]
     const weatherValues = item[1]
 
@@ -50,48 +58,6 @@ fetch('http://api.openweathermap.org/data/2.5/weather?q=Stockholm,Sweden&units=m
   })
 }
 
-fetch('http://api.openweathermap.org/data/2.5/forecast?q=Stockholm,Sweden&units=metric&cnt=39&APPID=60a9f45b45a7b39b290afa28477d7241')
+fetch('https://api.openweathermap.org/data/2.5/forecast?q=Stockholm,Sweden&units=metric&cnt=39&APPID=60a9f45b45a7b39b290afa28477d7241')
   .then((res) => res.json())
   .then(handle5DayForecast)
-
-
-
-  // fetch('http://api.openweathermap.org/data/2.5/forecast?q=Stockholm,Sweden&units=metric&cnt=15&APPID=60a9f45b45a7b39b290afa28477d7241')
-  // .then((response) => {
-  //   return response.json()
-  // })
-  // .then((json) => {
-  //   console.log(json)
-  //   console.log(json.list[0].main.temp)
-  //   // let currentDay = new Date((json.list[1].dt)*1000).toLocaleDateString([], {weekday: 'long'})
-  //   // console.log(currentDay)
-  //   let currentDay;
-  //   let lowestTemp=json.list[0].main.temp_min; 
-  //   let currTempLow=0;
-  //   let highestTemp=json.list[0].main.temp_max;
-  //   let currTempHigh=0;
-    
-  //   for(let i=1; i<json.list.length; i++) {
-  //     currentDay = new Date((json.list[i].dt)*1000).toLocaleDateString([], {weekday: 'long'})
-  //     currTempLow=json.list[i].main.temp_min
-  //     console.log(`${currentDay} min temp: ${currTempLow}`)
-
-  //     if(currTempLow < lowestTemp) {
-  //       lowestTemp = currTempLow
-  //     } 
-  //   }
-    
-  //   for(let i=1; i < json.list.length; i++) {
-  //     currTempHigh = json.list[i].main.temp_max
-  //     console.log(`${currentDay} max temp: ${currTempHigh}`)
-    
-  //   if(currTempHigh > highestTemp) {
-  //     highestTemp = currTempHigh
-  //   } 
-
-  //   currentDay = new Date((json.list[i].dt)*1000).toLocaleDateString([], {weekday: 'long'})
-  //   }
-
-  //   containerWeekly.innerHTML += `<p>Min: ${lowestTemp} &#8451 </p>
-  //   <p> Max: ${highestTemp} &#8451`
-  // })
