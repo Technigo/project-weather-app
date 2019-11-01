ed7c31a62a5e8e5dfe5914b879dc7d479db24c6e
Disa de Beau <Disan@Disas-MacBook-Air.local>
2019-11-01 22:23:01 +0100

    Step 4, but not working for desktop

--- script.js
+++ script.js
@@ -1,21 +1,76 @@
+"use strict";
 
-
+const apiKey = "e57c9fd6de5974ecd857b3b40415a881"
+// const location = "Alicante, ES"
 
 const container = document.getElementById("weatherHeadline")
+const container2 = document.getElementById("temperature")
+const container3 = document.getElementById("sunriseSunset")
 
-fetch("http://api.openweathermap.org/data/2.5/weather?q=Stockholm,Sweden&units=metric&APPID=e57c9fd6de5974ecd857b3b40415a881")
+
+
+
+fetch("http://api.openweathermap.org/data/2.5/weather?q=Alicante,es&units=metric&APPID=e57c9fd6de5974ecd857b3b40415a881")
   .then((response) => {
     return response.json()
   })
 
-
-  // .then((json) => {
-  //   console.log(json)
-  // })
-
   .then((json) => {
-    container.innerHTML = `<h1>The weather in Stockholm right now is ${json.weather[Description]}</h1 > `
+    container.innerHTML = `<h1>${json.name}: <br>
+    ${json.weather[0]["description"]}</h1 >`
+
+    container2.innerHTML = `<h2> Temperature ${json.main.temp.toFixed(0)} C°</h2>`
+
+    const unixTimestampSunrise = json.sys.sunrise
+    const unixTimestampSunset = json.sys.sunset
+
+    const sunrise = new Date(unixTimestampSunrise * 1000)
+    const sunset = new Date(unixTimestampSunset * 1000)
+
+    const sunriseTime = sunrise.toLocaleTimeString([], { timeStyle: 'short' })
+    const sunsetTime = sunset.toLocaleTimeString([], { timeStyle: 'short' })
+
+    container3.innerHTML = `<h3> Sunrise ${sunriseTime} Sunset ${sunsetTime}</h3>`
+
+
+
 
   })
 
 
+// *** 5 day prognosis *** //
+
+const handle5DayForecast = (json) => {
+  const forecastDiv = document.getElementById("forecast")
+  const dates = {}
+
+  json.list.forEach((weather) => {
+    const date = weather.dt_txt.split(' ')[0]
+    if (dates[date]) {
+      dates[date].push(weather)
+    } else {
+      dates[date] = [weather]
+    }
+  })
+
+  Object.entries(dates).forEach((item, index) => {
+    if (index === 0) {
+      return
+    }
+
+    const date = item[0]
+    const weatherValues = item[1]
+
+    const temps = weatherValues.map((value) => value.main.temp)
+    const minTemp = Math.min(...temps)
+    const maxTemp = Math.max(...temps)
+
+    forecastDiv.innerHTML += `<li> ${date} - min: ${minTemp.toFixed(0)}, max: ${maxTemp.toFixed(0)}</li > `
+  })
+
+
+}
+
+fetch(`https://api.openweathermap.org/data/2.5/forecast?q=Alicante, Spain&appid=${apiKey}&units=metric`)
+  .then((res) => res.json())
+  .then(handle5DayForecast)
