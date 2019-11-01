f269e0c7305f07908b852b80a85c5486693ea412
Karol Cislowski <cislowski.karol@hotmail.com>
2019-11-02 00:12:37 +0100

    Commenting JS file

--- assets/js/index.js
+++ assets/js/index.js
@@ -14,12 +14,14 @@ fetch(currentWeatherAPI)
     return response.json()
   })
   .then((json) => {
+    //Creating the weather fields and populate them with data from current weather API
     weather.temp = (Math.round(json.main.temp * 10) / 10).toFixed(1)
     weather.city = json.name
     weather.id = json.weather[0].id
     weather.sunrise = new Date(json.sys.sunrise * 1000).toLocaleTimeString([], { timeStyle: 'short' })
     weather.sunset = new Date(json.sys.sunset * 1000).toLocaleTimeString([], { timeStyle: 'short' })
     weather.day = new Date(json.dt * 1000).getDay()
+    //Building DOM for Current Weather
     printWeather()
   })
 
@@ -28,13 +30,16 @@ fetch(forecastWeatherAPI)
     return response.json()
   })
   .then((json) => {
+    //Building array of single days 
     let index = 0
     while (index < json.list.length) {
+      //Adding amount of used items to build day object to index to skip no 1 item but all items from that day
       index += createForecast(index, json)
     }
     printForecast()
   })
 
+//Template for current weather section
 const printWeather = () => {
   theCurrent.innerHTML = ` 
     <div class="current-top">
@@ -59,29 +64,38 @@ const printWeather = () => {
   `
 }
 
+//Creating every single day and adding to array 
 const createForecast = (index, json) => {
+  //Extracting day from json
   let day = new Date(json.list[index].dt * 1000).getDay()
+  //Creating min and max temperature from first item for that day
   let minTemp = json.list[index].main.temp_min
   let maxTemp = json.list[index].main.temp_max
+  //Looping thru all item for that day 
   let i = 0;
   while ((index + i) < json.list.length && (new Date(json.list[index + i].dt * 1000).getDay()) === day) {
+    //Finding minimum temperature
     if (minTemp > json.list[index + i].main.temp_min) {
       minTemp = json.list[index + i].main.temp_min
     }
+    //Finding maximum temperature
     if (minTemp > json.list[index + i].main.temp_min) {
       minTemp = json.list[index + i].main.temp_min
     }
     i++
   }
+  //Building object and adding it to array
   forecast.push({
     day: day,
     minTemp: (Math.round(minTemp * 10) / 10).toFixed(1),
     maxTemp: (Math.round(maxTemp * 10) / 10).toFixed(1),
     id: json.list[index+Math.floor(i/2)].weather[0].id
   })
+  //Returning how meny items from list array we used to build that day object 
   return i
 }
 
+//Template for weather forecast section
 const printForecast = () => {
   theForecast.innerHTML = `
     ${forecast.map((day) => {
