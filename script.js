3ff2fcac115e2c86a9b75f375c6b700085e4863a
SaraBorg <50766676+SaraBorg@users.noreply.github.com>
2019-11-01 10:20:45 +0100

    Added the forecast and trying to change to days

--- script.js
+++ script.js
@@ -32,7 +32,6 @@ const snow {
 } */
 
 
-
 fetch('http://api.openweathermap.org/data/2.5/weather?q=Stockholm,Sweden&units=metric&APPID=80c5dd84564bfbfbbae5184faea61c48')
   .then((response) => {
     return response.json()
@@ -51,41 +50,48 @@ fetch('http://api.openweathermap.org/data/2.5/weather?q=Stockholm,Sweden&units=m
       container.innerHTML = `${weatherType.description}`
     })
     container.innerHTML += `<h1>Todays weather in: ${json.name}</h1>`
-    container.innerHTML += `<p>Temp is ${json.main.temp.toFixed(1)}</p>`
+    container.innerHTML += `<p>Temp is ${json.main.temp.toFixed(1)} &#176</p>`
     container.innerHTML += `<p>Sunrise: ${sunriseTime}</p>`
     container.innerHTML += `<p>Sunset: ${sunsetTime}</p>`
   })
 
 
-const handle5DaysForecast = (json) => {
-  const forecastDiv = document.getElementById('forecast')
+const handle5DayForecast = json => {
+  const forecastDiv = document.getElementById("forecast")
   const dates = {}
-
-  json.list.forEach((weather) => {
-    const date = wather.dt_txt.split('')[0]
+  json.list.forEach(weather => {
+    const date = weather.dt_txt.split(" ")[0]
     if (dates[date]) {
       dates[date].push(weather)
     } else {
       dates[date] = [weather]
     }
   })
-
-  object.entries(dates).forEach((item, index) => {
+  Object.entries(dates).forEach((item, index) => {
     if (index === 0) {
       return
     }
+
+
     const date = item[0]
     const weatherValues = item[1]
-
-    const temps = weatherValues.map((value) => value.main.temp)
+    const temps = weatherValues.map(value => value.main.temp)
     const minTemp = Math.min(...temps)
     const maxTemp = Math.max(...temps)
 
-    forecastDiv.innerHTML = `<li>${date} - min: ${minTemp.toFixed(1)}, max: ${maxTemp(1)}</li>`
+    /*     const date = new Date()
+        const dayName = ['Sun', 'Mon', 'Tue', 'Wed', 'Thu', 'Fri', 'Sat']
+
+        console.log(dayName[date.getDay()]) */
+
+    forecastDiv.innerHTML += `<li>${date} - min: ${minTemp.toFixed(
+        1
+      )}, max: ${maxTemp.toFixed(1)}</li>`
+
   })
 }
 
 
 fetch('http://api.openweathermap.org/data/2.5/forecast?id=2673730&APPID=80c5dd84564bfbfbbae5184faea61c48')
   .then((res) => res.json())
-  .then(handle5DaysForecast)
+  .then(handle5DayForecast)
