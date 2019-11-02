ab67549cf3e66f7a8f9bfd3fa1f6c688e65fc48e
AceTircevski <47758694+AceTircevski@users.noreply.github.com>
2019-11-02 15:50:52 +0100

    javascript work

--- script.js
+++ script.js
@@ -1,8 +1,15 @@
 "use strict";
 
 const api_key = "3927ba6963ab68cfceebff54c1ee693f";
-const container = document.getElementById("weather");
+const container = document.getElementById("today");
+
 const days = ["Sun", "Mon", "Tues", "Wed", "Thurs", "Fri", "Sat"];
+
+const weathers = {
+  Cloudy: "noun_weather.png",
+  Sunny: "noun_weather.png",
+  Drizzle: "noun_weather.png"
+};
 
 fetch(
   `http://api.openweathermap.org/data/2.5/weather?q=Stockholm,Sweden&units=metric&APPID=${api_key}`
@@ -13,23 +20,30 @@ fetch(
   .then(json => {
     console.log(json);
 
-    let sunriseHour = new Date(json.sys.sunrise * 1000).toLocaleTimeString([], {
-      hour: "2-digit",
-      minute: "2-digit"
-    });
-    let sunsetHour = new Date(json.sys.sunset * 1000).toLocaleTimeString([], {
+    const sunriseHour = new Date(json.sys.sunrise * 1000).toLocaleTimeString(
+      [],
+      {
+        hour: "2-digit",
+        minute: "2-digit"
+      }
+    );
+    const sunsetHour = new Date(json.sys.sunset * 1000).toLocaleTimeString([], {
       hour: "2-digit",
       minute: "2-digit"
     });
 
     console.log(`Sunrise: ${sunriseHour}, Sunset: ${sunsetHour}`);
 
-    container.innerHTML = `<h2>Todays weather in Stockholm  ${json.main.temp} degrees with a daily min of ${json.main.temp_min} and daily max of
-   ${json.main.temp_max} ,  ${sunriseHour} ${sunsetHour}</h2>`;
+    container.innerHTML = `<h1>Today's weather in: </h1> <h1 id="location">${
+      json.name
+    }</h1>  <h2>${json.main.temp}&#8451;</h2><img src="${
+      weathers[json.weather[0].main]
+    }" />   <h3 id="todaysMinMax">${json.main.temp_min}&#8451; /
+   ${json.main.temp_max}&#8451;</h3>	<h3>${sunriseHour} ${sunsetHour}</h3>`;
   });
 
 const handle5DayForecast = json => {
-  const forecastDiv = document.getElementById("forecast");
+  const forecastDiv = document.getElementById("forecastdates");
   const dates = {};
 
   json.list.forEach(weather => {
@@ -53,9 +67,9 @@ const handle5DayForecast = json => {
     const minTemp = Math.min(...temps);
     const maxTemp = Math.max(...temps);
 
-    forecastDiv.innerHTML += `<li>${date} - min: ${minTemp.toFixed(
+    forecastDiv.innerHTML += `<h3><li>${date} - min: ${minTemp.toFixed(
       1
-    )}, max: ${maxTemp.toFixed(1)}</li>`;
+    )}, max: ${maxTemp.toFixed(1)}</li></h3>`;
   });
 };
 
