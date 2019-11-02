75091b8c9ea1a1e79712c1315aa3b611d87281d0
Linda <isell.linda@gmail.com>
2019-11-02 22:58:31 +0100

    added style and footer and changed of background when night and day

--- script1.js
+++ script1.js
@@ -5,16 +5,11 @@ const temperatureMinMax = document.getElementById("todaysTempMinMax");
 const locationTimezone = document.getElementById("location-timezone");
 const theSunset = document.getElementById("sunset-time");
 const theSunrise = document.getElementById("sunrise-time");
-
-// SECOND API LINK - Forecast 5 days
-const theDayDate = document.getElementById("day-date");
-const theDayTemp = document.getElementById("day-temp");
-
-const forecastDiv = document.getElementById('forecast-section')
-
-// TESTING ICON PICTURE
 const theCurrentIcon = document.getElementById("currentIcon");
 
+// SECOND API LINK - Forecast 5 days
+
+const forecastDiv = document.getElementById('forecast-section')
 
 
 fetch("https://api.openweathermap.org/data/2.5/weather?q=Malm%C3%B6&units=metric&APPID=8cbd9193bf1986e2387d169ac2d73a9e")
@@ -25,27 +20,37 @@ fetch("https://api.openweathermap.org/data/2.5/weather?q=Malm%C3%B6&units=metric
         console.log(json);
         locationTimezone.innerHTML = json.name
         temperatureDegree.innerHTML = json.main.temp.toFixed(1) + " °C"
-        temperatureMinMax.innerHTML = `min ${json.main.temp_min.toFixed(1)} °C / max ${json.main.temp_max.toFixed(1)} °C`
-        temperatureDescription.innerHTML = `It's ${json.weather[0].description}`
+        temperatureMinMax.innerHTML = `min ${json.main.temp_min.toFixed(1)} °C | max ${json.main.temp_max.toFixed(1)} °C`
+        temperatureDescription.innerHTML = `It's ${json.weather[0].description} in`
         theCurrentIcon.innerHTML = `<img src="https://openweathermap.org/img/wn/${json.weather[0].icon}@2x.png"  alt="icon for weather" />`
 
         let theSunriseTime = new Date(json.sys.sunrise * 1000).toLocaleTimeString([], { hour: '2-digit', minute: '2-digit' })
         let theSunsetTime = new Date(json.sys.sunset * 1000).toLocaleTimeString([], { hour: '2-digit', minute: '2-digit' })
 
-        theSunset.innerHTML = `The sun goes down: ${theSunsetTime}`
-        theSunrise.innerHTML = `The sun rises: ${theSunriseTime}`
+        theSunrise.innerHTML = `Sunrise ${theSunriseTime}`
+        theSunset.innerHTML = `Sunset ${theSunsetTime}`
+
 
         // Change background color depending on if its hot or cold temperature. 
         //less than 10 degree = blue background. 11-20 degree green background. 20+ yellow backround.
-        let todaysTemp = json.main.temp.toFixed(1)
+        // let todaysTemp = json.main.temp.toFixed(1)
+        // const theBodyBackground = document.getElementById("backgroundColor")
+        // if (todaysTemp <= 15) {
+        //     theBodyBackground.style.background = "linear-gradient(to right bottom, #065fa3, #0d74ae, #2a88b6, #499cbd, #68afc4)"
+        // } else {
+        //     theBodyBackground.style.background = "linear-gradient(to right bottom, #dc831e, #e29929, #e7af37, #ebc548, #eeda5c)"
+        // }
+
+        // CHANGE BACKGROUND NIGHT/DAY
         const theBodyBackground = document.getElementById("backgroundColor")
-        if (todaysTemp <= 10) {
-            theBodyBackground.style.background = "linear-gradient(to right bottom, #065fa3, #0d74ae, #2a88b6, #499cbd, #68afc4)"
-        } else if (todaysTemp >= 11 && todaysTemp <= 20) {
-            theBodyBackground.style.background = "linear-gradient(to right bottom, #157116, #3a8e30, #5aac4a, #79ca64, #99ea7f)"
-        } else {
+        let todayTime = new Date()
+        let timeNow = todayTime.getTime();
+        if (timeNow >= theSunrise && timeNow <= theSunset) {
             theBodyBackground.style.background = "linear-gradient(to right bottom, #dc831e, #e29929, #e7af37, #ebc548, #eeda5c)"
+        } else {
+            theBodyBackground.style.background = "linear-gradient(to bottom, #103554, #12426a, #145081, #175e98, #1c6cb0)"
         }
+
     });
 
 // FORECAST SECTION
@@ -89,7 +94,12 @@ const handle5DayForecast = (json) => {
         const descriptions = weatherValues[0].weather[0].description
         const icons = weatherValues[0].weather[0].icon
 
-        forecastDiv.innerHTML += `<li>${weekdayName} - min: ${minTemp.toFixed(1)} °C, max: ${maxTemp.toFixed(1)} °C <img src="https://openweathermap.org/img/wn/${weatherValues[0].weather[0].icon}@2x.png" id="icon" alt="icons for weather" /> </li>`
+        forecastDiv.innerHTML +=
+            `<ul>
+        <li id="weekdays">${weekdayName}</li>  
+        <li id="icon-images"><img src="https://openweathermap.org/img/wn/${weatherValues[0].weather[0].icon}@2x.png" id="icon" alt="icons for weather" /> </li>
+        <li id="minmax-Temp"> min: ${minTemp.toFixed(1)} °C / max: ${maxTemp.toFixed(1)} °C</li>
+        </ul>`
     })
 }
 
