352bbcdc7647751f750b2bca360a0e29a255874c
microlinamedia <evalisa.saksi@gmail.com>
2019-11-03 22:56:21 +0100

    weather app changes 2 sunriseandsunset

--- Code/script.js
+++ Code/script.js
@@ -1,7 +1,6 @@
 const theCity = document.getElementById("city")
 const theTemperature = document.getElementById("temperature")
-const theTodaysWeather = document.getElementById("todaysWeather")
-const theWeeklyForecast = document.getElementById("weeklyForecast")
+const thePic = document.getElementById("weatherPic")
 
 fetch(`http://api.openweathermap.org/data/2.5/weather?q=Stockholm,Sweden&units=metric&APPID=0cd5fdd065f4fb92c2718ca6e5aa8e02`)
 
@@ -9,11 +8,27 @@ fetch(`http://api.openweathermap.org/data/2.5/weather?q=Stockholm,Sweden&units=m
     return response.json();
 })
 .then((json) => {
-    console.log(json)
+    
+   const theTodaysWeather = document.getElementById("todaysWeather")
    
+   const unixTimeStampSunrise = json.sys.sunrise
+   const unixTimeStampSunset = json.sys.sunset
+
+   const sunrise = new Date(unixTimeStampSunrise * 1000)
+   console.log()
+   const sunset = new Date(unixTimeStampSunset * 1000)
+   console.log()
+
+   const sunriseHour = sunrise.toLocaleTimeString([], { timeStyle: 'short' })
+   console.log(sunriseHour)
+   const sunsetHour = sunset.toLocaleTimeString([], { timeStyle: 'short'})
+   console.log(sunsetHour)
+
    theCity.innerHTML = `<h2> ${json.name}<h2/>`
    theTemperature.innerHTML += `<h2>${Math.floor(json.main.temp)}°C</h2>`
    theTodaysWeather.innerHTML += `<h2>${json.weather[0].description}</h2>`
+   theTodaysWeather.innerHTML += `<p>${sunriseHour}`
+   theTodaysWeather.innerHTML += `<p>${sunsetHour}`
 
 })
 
