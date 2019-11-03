3e5959e1f7652449d09fa3835f07557588b76255
Jenny Jacobsson <enni@oans.net>
2019-11-03 16:15:30 +0100

    added min/max temp, minor css changes

--- code/script.js
+++ code/script.js
@@ -1,24 +1,19 @@
-const days = [
-  "Sunday",
-  "Monday",
-  "Tuesday",
-  "Wednesday",
-  "Thursday",
-  "Friday",
-  "Saturday"
-];
+
+
+const apiKey = "68b38084465c789f4422e7d217060b68";
+const city = "Stockholm,Sweden";
+
+const days = ["Sunday","Monday","Tuesday","Wednesday","Thursday","Friday","Saturday"];
 
 const containerWeather = document.getElementById("weather");
 const containerTemp = document.getElementById("temp");
 const containersunrise = document.getElementById("sunrise");
 const containersunset = document.getElementById("sunset");
+const forecast = document.getElementById("forecast");
 
-const forecast = document.querySelector("#forecast");
 
 // API for today
-fetch(
-  "https://api.openweathermap.org/data/2.5/weather?q=Stockholm,Sweden&units=metric&APPID=68b38084465c789f4422e7d217060b68"
-)
+fetch(`https://api.openweathermap.org/data/2.5/weather?q=${city}&units=metric&APPID=${apiKey}`)
   .then(response => {
     return response.json();
   })
@@ -28,19 +23,17 @@ fetch(
     const sunset = new Date(json.sys.sunset * 1000);
 
     //Declare new variable to show only hh:mm
-    const sunriseTime = sunrise.toLocaleTimeString([], { timeStyle: "short" });
+    const sunriseTime = sunrise.toLocaleTimeString([], {timeStyle: "short"});
     const sunsetTime = sunset.toLocaleTimeString([], { timeStyle: "short" });
 
     containerWeather.innerHTML = `<p><img src="https://openweathermap.org/img/wn/${json.weather[0].icon}@2x.png"><br>${json.weather[0].main}</p>`;
-    containerTemp.innerHTML = `<p>${Math.round(json.main.temp)} C°</p>`;
+    containerTemp.innerHTML = `<p>Temp: ${Math.round(json.main.temp_max)}°C | ${Math.round(json.main.temp_min)}°C</p>`;
     containersunrise.innerHTML = `<p><img src="sunrise.png">${sunriseTime}</p>`;
     containersunset.innerHTML = `<p><img src="sunset.png">${sunsetTime}</p>`;
   });
 
 /// API for 5 days
-fetch(
-  "https://api.openweathermap.org/data/2.5/forecast?q=Stockholm,Sweden&units=metric&cnt=40&APPID=68b38084465c789f4422e7d217060b68"
-)
+fetch(`https://api.openweathermap.org/data/2.5/forecast?q=${city}&units=metric&cnt=40&APPID=${apiKey}`)
   .then(response => {
     return response.json();
   })
@@ -58,7 +51,7 @@ fetch(
         return;
       }
 
-      // Gives back a number between 0-6 to select a day from the array 'days'
+      currentDay = day;
 
       // Prints out the data we've collected, and divied them into divs and classes.
       forecast.innerHTML +=
@@ -69,8 +62,5 @@ fetch(
         `</div>` +
         `<div class="temp">${Math.round(item.main.temp)}°</div>` +
         `</div>`;
-
-      // Save printed day number
-      currentDay = day;
     });
   });
