2a4255ece84f5db1182f881bb3101d097761d368
tehvicke <vikda574@student.liu.se>
2019-11-03 11:10:05 +0100

    refactored testFunction to  be dynamic and made some global variables for the URLs buildup

--- code/script.js
+++ code/script.js
@@ -1,11 +1,15 @@
 const API_KEY = "ad96db07a123e6ca29acb372e202f428" // Should be put in a file that is not committed to Github but let's skip it for now.
-let cityName = "Stockholm"
-let countryCode = "se"
+const FORECAST_URL = "http://api.openweathermap.org/data/2.5/forecast?q="
+let cityName = "Norsjö"
+const URL_SUFFIX = `&units=metric&appid=${API_KEY}`
 
+// The weather URL is made up of FORECAST_URL + cityName + URL_SUFFIX
+
+let weatherObject
 // function to test the API, not used
-let testFunction = () => {
+let testFunction = (requestTypeUrl) => {
     console.log("Started testFunction.")
-    fetch(`http://api.openweathermap.org/data/2.5/forecast?q=${cityName},${countryCode}&appid=${API_KEY}`).then((response) => {
+    fetch(`${requestTypeUrl}${cityName}${URL_SUFFIX}`).then((response) => {
         console.log("1.")
         return response.json()
     }).then((json) => {
@@ -16,3 +20,5 @@ let testFunction = () => {
     })
     console.log("Finished testFunction.")
 }
+testFunction(FORECAST_URL)
+
