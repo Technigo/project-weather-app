7ce0695a917684721da130a4ba4ebc1d7570c70e
Michel Elmoznino Laufer <mitchel_85@hotmail.com>
2019-11-04 12:36:10 +0100

    Added another city and changed some styling

--- script.js
+++ script.js
@@ -2,38 +2,36 @@ const container = document.getElementById("weatherToday")
 const containerCity = document.getElementById("weatherCity")
 const containerWeekly = document.getElementById("weatherWeekly")
 
+const mainTemperature = (json) => {
+  console.log(json)
+  let mainTemp = json.main.temp.toFixed(1)
+  let sunriseHour = new Date((json.sys.sunrise)*1000).toLocaleTimeString([], {hour: '2-digit', minute: '2-digit'})
+  let sunsetHour = new Date((json.sys.sunset)*1000).toLocaleTimeString([], {hour: '2-digit', minute: '2-digit'})
+  console.log(`Main temp ${mainTemp} Sunrise ${sunriseHour}, Sunset ${sunsetHour}`)
 
-fetch('https://api.openweathermap.org/data/2.5/weather?q=Stockholm,Sweden&units=metric&APPID=60a9f45b45a7b39b290afa28477d7241')
-  .then((response) => {
-    return response.json()
-  })
-  .then((json) => {
-    console.log(json)
-    let mainTemp = json.main.temp.toFixed(1);
-    let sunriseHour = new Date((json.sys.sunrise)*1000).toLocaleTimeString([], {hour: '2-digit', minute: '2-digit'})
-    let sunsetHour = new Date((json.sys.sunset)*1000).toLocaleTimeString([], {hour: '2-digit', minute: '2-digit'})
-    console.log(`Main temp ${mainTemp} Sunrise ${sunriseHour}, Sunset ${sunsetHour}`)
+  if(mainTemp < 10) {
+    document.body.style.background = 'linear-gradient(to left, rgba(97, 153, 153, 0), rgb(50, 162, 226))' 
+    document.body.style.color = 'darkblue'
+  } else {
+    document.body.style.background = 'linear-gradient(to right, rgba(228, 200, 45, 0), rgb(226, 200, 50))'
+    document.body.style.color = 'darkgreen'
+    document.getElementById("image").src="sunglasses.svg"
+  }
 
-    if(mainTemp < 10) {
-      document.body.style.background = 'linear-gradient(to right, rgba(97, 153, 153, 0), rgb(50, 162, 226))' 
-    } else {
-      document.body.style.background = 'linear-gradient(to right, rgba(228, 200, 45, 0), rgb(226, 200, 50))'
-    }
-
-    container.innerHTML = `${json.weather[0].description} | ${mainTemp} &#8451 
-    <p id="infoTop">sunrise ${sunriseHour}</p> <p id="infoTop">sunset ${sunsetHour}</p>`
+  container.innerHTML = `${json.weather[0].description} | ${mainTemp} &#176
+  <p id="infoTop">sunrise ${sunriseHour}</p> <p id="infoTop">sunset ${sunsetHour}</p>`
     
-    containerCity.innerHTML = `<h2>${json.name}</h2>`
-  })
+  containerCity.innerHTML = `<h2>${json.name}</h2>`
+}
 
 
-  const handle5DayForecast = (json) => {
+const handle5DayForecast = (json) => {
   const forecastDiv = document.getElementById('forecast')
   let weekDays = {}
   console.log(json)
 
   json.list.forEach((weather) => {
-    let weekDay = new Date((weather.dt)*1000).toLocaleDateString("en-US", {weekday: 'short'})
+    let weekDay = new Date((weather.dt)*1000).toLocaleDateString("en-US", {weekday: 'long'})
     if (weekDays[weekDay]) {
       weekDays[weekDay].push(weather)
     } else {
@@ -53,11 +51,45 @@ fetch('https://api.openweathermap.org/data/2.5/weather?q=Stockholm,Sweden&units=
     const minTemp = Math.min(...temps)
     const maxTemp = Math.max(...temps)
 
-    forecastDiv.innerHTML += `<li>${weekDay} - max: ${maxTemp.toFixed(1)} &#8451 
-    min: ${minTemp.toFixed(1)} &#8451 </li>`
+    forecastDiv.innerHTML += `${weekDay} <li id="minMax"> ${maxTemp.toFixed(1)} &#176 
+    | ${minTemp.toFixed(1)} &#176 </li>`
   })
 }
 
+
+const veniceWeather = () => {
+  const forecastDiv = document.getElementById('forecast')
+  container.innerHTML=``
+  forecastDiv.innerHTML=``
+
+  fetch('https://api.openweathermap.org/data/2.5/weather?q=Venice,Italy&units=metric&APPID=60a9f45b45a7b39b290afa28477d7241')
+  .then((res) => res.json())
+  .then(mainTemperature)
+
+  fetch('https://api.openweathermap.org/data/2.5/forecast?q=Venice,Italy&units=metric&cnt=39&APPID=60a9f45b45a7b39b290afa28477d7241')
+  .then((res) => res.json())
+  .then(handle5DayForecast)
+}
+
+
+const stockholmWeather = () => {
+  const forecastDiv = document.getElementById('forecast')
+  container.innerHTML=``
+  forecastDiv.innerHTML=``
+  fetch('https://api.openweathermap.org/data/2.5/weather?q=Stockholm,Sweden&units=metric&APPID=60a9f45b45a7b39b290afa28477d7241')
+  .then((res) => res.json())
+  .then(mainTemperature)
+
+  fetch('https://api.openweathermap.org/data/2.5/forecast?q=Stockholm,Sweden&units=metric&cnt=39&APPID=60a9f45b45a7b39b290afa28477d7241')
+  .then((res) => res.json())
+  .then(handle5DayForecast)
+}
+
+
+fetch('https://api.openweathermap.org/data/2.5/weather?q=Stockholm,Sweden&units=metric&APPID=60a9f45b45a7b39b290afa28477d7241')
+  .then((res) => res.json())
+  .then(mainTemperature)
+  
 fetch('https://api.openweathermap.org/data/2.5/forecast?q=Stockholm,Sweden&units=metric&cnt=39&APPID=60a9f45b45a7b39b290afa28477d7241')
   .then((res) => res.json())
   .then(handle5DayForecast)
