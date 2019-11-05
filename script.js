69768b2f1c74a0f559e2e5d7f0c70549b9572e66
Jens_Nilsen <jensnilsen89@gmail.com>
2019-11-05 17:09:27 +0100

    content and functions added, needs cleaning an styling

--- script.js
+++ script.js
@@ -11,14 +11,69 @@ fetch(
   })
 
   .then(json => {
-    const weather = template({
+    const sunRise = new Date(json.sys.sunrise * 1000);
+    const sunSet = new Date(json.sys.sunset * 1000);
+    const sunriseTime = sunRise.toLocaleTimeString("sv-SE", {
+      timeStyle: "short"
+    });
+    const sunsetTime = sunSet.toLocaleTimeString("sv-SE", {
+      timeStyle: "short"
+    });
+
+    const weather = today({
       city: json.name,
       temp: json.main.temp,
-      sunUp: json.sys.sunrise,
-      sunSet: json.sys.sunset,
+      sunUp: sunriseTime,
+      sunSet: sunsetTime,
       icon: json.weather[0].icon,
       weatherType: json.weather[0].main.toLowerCase()
     });
 
     document.getElementById("todaysWeather").innerHTML = weather;
+  });
+
+const getWeekDayFromDayOfWeek = dayOfWeek => {
+  switch (dayOfWeek) {
+    case 1:
+      return "Monday";
+    case 2:
+      return "Tuesday";
+    case 3:
+      return "Wendesday";
+    case 4:
+      return "Thursday";
+    case 5:
+      return "Friday";
+    case 6:
+      return "Saturday";
+    default:
+      return "Sunday";
+  }
+};
+
+fetch(
+  `http://api.openweathermap.org/data/2.5/forecast?q=Stockholm,se&APPID=${apiKey}&units=metric`
+)
+  .then(response => {
+    return response.json();
+  })
+
+  .then(json => {
+    const arr = [];
+    json.list.forEach(item => {
+      const date = new Date(item.dt_txt);
+      const isNoon = date.getHours() === 12;
+
+      if (isNoon) {
+        const weatherObject = forecast({
+          noonTemp: item.main.temp,
+          weatherType: item.weather[0].main,
+          icon: item.weather[0].icon,
+          day: getWeekDayFromDayOfWeek(date.getDay())
+        });
+        arr.push(weatherObject);
+      }
+    });
+    const innerHTML = arr.join("");
+    document.querySelector(".forecastContainer").innerHTML = innerHTML;
   });
--- template.js
+++ template.js
@@ -1,12 +1,19 @@
-const template = ({ city, temp, sunUp, sunSet, icon, weatherType }) => `
-<article class="todaysWeather todaysWeather-${weatherType}">
-
-<h2>${city}</h2>
-<h1>${Math.round(temp)}°C</h1>
-${weatherType}
-<img src="http://openweathermap.org/img/wn/${icon}@2x.png" alt="">
-<h4>The sun goes up at: ${sunUp}</h4>
-<h4>The sun goes down at: ${sunSet}</h4>
+const today = ({ city, temp, sunUp, sunSet, icon, weatherType }) => `
+<article class="todaysWeather weatherapp-${weatherType}">
+  <h2>${city}</h2>
+  <h1>${Math.round(temp)}°C</h1>
+  ${weatherType}
+  <img src="http://openweathermap.org/img/wn/${icon}@2x.png" alt="">
+  <h4>The sun goes up at: ${sunUp}</h4>
+  <h4>The sun goes down at: ${sunSet}</h4>
 </article>
+`;
 
+const forecast = ({ day, noonTemp, weatherType, icon }) => `
+<li class="forecastWeather"> 
+  ${day}
+  ${weatherType}
+  ${noonTemp}
+  <img src="http://openweathermap.org/img/wn/${icon}@2x.png" alt="">
+</li>
 `;
