0f5d5c84a7c2d51f460a726bec0724a395406e76
crushoftheyear <kajsalovisaholmstrom@gmail.com>
2020-02-12 22:10:12 +0100

    Fetch city, temperature & description fron JSON & print it

--- code/script.js
+++ code/script.js
@@ -1,9 +1,15 @@
+const city = document.querySelector('#city')
+const temperature = document.querySelector('#temperature')
+const description = document.querySelector('#description')
+
 fetch('http://api.openweathermap.org/data/2.5/weather?q=Malmo,Sweden&units=metric&APPID=302165d90858a8a500d4198d9bc63d2b')
   .then((response) => {
     return response.json()
   })
   .then((json) => {
-    console.log(json)
+    city.innerHTML = json.name
+    description.innerHTML = json.weather[0].description
+    temperature.innerHTML = json.main.temp.toFixed(1)
   })
   .catch((err) => {
     console.log("oops error", err)
