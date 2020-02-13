ff9c054437e2ff3ee955312215122041e81895df
palhamel <pal.hamel@gmail.com>
2020-02-13 21:12:55 +0100

    Add sunset and sunrise functions

--- code/script.js
+++ code/script.js
@@ -1,5 +1,12 @@
 const container = document.getElementById("weather")
-const sunStatus = document.getElementById("sunstatus")
+
+const sunStatus = document.getElementById("sunStatus")
+
+const sunRise = document.getElementById("sunRise")
+const sunSet = document.getElementById("sunSet")
+
+const weatherForecast = "http://api.openweathermap.org/data/2.5/forecast?q=Stockholm,se&units=metric&APPID=44527f8be39feab9d034d48604c6b81d&lang=en"
+// console.log(weatherForecast)
 
 fetch('http://api.openweathermap.org/data/2.5/weather?q=Stockholm,se&units=metric&APPID=44527f8be39feab9d034d48604c6b81d&lang=en')
   .then((response) => {
@@ -7,14 +14,19 @@ fetch('http://api.openweathermap.org/data/2.5/weather?q=Stockholm,se&units=metri
   })
   .then((json) => {
 
-    console.log(json) // checking the json from open weather
-
-    container.innerHTML = `<h1>${json.name}, Temp is: ${json.main.temp}°C </h1>`
-
+    //console.log(json) // checking the json from open weather
+    container.innerHTML = `<h1>${json.name} & Temp is: ${json.main.temp.toFixed(1)}°C (using toFixed)</h1>`
     json.weather.forEach((sky) => {
-      container.innerHTML += `<p>Weather is: ${sky.description} </p>`
+      container.innerHTML += `<h1>Weather type is: ${sky.description} </h1>`
     });
-
-    sunStatus.innerHTML = `<h1>Sunrise: ${json.sys.sunrise} and Sunset: ${json.sys.sunset}</h1>`
-
+    // sunStatus.innerHTML = `<h1>Sunrise: ${json.sys.sunrise} and Sunset: ${json.sys.sunset}</h1>`
+    // console.log(weather.sunrise = new Date(json.sys.sunrise * 1000).toLocaleTimeString([], { timeStyle: 'short' }))
+    // console.log(weather.sunset = new Date(json.sys.sunset * 1000).toLocaleTimeString([], { timeStyle: 'short' }))
+    sunRise.innerHTML = (weather.sunrise = new Date(json.sys.sunrise * 1000).toLocaleTimeString([], {
+      timeStyle: 'short'
+    }))
+    sunSet.innerHTML = (weather.sunset = new Date(json.sys.sunset * 1000).toLocaleTimeString([], {
+      timeStyle: 'short'
+    }))
   })
+
