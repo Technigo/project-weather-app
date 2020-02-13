81655e00c6fa01e9830143a9b0cd26dda0f002f4
bealun <53275597+bealun@users.noreply.github.com>
2020-02-13 11:09:40 +0100

    getting time for sunrise/sunset

--- code/weather.js
+++ code/weather.js
@@ -5,18 +5,24 @@ const forecast = []
 const weather = {}
 const week = ['Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday', 'Sunday']
 
-const weatherAPI = ''
-const forecastAPI = ''
+// const weatherAPI = ''
+// const forecastAPI = ''
 
 //01b7bdc37404b6f3860ddce923c61a11
+// My API links with API keys
+const weatherNow = "http://api.openweathermap.org/data/2.5/weather?q=Stockholm,Sweden&units=metric&APPID=01b7bdc37404b6f3860ddce923c61a11"
 
-fetch(
-  "http://api.openweathermap.org/data/2.5/weather?q=Stockholm,Sweden&units=metric&APPID=01b7bdc37404b6f3860ddce923c61a11"
-  )
+fetch(weatherNow)
   .then(response => {
     return response.json()
-
+  
   .then(json => {
     console.log(json)
+    console.log(weather.sunrise = new Date(json.sys.sunrise * 1000).toLocaleTimeString([], { timeStyle: 'short' }))
+    console.log(weather.sunset = new Date(json.sys.sunset * 1000).toLocaleTimeString([], { timeStyle: 'short' }))
+    // console.log(weather.temp = (Math.round(json.main.temp * 10) / 10))
+    // console.log(weather.city = json.name)
+    // console.log(weather.id = json.weather[0].id)
   })
+
   })
