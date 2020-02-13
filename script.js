f9d24d1808a51f843796be7e6393954d82547b06
Kajsa Bootin <kajsa@Kajsas-MacBook-Air.local>
2020-02-13 13:34:46 +0100

    step 2

--- script.js
+++ script.js
@@ -1,9 +1,12 @@
+const containerToday = document.getElementById("todaysweather")
+
 fetch("https://api.openweathermap.org/data/2.5/weather?q=stockholm,Sweden&units=metric&APPID=8ba6b8f613b670c947149eaad6fdfef7")
   .then((response) => {
     return response.json();
   })
   .then ((json) => {
-    console.log(json)    
+    console.log(json);
+    containerToday.innerHTML = `<h1> The weather in ${json.name} today is ${json.main.temp} celsius and ${json.weather[0].main}</h1>`
   })
   .catch((err) =>{
     console.log("caught error", err)
