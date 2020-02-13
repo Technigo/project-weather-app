39bf02268ba579db8a5eb9ba43ef978dfbc3864b
sara <sara.lovens.lovn@gmail.com>
2020-02-13 16:07:30 +0100

    second fetch and fixing it

--- script.js
+++ script.js
@@ -16,6 +16,9 @@ json.people.forEach(element => {
 
 })
 document.getElementById('killerImage').src = mystery.killer.image
+   var date = new Date(1546108200 * 1000);
+    console.log(date.toUTCString())
+    var fixed = rounded.toFixed(1);
 ========================================== 
 */
 
@@ -28,11 +31,14 @@ fetch('https://api.openweathermap.org/data/2.5/weather?q=London&appid=2b9468766d
     return response.json()
 }).then((json) => {
     console.log(json)
+
     let weatherObject = {
         name: json.weather[0].main,
         description: json.weather[0].description,
-        temp: (json.main.temp - 273.15),
+        temp: (json.main.temp - 273.15).toFixed(1),
+        date: (new Date(json.dt * 1000)).toUTCString()
     }
+
     console.log("my object", weatherObject)
     const icons = {
         clouds: "media/clouds.svg",
@@ -61,12 +67,37 @@ fetch('https://api.openweathermap.org/data/2.5/weather?q=London&appid=2b9468766d
     console.log(weatherObject)
 
     today.innerHTML += `<h1> The city is ${json.name}</h1>`
-    today.innerHTML += `<p> Weather ${json.weather[0].main} the description ${json.weather[0].description} icon ${json.weather[0].icon} </p>`
+    today.innerHTML += `<p> Weather ${json.weather[0].main} the description ${json.weather[0].description} <br> today ${weatherObject.date} temperature ${weatherObject.temp} </p>`
     const todayWeather = [json.main.temp, json.main.feels_like, json.main.temp_min, json.main.temp_max]
     document.getElementById('todayIcon').src = pictures()
     console.log(todayWeather)
     console.log(json.weather[0])
 
+})
+// fetch 5 -days
+// api.openweathermap.org/data/2.5/forecast?q=London&appid=2b9468766d0e54560c7e599762d2e80b
+fetch('https://api.openweathermap.org/data/2.5/forecast?q=London&appid=2b9468766d0e54560c7e599762d2e80b').then((response) => {
+    return response.json()
+}).then((jsonweek) => {
+    console.log('week', jsonweek)
+    console.log("day1", jsonweek.list[0])
 
+    const myFunc = () => {
+        let dayListWeather = []
+        jsonweek.list.forEach((item) => {
+            dayListWeather.push({ name: item.weather[0].main, description: item.weather[0].description, temp: (item.main.temp - 273.15), date: item.dt_txt, wind: item.wind.speed })
+
+        })
+        return dayListWeather
+
+    }
+    const newArr = myFunc()
+    console.log(newArr)
 })
 
+
+// console.log(moreAboutPokemons);
+// const newArray = moreAboutPokemons.map(val => {
+//   return val.name;
+// });
+// console.log(newArray);
