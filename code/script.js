72f2da43c112059744581022936b80e9f34a3e33
crushoftheyear <kajsalovisaholmstrom@gmail.com>
2020-02-13 20:25:17 +0100

    Create function that converts milliseconds to hours & minutes to print sunrise & sunset

--- code/script.js
+++ code/script.js
@@ -1,6 +1,10 @@
 const city = document.querySelector('#city')
 const temperature = document.querySelector('#temperature')
 const description = document.querySelector('#description')
+const sunrise = document.querySelector('#sunrise')
+const sunset = document.querySelector('#sunset')
+
+
 
 fetch('http://api.openweathermap.org/data/2.5/weather?q=Malmo,Sweden&units=metric&APPID=302165d90858a8a500d4198d9bc63d2b')
   .then((response) => {
@@ -9,7 +13,20 @@ fetch('http://api.openweathermap.org/data/2.5/weather?q=Malmo,Sweden&units=metri
   .then((json) => {
     city.innerHTML = json.name
     description.innerHTML = json.weather[0].description
-    temperature.innerHTML = json.main.temp.toFixed(1)
+    temperature.innerHTML = `${json.main.temp.toFixed(1)}°`
+
+    const getHoursAndMinutes = (milliseconds) => {
+      let date = new Date(milliseconds)
+      let hours = date.getHours()
+      let minutes = date.getMinutes()
+
+      return `${hours}:${minutes}`
+    }
+
+    sunrise.innerHTML = getHoursAndMinutes(json.sys.sunrise)
+    sunset.innerHTML = getHoursAndMinutes(json.sys.sunset)
+
+
   })
   .catch((err) => {
     console.log("oops error", err)
