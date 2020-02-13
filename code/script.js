b4e008d9f254bfcfb660b2f293d4529797fc2c27
Frida Eriksson <Frida.Eriksson@silf.se>
2020-02-13 23:16:30 +0100

    step 2, present city, temp and weather description

--- code/script.js
+++ code/script.js
@@ -1,9 +1,15 @@
-const container = document.getElementById('weather')
+const city = document.getElementById('city')
+const temp = document.getElementById('temp')
+const description = document.getElementById('description')
 
 fetch('http://api.openweathermap.org/data/2.5/weather?q=Stockholm,Sweden&units=metric&APPID=60032cdd91d77852bfb39762c09118fe')
   .then((response) => {
     return response.json()
   })
   .then((json) => {
-    container.innerHTML = `<h1>The visibility is ${json.visibility}`
+    city.innerHTML = json.name
+
+    temp.innerHTML = `${json.main.temp.toFixed(1)} °`
+
+    description.innerHTML = json.weather[0].description
   })
