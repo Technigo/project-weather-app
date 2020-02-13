4e3e875363f9a8bbf634040b0593efa22cfde1e8
Christina Persson <christina@Christinas-MacBook.local>
2020-02-13 13:03:27 +0100

    More responsive features5

--- code/script.js
+++ code/script.js
@@ -2,6 +2,9 @@ const containerToday = document.getElementById("todays-weather");
 const containerFiveDays = document.getElementById("five-day-weather");
 const containerWeather = document.getElementById("weather");
 
+const header = document.getElementById("header");
+const footer = document.getElementById("footer");
+
 fetch(
   "https://api.openweathermap.org/data/2.5/weather?q=Malmoe,Sweden&units=metric&APPID=778e796f3254363f06afef1bc4ea2b4f"
 )
@@ -16,26 +19,30 @@ fetch(
     const sunriseTime = timeFormat(json.sys.sunrise);
     const sunsetTime = timeFormat(json.sys.sunset);
     const weather = json.weather[0].main;
+    const temperature = json.main.temp;
 
     containerToday.innerHTML = `<h3>${json.name}</h3>`;
     containerToday.innerHTML += `<img src="${checkWeather(weather)}" alt="">`;
-    containerToday.innerHTML += `<p>${temperatureFormat(
-      json.main.temp
-    )}°C and ${json.weather[0].description}
+    containerToday.innerHTML += `<p>${temperatureFormat(temperature)}°C and ${
+      json.weather[0].description
+    }
     </p>`;
 
     containerToday.innerHTML += `<p>  Sunrise: ${sunriseTime}<br>
      Sunset: ${sunsetTime}</p>`;
 
-    if (temperatureFormat(json.main.temp) < 10) {
-      containerWeather.style.background =
-        "linear-gradient(15deg, rgba(34,122,224,1) 0%, rgba(81,217,255,1) 100%)";
-    } else if (temperatureFormat(json.main.temp) > 20) {
-      containerWeather.style.background =
-        "linear-gradient(15deg, rgba(226,44,44,1) 0%, rgba(246,255,115,1) 100%)";
+    if (temperatureFormat(temperature) < 10) {
+      styleContainers(
+        "linear-gradient(15deg, rgba(34,122,224,1) 0%, rgba(81,217,255,1) 100%)"
+      );
+    } else if (temperatureFormat(temperature) > 20) {
+      styleContainers(
+        "linear-gradient(15deg, rgba(226,44,44,1) 0%, rgba(246,255,115,1) 100%)"
+      );
     } else {
-      containerWeather.style.background =
-        "linear-gradient(15deg, rgba(78,221,134,1) 0%, rgba(249,255,179,1) 100%)";
+      styleContainers(
+        "linear-gradient(15deg, rgba(78,221,134,1) 0%, rgba(249,255,179,1) 100%)"
+      );
     }
   });
 
@@ -59,6 +66,12 @@ fetch(
       )}°C</p>`;
     });
   });
+
+const styleContainers = backgroundColour => {
+  containerWeather.style.background = backgroundColour;
+  footer.style.background = backgroundColour;
+  header.style.background = backgroundColour;
+};
 
 const checkWeather = weatherType => {
   const typesOfWeather = [
