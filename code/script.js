cfe8a45027da2c4748705a206c8aa662f9e8b5f4
Ragna Þorbjörg Úlfarsdóttir <Ragna@Ragnas-MBP.lan>
2020-02-13 14:43:50 +0100

    Step 3 - Sunrise and sunset

--- code/script.js
+++ code/script.js
@@ -6,8 +6,23 @@ fetch('https://api.openweathermap.org/data/2.5/weather?q=Reykjavik,Iceland&units
   })
   .then((json) => {
     
-    container.innerHTML = `<p>${json.weather[0].description} | ${json.main.temp.toFixed(1)}°c</p>`
-    container.innerHTML += `<p>feels like: ${json.main.feels_like.toFixed(1)}°c</p>`
-    container.innerHTML += `<p>The temperature in ${json.name} is currently ${json.main.temp.toFixed(1)}°c, feels like ${json.main.feels_like.toFixed(1)}°c</p>`
+    container.innerHTML = `<p>${json.weather[0].description} | ${json.main.temp.toFixed(1)}°</p>`
+    container.innerHTML += `<p>feels like: ${json.main.feels_like.toFixed(1)}°</p>`
+    container.innerHTML += `<p>The temperature in ${json.name} is currently ${json.main.temp.toFixed(1)}°, feels like ${json.main.feels_like.toFixed(1)}°</p>`
   
+    const timestampSunrise = json.sys.sunrise
+    const timestampSunset = json.sys.sunset
+
+    let sunrise = new Date (timestampSunrise * 1000)
+    let sunset = new Date (timestampSunset * 1000)
+
+    let sunriseTime = sunrise.toLocaleTimeString(undefined, { timeStyle: 'short'})
+    let sunsetTime = sunset.toLocaleTimeString(undefined, {timeStyle: 'short'})
+
+    container.innerHTML += `<p>Sunrise: ${sunriseTime}</p>`
+    container.innerHTML += `<p>Sunset: ${sunsetTime}</p>`
   })
+
+    
+    // container.innerHTML += `Sunrise: ${Date.prototype.getHours(json.sys.sunrise)}`
+ 
