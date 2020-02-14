45db490a04ed5280ba8bcd7b5c12b386aee53cd6
Josephine <josephine@Josephines-Air.lan>
2020-02-14 19:53:21 +0100

    finished step two

--- code/script.js
+++ code/script.js
@@ -1,9 +1,14 @@
-const container = document.getElementById("Gothenburg")
+const container = document.getElementById('Gothenburg')
 
-fetch('http://api.openweathermap.org/data/2.5/weather?id=5695743&appid=3b69213b480a303abeec34f0262802f0')
+fetch('http://api.openweathermap.org/data/2.5/weather?q=Gotenburg&units=metric&appid=3b69213b480a303abeec34f0262802f0')
   .then((response) => {
     return response.json()
   })
   .then((json) => {
-    console.log(json)
+    json.main.temp = Math.round(json.main.temp)
+    container.innerHTML = `<h2> The temperature is ${json.main.temp}</h2>`
+    // console.log(json.main.temp)
+    json.weather.forEach((now) => {
+      container.innerHTML += `<p>${now.description} </p>`
+    })
   })
