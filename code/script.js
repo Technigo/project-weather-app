b92bf8a447126b1795268da669ed4b0e7d5a4054
Anna Gustafsson <annagustafsson@MacBook-Pro-som-tillhor-Anna.local>
2020-02-14 09:05:32 +0100

    added sunrise and sunset

--- code/script.js
+++ code/script.js
@@ -1,13 +1,16 @@
 const container = document.getElementById('weatherApp')
 
-fetch('https://api.openweathermap.org/data/2.5/weather?q=Gothenburg,Sweden&units=metric&APPID=150f4ff6ea1bf24cf1f0e1bdecefa90f')
+fetch('https://api.openweathermap.org/data/2.5/weather?q=Majorna,Sweden&units=metric&APPID=150f4ff6ea1bf24cf1f0e1bdecefa90f')
   .then((response) => {
     return response.json()
   })
   .then((json) => {
     container.innerHTML = `<h1>Todays weather in: ${json.name}</h1>`
 
+    const temp = json.main.temp
+    const tempRounded = temp.toFixed(0.1)
+
     json.weather.forEach((today) => {
-      container.innerHTML += `<p>${json.main.temp}° ${today.description}</p>`
+      container.innerHTML += `<p>${tempRounded}°C ${today.description}</p>`
     })
   })
