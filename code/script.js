b0a1d03cf8f6996b4876925ec6ebbc77b48d4c5d
Josephine <josephine@Josephines-Air.lan>
2020-02-14 21:20:04 +0100

    finished step 3

--- code/script.js
+++ code/script.js
@@ -8,7 +8,23 @@ fetch('http://api.openweathermap.org/data/2.5/weather?q=Gotenburg&units=metric&a
     json.main.temp = Math.round(json.main.temp)
     container.innerHTML = `<h2> The temperature is ${json.main.temp}</h2>`
     // console.log(json.main.temp)
+    // DESCRIPTION
     json.weather.forEach((now) => {
       container.innerHTML += `<p>${now.description} </p>`
+      //SUNRISE; SUNSET
+      let rise = new Date(json.sys.sunrise * 1000);
+      let up = rise.toLocaleTimeString([], {
+        timeStyle: 'short'
+      })
+      let set = new Date(json.sys.sunset * 1000);
+      let down = set.toLocaleTimeString([], {
+        timeStyle: 'short'
+      })
+      container.innerHTML += `<p>Sunrise: ${up} - Sunset: ${down}</p>`
+      // expected output: 01:15:30
+      // console.log(up)
+      // console.log(json.sys.sunset)
+      // console.log(set)
+
     })
   })
