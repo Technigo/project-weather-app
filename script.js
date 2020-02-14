1b263291570a0e47795ea5827799dfd2572353c9
Johanna Åstrand <johanna@johannas-mbp.mynet>
2020-02-14 21:22:52 +0100

    Restarted the project to structure the code better added location, temperature, description and time for sunrise and sunset

--- script.js
+++ script.js
@@ -1,4 +1,12 @@
-const container = document.getElementById('weather')
+const userLocation = document.getElementById('location')
+const tempDegees = document.getElementById('tempDegrees')
+const tempDescription = document.getElementById('tempDescription')
+const tempFeelsLike = document.getElementById('tempFeelsLike')
+//const sunriseTime = document.getElementById('sunrise')
+//const sunsetTime = document.getElementById('sunset')
+const days = document.getElementById('days')
+const minTemp = document.getElementById('minTemp')
+const maxTemp = document.getElementById('maxTemp')
 
 fetch('http://api.openweathermap.org/data/2.5/weather?q=Stockholm,Sweden&units=metric&APPID=10c15495461885dfddf7c2f3846d4e30')
   .then((response) => {
@@ -6,12 +14,26 @@ fetch('http://api.openweathermap.org/data/2.5/weather?q=Stockholm,Sweden&units=m
   })
 
   .then((json) => {
-    container.innerHTML = `<h1>Todays weather in ${json.name} and it is ${json.main.temp}.</h1>`
+    console.log(json)
+    userLocation.innerHTML = `${json.name}`
+    tempDegrees.innerHTML = `${json.main.temp.toFixed(1)}°C`
+    tempFeelsLike.innerHTML += `${json.main.feels_like.toFixed(1)}°C`
 
     json.weather.forEach((weather) => {
-      container.innerHTML += `<p>You can expekt ${weather.description}</p>`
+      tempDescription.innerHTML = `${weather.description}`
     })
+
+    // SUNSET AND SUNRISE TIME ?? 
+    const unixTimestampSunrise = json.sys.sunrise
+    const unixTimestampSunset = json.sys.sunset
+    const sunrise = new Date(unixTimestampSunrise * 1000)
+    const sunset = new Date(unixTimestampSunset * 1000)
+    const sunriseTime = sunrise.toLocaleTimeString([], { timeStyle: 'short' })
+    const sunsetTime = sunset.toLocaleTimeString([], { timeStyle: 'short' })
+    sunr.innerHTML += `${sunriseTime}`
+    suns.innerHTML += `${sunsetTime}`
   })
+
 
 
 
