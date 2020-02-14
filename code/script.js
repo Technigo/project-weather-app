75dd50ddaa59a74e55d8b61864ba96f52134eb58
Josephine <josephine@Josephines-Air.lan>
2020-02-14 23:21:46 +0100

    step 4 done

--- code/script.js
+++ code/script.js
@@ -28,3 +28,30 @@ fetch('http://api.openweathermap.org/data/2.5/weather?q=Gotenburg&units=metric&a
 
     })
   })
+
+
+//FORECAST
+
+fetch('http://api.openweathermap.org/data/2.5/forecast?q=Gothenburg&units=metric&appid=3b69213b480a303abeec34f0262802f0')
+  .then((response) => {
+    return response.json()
+  })
+  .then((json) => {
+
+    const filteredForecast = json.list.filter(item => item.dt_txt.includes('12:00'))
+
+
+    filteredForecast.forEach((weekday) => {
+      const days = ['Sunday', 'Monday', 'Tuesday', 'Wednesday', 'Thursdagy', 'Friday', 'Saturday']
+      let day = new Date(weekday.dt * 1000).getDay()
+      weekday.main.temp = Math.round(weekday.main.temp)
+      container.innerHTML += `<p>${days[day]}  ${weekday.main.temp}  ${weekday.weather[0].description}</p> 
+      <img src=https://openweathermap.org/img/wn/${weekday.weather[0].icon}@2x.png></img>`
+      console.log(filteredForecast)
+    })
+  })
+
+
+  .catch((err) => {
+    console.log(`caughterror`, err)
+  })
