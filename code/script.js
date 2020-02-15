c1f15a583eb53c7fa75f102202875c5e9c0ed70c
Frida Eriksson <Frida.Eriksson@silf.se>
2020-02-15 15:55:35 +0100

    Step 4 - Weather forecast

--- code/script.js
+++ code/script.js
@@ -10,7 +10,7 @@ fetch('http://api.openweathermap.org/data/2.5/weather?q=Stockholm,Sweden&units=m
   })
   .then((json) => {
     cityContainer.innerHTML = json.name
-    tempContainer.innerHTML = `${json.main.temp.toFixed(1)} °`
+    tempContainer.innerHTML = `${json.main.temp.toFixed(1)} <sup>&#730 C</sup>`
     descriptionContainer.innerHTML = json.weather[0].description
 
     const sunriseConversion = new Date(json.sys.sunrise * 1000)
@@ -22,3 +22,31 @@ fetch('http://api.openweathermap.org/data/2.5/weather?q=Stockholm,Sweden&units=m
     sunriseContainer.innerHTML = `Sunrise: ${sunriseTime}`
     sunsetContainer.innerHTML = `Sunset: ${sunsetTime}`
   })
+
+  .catch((err) => {
+    console.log("caught error", err)
+  })
+
+const forecastContainer = document.getElementById('forecast')
+
+fetch('https://api.openweathermap.org/data/2.5/forecast?q=Stockholm,Sweden&units=metric&APPID=60032cdd91d77852bfb39762c09118fe')
+  .then((response) => {
+    return response.json()
+  })
+  .then((json) => {
+
+    const filteredForecast = json.list.filter(item => item.dt_txt.includes('12:00'));
+
+    filteredForecast.forEach(day => {
+      const date = new Date(day.dt * 1000)
+      const weekdays = ['SUN', 'MON', 'TUE', 'WED', 'THU', 'FRI', 'SAT'];
+      let dayOfWeek = weekdays[date.getDay()];
+
+      forecastContainer.innerHTML += `<section class="dayForecast"><h2>${dayOfWeek}</h2> <p>${day.main.temp.toFixed(1)} <sup>&#730 C</sup></p></section>`
+    })
+  })
+
+  .catch((err) => {
+    console.log("caught error", err)
+  })
+
