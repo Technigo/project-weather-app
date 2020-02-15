6597426cf5d91fbfde183a5ddf69706f90b9958e
jenniferot <56264114+Jenniferot@users.noreply.github.com>
2020-02-15 22:49:40 +0100

    Börjat styla

--- script.js
+++ script.js
@@ -1,5 +1,3 @@
-const container = document.getElementById("currentWeather")
-
 fetch("http://api.openweathermap.org/data/2.5/weather?q=Stockholm,Sweden&units=metric&APPID=8e3b288ccb7e41d96fd2f44603191864")
   .then((response) => {
     return response.json()
@@ -19,11 +17,16 @@ fetch("http://api.openweathermap.org/data/2.5/weather?q=Stockholm,Sweden&units=m
     })
     const longTemp = json.main.temp
     const shortTemp = longTemp.toFixed(1);
-
-    container.innerHTML = `<h2>The current weather in ${json.name} is ${shortTemp}ºC and the weather is ${json.weather[0].description}. Soluppgång ${sunriseTime}. Solnedgång ${sunsetTime}.`
+    document.getElementById("description").innerHTML = `${json.weather[0].description}`
+    document.getElementById("todayTemp").innerHTML = `${shortTemp}˚`
+    document.getElementById("currentCondition").innerHTML += `<img src="https://openweathermap.org/img/wn/${json.weather[0].icon}@2x.png" alt="" />`
+    document.getElementById("currentWeather").innerHTML = `<h2>The current weather in ${json.name} is ${shortTemp}ºC and the weather is ${json.weather[0].description}. Soluppgång ${sunriseTime}. Solnedgång ${sunsetTime}.</h2>`
+    document.getElementById("sr").innerHTML = `${sunriseTime}`
+    document.getElementById("ss").innerHTML = `${sunsetTime}`
   })
 
-const week = document.getElementById("cityName")
+
+//const week = document.getElementById("city")
 
 fetch("https://api.openweathermap.org/data/2.5/forecast?q=Stockholm,Sweden&units=metric&APPID=8e3b288ccb7e41d96fd2f44603191864")
   .then((response) => {
@@ -32,10 +35,30 @@ fetch("https://api.openweathermap.org/data/2.5/forecast?q=Stockholm,Sweden&units
   .then((json) => {
     const filteredForecast = json.list.filter(item => item.dt_txt.includes('12:00'))
     console.log(filteredForecast)
-    filteredForecast.forEach(item => {
-      console.log(item.weather[0].description)
+
+    const containerFiveDays = document.getElementById("weekWeather")
+
+    containerFiveDays.innerHTML = "";
+    filteredForecast.forEach(day => {
+      const date = new Date(day.dt * 1000)
+      const weekdays = ["Sun", "Mon", "Tue", "Wed", "Thu", "Fri", "Sat"];
+      let dayOfWeek = weekdays[date.getDay()];
+      containerFiveDays.innerHTML += `<div class="dayRow"><div class="dayLabel">${dayOfWeek}</div> <div class="dayTemp">${day.main.temp.toFixed(0)} °C </div></div>`
     })
-    filteredForecast.forEach(item => {
-      console.log(item.main.temp)
-    })
-  });
+    console.log(containerFiveDays)
+  })
+
+/*filteredForecast.forEach(item => {
+  console.log(item.weather[0].description)
+  console.log(item.main.temp)
+})
+
+filteredForecast.forEach(item => {
+  console.log(item.main.temp)
+  console.log(item.main.temp)
+
+filteredForecast.forEach(item => {
+  console.log(item.main.temp_min)
+  console.log(item.main.temp_max)
+})
+*/
