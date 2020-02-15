fc7a9cb2dc032c84b04016d5f26cc7c98389c80d
Kajsa Bootin <kajsa@Kajsas-Air.zyxel.com>
2020-02-15 12:23:58 +0100

    step 4 done

--- Code/script.js
+++ Code/script.js
@@ -5,8 +5,7 @@ const containerSunrise = document.getElementById("sunrise")
 const containerSunset = document.getElementById("sunset")
 
 const containerDayOne = document.getElementById("dayone")
-const containerFiveDays = document.getElementById("forecast")
-    
+const containerFiveDays = document.getElementById("forecast")   
 
 
 fetch("https://api.openweathermap.org/data/2.5/weather?q=stockholm,Sweden&units=metric&APPID=8ba6b8f613b670c947149eaad6fdfef7")
@@ -14,24 +13,19 @@ fetch("https://api.openweathermap.org/data/2.5/weather?q=stockholm,Sweden&units=
     return response.json();
   })
   .then ((json) => {
-    console.log(json);
+    //console.log(json);
     containerTodayCity.innerHTML = `<h1>  City: ${json.name} </h1>`
     containerTodayCelsius.innerHTML = `<h1> °C ${json.main.temp.toFixed(0)} </h1>`
     containerTodayCloud.innerHTML = `<h1> Feels like ${json.weather[0].main}</h1>`
-
-    //let sunrise = json.sys.sunrise;
-    //let sunriseDate = new Date(sunrise*1000);
 
     let sunrise = new Date(json.sys.sunrise * 1000)
     let sunriseTime = sunrise.toLocaleTimeString([], { timeStyle: 'short' });
 
     containerSunrise.innerHTML = `<h2> 🔝 ${sunriseTime} </h2>`
 
-    //let sunset = json.sys.sunset;
-    //let sunsetDate = new Date(sunset*1000);
-
     const sunset = new Date(json.sys.sunset * 1000)
     const sunsetTime = sunset.toLocaleTimeString([], { timeStyle: 'short' });
+
 
     containerSunset.innerHTML = `<h2> 🔙 ${sunsetTime} </h2>`
     })
@@ -40,29 +34,27 @@ fetch("https://api.openweathermap.org/data/2.5/weather?q=stockholm,Sweden&units=
   })
 
 
-
   fetch("https://api.openweathermap.org/data/2.5/forecast?q=Stockholm,Sweden&units=metric&APPID=8ba6b8f613b670c947149eaad6fdfef7")
     .then((response) => {
     return response.json();
     })
 
     .then ((json) => {
-      console.log(json);
-      const filteredForecast = json.list.filter(item => item.dt_txt.includes('12:00'));
+      //console.log(json);
+      const filteredForecast = json.list.filter(item => item.dt_txt.includes('03:00'));
 
-      console.log(filteredForecast);
+
+      //console.log(filteredForecast);
       containerFiveDays.innerHTML = "";
       filteredForecast.forEach(day => {
         const date = new Date(day.dt * 1000)
-        const dayOfWeek = date.getDay()
-        console.log(day)
+        const weekdays = ["Sun", "Mon", "Tue", "Wed", "Thu", "Fri", "Sat"];
+        let dayOfWeek = weekdays[date.getDay()];
+        
         containerFiveDays.innerHTML += `<p> ${dayOfWeek} ${day.main.temp.toFixed(0)} °C </p>`
-      
       })
-      
     })
 
     .catch((err) =>{
-      console.log("caught error", err)
+      //console.log("caught error", err)
     })
-
