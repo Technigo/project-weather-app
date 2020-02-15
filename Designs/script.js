f0a692c154c906a0ad6adf67607450a3f95f0e70
Emelie Loheden <emelie_loheden@hotmail.com>
2020-02-15 20:44:50 +0100

    second fetch for sunrise and sunset output in html

--- Designs/script.js
+++ Designs/script.js
@@ -1,13 +1,33 @@
-const container =  document.getElementById('someText')
+const container =  document.getElementById('weatherInfo')
+const containerSunrise = document.getElementById("sunrise")
+const containerSunset = document.getElementById("sunset")
 
 fetch('http://api.openweathermap.org/data/2.5/weather?q=Stockholm,Sweden&units=metric&APPID=69828f6ac304f247815bc18fa686b778')
   .then((response) => {
     return response.json();
   })
   .then((myJson) => {
-    console.log(myJson)
-    container.innerHTML = `<h1>${myJson.name} ${myJson.main.temp} c and ${myJson.weather[0].description}</h1>` //name.description.temperature
+    container.innerHTML = `<h1>${myJson.name} ${myJson.main.temp} c and ${myJson.weather[0].description}</h1>`
+ 
   })
+
+  fetch('http://api.openweathermap.org/data/2.5/weather?q=Stockholm,Sweden&units=metric&APPID=69828f6ac304f247815bc18fa686b778')
+  .then((response) => {
+    return response.json();
+  })
+  .then((myJson) => {
+    let sunrise = new Date(myJson.sys.sunrise * 1000)
+    let sunriseTime = sunrise.toLocaleTimeString([], { timeStyle: 'short' });
+
+    let sunset = new Date(myJson.sys.sunset * 1000)
+    let sunsetTime = sunset.toLocaleTimeString([], { timeStyle: 'short' });
+
+    containerSunrise.innerHTML = `<h2> Sunrise: ${sunriseTime} </h2>`
+ 
+    containerSunset.innerHTML = `<h2> Sunset: ${sunsetTime} </h2>`
+  })
+
+  
  
 
  
