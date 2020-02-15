e763598d01228f2499b56fdd3b1703c2118b7f4c
ebbabw <ebbabergholtz@gmail.com>
2020-02-16 00:39:48 +0100

    Step 2 - Present some data on your web app

--- Designs/Code/script.js
+++ Designs/Code/script.js
@@ -1,10 +1,20 @@
-const container = document.getElementById('wetherService')
 
 
-fetch('https://samples.openweathermap.org/data/2.5/find?q=London&units=metric&appid=bb2b0bb45cd18a1f48ff2ac55b77750a', {mode: 'no-cors'})
+const cityName = document.getElementById('city')
+const cityTemp = document.getElementById('temp')
+const cityDescript =document.getElementById('Descript')
+
+
+fetch('http://api.openweathermap.org/data/2.5/weather?q=New%20York&units=metric&APPID=bb2b0bb45cd18a1f48ff2ac55b77750a')
 .then((Response) => {
     return Response.json()
 })
 .then((json) => {
-    console.log(json)
+
+    cityName.innerHTML = json.name 
+    cityTemp.innerHTML = `<p> Temp: ${json.main.temp.toFixed(0)} &#8451; </p>`
+    cityDescript.innerHTML = json.weather[0].description
+
+
 })
+
