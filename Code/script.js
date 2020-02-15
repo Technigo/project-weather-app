bf4d1d427f06e1448630eb54e3506f69649927de
Nathalie Rautell <nathalierautell@Nathalies-MacBook-Pro.local>
2020-02-15 13:06:06 +0100

    done with step 3

--- Code/script.js
+++ Code/script.js
@@ -4,9 +4,11 @@ const currentWeatherAPI = "http://api.openweathermap.org/data/2.5/weather?q=hels
 // Data storage
 const weather = {}
 
+// DOM elements
+const currentWeather = document.getElementById('current')
+
 
 // Fetch data
-
 fetch(currentWeatherAPI)
 
   .then((response) => {
@@ -14,11 +16,32 @@ fetch(currentWeatherAPI)
   })
 
   .then((jsonFile) => {
-    weather.city = jsonFile.name
+    weather.location = jsonFile.name
     weather.temp = (jsonFile.main.temp / 100).toFixed(1)
     weather.description = jsonFile.weather[0].description
-    console.log(weather)
+
+    let sunrise = new Date((jsonFile.sys.sunrise + jsonFile.timezone) * 1000)
+    sunriseTime = sunrise.toLocaleTimeString([], { timeStyle: 'short', timeZone: "UTC" })
+    weather.sunrise = sunriseTime
+
+    let sunset = new Date((jsonFile.sys.sunset + jsonFile.timezone) * 1000)
+    sunsetTime = sunset.toLocaleTimeString([], { timeStyle: 'short', timeZone: "UTC" })
+    weather.sunset = sunsetTime
+
+    printWeather()
   })
 
+// Print data onto DOM
+const printWeather = () => {
+  currentWeather.innerHTML = `
+      <div>
+          ${weather.location}
+          ${weather.temp}
+          ${weather.description}
+          ${weather.sunrise}
+          ${weather.sunset}
+      </div>
+    `
+}
 
 
