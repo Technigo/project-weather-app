47c80f75959f1b97ea1803cda7b5b27e055b4b25
Frida Eriksson <Frida.Eriksson@silf.se>
2020-02-15 23:54:00 +0100

    updated url for first api, removed icons not used

--- code/script.js
+++ code/script.js
@@ -5,7 +5,7 @@ const descriptionContainer = document.getElementById('description')
 const sunriseContainer = document.getElementById('sunrise')
 const sunsetContainer = document.getElementById('sunset')
 
-fetch('http://api.openweathermap.org/data/2.5/weather?q=Stockholm,Sweden&units=metric&APPID=60032cdd91d77852bfb39762c09118fe')
+fetch('https://api.openweathermap.org/data/2.5/weather?q=Stockholm,Sweden&units=metric&APPID=60032cdd91d77852bfb39762c09118fe')
   .then((response) => {
     return response.json()
   })
