3de93ada3f7231e366476756edc765556127d462
Lisas Sturegard <lisasturegard@hotmail.com>
2020-02-16 12:19:53 +0100

    Step Two Weather App

--- weather.js
+++ weather.js
@@ -1,14 +1,21 @@
 const container = document.getElementById('weather')
 
-fetch('http://api.openweathermap.org/data/2.5/weather?q=Stockholm,sweden&APPID=00620bb638ed0fa5525452696e39c3ed')
+
+fetch('http://api.openweathermap.org/data/2.5/weather?q=Stockholm,sweden&units=metric&APPID=00620bb638ed0fa5525452696e39c3ed')
   .then((response) =>{
     return response.json()
   })
   .then((json) => {
     console.log(json)
-    container.innerHTML = `<p>Kolla om ${json.main.temp} funkar</p> `
+    container.innerHTML = `<h1>Weather in ${json.name}<h/1>`;
+    let temperature = Math.round(json.main.temp * 10) /10;
+    let feelsLike = Math.round(json.main.feels_like *10) /10;
+    container.innerHTML += `${temperature}&#730; feels like ${feelsLike}&#730;`
+    json.weather.forEach((sky) => {
+      container.innerHTML += `<p>${sky.description}</p>`;
+    })
   })
 
-/*Jason.outlook.forEach((json) => {
-  container.innerHTML += `Försöker med ${json}`
-})*/
+  .catch((err) => {
+    console.log('cauhgt error', err)
+  })
