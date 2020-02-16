dacc0a6be12344a18a564f034010962b15055067
crushoftheyear <kajsalovisaholmstrom@gmail.com>
2020-02-16 21:31:32 +0100

    Add comments to JS and geolocation test

--- code/script.js
+++ code/script.js
@@ -1,48 +1,75 @@
-const city = document.querySelector('#city')
-const temperature = document.querySelector('#temperature')
-const description = document.querySelector('#description')
-const wind = document.querySelector('#wind')
-const sunrise = document.querySelector('#sunrise')
-const sunset = document.querySelector('#sunset')
-const forecast = document.querySelector('#forecast')
-
-
-const timeFormat = (ms) => {
-  let time = new Date(ms * 1000).toLocaleTimeString([], {
-    timeStyle: 'short'
-  })
-  return time;
-}
-
-
-fetch('http://api.openweathermap.org/data/2.5/weather?q=Malmo,Sweden&units=metric&APPID=302165d90858a8a500d4198d9bc63d2b')
+// Todays weather
+fetch('https://api.openweathermap.org/data/2.5/weather?q=Malmo,Sweden&units=metric&APPID=302165d90858a8a500d4198d9bc63d2b')
   .then((response) => {
     return response.json()
   })
   .then((json) => {
-    city.innerHTML = json.name
-    description.innerHTML = json.weather[0].description
-    temperature.innerHTML = `${json.main.temp.toFixed(1)}°`
-    wind.innerHTML = `${json.wind.speed.toFixed(1)} m/s`
 
-    sunrise.innerHTML = timeFormat(json.sys.sunrise)
-    sunset.innerHTML = timeFormat(json.sys.sunset)
+    // Convert milliseconds to HH:MM
+    const timeFormat = (ms) => {
+      let time = new Date(ms * 1000).toLocaleTimeString([], {
+        timeStyle: 'short'
+      })
+      return time;
+    }
+
+    // Geolocation test
+    const geoBtn = document.getElementById("geo");
+
+    function getLocation() {
+      if (navigator.geolocation) {
+        navigator.geolocation.getCurrentPosition(showPosition);
+      } else {
+        geoBtn.innerHTML = "Geolocation is not supported by this browser.";
+      }
+    }
+
+    function showPosition(position) {
+      let coordLat = position.coords.latitude
+      let coordLong = position.coords.longitude
+      let apiString = `https://api.openweathermap.org/data/2.5/forecast?lat=${coordLat}&lon=${coordLong}&appid=302165d90858a8a500d4198d9bc63d2b`
+      console.log(apiString)
+    }
+
+    getLocation()
+
+
+    // Print out to DOM
+    document.querySelector('#city').innerHTML = json.name
+    document.querySelector('#description').innerHTML = json.weather[0].description
+    document.querySelector('#temperature').innerHTML = `${json.main.temp.toFixed(1)}°`
+    document.querySelector('#wind').innerHTML = `${json.wind.speed.toFixed(1)} m/s`
+
+    document.querySelector('#sunrise').innerHTML = timeFormat(json.sys.sunrise)
+    document.querySelector('#sunset').innerHTML = timeFormat(json.sys.sunset)
 
   })
   .catch((err) => {
     console.log("oops error", err)
   })
 
-
-fetch('http://api.openweathermap.org/data/2.5/forecast?q=Malmo&units=metric&appid=302165d90858a8a500d4198d9bc63d2b')
+// 5 day forecast
+fetch('https://api.openweathermap.org/data/2.5/forecast?q=Malmo&units=metric&appid=302165d90858a8a500d4198d9bc63d2b')
   .then((response) => {
     return response.json()
   })
   .then((json) => {
+    // Filter the forecast list array to get info from 06:00 each day
     const filteredForecast = json.list.filter(item => item.dt_txt.includes('06:00'))
 
+    // Return weekday
+    const getDayOfWeek = (param) => {
+
+      let weekdays = ["Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday"];
+      let date = new Date(param * 1000);
+      let specificDay = date.getDay();
+
+      return weekdays[specificDay]
+    }
+
+    // Loops through filteredForecast
     filteredForecast.forEach(day => {
-      console.log(`${day.dt_txt} – ${day.main.temp.toFixed(1)}° – ${day.wind.speed.toFixed(1)}m/s`)
+      document.querySelector('#forecast').innerHTML += `<p>${getDayOfWeek(day.dt)} ${day.main.temp.toFixed(1)}° ${day.wind.speed.toFixed(1)}m/s</p>`
     })
 
   })
