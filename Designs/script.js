0734185c047228f70ca7e4e8f223bf2b498e7f56
Tuliany <tuliany@mail.com>
2020-02-16 16:37:13 +0100

    step 2 done

--- Designs/script.js
+++ Designs/script.js
@@ -3,11 +3,10 @@ const container = document.getElementById('astros')
 fetch('http://api.openweathermap.org/data/2.5/weather?q=Marbella,Spain&units=metric&APPID=329f2a2705f51547d2ed78a937fa0051')
   .then((response) => {
     return response.json()
+
   })
   .then((json) => {
-    container.innerHTML = `<h1> vädret i ${json.name} </h1>`
-    json.weather.forEach((vader) => {
-      container.innerHTML += `<p>dagarna är ${vader.main}</p>`
-    })
-    // console.log(json)
+    city.innerHTML = json.name
+    temp.innerHTML = `${json.main.temp.toFixed(1)} °`
+    description.innerHTML = json.weather[0].description
   })
