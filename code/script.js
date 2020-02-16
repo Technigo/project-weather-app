abd3e19ca441ab576324c482ba79ab835c1f96c4
Josephine <josephine@Josephines-Air.lan>
2020-02-16 12:56:25 +0100

    added a footer and cleaned up code

--- code/script.js
+++ code/script.js
@@ -8,21 +8,19 @@ fetch('http://api.openweathermap.org/data/2.5/weather?q=Gotenburg&units=metric&a
     json.main.temp = Math.round(json.main.temp)
     container.innerHTML = `<h2>${json.main.temp}&degC</h2>`
 
-
+    // IMG-COLOR CHANGE WITH WARMER TEMP
     if (json.main.temp > 6) {
       document.getElementById("gbg-image").style.backgroundImage = 'linear-gradient(to bottom, rgba(143, 249, 185, 0.52), rgba(244, 244, 247, 0.73)), url("gbg-img.jpg")';
     } else if (json.main.temp > 16) {
       document.getElementById("gbg-image").style.backgroundImage = 'linear-gradient(to bottom, rgba(249, 190, 130, 0.52), rgba(244, 244, 247, 0.73)), url("gbg-img.jpg")';
     } else if (json.main.temp > 22) {
       document.getElementById("gbg-image").style.backgroundImage = 'linear-gradient(to bottom, rgba(241, 69, 41, 0.28), rgba(244, 244, 247, 0.73)), url("gbg-img.jpg")';
-
     }
-    // DESCRIPTION
+    // DESCRIPTION AND ICON
     json.weather.forEach((now) => {
       container.innerHTML += `<p>${now.description} </p>
       <img src=https://openweathermap.org/img/wn/${now.icon}@2x.png></img>`
       //SUNRISE; SUNSET
-
       let rise = new Date(json.sys.sunrise * 1000);
       let up = rise.toLocaleTimeString([], {
         timeStyle: 'short'
@@ -32,20 +30,10 @@ fetch('http://api.openweathermap.org/data/2.5/weather?q=Gotenburg&units=metric&a
         timeStyle: 'short'
       })
       container.innerHTML += `<p>Sunrise: ${up} - Sunset: ${down}</p>`
-      // expected output: 01:15:30
-      // console.log(up)
-      // console.log(json.sys.sunset)
-      // console.log(set)
-
-
     })
   })
 
-
-
 //FORECAST
-
-//const forecastContainer = document.getElementById('forecast-container')
 
 fetch('http://api.openweathermap.org/data/2.5/forecast?q=Gothenburg&units=metric&appid=3b69213b480a303abeec34f0262802f0')
   .then((response) => {
@@ -61,35 +49,9 @@ fetch('http://api.openweathermap.org/data/2.5/forecast?q=Gothenburg&units=metric
       document.getElementById('forecast-temp').innerHTML += `<p>${weekday.main.temp}&degC</p>`
       document.getElementById('forecast-description').innerHTML += `<p>${weekday.weather[0].description}</p>`
       document.getElementById('forecast-icon').innerHTML += `<img src=https://openweathermap.org/img/wn/${weekday.weather[0].icon}@2x.png></img>`
-
-
-
-      console.log(filteredForecast)
     })
   })
 
   .catch((err) => {
     console.log(`caughterror`, err)
   })
-
-/*const forecastIcon = document.getElementById('forecast-icon')
-
-fetch('http://api.openweathermap.org/data/2.5/forecast?q=Gothenburg&units=metric&appid=3b69213b480a303abeec34f0262802f0')
-  .then((response) => {
-    return response.json()
-  })
-  .then((json) => {
-    filteredForecastIcon.forEach((weekday) => {
-      const days = ['Sunday', 'Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday']
-      let day = new Date(weekday.dt * 1000).getDay()
-      const filteredForecastIcon = json.list.filter(item => item.dt_txt.includes('12:00'))
-
-      forecastIcon.innerHTML += `<img src=https://openweathermap.org/img/wn/${weekday.weather[0].icon}@2x.png></img>`
-
-      console.log(filteredForecastIcon)
-    })
-  })
-
-  .catch((err) => {
-    console.log(`caughterror`, err)
-  })*/
