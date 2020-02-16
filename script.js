e66797d69890fa07d8d4d3c5f85d4cdd3d82d0db
Emelie Loheden <emelie_loheden@hotmail.com>
2020-02-16 12:37:50 +0100

    change in url

--- script.js
+++ script.js
@@ -1,18 +1,18 @@
 const container =  document.getElementById('weatherInfo')
 const containerSunrise = document.getElementById('sunrise')
 const containerSunset = document.getElementById('sunset')
-const URLDay = 'http://api.openweathermap.org/data/2.5/weather?q=Stockholm,Sweden&units=metric&APPID=69828f6ac304f247815bc18fa686b778'
+const URLDay = 'https://api.openweathermap.org/data/2.5/weather?q=Stockholm,Sweden&units=metric&APPID=69828f6ac304f247815bc18fa686b778'
 const URLForecast ='https://api.openweathermap.org/data/2.5/forecast?q=Stockholm,Sweden&units=metric&APPID=69828f6ac304f247815bc18fa686b778'
-const fiveDays = document.getElementById('fiveDays')
 const titleDays = document.getElementById('title')
+
+const xcontainer =  document.getElementById('weatherInfoX')
 
 fetch(URLDay)
   .then((response) => {
     return response.json();
   })
   .then((myJson) => {
-    container.innerHTML = `<h1>${myJson.name} ${Math.round(myJson.main.temp)} celcius and ${myJson.weather[0].description}</h1>`
- 
+    container.innerHTML = `<h1>${myJson.name} ${Math.round(myJson.main.temp.toFixed(1))}<sup>°C</sup> and ${myJson.weather[0].description}</h1>`
   })
 
   fetch(URLDay)
@@ -29,24 +29,10 @@ fetch(URLDay)
     containerSunrise.innerHTML = `<h2> Sunrise: ${sunriseTime} </h2>`
  
     containerSunset.innerHTML = `<h2> Sunset: ${sunsetTime} </h2>`
-  })
-  
-  fetch(URLForecast)
-  .then((response) => {
-    return response.json();
-  })
+   
+ })
 
-  .then((json) => {
 
-    titleDays.innerHTML = `<h4>5 Days</h4>`
-    
-    json.forEach((days) => {
-
-    //fiveDays.innerHTML += `<h1> ${days.list.main.temp}</h1>`
-  
-  }) 
-
-})
 
 
 
