1c5253f63f56d35f61b450040d8906074d733e3e
Emelie Loheden <emelie_loheden@hotmail.com>
2020-02-16 16:16:13 +0100

    adding forEach loop and new array for five days forecast

--- script.js
+++ script.js
@@ -1,18 +1,21 @@
 const container =  document.getElementById('weatherInfo')
 const containerSunrise = document.getElementById('sunrise')
 const containerSunset = document.getElementById('sunset')
-const URLDay = 'https://api.openweathermap.org/data/2.5/weather?q=Stockholm,Sweden&units=metric&APPID=69828f6ac304f247815bc18fa686b778'
-const URLForecast ='https://api.openweathermap.org/data/2.5/forecast?q=Stockholm,Sweden&units=metric&APPID=69828f6ac304f247815bc18fa686b778'
 const titleDays = document.getElementById('title')
 
-const xcontainer =  document.getElementById('weatherInfoX')
+const URLDay = 'https://api.openweathermap.org/data/2.5/weather?q=Stockholm,Sweden&units=metric&APPID=69828f6ac304f247815bc18fa686b778'
+const URLForecast ='https://api.openweathermap.org/data/2.5/forecast?q=Stockholm,Sweden&units=metric&APPID=69828f6ac304f247815bc18fa686b778'
+
+const containerFive =  document.getElementById('fiveDays')
 
 fetch(URLDay)
   .then((response) => {
     return response.json();
   })
   .then((myJson) => {
-    container.innerHTML = `<h1>${myJson.name} ${Math.round(myJson.main.temp.toFixed(1))}<sup>°C</sup> and ${myJson.weather[0].description}</h1>`
+    container.innerHTML = `<h1>${myJson.name} ${Math.round(myJson.main.temp.toFixed(1))}
+    <sup>°C</sup></h1> 
+    <h1>${myJson.weather[0].description}</h1>`
   })
 
   fetch(URLDay)
@@ -32,7 +35,24 @@ fetch(URLDay)
    
  })
 
+ fetch(URLForecast)
+  .then((response) => {
+    return response.json();
+  })
+  .then((myJson) => {
+  const filteredForecast = myJson.list.filter(item => item.dt_txt.includes('12:00'))
+  console.log(filteredForecast)
+   //containerFive.innerHTML = `<h1>${filteredForecast}<sup>°C</sup></h1>`
 
+   filteredForecast.forEach((day) => {
+    
+    containerFive.innerHTML += `<h1>${Math.round(day.main.temp.toFixed(1))}<sup>°C</sup></h1>`
+
+   })
+   
+ })
+
+ 
 
 
 
