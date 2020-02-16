0c73ca0adadd5c358b37157da1ee6d58185e79b5
Elin Aarenstrup <hej@elinaarenstrup.com>
2020-02-16 21:01:45 +0100

    Styling

--- script.js
+++ script.js
@@ -1,23 +1,46 @@
-const container = document.getElementById('weatherToday')
+
+//HTML
+const container = document.getElementById('weather')
 const conSunset = document.getElementById('sunset')
 const conSunrise = document.getElementById('sunrise')
+const conForecast = document.getElementById('forecast')
 
-fetch('http://api.openweathermap.org/data/2.5/weather?q=Stockholm,Sweden&units=metric&APPID=3053f069033c799a9b5c60d9d3887e6c')
+fetch('https://api.openweathermap.org/data/2.5/weather?q=Stockholm,Sweden&units=metric&APPID=3053f069033c799a9b5c60d9d3887e6c')
+
+.then((response) => {
+  return response.json()
+})
+
+//Reminder, rounded to 1 decimal place 
+.then((json) => {
+  container.innerHTML = `<h2> ${json.name} is ${json.main.temp.toFixed(1)}&#730C and 
+  ${json.weather[0].description}.</h2>`
+
+    const sunrise = new Date (json.sys.sunrise*1000);
+    const sunriseTime = sunrise.toLocaleTimeString([], {timeStyle: 'short'});
+
+    const sunset = new Date (json.sys.sunset*1000);
+    const sunsetTime = sunset.toLocaleTimeString([], {timeStyle: 'short'});
+
+    conSunrise.innerHTML = `<h2> Sunrise ${sunriseTime}</h2>`
+    conSunset.innerHTML = `<h2> Sunset ${sunsetTime} </h2>`
+})
+
+fetch('https://api.openweathermap.org/data/2.5/forecast?q=Stockholm,Sweden&units=metric&APPID=3053f069033c799a9b5c60d9d3887e6c')
 
 .then((response) => {
   return response.json()
 })
 
 .then((json) => {
-  container.innerHTML = `<h1> ${json.name} is ${json.main.temp} celsius and ${json.weather[0].description}.</h1>`
 
-    let sunrise = new Date (json.sys.sunrise*1000);
-    let sunriseTime = sunrise.toLocaleTimeString([], {timeStyle: 'short'});
+  const filteredForecast = json.list.filter(item => item.dt_txt.includes('12:00'));
 
-    conSunrise.innerHTML = `<h2> Sunrise: ${sunriseTime} </h2>`
+  filteredForecast.forEach((day) => {
+    const date = new Date(day.dt * 1000)
+    const weekdays = ['SUN', 'MON', 'TUE', 'WED', 'THU', 'FRI', 'SAT'];
+    let dayOfWeek = weekdays[date.getDay()];
 
-    let sunset = new Date (json.sys.sunset*1000);
-    let sunsetTime = sunset.toLocaleTimeString([], {timeStyle: 'short'});
-
-    conSunset.innerHTML = `<h2> Sunset: ${sunsetTime} </h2>`
+    conForecast.innerHTML += `<h2>${dayOfWeek} <span>${day.main.temp.toFixed(1)}&#730C</span></h2>`
+  })
 })
