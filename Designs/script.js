c219be703fcd888cc384f61d441899e4e83f1792
Tuliany <tuliany@mail.com>
2020-02-16 20:48:17 +0100

    step 4 done

--- Designs/script.js
+++ Designs/script.js
@@ -24,3 +24,20 @@ fetch('http://api.openweathermap.org/data/2.5/weather?q=Marbella,Spain&units=met
     sunrise.innerHTML = `Sunrise: ${sunriseTime}`
     sunset.innerHTML = `Sunset: ${sunsetTime}`
   })
+
+fetch('http://api.openweathermap.org/data/2.5/forecast?q=marbella,spain&appid=329f2a2705f51547d2ed78a937fa0051')
+  .then((response) => {
+    return response.json()
+  })
+  .then((json) => {
+    const filteredForecast = json.list.filter(item => item.dt_txt.includes('12:00'));
+    containerFiveDays.innerHTML = "";
+    filteredForecast.forEach(day => {
+      const date = new Date(day.dt * 1000)
+      const weekdays = ["Sun", "Mon", "Tue", "Wed", "Thu", "Fri", "Sat"];
+      let dayOfWeek = weekdays[date.getDay()];
+      containerFiveDays.innerHTML += `<p> ${dayOfWeek} ${day.main.temp.toFixed(0)} ° </p>`
+    })
+
+
+  })
