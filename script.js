8783cb4fae657a674c2ee8e1570b8bbb80c3fca9
Emelie Loheden <emelie_loheden@hotmail.com>
2020-02-16 20:03:48 +0100

    styling

--- script.js
+++ script.js
@@ -1,21 +1,23 @@
 const container =  document.getElementById('weatherInfo')
 const containerSunrise = document.getElementById('sunrise')
 const containerSunset = document.getElementById('sunset')
-const titleDays = document.getElementById('title')
 
 const URLDay = 'https://api.openweathermap.org/data/2.5/weather?q=Stockholm,Sweden&units=metric&APPID=69828f6ac304f247815bc18fa686b778'
 const URLForecast ='https://api.openweathermap.org/data/2.5/forecast?q=Stockholm,Sweden&units=metric&APPID=69828f6ac304f247815bc18fa686b778'
 
+const titleDays = document.getElementById('title')
 const containerFive =  document.getElementById('fiveDays')
+const containerDay = document.getElementById('weekDay')
+
 
 fetch(URLDay)
   .then((response) => {
     return response.json();
   })
   .then((myJson) => {
-    container.innerHTML = `<h1>${myJson.name} ${Math.round(myJson.main.temp.toFixed(1))}
-    <sup>°C</sup></h1> 
-    <h1>${myJson.weather[0].description}</h1>`
+    container.innerHTML = `<h1>${Math.round(myJson.main.temp.toFixed(1))}
+    <sup>°C</sup></h1><h2>${myJson.name}</h2> 
+    <h3>${myJson.weather[0].description}</h3>`
   })
 
   fetch(URLDay)
@@ -40,22 +42,14 @@ fetch(URLDay)
     return response.json();
   })
   .then((myJson) => {
-  const filteredForecast = myJson.list.filter(item => item.dt_txt.includes('12:00'))
-  console.log(filteredForecast)
-   //containerFive.innerHTML = `<h1>${filteredForecast}<sup>°C</sup></h1>`
-
-   filteredForecast.forEach((day) => {
-    
-    containerFive.innerHTML += `<h1>${Math.round(day.main.temp.toFixed(1))}<sup>°C</sup></h1>`
-
-   })
-   
+    const filteredForecast = myJson.list.filter(item => item.dt_txt.includes('15:00'))
+    filteredForecast.forEach((day) => {
+    containerFive.innerHTML += `<li>${Math.round(day.main.temp.toFixed(1))}<sup>°C</sup></li>`
+     
+   }) 
  })
 
  
-
-
-
 
 
 
