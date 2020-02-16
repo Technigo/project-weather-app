a8954c746e9fd90d1e7e96f33c3b46d4da7353a9
Emma Prené <emma.prene@gmail.com>
2020-02-16 17:58:58 +0100

    Step 3 done

--- code/script.js
+++ code/script.js
@@ -1,51 +1,35 @@
-const place = document.getElementById('place')
-const temperature = document.getElementById('temperature')
-const description = document.getElementById('description')
+const placeContainer = document.getElementById('place')
+const temperatureContainer = document.getElementById('temperature')
+const descriptionContainer = document.getElementById('description')
+const sunriseContainer = document.getElementById('sunrise')
+const sunsetContainer = document.getElementById('sunset')
+
+
 
 fetch('http://api.openweathermap.org/data/2.5/weather?q=Malmo,Sweden&units=metric&APPID=224e607ac22e4aef9578da3aaa6f0b85')
   .then((response) => {
     return response.json()
   })
   .then((json) => {
-    place.innerHTML = `<h2>There are ${json.name} number of people in space right now.</h2>`
-    description.innerHTML = `<h2>The weather is ${json.weather[0].description} right now.</h2>`
-    temperature.innerHTML = `<h2>It is ${json.main.temp.toFixed(1)} degrees celsius.</h2>`
+    placeContainer.innerHTML = `<h2>The weather right now in ${json.name}.</h2>`
+    descriptionContainer.innerHTML = `<h2>The weather is ${json.weather[0].description} right now.</h2>`
+    temperatureContainer.innerHTML = `<h2>It is ${json.main.temp.toFixed(1)} degrees celsius.</h2>`
 
+    const sunrise = new Date(json.sys.sunrise * 1000)
+    const sunriseTime = sunrise.toLocaleTimeString([], {
+      timeStyle: 'short'
+    });
+    sunriseContainer.innerHTML = `<h2>The sunrise is at ${sunriseTime}</h2>`
+
+    const sunset = new Date(json.sys.sunset * 1000)
+    const sunsetTime = sunset.toLocaleTimeString([], {
+      timeStyle: 'short'
+    });
+    sunsetContainer.innerHTML = `<h2>The sunset is at ${sunsetTime}</h2>`
 
   })
 
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-// const place = document.getElementById('place')
-
-// fetch('http://api.openweathermap.org/data/2.5/weather?q=Malmo,Sweden&units=metric&APPID=224e607ac22e4aef9578da3aaa6f0b85')
-//   .then((response) => {
-//     return response.json()
-//   })
-//   .then((json) => {
-//     place.innerHTML = `<h2>You are in ${json.place} right now.</h2>`
-//     temperature.innerHTML = `<h2>It is ${json.temperature} degrees.</h2>`
-//     description.innerHTML = `<h2>It is ${json.description} right now.</h2>`
-
-//   })
 
 
 
