9d5144336045d0d6462d307af94c4c4ea5a72351
ebbabw <ebbabergholtz@gmail.com>
2020-02-17 20:09:54 +0100

    Step 3 - Sunrise and sunset

--- Designs/Code/script.js
+++ Designs/Code/script.js
@@ -2,7 +2,10 @@
 
 const cityName = document.getElementById('city')
 const cityTemp = document.getElementById('temp')
-const cityDescript =document.getElementById('Descript')
+const cityWeatherdescript = document.getElementById('Descript')
+const citySunrise = document.getElementById('sunrise')
+const citySunset = document.getElementById('sunset')
+
 
 
 fetch('http://api.openweathermap.org/data/2.5/weather?q=New%20York&units=metric&APPID=bb2b0bb45cd18a1f48ff2ac55b77750a')
@@ -12,9 +15,19 @@ fetch('http://api.openweathermap.org/data/2.5/weather?q=New%20York&units=metric&
 .then((json) => {
 
     cityName.innerHTML = json.name 
-    cityTemp.innerHTML = `<p> Temp: ${json.main.temp.toFixed(0)} &#8451; </p>`
-    cityDescript.innerHTML = json.weather[0].description
+    cityTemp.innerHTML = `<p> ${json.main.temp.toFixed(0)} &#8451; </p>`
+    cityWeatherdescript.innerHTML = json.weather[0].description
 
+    
+    const sunriseConvert = new Date(json.sys.sunrise * 1000) 
+    const sunriseTime = sunriseConvert.toLocaleDateString([],{ timeStyle: 'short'})
 
+    sunrise.innerHTML = `Sunrise: ${sunriseTime}`
+
+    const sunsetConvert = new Date(json.sys.sunset * 1000) 
+    const sunsetTime = sunsetConvert.toLocaleDateString([],{ timeStyle: 'short' })
+
+    sunset.innerHTML = `Sunset: ${sunsetTime}`
+    
 })
 
