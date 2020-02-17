20f884dd32364c835025e499561b4d51ee51a016
Louise Eriksson <louise@Louises-Air.lan>
2020-02-17 16:00:23 +0100

    finished all steps

--- script.js
+++ script.js
@@ -11,11 +11,12 @@ const todayPlus4 = document.getElementById('todayPlus4')
 const todayPlus5 = document.getElementById('todayPlus5')
 
 
+
 //TODAYS DATE
 const today = new Date()
 console.log(today)
 
-//JSON STOCKHOLM WEATHER TODAY
+//JSON STOCKHOLM WEATHER FOR TODAY
 fetch('http://api.openweathermap.org/data/2.5/weather?q=Stockholm,Sweden&units=metric&APPID=b09042e161870e44988114035ff61156')
   .then((response) => {
     return response.json()
@@ -52,55 +53,47 @@ fetch('https://api.openweathermap.org/data/2.5/forecast?q=Stockholm,Sweden&units
 
     console.log(json)
 
+    //WEATHER AT 12AM
     const filteredForecast = json.list.filter(item => item.dt_txt.includes('12:00'))
 
+    // TOMORROWS WEATHER
     const date1 = new Date(filteredForecast[0].dt_txt)
     const day1 = date1.toLocaleDateString('en-US', { weekday: 'long' })
     todayPlus1.innerHTML = day1
-    todayPlus1.innerHTML += `: ${filteredForecast[0].main.temp} ºC`
+    todayPlus1.innerHTML += `: ${filteredForecast[0].main.temp} ºC, `
+    todayPlus1.innerHTML += filteredForecast[0].weather[0].description
 
-
+    //DAY AFTER TOMORROW
     const date2 = new Date(filteredForecast[1].dt_txt)
     const day2 = date2.toLocaleDateString('en-US', { weekday: 'long' })
     todayPlus2.innerHTML = day2
-    todayPlus2.innerHTML += `: ${filteredForecast[1].main.temp} ºC`
+    todayPlus2.innerHTML += `: ${filteredForecast[1].main.temp} ºC, `
+    todayPlus2.innerHTML += filteredForecast[1].weather[0].description
 
+    // 2 DAYS AFTER TOMORROW
     const date3 = new Date(filteredForecast[2].dt_txt)
     const day3 = date3.toLocaleDateString('en-US', { weekday: 'long' })
     todayPlus3.innerHTML = day3
-    todayPlus3.innerHTML += `: ${filteredForecast[2].main.temp} ºC`
+    todayPlus3.innerHTML += `: ${filteredForecast[2].main.temp} ºC, `
+    todayPlus3.innerHTML += filteredForecast[2].weather[0].description
 
+    //AND SO ON..
     const date4 = new Date(filteredForecast[3].dt_txt)
     const day4 = date4.toLocaleDateString('en-US', { weekday: 'long' })
     todayPlus4.innerHTML = day4
-    todayPlus4.innerHTML += `: ${filteredForecast[3].main.temp} ºC`
+    todayPlus4.innerHTML += `: ${filteredForecast[3].main.temp} ºC, `
+    todayPlus4.innerHTML += filteredForecast[3].weather[0].description
 
+    // AND SO ON
     const date5 = new Date(filteredForecast[4].dt_txt)
     const day5 = date5.toLocaleDateString('en-US', { weekday: 'long' })
     todayPlus5.innerHTML = day5
-    todayPlus5.innerHTML += `: ${filteredForecast[4].main.temp} ºC`
+    todayPlus5.innerHTML += `: ${filteredForecast[4].main.temp} ºC, `
+    todayPlus5.innerHTML += filteredForecast[4].weather[0].description
 
 
 
     console.log(filteredForecast[0].dt_txt)
     console.log(filteredForecast)
-
-
-    console.log(test)
-    console.log(test2)
-
-
-
   });
 
-
-/*
-const launchDate = new Date(launch.launch_date_utc);
-console.log(launch);
-
-// Here we'll make the date readable
-const launchDateString = launchDate.toLocaleDateString('en-US', { weekday: 'short' });
-const launchTimeString = launchDate.toLocaleTimeString('en-US', { timestyle: 'short', hour12: false })
-
-console.log(launchDateString)
-*/
