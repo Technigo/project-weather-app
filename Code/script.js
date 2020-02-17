a9c8a6a729d3d28ef907aaf1c7a7802b5f69ae4c
Nathalie Rautell <nathalierautell@Nathalies-MacBook-Pro.local>
2020-02-17 23:01:29 +0100

    added conditionals for different weather-designs

--- Code/script.js
+++ Code/script.js
@@ -8,7 +8,7 @@ const days = ['SUN', 'MON', 'TUE', 'WED', 'THU', 'FRI', 'SAT']
 
 // DOM elements
 const header = document.getElementById('header')
-const figcaption = document.getElementById('figcaption')
+const headerCaption = document.getElementById('headerCaption')
 const figure = document.getElementById('figure')
 const forecastWeather = document.getElementById('forecast')
 
@@ -26,6 +26,7 @@ fetch(currentWeatherAPI)
     weather.location = jsonFile.name
     weather.temp = (jsonFile.main.temp / 100).toFixed(1)
     weather.description = jsonFile.weather[0].description
+    weather.code = jsonFile.weather[0].id
 
     let sunrise = new Date((jsonFile.sys.sunrise + jsonFile.timezone) * 1000)
     weather.sunriseTime = sunrise
@@ -48,16 +49,42 @@ const printWeather = () => {
     <h2>${weather.day}</h2>
     <h1>Weather in ${weather.location}</h1>
     `
-  figcaption.innerHTML = `
-    <img src="/Assets/Icons/temp.png" alt=""> ${weather.temp} &#8451;
+  headerCaption.innerHTML = `
+    <img src="/Assets/Icons/temperature.png" alt=""> ${weather.temp} &#8451;
     <img src="/Assets/Icons/sunrise.png" alt="">${weather.sunrise}
     <img src="/Assets/Icons/sunset.png" alt="">${weather.sunset}
     `
-  figure.innerHTML = `${weather.description}`
+  weatherImage()
 }
 
 // Weather images
-
+const weatherImage = () => {
+  let code = weather.code
+  if (code < 300) {
+    figure.innerHTML = `<img src="/Assets/thunder.png" alt="">`
+    figureCaption.innerHTML = `Not sure if just a thunderstorm, or if we're being invaded by Russia. However, stay inside it will be ${weather.description}!`
+  }
+  else if (code < 600) {
+    figure.innerHTML = `<img src="/Assets/rainy.png" alt="">`
+    figureCaption.innerHTML = `Why does Snoop Dog always have an umbrella? Fo'Drizzle! Yo, you should too, it will be ${weather.description} fo'shizzle!`
+  }
+  else if (code < 700) {
+    figure.innerHTML = `<img src="/Assets/snowy.png" alt="">`
+    figureCaption.innerHTML = `Hipster ice: I was water before it got cool. Soo, just for your information, it will be some ${weather.description}`
+  }
+  else if (code < 800) {
+    figure.innerHTML = `<img src="/Assets/windy.png" alt="">`
+    figureCaption.innerHTML = `Here in ${weather.location} you're mostly experiencing some ${weather.description}`
+  }
+  else if (code == 800) {
+    figure.innerHTML = `<img src="/Assets/sunny.png" alt="">`
+    figureCaption.innerHTML = `Here in ${weather.location} you're mostly experiencing some ${weather.description}`
+  }
+  else {
+    figure.innerHTML = `<img src="/Assets/cloudy.png" alt="">`
+    figureCaption.innerHTML = `Oh, for crying out cloud! You're mostly experiencing some ${weather.description} today.`
+  }
+}
 
 
 /*  ---- FORECAST WEATHER ---- */
