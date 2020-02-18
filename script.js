335e3513e3ea028878a369064ba21b6e29b23e71
Elin Aarenstrup <hej@elinaarenstrup.com>
2020-02-18 14:19:47 +0100

    cleaned up the code, added name in footer

--- script.js
+++ script.js
@@ -1,23 +1,23 @@
 //HTML
 const cityName = document.getElementById("city");
-const container = document.getElementById("weather");
-const conSunset = document.getElementById("sunset");
-const conSunrise = document.getElementById("sunrise");
-const conForecast = document.getElementById("forecast");
+const containerWeather = document.getElementById("weather");
+const containerSunset = document.getElementById("sunset");
+const containerSunrise = document.getElementById("sunrise");
+const containerForecast = document.getElementById("forecast");
 const weatherAPI =
   "https://api.openweathermap.org/data/2.5/weather?q=Stockholm,Sweden&units=metric&APPID=3053f069033c799a9b5c60d9d3887e6c";
 const forecastAPI =
   "https://api.openweathermap.org/data/2.5/forecast?q=Stockholm,Sweden&units=metric&APPID=3053f069033c799a9b5c60d9d3887e6c";
 
+//Weather section
 fetch(weatherAPI)
   .then(response => {
     return response.json();
   })
 
-  //Weather section
   .then(json => {
     cityName.innerHTML = `<h1>${json.name}</h1>`;
-    container.innerHTML = `<h2>${json.main.temp.toFixed(
+    containerWeather.innerHTML = `<h2>${json.main.temp.toFixed(
       1
     )}&#730<sup>C</sup> and  
   ${json.weather[0].description}.</h2>`;
@@ -29,8 +29,8 @@ fetch(weatherAPI)
     const sunset = new Date(json.sys.sunset * 1000);
     const sunsetTime = sunset.toLocaleTimeString([], { timeStyle: "short" });
 
-    conSunrise.innerHTML = `<h2> Sunrise ${sunriseTime}</h2>`;
-    conSunset.innerHTML = `<h2> Sunset ${sunsetTime} </h2>`;
+    containerSunrise.innerHTML = `<h2> Sunrise ${sunriseTime}</h2>`;
+    containerSunset.innerHTML = `<h2> Sunset ${sunsetTime} </h2>`;
   });
 
 //Five day forcast
@@ -49,7 +49,7 @@ fetch(forecastAPI)
       const weekdays = ["SUN", "MON", "TUE", "WED", "THU", "FRI", "SAT"];
       let dayOfWeek = weekdays[date.getDay()];
 
-      conForecast.innerHTML += `<h2>${dayOfWeek} <span>${day.main.temp.toFixed(
+      containerForecast.innerHTML += `<h2>${dayOfWeek} <span>${day.main.temp.toFixed(
         1
       )}&#730<sup>C</sup></span></h2>`;
     });
