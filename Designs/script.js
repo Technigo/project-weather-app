4c852b3e00209fe353ba2603f604943327eea0e9
Tuliany <tuliany@mail.com>
2020-02-18 11:43:05 +0100

    Dagens väder

--- Designs/script.js
+++ Designs/script.js
@@ -1,4 +1,4 @@
-const container = document.getElementById('astros')
+const container = document.getElementById('MarbellaWeather')
 
 fetch('http://api.openweathermap.org/data/2.5/weather?q=Marbella,Spain&units=metric&APPID=329f2a2705f51547d2ed78a937fa0051')
   .then((response) => {
@@ -10,6 +10,26 @@ fetch('http://api.openweathermap.org/data/2.5/weather?q=Marbella,Spain&units=met
     city.innerHTML = json.name
     temp.innerHTML = `${json.main.temp.toFixed(1)}°`
     description.innerHTML = json.weather[0].description
+
+    const clear = {
+      image: 'clearsky.png'
+    }
+    const cloudy = {
+      image: 'cloudy.png'
+    }
+    const bad = {
+      image: 'cold.png'
+    }
+    const weather = () => {
+      if (json.main.temp >= 19) {
+        document.getElementById('weatherImage').src = clear.image
+      } else if (json.main.temp >= 15) {
+        document.getElementById('weatherImage').src = cloudy.image
+      } else {
+        document.getElementByID('weatherImage').src = bad.image
+      }
+    }
+    weather()
 
     const sunriseConversion = new Date(json.sys.sunrise * 1000)
     const sunsetConversion = new Date(json.sys.sunset * 1000)
@@ -25,6 +45,7 @@ fetch('http://api.openweathermap.org/data/2.5/weather?q=Marbella,Spain&units=met
     sunset.innerHTML = `Sunset: ${sunsetTime}`
   })
 
+
 fetch('https://api.openweathermap.org/data/2.5/forecast?q=marbella,spain&units=metric&appid=329f2a2705f51547d2ed78a937fa0051')
   .then((response) => {
     return response.json()
@@ -37,9 +58,29 @@ fetch('https://api.openweathermap.org/data/2.5/forecast?q=marbella,spain&units=m
       const dateString = date.toLocaleDateString('en-us', {
         weekday: 'short'
       })
+
       containerFiveDays.innerHTML += ` <p> ${dateString} ${day.main.temp.toFixed(1)} °</p> `
-
     })
-
-
   })
+
+// const clear = {
+//   image: 'clearsky.png'
+// }
+// const cloudy = {
+//   image: 'cloudy.png'
+// }
+// const bad = {
+//   image: 'cold.png'
+// }
+// const weather = () => {
+
+//   if (json.weather.main === 'clear') {
+//     document.getElementById('weatherImage').src = clear.image
+//   } else if (json.weather.main === 'clouds') {
+//     document.getElementById('weatherImage').src = cloudy.image
+//   } else {
+//     document.getElementByID('weatherImage').src = bad.image
+//   }
+//   weather()
+//   containerFiveDays.innerHTML += ` <p> ${dateString} ${weather.image} ${day.main.temp.toFixed(1)} °</p> `
+// }
