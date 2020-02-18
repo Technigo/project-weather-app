caf4f195e529ea7c765a3fe70f539774851f88a0
camilla <camilla.gausland@gmail.com>
2020-02-18 16:43:18 +0100

    mobile version done

--- weather.js
+++ weather.js
@@ -10,6 +10,7 @@ const conditionImg = document.getElementById('weatherImage')
 
 
 
+
 fetch('http://api.openweathermap.org/data/2.5/weather?q=Stockholm,Sweden&units=metric&APPID=ed2d4804e9f1b16e037cdf4e25100bc4')
 .then((response) => {
   return response.json()
@@ -21,11 +22,21 @@ fetch('http://api.openweathermap.org/data/2.5/weather?q=Stockholm,Sweden&units=m
   containerCelcius.innerHTML += `<h1> ${tempAsJSON}${'&#730;'}<sup>C</sup>  </h1>`
   json.weather.forEach( (report)=> {
   containerWeather.innerHTML += `<h2>${report.description}</h2>`  
-  
-
-
 
 });
+
+
+if (tempAsJSON <= 0) {
+  document.getElementById("backgroundColor").style.backgroundColor = 'rgb(112, 112, 114)';
+} else if (tempAsJSON >= 0 && tempAsJSON <= 10) {
+  document.getElementById("backgroundColor").style.backgroundColor = "rgb(107, 144, 247)";
+} else if (tempAsJSON >= 10.1 && tempAsJSON <= 20) {
+  document.getElementById("backgroundColor").style.backgroundColor = "rgb(234, 243, 105)";
+} else {
+  document.getElementById("backgroundColor").style.backgroundColor = "rgb(240, 186, 85)";
+}
+
+
 
 
 
@@ -36,6 +47,15 @@ const weatherToday = () => {
   let sunset = new Date(json.sys.sunset * 1000).toLocaleTimeString([], {timeStyle:'short'})
  containerSunsetRise.innerHTML += `<h4> Sunrise: ${sunrise} </h4> <h4>Sunset: ${sunset} </h4>`
  
+
+ var now = new Date();
+ var short = now.toLocaleTimeString([], {timeStyle:'short'})
+ if (short >= sunrise && short <= sunset) {
+  document.getElementById("backgroundColor").style.backgroundColor = "rgb(107, 144, 247)"
+ } else {
+  document.getElementById("backgroundColor").style.backgroundColor = "rgb(45, 43, 131)";
+ }
+
 }
 
 
@@ -53,61 +73,29 @@ fetch('https://api.openweathermap.org/data/2.5/forecast?q=Stockholm,Sweden&units
 })
 
 .then((json) => {
-  //let middle = json.list.forEach(temperature => document.getElementById('forecast').innerHTML += `<p> ${temperature.dt} Min temp: ${temperature.main.temp_min} </p>`) 
   
   const filteredForecast = json.list.filter(item => item.dt_txt.includes('12:00'))
   
   
 
-// filteredForecast.forEach((temp) => {
-//   let temperature = temp.main.temp_max
-//   console.log(temperature)
-
-// })
-
-// filteredForecast.forEach((days) => {
-//   let daysName = new Date(days.dt)
-//   console.log(daysName.toDateString())
-// })
 
 //This Works
 filteredForecast.forEach((temp) => {
   let temperature = temp.main.temp 
-  //let daysName =  (temp.dt_txt)
   console.log(temperature)
   console.log(temp)
-  //console.log(daysName)
   const temperatureDays = new Date (temp.dt_txt)
   const correctDayFormat = temperatureDays.toLocaleDateString('en-US', {weekday:'short'})
   const pictures =  `<img id="picsForecast" src="https://openweathermap.org/img/wn/${temp.weather[0].icon}@2x.png" alt="" />`
-//`<img src="https://openweathermap.org/img/wn/${json.weather[0].icon}@2x.png" alt="" />`;
   document.getElementById('forecast').innerHTML += `<h6> ${correctDayFormat}  ${temperature} ${'&#730;'} c ${pictures} </h6>`
   
 })
 
 
 
-//fungerade dagar!!
-// filteredForecast.forEach((temp) => {
-//   const temperatureDays = new Date (temp.dt_txt)
-//   const correctDayFormat = temperatureDays.toLocaleDateString('en-US', {weekday:'short'})
-//   containerForecast.innerHTML+= `<p> ${correctDayFormat} </p>`
-// })
-
- 
-  //console.log(daysName.toDateString())
-
-
-
-//document.getElementById('forecast').innerHTML += `${days.daysName} ${temp.temperature}`
-  
-// filteredForecast.forEach((item) => {
-//  document.getElementById('forecast').innerHTML += `<p> ${item.dt} Max temp: ${item.main.temp_max} </p>`
-// })
 
 
   
   })
    
 
-//})
