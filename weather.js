3755808a8d7acf97e2fbb5ef56a341b7fcec7ff1
camilla <camilla.gausland@gmail.com>
2020-02-19 18:14:40 +0100

    responsive

--- weather.js
+++ weather.js
@@ -9,13 +9,11 @@ const currentCondition = document.getElementById('weather')
 const conditionImg = document.getElementById('weatherImage')
 const countries = document.getElementById('otherCountries')
 
-
 const uppsala = 'https://api.openweathermap.org/data/2.5/weather?q=Uppsala,Sweden&units=metric&APPID=ed2d4804e9f1b16e037cdf4e25100bc4'
 const uppsalaForecast = 'https://api.openweathermap.org/data/2.5/forecast?q=Stockholm,Sweden&units=metric&APPID=ed2d4804e9f1b16e037cdf4e25100bc4'
 const alicante = 'https://api.openweathermap.org/data/2.5/weather?q=Alicante,Spain&units=metric&APPID=ed2d4804e9f1b16e037cdf4e25100bc4'
 const london = 'https://api.openweathermap.org/data/2.5/weather?q=London,UK&units=metric&APPID=ed2d4804e9f1b16e037cdf4e25100bc4'
 const tampa = 'https://api.openweathermap.org/data/2.5/weather?q=Tampa,US&units=metric&APPID=ed2d4804e9f1b16e037cdf4e25100bc4'
-
 
 fetch(uppsala)
 .then((response) => {
@@ -36,40 +34,30 @@ const weatherToday = () => {
   conditionImg.innerHTML += `<img class="weatherSymbol" src="${weatherSymbol}.png" alt="">`
   let sunrise = new Date(json.sys.sunrise * 1000).toLocaleTimeString([], {timeStyle:'short'})
   let sunset = new Date(json.sys.sunset * 1000).toLocaleTimeString([], {timeStyle:'short'})
- containerSunsetRise.innerHTML += `<h4> Sunrise: ${sunrise} </h4> <h4>Sunset: ${sunset} </h4>`
+ containerSunsetRise.innerHTML += `<h4> Sunrise: ${sunrise} <img class="up-down" src="sunrise.png"> </h4> <h4>Sunset: ${sunset} <img class="up-down" src="sunset.png"> </h4>`
  
 
  var now = new Date();
  var short = now.toLocaleTimeString([], {timeStyle:'short'})
+
  if (short >= sunrise && short <= sunset) {
   document.getElementById("backgroundColor").classList.toggle('day')
-  
  } else {
   document.getElementById("backgroundColor").classList.toggle('night')
   containerSunsetRise.style.color = 'rgba(255, 255, 255, 0.911)'
-  
  }
-
 }
-
-
-
-
 
 weatherToday()
 })
-
-
 
 fetch(uppsalaForecast)
 .then((response) => {
   return response.json()
 })
-
 .then((json) => {
-  
   const filteredForecast = json.list.filter(item => item.dt_txt.includes('12:00'))
-filteredForecast.forEach((temp) => {
+  filteredForecast.forEach((temp) => {
   let temperature = temp.main.temp.toFixed(0) 
   console.log(temperature)
   console.log(temp)
