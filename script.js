0af391230e3965867efd1c8ef627c48cda26981d
linneanylen <48217204+linneanylen@users.noreply.github.com>
2020-02-19 12:31:31 +0100

    added if statements to change text and colors and added media queires

--- script.js
+++ script.js
@@ -1,8 +1,12 @@
 const text = document.getElementById('weather')
 const information = document.getElementById('information')
 const week = document.getElementById('week')
+const color = document.getElementById('color')
+const todaysWeatherURL = `http://api.openweathermap.org/data/2.5/weather?q=Stockholm,Sweden&units=metric&APPID=ed6c467c63ec2c7c04687ba8b5eb0732`
+const forecastWeatherURL = `https://api.openweathermap.org/data/2.5/forecast?q=Stockholm,Sweden&units=metric&APPID=ed6c467c63ec2c7c04687ba8b5eb0732`
 
-fetch('http://api.openweathermap.org/data/2.5/weather?q=Stockholm,Sweden&units=metric&APPID=ed6c467c63ec2c7c04687ba8b5eb0732')
+
+fetch(todaysWeatherURL)
   .then((response) => {
     return response.json()
   })
@@ -19,23 +23,44 @@ fetch('http://api.openweathermap.org/data/2.5/weather?q=Stockholm,Sweden&units=m
     information.innerHTML = `<h1>${json.weather[0].description} | ${temp}°</h1>`
     information.innerHTML += `<h1>sunrise ${sunriseTime}</h1>`
     information.innerHTML += `<h1>sunset ${sunsetTime}</h1>`
-    text.innerHTML = `<h1>Light a fire and get cosy. ${json.name} is looking grey today.</h1>`
+
+
+    const todaysWeather = json.weather[0].main
+
+    const icon = `Designs/Design-2/icons/noun_${todaysWeather}.svg`
+
+    image.innerHTML = `<img src= ${icon}>`
+
+    if (todaysWeather === "Clouds")
+      text.innerHTML = `<h1>Light a fire and get cosy. ${json.name} is looking grey today.</h1>`
+    else if (todaysWeather === "Clear")
+      text.innerHTML = `<h1>Get your sunnies on. ${json.name} is looking rather great today.</h1>`
+    else text.innerHTML = `<h1>Don't forget your umbrella. It's wet in ${json.name}  today.</h1>`
+
+    if (todaysWeather === "Clouds")
+      document.getElementById("color").className = "clouds"
+    else if (todaysWeather === "Clear")
+      document.getElementById("color").className = "clear"
+    else document.getElementById("color").className = "rain"
+
   })
 
 
-fetch('https://api.openweathermap.org/data/2.5/forecast?q=Stockholm,Sweden&units=metric&APPID=ed6c467c63ec2c7c04687ba8b5eb0732')
+fetch(forecastWeatherURL)
   .then((response) => {
     return response.json()
   })
   .then((json) => {
-    const filteredForecast = json.list.filter(item => item.dt_txt.includes('03:00'))
+    const filteredForecast = json.list.filter(item => item.dt_txt.includes('12:00'))
 
     containerFiveDays.innerHTML = ""
     filteredForecast.forEach(day => {
       const date = new Date(day.dt * 1000)
-      const weekdays = ["Sun", "Mon", "Tue", "Wed", "Thu", "Fri", "Sat"]
-      let dayOfWeek = weekdays[date.getDay()]
+      console.log(date)
+      let dayOfWeek = date.toLocaleDateString('en-US', { weekday: 'short' })
+
       containerFiveDays.innerHTML += `<p> ${dayOfWeek} ${day.main.temp.toFixed(0)} °C </p>`
     })
 
   })
+
