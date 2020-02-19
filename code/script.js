cb01f61adad274dc5bed59e903ed90e054a8a2cf
annesophiegendron <annesophie.gendron@gmail.com>
2020-02-19 10:06:41 +0100

    dark mode updated

--- code/script.js
+++ code/script.js
@@ -76,3 +76,23 @@ fetch(apiForecast)
       containerFiveDays.innerHTML += `<p> ${weekDaysString} ${day.main.temp.toFixed(0)} °C</p>`
     })
   });
+
+// this one is jut to wait for the page to load
+document.addEventListener('DOMContentLoaded', () => {
+
+  const themeStylesheet = document.getElementById('theme');
+  const themeToggle = document.getElementById('theme-toggle');
+  themeToggle.addEventListener('click', () => {
+    // if it's light -> go dark
+    if (themeStylesheet.href.includes('light')) {
+      themeStylesheet.href = 'dark-theme.css';
+      themeToggle.innerText = 'Switch to light mode';
+    } else {
+      // if it's dark -> go light
+      themeStylesheet.href = 'light-theme.css';
+      themeToggle.innerText = 'Switch to dark mode';
+    }
+    localStorage.setItem('theme', 'dark-theme.css');
+
+  })
+})
