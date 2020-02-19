6b3ed1d5c00537be55d7fbec2184be0c1abbfaa7
annesophiegendron <annesophie.gendron@gmail.com>
2020-02-19 22:39:25 +0100

    local storage updated

--- code/script.js
+++ code/script.js
@@ -83,15 +83,13 @@ document.addEventListener('DOMContentLoaded', () => {
   const themeStylesheet = document.getElementById('theme');
   const themeToggle = document.getElementById('theme-toggle');
   themeToggle.addEventListener('click', () => {
-    // if it's light -> go dark
     if (themeStylesheet.href.includes('light')) {
       themeStylesheet.href = 'dark-theme.css';
       themeToggle.innerText = 'Switch to light mode';
     } else {
-      // if it's dark -> go light
       themeStylesheet.href = 'light-theme.css';
       themeToggle.innerText = 'Switch to dark mode';
     }
-    localStorage.setItem('theme', 'dark-theme.css');
+    localStorage.setItem('theme', theneStylesheet.href);
   })
-})
+});
