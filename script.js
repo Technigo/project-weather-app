d0a768fba6f88213ee7701f5e898496378b238f9
Anna T <annat@salmas-MacBook-Air.local>
2020-02-19 13:36:08 +0100

    added weather icons

--- script.js
+++ script.js
@@ -1,9 +1,10 @@
-const temperature = document.getElementById('temperature')
-const city = document.getElementById('city')
-const description = document.getElementById('description')
-const sunrise = document.getElementById('sunrise')
-const sunset = document.getElementById('sunset')
-const weatherPic = document.getElementById('weatherPic')
+const temp = document.getElementById('temperature')
+const cityName = document.getElementById('city')
+const weatherDescription = document.getElementById('description')
+const sunriseHour = document.getElementById('sunrise')
+const sunsetHour = document.getElementById('sunset')
+const weatherIcon = document.getElementById('weatherPic')
+const weatherIconSmall = document.getElementById('weatherPicSmall')
 
 
 fetch('http://api.openweathermap.org/data/2.5/weather?q=Stockholm&units=metric&appid=daa63c3c6a7ab1c38288ee0bfde25241')
@@ -19,26 +20,30 @@ fetch('http://api.openweathermap.org/data/2.5/weather?q=Stockholm&units=metric&a
     const sunsetTime = new Date(json.sys.sunset * 1000).toLocaleTimeString([], { hour: '2-digit', minute: '2-digit' })
     sunset.innerHTML = `sunset ${sunsetTime}`
 
-    let weatherIcon
     if (json.weather[0].id === 804) {
       //clouds
-      weatherIcon = '<img src="cloud.png"></img>'
+      weatherPic.innerHTML = '<img src="cloud.png">'
+      todaysWeather.style.background = 'linear-gradient(#747d8c, #ced6e0)'
     } else if (json.weather[0].id < 804 && json.weather[0].id > 800) {
       //partly cloudy
-      weatherIcon = '<img src="cloudy.png"></img>'
+      weatherPic.innerHTML = '<img src="cloudy.png">'
+      todaysWeather.style.background = 'linear-gradient(#70a1ff, #4b6584)'
     } else if (json.weather[0].id < 532 && json.weather[0].id > 499) {
       //rain
-      weatherIcon = '<img src="rainy.png"></img>'
+      weatherPic.innerHTML = '<img src="rainy.png">'
+      todaysWeather.style.background = 'linear-gradient(#2f3640, #7f8fa6)'
     } else if (json.weather[0].id < 623 && json.weather[0].id > 599) {
       //snow
-      weatherIcon = '<img src="snowy.png"></img>'
+      weatherPic.innerHTML = '<img src="snowy.png">'
+      todaysWeather.style.background = 'linear-gradient( #f7f1e3, #d1d8e0)'
     } else if (json.weather[0].id === 800) {
-      //clear/ sunny
-      weatherIcon = '<img src="sun.png"></img>'
+      //clear/sunny
+      weatherPic.innerHTML = '<img src="sun.png">'
+      todaysWeather.style.background = 'linear-gradient( #9599E2,  #8BC6EC)'
     } else {
-      weatherIcon = ' '
+      weatherPic.innerHTML = ' '
+      todaysWeather.style.background = 'linear-gradient( #9599E2,  #8BC6EC)'
     }
-    weatherPic.innerHTML = `${weatherIcon}`
   })
 
 
@@ -47,10 +52,30 @@ fetch('http://api.openweathermap.org/data/2.5/forecast?q=Stockholm&units=metric&
     return response.json()
   })
   .then((json) => {
-    const filteredForecast = json.list.filter(item => item.dt_txt.includes('03:00', '06:00', '09:00', '12:00', '15:00', '18:00', '21:00', '00:00'))
+    const filteredForecast = json.list.filter(item => item.dt_txt.includes('12:00'))
     filteredForecast.forEach((day) => {
-      const days = new Date(day.dt * 1000).toLocaleDateString('sv-SE', { weekday: 'long' })
-      fiveDays.innerHTML += `<p>${days} ${day.main.temp.toFixed(0)}<sup>&#8451</sup></p>`
+      const weekday = new Date(day.dt * 1000).toLocaleDateString('sv-SE', { weekday: 'long' })
+      fiveDays.innerHTML += `<p>${weekday} ${day.main.temp.toFixed(0)}<sup>&#8451</sup></p>`
+
+      if (day.weather[0].id === 800) {
+        //clear/sunny
+        weatherPicSmall.innerHTML += '<img src="sun.png">'
+      } else if (day.weather[0].id === 804) {
+        //clouds
+        weatherPicSmall.innerHTML += '<img src="cloud.png">'
+      } else if (day.weather[0].id < 532 && day.weather[0].id > 499) {
+        //rain
+        weatherPicSmall.innerHTML += '<img src="rainy.png">'
+      } else if (day.weather[0].id < 623 && day.weather[0].id > 599) {
+        //snow
+        weatherPicSmall.innerHTML += '<img src="snowy.png">'
+      } else if (day.weather[0].id < 804 && day.weather[0].id > 800) {
+        //partly cloudy
+        weatherPicSmall.innerHTML += '<img src="cloudy.png">'
+      }
+      else {
+        weatherPicSmall.innerHTML += ' '
+      }
     })
   })
 
