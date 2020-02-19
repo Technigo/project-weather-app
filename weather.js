f5f0779afa64272103f8300eeab90e5c3c49a5dc
camilla <camilla.gausland@gmail.com>
2020-02-19 17:26:40 +0100

    new pictures, new design

--- weather.js
+++ weather.js
@@ -7,42 +7,33 @@ const containerSunsetRise = document.getElementById('sunsetSunrise')
 const containerForecast = document.getElementById('forecast')
 const currentCondition = document.getElementById('weather')
 const conditionImg = document.getElementById('weatherImage')
+const countries = document.getElementById('otherCountries')
 
 
+const uppsala = 'https://api.openweathermap.org/data/2.5/weather?q=Uppsala,Sweden&units=metric&APPID=ed2d4804e9f1b16e037cdf4e25100bc4'
+const uppsalaForecast = 'https://api.openweathermap.org/data/2.5/forecast?q=Stockholm,Sweden&units=metric&APPID=ed2d4804e9f1b16e037cdf4e25100bc4'
+const alicante = 'https://api.openweathermap.org/data/2.5/weather?q=Alicante,Spain&units=metric&APPID=ed2d4804e9f1b16e037cdf4e25100bc4'
+const london = 'https://api.openweathermap.org/data/2.5/weather?q=London,UK&units=metric&APPID=ed2d4804e9f1b16e037cdf4e25100bc4'
+const tampa = 'https://api.openweathermap.org/data/2.5/weather?q=Tampa,US&units=metric&APPID=ed2d4804e9f1b16e037cdf4e25100bc4'
 
 
-fetch('http://api.openweathermap.org/data/2.5/weather?q=Stockholm,Sweden&units=metric&APPID=ed2d4804e9f1b16e037cdf4e25100bc4')
+fetch(uppsala)
 .then((response) => {
   return response.json()
 })
 
 .then((json) => {
   container.innerHTML = `<h1>${json.name}</h1>`
-  let tempAsJSON = json.main.temp.toFixed(1)
+  let tempAsJSON = json.main.temp.toFixed(0)
   containerCelcius.innerHTML += `<h1> ${tempAsJSON}${'&#730;'}<sup>C</sup>  </h1>`
   json.weather.forEach( (report)=> {
-  containerWeather.innerHTML += `<h2>${report.description}</h2>`  
+  containerWeather.innerHTML += `<h2>${report.description}</h2>` 
 
 });
-
-
-if (tempAsJSON <= 0) {
-  document.getElementById("backgroundColor").style.backgroundColor = 'rgb(112, 112, 114)';
-} else if (tempAsJSON >= 0 && tempAsJSON <= 10) {
-  document.getElementById("backgroundColor").style.backgroundColor = "rgb(107, 144, 247)";
-} else if (tempAsJSON >= 10.1 && tempAsJSON <= 20) {
-  document.getElementById("backgroundColor").style.backgroundColor = "rgb(234, 243, 105)";
-} else {
-  document.getElementById("backgroundColor").style.backgroundColor = "rgb(240, 186, 85)";
-}
-
-
-
-
-
 const weatherToday = () => {
   let windSpeed = json.wind.speed
-  conditionImg.innerHTML += `<img id="weatherPicToday" src="https://openweathermap.org/img/wn/${json.weather[0].icon}@2x.png" alt="" />`;
+  const weatherSymbol= json.weather[0].icon
+  conditionImg.innerHTML += `<img class="weatherSymbol" src="${weatherSymbol}.png" alt="">`
   let sunrise = new Date(json.sys.sunrise * 1000).toLocaleTimeString([], {timeStyle:'short'})
   let sunset = new Date(json.sys.sunset * 1000).toLocaleTimeString([], {timeStyle:'short'})
  containerSunsetRise.innerHTML += `<h4> Sunrise: ${sunrise} </h4> <h4>Sunset: ${sunset} </h4>`
@@ -51,23 +42,26 @@ const weatherToday = () => {
  var now = new Date();
  var short = now.toLocaleTimeString([], {timeStyle:'short'})
  if (short >= sunrise && short <= sunset) {
-  document.getElementById("backgroundColor").style.backgroundColor = "rgb(107, 144, 247)"
+  document.getElementById("backgroundColor").classList.toggle('day')
+  
  } else {
-  document.getElementById("backgroundColor").style.backgroundColor = "rgb(45, 43, 131)";
+  document.getElementById("backgroundColor").classList.toggle('night')
+  containerSunsetRise.style.color = 'rgba(255, 255, 255, 0.911)'
+  
  }
 
 }
 
 
 
+
+
 weatherToday()
-
-
 })
 
 
 
-fetch('https://api.openweathermap.org/data/2.5/forecast?q=Stockholm,Sweden&units=metric&APPID=ed2d4804e9f1b16e037cdf4e25100bc4')
+fetch(uppsalaForecast)
 .then((response) => {
   return response.json()
 })
@@ -75,27 +69,46 @@ fetch('https://api.openweathermap.org/data/2.5/forecast?q=Stockholm,Sweden&units
 .then((json) => {
   
   const filteredForecast = json.list.filter(item => item.dt_txt.includes('12:00'))
-  
-  
-
-
-//This Works
 filteredForecast.forEach((temp) => {
-  let temperature = temp.main.temp 
+  let temperature = temp.main.temp.toFixed(0) 
   console.log(temperature)
   console.log(temp)
   const temperatureDays = new Date (temp.dt_txt)
   const correctDayFormat = temperatureDays.toLocaleDateString('en-US', {weekday:'short'})
-  const pictures =  `<img id="picsForecast" src="https://openweathermap.org/img/wn/${temp.weather[0].icon}@2x.png" alt="" />`
-  document.getElementById('forecast').innerHTML += `<h6> ${correctDayFormat}  ${temperature} ${'&#730;'} c ${pictures} </h6>`
-  
-})
-
-
-
-
-
-  
+  const weatherSymbol= temp.weather[0].icon
+  document.getElementById('forecast').innerHTML += `<h6> ${correctDayFormat} ${temperature} ${'&#730;'}c <img class="weatherSymbolFore" src="${weatherSymbol}.png" alt=""> </h6> `
+})  
   })
    
+  fetch(london)
+  .then((response) => {
+    return response.json()
+  })
 
+  .then((json) => {
+    let tempCountries = json.main.temp.toFixed(0)
+    const weatherSymbol= json.weather[0].icon
+    countries.innerHTML += `<h2> ${json.name} ${tempCountries}${'&#730;'} c  <img class="weatherSymbolFore" src="${weatherSymbol}.png" alt=""></h2>`
+  })
+
+  fetch(alicante)
+  .then((response) => {
+    return response.json()
+  })
+
+  .then((json) => {
+    let tempCountries = json.main.temp.toFixed(0)
+    const weatherSymbol= json.weather[0].icon
+    countries.innerHTML += `<h2> ${json.name} ${tempCountries}${'&#730;'} c <img class="weatherSymbolFore" src="${weatherSymbol}.png" alt=""> </h2>`
+  })
+
+  fetch(tampa)
+  .then((response) => {
+    return response.json()
+  })
+
+  .then((json) => {
+    let tempCountries = json.main.temp.toFixed(0)
+   const weatherSymbol= json.weather[0].icon 
+   countries.innerHTML += `<h2> ${json.name} ${tempCountries}${'&#730;'} c  <img class="weatherSymbolFore" src="${weatherSymbol}.png" alt=""> </h2>`
+  })
