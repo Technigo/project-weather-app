8739b84a6f1f36756d7b124452253990e43183f3
elsareutersward <elsa.reutersward@gmail.com>
2020-02-20 22:10:27 +0100

    Final with Read me

--- README.md
+++ README.md
@@ -1,13 +1,13 @@
 # Weather App
 
-Replace this readme with your own information about your project.
-
-Start by briefly describing the assignment in a sentence or two. Keep it short and to the point.
+The aim of this project was to build a simple weather web app that shows today's weather and temperature, and a 5-day forecast using a weather API.
 
 ## The problem
 
-Describe how you approached to problem, and what tools and techniques you used to solve it. How did you plan? What technologies did you use? If you had more time, what would be next?
+First I focused on the main objective of getting the app to fetch the weather info for today's weather and the 5-day forecast. After I managed to get all the information I wanted to be displayed in the app, I focused on styling the app depending on the weather. I also decided to add animations for each weather type. After I had gotten the app to look as desired, I focused on making it more responsive and interactive. I wanted a potential user to be able to choose which city they wanted to see the weather for, why I created a search function which gets the weather info of a chosen city from the API. 
+
+If I had more time, I would change the default city (Stockholm) to the users location. I also think the styling for larger screens could be more fun.  
 
 ## View it live
 
-Every project should be deployed somewhere. Be sure to include the link to the deployed project so that the viewer can click around and see what it's all about.
+https://compassionate-hoover-547cdb.netlify.com/
--- script.js
+++ script.js
@@ -8,6 +8,7 @@ const containerSunset = document.getElementById('sunset')
 const containerSunsetTime = document.getElementById('sunsetTime')
 const containerForecastDay = document.getElementById('forecastWeatherDay')
 const containerForecastTemp = document.getElementById('forecastWeatherTemp')
+const errorMessage = document.getElementById('errorMessage')
 
 const clearSky = {
     name: '01d',
@@ -102,88 +103,100 @@ const weatherIcons = [
 ]
 
 
-fetch('https://api.openweathermap.org/data/2.5/weather?q=Stockholm&units=metric&APPID=d36e9003d656caeed3587a56f370edb7')
-    .then((response) => {
-        return response.json()
-    })
-    .then((json) => {
-        containerCurrentTemp.innerHTML = `<h1>${json.main.temp.toFixed(1)}&deg;C</h1>`
-        containerCity.innerHTML = `<h2>${json.name}</h2>`
-        containerCurrentWeather.innerHTML = `<p>${json.weather[0].description}</p>`
-        containerFeelsLike.innerHTML = `<p>feels like: ${json.main.feels_like}</p>`
-        
-        const unixTimestampSunrise = json.sys.sunrise
-        const unixTimestampSunset = json.sys.sunset
-        const sunrise = new Date(unixTimestampSunrise * 1000)
-        const sunset = new Date(unixTimestampSunset * 1000)
-        const sunriseTime = sunrise.toLocaleTimeString([], { timeStyle: 'short' })
-        const sunsetTime = sunset.toLocaleTimeString([], { timeStyle: 'short' })
-        containerSunrise.innerHTML = `<p>sunrise</p>`
-        containerSunriseTime.innerHTML = `<p>${sunriseTime}</p>`
-        containerSunset.innerHTML = `<p>sunset</p>` 
-        containerSunsetTime.innerHTML = `<p>${sunsetTime}</p>` 
-
-        function toggleAnimation(animation) {
-            document.getElementById(animation).style.visibility = 'visible'
-        }
-
-        console.log(json)
-        console.log(json.weather[0].icon.includes('n'))
-
-        const backgroundColor = weatherIcons.find(weatherIcon => json.weather[0].icon.includes(weatherIcon.name))
-        if (backgroundColor) {
-            const body = document.getElementById('body')
-            body.style.background = backgroundColor.backgroundColor
-            body.style.color = backgroundColor.color
-            toggleAnimation(backgroundColor.animation)
-        }
-    })
-
-
-    fetch('https://api.openweathermap.org/data/2.5/forecast?q=Stockholm&units=metric&APPID=d36e9003d656caeed3587a56f370edb7')
-    .then((response) => {
-        return response.json()
-    })
-    .then((json) => {
-        const filteredForecastMidnight = json.list.filter(item => item.dt_txt.includes('00:00:00'))
-        filteredForecastMidnight.forEach(forecast => {
-            let forecastElement = document.createElement('div')
-            let day = new Date(forecast.dt * 1000).getDay()
-            const days = ['sun', 'mon', 'tue', 'wed', 'thu', 'fri', 'sat']
-            forecastElement.innerHTML = `
-                <div class="forecastDays">${days[day]}</div>`
-            forecastElement.classList.add(`forecast1`)
-            containerForecastDay.appendChild(forecastElement)
+const chooseWeather = (city) => {
+    containerForecastTemp.innerHTML = ''
+    containerForecastDay.innerHTML = ''
+    errorMessage.innerHTML = ''
+    
+    fetch(`https://api.openweathermap.org/data/2.5/weather?q=${city}&units=metric&APPID=d36e9003d656caeed3587a56f370edb7`)
+        .then((response) => {
+            return response.json()
         })
+        .then((json) => {
+            containerCurrentTemp.innerHTML = `<h1>${json.main.temp.toFixed(1)}&deg;C</h1>`
+            containerCity.innerHTML = `<h2>${json.name}</h2>`
+            containerCurrentWeather.innerHTML = `<p>${json.weather[0].description}</p>`
+            containerFeelsLike.innerHTML = `<p>feels like: ${json.main.feels_like}</p>`
+            
+            const unixTimestampSunrise = json.sys.sunrise
+            const unixTimestampSunset = json.sys.sunset
+            const sunrise = new Date(unixTimestampSunrise * 1000)
+            const sunset = new Date(unixTimestampSunset * 1000)
+            const sunriseTime = sunrise.toLocaleTimeString([], { timeStyle: 'short' })
+            const sunsetTime = sunset.toLocaleTimeString([], { timeStyle: 'short' })
+            containerSunrise.innerHTML = `<p>sunrise</p>`
+            containerSunriseTime.innerHTML = `<p>${sunriseTime}</p>`
+            containerSunset.innerHTML = `<p>sunset</p>` 
+            containerSunsetTime.innerHTML = `<p>${sunsetTime}</p>` 
 
-        const dateObject = {}
-        const forecastData = json.list
-        forecastData.forEach(item => {
-            const date = item.dt_txt.split(" ")[0]
-            if (dateObject[date]) {
-                dateObject[date].push(item)
-            } else {
-                dateObject[date] = [item]
+            function toggleAnimation(animation) {
+                document.getElementById(animation).style.visibility = 'visible'
             }
-            console.log(dateObject)
+
+            const backgroundColor = weatherIcons.find(weatherIcon => json.weather[0].icon.includes(weatherIcon.name))
+            if (backgroundColor) {
+                const body = document.getElementById('body')
+                body.style.background = backgroundColor.backgroundColor
+                body.style.color = backgroundColor.color
+                toggleAnimation(backgroundColor.animation)
+            }
+        }).catch((error) => {
+            errorMessage.innerHTML = `Ops! Somebody didn't do their geography homework...`
+        }) 
+
+    fetch(`https://api.openweathermap.org/data/2.5/forecast?q=${city}&units=metric&APPID=d36e9003d656caeed3587a56f370edb7`)
+        .then((response) => {
+            return response.json()
         })
+        .then((json) => {
+            const filteredForecastMidnight = json.list.filter(item => item.dt_txt.includes('00:00:00'))
+            filteredForecastMidnight.forEach(forecast => {
+                let forecastElement = document.createElement('div')
+                let day = new Date(forecast.dt * 1000).getDay()
+                const days = ['sun', 'mon', 'tue', 'wed', 'thu', 'fri', 'sat']
+                forecastElement.innerHTML = `
+                    <div class="forecastDays">${days[day]}</div>`
+                forecastElement.classList.add(`forecast1`)
+                containerForecastDay.appendChild(forecastElement)
+            })
 
-        const filteredForecastLunch = json.list.filter(item => item.dt_txt.includes('12:00:00'))
-        filteredForecastLunch.forEach(forecast => {
-            let forecastElement = document.createElement('div')
-            const icon = weatherIcons.find(weatherIcon => forecast.weather[0].icon.includes(weatherIcon.name))
+            const dateObject = {}
+            const forecastData = json.list
+            forecastData.forEach(item => {
+                const date = item.dt_txt.split(" ")[0]
+                if (dateObject[date]) {
+                    dateObject[date].push(item)
+                } else {
+                    dateObject[date] = [item]
+                }
+            })
 
-            const date = forecast.dt_txt.split(" ")[0]
-            const weatherData = dateObject[date]
-            const temps = weatherData.map(value => value.main.temp)
+            const filteredForecastLunch = json.list.filter(item => item.dt_txt.includes('12:00:00'))
+            filteredForecastLunch.forEach(forecast => {
+                let forecastElement = document.createElement('div')
+                const icon = weatherIcons.find(weatherIcon => forecast.weather[0].icon.includes(weatherIcon.name))
 
-            const minTemp = Math.min(...temps)
-            const maxTemp = Math.max(...temps)
+                const date = forecast.dt_txt.split(" ")[0]
+                const weatherData = dateObject[date]
+                const temps = weatherData.map(value => value.main.temp)
 
-            forecastElement.innerHTML = `
-                <img class="forecastIcon" src="${icon.image}" alt="${forecast.weather[0].icon}"></img>
-                <div class="forecastTemp">${minTemp}&deg;C / ${maxTemp}&deg;C</div>`
-            forecastElement.classList.add(`forecast2`)
-            containerForecastTemp.appendChild(forecastElement)
-        })
-    })
+                const minTemp = Math.min(...temps)
+                const maxTemp = Math.max(...temps)
+
+                forecastElement.innerHTML = `
+                    <img class="forecastIcon" src="${icon.image}" alt="${forecast.weather[0].icon}"></img>
+                    <div class="forecastTemp">${minTemp}&deg;C / ${maxTemp}&deg;C</div>`
+                forecastElement.classList.add(`forecast2`)
+                containerForecastTemp.appendChild(forecastElement)
+            })
+        }).catch((error) => {
+            console.error(error)            
+        }) 
+}
+
+function search() {
+    const city = document.getElementById('citySearcher').value;
+    chooseWeather(city)
+}
+
+chooseWeather('Stockholm')
