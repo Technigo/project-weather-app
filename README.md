d2e07e44cf9007f8ed0ea722e950b2642fb2a0ab
palhamel <pal.hamel@gmail.com>
2020-02-20 11:06:13 +0100

    Add README and cleaned up code

--- README.md
+++ README.md
@@ -1,13 +1,17 @@
 # Weather App
 
-Replace this readme with your own information about your project.
+Mission was to build a simple weather web app that shows today's weather and temperature, and a 5-day forecast using a weather API.
 
-Start by briefly describing the assignment in a sentence or two. Keep it short and to the point.
 
 ## The problem
 
-Describe how you approached to problem, and what tools and techniques you used to solve it. How did you plan? What technologies did you use? If you had more time, what would be next?
+This has been a very fun project! 
+I started by getting the raw API data to show in the right way. Used the videos, tried a lot of things and used Google even more. Also had great conversations with my team. Then I started some simple styling and responsive design, mobile priority. Also some stretch goals i think.
+I coded in VS Code, used Live Server to update and the Inspector in Chrome to view code and errors. Also used some plug-ins for JSON to view the structure easier.
 
-## View it live
+If I hade more time it would be put into more functions and design. Would love to have the background color and gradient to match the local time and weather description. So if it's a bright sunrise it would show and so on.
 
-Every project should be deployed somewhere. Be sure to include the link to the deployed project so that the viewer can click around and see what it's all about.
+
+## View it live on Netlify:
+
+https://angry-engelbart-e435a1.netlify.com/
--- code/script.js
+++ code/script.js
@@ -16,32 +16,23 @@ const weatherOneDayApi =
   "https://api.openweathermap.org/data/2.5/weather?q=Stockholm,se&units=metric&APPID=44527f8be39feab9d034d48604c6b81d&lang=en";
 const weatherForecastApi =
   "https://api.openweathermap.org/data/2.5/forecast?q=Stockholm,se&units=metric&APPID=44527f8be39feab9d034d48604c6b81d&lang=en";
-// console.log(weatherForecast)
-// -------------------------------- 
 
-// document.body.style.background = "#"+((1<<24)*Math.random()|0).toString(16);
-
+// First API - day weather
 fetch(weatherOneDayApi)
   .then((response) => {
     return response.json();
   })
   .then((json) => {
-    console.log('checking first json', json) // checking the json from open weather
-    // container.innerHTML = `${json.main.temp.toFixed(1)}°`;
-    // cityWeather.innerHTML = `${json.name} is a great city! We need some more text.`;
-
+    // console.log('checking first json', json) // checking the json from open weather
     json.weather.forEach(sky => {
       weatherDescription.innerHTML += `${sky.description} | ${json.main.temp.toFixed(1)}° `;
       cityWeather.innerHTML = `${json.name}, prepare for ${sky.description} today.`;
     });
-    // weatherImage.innerHTML = `<img src="https://openweathermap.org/img/wn/${json.weather[0].icon}@2x.png" alt="Weather Icon" />`;
     weatherImage.innerHTML = `<img src="./img/${json.weather[0].icon}@2x.png" alt="Weather Icon" />`;
-
     // console.log('check if image is picked up:', weatherImage)
-
     // console.log(json.main.temp)
 
-    // Change main text color and icons based on temp:
+    // Change main text color and icons, based on temp:
     if (json.main.temp < 5) {
       cityWeather.classList.toggle('cold-temp-city')
       iconsTemp.innerHTML = `❄️☃️🙌🏻`
@@ -49,39 +40,30 @@ fetch(weatherOneDayApi)
       cityWeather.classList.toggle('hot-temp-city')
       iconsTemp.innerHTML = `☀️⛱😅`
     } else {
-      cityWeather.classList.remove('neutral-temp-city')
+      cityWeather.classList.toggle('neutral-temp-city')
       iconsTemp.innerHTML = `🌻👍🏻`
     }
-
     // Sunrise and Sunset:
     sunRise.innerHTML = weather.sunrise = new Date(json.sys.sunrise * 1000).toLocaleTimeString([], {
       hour: '2-digit', minute: '2-digit'
     });
-    // console.log(sunRise.innerHTML)
     sunSet.innerHTML = weather.sunset = new Date(json.sys.sunset * 1000).toLocaleTimeString([], {
       hour: '2-digit', minute: '2-digit'
     });
-    // -------------- 
   });
 
-// the weatherForecast - Step 4 :
-
+// Second API - forecast weather
 fetch(weatherForecastApi)
   .then((response) => {
     return response.json();
   })
   .then((json) => {
-    console.log('forecast json date split', json.list[0].dt_txt.split(' ')[0]);
-    console.log('forecast json object', json);
-    // ny Array på kl 12: 
+    // console.log('forecast json date split', json.list[0].dt_txt.split(' ')[0]);
+    // console.log('forecast json object', json);
+    // New filtered array: 
     const filteredForecast = json.list.filter(item => item.dt_txt.includes("09:00"));
-    console.log('filtered forecast array', filteredForecast);
+    // console.log('filtered forecast array', filteredForecast);
  
-    /*
-    filteredForecast.forEach((forecast) => {
-      weatherForecast.innerHTML += `<h3>date: ${forecast.dt} / ${forecast.weather[0].description} / ${forecast.main.temp.toFixed(1)}°C `;
-    });
-   */
     filteredForecast.forEach(day => {
       const date = new Date(day.dt * 1000)
       // console.log('new date:', date)
@@ -89,44 +71,7 @@ fetch(weatherForecastApi)
       let dayOfWeek = weekdays[date.getDay()];
       weatherForecastDay.innerHTML += `<p> ${dayOfWeek} <img src="./img/${day.weather[0].icon}@2x.png" alt="Weather Icon" /></p>`
       weatherForecastTemp.innerHTML += `<p> ${day.main.temp.toFixed(0)} ° | ${day.weather[0].main} </p>`
-      // weatherForecastIcon.innerHTML += `<img src="./img/${day.weather[0].icon}@2x.png" alt="Weather Icon" />`
-      // weatherForecastIcon.innerHTML += `<img src="./img/${filteredForecast.weather[0].icon}@2x.png" alt="Weather Icon" />`;
-    })
-    // console.log(filteredForecast.toLocaleString(`en-US`)); 
-    // console.log(weatherForecast);
-    // console.log(filteredForecast.forecast.main.temp);
-    // console.log(weather.sunrise = new Date(json.sys.sunrise * 1000).toLocaleTimeString([], {timeStyle: "short"}))
-  })
-
-
-/*
-const filteredForecast = json.list.filter(item => item.dt_txt.includes('03:00'));
-containerFiveDays.innerHTML = "";
-
-filteredForecast.forEach(day => {
- const date = new Date(day.dt * 1000)
- const weekdays = ["Sun", "Mon", "Tue", "Wed", "Thu", "Fri", "Sat"];
- let dayOfWeek = weekdays[date.getDay()];
-containerFiveDays.innerHTML += `<p> ${dayOfWeek} ${day.main.temp.toFixed(0)} °C </p>`
-      })
-*/
-
-/*
-   filteredForecast.forEach((forecast) => {
-      // let daysName = forecast.dt_txt
-      // console.log('daysName info:', daysName)
-      // weatherForecast.innerHTML += `<h3>${forecast.dt} </h3>`;
-      weatherForecast.innerHTML += `<h3>${forecast.dt} eller ${forecast.dt_txt} / ${forecast.weather[0].description} / ${forecast.main.temp.toFixed(1)}°C </h3>`;
     });
-*/
+  });
 
-// -------------- 
-// Weather Icon - Works men behöver flera statements
-/*
-const currentWeather = json.weather[0].icon
-console.log('icon:', currentWeather)
-if (json.weather[0].icon === '04d') {
-  weatherImage.innerHTML = `<img src="https://openweathermap.org/img/wn/${json.weather[0].icon}@2x.png" alt="" />`;
-}
-*/
-// -------------- 
+
