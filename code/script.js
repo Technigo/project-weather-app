561a64e59c8af8474d71ce85162476201f990de0
Your Name <malin.marie@hotmail.se>
2020-02-20 15:11:28 +0100

    changed file order

--- code/script.js
+++ code/script.js
@@ -20,13 +20,13 @@ fetch("http://api.openweathermap.org/data/2.5/forecast?q=karlstad,SWE&units=metr
   document.getElementById("sunrise").innerHTML += `&nbsp ${sunriseDateTime.getHours()}.${sunriseDateTime.getMinutes()}`;
   document.getElementById("sunset").innerHTML += `&nbsp ${sunsetDateTime.getHours()}.${sunsetDateTime.getMinutes()}`;
 
-  if (json.list[0].weather[0].id >= 802 && json.list[0].weather[0].id <= 804 ) { // cloudy weather
+  if (json.list[0].weather[0].main === "Clouds") { // cloudy weather
     document.getElementById("weatherAdvice").innerHTML = `Light a fire and get cosy. ${json.city.name} is looking grey today.`;
     document.getElementById("body").classList.add("cloudy");
     document.getElementById("weatherConditionsImage").src = "Designs/Design-2/icons/noun_Cloud_1188486.svg"
   }
 
-  else if (json.list[0].weather[0].id === 800 || json.list[0].weather[0].id === 801 )  { //sunny weather
+  else if (json.list[0].weather[0].id === 800)  { //sunny weather
     document.getElementById("weatherAdvice").innerHTML = `Don't forget your sunglasses. 
     ${json.city.name} is shining today!`;
     document.getElementById("body").classList.add("sunny");
@@ -43,46 +43,48 @@ fetch("http://api.openweathermap.org/data/2.5/forecast?q=karlstad,SWE&units=metr
     document.getElementById("weatherAdvice").innerHTML = `Maybe indoor activities today. 
     ${json.city.name} is full of ${json.list[0].weather[0].main.toLowerCase()}`;
     document.getElementById("body").classList.add("atmosphere");
-    
   }
   
-  else if (json.list[0].weather[0].icon === "13d" ) { // snow
+  else if (json.list[0].weather[0].main === "Snow") { // snow
       document.getElementById("weatherAdvice").innerHTML = `Is it Christmas already? 
       ${json.city.name} is full of ${json.list[0].weather[0].main.toLowerCase()} today`;
       document.getElementById("body").classList.add("snow");
       document.getElementById("weatherConditionsImage").src = "Designs/Design-2/icons/noun_snow.svg"
-    }
+  }
 
-  else if (json.list[0].weather[0].id >= 300 && json.list[0].weather[0].id <= 531) { // rain / drizzle
+  else if (json.list[0].weather[0].main === "Rain") { // rain / drizzle
       document.getElementById("weatherAdvice").innerHTML = `Bring your umbrella! 
       ${json.city.name} is full of ${json.list[0].weather[0].main.toLowerCase()} today`;
       document.getElementById("body").classList.add("rain");
       document.getElementById("weatherConditionsImage").src = "Designs/Design-2/icons/noun_Umbrella_2030530.svg"
-    }
+  }
 
-    else if (json.list[0].weather[0].id >= 300 && json.list[0].weather[0].id <= 531) { // thunder
+    else if (json.list[0].weather[0].main === "Thunderstorm") { // thunder
       document.getElementById("weatherAdvice").innerHTML = `Relax  indoors today! 
      There is a ${json.list[0].weather[0].main.toLowerCase()} in ${json.city.name}!`;
       document.getElementById("body").classList.add("thunder");
       document.getElementById("weatherConditionsImage").src = "Designs/Design-2/icons/noun_storm.svg"
-    }
+  }
  
 // this week's forecast
 
-const filteredJson = json.list.filter(item => item.dt_txt.includes('00:00:00')) // each days' weather at midnight
-console.log(filteredJson)
+const filteredJsonDay = json.list.filter(item => item.dt_txt.includes('00:00:00')) // each days' day of week
+console.log(filteredJsonDay)
 
-document.getElementById("day1").innerHTML = `${getDayOfWeek(filteredJson[0].dt)}`
-document.getElementById("day2").innerHTML = `${getDayOfWeek(filteredJson[1].dt)}`
-document.getElementById("day3").innerHTML = `${getDayOfWeek(filteredJson[2].dt)}`
-document.getElementById("day4").innerHTML = `${getDayOfWeek(filteredJson[3].dt)}`
-document.getElementById("day5").innerHTML = `${getDayOfWeek(filteredJson[4].dt)}`
+const filteredJsonTemp = json.list.filter(item => item.dt_txt.includes('12:00')) // each days' temp at noon
+console.log(filteredJsonTemp)
 
-document.getElementById("tempDay1").innerHTML = `${roundNumber(filteredJson[0].main.temp)}°c`
-document.getElementById("tempDay2").innerHTML = `${roundNumber(filteredJson[1].main.temp)}°c`
-document.getElementById("tempDay3").innerHTML = `${roundNumber(filteredJson[2].main.temp)}°c`
-document.getElementById("tempDay4").innerHTML = `${roundNumber(filteredJson[3].main.temp)}°c`
-document.getElementById("tempDay5").innerHTML = `${roundNumber(filteredJson[4].main.temp)}°c`
+document.getElementById("day1").innerHTML = `${getDayOfWeek(filteredJsonDay[0].dt)}`
+document.getElementById("day2").innerHTML = `${getDayOfWeek(filteredJsonDay[1].dt)}`
+document.getElementById("day3").innerHTML = `${getDayOfWeek(filteredJsonDay[2].dt)}`
+document.getElementById("day4").innerHTML = `${getDayOfWeek(filteredJsonDay[3].dt)}`
+document.getElementById("day5").innerHTML = `${getDayOfWeek(filteredJsonDay[4].dt)}`
+
+document.getElementById("tempDay1").innerHTML = `${roundNumber(filteredJsonTemp[0].main.temp)}°c`
+document.getElementById("tempDay2").innerHTML = `${roundNumber(filteredJsonTemp[1].main.temp)}°c`
+document.getElementById("tempDay3").innerHTML = `${roundNumber(filteredJsonTemp[2].main.temp)}°c`
+document.getElementById("tempDay4").innerHTML = `${roundNumber(filteredJsonTemp[3].main.temp)}°c`
+document.getElementById("tempDay5").innerHTML = `${roundNumber(filteredJsonTemp[4].main.temp)}°c`
  
 })
 
