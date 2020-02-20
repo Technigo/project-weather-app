98bde62d5fbe37ccc3a0ab843a4787987c4ea0ed
Lisas Sturegard <lisasturegard@hotmail.com>
2020-02-20 13:46:18 +0100

    In the middle of step five Weather App, requierements there, but lots of stlying to do

--- weather.js
+++ weather.js
@@ -1,42 +1,68 @@
+const weatherURL = `https://api.openweathermap.org/data/2.5/weather?q=Stockholm,sweden&units=metric&APPID=00620bb638ed0fa5525452696e39c3ed`
+const forecast = `https://api.openweathermap.org/data/2.5/forecast?q=Stockholm,Sweden&units=metric&APPID=00620bb638ed0fa5525452696e39c3ed`
+//APID = 00620bb638ed0fa5525452696e39c3ed
+
+
+// http://openweathermap.org/img/wn/10d@2x.png
+
+const tempToday = document.getElementById('headerTemp')
 const weatherInfo = document.getElementById('weather')
-const forecasted = document.getElementById('five-day-forecast')
+//const sunMoves = document.getElementById('SunRiseSet')
+const forecasted = document.getElementById('fiveDays')
+const body = document.getElementById('body')
 
-fetch('https://api.openweathermap.org/data/2.5/weather?q=Stockholm,sweden&units=metric&APPID=00620bb638ed0fa5525452696e39c3ed')
-  .then((response) =>{
-    return response.json()
-  })
-  .then((json) => {
-    console.log(json)
-    weatherInfo.innerHTML = `<h1>Weather in ${json.name}<h/1>`;
-    let temperature = Math.round(json.main.temp * 10) /10;
-    let feelsLike = Math.round(json.main.feels_like *10) /10;
-    weatherInfo.innerHTML += `<p>${temperature}&#730; feels like ${feelsLike}&#730;</p>`;
-    json.weather.forEach((sky) => {
-      weatherInfo.innerHTML += `<p>${sky.description}</p>`;
-    })
-    const rise = new Date(json.sys.sunrise * 1000);
+
+fetch(weatherURL)
+  .then(response => response.json())
+  .then((jsonWeather) => {
+    console.log(jsonWeather)
+    const temperature = Math.round(jsonWeather.main.temp * 10) /10;
+    const feelsLike = Math.round(jsonWeather.main.feels_like *10) /10;
+    tempToday.innerHTML = `<h1>${temperature}&#730;<h/1>`;
+    weatherInfo.innerHTML = `<h4>But feels like ${feelsLike}&#730; in </h4>`;
+    weatherInfo.innerHTML += `<h2>${jsonWeather.name}</h2>`;
+    jsonWeather.weather.forEach((sky) => {
+      weatherInfo.innerHTML += `<h3>${sky.description}</h3>`;
+    const rise = new Date(jsonWeather.sys.sunrise * 1000);
     const sunRise = rise.toLocaleTimeString('en-SE', {timeStyle: 'short'});
-    const set = new Date(json.sys.sunset * 1000);
+    const set = new Date(jsonWeather.sys.sunset * 1000);
     const sunSet = set.toLocaleTimeString('en-SE', {timeStyle: 'short'});
-    weatherInfo.innerHTML += `<p>Sunrise: ${sunRise}  Sunset: ${sunSet}</p>`
+    weatherInfo.innerHTML += `<p>Sunrise: ${sunRise} </p>`
+    weatherInfo.innerHTML +=`<p>Sunset: ${sunSet}</p>`
+    })
+    if (jsonWeather.main.temp > 20 ) {
+      body.classList.toggle("hot");
+    } else if (jsonWeather.main.temp > 10) {
+        body.classList.toggle("warm");
+    } else if (jsonWeather.main.temp > 0) {
+        body.classList.toggle("medium");
+    } else if (jsonWeather.main.temp > -10) {
+        body.classList.toggle("cold");
+    } else {
+        body.classList.toggle("freezing");
+      }
   })
 
-fetch('https://api.openweathermap.org/data/2.5/forecast?q=Stockholm,Sweden&units=metric&APPID=00620bb638ed0fa5525452696e39c3ed')
-  .then((response) => {
-    return response.json()
-  })
-  .then((json) => {
-    console.log(json)
-    const filteredForecast = json.list.filter(item => item.dt_txt.includes('12:00'))
+// http://openweathermap.org/img/wn/10d@2x.png
+
+fetch(forecast)
+  .then(response => response.json())
+  .then((jsonForecast) => {
+    console.log(jsonForecast)
+    const filteredForecast = jsonForecast.list.filter(item => item.dt_txt.includes('12:00'))
     console.log(filteredForecast)
     filteredForecast.forEach((forecast) => {
-      let temperature = Math.round(forecast.main.temp * 10) / 10;
-      let feelsLike = Math.round(forecast.main.feels_like * 10) / 10;
-      let upcom = new Date(forecast.dt_txt);
-      const fiveDays = upcom.toLocaleDateString('en-SE', {weekday: 'long'});
+     //  const forecastIcon = 
+      const temperature = Math.round(forecast.main.temp * 10) / 10;
+      const feelsLike = Math.round(forecast.main.feels_like * 10) / 10;
+      const upcomingDays = new Date(forecast.dt_txt);
+      const fiveDays = upcomingDays.toLocaleDateString('en-SE', {weekday: 'long'});
+      const icon = (forecast.weather[0].icon)
       console.log(fiveDays) 
       console.log(temperature, feelsLike)
-      forecasted.innerHTML += `<p>${fiveDays} ${temperature} but beware ${feelsLike}</p>`;
+      console.log("https://openweathermap.org/img/wn/${icon}@2x.png");
+      forecasted.innerHTML += `<p>${fiveDays} <p/>
+      <img src="https://openweathermap.org/img/wn/${icon}.png" class= "forecast-icon"/><p>${temperature}&#730; (Will feel like ${feelsLike}&#730;)</p>`
     })
   })
 
