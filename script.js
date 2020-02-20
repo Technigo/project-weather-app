992a51110db53fe3eef7f41816e3b2ce2c2ba8ab
Ida Abrahamsson <Ida Abrahamsson>
2020-02-20 23:31:34 +0100

    Made a city dropdown and added some bits

--- script.js
+++ script.js
@@ -1,30 +1,36 @@
 //API
-const weatherUrl = `http://api.openweathermap.org/data/2.5/weather?q=Umea,Sweden&units=metric&APPID=30014767311a3c96ba7b2be3dae96ec4`
+const weatherUrl = `http://api.openweathermap.org/data/2.5/weather?q=${city}&units=metric&APPID=30014767311a3c96ba7b2be3dae96ec4`
 
 
 // DOM Selectors
 const weatherHeader = document.getElementById('weatherHeader')
 const weatherSection = document.getElementById('weatherSection')
+const cityDropDown = document.getElementById('cityDropDown')
 
 
 
-
-
-
-fetch(weatherUrl)
-  .then((response) => {
-    return response.json()
-  })
-  .then((json) => {
-    console.log(json)
-    const icon = `http://openweathermap.org/img/w/${json.weather[0].icon}.png`
-    weatherSection.innerHTML = ` 
-      <h1>${json.name} </h1> 
-      <img class"weatherIcon" id="icon" src=${icon} />    
+const showWheater = city => {
+  fetch(weatherUrl)
+    .then((response) => {
+      return response.json()
+    })
+    .then((json) => {
+      console.log(json)
+      const icon = `http://openweathermap.org/img/w/${json.weather[0].icon}.png`
+      weatherSection.innerHTML = ` 
+      <h1>${json.name}</h1> 
+      <img class="weatherIcon" id="icon" src=${icon} alt="Weather icon"/>    
       <p class="description">${json.weather[0].main}</p>  
       <h2>${json.main.temp.toFixed(0)} °</h2>
-      
+      <img class="sunUp" id="upSun" src='assets/sunrise.png' alt="Sunrise icon"/>
+      <p class="sunUpData">${json.sys.sunrise}</p>
+      <img class="sunDown" id"downSun" src='assets/sunset.png' alt="Sunset icon"/>
+      <p class="sunDownData">${json.sys.sunset}</p>
       <img class="windImage" id='windIcon' src='assets/wind.png' alt="wind icon"/>
-      <p class="windData">${json.wind.speed} m/s</p>`
+      <p class="windData">${json.wind.speed} m/s</p>
+      `
 
-  })
+    })
+}
+
+cityDropDown.addEventListener("change", () => showWheater(cityDropDown.value))
