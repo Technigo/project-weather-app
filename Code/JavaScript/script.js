f9eebd0202cadbeceefdf642e32e3ad8a31a729a
Nathalie Rautell <nathalierautell@Nathalies-MacBook-Pro.local>
2020-02-21 20:49:54 +0100

    changed the styling in the different html files

--- Code/JavaScript/script.js
+++ Code/JavaScript/script.js
@@ -1,1 +1,40 @@
-const head = document.getElementById('head')
+// Select DOM items
+const menuBtn = document.getElementById("menuBtn");
+const closeBtn = document.getElementsByClassName("show");
+const menu = document.getElementById("menu");
+const menuNav = document.getElementById("menuNav");
+const menuBranding = document.getElementById("menuBranding");
+const navItems = document.querySelectorAll(".nav-item");
+
+
+// Open menu
+
+const toggleMenu = () => {
+  menuBtn.classList.add("close");
+  menu.classList.add("show");
+  menuNav.classList.add("show");
+  menuBranding.classList.add("show");
+  navItems.forEach(item => item.classList.add("show"))
+}
+
+// Close menu
+
+const closeMenu = () => {
+  menuBtn.classList.remove("close");
+  menu.classList.remove("show");
+  menuNav.classList.remove("show");
+  menuBranding.classList.remove("show");
+  navItems.forEach(item => item.classList.remove("show"))
+}
+
+const outsideClick = (click) => {
+
+}
+
+// Open or close menu by clicking or touching
+
+menuBtn.addEventListener("click", toggleMenu);
+menuBtn.addEventListener("touchstart", toggleMenu);
+
+menu.addEventListener("click", closeMenu);
+menu.addEventListener("touchstart", closeMenu);
