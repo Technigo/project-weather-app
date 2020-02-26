04f05ba9dfa8d49aff26450494290e19e617e515
Ragna Úlfars <55483422+Ragtulf@users.noreply.github.com>
2020-02-26 10:30:32 +0100

    Update code/script.js
    Co-Authored-By: Erica  Glimsholt <ericaglimsholt@gmail.com>

--- code/script.js
+++ code/script.js
@@ -30,7 +30,6 @@ fetch('https://api.openweathermap.org/data/2.5/weather?q=Reykjavik,Iceland&units
 
     const colorOfNight = {
       backgroundColor: '#160c3b',
-      // backgroundImage: linear-gradient('135deg', '#160c3b', '1%', '#5c61e7', '100%')
 
     }
 
