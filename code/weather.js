6a09fe355295a41369063cc4e15376a9e0823061
Lisas Sturegard <lisasturegard@hotmail.com>
2020-07-24 13:17:05 +0200

    Updated styling

--- code/weather.js
+++ code/weather.js
@@ -1,7 +1,8 @@
 const tempToday = document.getElementById('headerTemp')
 const skyToday = document.getElementById('headerPicture')
 const weatherInfo = document.getElementById('weather')
-const sunMoves = document.getElementById('sunRiseSet')
+const sunUp = document.getElementById('sunRise')
+const sunDown = document.getElementById('sunSet')
 const forecasted = document.getElementById('fiveDays')
 const body = document.getElementById('body')
 const locationDropdown = document.getElementById('locationDropdown')
@@ -15,6 +16,9 @@ function displayLocationInfo(position) {
   const longitude = position.coords.longitude;
   const latitude = position.coords.latitude;
 
+  // const apiURL https://api.openweathermap.org/data/2.5/
+  // const APPID
+
   const weatherURL = `https://api.openweathermap.org/data/2.5/weather?lat=${latitude}&lon=${longitude}&units=metric&APPID=00620bb638ed0fa5525452696e39c3ed`
   const forecastURL = `https://api.openweathermap.org/data/2.5/forecast?lat=${latitude}&lon=${longitude}&units=metric&APPID=00620bb638ed0fa5525452696e39c3ed`
 
@@ -23,7 +27,7 @@ function displayLocationInfo(position) {
     .then((jsonWeather) => {
       const temperature = Math.round(jsonWeather.main.temp * 10) / 10;
       const feelsLike = Math.round(jsonWeather.main.feels_like * 10) / 10;
-      tempToday.innerHTML += `<h1>${temperature}&#730;<h/1>`;
+      tempToday.innerHTML += `<h1>${temperature}&#730;</h1>`;
       weatherInfo.innerHTML = `<h4>But feels like ${feelsLike}&#730; in </h4>`;
       weatherInfo.innerHTML += `<h2>${jsonWeather.name}</h2>`;
       jsonWeather.weather.forEach((sky) => {
@@ -32,8 +36,8 @@ function displayLocationInfo(position) {
         const sunRise = rise.toLocaleTimeString('en-SE', { timeStyle: 'short' });
         const set = new Date(jsonWeather.sys.sunset * 1000);
         const sunSet = set.toLocaleTimeString('en-SE', { timeStyle: 'short' });
-        sunMoves.innerHTML += `<p>Sunrise: ${sunRise} </p>`
-        sunMoves.innerHTML += `<p>Sunset: ${sunSet}</p>`
+        sunUp.innerHTML += `<p>Sunrise: </p> <p>${sunRise} </p>`
+        sunDown.innerHTML += `<p>Sunset: </> <p>${sunSet} </p>`
       })
       if (jsonWeather.main.temp > 20) {
         body.classList.toggle("hot");
@@ -46,16 +50,16 @@ function displayLocationInfo(position) {
       } else {
         body.classList.toggle("freezing");
       }
-      if (jsonWeather.weather[0].main == "Clear") {
+      if (jsonWeather.weather[0].main === "Clear") {
         skyToday.innerHTML = `<img class="weather-image" src="./assets/sun.png">`
-      } else if (jsonWeather.weather[0].main == "Clouds") {
+      } else if (jsonWeather.weather[0].main === "Clouds") {
         skyToday.innerHTML = `<img class="weather-image" src="./assets/cloud.png">`
-      } else if (jsonWeather.weather[0].main == "Rain") {
+      } else if (jsonWeather.weather[0].main === "Rain" || "Drizzle") {
         skyToday.innerHTML = `<img class="weather-image" src="./assets/rain.png">`
-      } else if (jsonWeather.weather[0].main == "Snow") {
+      } else if (jsonWeather.weather[0].main === "Snow") {
         skyToday.innerHTML = `<img class="weather-image" src="./assets/snow.png">`
       } else {
-        skyToday.innerHTML = `<img class="weather-image" src=".assets/alltInget.png">`
+        skyToday.innerHTML = `<img class="weather-image" src=".assets/cloud.png">`
       }
     })
 
