6566c48f50a384e7c3512ffec3686a165fc4c287
Andrea Osmar <andrea88@live.se>
2020-09-19 18:46:09 +0200

    Step 1 - Starting with API's

---
+++ script.js
@@ -0,0 +1,4 @@
+// API URL'S
+
+const weatherURL = 'https://api.openweathermap.org/data/2.5/weather?q=Stockholm,Sweden&units=metric&APPID=60032cdd91d77852bfb39762c09118fe'
+const forecastURL = 'https://api.openweathermap.org/data/2.5/forecast?q=Stockholm,Sweden&units=metric&APPID=60032cdd91d77852bfb39762c09118fe'
