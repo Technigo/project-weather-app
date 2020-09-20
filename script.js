051dfde03a0d8cafbc8d42180585e4f9c53758f6
Moa <63230455+mhammarback@users.noreply.github.com>
2020-09-20 12:13:44 +0200

    fetch city name and temperature achieved

---
+++ script.js
@@ -0,0 +1,35 @@
+const nameContainer = document.getElementById('name');
+const tempContainer = document.getElementById('temperature');
+const sortContainer = document.getElementById('sort');
+
+//Console.log test 
+fetch('https://api.openweathermap.org/data/2.5/weather?q=Stockholm,Sweden&units=metric&APPID=54a820a4b63e82050a15212c06998bb0')
+    .then((Response) => { 
+        return Response.json()
+    })
+    .then ((json) => {
+    console.log(json)   
+ })    
+
+
+//NAME OF CITY 
+fetch('https://api.openweathermap.org/data/2.5/weather?q=Stockholm,Sweden&units=metric&APPID=54a820a4b63e82050a15212c06998bb0')
+    .then((Response) => { 
+        return Response.json()
+    })
+    .then ((json) => {
+        nameContainer.innerHTML = `<h1>Lets find out how the wheater is in ${json.name}</h1>`
+    })         
+
+
+
+// TEMPERATURE 
+fetch('https://api.openweathermap.org/data/2.5/weather?q=Stockholm,Sweden&units=metric&APPID=54a820a4b63e82050a15212c06998bb0')
+    .then((Response) => { 
+        return Response.json()
+    })
+    .then ((json) => {
+        tempContainer.innerHTML = `<h1>The temperature is${json.main.temp}</h1>`
+    }) 
+   
+ 
