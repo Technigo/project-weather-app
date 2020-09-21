e8f5af8b2a0185101311b27fa30a54516ca84d10
Vanessa Sue Smith <sue27.vanessa@gmail.com>
2020-09-21 19:53:17 +0200

    Optimize function to generate data for forecast 5 days

--- code/script.js
+++ code/script.js
@@ -39,65 +39,53 @@ fetch(`http://api.openweathermap.org/data/2.5/weather?q=Stockholm,Sweden&units=m
   });
 
 // New Fetch for the 5 days Forecast
-  fetch(`https://api.openweathermap.org/data/2.5/forecast?q=Stockholm,Sweden&units=metric&APPID=${API_KEY}`)
-    .then((response) => {
-      return response.json();
-    })
+const forecastContent = document.getElementById('main-forecast-data');
 
-    .then ((json) => {
-      //Filters out forecast at 12:00 for coming 5 days
-      const filteredForecast = json.list.filter(item => item.dt_txt.includes('12:00'));
+fetch(`https://api.openweathermap.org/data/2.5/forecast?q=Stockholm,Sweden&units=metric&APPID=${API_KEY}`)
+  .then((response) => {
+    return response.json();
+  })
 
-      for (let i=0; i < filteredForecast.length; i++) {  //Loop to generate data to be shown for coming 5 days
+  .then ((json) => {
+    //Filters out forecast at 12:00 for coming 5 days
+    const filteredForecast = json.list.filter(item => item.dt_txt.includes('12:00'));  //Creates array with data for coming 5 days
+    console.log(filteredForecast);
 
-        const unixDay = filteredForecast[i].dt;  // Date in UNIX
-        const unixDayToMili = new Date(unixDay * 1000);  // Convert to nice date format we can use
+    filteredForecast.forEach((forecastDay) => { //Calls generateHTMLForForecast function to dynamically create the HTML content for the coming 5 days
+      forecastContent.innerHTML += generateHTMLForForecast(forecastDay);
+    });  
+  })
 
-        let weekday = unixDayToMili.getDay();  //getDay will return a number between 0-6, switch statement to return the date's name, not just a number
-        switch (weekday) {
-          case 0:
-            weekday = "Sunday";
-            break;
-          case 1:
-            weekday = "Monday";
-            break;
-          case 2:
-            weekday = "Tuesday";
-            break;
-          case 3:
-            weekday = "Wednesday";
-            break;
-          case 4:
-            weekday = "Thursday";
-            break;
-          case 5:
-            weekday = "Friday";
-            break;
-          case  6:
-            weekday = "Saturday";
-        };
+  .catch((error) => {
+    console.log(error);
+  });
 
-        const dayElement = document.getElementById(`day${[i]}`);
-        dayElement.innerText = `${weekday}`;
-        
-        const iconID = filteredForecast[i].weather[0].icon; //Gets icon code from API
-        const iconElement = document.getElementById(`icon${[i]}`);
-        iconElement.src = `./assets/${iconID}.png`;
+const generateHTMLForForecast = day => {
+  //Get the weekday it is
+  const weekdayInUnix = day.dt;  // Date in UNIX
+  const weekdayLongFormat = new Date(weekdayInUnix * 1000);  // Convert to nice date format we can use
+  const specificWeekday = weekdayLongFormat.toLocaleDateString('en-US', {weekday: 'long'}); 
 
-        const minTemp = Math.floor(filteredForecast[i].main.temp_min); //Gets min and max temp from API and rounds it up
-        const maxTemp = Math.floor(filteredForecast[i].main.temp_max);
-        const minMaxTemp = document.getElementById(`minMax${[i]}`);
-        minMaxTemp.innerText = `${minTemp}°C / ${maxTemp}°C`;
+  //Get image icon
+  const iconID = day.weather[0].icon; //Gets icon code from API
 
-        const descriptionFromAPI = filteredForecast[i].weather[0].description; //Gets description from API
-        const description = document.getElementById(`description${[i]}`);
-        description.innerText = descriptionFromAPI;
-      };
-    })
+  //Get description
+  const descriptionFromAPI = day.weather[0].description; //Gets description from API
 
-    .catch((error) => {
-      console.log(error);
-    });
+  //Get Min Max Temperatures
+  const minTemp = Math.floor(day.main.temp_min); //Gets min and max temp from API and rounds it up
+  const maxTemp = Math.floor(day.main.temp_max);
+
+  // Create dynamic HTML code to return
+  let forecastHTML = '';
+  forecastHTML += `<div class="forecast-container">`;
+  forecastHTML += `<p class="forecast-day">${specificWeekday}</p>`;
+  forecastHTML += `<img class="forecast-icon" src='./assets/${iconID}.png'>`;
+  forecastHTML += `<p class="forecast-description">${descriptionFromAPI}</p>`;
+  forecastHTML += `<p class="forecast-minmax">${minTemp}°C / ${maxTemp}°C</p>`;
+  forecastHTML += `</div>`;
+  return forecastHTML;
+};
 
 // Fetch to get Panama weather
 fetch(`http://api.openweathermap.org/data/2.5/weather?q=Panama&units=metric&APPID=${API_KEY}`)
