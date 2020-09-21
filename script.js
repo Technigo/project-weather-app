30a5ba08c35e90990a1acc1491c23db4b7c33783
Moa <63230455+mhammarback@users.noreply.github.com>
2020-09-21 16:47:04 +0200

    more readable sunset and sunrise time

--- script.js
+++ script.js
@@ -1,4 +1,7 @@
 const apiUrl = "https://api.openweathermap.org/data/2.5/weather?q=Stockholm,Sweden&units=metric&APPID=54a820a4b63e82050a15212c06998bb0";
+const apiUrlForecast = "https://api.openweathermap.org/data/2.5/forecast?q=Stockholm,Sweden&units=metric&APPID=54a820a4b63e82050a15212c06998bb0";
+
+
 const nameContainer = document.getElementById('name');
 const tempContainer = document.getElementById('temperature');
 const sortContainer = document.getElementById('sort');
@@ -23,29 +26,41 @@ fetch(apiUrl)
     .then((Response) => { 
         return Response.json()
     })
-    .then ((weatherArray) => {
-         nameContainer.innerHTML = `<h1>Lets find out how the wheater is in ${weatherArray.name}</h1>`
-         tempContainer.innerHTML = `<h1>The temperature is ${weatherArray.main.temp}</h1>`
-         sortContainer.innerHTML = `<h1>The temperature is ${weatherArray.weather[0].description}</h1>`
+    .then ((json) => {
+         nameContainer.innerHTML = `<h1>Lets find out how the wheater is in ${json.name}</h1>`
+         tempContainer.innerHTML = `<h1>The temperature is ${json.main.temp}</h1>`
+         sortContainer.innerHTML = `<h1>The temperature is ${json.weather[0].description}</h1>`
     
-//This changes the sunrise to readable 
-        const sunriseDate = new Date(weatherArray.sys.sunrise);
-        const sunriseTimeString = sunriseDate.toLocaleTimeString('en-US', {
-            timestyle: 'short',
-        });
+//This changes the sunrise/sunset to readable
+        const sunrise = new Date(json.sys.sunrise * 1000);
+        const sunset = new Date(json.sys.sunset* 1000);
 
-        sunriseContainer.innerHTML = `<h3>The sunrise ${sunriseTimeString} </h3>`
-
-//this changes the sunset to be readable 
-        const sunsetDate = new Date(weatherArray.sys.sunset);
-        const sunsetTimesString = sunsetDate.toLocaleTimeString ('en-US', {
-            timestyle: 'short',
-        });
+        const sunriseTime = sunrise.toLocaleTimeString([], {timeStyle: 'short'});
+        const sunsetTime = sunset.toLocaleTimeString ([], {timeStyle: 'short'});
        
-        sunsetContainer.innerHTML =  `<h3>The sun sets at ${sunsetTimesString} </h3>`
-})
+        sunriseContainer.innerHTML = `<h3>The sunrise ${sunriseTime} </h3>`
+        sunsetContainer.innerHTML =  `<h3>The sun sets ${sunsetTime} </h3>` 
+    });
 
 
+
+//WEATHER FORECAST 
+
+fetch(apiUrlForecast)
+    .then((Response) => { 
+        return Response.json()
+    })
+    .then ((json) => {
+        console.log(json)  
+        const limitedArray = json.list.splice(1,5,)
+        console.log(limitedArray) 
+
+        forecastContainer.innerHTML = `<h4>  ${limitedArray}  </h4>`
+ })   
+
+
+
+ //const filteredForecast = json.list.filter(item => item.dt_txt.includes('12:00'))
 
 
 
