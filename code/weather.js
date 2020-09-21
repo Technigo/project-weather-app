cb07f172061a280c66f897a25c0c58f18f40b60f
caroline birgersson <caroline@birgerochblom.se>
2020-09-21 21:27:12 +0200

    collecting some data by iterating through objects

--- code/weather.js
+++ code/weather.js
@@ -1,11 +1,21 @@
 const apiUrl = 'http://api.openweathermap.org/data/2.5/weather?q=Stockholm,Sweden&units=metric&APPID=038843ef5b6fc0faa93abadfa0f18663'
-const container = document.getElementById('weather');
+const container = document.getElementById('main');
+const weatherHeader = document.getElementById('weatherHeader');
  
 
-    fetch (apiUrl)
+    fetch(apiUrl)
         .then((response) => {
             return response.json();
+            console.log(response)
          })
-        .then((json) => {
-        console.log(json);
+        .then((weatherObjects) => {
+        weatherHeader.innerHTML = weatherObjects.weather.length;
+        console.log(weatherObjects);
+            for (weather in weatherObjects) {
+                console.log(weather)
+            }
         });
+
+        //Your task is to present the data: the city name, the temperature (rounded to 1 decimal place), and what type of weather it is (the "description" in the JSON)
+
+
