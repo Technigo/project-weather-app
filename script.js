69ddb715b543d1080fd14e88ae7246da7c1cba63
Moa <63230455+mhammarback@users.noreply.github.com>
2020-09-21 15:56:31 +0200

    formated and cleaned up the js code after lecture, more readable sunset and sunrise time added

--- script.js
+++ script.js
@@ -1,4 +1,4 @@
-
+const apiUrl = "https://api.openweathermap.org/data/2.5/weather?q=Stockholm,Sweden&units=metric&APPID=54a820a4b63e82050a15212c06998bb0";
 const nameContainer = document.getElementById('name');
 const tempContainer = document.getElementById('temperature');
 const sortContainer = document.getElementById('sort');
@@ -8,7 +8,7 @@ const forecastContainer = document.getElementById('forecast')
 
 
 //Console.log test 
-fetch('https://api.openweathermap.org/data/2.5/weather?q=Stockholm,Sweden&units=metric&APPID=54a820a4b63e82050a15212c06998bb0')
+fetch(apiUrl)
     .then((Response) => { 
         return Response.json()
     })
@@ -17,71 +17,40 @@ fetch('https://api.openweathermap.org/data/2.5/weather?q=Stockholm,Sweden&units=
  })    
 
 
-//NAME OF CITY 
-fetch('https://api.openweathermap.org/data/2.5/weather?q=Stockholm,Sweden&units=metric&APPID=54a820a4b63e82050a15212c06998bb0')
+//NAME OF CITY, TEMPERATURE, SORT OF WEATHER, SUNRISE, SUNSET
+
+fetch(apiUrl)
     .then((Response) => { 
         return Response.json()
     })
-    .then ((json) => {
-        nameContainer.innerHTML = `<h1>Lets find out how the wheater is in ${json.name}</h1>`
-    })         
+    .then ((weatherArray) => {
+         nameContainer.innerHTML = `<h1>Lets find out how the wheater is in ${weatherArray.name}</h1>`
+         tempContainer.innerHTML = `<h1>The temperature is ${weatherArray.main.temp}</h1>`
+         sortContainer.innerHTML = `<h1>The temperature is ${weatherArray.weather[0].description}</h1>`
+    
+//This changes the sunrise to readable 
+        const sunriseDate = new Date(weatherArray.sys.sunrise);
+        const sunriseTimeString = sunriseDate.toLocaleTimeString('en-US', {
+            timestyle: 'short',
+        });
 
+        sunriseContainer.innerHTML = `<h3>The sunrise ${sunriseTimeString} </h3>`
 
-
-// TEMPERATURE 
-fetch('https://api.openweathermap.org/data/2.5/weather?q=Stockholm,Sweden&units=metric&APPID=54a820a4b63e82050a15212c06998bb0')
-    .then((Response) => { 
-        return Response.json()
-    })
-    .then ((json) => {
-        tempContainer.innerHTML = `<h1>The temperature is ${json.main.temp}</h1>` 
-    }) 
-
-
-// SORT OF WEATHER    
-    fetch('https://api.openweathermap.org/data/2.5/weather?q=Stockholm,Sweden&units=metric&APPID=54a820a4b63e82050a15212c06998bb0')
-    .then((Response) => { 
-        return Response.json()
-    })
-    .then ((json) => {
-        sortContainer.innerHTML = `<h1>The temperature is ${json.weather[0].description}</h1>` 
-    }) 
-
-
-// SUNRISE
-    fetch('https://api.openweathermap.org/data/2.5/weather?q=Stockholm,Sweden&units=metric&APPID=54a820a4b63e82050a15212c06998bb0')
-    .then((Response) => { 
-        return Response.json()
-    })
-    .then ((json) => {
-        sunriseContainer.innerHTML = `<p> The sunrise starts at  ${json.sys.sunrise}</p>` 
-    }) 
-
-
-
-//SUNSET
-    fetch('https://api.openweathermap.org/data/2.5/weather?q=Stockholm,Sweden&units=metric&APPID=54a820a4b63e82050a15212c06998bb0')
-    .then((Response) => { 
-        return Response.json()
-    })
-    .then ((json) => {
-        sunsetContainer.innerHTML = `<p> The sunsets at ${json.sys.sunset}  </p>` 
-    }) 
+//this changes the sunset to be readable 
+        const sunsetDate = new Date(weatherArray.sys.sunset);
+        const sunsetTimesString = sunsetDate.toLocaleTimeString ('en-US', {
+            timestyle: 'short',
+        });
+       
+        sunsetContainer.innerHTML =  `<h3>The sun sets at ${sunsetTimesString} </h3>`
+})
 
 
 
 
-//WEATHER FORCAST
-
-fetch('https://api.openweathermap.org/data/2.5/forecast?q=Stockholm,Sweden&units=metric&APPID=54a820a4b63e82050a15212c06998bb0')  
-    .then((Response) => {   
-        return Response.json()
-    })
-    .then ((json) => {
-        console.log(json)
-    })
-  
 
 
     //const filteredForecast = json.list.filter(item => item.dt_txt.includes('12:00'))
     // filteredForecast is now an array with only the data from 12:00 each day.
+
+
