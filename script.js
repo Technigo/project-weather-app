428328c42a0f18c762ab23bfdfc5acd689cec92b
Moa <63230455+mhammarback@users.noreply.github.com>
2020-09-21 08:42:22 +0200

    step 2 complete, sort of weather added

--- script.js
+++ script.js
@@ -1,6 +1,10 @@
+
 const nameContainer = document.getElementById('name');
 const tempContainer = document.getElementById('temperature');
 const sortContainer = document.getElementById('sort');
+const sunriseContainer = document.getElementById('sunrise');
+const sunsetContainer = document.getElementById('sunset');
+
 
 //Console.log test 
 fetch('https://api.openweathermap.org/data/2.5/weather?q=Stockholm,Sweden&units=metric&APPID=54a820a4b63e82050a15212c06998bb0')
@@ -29,7 +33,16 @@ fetch('https://api.openweathermap.org/data/2.5/weather?q=Stockholm,Sweden&units=
         return Response.json()
     })
     .then ((json) => {
-        tempContainer.innerHTML = `<h1>The temperature is${json.main.temp}</h1>`
+        tempContainer.innerHTML = `<h1>The temperature is ${json.main.temp}</h1>` 
     }) 
-   
- 
+
+
+// SORT OF WEATHER    
+    fetch('https://api.openweathermap.org/data/2.5/weather?q=Stockholm,Sweden&units=metric&APPID=54a820a4b63e82050a15212c06998bb0')
+    .then((Response) => { 
+        return Response.json()
+    })
+    .then ((json) => {
+        sortContainer.innerHTML = `<h1>The temperature is ${json.weather[0].description}</h1>` 
+    }) 
+
