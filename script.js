b917466c103c3fff944785e8e64eff20e7076919
Emelie Svensson <emeliesv@live.se>
2020-09-21 16:41:03 +0200

    Finished step 3

--- script.js
+++ script.js
@@ -1,17 +1,35 @@
 const containerToday = document.getElementById('weatherToday')
 const descriptionToday = document.getElementById('text')
 const apiUrlToday = 'http://api.openweathermap.org/data/2.5/weather?q=Stockholm,Sweden&units=metric&APPID=95b6172379fabb04319de6c9e2aa34ae'
+const apiUrlForecast = 'https://api.openweathermap.org/data/2.5/forecast?q=Stockholm,Sweden&units=metric&APPID=95b6172379fabb04319de6c9e2aa34ae'
 
 const calculateTemperature = (number) => {
     const roundedTemp = Math.round(number*10)/10 //By adding *10 AND adding /10 the number is rounded up to nearest integer with one decimal. If only using round() the number is rounded up to nearest integer.
     return roundedTemp
 }
 
-const generatedHTMLForWeatherToday = (weatherToday) => {
+const calculatingSun = (time) => {
+    const sunTime = new Date(time*1000);
+    const sunTimeString = sunTime.toLocaleTimeString('sv-SE', {
+        hour: '2-digit',
+        minute: '2-digit',
+        hour12: false,
+    })
+    console.log(sunTimeString)
+    return sunTimeString
 
+} 
+
+const generatedHTMLForWeatherToday = (weatherToday) => {
     const temperature = calculateTemperature(weatherToday.main.temp)//This is using json.main.temp as a parameter instead of number.
+    console.log(weatherToday.sys.sunrise)
+    const sunrise = calculatingSun(weatherToday.sys.sunrise)
+    const sunset = calculatingSun(weatherToday.sys.sunset)
     containerToday.innerHTML = `<h1>This is ${weatherToday.name}</h1>`
-    descriptionToday.innerHTML = `The temperature today is ${temperature} degrees and it's ${weatherToday.weather[0].description} outside.`//Since weather is an array, we need to access the index of 0, and then we can locate the object keyvalues i.e .description. This has to be done even if there is only one array, as in this case.
+    descriptionToday.innerHTML = `The temperature today is ${temperature} degrees and it's ${weatherToday.weather[0].description} outside.`
+    //Since weather is an array, we need to access the index of 0, and then we can locate the object keyvalues i.e .description. This has to be done even if there is only one array, as in this case.
+    descriptionToday.innerHTML += `The sun rises at ${sunrise} and sets at ${sunset}`
+    
 }
 
 const fetchWeatherToday = () => {
@@ -23,6 +41,3 @@ const fetchWeatherToday = () => {
         generatedHTMLForWeatherToday(weatherToday)
     })
 }
-
-
-
