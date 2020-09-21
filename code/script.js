d7a6a0c9384319b5a5b569e44a67294a4cf9f68d
AnnaHellkvist <51534839+AnnaHellkvist@users.noreply.github.com>
2020-09-21 20:27:30 +0200

    Step 2 done, presented some data in the app

--- code/script.js
+++ code/script.js
@@ -6,6 +6,9 @@ fetch(`http://api.openweathermap.org/data/2.5/weather?q=Karlstad,Sweden&units=me
 })
 .then((json) => {
     console.log(json)
+    containerCity.innerHTML = `<p>Presenting the weather in ${json.name}</p>`;
+    containerTemp.innerHTML = `<p>The temperature is ${json.main.temp} degrees Celsius</p>`;
+    containerDescription.innerHTML = `<p>Description: ${json.weather[0].description}</p>`;
 })
 
 fetch(`http://api.openweathermap.org/data/2.5/forecast?q=Karlstad,Sweden&units=metric&appid=${API_KEY}`)
