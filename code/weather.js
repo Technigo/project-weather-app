e51abba1fc14ee305f65f821eccb1a4789d6b664
caroline birgersson <caroline@birgerochblom.se>
2020-09-22 17:02:43 +0200

    got the data in iteration 1 in place

--- code/weather.js
+++ code/weather.js
@@ -1,22 +1,17 @@
 const apiUrl = 'http://api.openweathermap.org/data/2.5/weather?q=Stockholm,Sweden&units=metric&APPID=038843ef5b6fc0faa93abadfa0f18663'
+const apiForecast = 'https://api.openweathermap.org/data/2.5/forecast?q=Stockholm,Sweden&units=metric&APPID=038843ef5b6fc0faa93abadfa0f18663Y'
 const container = document.getElementById('main');
 const weatherHeader = document.getElementById('weatherHeader');
- 
 
     fetch(apiUrl)
         .then((response) => {
             return response.json();
-            console.log(response)
          })
         .then((weatherObjects) => {
-        weatherHeader.innerHTML = weatherObjects.weather.length;
-        console.log(weatherObjects);
-            for (weather in weatherObjects) {
-                console.log(weather)
-                container.innerHTML += `<p>${JSON.stringify(weather)}</p>`;  
-            };          
+        weatherHeader.innerHTML = weatherObjects.name + weatherObjects.weather[0].description + weatherObjects.main.temp
         });
 
         //Your task is to present the data: the city name, the temperature (rounded to 1 decimal place), and what type of weather it is (the "description" in the JSON)
 
+        
 
