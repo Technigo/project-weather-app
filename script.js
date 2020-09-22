c4a1bb3e5605b287e0fea6e64d64b8a3de7c859c
jonna ru <jonna.ru@icloud.com>
2020-09-22 21:58:17 +0200

    From .getHours/.getMInutes to .toLocalTimeString instead.

--- script.js
+++ script.js
@@ -8,35 +8,56 @@ let sunRise = document.getElementById("sunRise");
 let sunSet = document.getElementById("sunSet");
 
 const key = "5c01b021abe8da367bcecadd67235fb3";
-const apiURL = `https://api.openweathermap.org/data/2.5/forecast?q=Stockholm,Sweden&units=metric&APPID=${key}`;
+const apiURLcurrent = `https://api.openweathermap.org/data/2.5/weather?q=Stockholm,Sweden&units=metric&APPID=${key}`;
+const apiURLforcast = `https://api.openweathermap.org/data/2.5/forecast?q=Stockholm,Sweden&units=metric&APPID=${key}`;
 
 const container = document.getElementById("main");
-
-fetch(apiURL)
-  .then((response) => {
-    return response.json();
-  })
-  .then((weather) => {
-    console.log(weather);
-  });
 
 // round number to one decimal
 const round = (number) => {
   return Math.round(number * 10) / 10;
 };
 
-// from unix-time to milliseconds
+// from unix time to milliseconds
 const time = (number) => {
   return new Date(number * 1000);
 };
 
 const weatherToDay = () => {
-  fetch(apiURL)
+  fetch(apiURLcurrent)
     .then((response) => {
       return response.json();
     })
     .then((data) => {
+      console.log(data);
+      city.innerHTML = data.name;
+      temp.innerHTML = round(data.main.temp);
+      feelsLikeTemp.innerHTML = round(data.main.feels_like);
+      weatherToday.innerHTML = data.weather[0].description;
+      // sunrise
+      sunrise = time(data.sys.sunrise);
+      sunRise.innerHTML = `${sunrise.toLocaleTimeString([], {
+        timeStyle: "short",
+      })}`;
+      // sunset
+      sunset = time(data.sys.sunset);
+      sunSet.innerHTML = `${sunset.toLocaleTimeString([], {
+        timeStyle: "short",
+      })}`;
+    });
+};
+
+const weatherForcast = () => {
+  fetch(apiURLforcast)
+    .then((response) => {
+      return response.json();
+    })
+    .then((data) => {
+      console.log(data);
+      /*
       city.innerHTML = data.city.name;
+
+
       temp.innerHTML = round(data.list[1].main.temp);
       feelsLikeTemp.innerHTML = round(data.list[1].main.feels_like);
       weatherToday.innerHTML = data.list[1].weather[0].description;
@@ -46,7 +67,9 @@ const weatherToDay = () => {
       // sunset
       sunset = time(data.city.sunset);
       sunSet.innerHTML = `${sunset.getHours()}:${sunset.getMinutes()}`;
+      */
     });
 };
 
 weatherToDay();
+weatherForcast();
