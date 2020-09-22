8361305e7d13073f4deeeaf92955e85b837cf75d
Moa <63230455+mhammarback@users.noreply.github.com>
2020-09-22 09:16:37 +0200

    weather forecast icons added

--- script.js
+++ script.js
@@ -10,6 +10,7 @@ const sunsetContainer = document.getElementById('sunset');
 const forecastContainer = document.getElementById('forecast')
 
 
+
 //Console.log test 
 fetch(apiUrl)
     .then((Response) => { 
@@ -27,9 +28,13 @@ fetch(apiUrl)
         return Response.json()
     })
     .then ((json) => {
-         nameContainer.innerHTML = `<h1>Lets find out how the wheater is in ${json.name}</h1>`
-         tempContainer.innerHTML = `<h1>The temperature is ${json.main.temp}</h1>`
-         sortContainer.innerHTML = `<h1>The temperature is ${json.weather[0].description}</h1>`
+        nameContainer.innerHTML = `<h1>Lets find out how the wheater is in ${json.name}</h1>`;
+        sortContainer.innerHTML = `<h1>The temperature is ${json.weather[0].description}</h1>`;
+
+         const roundedTemperature = json.main.temp.toFixed(0.1);
+         tempContainer.innerHTML += `<h1>The temperature is ${roundedTemperature}°C</h1>`;
+         console.log(roundedTemperature)
+         
     
 //This changes the sunrise/sunset to readable
         const sunrise = new Date(json.sys.sunrise * 1000);
@@ -38,9 +43,12 @@ fetch(apiUrl)
         const sunriseTime = sunrise.toLocaleTimeString([], {timeStyle: 'short'});
         const sunsetTime = sunset.toLocaleTimeString ([], {timeStyle: 'short'});
        
-        sunriseContainer.innerHTML = `<h3>The sunrise ${sunriseTime} </h3>`
-        sunsetContainer.innerHTML =  `<h3>The sun sets ${sunsetTime} </h3>` 
-    });
+        sunriseContainer.innerHTML = `<h3>Sunrise: ${sunriseTime} </h3>`
+        sunsetContainer.innerHTML =  `<h3>Sunset: ${sunsetTime} </h3>` 
+    })
+    .catch((error) => {
+        console.log(error) 
+     }) ;
 
 
 
@@ -61,7 +69,8 @@ fetch(apiUrlForecast)
             const weekTemp = dayTemp.toFixed(0.1);
 
             document.getElementById('forecastDay').innerHTML += `<p>${dayName}</p>`
-            document.getElementById('forecastTemp').innerHTML += `<p>${weekTemp}</p>`
+            document.getElementById('forecastTemp').innerHTML += `<p>${weekTemp}°C</p>`
+            document.getElementById('forecastIcon').innerHTML += `<img src=https://openweathermap.org/img/wn/${day.weather[0].icon}@2x.png></img>`
     });  
  
 });
