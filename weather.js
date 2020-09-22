c854508c47b8095a4c94d377836451c47e35ca9d
idsintehittapa <jblom15@gmail.com>
2020-09-22 14:04:10 +0200

    Added and implementing function for rounding temp to one decimal palce

--- weather.js
+++ weather.js
@@ -1,19 +1,25 @@
 apiUrl = "https://api.openweathermap.org/data/2.5/weather?q=Longyearbyen,Norway&units=metric&APPID=8990d7a0fe5c73c6c0fe06bb994b1035"
 const containerToday = document.getElementById('weatherMain');
 const containerTemp = document.getElementById('weatherTemp');
+const containerTempFeel = document.getElementById('weatherTempFeel');
 const containerDescription = document.getElementById('weatherDescription');
 
-
-
-const generatedHTMLForWeatherToday = (weatherMain) => {
-    containerToday.innerHTML = `${weatherMain.weather[0].main}`
-    console.log(containerToday)
-    containerTemp.innerHTML = ` ${weatherMain.main.temp}`
-    containerDescription.innerHTML = `${weatherMain.weather[0].description}`
-    console.log(containerDescription)
+/// make this into a one line function instead?
+/// calculating a rounded number for the temp
+const roundtemp = (number) => {
+    const roundtemp = Math.round(number * 10) / 10;
+    return roundtemp;
 }
 
+/// Displaying todays weather forcast 
+const generatedHTMLForWeatherToday = (weatherMain) => {
+    containerToday.innerHTML = `${weatherMain.weather[0].main}`
+    containerTemp.innerHTML = roundtemp(weatherMain.main.temp)
+    containerTempFeel.innerHTML = roundtemp(weatherMain.main.feels_like)
+    containerDescription.innerHTML = `${weatherMain.weather[0].description}`
+}
 
+/// getting the API
 fetch(apiUrl)
     .then((response) => {
         return response.json()
