cc3ac0f5d069defc84c368e55cc349ac4b8ba850
jonna ru <jonna.ru@icloud.com>
2020-09-22 20:28:54 +0200

    Step 3 - sunrise and sunset: getHours + getMinutes // function from unixtime to milliseconds

--- script.js
+++ script.js
@@ -1,9 +1,11 @@
 // const stad = "stockholm";
 // const stad = dropdown.value;
+let city = document.getElementById("city");
 let temp = document.getElementById("temp");
 let feelsLikeTemp = document.getElementById("feelsLikeTemp");
 let weather = document.getElementById("weather");
-let city = document.getElementById("city");
+let sunRise = document.getElementById("sunRise");
+let sunSet = document.getElementById("sunSet");
 
 const key = "5c01b021abe8da367bcecadd67235fb3";
 const apiURL = `https://api.openweathermap.org/data/2.5/forecast?q=Stockholm,Sweden&units=metric&APPID=${key}`;
@@ -23,6 +25,11 @@ const round = (number) => {
   return Math.round(number * 10) / 10;
 };
 
+// from unix-time to milliseconds
+const time = (number) => {
+  return new Date(number * 1000);
+};
+
 const weatherToDay = () => {
   fetch(apiURL)
     .then((response) => {
@@ -32,8 +39,13 @@ const weatherToDay = () => {
       city.innerHTML = data.city.name;
       temp.innerHTML = round(data.list[1].main.temp);
       feelsLikeTemp.innerHTML = round(data.list[1].main.feels_like);
-      //console.log(data.list[1].weather[0].description);
       weatherToday.innerHTML = data.list[1].weather[0].description;
+      // sunrise
+      sunrise = time(data.city.sunrise);
+      sunRise.innerHTML = `${sunrise.getHours()}:${sunrise.getMinutes()}`;
+      // sunset
+      sunset = time(data.city.sunset);
+      sunSet.innerHTML = `${sunset.getHours()}:${sunset.getMinutes()}`;
     });
 };
 
