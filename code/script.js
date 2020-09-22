c36c36d8cc0829736f3dbfb6cd7135331fe4ab37
AxelHammarback <axel.hammarback@gmail.com>
2020-09-22 19:13:37 +0200

    Most logic is in place – except for showing the right symbol depending on the weather

--- code/script.js
+++ code/script.js
@@ -1,6 +1,8 @@
 // API key: 607d94111f1f9c343f38c10112b16e3c
 // REMOVE BEFORE SUBMISSION
 
+let cityName = "Stockholm"
+
 const city = document.getElementById("city");
 const country = document.getElementById("country");
 const localtime = document.getElementById("localtime");
@@ -11,61 +13,99 @@ const sunrise = document.getElementById("sunrise");
 const sunset = document.getElementById("sunset");
 const weatherType = document.getElementById("weathertype");
 
+const forecastDay1 = document.getElementById("day1");
+const forecastDay2 = document.getElementById("day2");
+const forecastDay3 = document.getElementById("day3");
+const forecastDay4 = document.getElementById("day4");
+const forecastDay5 = document.getElementById("day5");
 
+// formatTime() formats UNIX 10-digit timestamps to HH:MM format.
+const formatTime = (timestamp) => {
+  // Needed to add *1000 due to account for the absence of milliseconds in Unix timestamps. 
+  let readableTime = new Date(timestamp * 1000);
 
-const fetchData = () => {
-  fetch('https://api.openweathermap.org/data/2.5/weather?q=Stockholm,Sweden&units=metric&APPID=607d94111f1f9c343f38c10112b16e3c').then((response) => {
-    console.log("Inside the response");
-    console.log(typeof response);
+  //Only use the HH:MM's of the readableTime
+  readableTime = readableTime.toLocaleTimeString('sv-SE', {
+    hour: '2-digit', 
+    minute:'2-digit',
+    hour12: false
+  });
+  return readableTime;
+}
+
+const kelvinToCelsius = (temp) => {
+  let celsius = Math.round(temp-273.15);
+  return celsius
+}
+
+const fetchWeather = (city) => {
+  fetchForecast(city);
+  fetchCurrentWeather(city);
+}
+
+// Function which fetches current weather data from a city, and populates the main objects in the DOM.
+const fetchCurrentWeather = (cityName) => {
+  fetch(`https://api.openweathermap.org/data/2.5/weather?q=${cityName}&units=metric&APPID=607d94111f1f9c343f38c10112b16e3c`).then((response) => {
     return response.json();
   }).then((weatherObject) => {
-    // This is the promise.
-    console.log("Here's the data 👇")
-    console.log(weatherObject)
-
 
     city.innerHTML = weatherObject.name;
-    country.innerHTML = weatherObject.sys.country;
-    localtime.innerHTML = weatherObject.dt;
-    temperature.innerHTML = weatherObject.main.temp;
-    tempFeelsLike.innerHTML = weatherObject.main.feels_like;
+    country.innerHTML = getCountryName(weatherObject.sys.country);
+    localtime.innerHTML = formatTime(weatherObject.dt);
+    temperature.innerHTML = Math.round(weatherObject.main.temp)+"°";
+    tempFeelsLike.innerHTML = Math.round(weatherObject.main.feels_like)+"°";
     weatherType.innerHTML = weatherObject.weather[0].main;
     description.innerHTML = weatherObject.weather[0].description;
-    sunrise.innerHTML = weatherObject.sys.sunrise;
-    sunset.innerHTML = weatherObject.sys.sunset;
-
-    
-    // weatherObject.weather.forEach(weather => {
-    //   console.log(weather.main)
-    //   console.log(weather.description)
-    // });
+    sunrise.innerHTML = formatTime(weatherObject.sys.sunrise);
+    sunset.innerHTML = formatTime(weatherObject.sys.sunset);
   })
 }
 
-fetchData();
+fetchCurrentWeather(cityName);
 
 
+// 5-day forecast function
+const fetchForecast = (cityName) => {
+  fetch(`https://api.openweathermap.org/data/2.5/forecast?q=${cityName}&appid=607d94111f1f9c343f38c10112b16e3c`).then((response) => {
+    return response.json();
+  }).then((forecastObject) => {
+    // Instead of an array of 40 objects, we want one object per day. Time ≈ the current time. Empty array creation time.
+    const fiveDayList = [];
 
-// http://api.open-notify.org/astros.json
+    // Get every 8th object from the forecastObject (since it's in 3-hour intervals), and push them into fiveDayList.
+    for (let i = 7; i<forecastObject.list.length; i+=8) {
+      fiveDayList.push(forecastObject.list[i]);
+    }
 
-// Define the container where we should put the content.
-// const container = document.getElementById("astros")
+    // Populate the DOM with data fetched. First, create a var for the section "forecastBox"
+    const forecastBox = document.getElementById("forecastBox");
+    
+    // Need to clear the forecastBox, since otherwise switching cities won't work (since the forEach only *adds* stuff)
+    forecastBox.innerHTML = ``
+    console.log(fiveDayList);
+    fiveDayList.forEach(element => { 
+      const date = element.dt_txt.slice(0,10);
+      const temp = element.main.temp;
+      const weather = element.weather[0].main;
 
-// fetch('http://api.open-notify.org/astros.json')
-//     .then((response) => {
-//         console.log(typeof response)
-//         return response.json()
-//     })
-//     .then((astros) => {
-//         console.log(typeof astros)
-//         container.innerHTML = `<h1>${astros.number} people are in space right now</h1>`
+      forecastBox.innerHTML += `
+      <section id="forecastBox"></section>
+      <div id="forecastRow" class="forecast-row">
+        <span class="forecast-date">${date}</span>
+        <div class="forecast-temp">
+          <span>☀</span>
+          <span>${kelvinToCelsius(temp)}°</span>
+        </div>
+        <span class="forecast-weather">${weather}</span>        
+        </div>
+      </section>`
 
-//         astros.people.forEach((person) => {
-//             container.innerHTML += `<p>${person.name} is on the ${person.craft}</p>`
-//         })
-//     })
+    
+    });
+
+    
+  })
+}
 
 
-
-// i) fetch
-// 2) .then(response)()
+fetchForecast(cityName);
