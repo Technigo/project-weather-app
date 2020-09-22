ede090e4f82f8b5e035b14c5bdf9c9a314273529
Andrea Osmar <andrea88@live.se>
2020-09-22 18:14:21 +0200

    fetching api

--- script.js
+++ script.js
@@ -1,4 +1,17 @@
-// API URL'S
+// APP ID FOR OPEN WEATHER MAP
+let appId = '38d8ead396b75510c605134ba40b95f7';
+// CHOICE OF UNITS
+let units = 'metric';
+// TO SEARCH FOR A CITY
+let searchMethod = 'q';
 
-const weatherURL = 'https://api.openweathermap.org/data/2.5/weather?q=Stockholm,Sweden&units=metric&APPID=60032cdd91d77852bfb39762c09118fe'
-const forecastURL = 'https://api.openweathermap.org/data/2.5/forecast?q=Stockholm,Sweden&units=metric&APPID=60032cdd91d77852bfb39762c09118fe'
+// FETCH API
+function searchWeather(searchTerm) {
+    // getSearchMethod(searchTerm);
+    fetch(`http://api.openweathermap.org/data/2.5/weather?${searchMethod}=${searchTerm}&APPID=${appId}&units=${units}`)
+        .then(result => {
+            return result.json();
+        }).then(result => {
+            init(result);
+        })
+}
