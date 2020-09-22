cf7adc5cd0c57115a943a34b6726ec0a69dcc72b
idsintehittapa <jblom15@gmail.com>
2020-09-22 14:51:33 +0200

    Added and implementing function for readable time for sunset and sunrise

--- weather.js
+++ weather.js
@@ -3,6 +3,8 @@ const containerToday = document.getElementById('weatherMain');
 const containerTemp = document.getElementById('weatherTemp');
 const containerTempFeel = document.getElementById('weatherTempFeel');
 const containerDescription = document.getElementById('weatherDescription');
+const containerSunRise = document.getElementById('sunrise');
+const containerSunSet = document.getElementById('sunset');
 
 /// make this into a one line function instead?
 /// calculating a rounded number for the temp
@@ -11,12 +13,26 @@ const roundtemp = (number) => {
     return roundtemp;
 }
 
+/// calculating the time into readable format
+const readableTime = (time) => {
+    const readableTime = new Date(time * 1000)
+    const sunTimeString = readableTime.toLocaleTimeString('en-US', {
+        hour: '2-digit',
+        minute: '2-digit',
+        hour12: false,
+    });
+    console.log(readableTime)
+    return sunTimeString
+}
+
 /// Displaying todays weather forcast 
 const generatedHTMLForWeatherToday = (weatherMain) => {
     containerToday.innerHTML = `${weatherMain.weather[0].main}`
     containerTemp.innerHTML = roundtemp(weatherMain.main.temp)
     containerTempFeel.innerHTML = roundtemp(weatherMain.main.feels_like)
     containerDescription.innerHTML = `${weatherMain.weather[0].description}`
+    containerSunRise.innerHTML = readableTime(weatherMain.sys.sunrise)
+    containerSunSet.innerHTML = readableTime(weatherMain.sys.sunset)
 }
 
 /// getting the API
