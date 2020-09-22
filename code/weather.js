dc25b007c7ec44a296e120f60bca9b7dd8cda8ac
Olof Lindahl <olindahl@gmail.com>
2020-09-22 17:36:51 +0200

    location picker and current position working

--- code/weather.js
+++ code/weather.js
@@ -1,12 +1,40 @@
-const fetchWeather = () => {
+const changeCity = () => {
+    let city = document.getElementById("city-label").value 
+    console.log(city)
+    fetchWeather(city)
+}
+
+const getLocation = () => {
+    if (navigator.geolocation) {
+      navigator.geolocation.getCurrentPosition(getLatLongCurrent);
+    } else { 
+      console.log("Geolocation is not supported by this browser.");
+    }
+}
+
+const getLatLongCurrent = (position) => {
+    console.log(position.coords.latitude)
+    console.log(position.coords.longitude)
+    fetchForecast(position.coords.latitude, position.coords.longitude)
+}
+
+const getLatLong = (forecast) => {
+    const lat = forecast.coord.lat
+    console.log(lat)
+    const lon = forecast.coord.lon
+    console.log(lon)
+    fetchForecast(lat, lon)
+}
+
+const fetchWeather = (city) => {
   return fetch(
-    `http://api.openweathermap.org/data/2.5/weather?q=Stockholm,Sweden&units=metric&APPID=7d01b328e34c450986cb7faef032a771`
+    `http://api.openweathermap.org/data/2.5/weather?q=${city},Sweden&units=metric&APPID=7d01b328e34c450986cb7faef032a771`
   )
     .then((response) => {
       return response.json();
     })
     .then((json) => {
-      sthlmWeather(json);
+      getLatLong(json);
     });
 };
 
@@ -24,9 +52,10 @@ const getDayOfWeek = (dayOfWeek) => {
       ][dayOfWeek];
 };
 
-const fetchForecast = () => {
+const fetchForecast = (lat, lon) => {
   return fetch(
-    `https://api.openweathermap.org/data/2.5/onecall?lat=59.3326&lon=18.0649&%20exclude=current,minutely,hourly&appid=7d01b328e34c450986cb7faef032a771&units=metric`
+    // `https://api.openweathermap.org/data/2.5/onecall?lat=59.3326&lon=18.0649&%20exclude=current,minutely,hourly&appid=7d01b328e34c450986cb7faef032a771&units=metric`
+    `https://api.openweathermap.org/data/2.5/onecall?lat=${lat}&lon=${lon}&%20exclude=current,minutely,hourly&appid=7d01b328e34c450986cb7faef032a771&units=metric`
   )
     .then((response) => {
       return response.json();
@@ -34,22 +63,6 @@ const fetchForecast = () => {
     .then((json) => {
       sthlmForecast(json);
     });
-};
-
-const sthlmWeather = (weather) => {
-  console.log(weather.main.temp);
-  document.getElementById("sthlm").innerHTML += `${weather.main.temp}`;
-  //console.log(new Date(weather.sys.sunrise * 1000));
-  const sunrise = new Date(weather.sys.sunrise * 1000);
-  const sunset = new Date(weather.sys.sunset * 1000);
-  console.log(new Date(weather.sys.sunset * 1000).getMinutes());
-  document.getElementById("sthlm").innerHTML += `<p>Sunrise: ${
-    sunrise.getHours() <= 9 ? `0` : ``
-  }${sunrise.getHours()}:${sunrise.getMinutes()}</p>`;
-  document.getElementById("sthlm").innerHTML += `<p>Sunset: ${
-    sunset.getHours() <= 9 ? `0` : ``
-  }${sunset.getHours()}:${sunset.getMinutes()}</p>`;
-  //console.log(sunrise.getHours());
 };
 
 const getIcon = (desc) => {
@@ -60,7 +73,12 @@ const getIcon = (desc) => {
 };
 
 const sthlmForecast = (forecast) => {
-  //Current temp. movw to new function
+  //get current city
+  const currentCity = forecast.timezone.split("/")[1]
+  const currentCityHTML = document.getElementById("city")
+  currentCityHTML.innerHTML = currentCity
+
+  //get current temp
   const currentTemp = document.getElementById("current");
   currentTemp.innerHTML = `${Math.round(forecast.current.temp)}&#8451`;
 
@@ -81,8 +99,8 @@ const sthlmForecast = (forecast) => {
     hour: "2-digit",
     minute: "2-digit",
   });
-  sunriseHTML.innerHTML += sunriseString;
-  sunsetHTML.innerHTML += sunsetString;
+  sunriseHTML.innerHTML = `Sunrise:  ${sunriseString}`;
+  sunsetHTML.innerHTML = `Sunset:  ${sunsetString}`;
 
   //Get days for forecast
   document.getElementById("day1").innerHTML = `${getDayOfWeek(
@@ -100,7 +118,7 @@ const sthlmForecast = (forecast) => {
   document.getElementById("day5").innerHTML = `${getDayOfWeek(
     new Date(forecast.daily[4].dt * 1000).getDay()
   )}`;
-  document.getElementById("day1-temp").innerHTML += `<img src='${getIcon(
+  document.getElementById("day1-temp").innerHTML = `<img src='${getIcon(
     forecast.daily[0].weather[0].main
   )}'>   `;
 
@@ -120,9 +138,23 @@ const sthlmForecast = (forecast) => {
   document.getElementById("day5-temp").innerHTML = `${Math.round(
     forecast.daily[4].temp.min
   )} / ${Math.round(forecast.daily[4].temp.max)}`;
-  console.log(forecast.daily[0].temp.min);
-  console.log(forecast.daily[0].temp.max);
 };
+
+/*const sthlmWeather = (weather) => {
+  console.log(weather.main.temp);
+  document.getElementById("sthlm").innerHTML += `${weather.main.temp}`;
+  //console.log(new Date(weather.sys.sunrise * 1000));
+  const sunrise = new Date(weather.sys.sunrise * 1000);
+  const sunset = new Date(weather.sys.sunset * 1000);
+  console.log(new Date(weather.sys.sunset * 1000).getMinutes());
+  document.getElementById("sthlm").innerHTML += `<p>Sunrise: ${
+    sunrise.getHours() <= 9 ? `0` : ``
+  }${sunrise.getHours()}:${sunrise.getMinutes()}</p>`;
+  document.getElementById("sthlm").innerHTML += `<p>Sunset: ${
+    sunset.getHours() <= 9 ? `0` : ``
+  }${sunset.getHours()}:${sunset.getMinutes()}</p>`;
+  //console.log(sunrise.getHours());
+};*/
 
 /*const fetchForecast = () => {
     return fetch(
