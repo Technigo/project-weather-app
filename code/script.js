11a47f764d46ea79be78d83392d49243b4bf4006
MindstormingAB <s.elander@mindstorming.se>
2020-09-22 15:01:10 +0200

    Step 2 - Present some data on the web app

--- code/script.js
+++ code/script.js
@@ -2,10 +2,17 @@ const weatherApiKey = config.WEATHER_API_KEY;
 
 const apiUrl = 'https://api.openweathermap.org/data/2.5/weather?q=Stockholm,Sweden&units=metric&APPID=' + weatherApiKey;
 
+const city = document.getElementById('city');
+const temperature = document.getElementById('temperature');
+const description = document.getElementById('description');
+
 fetch(apiUrl)
   .then((response) => {
     return response.json();
   })
   .then((weatherArray) => {
     console.log(weatherArray);
+    city.innerHTML = weatherArray.name;
+    temperature.innerHTML = `${weatherArray.main.temp.toFixed(1)}\xB0`;
+    description.innerHTML = weatherArray.weather.map((a) => a.description);
   })
