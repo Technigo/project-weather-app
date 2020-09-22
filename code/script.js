108fde38775157a3a3bb2064725e207fd29e054f
Henrike Wiemker <65461017+HenrikeW@users.noreply.github.com>
2020-09-22 10:44:19 +0200

    added sunrise and sunset / completed step 3

--- code/script.js
+++ code/script.js
@@ -1,6 +1,8 @@
 const tempToday = document.getElementById("temperatureToday");
 const city = document.getElementById("city");
 const description = document.getElementById("description");
+const sunrise = document.getElementById("sunriseTime");
+const sunset = document.getElementById("sunsetTime");
 
 
 
@@ -17,4 +19,12 @@ const updateWeatherToday = (json) => {
     tempToday.innerHTML = json.main.temp.toFixed(1);
     city.innerHTML = json.name;
     description.innerHTML = json.weather[0].description;
-}
+
+    const sunriseMillSeconds = new Date(json.sys.sunrise * 1000);
+    const sunriseProperTime = sunriseMillSeconds.toLocaleTimeString([], {hour: "2-digit", minute:"2-digit"});
+    sunrise.innerHTML = sunriseProperTime;
+
+    const sunsetMillSeconds = new Date(json.sys.sunset * 1000);
+    const sunsetProperTime = sunsetMillSeconds.toLocaleTimeString([], {hour: "2-digit", minute: "2-digit"});
+    sunset.innerHTML = sunsetProperTime;
+};
