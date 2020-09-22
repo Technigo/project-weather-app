b217e34d9139b50f9c950d1932ccd8d5b908ab67
Peggy <64912576+pwangy@users.noreply.github.com>
2020-09-22 15:09:06 +0200

    Steps 1-3 done. 4 in progress.

--- script.js
+++ script.js
@@ -1,59 +1,100 @@
-// Step 1 - Get started with the weather API.
-// You will need to use the `fetch()` function in JavaScript to load the weather data into your page, and then select the values you want to inject into the DOM from the JSON which comes from the API.
-// Read the [endpoint documentation](https://openweathermap.org/current) for the current weather
+// Stockholm weather - Point to API and define a few locations in HTML
+const weatherTodaySTHLM = 'http://api.openweathermap.org/data/2.5/weather?id=2673730&units=metric&appid=eb46c8c17530a3d02461794022d39d32'
+const locationStockholm = document.getElementById('location-sthlm')
+const sunriseSTHLM = document.getElementById('sunrise-sthlm')
+const sunsetSTHLM = document.getElementById('sunset-sthlm')
+const nowSTHLM = document.getElementById('now-sthlm')
 
-const fetchSTHLM = () => {
-  fetch('http://api.openweathermap.org/data/2.5/weather?id=2673730&units=metric&callback=sthlm&appid=eb46c8c17530a3d02461794022d39d32')
-    .then((response) => {
-      return response.json()
-      // console.log(response)
-  })
-    .then((sthlm) => {
-      console.log(sthlm)
-
-      const locationStockholm = document.getElementById('location-sthlm')
-      const sunriseSTHLM = document.getElementById('sunrise-sthlm')
-      const sunsetSTHLM = document.getElementById('sunset-sthlm')
-      const nowSTHLM = document.getElementById('now-sthlm')
-
-      locationStockholm.innerHTML = `${sthlm.weather.main} | ${sthlm.main.temp}`
-      sunriseSTHLM.innerHTML = sthlm.sys.sunrise
-      sunsetSTHLM.innerHTML = sthlm.sys.sunset
-      nowSTHLM.innerHTML = `It is currently ${sthlm.weather.description} and feels like ${sthlm.main.feels_like} in ${sthlm.name}.`
-  })
-  //   .catch((error) => {
-  //     console.log(error)  
-  // })
-};
-fetchSTHLM()
-
-const day1STHLM = document.getElementById('day1-sthlm')
-const day2STHLM = document.getElementById('day2-sthlm')
-const day3STHLM = document.getElementById('day3-sthlm')
-const day4STHLM = document.getElementById('day4-sthlm')
-const day5STHLM = document.getElementById('day5-sthlm')
-
-//  Step 2 - Present some data on your web app.
-// Your task is to present the data: the city name, the temperature (rounded to 1 decimal place), and what type of weather it is (the "description" in the JSON)
-
-
-
-// Step 3 - Sunrise and sunset
-// Show the time for sunrise and sunset in a readable time format (Example: 13:00 or 1 PM).
-// You will have to **format the date from milliseconds to a readable format**.
-// [Here](https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/Date) is a useful resource for how to do this.
-
-
+// Fetch today's weather and display data - Stockholm
+fetch(weatherTodaySTHLM)
+  .then((response) => {
+    return response.json()
+})
+  .then((sthlm) => {
+    console.log(sthlm)
+    const tempNow = Math.round(sthlm.main.temp)
+    const nowFeelsLike = Math.round(sthlm.main.feels_like)
+    const sunriseCalc = new Date(sthlm.sys.sunrise * 1000)
+    const sunrise = ( new Date(sunriseCalc)).toLocaleTimeString('sv-SE', {
+      hour12: false, 
+      hour: '2-digit',
+      minute: '2-digit'
+    })
+    const sunsetCalc = new Date(sthlm.sys.sunset * 1000)
+    const sunset = ( new Date(sunsetCalc)).toLocaleTimeString('sv-SE', {
+      hour12: false, 
+      hour: '2-digit',
+      minute: '2-digit'
+    })
+    locationStockholm.innerHTML = `${sthlm.weather[0].main} | ${tempNow} \u00b0`
+    sunriseSTHLM.innerHTML = `Sunrise ${sunrise}`
+    sunsetSTHLM.innerHTML = `Sunset ${sunset}`
+    nowSTHLM.innerHTML = `Right now we're seeing ${sthlm.weather[0].description} and it feels like ${nowFeelsLike} \u00b0 in ${sthlm.name}.`
+})
 
 // Step 4 - Weather forecast
 // Show a forecast for the next 5 days. You can choose how to display the forecast - perhaps you want to show the min and max temperature for each day, or perhaps you want to show the temperature from the middle of the day, or the humidity, what it feels like and so on.
-// [https://api.openweathermap.org/data/2.5/forecast?q=Stockholm,Sweden&units=metric&APPID=YOUR_API_KEY](https://api.openweathermap.org/data/2.5/forecast?q=Stockholm,Sweden&units=metric&APPID=YOUR_API_KEY)
 
 // The API gives us the next 5 days but for every third hour. So a good idea could be to only use the weather data from the same time everyday. You can filter the forecast list array to only get the info from 12:00 each day for example. 
 
 // HINTS Here
 
 
+
+// Stockholm forecast - Define locations in HTML to display data
+const forecastSTHLM = 'http://api.openweathermap.org/data/2.5/forecast?id=2673730&$cnt=5&units=metric&appid=eb46c8c17530a3d02461794022d39d32'
+const fiveDay = document.getElementById('five-Day') 
+// const day1DateSTHLM = document.getElementById('day1-date-sthlm')
+// const day2DateSTHLM = document.getElementById('day2-date-sthlm')
+// const day3DateSTHLM = document.getElementById('day3-date-sthlm')
+// const day4DateSTHLM = document.getElementById('day4-date-sthlm')
+// const day5DateSTHLM = document.getElementById('day5-date-sthlm')
+// const day1STHLM = document.getElementById('day1-sthlm')
+// const day2STHLM = document.getElementById('day2-sthlm')
+// const day3STHLM = document.getElementById('day3-sthlm')
+// const day4STHLM = document.getElementById('day4-sthlm')
+// const day5STHLM = document.getElementById('day5-sthlm')
+
+// Fetch and display a 5 day forecast - Stockholm
+fetch(forecastSTHLM)
+  .then((response) => {
+    return response.json()
+  })
+  .then((fiveDaySTHLM) => {
+    console.log(fiveDaySTHLM)
+    const calcDay = new Date(fiveDaySTHLM.list[0].dt * 1000)
+    const forecastDay = ( new Date(calcDay)).toLocaleDateString('en-US', { 
+    weekday: 'short',
+    day: 'numeric'
+  })
+    const dayFeelsLike = Math.round(fiveDaySTHLM.list[0].main.feels_like)
+    const day = ''
+  
+    fiveDaySTHLM.list.forEach((day) => {
+      fiveDay.innerHTML = `<p class="date">${forecastDay}</p><p p class="temp">${dayFeelsLike}\u00b0</p>`
+    return day
+  })
+})
+
+
+// types.innerHTML = snorlax.types.map((a) => a.type.name)
+ // day1DateSTHLM.innerHTML = `${dayOne}`
+    // day1STHLM.innerHTML = `${day1FeelsLike}\u00b0`
+    
+
+    // const calcDay1 = new Date(fiveDaySTHLM.list.dt * 1000)
+    // const dayOne = ( new Date(calcDay1)).toLocaleDateString('en-US', { 
+    //   weekday: 'short',
+    //   day: 'numeric'
+    // })
+    // const day1FeelsLike = Math.round(fiveDaySTHLM.list[0].main.feels_like)
+    // const day1FeelsLike = Math.round(fiveDaySTHLM.list[1].main.feels_like)
+    // const day1FeelsLike = Math.round(fiveDaySTHLM.list[2].main.feels_like)
+    // const day1FeelsLike = Math.round(fiveDaySTHLM.list[3].main.feels_like)
+    // const day1FeelsLike = Math.round(fiveDaySTHLM.list[4].main.feels_like)
+
+    // day1DateSTHLM.innerHTML = `${dayOne}`
+    // day1STHLM.innerHTML = `${day1FeelsLike}\u00b0`
 
 
 // Step 5 - Style your weather app
