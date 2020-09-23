07c59f369596d398b05442c161037abd128b8c86
Moa Blomkvist <moa@birgerochblom.se>
2020-09-23 20:49:17 +0200

    sunset/sunrise - step 3

--- script.js
+++ script.js
@@ -2,26 +2,49 @@
 
 const apiToday = 'http://api.openweathermap.org/data/2.5/weather?q=Stockholm,Sweden&units=metric&APPID=e33f1cc192401277e601a6aed3a82800'
 const apiKey = "e33f1cc192401277e601a6aed3a82800"
+
 const weatherLocation = document.getElementById('location');
 const weatherDescription = document.getElementById('description');
 const weatherTemperature = document.getElementById('temperature');
-const weatherSunrise = document.getElementById('sunrise');
-const weatherSunset = document.getElementById('sunset');
+//const weatherSunrise = document.getElementById('sunrise');
+//const weatherSunset = document.getElementById('sunset');
 
+const fetchToday = () => {
 fetch(apiToday)
   .then((response) => {
     return response.json();
     })
   .then((json) => {
-    weatherToday(json)
-  })
 
-const weatherToday = (json) => {
 weatherLocation.innerHTML = json.name;
 weatherDescription.innerHTML = json.weather[0].description
 weatherTemperature.innerHTML = json.main.temp.toFixed(0.5)
-weatherSunrise.innerHTML = new Date(json.sys.sunrise * 1000).toLocaleTimeString([]);
-weatherSunset.innerHTML = new Date(json.sys.sunset * 1000).toLocaleTimeString([]);
-}
 
+//weatherSunrise.innerHTML = new Date(json.sys.sunrise * 1000).toLocaleTimeString([]);//
+//weatherSunset.innerHTML = new Date(json.sys.sunset * 1000).toLocaleTimeString([]);//
 
+//Made a funtion of Sunrise/sunset instead//
+const weatherSunset = () => {
+  const dateSunset = new Date(json.sys.sunset * 1000);
+  const timeSunset = dateSunset.toLocaleTimeString([], {
+    hour: '2-digit',
+    minute: '2-digit',
+  });
+  document.getElementById('sunset').innerHTML = timeSunset;
+};
+weatherSunset();
+
+const weatherSunrise = () => {
+  const dateSunrise = new Date(json.sys.sunrise * 1000);
+  const timeSunrise = dateSunrise.toLocaleTimeString([], {
+    hour: '2-digit',
+    minute: '2-digit',
+  });
+  document.getElementById('sunrise').innerHTML = timeSunrise;
+};
+weatherSunrise();
+});
+};
+fetchToday();
+
+//Weather for the Week //
