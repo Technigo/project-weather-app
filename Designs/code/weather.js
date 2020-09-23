208ea13de18b8010f065c068784765904594ca44
Katsiez <katarinajovanovic.a@gmail.com>
2020-09-23 11:41:34 +0200

    FINALLY getting somewhere, have displayed the data for temp and for description, praying I'll be able to display some more lol

--- Designs/code/weather.js
+++ Designs/code/weather.js
@@ -1,16 +1,20 @@
-// const apiKey = "c2889b12ee617ea787319a19a98a5906"
-
-const city = 'Toronto, Canada'
-const cityName = document.getElementById('city')
-const currentWeather = `http://api.openweathermap.org/data/2.5/weather?q=${city}&units=metric&APPID=c2889b12ee617ea787319a19a98a5906`
-const weatherForecast = `http://api.openweathermap.org/data/2.5/forecast?q=${city}&units=metric&APPID=c2889b12ee617ea787319a19a98a5906`
-
+const city = document.getElementById('city')
+const apiKey = "c2889b12ee617ea787319a19a98a5906"
+const currentWeatherUrl = `http://api.openweathermap.org/data/2.5/weather?q=Toronto,Canada&units=metric&APPID=c2889b12ee617ea787319a19a98a5906`
+const weatherForecast = `http://api.openweathermap.org/data/2.5/forecast?q=Toronto,Canada&units=metric&APPID=c2889b12ee617ea787319a19a98a5906`
+const temperature = document.getElementById('temperature')
+const description = document.getElementById('description')
 //Fetch with JSON
-fetch(currentWeather)
+fetch("http://api.openweathermap.org/data/2.5/weather?q=Toronto,Canada&units=metric&APPID=c2889b12ee617ea787319a19a98a5906")
     .then((response) => {
         return response.json()
     })
-    .then((theSix) => {
-        console.log(theSix)
-        cityName.innerHTML = city
+    .then((json) => {
+        currentWeatherToday(json)
     })
+
+const currentWeatherToday = (json) => {
+    temperature.innerHTML = json.main.temp.toFixed(0.5)
+    city.innerHTML = json.name
+    description.innerHTML = json.weather[0].description
+}
