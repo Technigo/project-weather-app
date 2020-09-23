c08eebf6dc2d08ac3c772f863f7ffce6b2f8ad0f
Ingela Löfgren <ingela.lofgren@gmail.com>
2020-09-23 20:53:08 +0200

    Step 3 finished

--- script.js
+++ script.js
@@ -1,7 +1,9 @@
 
 const city = document.getElementById("city");
-const weatherToday = document.getElementById("weatherToday")
-const temperature = document.getElementById("temperature")
+const weatherToday = document.getElementById("weatherToday");
+const temperature = document.getElementById("temperature");
+const sunrise = document.getElementById("sunrise");
+const sunset = document.getElementById("sunset");
 
 const weatherAPI = "http://api.openweathermap.org/data/2.5/weather?q=Stockholm,Sweden&units=metric&APPID=1c055fa282f5d9954e97fd78b7cd3c03";
 
@@ -11,7 +13,14 @@ fetch(weatherAPI)
     return response.json()
     })
     .then((json) => {
-    city.innerHTML += `<p>${json.name}</p>`;
-    weatherToday.innerHTML += `<p>${json.weather[0].description}</p>`;
-    temperature.innerHTML += `<p>${json.main.temp.toFixed(1)}</p>`;
-})
+    city.innerHTML += `<h2>${json.name}</h2>`;
+    
+    temperature.innerHTML += `<h1>${json.main.temp.toFixed(1)} °</h1>`;
+    weatherToday.innerHTML += `<h3>${json.weather[0].description}</h3>`;
+
+    const sunriseTime = new Date(json.sys.sunrise * 1000).toLocaleTimeString([], { hour: '2-digit', minute: '2-digit' });
+    sunrise.innerHTML += `<h4>Sunrise: ${sunriseTime}</h4>`;
+    
+    const sunsetTime = new Date(json.sys.sunset * 1000).toLocaleTimeString([], { hour: '2-digit', minute: '2-digit' });
+    sunset.innerHTML += `<h4>Sunset: ${sunsetTime}</h4>`;
+    })
