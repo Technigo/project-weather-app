c4654796ece0eef77f27bdedba83248f9e3e6d66
Liza Minicangit config --global user.email liza.minican@gmail.com <liza.minican@gmail.com>
2020-09-23 13:15:13 +0200

    sunrise/sunset in acceptable format, some design touches, data displayed in a correct way

--- code/script.js
+++ code/script.js
@@ -4,14 +4,13 @@ const url2 =
   "http://api.openweathermap.org/data/2.5/forecast?q=Barcelona,Spain&units=metric&APPID=65baa001e8c7b91e12c081e5f04cb9a6";
 
 //variables that will be used to display the results in html
-const weatherNow = document.getElementById("description");
-const degreesNow = document.getElementById("degrees");
-const sunriseTime = document.getElementById("sunrise");
-const sunsetTime = document.getElementById("sunset");
-const city = document.getElementById("cityName");
+//const weatherNow = document.getElementById("description");
+//const degreesNow = document.getElementById("degrees");
+//const sunriseTimeToday = document.getElementById("sunrise");
+//const sunsetTimeToday = document.getElementById("sunset");
+//const city = document.getElementById("cityName");
 const icon = document.getElementById("icon");
 const weatherMessage = document.getElementById("message");
-const container = document.getElementById("main");
 
 //function that fetches the weather information at the moment
 const fetchWeather = () => {
@@ -21,17 +20,36 @@ const fetchWeather = () => {
     })
     .then((weatherDoc) => {
       //weather conditions description at the moment
-      weatherNow.innerHTML = weatherDoc.weather[0].description;
+      document.getElementById("description").innerHTML =
+        weatherDoc.weather[0].description;
       //information on degreees at the moment
-      degreesNow.innerHTML = Math.round(weatherDoc.main.temp);
+      document.getElementById("degrees").innerHTML = Math.round(
+        weatherDoc.main.temp
+      );
 
       //console.log(weatherDoc);
       //sunrise time in the city
-      sunriseTime.innerHTML = weatherDoc.sys.sunrise;
+      const sunrise = () => {
+        const newSunrise = new Date(weatherDoc.sys.sunrise * 1000);
+        const sunriseTime = newSunrise.toLocaleTimeString([], {
+          hour: "2-digit",
+          minute: "2-digit",
+        });
+        document.getElementById("sunrise").innerHTML = sunriseTime;
+      };
+      sunrise();
       //sunset time in the city
-      sunsetTime.innerHTML = weatherDoc.sys.sunset;
+      const sunset = () => {
+        const newSunset = new Date(weatherDoc.sys.sunset * 1000);
+        const sunsetTime = newSunset.toLocaleTimeString([], {
+          hour: "2-digit",
+          minute: "2-digit",
+        });
+        document.getElementById("sunset").innerHTML = sunsetTime;
+      };
+      sunset();
       //city name
-      city.innerHTML = weatherDoc.name;
+      document.getElementById("cityName").innerHTML = weatherDoc.name;
     });
 };
 fetchWeather();
@@ -48,6 +66,25 @@ const fetchWeatherForecast = () => {
         item.dt_txt.includes("12:00")
       );
       console.log(filteredForecast);
+      //function to populate the main container
+      filteredForecast.forEach((day) => {
+        //console.log(day);
+        const dayFromData = new Date(day.dt_txt);
+        //const options = { weekday: "short", day: "numeric", month: "short" };
+        const theWeekDay = dayFromData.toLocaleDateString("en-US", {
+          weekday: "short",
+          day: "numeric",
+          month: "short",
+        });
+        document.getElementById(
+          "weatherByDay"
+        ).innerHTML += `<p>${theWeekDay}....................${Math.round(
+          day.main.temp
+        )}ºC</p>`;
+      });
     });
 };
 fetchWeatherForecast();
+
+//${day.main.humidity}%
+//${Math.round(day.main.feels_like)}
