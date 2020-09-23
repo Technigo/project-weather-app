b9b4223b4460abb44e60427059657e75bb7e6a40
Lili <lilihorvath@github.com>
2020-09-23 15:49:10 +0200

    Got a hint from StackOverflow for the sunrise/sunset

--- index.js
+++ index.js
@@ -1,7 +1,25 @@
+const city = document.getElementById("city");
+const weatherIcon = document.getElementById("weatherIcon");
+const temperature = document.getElementById("temperature");
+const sunrise = document.getElementById("sunrise");
+const sunset = document.getElementById("sunset");
+const weekdays = [
+    "Mon",
+    "Tue",
+    "Wed",
+    "Thu",
+    "Fri",
+    "Sat",
+    "Sun"
+];
+
 fetch("https://api.openweathermap.org/data/2.5/forecast?q=Stockholm,Sweden&units=metric&APPID=25b3459d6eea6c3844f60f68deed9511")
     .then(response => {
         return response.json();
     })
     .then(json => {
         console.log(json);
+        let sunRise = new Date (json.city.sunriese *1000);
+        let sunSet = new Date (json.city.sunriese *1000);
+        city.innerHTML = json.city.name;
     })
