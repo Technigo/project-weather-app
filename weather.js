a947f0697144664c0455a73cd74f15811b57bae1
elisa blomberg <elisa.blomberg@outlook.com>
2020-09-23 10:02:16 +0200

    Step 2 done

--- weather.js
+++ weather.js
@@ -1,8 +1,14 @@
 const apiUrl = 'https://api.openweathermap.org/data/2.5/weather?q=Stockholm,Sweden&units=metric&APPID=8b97619989976c72fc1e602d8c793890';
 
+
 fetch(apiUrl).then((response) => {
     return response.json();
-}).then((weather) => {
-    container.innerHTML += `<h1>In ${weather.name} we have ${weather.main.temp} celsius right now. Clouds = ${weather.clouds.all}.</h1>`;
-    console.log(weather)
+}).then((json) => {
+    container.innerHTML += `<h1>In ${json.name} temperature is ${json.main.temp.toFixed(1)} celsius right now.</h1>`;
+    //.toFixed(1) rounds the temperature to 1 decimal
+    
+    json.weather.forEach((weather) => {
+    container.innerHTML += `<p>The weather is ${weather.description}</p>`
+    });
+    console.log(json)
 });
