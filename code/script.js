3b302f97cd96d5575d2aec21105508df71b783dc
Sofia Sousa <sofiavazsousa@gmail.com>
2020-09-23 21:55:50 +0200

    Added weather different cities + weather icons depending on the current weather

--- code/script.js
+++ code/script.js
@@ -1,12 +1,9 @@
 
-// Location Variables
-const city = 'Porto, Portugal';
+// Location + UV index coordinates Variables
+let city = 'Porto, Portugal';
+let latitude ='41.15';
+let longitude = '8.61';
 const cityName = document.getElementById('city');
-
-// API Variables
-const apiCurrentWeatherUrl = `http://api.openweathermap.org/data/2.5/weather?q=${city}&units=metric&APPID=09124a2f59a3124951523d476ed8a36d`;
-const apiForecastWeatherUrl = `http://api.openweathermap.org/data/2.5/forecast?q=${city}&units=metric&appid=09124a2f59a3124951523d476ed8a36d`;
-const apiUvIndexUrlPorto = 'http://api.openweathermap.org/data/2.5/uvi?lat=41.15&lon=8.61&appid=09124a2f59a3124951523d476ed8a36d';
 
 // Weather Variables 
 const currentTemperature = document.getElementById('temperature');
@@ -15,11 +12,24 @@ const realFeel = document.getElementById('real-feel');
 const currentWind = document.getElementById('wind');
 const sunrise = document.getElementById('sunrise');
 const sunset = document.getElementById('sunset');
-const uvIndexPorto = document.getElementById('uv')
+const uvIndexPorto = document.getElementById('uv');
+const body = document.getElementsByTagName('body')[0];
 
 // Date Variables
 let todaysDate = new Date();
 const daysOfTheWeek = ["Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday"];
+
+
+// Function to change background 
+
+const backgroundChange = (weatherInfo) => { 
+    if (weatherInfo.main.temp.toFixed(1) >= 18 || weatherInfo.main.feels_like.toFixed(1) >= 18) {
+        body.classList.add('warm')
+    } else{ 
+        body.classList.remove('warm')
+    }
+}
+
 
 // Main function that shows the updated weather
 const updateCityWeather = (weatherInfo) => { 
@@ -28,63 +38,66 @@ const updateCityWeather = (weatherInfo) => {
     sunriseSunset(weatherInfo);
     updateRoundTemperatures(weatherInfo);
     updateWind(weatherInfo);
+    backgroundChange(weatherInfo);
+    weatherIconChange(weatherInfo.weather[0].main)
 };
 
+const weatherIconChange = (weather) =>{
+    if (weather == "Rain") {
+    document.getElementById('weatherIcon').src = './assets/rain.png';
+    } if (weather == "Clouds") {
+    document.getElementById('weatherIcon').src = './assets/cloudy.png';
+    } if (weather == "Clear" || weather == "Sunny") {
+    document.getElementById('weatherIcon').src= './assets/sunny.png';
+    }
+  }
+  
 // Current and real feel rounded temperature function
 
 const updateRoundTemperatures = (weatherInfo) => {
     const roundedTemperature = weatherInfo.main.temp.toFixed(1);
-    currentTemperature.innerHTML += `${roundedTemperature} °C`;
+    currentTemperature.innerHTML = `${roundedTemperature} °C`;
     const realFeelRoundedTemp = weatherInfo.main.feels_like.toFixed(1);
-    realFeel.innerHTML += `${realFeelRoundedTemp}°C`;
+    realFeel.innerHTML = `Feels like: ${realFeelRoundedTemp}°C`;
 };
 
 // WIND Function 
 
 const updateWind = (weatherInfo) => { 
     const wind = weatherInfo.wind.speed;
-    currentWind.innerHTML += `${wind} m/s`;
+    currentWind.innerHTML = `Wind Speed: ${wind} m/s`;
 };
 
 // SUNRISE & SUNSET Function
 const sunriseSunset = (weatherInfo) => {
     const sunriseTime = new Date(weatherInfo.sys.sunrise * 1000).toLocaleTimeString([], { hour: '2-digit', minute: '2-digit' });
-    sunrise.innerHTML += `${sunriseTime}`;
+    sunrise.innerHTML = `Sunrise: ${sunriseTime}`;
     const sunsetTime = new Date(weatherInfo.sys.sunset * 1000).toLocaleTimeString([], { hour: '2-digit', minute: '2-digit' });
-    sunset.innerHTML += `${sunsetTime}`;
+    sunset.innerHTML = `Sunset: ${sunsetTime}`;
 };
 
 // UV index Function 
 const updateUvIndex = (uvInfo) => {
     const uvIndex = uvInfo.value;
-    uv.innerHTML += `${uvIndex}`;
+    uv.innerHTML = `UV index: ${uvIndex}`;
 }
 
-// Fetch Weather Data
-fetch(apiCurrentWeatherUrl)
-    .then((response) => { 
-        return response.json()
-    })
-    .then((json) => { 
-        console.log(json);
-        updateCityWeather(json);    
-    })
-      .catch((error) => {
-        console.log(error) 
-    })
+// Fetch UV index Data 
 
-// Fetch UV index Data for Porto
-fetch(apiUvIndexUrlPorto)
+const loadUvIndex = (latitude, longitude) => { 
+    const apiUvIndexUrl = `http://api.openweathermap.org/data/2.5/uvi?lat=${latitude}&lon=${longitude}&appid=09124a2f59a3124951523d476ed8a36d`;
+    fetch(apiUvIndexUrl)
     .then((response) => {
         return response.json()
     })
-.then ((json) => {
-    console.log(json)
-    updateUvIndex(json);
-})
-.catch((error) => {
-    console.log(error) 
-})
+    .then ((json) => {
+        console.log(json)
+        updateUvIndex(json);
+    })
+    .catch((error) => {
+        console.log(error) 
+    })
+};
 
 
 // Function to get the min and max temps over the day from the 5 day forecast (3hr interval)
@@ -111,8 +124,9 @@ const updateMinMaxTemps = (data) => {
         }
     });
     console.log(minMaxTemps)
+    const forecast = document.getElementById('forecast');
+    forecast.innerHTML = "";
     for (const date in minMaxTemps) { 
-        const forecast = document.getElementById('forecast');
         forecast.innerHTML += `<div class="column">${minMaxTemps[date].dayOfWeek}</div>`
         forecast.innerHTML += `<div class="column">${minMaxTemps[date].minTemp.toFixed(1)} °C | ${minMaxTemps[date].maxTemp.toFixed(1)} °C </div>`
     };
@@ -120,12 +134,60 @@ const updateMinMaxTemps = (data) => {
 }
 
 // Fetch 5-day Forecast Data
-fetch(apiForecastWeatherUrl)
-.then((response) => { return response.json(); })
-.then((data) => { updateMinMaxTemps(data); }); 
+
+const loadFiveDaysWeatherForecast = (city) => { 
+    const apiForecastWeatherUrl = `http://api.openweathermap.org/data/2.5/forecast?q=${city}&units=metric&appid=09124a2f59a3124951523d476ed8a36d`;
+    
+    fetch(apiForecastWeatherUrl)
+    .then((response) => { return response.json(); })
+    .then((data) => { updateMinMaxTemps(data); }); 
+}
 
 
+// Fetch Weather Data
+const loadWeatherCity = (city) => {
+    const apiCurrentWeatherUrl = `http://api.openweathermap.org/data/2.5/weather?q=${city}&units=metric&APPID=09124a2f59a3124951523d476ed8a36d`;
+    fetch(apiCurrentWeatherUrl)
+    .then((response) => { 
+        return response.json()
+    })
+    .then((json) => { 
+        console.log(json);
+        updateCityWeather(json);    
+    })
+      .catch((error) => {
+        console.log(error) 
+    })
+}
 
+const initializePage = (city, latitude, longitude) => {
+    loadWeatherCity(city);
+    loadFiveDaysWeatherForecast(city);
+    loadUvIndex(latitude,longitude);
+}
+
+
+document.getElementById('porto').addEventListener('click', () => {
+    city = 'Porto, Portugal';
+    latitude = '41.15';
+    longitude = '8.61';
+    initializePage(city, latitude, longitude);
+});
+
+document.getElementById('london').addEventListener('click', () => {
+    city = 'London, UK';
+    latitude = '51.51';
+    longitude = '-0.13';
+    initializePage(city, latitude, longitude);
+});
+
+document.getElementById('stockholm').addEventListener('click', () => {
+    city = 'Stockholm, Sweden';
+    latitude = '59.33';
+    longitude = '18.06';
+    initializePage(city, latitude, longitude);
+});
+initializePage(city, latitude, longitude);
 
 
 
