8d5b14fc5ede5f1f05456bd35785c199b6091e2a
Peggy <64912576+pwangy@users.noreply.github.com>
2020-09-23 13:31:14 +0200

    added styling. reworked js to have city variable.

--- script.js
+++ script.js
@@ -1,42 +1,69 @@
-// Stockholm - Current weather
-const weatherTodaySTHLM = 'http://api.openweathermap.org/data/2.5/weather?id=2673730&units=metric&appid=eb46c8c17530a3d02461794022d39d32'
-const locationStockholm = document.getElementById('location-sthlm')
-const sunriseSTHLM = document.getElementById('sunrise-sthlm')
-const sunsetSTHLM = document.getElementById('sunset-sthlm')
-const nowSTHLM = document.getElementById('now-sthlm')
-const icoSTHLM = document.getElementById('now-icon')
-const descSTHLM = document.getElementById('now-desc-sthlm')
+// Current weather
+const stockholm = 2673730
 
-fetch(weatherTodaySTHLM)
+// const cities = [
+//   {
+//     name: 'Stockholm',
+//     id: 2673730
+//   },
+//   {
+//     name: 'San Francisco',
+//     id: 5391959
+//   },
+//   {
+//     name: 'London',
+//     id: 2643743
+//   },
+//   {
+//     name: 'Taipei',
+//     id: 1665148
+//   }
+// ]
+// need method of picking city along with default to Stockholm on page load.
+
+const weatherSTHLM = `http://api.openweathermap.org/data/2.5/weather?id=${stockholm}&units=metric&appid=eb46c8c17530a3d02461794022d39d32`
+const where = document.getElementById('location')
+const sunriseTime = document.getElementById('sunrise')
+const sunsetTime = document.getElementById('sunset')
+const now = document.getElementById('now')
+const ico = document.getElementById('now-icon')
+const desc = document.getElementById('now-desc')
+
+fetch(weatherSTHLM)
   .then((response) => {
     return response.json()
 })
-  .then((sthlm) => {
-    console.log(sthlm)
-    const tempNow = Math.round(sthlm.main.temp)
-    const nowFeelsLike = Math.round(sthlm.main.feels_like)
-    const sunriseCalc = new Date(sthlm.sys.sunrise * 1000)
-    const sunrise = ( new Date(sunriseCalc)).toLocaleTimeString('sv-SE', {
+  .then((city) => {
+    console.log(city)
+    const dateCheck = new Date(city.dt * 1000)
+    const todayIs = (new Date(dateCheck)).toLocaleDateString()
+    console.log(`TODAY: ${todayIs}`)
+
+
+    const tempNow = Math.round(city.main.temp)
+    const nowFeelsLike = Math.round(city.main.feels_like)
+    const sunriseCalc = new Date(city.sys.sunrise * 1000)
+    const sunrise = (new Date(sunriseCalc)).toLocaleTimeString('sv-SE', {
       hour12: false, 
       hour: '2-digit',
       minute: '2-digit'
     })
-    const sunsetCalc = new Date(sthlm.sys.sunset * 1000)
-    const sunset = ( new Date(sunsetCalc)).toLocaleTimeString('sv-SE', {
+    const sunsetCalc = new Date(city.sys.sunset * 1000)
+    const sunset = (new Date(sunsetCalc)).toLocaleTimeString('sv-SE', {
       hour12: false, 
       hour: '2-digit',
-      miu8sznute: '2-digit'
+      minute: '2-digit'
     })
-    locationStockholm.innerHTML = `${sthlm.weather[0].main} | ${tempNow}\u00b0C`
-    sunriseSTHLM.innerHTML = `Sunrise ${sunrise}`
-    sunsetSTHLM.innerHTML = `Sunset ${sunset}`
-    nowSTHLM.innerHTML = `It feels like ${nowFeelsLike}\u00b0C in ${sthlm.name}.`
-    descSTHLM.innerHTML = sthlm.weather[0].description
-    icoSTHLM.src = `http://openweathermap.org/img/wn/${sthlm.weather[0].icon}@2x.png`
+    where.innerHTML = `${city.weather[0].main} | ${tempNow}\u00b0C`
+    sunriseTime.innerHTML = `Sunrise ${sunrise}`
+    sunsetTime.innerHTML = `Sunset ${sunset}`
+    now.innerHTML = `It feels like ${nowFeelsLike}\u00b0C in ${city.name}.`
+    desc.innerHTML = city.weather[0].description
+    ico.src = `http://openweathermap.org/img/wn/${city.weather[0].icon}@2x.png`
 })
 
 // Stockholm - 5 day forecast
-const forecastSTHLM = 'http://api.openweathermap.org/data/2.5/forecast?id=2673730&units=metric&appid=eb46c8c17530a3d02461794022d39d32'
+const forecastSTHLM = `http://api.openweathermap.org/data/2.5/forecast?id=${stockholm}&units=metric&appid=eb46c8c17530a3d02461794022d39d32`
 const fiveDay = document.getElementById('five-Day') 
 
 
@@ -55,7 +82,9 @@ fetch(forecastSTHLM)
         day: 'numeric'
       })
       const dayTemp = Math.round(day.main.feels_like)
-      
+
+      console.log(date)
+
       fiveDay.innerHTML += 
       `<p class="date">${dayName}</p>
       <p class="temp">${dayTemp}\u00b0C</p>
@@ -65,7 +94,6 @@ fetch(forecastSTHLM)
 })
 
 
-    
 // Step 5 - Style your weather app
 
 // Once you get the data onto your site, style it to look like one of the provided designs.
@@ -74,7 +102,7 @@ fetch(forecastSTHLM)
 
 // Requirements
 // 🔵  Blue Level (Minimum Requirements)
-
+// - You should fetch data from the API using `fetch()` in JavaScript
 // - All data in the sketch above should be present and fetched from the API
 // - The presentation of the data should be in the specified format.
 // - The page should work on mobile (mobile first!), tablet and desktop (Be responsive)
