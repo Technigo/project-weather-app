29a8f0a903442d722b3b7d2e47c579951b4930c4
nathalie <nathalie-nordstrom@hotmail.com>
2020-09-23 17:04:21 +0200

    adding url

--- Designs/Design-1/index.js
+++ Designs/Design-1/index.js
@@ -1,15 +1,47 @@
-const weatherUrl = 'https://api.openweathermap.org/data/2.5/weather?q=Stockholm,Sweden&units=metric&APPID=742fe8feec76d1ec7cd8207fdf08fb30';
-const container = document.getElementById('main');
-const weatherCity = document.getElementById('city')
+const currentUrl = 'https://api.openweathermap.org/data/2.5/weather?q=Stockholm,Sweden&units=metric&APPID=742fe8feec76d1ec7cd8207fdf08fb30';
+const fiveDayUrl = 'http://api.openweathermap.org/data/2.5/forecast?q=Stockholm,Sweden&units=metric&APPID=742fe8feec76d1ec7cd8207fdf08fb30';
 
+const degree = document.getElementById("degree");
+const city = document.getElementById("city");
+const country = document.getElementById("country");
+const weather = document.getElementById("weather");
+const sunrise = document.getElementById("sunrise");
+const sunset = document.getElementById("sunset");
+const feels = document.getElementById("feels");
 
-fetch(weatherUrl)
+const today = document.getElementsByClassName("today");
+console.log(today);
+
+fetch(currentUrl)
 .then((response) => {
     return response.json()
+    })
+
+.then((json) => {
+   console.log(json);
+    degree.innerHTML = (json.main.temp) + ` C °`;
+    feels.innerHTML = (json.main.feels_like) + ` C °`;
+    city.innerHTML = json.name;
+    country.innerHTML = json.sys.country;
+
+    json.weather.forEach(item => {
+        weather.innerHTML = item.description
+    })
+    
+    sunrise.innerHTML = json.sys.sunrise;
+    sunset.innerHTML = json.sys.sunset;
 })
-.then((weatherArray) => {
-    weatherCity.innerHTML = weatherArray.name;
-
-});
 
 
+//5-days forcast //
+
+  
+fetch(fiveDayUrl)
+.then((response) => {
+    return response.json()
+    })
+
+.then((json) => {
+    console.log(json);
+    monday.innerHTML = (json.main.temp) + ` C °`;
+})
