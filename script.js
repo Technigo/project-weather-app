d0e6c97be054586d1e66174d892d704b9b995d54
Emelie Svensson <emeliesv@live.se>
2020-09-23 09:45:30 +0200

    changed generatedHTMLForWeatherToday

--- script.js
+++ script.js
@@ -8,28 +8,28 @@ const apiUrlForecast =
 
 //Creat function that calls all temperaturefunctions
 const callTempFunctions = (number) => {
-  calculateTemperature(number)
-  minMaxTemperature(number)
-  feelsLikeTemperature(number)
-} 
+  calculateTemperature(number);
+  minMaxTemperature(number);
+  feelsLikeTemperature(number);
+};
 
 const calculateTemperature = (number) => {
   const roundedTemp = Math.round(number * 10) / 10; //By adding *10 AND adding /10 the number is rounded up to nearest integer with one decimal. If only using round() the number is rounded up to nearest integer.
   return roundedTemp;
-}; 
+};
 
 //Create function that shows min/max temp
 const minMaxTemperature = (number) => {
   const minTemp = Math.round(number * 10) / 10;
-  console.log(`The min is ${minTemp}`)
+  console.log(`The min is ${minTemp}`);
   //minMaxTemperature(filteredForecast.list[0].main.temp_min)
-//API main.temp_min main.temp_max
-}
+  //API main.temp_min main.temp_max
+};
 
 //Create function that shows 'feels like'temp
 const feelsLikeTemperature = (number) => {
- //API main.feels_like
-}
+  //API main.feels_like
+};
 
 const calculatingSun = (time) => {
   const sunTime = new Date(time * 1000);
@@ -45,12 +45,12 @@ const calculatingSun = (time) => {
 const printDay = (day) => {
   const forecastDays = new Date(day);
   const forecastDaysString = forecastDays.toLocaleDateString("en-US", {
-    weekday: "short", 
-    month: 'short',
-    day: 'numeric'
+    weekday: "short",
+    month: "short",
+    day: "numeric",
   });
   /* console.log(forecastDaysString); */
-  return forecastDaysString; 
+  return forecastDaysString;
 };
 
 const generatedHTMLForWeatherToday = (weatherToday) => {
@@ -58,10 +58,21 @@ const generatedHTMLForWeatherToday = (weatherToday) => {
   console.log(weatherToday.sys.sunrise);
   const sunrise = calculatingSun(weatherToday.sys.sunrise);
   const sunset = calculatingSun(weatherToday.sys.sunset);
-  containerToday.innerHTML = `<h1>This is ${weatherToday.name}</h1>`;
-  descriptionToday.innerHTML = `The temperature today is ${temperature} degrees and it's ${weatherToday.weather[0].description} outside.`;
+  const description = weatherToday.weather[0].description;
+
+  let dailyForecastHTML = "";
+  dailyForecastHTML += `<img src= ''/>`;
+  dailyForecastHTML += `<div class="local-info">`;
+  dailyForecastHTML += `<div class="temperature">${temperature} \xB0 </div>`;
+  dailyForecastHTML += `<div class="city">${weatherToday.name}</div>`;
+  dailyForecastHTML += `<div class="description">${description}</div>`;
+  dailyForecastHTML += `<div class="description">${sunrise}</div>`;
+  dailyForecastHTML += `<div class="description">${sunset}</div>`;
+  dailyForecastHTML += `</div>`;
+  return dailyForecastHTML
+
   //Since weather is an array, we need to access the index of 0, and then we can locate the object keyvalues i.e .description. This has to be done even if there is only one array, as in this case.
-  descriptionToday.innerHTML += `The sun rises at ${sunrise} and sets at ${sunset}`;
+  //descriptionToday.innerHTML += `The sun rises at ${sunrise} and sets at ${sunset}`;
 };
 
 const generatedHTMLForWeatherForecast = (filteredForecast) => {
@@ -69,22 +80,21 @@ const generatedHTMLForWeatherForecast = (filteredForecast) => {
   //Tells what day it is
   console.log(filteredForecast.main.temp);
   const dailyTemp = calculateTemperature(filteredForecast.main.temp); //Would also like for this to tell min/max-temp
-  //const minMax = minMaxTemperature(filteredForecast.list[0].main.temp_min) //Make use of minMaxfunction! Get an errormessage ATM 
+  //const minMax = minMaxTemperature(filteredForecast.list[0].main.temp_min) //Make use of minMaxfunction! Get an errormessage ATM
   /*Uncaught (in promise) TypeError: Cannot read property '0' of undefined
   at generatedHTMLForWeatherForecast (script.js:70)
   at script.js:113
   at Array.forEach (<anonymous>)
   at script.js:112 */
-  
-  let innerText = '';
+
+  let innerText = "";
   innerText += `<div class="day-box">`;
   innerText += `<p class="day">${day}</p>`;
-  innerText += `<p>${dailyTemp} degrees</p>`
+  innerText += `<p>${dailyTemp} degrees</p>`;
   innerText += `</div>`;
   return innerText;
   //Weather description for the next five days
   //Humidity and wind
-  
 
   /*let launchHTML = '';
   launchHTML += `<section class="launch">`;
@@ -100,7 +110,7 @@ const fetchWeatherToday = () => {
       return response.json();
     })
     .then((weatherToday) => {
-      generatedHTMLForWeatherToday(weatherToday);
+      containerToday.innerHTML += generatedHTMLForWeatherToday(weatherToday);
     });
 };
 
@@ -114,7 +124,7 @@ const fetchWeatherForecast = () => {
         item.dt_txt.includes("12:00")
       );
       console.log(filteredForecast);
-      containerForecast.innerHTML = `<h1>The weather for the next five days will be:</h1>`
+      containerForecast.innerHTML = `<h1>The weather for the next five days will be:</h1>`;
 
       filteredForecast.forEach((forecast) => {
         containerForecast.innerHTML += generatedHTMLForWeatherForecast(
