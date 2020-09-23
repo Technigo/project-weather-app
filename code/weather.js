5756c5cf0404030141850340be173622fa07e1ae
caroline birgersson <caroline@birgerochblom.se>
2020-09-23 21:20:45 +0200

    created some functions in iteration 2 & 3

--- code/weather.js
+++ code/weather.js
@@ -1,31 +1,49 @@
-//Weather Today in Stockholm
+//Weather from API
 
 const apiToday = 'http://api.openweathermap.org/data/2.5/weather?q=Stockholm,Sweden&units=metric&APPID=e33f1cc192401277e601a6aed3a82800'
 const apiKey = "e33f1cc192401277e601a6aed3a82800"
 const weatherLocation = document.getElementById('location');
 const weatherDescription = document.getElementById('description');
 const weatherTemperature = document.getElementById('temperature');
-const weatherSunrise = document.getElementById('sunrise')
-const weatherSunset = document.getElementById('sunset')
 
-fetch(apiToday)
-  .then((response) => {
+// Fetch function
+const fetchWeather = () => {
+    fetch(apiToday)
+    .then((response) => {
     return response.json();
     })
-  .then((json) => {
-    weatherToday(json)
-  })
+    .then((json) => {
+    weatherLocation.innerHTML = json.name;
+    weatherDescription.innerHTML = json.weather[0].description
+    weatherTemperature.innerHTML = json.main.temp.toFixed(0.5)
 
-const weatherToday = (json) => {
-weatherLocation.innerHTML = json.name;
-weatherDescription.innerHTML = json.weather[0].description
+// Sunrise
+    const weatherSunrise = () => {
+        const dateSunrise = new Date(json.sys.sunrise * 1000);
+        const timeSunrise = dateSunrise.toLocaleTimeString([], {
+            hour: '2-digit',
+            minute: '2-digit',
+        });
+        document.getElementById('sunrise').innerHTML = timeSunrise;
+  };
 
-// Sunrise & Sunset
-weatherSunrise.innerHTML = new Date(json.sys.sunrise * 1000).toLocaleTimeString([]);
-weatherSunset.innerHTML = new Date(json.sys.sunset * 1000).toLocaleTimeString([]);
-}
+    weatherSunrise();
 
+// Sunset
+    const weatherSunset = () => {
+        const dateSunset = new Date(json.sys.sunset * 1000);
+        const timeSunset = dateSunset.toLocaleTimeString([], {
+            hour: '2-digit',
+            minute: '2-digit',
+        });
+        document.getElementById('sunset').innerHTML = timeSunset;
+    };
 
+    weatherSunset();
+ });
+};
+
+fetchWeather();
 
 
 
