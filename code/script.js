137fdb821843f72cdcc6f6db691bbeff2b82aa37
MindstormingAB <s.elander@mindstorming.se>
2020-09-23 19:28:06 +0200

    Step 3 - Sunrise and sunset

--- code/script.js
+++ code/script.js
@@ -1,18 +1,31 @@
 const weatherApiKey = config.WEATHER_API_KEY;
 
-const apiUrl = 'https://api.openweathermap.org/data/2.5/weather?q=Stockholm,Sweden&units=metric&APPID=' + weatherApiKey;
+const WEATHER_API_URL = 'https://api.openweathermap.org/data/2.5/weather?q=Stockholm,Sweden&units=metric&APPID=' + weatherApiKey;
 
 const city = document.getElementById('city');
 const temperature = document.getElementById('temperature');
 const description = document.getElementById('description');
+const picto = document.getElementById('picto');
+const sunset = document.getElementById('sunset');
+const sunrise = document.getElementById('sunrise');
 
-fetch(apiUrl)
+fetch(WEATHER_API_URL)
   .then((response) => {
     return response.json();
   })
   .then((weatherArray) => {
-    console.log(weatherArray);
+    // console.log(weatherArray);
     city.innerHTML = weatherArray.name;
     temperature.innerHTML = `${weatherArray.main.temp.toFixed(1)}\xB0`;
     description.innerHTML = weatherArray.weather.map((a) => a.description);
-  })
+    sunrise.innerHTML = (new Date(weatherArray.sys.sunrise * 1000)).toLocaleTimeString([], {
+      hour: '2-digit',
+      minute: '2-digit',
+      hour12: false,
+    });;
+    sunset.innerHTML = (new Date(weatherArray.sys.sunset * 1000)).toLocaleTimeString([], {
+      hour: '2-digit',
+      minute: '2-digit',
+      hour12: false,
+    });;
+  });
