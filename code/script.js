a290f7b45b438560532535a51944a8677a52acd7
AnnaHellkvist <51534839+AnnaHellkvist@users.noreply.github.com>
2020-09-23 13:23:16 +0200

    Step 3 done, showing sunrise and sunset on page

--- code/script.js
+++ code/script.js
@@ -1,6 +1,9 @@
 import { API_KEY } from "./api.js";
 
-fetch(`http://api.openweathermap.org/data/2.5/weather?q=Karlstad,Sweden&units=metric&appid=${API_KEY}`)
+const API_URL_TODAY = `http://api.openweathermap.org/data/2.5/weather?q=Karlstad,Sweden&units=metric&appid=${API_KEY}`;
+const API_URL_FORECAST = `http://api.openweathermap.org/data/2.5/forecast?q=Karlstad,Sweden&units=metric&appid=${API_KEY}`;
+
+fetch(API_URL_TODAY)
 .then((response) => {
     return response.json( )
 })
@@ -9,12 +12,21 @@ fetch(`http://api.openweathermap.org/data/2.5/weather?q=Karlstad,Sweden&units=me
     containerCity.innerHTML = `<p>Presenting the weather in ${json.name}</p>`;
     containerTemp.innerHTML = `<p>The temperature is ${json.main.temp} degrees Celsius</p>`;
     containerDescription.innerHTML = `<p>Description: ${json.weather[0].description}</p>`;
+
+    const sunrise = new Date(json.sys.sunrise * 1000).toLocaleTimeString('en-US', { timeStyle: "short" });
+    containerSunrise.innerHTML = `<p>Sunrise at ${sunrise}</p>`;
+    console.log(sunrise);
+    let sunset = new Date(json.sys.sunset * 1000).toLocaleTimeString('en-US', { timeStyle: "short" });
+    containerSunset.innerHTML = `<p>Sunset at ${sunset}</p>`;
+    console.log(sunset);
 })
 
-fetch(`http://api.openweathermap.org/data/2.5/forecast?q=Karlstad,Sweden&units=metric&appid=${API_KEY}`)
+fetch(API_URL_FORECAST)
 .then((response) => {
     return response.json( )
 })
 .then((json) => {
     console.log(json)
 })
+
+
