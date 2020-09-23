2a4ac331e0d208604a99bb6641aceaab76ada0c2
Hanna Skeppe <hannaskeppe@gmail.com>
2020-09-23 11:04:43 +0200

    Continued to work on generatedHTMLForWeatherToday and generatedHTMLForWeatherForecast

--- script.js
+++ script.js
@@ -2,7 +2,7 @@ const containerToday = document.getElementById("weatherToday");
 const descriptionToday = document.getElementById("text");
 const containerForecast = document.getElementById("forecastWrapper");
 const apiUrlToday =
-  "http://api.openweathermap.org/data/2.5/weather?q=Stockholm,Sweden&units=metric&APPID=92053ce17f6df07312088f05e0a431e0";
+  "https://api.openweathermap.org/data/2.5/weather?q=Stockholm,Sweden&units=metric&APPID=92053ce17f6df07312088f05e0a431e0";
 const apiUrlForecast =
   "https://api.openweathermap.org/data/2.5/forecast?q=Stockholm,Sweden&units=metric&APPID=92053ce17f6df07312088f05e0a431e0";
 
@@ -22,40 +22,76 @@ const calculatingSun = (time) => {
   return sunTimeString;
 };
 
-const printDay = (day) => {
+const printDay = (day) => { //
   const forecastDays = new Date(day);
   const forecastDaysString = forecastDays.toLocaleDateString("en-US", {
     weekday: "short",
+    month: "short", //Emelie lade till
+    day: "numeric" //Emelie lade till
   });
   console.log(forecastDaysString);
   return forecastDaysString;
 };
 
+const iconDependingOnWeather = (item) => {
+  const iconMainDescription = item
+
+  if (iconMainDescription === 'Clouds') {
+    return 'http://openweathermap.org/img/wn/03d@2x.png'
+  } else if (iconMainDescription === 'Clear') {
+    return 'http://openweathermap.org/img/wn/01d@2x.png'
+  } else if (iconMainDescription === 'Snow') {
+    return 'http://openweathermap.org/img/wn/13d@2x.png'
+  } else if (iconMainDescription === 'Rain') {
+    return 'http://openweathermap.org/img/wn/10d@2x.png'
+  } else if (iconMainDescription === 'Drizzle') {
+    return 'http://openweathermap.org/img/wn/09d@2x.png'
+  } else if (iconMainDescription === 'Thunderstorm') {
+    return 'http://openweathermap.org/img/wn/11d@2x.png'
+  } else (iconMainDescription === 'Thunderstorm')
+    return 'http://openweathermap.org/img/wn/50d@2x.png'
+}
+
 const generatedHTMLForWeatherToday = (weatherToday) => {
   const temperature = calculateTemperature(weatherToday.main.temp); //This is using json.main.temp as a parameter instead of number.
-  console.log(weatherToday.sys.sunrise);
+  //console.log(weatherToday.sys.sunrise);
+  //Lägg in time in city
   const sunrise = calculatingSun(weatherToday.sys.sunrise);
   const sunset = calculatingSun(weatherToday.sys.sunset);
-  containerToday.innerHTML = `<h1>This is ${weatherToday.name}</h1>`;
-  descriptionToday.innerHTML = `The temperature today is ${temperature} degrees and it's ${weatherToday.weather[0].description} outside.`;
+  const iconToday = iconDependingOnWeather(weatherToday.weather[0].main)
+  const description = weatherToday.weather[0].description
+  // containerToday.innerHTML = `<h1>This is ${weatherToday.name}</h1>`;
+  // descriptionToday.innerHTML = `The temperature today is ${temperature} degrees and it's ${weatherToday.weather[0].description} outside.`; 
   //Since weather is an array, we need to access the index of 0, and then we can locate the object keyvalues i.e .description. This has to be done even if there is only one array, as in this case.
-  descriptionToday.innerHTML += `The sun rises at ${sunrise} and sets at ${sunset}`;
+  // descriptionToday.innerHTML += `The sun rises at ${sunrise} and sets at ${sunset}`; //Kommenterar ut pga specar längre ner istället
+  let weatherTodayHTML = '';
+  weatherTodayHTML += `<div class="location-information">`;
+  weatherTodayHTML += `<div class="temp">${temperature}</div>`
+  weatherTodayHTML += `<div class="location">${weatherToday.name}</div>`//Staden
+  weatherTodayHTML += `<div class="description">${description}</div>`//description typ broken clouds
+  weatherTodayHTML += `<div class="description"></div>`//description typ broken clouds
+  
+  weatherTodayHTML += `<div class="description"></div>`//Time in city
+  weatherTodayHTML += `</div>`;
+  weatherTodayHTML += `<img src='${iconToday}'>`; //Bilden utanför pga kunna sätta position absolute
+  return weatherTodayHTML;
 };
 
+
 const generatedHTMLForWeatherForecast = (filteredForecast) => {
-  //printDay(filteredForecast.dt_txt) //Should tell what day it is but doesn't work at the moment
+  const weekday = printDay(filteredForecast.dt_txt); //Should tell what day it is but doesn't work at the moment
     console.log(filteredForecast.main.temp)
   const dailyTemp = calculateTemperature(filteredForecast.main.temp);
+  const iconForecast = iconDependingOnWeather(filteredForecast.weather[0].main);
   return dailyTemp
+
+  let WeatherForecast ='';
+  weatherForecast += `<div class="weather-forecast">`;
+  weatherForecast += `<div class="weekday"></div>`;
+  weatherForecast += `<img src='${icon}'>`;
+  weatherForecast += `<p>${dailyTemp} \xB0/ ${}`
   //Weather description for the next five days
   //Humidity and wind
-
- /*let launchHTML = '';
-  launchHTML += `<section class="launch">`;
-  launchHTML += ` <img src='${launchOutcomeImageUrl}'>`;
-  launchHTML += ` <p>${launch.flight_number}: ${launch.mission_name} - ${launchDateString} ${launchTimeString}</p>`;
-  launchHTML += `</section>`;
-  return launchHTML;*/ //This is code from Van to use in forecast HTML
 };
 
 const fetchWeatherToday = () => {
@@ -64,9 +100,10 @@ const fetchWeatherToday = () => {
       return response.json();
     })
     .then((weatherToday) => {
-      generatedHTMLForWeatherToday(weatherToday);
+      containerToday.innerHTML = generatedHTMLForWeatherToday(weatherToday);
     });
 };
+fetchWeatherToday();
 
 const fetchWeatherForecast = () => {
   fetch(apiUrlForecast)
@@ -84,3 +121,4 @@ const fetchWeatherForecast = () => {
       });
     });
 };
+fetchWeatherForecast();
