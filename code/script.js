f424df6a09bf2cc1fe96fab376377be2bdb5e281
Liza Minicangit config --global user.email liza.minican@gmail.com <liza.minican@gmail.com>
2020-09-24 00:36:22 +0200

    added the functionality - icon and color change according to the weather conditions

--- code/script.js
+++ code/script.js
@@ -9,8 +9,8 @@ const url2 =
 //const sunriseTimeToday = document.getElementById("sunrise");
 //const sunsetTimeToday = document.getElementById("sunset");
 //const city = document.getElementById("cityName");
-const icon = document.getElementById("icon");
-const weatherMessage = document.getElementById("message");
+//const icon = document.getElementById("icon");
+//const weatherMessage = document.getElementById("message");
 
 //function that fetches the weather information at the moment
 const fetchWeather = () => {
@@ -27,13 +27,14 @@ const fetchWeather = () => {
         weatherDoc.main.temp
       );
 
-      //console.log(weatherDoc);
+      console.log(weatherDoc);
       //sunrise time in the city
       const sunrise = () => {
         const newSunrise = new Date(weatherDoc.sys.sunrise * 1000);
         const sunriseTime = newSunrise.toLocaleTimeString([], {
           hour: "2-digit",
           minute: "2-digit",
+          hour12: false,
         });
         document.getElementById("sunrise").innerHTML = sunriseTime;
       };
@@ -44,10 +45,34 @@ const fetchWeather = () => {
         const sunsetTime = newSunset.toLocaleTimeString([], {
           hour: "2-digit",
           minute: "2-digit",
+          hour12: false,
         });
         document.getElementById("sunset").innerHTML = sunsetTime;
       };
       sunset();
+      //if statement to change the main icon, the text of the message, the background and font colors according to the weather conditions
+      const weatherConditions = weatherDoc.weather[0].main;
+      if (weatherConditions === "Clouds") {
+        document.getElementById("icon").src = "./images/noun_Cloud_1188486.svg";
+        document.getElementById("weatherComment").innerHTML =
+          "Light a fire and get cozy, looks like the sky is grey today";
+        document.body.style.background = "#F4F7F8";
+        document.body.style.color = "#F47775";
+      } else if (weatherConditions === "Rain") {
+        document.getElementById("icon").src =
+          "./images/noun_Umbrella_2030530.svg";
+        document.getElementById("weatherComment").innerHTML =
+          "Don't forget your umbrella, it is rainy today";
+        document.body.style.background = "#A3DEF7";
+        document.body.style.color = "#164A68";
+      } else {
+        document.getElementById("icon").src =
+          "./images/noun_Sunglasses_2055147.svg";
+        document.getElementById("weatherComment").innerHTML =
+          "Get your sunnies on. The weather is looking great today";
+        document.body.style.background = "#F7E9B9";
+        document.body.style.color = "#2A5510";
+      }
       //city name
       document.getElementById("cityName").innerHTML = weatherDoc.name;
     });
@@ -65,26 +90,44 @@ const fetchWeatherForecast = () => {
       const filteredForecast = json.list.filter((item) =>
         item.dt_txt.includes("12:00")
       );
-      console.log(filteredForecast);
+      //console.log(filteredForecast);
       //function to populate the main container
       filteredForecast.forEach((day) => {
-        //console.log(day);
-        const dayFromData = new Date(day.dt_txt);
-        //const options = { weekday: "short", day: "numeric", month: "short" };
-        const theWeekDay = dayFromData.toLocaleDateString("en-US", {
-          weekday: "short",
-          day: "numeric",
-          month: "short",
-        });
-        document.getElementById(
-          "weatherByDay"
-        ).innerHTML += `<p>${theWeekDay}....................${Math.round(
-          day.main.temp
-        )}ºC</p>`;
+        document.getElementById("weatherByDay").innerHTML += showWeatherByDay(
+          day
+        );
       });
     });
 };
-fetchWeatherForecast();
 
+const showWeatherByDay = (day) => {
+  const dayFromData = new Date(day.dt_txt);
+  const theWeekDay = dayFromData.toLocaleDateString("en-US", {
+    weekday: "short",
+    day: "numeric",
+    month: "short",
+  });
+
+  // function for changing icons in each day
+
+  // const conditions = day.weather[0].main;
+  // const whichIconToShow = () => {
+  // if (conditions === "Clear") {
+  //   ("./images/sun.png");
+  //   } else if (conditions === "Rain"){
+  //     ("./images/umbrella.png");
+  //   } else {
+  // ("./images/clouds.png");
+  // };
+
+  // console.log(whichIconToShow());
+
+  //<img src="${whichIconToShow}"
+
+  return `<p>${theWeekDay}....................${Math.round(
+    day.main.temp
+  )}ºC</p>`;
+};
+fetchWeatherForecast();
 //${day.main.humidity}%
 //${Math.round(day.main.feels_like)}
