557b995bb3ab6c27bf2bdb04c11439288e5caf27
Sara Heijkenskjöld <sara.heijkenskjold@gmail.com>
2020-09-23 21:10:40 +0200

    Redid functions to fetch data from API

--- code/script.js
+++ code/script.js
@@ -1,22 +1,18 @@
 import { API_KEY } from './api.js';
-const forecastContainer = document.getElementsByClassName('forecast-container');
+const forecastContainer = document.getElementsByClassName('forecasts');
 const button = document.getElementById('search-button');
 const input = document.getElementById('input');
 console.log(typeof input.value);
-//console.log(city);
-const API_URL_TODAY = `https://api.openweathermap.org/data/2.5/weather?q=stockholm&units=metric&APPID=${API_KEY}`;
-const API_URL_FORECAST = `https://api.openweathermap.org/data/2.5/forecast?q=stockholm&units=metric&APPID=${API_KEY}`;
-const mainContainer = document.getElementById('main-container');
+let cityName = 'Stockholm'; // ändra till geolocation sen
+let API_URL_TODAY = `https://api.openweathermap.org/data/2.5/weather?q=stockholm&units=metric&APPID=${API_KEY}`;
+let API_URL_FORECAST = `https://api.openweathermap.org/data/2.5/forecast?q=${cityName}&units=metric&APPID=${API_KEY}`;
+//const mainContainer = document.getElementById('main-container');
 
 button.addEventListener('click', () => {
   const inputValue = input.value;
-  let cityName = '';
-  if (inputValue === '') {
-    cityName = 'Stockholm'; // ändra till geolocation sen!
-  } else {
-    cityName = inputValue;
-  }
-  const API_URL_FORECAST = `https://api.openweathermap.org/data/2.5/forecast?q=${cityName}&units=metric&APPID=${API_KEY}`;
+
+  cityName = inputValue;
+  API_URL_FORECAST = `https://api.openweathermap.org/data/2.5/forecast?q=${cityName}&units=metric&APPID=${API_KEY}`;
   console.log(API_URL_FORECAST);
 
   console.log(inputValue);
@@ -24,6 +20,7 @@ button.addEventListener('click', () => {
   console.log(cityName);
   //fetchWeatherToday();
   fetchWeatherForecast(API_URL_FORECAST);
+  console.log(cityName);
   input.value = '';
 });
 
@@ -31,41 +28,64 @@ const fetchWeatherForecast = (url) => {
   fetch(url)
     .then((response) => response.json())
     .then((forecastArray) => {
-      console.log(forecastArray);
-
+      // filter the array to only contain the forecast for 12:00
       const filteredArray = forecastArray.list.filter((item) =>
         item.dt_txt.includes('12:00')
       );
+
       // map to create new and filtered array of weather forecast
-      const weather = filteredArray.map((forecast) => {
+      const forecasts = filteredArray.map((forecast) => {
         const day = setDayDate(forecast.dt).forecastDayString;
-        console.log('day: ' + day);
         const date = setDayDate(forecast.dt).forecastDateString;
-        const iconSrc = `http://openweathermap.org/img/wn/${forecast.weather[0].icon}@2x.png>`;
+        const iconSrc = `http://openweathermap.org/img/wn/${forecast.weather[0].icon}@2x.png`;
         const temperature = temperatureRounded(forecast.main.temp);
         const feelsLike = temperatureRounded(forecast.main.feels_like);
         return { day, date, iconSrc, temperature, feelsLike };
       });
 
-      console.log(forecastArray);
-      console.log(filteredArray);
-      console.log(weather);
-
       // forEach to add to HTML
-      weather.forEach((item, index) => {
-        console.log(item.day);
-        console.log(item.date);
-        console.log(forecastContainer);
-        //forecastContainer[index].querySelector('.day').innerText = item.day;
-
-        //forecastContainer[index].querySelector('.date').innerText = item.date;
+      forecasts.forEach((item, index) => {
+        forecastContainer[index].querySelector('.day').innerText = item.day;
+        forecastContainer[index].querySelector('.date').innerText = item.date;
+        console.log(forecastContainer[index].querySelector('.icon'));
         forecastContainer[index].querySelector('.icon').src = item.iconSrc;
         forecastContainer[index].querySelector(
           '.temperature'
-        ).innerText = `${item.temperature}&#176 / ${item.feelsLike}&#176`;
+        ).innerText = `${item.temperature}\u00B0 / ${item.feelsLike}\u00B0`;
       });
     });
 };
+
+const fetchWeatherToday = (url) => {
+  fetch(url)
+    .then((response) => response.json())
+    .then((weatherArray) => {
+      console.log(weatherArray);
+
+      // const forecasts = filteredArray.map((forecast) => {
+      //   const day = setDayDate(forecast.dt).forecastDayString;
+      //   const date = setDayDate(forecast.dt).forecastDateString;
+      //   const iconSrc = `http://openweathermap.org/img/wn/${forecast.weather[0].icon}@2x.png`;
+      //   const temperature = temperatureRounded(forecast.main.temp);
+      //   const feelsLike = temperatureRounded(forecast.main.feels_like);
+      //   return { day, date, iconSrc, temperature, feelsLike };
+      // });
+
+      //map to create new and filtered array of today's weather
+      //const weathers = weatherArray.list.map((weather) => {
+      const temperature = temperatureInteger(weatherArray.main.temp);
+      console.log(temperature);
+      const city = weatherArray.name;
+      const description = weatherArray.weather[0].main;
+      const sunrise = setSunTime(weatherArray.sys.sunrise);
+      const sunset = setSunTime(weatherArray.sys.sunset);
+      return { city, description, sunrise, sunset };
+      //  });
+      //  console.log(newWeatherArray);
+    });
+};
+
+fetchWeatherToday(API_URL_TODAY);
 
 // fetch(API_URL_TODAY)
 //   .then((response) => {
@@ -191,3 +211,5 @@ const filterForecast = (forecastArray) => {
   console.log(filteredForecast);
   return filteredForecast;
 };
+
+fetchWeatherForecast(API_URL_FORECAST);
