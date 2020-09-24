0ab91035af0ea283b622bd0c2036923e2b009d0b
Corneliaryas <hello@corneliaryas.com>
2020-09-24 09:46:24 +0200

    Min and Max temp with one call API

--- code/script.js
+++ code/script.js
@@ -7,6 +7,11 @@ const BodenAPI = 'https://api.openweathermap.org/data/2.5/weather?q=Boden,Sweden
 const BodenForecastAPI = 'https://api.openweathermap.org/data/2.5/forecast?q=Boden,Sweden&units=metric&APPID=2163e0bcc8eaa7f0951284d8a650a723';
 const KlintanAPI = 'https://api.openweathermap.org/data/2.5/weather?q=Robertsfors,Sweden&units=metric&appid=2163e0bcc8eaa7f0951284d8a650a723&lang=sv';
 const KlintanForecastAPI = 'https://api.openweathermap.org/data/2.5/forecast?q=Robertsfors,Sweden&units=metric&APPID=2163e0bcc8eaa7f0951284d8a650a723';
+const stockholmDailyAPI = 'https://api.openweathermap.org/data/2.5/onecall?lat=59.3293&lon=18.0686&exclude=current,minutely,hourly,alerts&units=metric&appid=2163e0bcc8eaa7f0951284d8a650a723'
+const umeaDailyAPI = 'https://api.openweathermap.org/data/2.5/onecall?lat=63.8258&lon=20.2630&exclude=current,minutely,hourly,alerts&units=metric&appid=2163e0bcc8eaa7f0951284d8a650a723'
+const bodenDailyAPI = 'https://api.openweathermap.org/data/2.5/onecall?lat=65.8251&lon=21.6887&exclude=current,minutely,hourly,alerts&units=metric&appid=2163e0bcc8eaa7f0951284d8a650a723'
+const klintanDailyAPI = 'https://api.openweathermap.org/data/2.5/onecall?lat=64.1918&lon=20.8489&exclude=current,minutely,hourly,alerts&units=metric&appid=2163e0bcc8eaa7f0951284d8a650a723'
+
 
 // Getting HTML elements
 
@@ -22,6 +27,7 @@ let lat =''
 let lon =''
 let geoLocationAPI = ''
 let geoLocationForcastAPI = ''
+let geoLocationDailyAPI = ''
 
 const getLocationAPI = (callback) => {
 
@@ -38,6 +44,7 @@ const getLocationAPI = (callback) => {
          lon = position.coords.longitude
          geoLocationAPI = `https://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${lon}&units=metric&appid=2163e0bcc8eaa7f0951284d8a650a723&lang=sv`
          geoLocationForcastAPI = `https://api.openweathermap.org/data/2.5/forecast?lat=${lat}&lon=${lon}&units=metric&appid=2163e0bcc8eaa7f0951284d8a650a723`
+         geoLocationDailyAPI = `https://api.openweathermap.org/data/2.5/onecall?lat=${lat}&lon=${lon}&exclude=current,minutely,hourly,alerts&units=metric&appid=2163e0bcc8eaa7f0951284d8a650a723`
          callback();
      }
      
@@ -47,7 +54,8 @@ const getLocationAPI = (callback) => {
 
 const setLocationAPI = () => {
     showCurrentWeather(geoLocationAPI);
-    showWeatherForecast(geoLocationForcastAPI);
+    showWeatherForecast(geoLocationDailyAPI);
+    // showWeatherForecast(geoLocationForcastAPI);
 
 }
 
@@ -115,66 +123,124 @@ const showCurrentWeather = (API) => {
                 }
             }
             setWeatherColors();
-            currentWeatherContainer.innerHTML = `<h1>${weatherTemp}<sup>&degC</sup></h1><h2>${weather.name}</h2><p>${weatherDescription}</p><div class="sun"><p>Sunrise: ${sunriseString} </p> <p>Sunset: ${sunsetString} </p></div>`
+            currentWeatherContainer.innerHTML = `<p>Today</p><h1>${weatherTemp}<sup>&degC</sup></h1><h2>${weather.name}</h2><p>${weatherDescription}</p><div class="sun"><p>Sunrise: ${sunriseString} </p> <p>Sunset: ${sunsetString} </p></div>`
         })
 
 
 }
 
 //Fetches the API and shows the data of the 5 day forecast
-
 const showWeatherForecast = (forecastAPI) => {
-    fetch(forecastAPI)
-    .then ((response) => {
-        return response.json();
-    })
-    .then ((forecast) => {
-        const filteredForecast = forecast.list.filter(item => item.dt_txt.includes('12:00'));
-        console.log(filteredForecast)
-        weatherForecastContainer.innerHTML = '';
-        filteredForecast.forEach((forecastDay => {
-            const forecastTemp = forecastDay.main.temp.toFixed(0)
-            const forecastWeekday = new Date(forecastDay.dt * 1000).toLocaleDateString('se-SE', {weekday: 'short'})
-            const weatherIcon = forecastDay.weather[0].icon
-            let forecastImage = '';
-            let forecastHTML = '';
-
-            const setForecastImage = () => {
-                if (weatherIcon === "01d") {
-                    forecastImage = "http://openweathermap.org/img/wn/01d@2x.png"
-                } else if (weatherIcon === "02d") {
-                    forecastImage = "http://openweathermap.org/img/wn/02d@2x.png"
-                } else if (weatherIcon === "03d") {
-                    forecastImage = "http://openweathermap.org/img/wn/03d@2x.png"
-                } else if (weatherIcon === "04d") {
-                    forecastImage = "http://openweathermap.org/img/wn/04d@2x.png"
-                } else if (weatherIcon === "09d") {
-                    forecastImage = "http://openweathermap.org/img/wn/09d@2x.png"
-                } else if (weatherIcon === "10d") {
-                    forecastImage = "http://openweathermap.org/img/wn/10d@2x.png"
-                } else if (weatherIcon === "11d") {
-                    forecastImage = "http://openweathermap.org/img/wn/11d@2x.png"
-                } else if (weatherIcon === "13d") {
-                    forecastImage = "http://openweathermap.org/img/wn/13d@2x.png"
-                } else if (weatherIcon === "50d") {
-                    forecastImage = "http://openweathermap.org/img/wn/50d@2x.png"
+        fetch(forecastAPI)
+        .then ((response) => {
+            return response.json();
+        })
+        .then ((forecast) => {
+            // const filteredForecast = forecast.daily.filter(item => item.dt_txt.includes('12:00'));
+            // console.log(filteredForecast)
+            weatherForecastContainer.innerHTML = '';
+            const forecast5Days = forecast.daily.slice(1,6);
+            const todayMin = forecast.daily[0].temp.min.toFixed(0)
+            const todayMax = forecast.daily[0].temp.max.toFixed(0)
+            forecast5Days.forEach((forecastDay => {
+                const forecastTemp = forecastDay.temp.day.toFixed(0)
+                const forecastWeekday = new Date(forecastDay.dt * 1000).toLocaleDateString('se-SE', {weekday: 'short'})
+                const weatherIcon = forecastDay.weather[0].icon
+                const tempMin = forecastDay.temp.min.toFixed(0)
+                const tempMax = forecastDay.temp.max.toFixed(0)
+                let forecastImage = '';
+                let forecastHTML = '';
+    
+                const setForecastImage = () => {
+                    if (weatherIcon === "01d") {
+                        forecastImage = "http://openweathermap.org/img/wn/01d@2x.png"
+                    } else if (weatherIcon === "02d") {
+                        forecastImage = "http://openweathermap.org/img/wn/02d@2x.png"
+                    } else if (weatherIcon === "03d") {
+                        forecastImage = "http://openweathermap.org/img/wn/03d@2x.png"
+                    } else if (weatherIcon === "04d") {
+                        forecastImage = "http://openweathermap.org/img/wn/04d@2x.png"
+                    } else if (weatherIcon === "09d") {
+                        forecastImage = "http://openweathermap.org/img/wn/09d@2x.png"
+                    } else if (weatherIcon === "10d") {
+                        forecastImage = "http://openweathermap.org/img/wn/10d@2x.png"
+                    } else if (weatherIcon === "11d") {
+                        forecastImage = "http://openweathermap.org/img/wn/11d@2x.png"
+                    } else if (weatherIcon === "13d") {
+                        forecastImage = "http://openweathermap.org/img/wn/13d@2x.png"
+                    } else if (weatherIcon === "50d") {
+                        forecastImage = "http://openweathermap.org/img/wn/50d@2x.png"
+                    }
                 }
-        }
-        setForecastImage();
+                setForecastImage();
+    
+                forecastHTML += `<div class="forecast-day">`;
+                forecastHTML += ` <p>${forecastWeekday}</p>`;
+                forecastHTML += ` <div class="temp">`;
+                forecastHTML += `  <img src="${forecastImage}"/><p> ${tempMin}&degC / ${tempMax}&degC </p>`;
+                forecastHTML += ` </div>`;
+                forecastHTML += `</div>`;
+            
+                weatherForecastContainer.innerHTML += `${forecastHTML}`;    
+            }))
 
-        forecastHTML += `<div class="forecast-day">`;
-        forecastHTML += ` <p>${forecastWeekday}</p>`;
-        forecastHTML += ` <div class="temp">`;
-        forecastHTML += `  <img src="${forecastImage}"/><p> ${forecastTemp}&degC </p>`;
-        forecastHTML += ` </div>`;
-        forecastHTML += `</div>`;
+        currentWeatherContainer.innerHTML += `<p>Min ${todayMin}&degC / Max ${todayMax}&degC</p>`
+    
+        })
+    }
+    
+// const showWeatherForecast = (forecastAPI) => {
+//     fetch(forecastAPI)
+//     .then ((response) => {
+//         return response.json();
+//     })
+//     .then ((forecast) => {
+//         const filteredForecast = forecast.list.filter(item => item.dt_txt.includes('12:00'));
+//         console.log(filteredForecast)
+//         weatherForecastContainer.innerHTML = '';
+//         filteredForecast.forEach((forecastDay => {
+//             const forecastTemp = forecastDay.main.temp.toFixed(0)
+//             const forecastWeekday = new Date(forecastDay.dt * 1000).toLocaleDateString('se-SE', {weekday: 'short'})
+//             const weatherIcon = forecastDay.weather[0].icon
+//             let forecastImage = '';
+//             let forecastHTML = '';
+
+//             const setForecastImage = () => {
+//                 if (weatherIcon === "01d") {
+//                     forecastImage = "http://openweathermap.org/img/wn/01d@2x.png"
+//                 } else if (weatherIcon === "02d") {
+//                     forecastImage = "http://openweathermap.org/img/wn/02d@2x.png"
+//                 } else if (weatherIcon === "03d") {
+//                     forecastImage = "http://openweathermap.org/img/wn/03d@2x.png"
+//                 } else if (weatherIcon === "04d") {
+//                     forecastImage = "http://openweathermap.org/img/wn/04d@2x.png"
+//                 } else if (weatherIcon === "09d") {
+//                     forecastImage = "http://openweathermap.org/img/wn/09d@2x.png"
+//                 } else if (weatherIcon === "10d") {
+//                     forecastImage = "http://openweathermap.org/img/wn/10d@2x.png"
+//                 } else if (weatherIcon === "11d") {
+//                     forecastImage = "http://openweathermap.org/img/wn/11d@2x.png"
+//                 } else if (weatherIcon === "13d") {
+//                     forecastImage = "http://openweathermap.org/img/wn/13d@2x.png"
+//                 } else if (weatherIcon === "50d") {
+//                     forecastImage = "http://openweathermap.org/img/wn/50d@2x.png"
+//                 }
+//         }
+//         setForecastImage();
+
+//         forecastHTML += `<div class="forecast-day">`;
+//         forecastHTML += ` <p>${forecastWeekday}</p>`;
+//         forecastHTML += ` <div class="temp">`;
+//         forecastHTML += `  <img src="${forecastImage}"/><p> ${forecastTemp}&degC </p>`;
+//         forecastHTML += ` </div>`;
+//         forecastHTML += `</div>`;
         
-        weatherForecastContainer.innerHTML += `${forecastHTML}`;
+//         weatherForecastContainer.innerHTML += `${forecastHTML}`;
 
-        }))
+//         }))
 
-    })
-}
+//     })
+// }
 
 //On page load
 
