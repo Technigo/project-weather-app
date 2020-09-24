e3208d16a24caf82ea3d137df3e0e05a29effaf5
Evelina Berg <evelina.berg@outlook.com>
2020-09-24 19:07:30 +0200

    Forecast info fetched. Displays on site using map-loop and for-of-loop

--- script.js
+++ script.js
@@ -1,14 +1,14 @@
 import {API_KEY} from './api-key.js';
 
 //--------------------------------General variables-------------------------------
-const currentWeatherUrl = `http://api.openweathermap.org/data/2.5/weather?q=Göteborg,Sweden&units=metric&APPID=${API_KEY}`
-const forecastWeatherUrl = `http://api.openweathermap.org/data/2.5/forecast?q=Gothenburg,Sweden&units=metric&appid=${API_KEY}`;
+const API_URL_CURRENT_WEATHER = `http://api.openweathermap.org/data/2.5/weather?q=Göteborg,Sweden&units=metric&APPID=${API_KEY}`
+const API_URL_FORECAST_WEATHER = `http://api.openweathermap.org/data/2.5/forecast?q=Gothenburg,Sweden&units=metric&appid=${API_KEY}`;
 const currentWeatherInfo = document.querySelector('.weather-info-textbox');
 const cityHeader = document.querySelector('.city');
-const forecastWeatherInfo = document.querySelector('.forecast-weather-info-textbox');
+const forecastInfo = document.getElementsByClassName('forecast-weather-info-textbox');
 
 //---------------------------------Fetch API current weather---------------------
-fetch(currentWeatherUrl)
+fetch(API_URL_CURRENT_WEATHER)
     .then((response) => {
         return response.json();
     })
@@ -37,7 +37,7 @@ const generateHTMLForCurrentWeatherInfo = currentWeather => {
         hour: '2-digit', 
         minute:'2-digit',
     });
-
+//Data consuming approach
     let currentWeatherInfoHTML = '';
     currentWeatherInfoHTML += `<p class="weather-info-text">${weatherDescription}</p>`;
     currentWeatherInfoHTML += `<p class="weather-info-text">${currentTemperature}°</p>`;
@@ -47,18 +47,35 @@ const generateHTMLForCurrentWeatherInfo = currentWeather => {
     return currentWeatherInfoHTML;
 };
 
-//--------------------
+//--------------------Fetch API forecast weather---------------------
 
-fetch(forecastWeatherUrl)
+fetch(API_URL_FORECAST_WEATHER)
     .then((response) => {
         return response.json();
     })
-    .then((forecastWeather) => {
+    .then(forecastWeather => {
         console.log(forecastWeather);
-        forecastWWeatherInfo.innerHTML += generateHTMLForForecastWeatherInfo(forecastWeather);
+//      // filteredForecast is now an array with only the data from 12:00 each day.
+        const filteredForecast = forecastWeather.list.filter(item => 
+            item.dt_txt.includes('12:00'));
+        const newForecast = filteredForecast.map(forecast => {
+            const dateDay = new Date(forecast.dt * 1000);
+            const dateDayString = dateDay.toLocaleDateString('en-GB', {
+                weekday: 'short',
+            });
+            const temperature = forecast.main.temp.toFixed(1);
+            const tempFeelsLike = forecast.main.feels_like.toFixed(1);
+
+            return { dateDayString, temperature, tempFeelsLike };
+        });
+
+        for (const [index, everyItem] of newForecast.entries()) {
+            forecastInfo[index].querySelector('.forecast-info-day').innerText = everyItem.dateDayString;
+            forecastInfo[index].querySelector('.forecast-info-temp').innerText = everyItem.temperature;
+            forecastInfo[index].querySelector('.forecast-info-feels-like').innerText = everyItem.tempFeelsLike;
+        };
+
+        console.log(newForecast);
+
     });
-
-    //-------------Function to generate HTML for current weather info on top of page-----
-//  const generateHTMLForForecastWeatherInfo = forecastWeather => {
-
-//  }
+   
