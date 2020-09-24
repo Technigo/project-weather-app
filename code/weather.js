c4ed2e8b7c3e0e8f2ce3e29c90919db0d096a16b
caroline birgersson <caroline@birgerochblom.se>
2020-09-24 16:42:31 +0200

    Iteration 4

--- code/weather.js
+++ code/weather.js
@@ -45,5 +45,27 @@ const fetchWeather = () => {
 
 fetchWeather();
 
+// Forecast for the next 5 days
+const apiForecast = "https://api.openweathermap.org/data/2.5/forecast?q=Stockholm,Sweden&units=metric&APPID=e33f1cc192401277e601a6aed3a82800";
 
+fetch(apiForecast)
+    .then((Response) => { 
+        return Response.json()
+    })
+    .then ((json) => {
+        const filteredForecast = json.list.filter(item =>
+        item.dt_txt.includes("12:00")
+        );
+        
+        filteredForecast.forEach(day => {
+            const date = new Date(day.dt * 1000);
 
+            const dayName = date.toLocaleDateString("en-EN", { weekday: "long" });
+
+            const dayTemp = day.main.temp;
+            const weekTemp = dayTemp.toFixed(0.1);
+
+            document.getElementById('weekday').innerHTML += `<p>${dayName}</p>`
+            document.getElementById('weekTemp').innerHTML += `<p>${weekTemp}°C</p>`
+    });
+});
