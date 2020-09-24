f244d62b28ec0c1f47b0b9ee256f2b1434af6f82
ClaireCaudwell <clairecaudwell@gmail.com>
2020-09-24 09:33:03 +0200

    JS file: 1. Retrieved the weather icon from the api which is in the same object as the description. Did this so I could access the code for this to then replace the image depending upon the weather description. This will then be linked to my images which I have given the same name as the open weather app icons, so they will be replaced when the weather changes. 2. Added to the 5 day forecast the description and icon from the weather api object and added this for each day of the 5 day forecast. Wanted to do this so I could have the icon and description included in the 5 day forecast.

--- code/weatherapp.js
+++ code/weatherapp.js
@@ -1,6 +1,5 @@
-//Putting the api link into a so we can access it through that
+//External location for the api links
 import { apiLinkWeatherInfo } from './api-keysecure.js';
-
 import { apiLinkFiveDayForecast } from './api-keysecure.js';
 
 
@@ -12,17 +11,20 @@ const fetchedApiInfo = () => {
     })
     .then((weatherInfo) => {
         //Accesing the data from the api via json and putting it into specified html elements
-        document.getElementById("city").innerText = weatherInfo.name;
+        //Stockholm weather section
+        document.getElementById("city").innerText = weatherInfo.name; //City
 
-        const temperature = weatherInfo.main.temp.toFixed(1);
+        const temperature = weatherInfo.main.temp.toFixed(1);//City temp
         //toFixed()rounds the number up or down
-        // const degreeSymbol = `<p>&#8451;</p>`;
-        document.getElementById("temperature").innerHTML = `${temperature} &#176c`; 
+        document.getElementById("temperature").innerHTML = `${temperature} &#176`; 
         document.getElementById("weatherType").innerText = weatherInfo.weather[0].description;
 
+        const iconId = weatherInfo.weather[0].icon;
+        document.getElementById("iconImage").src = `./images/${iconId}.png`;
+
+        //Sunrise & sunset section
         //new Date is creating a copy of a new object for us to fill
         const sunrise = new Date(weatherInfo.sys.sunrise * 1000);
-        //Time specified is in seconds, not milliseconds? So we have to convert it to milliseconds because seconds would take longer to convert?
         const sunriseTimeHours = sunrise.getHours();
         const sunriseTimeMinutes = sunrise.getMinutes();
         //getHours and getMinutes will convert the UTC to local time according to your computer
@@ -50,32 +52,64 @@ const fetchedApiInfo = () => {
         //Created an array with weekday names. Then used the dayOneDate together with the getDay() method to select from the array which day it is. The getDay method tells you this is number format e.g. 1 = Sunday. Which I think then selects that from the array of days.
         const dayOneTemp = filteredList[0].main.temp.toFixed(1); 
         //Taking the first array index from the filteredList and accessing the temperature for that day whilst rounding the temperature to one decimal place. 
+        const dayOneDescription = filteredList[0].weather[0].description;
+        const dayOneDescpIcon = filteredList[0].weather[0].icon;
+
+        document.getElementById("day1Day").innerHTML = `${finalDayOneDate}`;
+        document.getElementById("day1Type").innerHTML = `${dayOneDescription}`;
+        document.getElementById("day1Icon").src = `./images/${dayOneDescpIcon}.png`;
+        document.getElementById("day1Temp").innerHTML = `${dayOneTemp} &#176`;
+
 
         /* ------ DAY 2 FORECAST ---- */
         const dayTwoDate = new Date(filteredList[1].dt * 1000);
         const finalDayTwoDate = days[dayTwoDate.getDay()];
+        const dayTwoDescription = filteredList[1].weather[0].description;
+        const dayTwoDescpIcon = filteredList[1].weather[0].icon;
         const dayTwoTemp = filteredList[1].main.temp.toFixed(1);
+
+        document.getElementById("day2Day").innerHTML = `${finalDayTwoDate}`;
+        document.getElementById("day2Type").innerHTML = `${dayTwoDescription}`;
+        document.getElementById("day2Icon").src = `./images/${dayTwoDescpIcon}.png`;
+        document.getElementById("day2Temp").innerHTML = `${dayTwoTemp} &#176`;
 
         /* ------ DAY 3 FORECAST ---- */
         const dayThreeDate = new Date(filteredList[2].dt * 1000);
         const finalDayThreeDate = days[dayThreeDate.getDay()];
+        const dayThreeDescription = filteredList[2].weather[0].description;
+        const dayThreeDescpIcon = filteredList[2].weather[0].icon;
         const dayThreeTemp = filteredList[2].main.temp.toFixed(1);
+
+        document.getElementById("day3Day").innerHTML = `${finalDayThreeDate}`;
+        document.getElementById("day3Type").innerHTML = `${dayThreeDescription}`;
+        document.getElementById("day3Icon").src = `./images/${dayThreeDescpIcon}.png`;
+        document.getElementById("day3Temp").innerHTML = `${dayThreeTemp} &#176`;
+
 
         /* ------ DAY 4 FORECAST ---- */
         const dayFourDate = new Date(filteredList[3].dt * 1000);
         const finalDayFourDate = days[dayFourDate.getDay()];
+        const dayFourDescription = filteredList[3].weather[0].description;
+        const dayFourDescpIcon = filteredList[3].weather[0].icon;
         const dayFourTemp = filteredList[3].main.temp.toFixed(1);
+
+        document.getElementById("day4Day").innerHTML = `${finalDayFourDate}`;
+        document.getElementById("day4Type").innerHTML = `${dayFourDescription}`;
+        document.getElementById("day4Icon").src = `./images/${dayFourDescpIcon}.png`;
+        document.getElementById("day4Temp").innerHTML = `${dayFourTemp} &#176`;
+
 
         /* ------ DAY 5 FORECAST ---- */
         const dayFiveDate = new Date(filteredList[4].dt * 1000);
         const finalDayFiveDate = days[dayFiveDate.getDay()];
-        const dayFiveTemp = filteredList[4].main.temp.toFixed(1);
-
-        document.getElementById("day1").innerHTML = `${finalDayOneDate} ${dayOneTemp} &#176c`;
-        document.getElementById("day2").innerHTML = `${finalDayTwoDate} ${dayTwoTemp} &#176c`;
-        document.getElementById("day3").innerHTML = `${finalDayThreeDate} ${dayThreeTemp} &#176c`;
-        document.getElementById("day4").innerHTML = `${finalDayFourDate} ${dayFourTemp} &#176c`;
-        document.getElementById("day5").innerHTML = `${finalDayFiveDate} ${dayFiveTemp} &#176c`;
+        const dayFiveDescription = filteredList[4].weather[0].description;
+        const dayFiveDescpIcon = filteredList[4].weather[0].icon;
+        const dayFiveTemp = filteredList[4].main.temp.toFixed(1);  
+    
+        document.getElementById("day5Day").innerHTML = `${finalDayFiveDate}`;
+        document.getElementById("day5Type").innerHTML = `${dayFiveDescription}`;
+        document.getElementById("day5Icon").src = `./images/${dayFiveDescpIcon}.png`;
+        document.getElementById("day5Temp").innerHTML = `${dayFiveTemp} &#176`;
     });
 };
 
