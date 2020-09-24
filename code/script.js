c32fa6b83086742618dd175518941e36af71350a
MindstormingAB <s.elander@mindstorming.se>
2020-09-24 22:29:26 +0200

    Weather icons

--- code/script.js
+++ code/script.js
@@ -14,10 +14,10 @@ const forecasts = document.getElementsByClassName('forecast');
 fetch(WEATHER_API_URL)
   .then(response => response.json())
   .then(weatherArray => {
-    // console.log(weatherArray);
     city.innerHTML = weatherArray.name;
-    temperature.innerHTML = `${weatherArray.main.temp.toFixed(1)}\xB0`;
-    description.innerHTML = weatherArray.weather.map((a) => a.description);
+    temperature.innerHTML = `${weatherArray.main.temp.toFixed(0)}\xB0`;
+    description.innerHTML = weatherArray.weather.map((a) => a.main);
+    picto.src = 'http://openweathermap.org/img/wn/' + weatherArray.weather.map((a) => a.icon) + '@2x.png';
     sunrise.innerHTML = (new Date(weatherArray.sys.sunrise * 1000)).toLocaleTimeString([], {
       hour: '2-digit',
       minute: '2-digit',
@@ -33,21 +33,18 @@ fetch(WEATHER_API_URL)
 fetch(FORECAST_API_URL)
   .then(response => response.json())
   .then((forecastArray) => {
-    console.log(forecastArray);
     const filteredForecast = forecastArray.list.filter(item => item.dt_txt.includes('12:00'));
-    console.log(filteredForecast);
     const forecastWeather = filteredForecast.map(day => {
       const dayName = (new Date(day.dt * 1000)).toLocaleDateString([], {
         weekday: 'short',
       });
-      const main = day.weather.map((a) => a.main);
-      const midTemp = `${day.main.temp.toFixed(1)}\xB0`;
-      return { dayName, main, midTemp };
+      const icon = day.weather.map((a) => a.icon);
+      const midTemp = `${day.main.temp.toFixed(0)}\xB0`;
+      return { dayName, icon, midTemp };
     });
-    console.log(forecastWeather);
     forecastWeather.forEach((item, index) => {
       forecasts[index].querySelector('.day-name').innerHTML = item.dayName;
-      forecasts[index].querySelector('.day-main').innerHTML = item.main;
+      forecasts[index].querySelector('.day-icon').src = 'http://openweathermap.org/img/wn/' + item.icon + '.png';
       forecasts[index].querySelector('.day-temp').innerHTML = item.midTemp;
     })
   });
