0fab25f8610ac8b92e63582139bec7c2a35fc4a7
Trang Tara <trangnusi@gmail.com>
2020-09-24 12:00:15 +0200

    default page completed, city selection completed

--- code/script.js
+++ code/script.js
@@ -1,37 +1,61 @@
-const API_KEY = 'e27fc7790a6a4c3537de471b9d7612ce' 
-const apiUrl = "http://api.openweathermap.org/data/2.5/weather?q=Ho%20chi%20minh,Vietnam&units=metric&APPID=e27fc7790a6a4c3537de471b9d7612ce"
+const API_KEY = 'e27fc7790a6a4c3537de471b9d7612ce'
 
-const forecastUrl = "http://api.openweathermap.org/data/2.5/forecast?q=Ho%20Chi%20Minh,Vietnam&units=metric&appid=e27fc7790a6a4c3537de471b9d7612ce"
+// Default page base Ho Chi Minh City
+updateWeatherData('Ho chi minh');
 
+document.getElementById('city-select').addEventListener('change', (event) => {
+  const cityName = event.target.value;
+  console.log(cityName);
+  updateWeatherData(cityName);
+});
 
-fetch (apiUrl)
-  .then((response) => {
-    return response.json();
-  })
-  .then((todayForecast) => {
-    // populateHeader(todayForecast)
-    populateDetails(todayForecast);
-    populateSummary(todayForecast);
-  });
+function updateWeatherData(cityName) {
+  const todayWeatherUrl = `http://api.openweathermap.org/data/2.5/weather?q=${cityName}&units=metric&APPID=${API_KEY}`
+  const fiveDaysForecastUrl = `http://api.openweathermap.org/data/2.5/forecast?q=${cityName}&units=metric&appid=${API_KEY}`
+  
+  fetch(todayWeatherUrl)
+    .then((response) => {
+      return response.json();
+    })
+    .then((todayForecast) => {
+      populateDetails(todayForecast, cityName);
+      populateSummary(todayForecast);
+    });
+  
+  fetch(fiveDaysForecastUrl)
+    .then((response) => {
+      return response.json();
+    })
+    .then((forecast) => {
+      const filteredForecast = forecast.list.filter(item => item.dt_txt.includes('12:00'));
 
-  // function populateHeader(todayForecast) {
-  //   const city = todayForecast.name
-  //   const today = new Date(todayForecast.dt*1000);
-  //   // document.getElementById('headerMessage').innerHTML = `Welcome to the Weather Forecast of ${city} on ${today}`
-  // };
+      let output = '';
 
+      filteredForecast.forEach((item) => {
+        const temperature = item.main.temp;
+        const tempShort = parseFloat(temperature).toFixed(1);
+        const date = new Date(item.dt * 1000);
+        const weekday = date.toLocaleDateString('en-US', {
+          weekday: 'short'
+        });
+        output += `<p class="day">${weekday} <span>${tempShort}°C</span></p>`;
+      });
 
-  function populateDetails(todayForecast) {
-    const todayDescription = todayForecast.weather[0].description;
-    const todayTemperature = todayForecast.main.temp;
+      document.getElementById('forecast').innerHTML = output;
+    });
+}
+  
+function populateDetails(todayForecast, cityName) {
+  const todayDescription = todayForecast.weather[0].description;
+  const todayTemperature = todayForecast.main.temp;
 
-    const sunrise = new Date(todayForecast.sys.sunrise * 1000);
-    const sunset = new Date(todayForecast.sys.sunset * 1000);
-    const options = {
-        timeZone: 'Asia/Ho_Chi_Minh',
-        timeStyle: 'short',
-        hour12: false,
-      }
+  const sunrise = new Date(todayForecast.sys.sunrise * 1000);
+  const sunset = new Date(todayForecast.sys.sunset * 1000);
+  const options = {
+    timeZone: getTimeZone(cityName),
+    timeStyle: 'short',
+    hour12: false,
+  }
   const sunriseTime = new Intl.DateTimeFormat('en-US', options).format(sunrise);
   const sunsetTime = new Intl.DateTimeFormat('en-US', options).format(sunset);
 
@@ -56,8 +80,7 @@ function populateSummary(todayForecast) {
     document.getElementById('img').src = '../assets/cloudyDay.svg';
     document.getElementById('sumMessage').innerHTML = `Light a fire and get cosy. It is cloudy in ${city} today.`;
     document.querySelector('.main-container').classList.add('cloudy-day');
-
-  } else if (sunnyDay) {
+  } else if (sunnyDay){
     document.getElementById('img').src = '../assets/sunnyDay.svg';
     document.getElementById('sumMessage').innerHTML = `Get your sunnies on. ${city} is looking rather great today.`;
     document.querySelector('.main-container').classList.add('sunny-day');
@@ -66,24 +89,26 @@ function populateSummary(todayForecast) {
   }
 }
 
-fetch (forecastUrl)
-  .then((response) => {
-    return response.json();
-  })
-  .then((forecast) => {
-    const filteredForecast = forecast.list.filter(item => item.dt_txt.includes('12:00'));
-    filteredForecast.forEach((item) => {
-      const temperature = item.main.temp;
-      const tempShort = parseFloat(temperature).toFixed(1);
-      const date = new Date(item.dt * 1000);
-      const weekday = date.toLocaleDateString('en-VN', {
-          weekday: 'short'
-        });
-      document.getElementById('forecast').innerHTML += `<p class="day">${weekday} <span>${tempShort}°C</span></p>`;
+function getTimeZone(city) {
+  if (city === 'Ho Chi Minh') {
+    return 'Asia/Saigon'
+  } else if(city === "Los Angeles") {
+    return 'America/Los_Angeles'
+  } else if (city === 'Tokyo') {
+    return 'Japan'
+  } else if (city === 'Paris') {
+    return 'Europe/Paris'
+  } else if (city === 'Santiago') {
+    return 'America/Santiago'
+  } else if (city === 'Cairo') {
+    return 'Africa/Cairo'
+  }
+}
 
-      // // document.getElementById('weekday').innerHTML += `${weekday}`;
-      // document.getElementbyId('temp-forecast').innerHTML += `${tempShort}°C`;
-    })
-  });
+
+
+
+  
+
 
 
