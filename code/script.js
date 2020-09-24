fca76fa17867323f043d0eae89167fbd55cc9e29
JOHANNA REXIN <johanna.rexin@gmail.com>
2020-09-24 20:32:07 +0200

    ALmost done with step 2, no styling yet

--- code/script.js
+++ code/script.js
@@ -1,11 +1,35 @@
-const apiUrl = 'http://api.openweathermap.org/data/2.5/weather?q=Stockholm,Sweden&units=metric&APPID=380f07f94efb48727a6ae5c8f7d15f5d';
-const container = document.getElementById("main");
+const apiKey = '380f07f94efb48727a6ae5c8f7d15f5d';
+const apiStockholm = 'http://api.openweathermap.org/data/2.5/weather?q=Stockholm,Sweden&units=metric&APPID=380f07f94efb48727a6ae5c8f7d15f5d';
+const apiFiveDaysForecast = 'https://api.openweathermap.org/data/2.5/forecast?q=Stockholm,Sweden&units=metric&APPID=380f07f94efb48727a6ae5c8f7d15f5d';
 
-fetch(apiUrl)
+//Weather Variables
+const city = document.getElementById('city');
+const weatherDescription = document.getElementById('weatherDescription');
+const sunset = document.getElementById('sunset');
+const sunrise = document.getElementById('sunrise');
+const fiveDays = document.getElementById('fiveDays');
+const temperatures = document.getElementById('temperatures');
+
+fetch(apiStockholm)
     .then((response) => {
         return response.json();
 })
-.then((weather) => {
-    console.log(weather);
-});
 
+// City name
+.then((weatherObject) => {
+    city.innerHTML = weatherObject.name;
+    console.log(weatherObject);
+    weatherDescription.innerHTML = weatherObject.weather[0].description
+    sunrise.innerHTML = weatherObject.sys.sunrise
+    sunset.innerHTML = weatherObject.sys.sunset
+    console.log(weatherObject)
+
+// fetch(apiFiveDaysForecast)
+//     .then((response) => {
+//         return response.json();
+// })
+
+// // Five days forecast
+// .then((weatherObject) => {
+//     fiveDays.innerHTML = weatherObject.list.dt
+})
