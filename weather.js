f2b88478087906a64dec93e93cc80e28f88c8382
idsintehittapa <jblom15@gmail.com>
2020-09-24 11:29:52 +0200

    Added weather icons depending on weather, added changed background depending on weather

--- weather.js
+++ weather.js
@@ -2,8 +2,6 @@ apiUrl = "https://api.openweathermap.org/data/2.5/weather?q=Longyearbyen,Norway&
 apiUrlForcast = "https://api.openweathermap.org/data/2.5/forecast?q=Longyearbyen,Norway&units=metric&APPID=8990d7a0fe5c73c6c0fe06bb994b1035"
 
 const containerToday = document.getElementById('weatherMain');
-const containerTemp = document.getElementById('weatherTemp');
-const containerImage = document.getElementById('weatherImage');
 const containerTempFeel = document.getElementById('weatherTempFeel');
 const containerDescription = document.getElementById('weatherDescription');
 const containerSunRise = document.getElementById('sunrise');
@@ -41,18 +39,67 @@ const readableDate = (date) => {
 }
 
 /// function for icon of the day
-//const iconDependingOnWeather = (item) => {
-   // const iconMainDescription = item
-    //if (iconDependingOnWeather === "Clouds")
+const iconDependingOnWeather = (weather) => {
+   // if (weather === "Clouds")
     //console.log(iconDependingOnWeather)
-    //return 'http://openweathermap.org/img/wn/03d@2x.png'
-//}
+    return 'http://openweathermap.org/img/wn/03d@2x.png'
+}
+
+let weatherIcon = {
+    "Clear": "http://openweathermap.org/img/wn/01d@2x.png",
+    "Clouds": "http://openweathermap.org/img/wn/03d@2x.png",
+    "Drizzle": "http://openweathermap.org/img/wn/09d@2x.png",
+    "Fog": "http://openweathermap.org/img/wn/50d@2x.png",
+    "Haze": "http://openweathermap.org/img/wn/50d@2x.png",
+    "Mist": "http://openweathermap.org/img/wn/50d@2x.png",
+    "Rain": "http://openweathermap.org/img/wn/10d@2x.png",
+    "Smoke": "http://openweathermap.org/img/wn/50d@2x.png",
+    "Snow": "http://openweathermap.org/img/wn/13d@2x.png",
+    "Squall": "http://openweathermap.org/img/wn/50d@2x.png",
+    "Thunderstorm": "http://openweathermap.org/img/wn/11d@2x.png",
+    "Tornado": "http://openweathermap.org/img/wn/50d@2x.png",
+}
+
+let weatherColor = {
+    "Clear": "linear-gradient(to right, #e65c00, #F9D423)",
+    "Clouds": "linear-gradient(to right, #bdc3c7, #465e76)",
+    "Drizzle": "linear-gradient(to right, #D0E1E6, #8CAEAB)",
+    "Fog": "linear-gradient(to right, #DECBA4, #bdc3c7)",
+    "Haze": "linear-gradient(to right, #DECBA4, #bdc3c7)",
+    "Mist": "linear-gradient(to right, #DECBA4, #bdc3c7)",
+    "Rain": "linear-gradient(to right, #283048, #859398)",
+    "Smoke": "linear-gradient(to right, #203A43, #0F2027)",
+    "Snow": "linear-gradient(to right, #00B4DB, #0083B0)",
+    "Squall": "linear-gradient(to right, #DCDBDF, #bdc3c7)",
+    "Thunderstorm": "linear-gradient(to right, #0F2027, #203A43, #2C5364)",
+    "Tornado": "linear-gradient(to right, #3E5151, #DECBA4)",
+}
+
+/*
+/// function for changing background color depending on main weather
+const colorDependingOnWeather = (weather) => {
+    //weather = "Clear"
+    if (weather === "Clear") {
+        return "linear-gradient(to right, #e65c00, #F9D423)"
+    } else if (weather === "Rain") {
+        return "linear-gradient(to right, blue, gray)"
+    }
+
+}
+*/
 
 /// Displaying todays weather forcast 
 const generatedHTMLForWeatherToday = (weatherMain) => {
     containerToday.innerHTML = `${weatherMain.weather[0].main}`
-    containerTemp.innerHTML = roundtemp(weatherMain.main.temp)
-    ///containerImage.innerHTML = iconDependingOnWeather(weatherMain.weather[0].main)
+
+
+
+    // const weather = weatherMain.weather[0].main
+    const weather = "Tornado"
+    document.getElementById('backgroundColor').style.background = weatherColor[weather]
+    document.getElementById('weatherImage').src = weatherIcon[weather]
+
+    document.getElementById('weatherTemp').innerHTML = roundtemp(weatherMain.main.temp)
     containerTempFeel.innerHTML = roundtemp(weatherMain.main.feels_like)
     containerDescription.innerHTML = `${weatherMain.weather[0].description}`
     containerSunRise.innerHTML = readableTime(weatherMain.sys.sunrise)
@@ -61,7 +108,7 @@ const generatedHTMLForWeatherToday = (weatherMain) => {
 
 /// Display weather forecast
 const generatedHTMLForWeatherForcast = (forecast) => {
-    containerForcast.innerHTML += "<div>"
+    containerForcast.innerHTML += '<div class="test">'
     containerForcast.innerHTML += readableDate(forecast.dt_txt)
     containerForcast.innerHTML += "&nbsp;&nbsp;&nbsp;"
     containerForcast.innerHTML += `${roundtemp(forecast.main.temp)}&deg;`
