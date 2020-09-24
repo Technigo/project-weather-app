45974579f833ab997f9dc1825f181b50b897b3b2
Hanna Skeppe <hannaskeppe@gmail.com>
2020-09-25 00:36:07 +0200

    Tried to change background on Weather today depending on time in city, but didn't get it to work. Displayed weather forecast in html inside JS.

--- script.js
+++ script.js
@@ -7,24 +7,25 @@ const apiUrlForecast =
   "https://api.openweathermap.org/data/2.5/forecast?q=Stockholm,Sweden&units=metric&APPID=92053ce17f6df07312088f05e0a431e0";
 
 
-//Nedan: weater-today-funktioner:
-
+//Function to round temperature to one decimal
 const calculateTemperature = (number) => {
   const roundedTemp = Math.round(number * 10) / 10; //By adding *10 AND adding /10 the number is rounded up to nearest integer with one decimal. If only using round() the number is rounded up to nearest integer.
   return roundedTemp;
 };
 
-//Nedan: mitt försök att göra en funktion för att visa tiden i staden i weather today:
+//Function to show time in city in proper format (in weather today)
+//This seems to not change the local time when refreshing the page???
 const calculateTimeInCity = (time) => {
-  const cityTime = new Date(time * 1000);
+  const cityTime = new Date(time * 1000); // See lecture 20200921 @50 mins. about new Date and toLocaleTimeString
   const cityTimeString = cityTime.toLocaleTimeString("sv-SE", {
     hour: "2-digit",
     minute: "2-digit",
     hour12: false,
   });
+  console.log(cityTimeString);
   return cityTimeString;
 }
-
+//Function to show time for sunrise/sunset in proper format (in weather today). Same as CalculateTimeIncity, so could be integrated into a single time-function. 
 const calculatingSun = (time) => {
   const sunTime = new Date(time * 1000);
   const sunTimeString = sunTime.toLocaleTimeString("sv-SE", {
@@ -32,18 +33,18 @@ const calculatingSun = (time) => {
     minute: "2-digit",
     hour12: false,
   });
-  console.log(sunTimeString);
+  //console.log(sunTimeString);
   return sunTimeString;
 };
 
-const printDay = (day) => { //
+const printDay = (day) => { 
   const forecastDays = new Date(day);
   const forecastDaysString = forecastDays.toLocaleDateString("en-US", {
     weekday: "short",
-    month: "short", //Emelie lade till
-    day: "numeric" //Emelie lade till
+    month: "short", 
+    day: "numeric" 
   });
-  console.log(forecastDaysString);
+  //console.log(forecastDaysString);
   return forecastDaysString;
 };
 
@@ -66,13 +67,26 @@ const iconDependingOnWeather = (item) => {
     return 'http://openweathermap.org/img/wn/50d@2x.png'
 }
 
+//Change background color depending on time. What sould be the parameter? THIS DON'T WORK
+//  const weatherTodayBackground = (timeInCity) => {
+//  const containerColor = document.getElementByID('weatherToday')
+//  const timeInCity = calculateTimeInCity(weatherToday.dt);
+//  const sunrise = calculatingSun(weatherToday.sys.sunrise);
+//   const sunset = calculatingSun(weatherToday.sys.sunset);
+//    if (timeInCity >= sunrise, timeInCity < sunset) {   //daytime
+//       containerColor.style.background = 'lightblue';
+//    } else if (timeInCity >= sunset, timeInCity < sunrise) //nighttime
+//       containerColor.style.background = 'black';
+//  }
+
 const generatedHTMLForWeatherToday = (weatherToday) => {
   const temperature = calculateTemperature(weatherToday.main.temp); //This is using json.main.temp as a parameter instead of number.
-  const timeInCity = calculateTimeInCity (weatherToday.dt); //Add time in city also
+  const timeInCity = calculateTimeInCity(weatherToday.dt);
   const sunrise = calculatingSun(weatherToday.sys.sunrise);
   const sunset = calculatingSun(weatherToday.sys.sunset);
-  const iconToday = iconDependingOnWeather(weatherToday.weather[0].main)
+  const iconToday = iconDependingOnWeather(weatherToday.weather[0].main);
   const description = weatherToday.weather[0].description
+  // const backgroundToday = weatherTodayBackground(); //Specify BG-color depending on time. Doesnt work!!!
   // containerToday.innerHTML = `<h1>This is ${weatherToday.name}</h1>`;
   // descriptionToday.innerHTML = `The temperature today is ${temperature} degrees and it's ${weatherToday.weather[0].description} outside.`; 
   //Since weather is an array, we need to access the index of 0, and then we can locate the object keyvalues i.e .description. This has to be done even if there is only one array, as in this case.
@@ -80,7 +94,7 @@ const generatedHTMLForWeatherToday = (weatherToday) => {
   let weatherTodayHTML = '';
   weatherTodayHTML += `<div class="location-information">`;
   weatherTodayHTML += `<div class="temp">${temperature} <span class="celsius">&#8451;</span></div>`
-  weatherTodayHTML += `<div class="location">${weatherToday.name} ${timeInCity}</div>`//Location name
+  weatherTodayHTML += `<div class="location">${weatherToday.name} ${timeInCity}</div>`//Location name and local time
   weatherTodayHTML += `<div class="description">${description}</div>`//description like "broken clouds"
   weatherTodayHTML += `<div class="sunrise-sunset">`//added div to use flexbox
   weatherTodayHTML += `<div class="sunrise">Sunrise ${sunrise}</div>`
@@ -95,16 +109,18 @@ const generatedHTMLForWeatherToday = (weatherToday) => {
 
 const generatedHTMLForWeatherForecast = (filteredForecast) => {
   const weekday = printDay(filteredForecast.dt_txt); //Should tell what day it is but doesn't work at the moment
-    console.log(filteredForecast.main.temp)
+    //console.log(filteredForecast.main.temp)
   const dailyTemp = calculateTemperature(filteredForecast.main.temp);
+  const humidity = calculateTemperature(filteredForecast.main.humidity)
   const iconForecast = iconDependingOnWeather(filteredForecast.weather[0].main);
-  return dailyTemp
 
-  // let WeatherForecast ='';
-  // weatherForecast += `<div class="weather-forecast">`;
-  // weatherForecast += `<div class="weekday"></div>`;
-  // weatherForecast += `<img src='${icon}'>`;
-  // weatherForecast += `<p>${dailyTemp} \xB0/ ${}`
+   let weatherForecast ='';
+   weatherForecast += `<div class="weather-forecast">`;
+   weatherForecast += `<div class="weekday">${weekday}</div>`;
+   weatherForecast += `<img src='${iconForecast}'>`;
+   weatherForecast += `<p>${dailyTemp} \xB0/ Humidity: ${humidity}</p>`
+   weatherForecast += `</div>`;
+  return weatherForecast
   //Weather description for the next five days
   //Humidity and wind
 };
@@ -135,7 +151,7 @@ const fetchWeatherForecast = () => {
       console.log(filteredForecast);
 
       filteredForecast.forEach((forecast) => {
-        containerForecast.innerHTML += generatedHTMLForWeatherForecast(forecast);
+        containerForecast.innerHTML += generatedHTMLForWeatherForecast(forecast)
       });
     });
 };
