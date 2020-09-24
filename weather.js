6b2580487a5fbe4d73ba7060a614c58a4bbcc0ec
idsintehittapa <jblom15@gmail.com>
2020-09-24 17:35:31 +0200

    Updated styling

--- weather.js
+++ weather.js
@@ -1,11 +1,3 @@
-const containerToday = document.getElementById('weatherMain');
-const containerTempFeel = document.getElementById('weatherTempFeel');
-const containerDescription = document.getElementById('weatherDescription');
-const containerSunRise = document.getElementById('sunrise');
-const containerSunSet = document.getElementById('sunset');
-const containerForcast = document.getElementById('weatherForecast');
-
-
 /// calculating a rounded number for the temp
 const roundtemp = (number) => {
     const roundtemp = Math.round(number * 10) / 10;
@@ -60,7 +52,7 @@ let weatherColor = {
     "Mist": "linear-gradient(to right, #DECBA4, #bdc3c7)",
     "Rain": "linear-gradient(to right, #283048, #859398)",
     "Smoke": "linear-gradient(to right, #203A43, #0F2027)",
-    "Snow": "linear-gradient(to right, #00B4DB, #0083B0)",
+    "Snow": "linear-gradient(to right, #076585, #6dd5ed)",
     "Squall": "linear-gradient(to right, #DCDBDF, #bdc3c7)",
     "Thunderstorm": "linear-gradient(to right, #0F2027, #203A43, #2C5364)",
     "Tornado": "linear-gradient(to right, #3E5151, #DECBA4)",
@@ -71,28 +63,34 @@ let weatherColor = {
 
 /// Displaying todays weather forcast 
 const generatedHTMLForWeatherToday = (weatherMain) => {
-    containerToday.innerHTML = `${weatherMain.weather[0].main}`
+    document.getElementById('weatherMain').innerHTML = `${weatherMain.weather[0].main}`
 
-    // const weather = weatherMain.weather[0].main
-    const weather = "Clear"
+    const weather = weatherMain.weather[0].main
+    //const weather = "Snow"
     document.getElementById('backgroundColor').style.background = weatherColor[weather]
     document.getElementById('weatherImage').src = weatherIcon[weather]
 
     document.getElementById('weatherTemp').innerHTML = roundtemp(weatherMain.main.temp)
-    containerTempFeel.innerHTML = roundtemp(weatherMain.main.feels_like)
-    containerDescription.innerHTML = `${weatherMain.weather[0].description}`
-    containerSunRise.innerHTML = readableTime(weatherMain.sys.sunrise)
-    containerSunSet.innerHTML = readableTime(weatherMain.sys.sunset)
+    document.getElementById('weatherTempFeel').innerHTML = roundtemp(weatherMain.main.feels_like)
+    document.getElementById('weatherDescription').innerHTML = `${weatherMain.weather[0].description}`
+    document.getElementById('sunrise').innerHTML = readableTime(weatherMain.sys.sunrise)
+    document.getElementById('sunset').innerHTML = readableTime(weatherMain.sys.sunset)
 }
 
 /// Display weather forecast
 const generatedHTMLForWeatherForcast = (forecast) => {
+    const containerForcast = document.getElementById('weatherForecast');
     containerForcast.innerHTML += '<div class="test">'
     containerForcast.innerHTML += readableDate(forecast.dt_txt)
-    containerForcast.innerHTML += "&nbsp;&nbsp;&nbsp;"
     containerForcast.innerHTML += `${roundtemp(forecast.main.temp)}&deg;`
     containerForcast.innerHTML += "</div>"
 }
+
+/// Display weather forecast
+//const generatedHTMLForWeatherForcast = (forecast) => {
+   // document.getElementById('weatherForecastDate').innerHTML += readableDate(forecast.dt_txt)
+    //document.getElementById('weatherForecastTemp').innerHTML += `${roundtemp(forecast.main.temp)}&deg;`
+//}   
 
 
 /// getting the API for todays weather
