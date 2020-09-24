2fe74d5bf1ba762e93857771c3250f9a2b06c5fe
Annika Melin <melin.anki>
2020-09-24 11:20:12 +0200

    fetched and started with forecast data

--- code/script.js
+++ code/script.js
@@ -8,27 +8,44 @@ const descriptionElement = document.getElementById("description")
 const sunriseElement = document.getElementById("sunrise")
 const sunsetElement = document.getElementById("sunset")
 
+const dayOneElement = document.getElementById("dayOne")
+const dayTwoElement = document.getElementById("dayTwo")
+const dayThreeElement = document.getElementById("dayThree")
+const dayFourElement = document.getElementById("dayFour")
+const dayFiveElement = document.getElementById("dayFive")
+
 fetch(weatherUrl)
     .then((response) => {
         return response.json()
     })
     .then((weather) => {
-        generateHTML(weather) // use map to create new object, check Wed code session
+        generateWeather(weather) // use map to create new object, check Wed code session
     })
     .catch((error) => {
-        console.log("JSON not fetched", error)
+        console.log("Fetch error", error)
     })
 
-const generateHTML = weather => {
+fetch(forecastUrl)
+    .then((response) => {
+        return response.json()
+    })
+    .then((forecast) => {
+        generateForecast(forecast) // use map to create new object
+    })
+    .catch((error) => {
+        console.log("Fetch error", error)
+    })
+
+const generateWeather = weather => {
+    const icon = getIcon(weather.weather[0].icon)
+
     handleTime(weather.sys.sunrise, weather.sys.sunset)
     const sunrise = time[0]
     const sunset = time[1]
 
-    const icon = getIcon(weather.weather[0].icon)
-
     cityElement.innerHTML = weather.name
     tempElement.innerHTML = `${Math.round(weather.main.temp)}°`
-    iconElement.innerHTML = `<img src='${icon}'>` // this can't be the way to do it
+    iconElement.innerHTML = `<img src='${icon}'>` // this can't be the way to handle the icon, and how can i handle the icon size?
     descriptionElement.innerHTML = weather.weather[0].description.charAt(0).toUpperCase() + weather.weather[0].description.slice(1)
     sunriseElement.innerHTML = `Sunrise: ${sunrise} `
     sunsetElement.innerHTML = `Sunset: ${sunset} `
@@ -41,6 +58,25 @@ const handleTime = (sunrise, sunset) => {
     const sunriseTime = sunriseDate.toLocaleTimeString([], options)
     const sunsetTime = sunsetDate.toLocaleTimeString([], options)
     return time = [sunriseTime, sunsetTime] // did I just return TWO arguments from a function??
+}
+
+const generateForecast = forecast => {
+    const noonForecast = forecast.list.filter(item => item.dt_txt.includes('12:00')) // filter already in fetch?
+
+    handleDay(noonForecast[0].dt)
+    const day = forecastDay
+
+    const icon = getIcon(noonForecast[0].weather[0].icon)
+
+    dayOneElement.innerHTML = `${day} <br>`
+    dayOneElement.innerHTML += noonForecast[0].main.temp += `<br>`
+    dayOneElement.innerHTML += `<img src='${icon}'>`
+}
+
+const handleDay = day => { // want only short day name in english
+    const forecastDate = new Date(day * 1000)
+    return forecastDay = forecastDate.getDay()
+    // make array with weekdays? must be js built in function for this..
 }
 
 const getIcon = icon => { // YES!! I made the icon work! Without having to do endless conditionals based on weather/temperature..
