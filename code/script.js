8311cf06b80f8cff16efb9a3069e7c5c15f95b27
annahasselstrom <64977313+annahasselstrom@users.noreply.github.com>
2020-09-24 14:56:18 +0200

    More changes for responsiveness.

--- code/script.js
+++ code/script.js
@@ -1,14 +1,13 @@
 // TODAY'S FORECAST
 
 const apiUrl = 'https://api.openweathermap.org/data/2.5/weather?q=Stockholm,Sweden&units=metric&APPID=20216c09e09f267ccc58282554c77ecf';
-
 const container = document.getElementById('main');
 const weatherHeader = document.getElementById('weather-header');
 const tempToday = document.getElementById('temp-today');
 const descriptionToday = document.getElementById('description-today');
 const sunriseToday = document.getElementById('sunrise-today');
 const sunsetToday = document.getElementById('sunset-today');
-const weatherImage = document.getElementById('weather-image')
+const weatherImage = document.getElementById('weather-image');
 
 fetch(apiUrl)
     .then((response) => {
@@ -17,18 +16,17 @@ fetch(apiUrl)
     .then((weatherObject) => {
 
         // Update weather in Stockholm from API
-        weatherHeader.innerHTML = `Today's weather in ${weatherObject.name}`
+        weatherHeader.innerHTML = `Today's weather in ${weatherObject.name}`;
 
-        // Get temp and rounds up or down with 1 place decimal.
-        tempToday.innerHTML = `${Math.round(weatherObject.main.temp * 10) / 10}°`
-        descriptionToday.innerHTML = weatherObject.weather[0].description  
+        // Get temp and description and rounds up or down with 1 place decimal.
+        tempToday.innerHTML = `${Math.round(weatherObject.main.temp * 10) / 10}°`;
+        descriptionToday.innerHTML = weatherObject.weather[0].description;  
 
+        // Get icon value from the API and connects these to customized icons locally stored. 
+        //The names of the locally stored icons were changed to match the icon names from Open Weather app (thanks Vanessa!)
+        const imageValue = weatherObject.weather[0].icon; 
+        weatherImage.src = `./icons/${imageValue}.png`;
 
-        // HOW DO THIS?
-        const imageValue = weatherObject.weather[0].icon 
-        weatherImage.src = `./icons/${imageValue}.png`
-
-        
         // Create two variables that stores the API values
         const sunriseValue = weatherObject.sys.sunrise;
         const sunsetValue = weatherObject.sys.sunset;
@@ -41,7 +39,7 @@ fetch(apiUrl)
         const sunriseToLocaleString = sunriseConverted.toLocaleTimeString ('en-US', {hour: '2-digit', minute: '2-digit', hour12: false,});
         const sunsetToLocaleString = sunsetConverted.toLocaleTimeString ('en-US', {hour: '2-digit', minute: '2-digit', hour12: false,});
 
-        // The values from the API - collected, stored in new variabales and converted - is returned to the html to be output in browser
+        // The values from the API - collected, stored in new variabales and converted - are returned to the html to be output in browser
         sunriseToday.innerHTML = `sunrise: ${sunriseToLocaleString}`;
         sunsetToday.innerHTML = `sunset: ${sunsetToLocaleString}`;
     });
@@ -50,8 +48,7 @@ fetch(apiUrl)
 
 const apiUrlFiveDay = 'https://api.openweathermap.org/data/2.5/forecast?q=Stockholm,Sweden&units=metric&APPID=20216c09e09f267ccc58282554c77ecf';
 
-// Connects javascript with html - allows data generated from the generateHTMLForForcast
-//function to be outputted in browser
+// Connects javascript with html - allows data generated from the generateHTMLForForcast function to be outputted in browser
 const containerFiveDay = document.getElementById('container-five-day');
 
 fetch(apiUrlFiveDay)
