f35fd48c84f4d17911eaa3f813711ecd290f38d8
ClaireCaudwell <clairecaudwell@gmail.com>
2020-09-24 19:51:12 +0200

    Deleted gitignore file in order to publish my site on netlify. And updated the links inside the api file.

---
+++ code/api-keysecure.js
@@ -0,0 +1,3 @@
+export const apiLinkWeatherInfo = "https://api.openweathermap.org/data/2.5/weather?q=Stockholm,Sweden&units=metric&APPID=1aa12a879150a3460a48993824ba1347";
+
+export const apiLinkFiveDayForecast = "https://api.openweathermap.org/data/2.5/forecast?q=Stockholm,Sweden&units=metric&APPID=1aa12a879150a3460a48993824ba1347";
