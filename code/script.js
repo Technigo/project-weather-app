809cc0db8c734a2e770cc427790ec3dc4ae2d0d2
Annika Melin <melin.anki>
2020-09-24 23:04:28 +0200

    changed indentation

--- code/script.js
+++ code/script.js
@@ -10,86 +10,86 @@ const sunsetElement = document.getElementById("sunset")
 const forecastTableElement = document.getElementById("forecastTable")
 
 fetch(weatherUrl)
-    .then((response) => {
-        return response.json()
-    })
-    .then((weather) => {
-        generateWeather(weather) // create new object containing only the data I use?
-    })
-    .catch((error) => {
-        console.log("Fetch error", error)
-    })
+  .then((response) => {
+    return response.json()
+  })
+  .then((weather) => {
+    generateWeather(weather) // create new object containing only the data I use?
+  })
+  .catch((error) => {
+    console.log("Fetch error", error)
+  })
 
 fetch(forecastUrl)
-    .then((response) => {
-        return response.json()
-    })
-    .then((forecast) => {
-        generateForecast(forecast) // create new object containing only the data I use?
-    })
-    .catch((error) => {
-        console.log("Fetch error", error)
-    })
+  .then((response) => {
+    return response.json()
+  })
+  .then((forecast) => {
+    generateForecast(forecast) // create new object containing only the data I use?
+  })
+  .catch((error) => {
+    console.log("Fetch error", error)
+  })
 
 const generateWeather = weather => {
-    const city = weather.name
-    const temp = Math.round(weather.main.temp)
-    const icon = getIcon(weather.weather[0].icon)
-    const description = weather.weather[0].description.charAt(0).toUpperCase() + weather.weather[0].description.slice(1)
+  const city = weather.name
+  const temp = Math.round(weather.main.temp)
+  const icon = getIcon(weather.weather[0].icon)
+  const description = weather.weather[0].description.charAt(0).toUpperCase() + weather.weather[0].description.slice(1)
 
-    handleTime(weather.sys.sunrise, weather.sys.sunset)
-    const sunrise = time[0]
-    const sunset = time[1]
+  handleTime(weather.sys.sunrise, weather.sys.sunset)
+  const sunrise = time[0]
+  const sunset = time[1]
 
-    cityElement.innerHTML = city
-    tempElement.innerHTML = `${temp}°`
-    iconElement.innerHTML = `<img src='${icon}'>`
-    descriptionElement.innerHTML = description
-    sunriseElement.innerHTML = `Sunrise: ${sunrise} `
-    sunsetElement.innerHTML = `Sunset: ${sunset} `
+  cityElement.innerHTML = city
+  tempElement.innerHTML = `${temp}°`
+  iconElement.innerHTML = `<img src='${icon}'>`
+  descriptionElement.innerHTML = description
+  sunriseElement.innerHTML = `Sunrise: ${sunrise} `
+  sunsetElement.innerHTML = `Sunset: ${sunset} `
 }
 
 const generateForecast = forecast => {
-    const noonForecast = forecast.list.filter(item => item.dt_txt.includes('12:00')) // filter already in fetch?
+  const noonForecast = forecast.list.filter(item => item.dt_txt.includes('12:00')) // filter already in fetch?
 
-    noonForecast.forEach((forecast) => {
-        forecastTableElement.innerHTML += generateHTML(forecast)
-    })
+  noonForecast.forEach((forecast) => {
+    forecastTableElement.innerHTML += generateHTML(forecast)
+  })
 }
 
 const generateHTML = (forecast) => {
-    handleDay(forecast.dt)
-    const day = shortForecastDay
+  handleDay(forecast.dt)
+  const day = shortForecastDay
 
-    const icon = getIcon(forecast.weather[0].icon)
-    const temp = Math.round(forecast.main.temp)
+  const icon = getIcon(forecast.weather[0].icon)
+  const temp = Math.round(forecast.main.temp)
 
-    let forecastHTML = ''
-    forecastHTML += `<li class="forecast-row">`
-    forecastHTML += `<p>${day}</p> `
-    forecastHTML += `<div><img src='${icon}'></div > `
-    forecastHTML += `<p>${temp}°</p > `
-    forecastHTML += `</li> `
-    return (forecastHTML)
+  let forecastHTML = ''
+  forecastHTML += `<li class="forecast-row">`
+  forecastHTML += `<p>${day}</p> `
+  forecastHTML += `<div><img src='${icon}'></div > `
+  forecastHTML += `<p>${temp}°</p > `
+  forecastHTML += `</li> `
+  return (forecastHTML)
 }
 
 const handleTime = (sunrise, sunset) => {
-    const sunriseDate = new Date(sunrise * 1000)
-    const sunsetDate = new Date(sunset * 1000)
-    const options = { hour: '2-digit', minute: '2-digit' }
-    const sunriseTime = sunriseDate.toLocaleTimeString([], options)
-    const sunsetTime = sunsetDate.toLocaleTimeString([], options)
-    return time = [sunriseTime, sunsetTime]
+  const sunriseDate = new Date(sunrise * 1000)
+  const sunsetDate = new Date(sunset * 1000)
+  const options = { hour: '2-digit', minute: '2-digit' }
+  const sunriseTime = sunriseDate.toLocaleTimeString([], options)
+  const sunsetTime = sunsetDate.toLocaleTimeString([], options)
+  return time = [sunriseTime, sunsetTime]
 }
 
 const handleDay = day => {
-    const forecastDate = new Date(day * 1000)
-    const forecastDay = forecastDate.toString()
-    return shortForecastDay = forecastDay.substring(0, 3)
+  const forecastDate = new Date(day * 1000)
+  const forecastDay = forecastDate.toString()
+  return shortForecastDay = forecastDay.substring(0, 3)
 }
 
 const getIcon = icon => {
-    const iconUrl1 = 'http://openweathermap.org/img/wn/'
-    const iconUrl2 = '@2x.png'
-    return icon = iconUrl1.concat(icon.concat(iconUrl2))
+  const iconUrl1 = 'http://openweathermap.org/img/wn/'
+  const iconUrl2 = '@2x.png'
+  return icon = iconUrl1.concat(icon.concat(iconUrl2))
 }
