b50c753cd295bdb190de54854e37280926667b97
Jamie Cook <jamie.p.cook@outlook.com>
2020-09-24 21:19:34 +0200

    added some style and mobile first

--- Designs/Design-1/script.js
+++ Designs/Design-1/script.js
@@ -2,13 +2,20 @@ const API_KEY = '2c57bc1d70d090d5c103c4167c1a5a74';
 const API_URL1 = `https://api.openweathermap.org/data/2.5/weather?q=Stockholm,Sweden&units=metric&APPID=${API_KEY}`;
 const API_URL2 = `https://api.openweathermap.org/data/2.5/forecast?q=Stockholm,Sweden&units=metric&APPID=${API_KEY}`;
 
-const containerToday = document.getElementById('todays-weather');
+const containerTodaysTemp = document.getElementById('todays-temperature');
+const containerCurrentCity = document.getElementById('main-city');
+const containerMainDescription = document.getElementById('main-description');
+const containerSunTimes = document.getElementById('sun-times');
+const forecastContainer = document.getElementsByClassName('five-day-forecast');
+
 const containerForecast = document.getElementById('weather-forecast');
 const containerDayOne = document.getElementById('day-one');
 const containerDayTwo = document.getElementById('day-two');
 const containerDayThree = document.getElementById('day-three');
 const containerDayFour = document.getElementById('day-four');
 const containerDayFive= document.getElementById('day-five');
+
+
 
 let filteredForecast;
 
@@ -21,26 +28,52 @@ fetch(API_URL1)
        //containerToday.innerHTML = `<h1>Current City: ${todaysResults.name}</h1>`
        //containerToday.innerHTML += `<h2>Temperature: ${todaysResults.main.temp}.toFixed(1)c</h2>`
        //containerToday.innerHTML += `<h3>${todaysResults.weather[0].description}</h3>`
-       const city = `City: ${todaysResults.name}`;
+       const city = `${todaysResults.name}`;
        const temperature = tempConvert(todaysResults.main.temp);
        const description = todaysResults.weather[0].description;
        const sunrise = timeConvert(todaysResults.sys.sunrise);
        const sunset = timeConvert(todaysResults.sys.sunset);
-       containerToday.innerHTML = city;
-       containerToday.innerHTML += `${temperature}\u00B0`;
-       containerToday.innerHTML += description;
-       console.log(sunrise);
-       containerToday.innerHTML += sunrise;
-       containerToday.innerHTML += sunset;
+       //containerTodaysTemp.innerHTML = city;
+       containerTodaysTemp.innerHTML += `${temperature}\u00B0`;
+       containerCurrentCity.innerHTML += city;
+       containerMainDescription.innerHTML += description;
+       //console.log(sunrise);
+       containerSunTimes.innerHTML += `Sunrise: ${sunrise} | `;
+       containerSunTimes.innerHTML += `Sunset: ${sunset}`;
     });
 
     fetch(API_URL2)
         .then((response) => {
             return response.json()
         })
-        .then((fiveDayForecast) => {
+        .then((forecastArray) => {
+                // filter the array for 12:00 only
+                const filteredArray = forecastArray.list.filter((item) => 
+                    item.dt_txt.includes("12:00")
+                );
+                // map to create new and filtered array of weather forecast
+                const forecasts = filteredArray.map((forecast) => {
+                    const day = setDayDate(forecast.dt).forecastDayString;
+                    const date = setDayDate(forecast.dt).forecastDateString;
+                    const iconSrc = `http://openweathermap.org/img/wn/${forecast.weather[0].icon}@2x.png`;
+                    const temperature = tempConvert(forecast.main.temp);
+                    const feelsLike = tempConvert(forecast.main.feels_like);
+                    console.log(day, date, iconSrc, temperature, feelsLike);
+                    return {day, date, iconSrc, temperature, feelsLike};
+                });
+                // forEach loop to print data to HTML
+                forecasts.forEach((item, index) => {
+                    forecastContainer[index].querySelector('.day').innerText = item.day;
+                    forecastContainer[index].querySelector('.date').innerText = item.date;
+                    forecastContainer[index].querySelector('.icon').src = item.iconSrc;
+                    forecastContainer[index].querySelector('.temperture').innerText = `${item.temperature}\u00B0 / ${item.feelsLike}\u00B0`;
+                });
+             });
+
+                /*
                 filteredForecast = fiveDayForecast.list.filter(item => 
                 item.dt_txt.includes('12:00'))
+
                 console.log(filteredForecast);
                 const dayOne = tempConvert(filteredForecast[0].main.temp);
                 containerDayOne.innerHTML = dayOne;
@@ -54,13 +87,14 @@ fetch(API_URL1)
                 containerDayFour.innerHTML = dayFour;
                 const dayFive = tempConvert(filteredForecast[4].main.temp);
                 containerDayFive.innerHTML = dayFive;
-        });
+                */
+      
         
 
 
 function tempConvert(number) {
     return Math.round(number * 10) / 10;
-}
+};
 
 const timeConvert = (time) => {
     const sun = new Date(time * 1000);
@@ -71,23 +105,17 @@ const timeConvert = (time) => {
     return sunTimeString;
 };
 
-const printDay = (day) => {
-    const forecastDays = new Date(day * 1000);
-    const forecastDaysString = forecastDays.toLocaleDateString([], {
+const setDayDate = (day) => {
+    const dayDate = new Date(day * 1000);
+    const dayDateString = dayDate.toLocaleDateString([], {
         weekday: "short",
-        month: "short",
         day: "numeric",
+        month: "short",
     });
-    console.log(forecastDaysString);
-    return forecastDaysString;
-    
+    return dayDateString;
 };
 
-//const day = printDay(filteredForecast)
 
-/*const generateHTMLForTodaysWeather = () => {
-    
-}; */
 
 
 
