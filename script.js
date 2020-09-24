67cee4eacc9cf8e08d01fdf47aae4d8af13185d0
Evelina Berg <evelina.berg@outlook.com>
2020-09-24 22:02:58 +0200

    Created JS class for object template to hold current weather info after fetching the APi

--- script.js
+++ script.js
@@ -5,7 +5,7 @@ const API_URL_CURRENT_WEATHER = `http://api.openweathermap.org/data/2.5/weather?
 const API_URL_FORECAST_WEATHER = `http://api.openweathermap.org/data/2.5/forecast?q=Gothenburg,Sweden&units=metric&appid=${API_KEY}`;
 const currentWeatherInfo = document.querySelector('.weather-info-textbox');
 const cityHeader = document.querySelector('.city');
-const forecastInfo = document.getElementsByClassName('forecast-weather-info-textbox');
+const forecastInfo = document.querySelectorAll('.forecast-weather-info-textbox');
 
 //---------------------------------Fetch API current weather---------------------
 fetch(API_URL_CURRENT_WEATHER)
@@ -13,37 +13,53 @@ fetch(API_URL_CURRENT_WEATHER)
         return response.json();
     })
     .then((currentWeather) => {
-        currentWeatherInfo.innerHTML += generateHTMLForCurrentWeatherInfo(currentWeather);
+        const currentWeatherObject = generateCurrentWeatherInfo(currentWeather);
+        currentWeatherInfo.innerHTML += generateHTMLForCurrentWeatherInfo(currentWeatherObject);
         cityHeader.innerHTML = currentWeather.name;
     });
 
-//-------------Function to generate HTML for current weather info on top of page-----
-const generateHTMLForCurrentWeatherInfo = currentWeather => {
-    const weatherDescription = currentWeather.weather[0].main;       
-    const currentTemperature = currentWeather.main.temp.toFixed(1); //rounding to 1 decimal
-    const currentWeatherFeelsLike = currentWeather.main.feels_like.toFixed(1);
-    const sunriseTime = new Date(currentWeather.sys.sunrise * 1000);
-    const sunriseTimeString = sunriseTime.toLocaleTimeString('sv-SE', {
-       timestyle: 'long',
-       hour12: false,
-       hour: '2-digit', 
-       minute:'2-digit'
-    });
+//------------Create weather info from response---------------------
+//------------Current weather info template-------------------------
 
-    const sunsetTime = new Date(currentWeather.sys.sunset * 1000);
-    const sunsetTimeString = sunsetTime.toLocaleTimeString('sv-SE', {
-        timestyle: 'short',
+class currentWeatherTemplate {
+   constructor(weatherDescription, currentTemperature, currentWeatherFeelsLike, sunriseTime, sunsetTime) {
+     this.weatherDescription = weatherDescription;
+     this.currentTemperature = currentTemperature;
+     this.currentWeatherFeelsLike = currentWeatherFeelsLike;
+     this.sunriseTime = sunriseTime;
+     this.sunsetTime = sunsetTime;
+    }
+};
+
+const getSunOutTime = data => {
+    const sunTime = new Date(data * 1000)
+    return sunTime.toLocaleTimeString('sv-SE', {
+        timestyle: 'long',
         hour12: false,
         hour: '2-digit', 
-        minute:'2-digit',
-    });
+        minute:'2-digit'
+     })
+    }
+    //-------------Function to generate HTML for current weather info on top of page-----
+//const generateHTMLForCurrentWeatherInfo = currentWeather => {
+const generateCurrentWeatherInfo = currentWeather => {
+    const currentWeatherObject = new currentWeatherTemplate(
+        currentWeather.weather[0].main,
+        currentWeather.main.temp.toFixed(1),
+        currentWeather.main.feels_like.toFixed(1),
+        getSunOutTime(currentWeather.sys.sunrise),
+        getSunOutTime(currentWeather.sys.sunset)
+    );
+    return currentWeatherObject;
+};
 //Data consuming approach
+const generateHTMLForCurrentWeatherInfo = currentWeatherObject => {
     let currentWeatherInfoHTML = '';
-    currentWeatherInfoHTML += `<p class="weather-info-text">${weatherDescription}</p>`;
-    currentWeatherInfoHTML += `<p class="weather-info-text">${currentTemperature}°</p>`;
-    currentWeatherInfoHTML += `<p class="weather-info-text">° feels like: ${currentWeatherFeelsLike}°</p>`;
-    currentWeatherInfoHTML += `<p class="weather-info-text">Sunrise: ${sunriseTimeString}</p>`;
-    currentWeatherInfoHTML += `<p class="weather-info-text">Sunset: ${sunsetTimeString}</p>`;
+    currentWeatherInfoHTML += `<p class="weather-info-text">${currentWeatherObject.weatherDescription}</p>`;
+    currentWeatherInfoHTML += `<p class="weather-info-text">${currentWeatherObject.currentTemperature}°</p>`;
+    currentWeatherInfoHTML += `<p class="weather-info-text">° feels like: ${currentWeatherObject.currentWeatherFeelsLike}°</p>`;
+    currentWeatherInfoHTML += `<p class="weather-info-text">Sunrise: ${currentWeatherObject.sunriseTime}</p>`;
+    currentWeatherInfoHTML += `<p class="weather-info-text">Sunset: ${currentWeatherObject.sunsetTime}</p>`;
     return currentWeatherInfoHTML;
 };
 
@@ -55,27 +71,27 @@ fetch(API_URL_FORECAST_WEATHER)
     })
     .then(forecastWeather => {
         console.log(forecastWeather);
-//      // filteredForecast is now an array with only the data from 12:00 each day.
-        const filteredForecast = forecastWeather.list.filter(item => 
-            item.dt_txt.includes('12:00'));
-        const newForecast = filteredForecast.map(forecast => {
-            const dateDay = new Date(forecast.dt * 1000);
-            const dateDayString = dateDay.toLocaleDateString('en-GB', {
-                weekday: 'short',
-            });
-            const temperature = forecast.main.temp.toFixed(1);
-            const tempFeelsLike = forecast.main.feels_like.toFixed(1);
-
-            return { dateDayString, temperature, tempFeelsLike };
-        });
-
-        for (const [index, everyItem] of newForecast.entries()) {
-            forecastInfo[index].querySelector('.forecast-info-day').innerText = everyItem.dateDayString;
-            forecastInfo[index].querySelector('.forecast-info-temp').innerText = everyItem.temperature;
-            forecastInfo[index].querySelector('.forecast-info-feels-like').innerText = everyItem.tempFeelsLike;
-        };
-
-        console.log(newForecast);
-
+        generateHTMLForForecastWeatherInfo(forecastWeather);
     });
+
+    const generateHTMLForForecastWeatherInfo = forecastWeather => {
+    const filteredForecast = forecastWeather.list.filter(item => 
+        item.dt_txt.includes('12:00'));  // array with data from 12:00 each day.
+    const newForecast = filteredForecast.map(forecast => {
+        const dateDay = new Date(forecast.dt * 1000);
+        const dateDayString = dateDay.toLocaleDateString('en-GB', {
+            weekday: 'short',
+        });
+        const temperature = forecast.main.temp.toFixed(1);
+        const tempFeelsLike = forecast.main.feels_like.toFixed(1);
+
+        return { dateDayString, temperature, tempFeelsLike };
+    });
+
+    for (const [index, everyItem] of newForecast.entries()) {
+        forecastInfo[index].querySelector('.forecast-info-day').innerText = everyItem.dateDayString;
+        forecastInfo[index].querySelector('.forecast-info-temp').innerText = everyItem.temperature;
+        forecastInfo[index].querySelector('.forecast-info-feels-like').innerText = everyItem.tempFeelsLike;
+    };
+};
    
