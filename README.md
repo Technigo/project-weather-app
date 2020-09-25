85bfeb344c443d899bf2a90864d7b8c4a3bb0cd1
Claudia <ckjerrgren@gmail.com>
2020-09-25 12:26:10 +0200

    test

--- README.md
+++ README.md
@@ -1,8 +1,6 @@
 # Weather App
 
-Replace this readme with your own information about your project.
-
-Start by briefly describing the assignment in a sentence or two. Keep it short and to the point.
+Building a weather app using API's and fetch. 
 
 ## The problem
 
---
+++ code/script.js
@@ -0,0 +1,52 @@
+
+//API fo Weather made to constant for easy re-use 
+const weatherUrl ='https://api.openweathermap.org/data/2.5/weather?q=Stockholm,Sweden&units=metric&APPID=f03e81d089081eb6bc8048a2ff0048e8'
+
+
+//constants variables for weather temp/ description/ main
+const container = document.getElementById('container')
+const temperature = document.getElementById('temperature')
+const weather = document.getElementById('weather')
+
+const sunRise = document.getElementById('sunRise')
+const sunSet = document.getElementById('sunSet')
+
+
+// So you can read the time in HH:MM
+const formatTime = (timestamp) => {
+    let readableTime = new Date(timestamp * 1000);
+
+    readableTime = readableTime.toLocaleTimeString('sv-SE', {
+        hour: '2-digit',
+        minute: '2-digit',
+        hour12: false,
+    });
+    return readableTime;
+}
+
+
+//fetch - (stop trying to make fetch happen)
+fetch(weatherUrl)
+.then((response) => {
+    return response.json()
+})
+.then((json) => {
+    console.log(json)
+    
+    container.innerHTML = `<h1> The weather in ${json.name} is ${json.weather[0].main} with ${json.weather[0].description}. </h1>`;
+
+    temperature.innerHTML = Math.round(json.main.temp) + "°";
+
+    sunRise.innerHTML = `sun up picture? ${formatTime(json.sys.sunrise)}`;
+    sunSet.innerHTML = `sun up picture? ${formatTime(json.sys.sunset)}`;
+    
+
+
+    
+}); 
+
+   
+
+
+// Show on site:
+// city // temp // weather 
