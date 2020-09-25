830c72e45d81c11bf40da9877904dd376ffa0a4c
Karan Mann <62376840+karanmann@users.noreply.github.com>
2020-09-25 14:34:46 +0200

    Blue Level Completed

--- Code/script.js
+++ Code/script.js
@@ -1,9 +1,11 @@
 
 // GET WEATHER REPORT
 const enteredCityLocation = "Stockholm";
+
 //API's
 const API_ONE_DAY = `https://api.openweathermap.org/data/2.5/weather?q=${enteredCityLocation}&appid=3eb926770233f3bacc440bffc14e56a4`;
 const API_FIVE_DAYS = `https://api.openweathermap.org/data/2.5/forecast?q=${enteredCityLocation}&appid=3eb926770233f3bacc440bffc14e56a4`;
+
 // Weather Variables
 const cityLocation = document.getElementById("currentCityName");
 const tempIcon = document.getElementById("temp-icon");
@@ -11,6 +13,7 @@ const tempValue = document.getElementById("temp-value");
 const climate = document.getElementById("climate");
 const sunRiseTime = document.getElementById("sunrise");
 const sunSetTime = document.getElementById("sunset");
+
 // Getting Data for current Weather
 fetch(API_ONE_DAY)
   .then((response) => {
@@ -20,18 +23,21 @@ fetch(API_ONE_DAY)
     console.log(data);//CONSOLE LOG TO SEE API DATA
     const { id, main } = data.weather[0];
     cityLocation.textContent = data.name;
-    tempValue.textContent = Math.round(data.main.temp - 273);
+    tempValue.textContent = ((data.main.temp - 273).toFixed(1));
     climate.textContent = main;
+
     //Calculation of Sunrise time
     const sunriseHour = new Date(data.sys.sunrise * 1000).getHours();
     const sunriseMinutes = "0" + new Date(data.sys.sunrise * 1000).getMinutes();
     const formattedSunrise = `${sunriseHour}:${sunriseMinutes.substr(-2)}`;
+
     // Calculation of Sunset time
     const sunsetHour = new Date(data.sys.sunset * 1000).getHours();
     const sunsetMinutes = "0" + new Date(data.sys.sunset * 1000).getMinutes();
     const formattedSunset = `${sunsetHour}:${sunsetMinutes.substr(-2)}`;
     sunRiseTime.textContent = formattedSunrise;
     sunSetTime.textContent = formattedSunset;
+
     // Changing Icons for weather using if-else
     if (id < 250) {
       tempIcon.src = './images/icons/thunder.png';
@@ -61,6 +67,7 @@ fetch(API_ONE_DAY)
   .catch((error) => {
     console.log('There has been an error')
   })
+
 // Fetch Data for next 5 days
 fetch(API_FIVE_DAYS)
   .then((response) => {
@@ -68,7 +75,7 @@ fetch(API_FIVE_DAYS)
   })
   .then((forecast) => {
     console.log(forecast)  //CONSOLE LOG TO SEE API DATA
-    const filteredForecast = forecast.list.filter(item => item.dt_txt.includes('12:00'));
+    const filteredForecast = forecast.list.filter(item => item.dt_txt.includes('09:00'));
     let output = '';
     filteredForecast.forEach((item) => {
       const maxTemperature = Math.round(item.main.temp_max - 273);
@@ -77,7 +84,7 @@ fetch(API_FIVE_DAYS)
       const weekday = date.toLocaleDateString('en-US', {
         weekday: 'short'
       });
-      output += `<p class="day">${weekday} <span>${maxTemperature}°C</span>|<span>${minTemperature}°C</span> </p>`;
+      output += `<p>${weekday} : ${maxTemperature}°C | ${minTemperature}°C </p>`;
     });
     document.getElementById('forecast').innerHTML = output;
   });
