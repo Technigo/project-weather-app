8dc5a9af5ec7a4aa493a78eb621ce9c4585c92b7
LillanEriksson <nailsbylillan@hotmail.com>
2020-09-25 16:46:21 +0100

    Weather icons now working

--- script.js
+++ script.js
@@ -1,6 +1,6 @@
 const fetchWeather = () => {
   const weatherData =
-    'http://api.openweathermap.org/data/2.5/weather?q=Sollefteå,Sweden&units=metric&APPID=e91b147ba928204edc2b66bba9327d4c';
+    'https://api.openweathermap.org/data/2.5/weather?q=Sollefteå,Sweden&units=metric&APPID=e91b147ba928204edc2b66bba9327d4c';
 
   fetch(weatherData)
     .then((response) => {
@@ -16,16 +16,24 @@ const fetchWeather = () => {
       const sunset = new Date(weather.sys.sunset * 1000);
       const sunsetTime = sunset.toLocaleTimeString([]);
       const weatherContainer = document.getElementById('weather-container');
+      const sunContainer = document.getElementById('sunrise-sunset-container');
+      const bigWeatherIconContainer = document.getElementById(
+        'todays-weather-icon'
+      );
+      const weatherIcon = weather.weather[0].icon;
 
-      weatherContainer.innerHTML = `<h1>${city}</h1>`;
-      weatherContainer.innerHTML += `<h2>${temp.toFixed(
-        1
-      )} ${description}</h2>`; //get an icon here
+      bigWeatherIconContainer.innerHTML = `<img src="http://openweathermap.org/img/wn/${weatherIcon}@2x.png">`;
+
+      weatherContainer.innerHTML += `<h1>${temp.toFixed(1)}&degC </h1>`;
+      weatherContainer.innerHTML += `<h2>${city}</h2>`;
+      weatherContainer.innerHTML += `<h3>${description}</h3>`;
       weatherContainer.innerHTML += `<p>Feels like: ${tempFeelsLike.toFixed(
         1
       )}</p>`;
-      weatherContainer.innerHTML += `<p>Sunrise ${sunriseTime}</p>`;
-      weatherContainer.innerHTML += `<p>Sunset ${sunsetTime}</p>`;
+      sunContainer.innerHTML += `<p>Sunrise ${sunriseTime}</p>`;
+      sunContainer.innerHTML += `<p>Sunset ${sunsetTime}</p>`;
+
+      console.log(weather);
     });
 };
 
@@ -41,17 +49,21 @@ const fetchFiveDayForecast = () => {
       const filterForecast = weatherForecast.list.filter((item) =>
         item.dt_txt.includes('12:00')
       );
+
       filterForecast.forEach((day) => {
         const temp = day.main.temp;
         const date = new Date(day.dt_txt);
         const options = { weekday: 'short' };
-        const localDateString = date.toLocaleDateString('sv-SE', options);
-        const forecastContainer = document.getElementById('weather-container');
-        forecastContainer.innerHTML += `<p>${localDateString} ${temp.toFixed(
-          1
-        )}</p>`;
+        const forecastIcon = day.weather[0].icon;
+        const localDateString = date.toLocaleDateString('en-EN', options);
+        const forecastDayContainer = document.getElementById('forecast-date');
+        const forecastIconContainer = document.getElementById('forecast-icon');
+        const forecastTempContainer = document.getElementById('forecast-temp');
 
-        console.log(day);
+        forecastDayContainer.innerHTML += `<p>${localDateString}</p>`;
+
+        forecastIconContainer.innerHTML += `<img src="http://openweathermap.org/img/wn/${forecastIcon}@2x.png">`;
+        forecastTempContainer.innerHTML += `<p>${temp.toFixed(1)}&degC</p>`;
       });
     });
 };
