6c916d4b28433ceb11da72b398ace01ab6e8ad43
Jamie Cook <jamie.p.cook@outlook.com>
2020-09-26 20:44:05 +0200

    change file location again due to netlify deploy failing

--- script.js
+++ script.js
@@ -8,41 +8,23 @@ const containerMainDescription = document.getElementById('main-description');
 const containerSunTimes = document.getElementById('sun-times');
 const forecastContainer = document.getElementsByClassName('five-day-forecast');
 
-const containerForecast = document.getElementById('weather-forecast');
-const containerDayOne = document.getElementById('day-one');
-const containerDayTwo = document.getElementById('day-two');
-const containerDayThree = document.getElementById('day-three');
-const containerDayFour = document.getElementById('day-four');
-const containerDayFive= document.getElementById('day-five');
-
-
-
-let filteredForecast;
-
-fetch(API_URL1)
+fetch(API_URL1) // first fetch to get todays weather
     .then((response) => {
         return response.json();
     })
     .then((todaysResults) => {
-       //containerToday.innerHTML += generateHTMLForTodaysWeather(todaysResults);
-       //containerToday.innerHTML = `<h1>Current City: ${todaysResults.name}</h1>`
-       //containerToday.innerHTML += `<h2>Temperature: ${todaysResults.main.temp}.toFixed(1)c</h2>`
-       //containerToday.innerHTML += `<h3>${todaysResults.weather[0].description}</h3>`
        const city = `${todaysResults.name}`;
        const temperature = tempConvert(todaysResults.main.temp);
        const description = todaysResults.weather[0].description;
        const sunrise = timeConvert(todaysResults.sys.sunrise);
        const sunset = timeConvert(todaysResults.sys.sunset);
-       //containerTodaysTemp.innerHTML = city;
        containerTodaysTemp.innerHTML += `${temperature}\u00B0`;
        containerCurrentCity.innerHTML += city;
-       containerMainDescription.innerHTML += description;
-       //console.log(sunrise);
-       containerSunTimes.innerHTML += `Sunrise: ${sunrise} | `;
-       containerSunTimes.innerHTML += `Sunset: ${sunset}`;
+       containerMainDescription.innerHTML += description.toUpperCase();
+       containerSunTimes.innerHTML += `Sunrise: ${sunrise} | Sunset: ${sunset}`;
     });
 
-    fetch(API_URL2)
+    fetch(API_URL2) // second fetch to get 5 day forecast
         .then((response) => {
             return response.json()
         })
@@ -53,43 +35,21 @@ fetch(API_URL1)
                 );
                 // map to create new and filtered array of weather forecast
                 const forecasts = filteredArray.map((forecast) => {
-                    const day = setDayDate(forecast.dt).forecastDayString;
-                    const date = setDayDate(forecast.dt).forecastDateString;
+                    const day = setDayDate(forecast.dt);
                     const iconSrc = `http://openweathermap.org/img/wn/${forecast.weather[0].icon}@2x.png`;
                     const temperature = tempConvert(forecast.main.temp);
                     const feelsLike = tempConvert(forecast.main.feels_like);
-                    console.log(day, date, iconSrc, temperature, feelsLike);
-                    return {day, date, iconSrc, temperature, feelsLike};
+                    console.log(day, iconSrc, temperature, feelsLike);
+                    return {day, iconSrc, temperature, feelsLike};
                 });
                 // forEach loop to print data to HTML
                 forecasts.forEach((item, index) => {
+                    console.log(item);
                     forecastContainer[index].querySelector('.day').innerText = item.day;
-                    forecastContainer[index].querySelector('.date').innerText = item.date;
                     forecastContainer[index].querySelector('.icon').src = item.iconSrc;
-                    forecastContainer[index].querySelector('.temperture').innerText = `${item.temperature}\u00B0 / ${item.feelsLike}\u00B0`;
+                    forecastContainer[index].querySelector('.temperature').innerText = `${item.temperature}\u00B0 / ${item.feelsLike}\u00B0`;
                 });
-             });
-
-                /*
-                filteredForecast = fiveDayForecast.list.filter(item => 
-                item.dt_txt.includes('12:00'))
-
-                console.log(filteredForecast);
-                const dayOne = tempConvert(filteredForecast[0].main.temp);
-                containerDayOne.innerHTML = dayOne;
-                containerDayOne.innerHTML += printDay();
-                
-                const dayTwo = tempConvert(filteredForecast[1].main.temp);
-                containerDayTwo.innerHTML = dayTwo;
-                const dayThree = tempConvert(filteredForecast[2].main.temp);
-                containerDayThree.innerHTML = dayThree;
-                const dayFour = tempConvert(filteredForecast[3].main.temp);
-                containerDayFour.innerHTML = dayFour;
-                const dayFive = tempConvert(filteredForecast[4].main.temp);
-                containerDayFive.innerHTML = dayFive;
-                */
-      
-        
+             });        
 
 
 function tempConvert(number) {
@@ -108,9 +68,13 @@ const timeConvert = (time) => {
 const setDayDate = (day) => {
     const dayDate = new Date(day * 1000);
     const dayDateString = dayDate.toLocaleDateString([], {
-        weekday: "short",
-        day: "numeric",
-        month: "short",
+        weekday: "long",
     });
     return dayDateString;
 };
+
+
+
+
+
+
