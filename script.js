6b230521b35b598d644c5ff88d86ea2b3b3c4368
sannadw <sanna.dolck@gmail.com>
2020-09-26 19:05:39 +0200

    styling

--- script.js
+++ script.js
@@ -13,7 +13,7 @@ fetch(API_URL)
         // weatherContainer.innerHTML = `<h1>${json.name}</h1>`
 
         const cityContainer = document.getElementById('city');
-        cityContainer.innerHTML = `<h1>${json.name} hej hej</h1>`
+        cityContainer.innerHTML = `<h1>Light a fire and get cosy. ${json.name} is looking grey today.</h1>`
        
         const weatherContainer = document.getElementById('weather')
         json.weather.forEach((weather) => {
@@ -22,22 +22,40 @@ fetch(API_URL)
             const sunset = new Date(json.sys.sunset * 1000);
             const sunSetTime = sunset.toLocaleTimeString ('se-SV', { hour : '2-digit', minute : '2-digit'});
             
-            weatherContainer.innerHTML += `<p>${weather.description} | ${json.main.temp.toFixed(1)} </p>`
+            weatherContainer.innerHTML += `<p>${weather.description} | ${json.main.temp.toFixed(1)}° </p>`
             weatherContainer.innerHTML += `<p>sunrise ${sunRiseTime}</p>`
             weatherContainer.innerHTML += `<p>sunset ${sunSetTime}</p>`
+       
+// const weatherType = weather.weather[0].main
+// const setWeatherColors = () => {
+//     if  (weatherType === 'Clouds') {
+//         weatherContainer.classList.add(cloud);
+//         weatherContainer.classList.remove(sunny);
+//         weatherContainer.classList.remove(rain);
+//     } else if (weatherType === 'Clear') {
+//         weatherContainer.classList.add(sunny);
+//         weatherContainer.classList.remove(cloud);
+//         weatherContainer.classList.remove(rain);   
+//     } else (weatherType === 'Rain') || (weather.main === 'Drizzle')
+//          weatherContainer.classList.add(rain);
+//          weatherContainer.classList.remove(sunny);
+//          weatherContainer.classList.remove(cloud);
+            
+//             }
+       
+       
         });
 
      }) 
     
 
-//    const weatherColors = () => {
-//        if (weather.main === "clouds")
-        
+// const div = document.createElement('div');
+// div.className = 'sunglasses-image';
 
-//        else (weather.main === "clear")
+// div.classList.remove("forecast-ul");
+// div.classList.add("sunglasses-image");
 
-//        else if (weather.main === "rain") || (weather.main === "drizzle")
-//  }
+// console.log(div.outerHTML);
 
 
 
@@ -65,7 +83,7 @@ fetch(API_URL_5DAYS)
             const options = { weekday: 'short' };
             const weekdayString = new Intl.DateTimeFormat('en-US', options).format(forecastDate);
 
-           forecastUl.innerHTML += `<li>${weekdayString} ${forecast.main.temp.toFixed(0)}°</li>`       
+           forecastUl.innerHTML += `<li>${weekdayString} ${forecast.main.temp.toFixed(0)}°</li>`     
         });
      }) 
 
