b92d6fd3e93642e8d1f27f02d70d1e7489d2f3f3
elisa blomberg <elisa.blomberg@outlook.com>
2020-09-26 15:18:20 +0200

    Connecting the HTML-elements(id names) to JS, changing some...

--- weather.js
+++ weather.js
@@ -2,16 +2,15 @@ const apiUrl = 'https://api.openweathermap.org/data/2.5/weather?q=Stockholm,Swed
 const apiUrl2 = 'https://api.openweathermap.org/data/2.5/forecast?q=Stockholm,Sweden&units=metric&APPID=8b97619989976c72fc1e602d8c793890';
 
 
-//CURRENT WEATHER
+//CURRENT WEATHER:
 fetch(apiUrl).then((response) => {
     return response.json();
 }).then((json) => {
     mainTemp.innerHTML += `<h1>${json.main.temp.toFixed(1)}&#730<sup>c</sup></h1>`;
     city.innerHTML += `<h1>${json.name}<h1>`;
     //.toFixed(1) rounds the temperature to 1 decimal
-    
     json.weather.forEach((weather) => {
-    container.innerHTML += `<p>The weather is ${weather.description}</p>`
+    weatherDescription.innerHTML += `<p>The weather is ${weather.description}</p>`
     });
     const sunrise = new Date(json.sys.sunrise * 1000);
     const sunriseTime = sunrise.toLocaleTimeString([], { timeStyle: "short" });
@@ -19,12 +18,12 @@ fetch(apiUrl).then((response) => {
     const sunset = new Date(json.sys.sunset * 1000);
     const sunsetTime = sunset.toLocaleTimeString([], { timeStyle: "short" });
  
-    container.innerHTML += `<h2> Sunrise ${sunriseTime}</h2>`;
-    container.innerHTML += `<h2> Sunset ${sunsetTime} </h2>`;
+    sunriseInfo.innerHTML += `<h2> Sunrise ${sunriseTime}</h2>`;
+    sunsetInfo.innerHTML += `<h2> Sunset ${sunsetTime} </h2>`;
  
 });
 
-//Weather Forecast for 5 days:
+//FORECAST FOR 5 DAYS:
 fetch(apiUrl2).then((response) => {
     return response.json();
 }).then((json) => {
@@ -35,7 +34,8 @@ fetch(apiUrl2).then((response) => {
         const weekdays = ["SUN", "MON", "TUE", "WED", "THU", "FRI", "SAT"];
         let dayOfWeek = weekdays[date.getDay()];
   
-        container.innerHTML += `<h2>${dayOfWeek} <span>${day.main.temp.toFixed(1)}&#730<sup>c</sup></span></h2>`;
+        theWeekdays.innerHTML += `<h2>${dayOfWeek}</h2>`;
+        forecastMainTemp.innerHTML += `<h2><span>${day.main.temp.toFixed(1)}&#730<sup>c</sup></span></h2>`;
     });
     console.log(json)
 });
