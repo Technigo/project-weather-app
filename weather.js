0361ffb4f85ab2d393fd56eded0e54d9b5518e23
elisa blomberg <elisa.blomberg@outlook.com>
2020-09-26 21:15:55 +0200

    Adding svg/icons, showing depending on weather

--- weather.js
+++ weather.js
@@ -1,6 +1,7 @@
 const apiUrl = 'https://api.openweathermap.org/data/2.5/weather?q=Stockholm,Sweden&units=metric&APPID=8b97619989976c72fc1e602d8c793890';
 const apiUrl2 = 'https://api.openweathermap.org/data/2.5/forecast?q=Stockholm,Sweden&units=metric&APPID=8b97619989976c72fc1e602d8c793890';
 
+
 const weatherDescription = document.getElementById('weatherDescription');
 //const city = document.getElementById('city');
 //const mainTemp = document.getElementById('mainTemp');
@@ -14,8 +15,20 @@ fetch(apiUrl).then((response) => {
     city.innerHTML += `<h1>${json.name}<h1>`;
     //.toFixed(1) rounds the temperature to 1 decimal
     json.weather.forEach((weather) => {
-    weatherDescription.innerHTML += `<h1>${weather.description}</h1>`
-    });
+    weatherDescription.innerHTML += `<h1>${weather.description}</h1>` 
+    weatherIconShow(json.weather[0].main)
+});
+
+    function weatherIconShow(weather) {
+        if (weather == "Rain") {
+            document.getElementById('weatherIcon').src = './icons/noun_Umbrella_2030530.svg';
+        } else if (weather == "Clouds") {
+            document.getElementById('weatherIcon').src = './icons/noun_Cloud_1188486.svg';
+        } else if (weather == "Sunny") {
+            document.getElementById('weatherIcon').src = './icons/noun_Sunglasses_2055147.svg';
+        } 
+    }
+
     const sunrise = new Date(json.sys.sunrise * 1000);
     const sunriseTime = sunrise.toLocaleTimeString([], { timeStyle: "short" });
  
@@ -26,6 +39,8 @@ fetch(apiUrl).then((response) => {
     sunsetInfo.innerHTML += `<h2> Sunset ${sunsetTime} </h2>`;
  
 });
+
+
 
 //FORECAST FOR 5 DAYS:
 fetch(apiUrl2).then((response) => {
