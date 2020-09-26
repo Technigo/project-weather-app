a7de30c60d96d9dcabccaca952a0dcda0d5c590b
caroline birgersson <caroline@birgerochblom.se>
2020-09-26 19:00:47 +0200

    adding some styling + optimizing the structure

--- code/weather.js
+++ code/weather.js
@@ -1,21 +1,23 @@
 //Weather from API
+const apiWeatherToday = 'http://api.openweathermap.org/data/2.5/weather?q=Stockholm,Sweden&units=metric&APPID=e33f1cc192401277e601a6aed3a82800';
+const apiForecast = "https://api.openweathermap.org/data/2.5/forecast?q=Stockholm,Sweden&units=metric&APPID=e33f1cc192401277e601a6aed3a82800";
 
-const apiToday = 'http://api.openweathermap.org/data/2.5/weather?q=Stockholm,Sweden&units=metric&APPID=e33f1cc192401277e601a6aed3a82800'
-const apiKey = "e33f1cc192401277e601a6aed3a82800"
+//Catching id's from html
 const weatherLocation = document.getElementById('location');
 const weatherDescription = document.getElementById('description');
 const weatherTemperature = document.getElementById('temperature');
+const weatherIcon = document.getElementById('icon');
 
 // Fetch function
 const fetchWeather = () => {
-    fetch(apiToday)
+    fetch(apiWeatherToday)
     .then((response) => {
     return response.json();
     })
     .then((json) => {
     weatherLocation.innerHTML = json.name;
     weatherDescription.innerHTML = json.weather[0].description
-    weatherTemperature.innerHTML = json.main.temp.toFixed(0.5)
+    weatherTemperature.innerHTML = Math.round(json.main.temp)
 
 // Sunrise
     const weatherSunrise = () => {
@@ -31,11 +33,13 @@ const fetchWeather = () => {
 
 // Sunset
     const weatherSunset = () => {
+      document.getElementById('sunset').innerHTML += `<p>Sunset</p>`;
         const dateSunset = new Date(json.sys.sunset * 1000);
         const timeSunset = dateSunset.toLocaleTimeString([], {
             hour: '2-digit',
             minute: '2-digit',
         });
+
         document.getElementById('sunset').innerHTML = timeSunset;
     };
 
@@ -46,8 +50,6 @@ const fetchWeather = () => {
 fetchWeather();
 
 // Forecast for the next 5 days
-const apiForecast = "https://api.openweathermap.org/data/2.5/forecast?q=Stockholm,Sweden&units=metric&APPID=e33f1cc192401277e601a6aed3a82800";
-
 fetch(apiForecast)
     .then((Response) => { 
         return Response.json()
@@ -56,11 +58,11 @@ fetch(apiForecast)
         const filteredForecast = json.list.filter(item =>
         item.dt_txt.includes("12:00")
         );
-        
+
         filteredForecast.forEach(day => {
             const date = new Date(day.dt * 1000);
 
-            const dayName = date.toLocaleDateString("en-EN", { weekday: "long" });
+            const dayName = date.toLocaleDateString("en-EN", { weekday: "short" });
 
             const dayTemp = day.main.temp;
             const weekTemp = dayTemp.toFixed(0.1);
