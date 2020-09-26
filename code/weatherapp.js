0207bee578a6c107e84722844af301176316ca52
ClaireCaudwell <clairecaudwell@gmail.com>
2020-09-26 20:26:30 +0200

    JS file & README: 1. Updated the 5 day forecast with a for loop. In the for loop I access the data I need from the API, which is then repeated for every time the for loop is run (is true) for the length of the filteredList array. 2. I also used the for loop when accessing the HTML id's and adding 1 each time the loop is done to access each of teh HTML elements e.g. day1Day, day2Day etc. 3. Updated README file stating that I have created a for loop with some help as I had earlier written that I had not done this and duplicated the fetched data from the API for each day in the forecast.

--- code/weatherapp.js
+++ code/weatherapp.js
@@ -3,7 +3,7 @@ const apiLinkWeatherInfo = "https://api.openweathermap.org/data/2.5/weather?q=St
 
 const apiLinkFiveDayForecast = "https://api.openweathermap.org/data/2.5/forecast?q=Stockholm,Sweden&units=metric&APPID=1aa12a879150a3460a48993824ba1347";
 
-//Stockholm current weather info fetched from city weather  API
+//Stockholm current weather info fetched from city weather API
 const fetchedApiInfoCityWeather = () => {
     fetch(apiLinkWeatherInfo)
     .then((response) => {
@@ -16,7 +16,7 @@ const fetchedApiInfoCityWeather = () => {
 
         //Get today's date in day/month/year format
         const todaysDate = new Date(weatherInfo.dt * 1000);
-        const options = { day: 'numeric', month: 'short', year: 'numeric'};
+        const options = {day: 'numeric', month: 'short', year: 'numeric'};
         const todaysDateFormat = todaysDate.toLocaleDateString('en-GB', options);
         document.getElementById("todaysDate").innerText = `${todaysDateFormat}`;
 
@@ -58,75 +58,26 @@ const fiveDayForecastApiInfo = () => {
         return response.json()
     })
     .then((fiveDayForecastInfo) => {
-        //Get forecast in Stockholm for next 5 days
-        //Filtering the next 5 day's with the list info at 12:00 that day
+
+        //Filtering the temperature at midday for the next 5 days
         const filteredList = fiveDayForecastInfo.list.filter(item => item.dt_txt.includes('12:00'));
 
-        /* ------ DAY 1 FORECAST ---- */
-        //Taking the first array from the filteredList and accessing the date for that day. Passing that as an argument in the new Date so it can convert it into today's date and time.
-        const dayOneDate = new Date(filteredList[0].dt * 1000);  
-        //Created an array with weekday names.Then used the dayOneDate together with the getDay() method to select from the array which day it is. The getDay method tells you this is number format e.g. 1 = Sunday. Which I think then selects that from the array of days.     
-        var days = ["Sunday","Monday","Tuesday","Wednesday","Thursday","Friday","Saturday"];
-        const finalDayOneDate = days[dayOneDate.getDay()];
-        //Taking the first array index from the filteredList and accessing the temperature for that day whilst rounding the temperature to one decimal place.
-        const dayOneTemp = filteredList[0].main.temp.toFixed(1);  
-        //Getting description and icon for the next five days
-        const dayOneDescription = filteredList[0].weather[0].description;
-        const dayOneDescpIcon = filteredList[0].weather[0].icon;
+        //For loop that loops through each of the array elements (objects), accesses specific information from the objects and prints to HTML. In HTML getElementById have used the for loop to access each of the HTML elements by adding 1 each time to what the loop returns, as they are named differently e.g. day1Day, day2Day etc.
+        for (let i=0; i < filteredList.length; i++) {
+            const day = new Date(filteredList[i].dt * 1000);
+            var days = ["Sunday","Monday","Tuesday","Wednesday","Thursday","Friday","Saturday"];
+            const finalDay = days[day.getDay()];
+            document.getElementById(`day${i+1}Day`).innerHTML = `${finalDay}`;
 
-        document.getElementById("day1Day").innerHTML = `${finalDayOneDate}`;
-        document.getElementById("day1Type").innerHTML = `${dayOneDescription}`;
-        document.getElementById("day1Icon").src = `./images/${dayOneDescpIcon}.png`;
-        document.getElementById("day1Temp").innerHTML = `${dayOneTemp}&#176`;
+            const temp = filteredList[i].main.temp.toFixed(1);
+            document.getElementById(`day${i+1}Temp`).innerHTML = `${temp}&#176`;
 
+            const description = filteredList[i].weather[0].description;
+            document.getElementById(`day${i+1}Type`).innerHTML = `${description}`;
 
-        /* ------ DAY 2 FORECAST ---- */
-        const dayTwoDate = new Date(filteredList[1].dt * 1000);
-        const finalDayTwoDate = days[dayTwoDate.getDay()];
-        const dayTwoDescription = filteredList[1].weather[0].description;
-        const dayTwoDescpIcon = filteredList[1].weather[0].icon;
-        const dayTwoTemp = filteredList[1].main.temp.toFixed(1);
-
-        document.getElementById("day2Day").innerHTML = `${finalDayTwoDate}`;
-        document.getElementById("day2Type").innerHTML = `${dayTwoDescription}`;
-        document.getElementById("day2Icon").src = `./images/${dayTwoDescpIcon}.png`;
-        document.getElementById("day2Temp").innerHTML = `${dayTwoTemp}&#176`;
-
-        /* ------ DAY 3 FORECAST ---- */
-        const dayThreeDate = new Date(filteredList[2].dt * 1000);
-        const finalDayThreeDate = days[dayThreeDate.getDay()];
-        const dayThreeDescription = filteredList[2].weather[0].description;
-        const dayThreeDescpIcon = filteredList[2].weather[0].icon;
-        const dayThreeTemp = filteredList[2].main.temp.toFixed(1);
-
-        document.getElementById("day3Day").innerHTML = `${finalDayThreeDate}`;
-        document.getElementById("day3Type").innerHTML = `${dayThreeDescription}`;
-        document.getElementById("day3Icon").src = `./images/${dayThreeDescpIcon}.png`;
-        document.getElementById("day3Temp").innerHTML = `${dayThreeTemp}&#176`;
-
-        /* ------ DAY 4 FORECAST ---- */
-        const dayFourDate = new Date(filteredList[3].dt * 1000);
-        const finalDayFourDate = days[dayFourDate.getDay()];
-        const dayFourDescription = filteredList[3].weather[0].description;
-        const dayFourDescpIcon = filteredList[3].weather[0].icon;
-        const dayFourTemp = filteredList[3].main.temp.toFixed(1);
-
-        document.getElementById("day4Day").innerHTML = `${finalDayFourDate}`;
-        document.getElementById("day4Type").innerHTML = `${dayFourDescription}`;
-        document.getElementById("day4Icon").src = `./images/${dayFourDescpIcon}.png`;
-        document.getElementById("day4Temp").innerHTML = `${dayFourTemp}&#176`;
-
-        /* ------ DAY 5 FORECAST ---- */
-        const dayFiveDate = new Date(filteredList[4].dt * 1000);
-        const finalDayFiveDate = days[dayFiveDate.getDay()];
-        const dayFiveDescription = filteredList[4].weather[0].description;
-        const dayFiveDescpIcon = filteredList[4].weather[0].icon;
-        const dayFiveTemp = filteredList[4].main.temp.toFixed(1);  
-    
-        document.getElementById("day5Day").innerHTML = `${finalDayFiveDate}`;
-        document.getElementById("day5Type").innerHTML = `${dayFiveDescription}`;
-        document.getElementById("day5Icon").src = `./images/${dayFiveDescpIcon}.png`;
-        document.getElementById("day5Temp").innerHTML = `${dayFiveTemp}&#176`;
+            const icon = filteredList[i].weather[0].icon;
+            document.getElementById(`day${i+1}Icon`).src = `./images/${icon}.png`;
+        };
     });
 };
 
