247fc0a242a716890cc659f53ddf00fec2eaa229
JOHANNA REXIN <johanna.rexin@gmail.com>
2020-09-26 16:03:49 +0200

    Tried solving the sunrise/sunset

--- code/script.js
+++ code/script.js
@@ -5,8 +5,8 @@ const apiFiveDaysForecast = 'https://api.openweathermap.org/data/2.5/forecast?q=
 //Weather Variables
 const city = document.getElementById('city');
 const weatherDescription = document.getElementById('weatherDescription');
-const sunset = document.getElementById('sunset');
 const sunrise = document.getElementById('sunrise');
+const sunset = document.getElementById('sunset');
 const fiveDays = document.getElementById('fiveDays');
 const temperatures = document.getElementById('temperatures');
 
@@ -15,15 +15,30 @@ fetch(apiStockholm)
         return response.json();
 })
 
-// City name
 .then((weatherObject) => {
     city.innerHTML = weatherObject.name;
     console.log(weatherObject);
-    weatherDescription.innerHTML = weatherObject.weather[0].description
+    weatherDescription.innerHTML = weatherObject.weather[0].description.toUpperCase()
     temperatures.innerHTML = weatherObject.main.temp.toFixed(0.5)
-    sunrise.innerHTML = weatherObject.sys.sunrise
-    sunset.innerHTML = weatherObject.sys.sunset
-    console.log(weatherObject)
+    // sunrise.innerHTML = weatherObject.sys.sunrise
+    // sunset.innerHTML = weatherObject.sys.sunset
+
+    let sunrise = weatherObject.sys.sunrise;
+    let sunriseDate = new Date(sunrise*1000);
+
+    sunrise.innerHTML = `<h2> Sunrise: ${sunriseDate} </h2>`
+
+    let sunset = weatherObject.sys.sunset;
+    let sunsetDate = new Date(sunset*1000);
+
+    sunset.innerHTML = `<h2> Sunset:${sunsetDate} </h2>`
+    console.log()
+})
+
+
+
+
+
 
 // fetch(apiFiveDaysForecast)
 //     .then((response) => {
@@ -33,4 +48,3 @@ fetch(apiStockholm)
 // // Five days forecast
 // .then((weatherObject) => {
 //     fiveDays.innerHTML = weatherObject.list.dt
-})
