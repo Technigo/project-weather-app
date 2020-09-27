1b1af860edcd16faf2241c8b1354db3a0a634787
Hanna Skeppe <hannaskeppe@gmail.com>
2020-09-27 22:53:53 +0200

    One last push. I changed the vw for weather today on very small mobile screens.

--- script.js
+++ script.js
@@ -139,7 +139,7 @@ const fetchWeatherForecast = () => {
     })
     .then((weatherForecast) => {
       const filteredForecast = weatherForecast.list.filter((item) =>
-        item.dt_txt.replace(" ", "T").includes("12:00") //Filters out the 12 o'clock temps in weather forecast. This becomes a new array with objects. Tried to solve invalid date on iphone by adding T to date-format.
+        item.dt_txt.replace(" ", "T").includes("12:00") //Filters out the 12 o'clock temps in weather forecast. This becomes a new array with objects. Tried to solve invalid date on iphone by adding T to date-format. Not sure how to fix it. Looked on SO and found this, but don't know if I applied it correctly, because it doesn't work.
       );
       console.log(filteredForecast);
 
