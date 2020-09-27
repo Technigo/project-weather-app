9fe426465bfd9c9792522bca7dee98ac3ab56774
emma-tech <emma.urman@gmail.com>
2020-09-27 22:47:16 +0200

    Finished blue level requirements. Finished styling. Changed icons. Added favicon. Finished functionality for fiveday forecast.

---
+++ code /api.js
@@ -0,0 +1,1 @@
+export const apiKey = '277afbd3cf32e0e8cc059dd7cb8bcb95';
--- code /script.js
+++ code /script.js
@@ -1,9 +1,9 @@
 // SCRIPT FOR WEATHER APP
-//import {apiKey} from './key.js';
+import {apiKey} from './api.js';
 
-const apiKey = '277afbd3cf32e0e8cc059dd7cb8bcb95';
 const weatherUrl = `http://api.openweathermap.org/data/2.5/weather?q=Jönköping,Sweden&units=metric&APPID=${apiKey}`;
 const forecastUrl = `http://api.openweathermap.org/data/2.5/forecast?q=Jönköping,Sweden&units=metric&APPID=${apiKey}`;
+const weatherImage = document.getElementById('weatherImage');
 
 
 // Script for todays weather in Jönköping
@@ -13,13 +13,14 @@ fetch(weatherUrl)
      .then((response) => {
          return response.json();
      })
-     .then((weatherInfo) => {
-        todaysWeather.innerHTML = weatherInfo.name;
-        
-
+     .then((weatherInfo) => {        
+        const weatherImageID = weatherInfo.weather[0].icon;
+        weatherImage.src = `./img/${weatherImageID}.png`;
+    
         weatherInfo.weather.forEach((currentWeather) => {
-            todaysWeather.innerHTML += `<p>The weather in ${weatherInfo.name}: ${currentWeather.main}, ${currentWeather.description} ${currentWeather.icon}</p>`
-            todaysWeather.innerHTML += `<p>Temperature: ${weatherInfo.main.temp.toFixed(1)} °c (feels like: ${weatherInfo.main.feels_like.toFixed(1)} °c)</p>`
+            todaysWeather.innerHTML += `<h1> ${weatherInfo.name} </h1>`
+            todaysWeather.innerHTML += `<p>Current weather: ${currentWeather.main} | ${currentWeather.description}</p>`
+            todaysWeather.innerHTML += `<p>Temperature: ${weatherInfo.main.temp.toFixed(1)} °C | Feels like: ${weatherInfo.main.feels_like.toFixed(1)} °C</p>`
 
             const sunrise = new Date(weatherInfo.sys.sunrise * 1000);
             const sunriseTime = (sunrise.toLocaleTimeString([], { hour: '2-digit', minute: '2-digit' }));
@@ -32,12 +33,33 @@ fetch(weatherUrl)
         });
     });
 
-    // Functionality for five day forecast 
+
+    // Script for five day forecast, Jönköping
+    const forecastContainer = document.getElementById('forecastContainer');
+
     fetch(forecastUrl)
     .then((response) => {
         return response.json();
     })
     .then((json) => {
-        const filteredForecast = json.list.filter(item => item.dt_txt.includes('12:00'))
-        console.log(json);
-    });
+        const filteredForecast = json.list.filter(item => item.dt_txt.includes('12:00'));
+        filteredForecast.forEach((forecastDay) => {
+            forecastContainer.innerHTML += forecastInfo(forecastDay);
+        });
+    })
+    
+    const forecastInfo = day => {
+        const weekdayUnix = day.dt;
+        const weekdayLong = new Date(weekdayUnix * 1000);
+        const newWeekday = weekdayLong.toLocaleDateString('en-US', {weekday: 'long'});
+    
+        //Get weather description
+        const descriptionForecast = day.weather[0].description; 
+        
+        //Get temperature
+        const maxTemp = day.main.temp_max.toFixed();
+        
+        let forecastContent = ''
+        forecastContent += `<p class="forecast-day">${newWeekday} | ${descriptionForecast} | ${maxTemp}°C</p>`;
+        return forecastContent;
+        };
