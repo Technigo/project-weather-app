95a7847ee9d1634b4837697a481b019f5981fd55
sannadw <sanna.dolck@gmail.com>
2020-09-27 16:26:03 +0200

    The images are working

--- script.js
+++ script.js
@@ -9,57 +9,61 @@ fetch(API_URL)
         return response.json()
     })
     .then((json) => {
-        // const weatherContainer = document.getElementById('weather');
-        // weatherContainer.innerHTML = `<h1>${json.name}</h1>`
-
         const cityContainer = document.getElementById('city');
         cityContainer.innerHTML = `<h1>Light a fire and get cosy. ${json.name} is looking grey today.</h1>`
-       
+
         const weatherContainer = document.getElementById('weather')
         json.weather.forEach((weather) => {
+            console.log("weather");
             const sunrise = new Date(json.sys.sunrise * 1000);
-            const sunRiseTime = sunrise.toLocaleTimeString ('se-SV', { hour : '2-digit', minute : '2-digit'});
+            const sunRiseTime = sunrise.toLocaleTimeString('se-SV', { hour: '2-digit', minute: '2-digit' });
             const sunset = new Date(json.sys.sunset * 1000);
-            const sunSetTime = sunset.toLocaleTimeString ('se-SV', { hour : '2-digit', minute : '2-digit'});
-            
-            weatherContainer.innerHTML += `<p>${weather.description} | ${json.main.temp.toFixed(1)}° </p>`
+            const sunSetTime = sunset.toLocaleTimeString('se-SV', { hour: '2-digit', minute: '2-digit' });
+
+            // ELLER SKA DET VARA WEATHER.DESCRIPTION? istället för main? 
+            weatherContainer.innerHTML += `<p>${weather.main.toLowerCase()} | ${json.main.temp.toFixed(1)}° </p>`
             weatherContainer.innerHTML += `<p>sunrise ${sunRiseTime}</p>`
             weatherContainer.innerHTML += `<p>sunset ${sunSetTime}</p>`
-       
-// const weatherType = weather.weather[0].main
-// const setWeatherColors = () => {
-//     if  (weatherType === 'Clouds') {
-//         weatherContainer.classList.add(cloud);
-//         weatherContainer.classList.remove(sunny);
-//         weatherContainer.classList.remove(rain);
-//     } else if (weatherType === 'Clear') {
-//         weatherContainer.classList.add(sunny);
-//         weatherContainer.classList.remove(cloud);
-//         weatherContainer.classList.remove(rain);   
-//     } else (weatherType === 'Rain') || (weather.main === 'Drizzle')
-//          weatherContainer.classList.add(rain);
-//          weatherContainer.classList.remove(sunny);
-//          weatherContainer.classList.remove(cloud);
-            
-//             }
-       
-       
+
+            // WEATHER IMAGE DEPENDING ON WEATHER
+            const cloudImage = document.getElementById('cloud-image')
+            const sunImage = document.getElementById('sunglasses-image')
+            const rainImage = document.getElementById('umbrella-image')
+            const weatherType = weather.main
+            const setWeatherImages = () => {
+                if (weatherType === 'Clouds') {
+                    cloudImage.classList.add("visible");
+                    cloudImage.classList.remove("hidden");
+                    sunImage.classList.add("hidden");
+                    sunImage.classList.remove("visible");
+                    rainImage.classList.add("hidden");
+                    rainImage.classList.remove("visible");
+                } else if (weatherType === 'Clear') {
+                    sunImage.classList.add("visible");
+                    sunImage.classList.remove("hidden");
+                    cloudImage.classList.add("hidden");
+                    cloudImage.classList.remove("visible");
+                    rainImage.classList.add("hidden");
+                    rainImage.classList.remove("visible");
+                } else if (weatherType === 'Rain' || weather.main === 'Drizzle') {
+                    rainImage.classList.add("visible");
+                    rainImage.classList.remove("hidden");
+                    sunImage.classList.add("hidden");
+                    sunImage.classList.remove("visible");
+                    cloudImage.classList.add("hidden");
+                    cloudImage.classList.remove("visible");
+                }
+            }
+
+
+            setWeatherImages()
+
         });
 
-     }) 
-    
-
-// const div = document.createElement('div');
-// div.className = 'sunglasses-image';
-
-// div.classList.remove("forecast-ul");
-// div.classList.add("sunglasses-image");
-
-// console.log(div.outerHTML);
+    })
 
 
 
-     
 
 fetch(API_URL_5DAYS)
     .then((response) => {
@@ -68,7 +72,7 @@ fetch(API_URL_5DAYS)
     .then((json) => {
         const forecastUl = document.getElementById('forecast');
         const filteredForecastMidDay = json.list.filter(item => item.dt_txt.includes('12:00'));
-        
+
         // Didn't need all this code since todays date isn't showing
         // const dateToday = new Date();
         // const dayOfMonth = dateToday.getDate();
@@ -83,8 +87,6 @@ fetch(API_URL_5DAYS)
             const options = { weekday: 'short' };
             const weekdayString = new Intl.DateTimeFormat('en-US', options).format(forecastDate);
 
-           forecastUl.innerHTML += `<li>${weekdayString} ${forecast.main.temp.toFixed(0)}°</li>`     
+            forecastUl.innerHTML += `<li>${weekdayString} ${forecast.main.temp.toFixed(0)}°</li>`
         });
-     }) 
-
-  
+    }) 
