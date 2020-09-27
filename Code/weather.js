f977788ac6d4769ee51121ca25e036452d9a4faf
Therese <64402455+Thessan@users.noreply.github.com>
2020-09-27 21:30:14 +0200

    updated styling mobile version, not finsihed

--- Code/weather.js
+++ Code/weather.js
@@ -7,27 +7,29 @@ fetch('http://api.openweathermap.org/data/2.5/weather?q=Nanaimo,CA&units=metric&
         // city, country, current temperature & weather description
         const cityCountry = document.getElementById('city-country');
         const currentTemp = document.getElementById('current-temp');
-       
+        const description = document.getElementById('description');
 
         cityCountry.innerHTML = `${json.name}, ${json.sys.country}`;
-        currentTemp.innerHTML = `${json.main.temp}°C / ${json.weather[0].description}`;
+        currentTemp.innerHTML = `${json.main.temp}°C`;
+        description.innerHTML = `${json.weather[0].description}`;
         
 
         // sunrise  
         const sunrise = document.getElementById('sunrise');
-        const sunriseValue = json.sys.sunrise
+        const sunriseValue = json.sys.sunrise;
         const sRise = new Date(sunriseValue * 1000); // multiple with 1000 since the data is given in seconds and JS uses milliseconds
         const sunriseHour = sRise.toLocaleTimeString('en-US', {hour: '2-digit', minute:'2-digit', hour12: false});
             
-        sunrise.innerHTML = `Sunrise: ${sunriseHour}`;
+        sunrise.innerHTML = `<img src="./Images/sunrise.png" style="float: left;"> Sunrise: ${sunriseHour}`;
 
+        
         // sunset
         const sunset = document.getElementById('sunset');
         const sunsetValue = json.sys.sunset
         const sSet = new Date(sunsetValue * 1000);
         const sunsetHour = sSet.toLocaleTimeString('en-US', {hour: '2-digit', minute:'2-digit', hour12: false,});
 
-        sunset.innerHTML = `Sunset: ${sunsetHour}`;
+        sunset.innerHTML = `<img src="./Images/sunset.png" style="float: right;">Sunset: ${sunsetHour}`;
         
     });
 
