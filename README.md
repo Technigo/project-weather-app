572da8c678f5fb315bed3fe0fa500a98263ac194
emma-tech <emma.urman@gmail.com>
2020-09-27 23:03:10 +0200

    Added some last comments. Updated readme.

--- README.md
+++ README.md
@@ -4,7 +4,9 @@ This weeks assignment was to build a simple weather web app that shows today's w
 
 ## The problem
 
-Describe how you approached to problem, and what tools and techniques you used to solve it. How did you plan? What technologies did you use? If you had more time, what would be next?
+Firstly I searched for icons and started out with the variables for the key and url. After that I proceeded to fetch the data for the current weather in Jönköping. I got stuck in trying to get the app to show multiple cities, so I went back to showing only one city in the end. I will try to make the app more flexible with some kind of search-function when I have the time.  
+
+After fetching data for current weather and displaying it on the html-page  I went back to the weather forecast. This was a challenge because I hadn't really decided beforehand on how I wanted to display the data. Filtering the list-array made things easier and I'm happy with the result!
 
 ## View it live
 
--- code /script.js
+++ code /script.js
@@ -9,20 +9,21 @@ const weatherImage = document.getElementById('weatherImage');
 // Script for todays weather in Jönköping
 const todaysWeather = document.getElementById('todaysWeather');
 
-fetch(weatherUrl)
+fetch(weatherUrl) //Fetching data
      .then((response) => {
-         return response.json();
+         return response.json(); //Returning json
      })
-     .then((weatherInfo) => {        
+     .then((weatherInfo) => { //Fetching weather icon, but changing them to other choosen icons
         const weatherImageID = weatherInfo.weather[0].icon;
         weatherImage.src = `./img/${weatherImageID}.png`;
     
-        weatherInfo.weather.forEach((currentWeather) => {
+        weatherInfo.weather.forEach((currentWeather) => { // Showing content in index.html
             todaysWeather.innerHTML += `<h1> ${weatherInfo.name} </h1>`
             todaysWeather.innerHTML += `<p>Current weather: ${currentWeather.main} | ${currentWeather.description}</p>`
             todaysWeather.innerHTML += `<p>Temperature: ${weatherInfo.main.temp.toFixed(1)} °C | Feels like: ${weatherInfo.main.feels_like.toFixed(1)} °C</p>`
 
-            const sunrise = new Date(weatherInfo.sys.sunrise * 1000);
+            //Functionality for showing time for sunrise and sunset in the correct way
+            const sunrise = new Date(weatherInfo.sys.sunrise * 1000); 
             const sunriseTime = (sunrise.toLocaleTimeString([], { hour: '2-digit', minute: '2-digit' }));
             todaysWeather.innerHTML += `<p>Sunrise: ${sunriseTime} </p>`
 
@@ -37,29 +38,29 @@ fetch(weatherUrl)
     // Script for five day forecast, Jönköping
     const forecastContainer = document.getElementById('forecastContainer');
 
-    fetch(forecastUrl)
+    fetch(forecastUrl) //Fetching data
     .then((response) => {
-        return response.json();
+        return response.json(); //Returning json
     })
-    .then((json) => {
+    .then((json) => { //Filtering the list array in weather forecast
         const filteredForecast = json.list.filter(item => item.dt_txt.includes('12:00'));
         filteredForecast.forEach((forecastDay) => {
             forecastContainer.innerHTML += forecastInfo(forecastDay);
         });
     })
     
-    const forecastInfo = day => {
+    const forecastInfo = day => { // Function to get the weekdays to show in the right format
         const weekdayUnix = day.dt;
         const weekdayLong = new Date(weekdayUnix * 1000);
         const newWeekday = weekdayLong.toLocaleDateString('en-US', {weekday: 'long'});
     
-        //Get weather description
+        //Get forecast weather description
         const descriptionForecast = day.weather[0].description; 
         
-        //Get temperature
+        //Get forecast temperature
         const maxTemp = day.main.temp_max.toFixed();
         
-        let forecastContent = ''
+        let forecastContent = '' // Showing content in index.html
         forecastContent += `<p class="forecast-day">${newWeekday} | ${descriptionForecast} | ${maxTemp}°C</p>`;
         return forecastContent;
         };
