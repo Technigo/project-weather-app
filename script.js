84af792a20a5797c66af0b069523c96ea607bc3d
Sara Stjernberg <sara_stjernberg@hotmail.com>
2020-09-28 20:35:46 +0200

    search by city

--- script.js
+++ script.js
@@ -1,32 +1,35 @@
 import { API_KEY } from './api.js';
 
-const weatherbase = "https://api.openweathermap.org/data/2.5/";
-const forecastUrl = `https://api.openweathermap.org/data/2.5/forecast?q=Kaxholmen,Sweden&units=metric&APPID=${API_KEY}`;
-const searchbox = document.querySelector('#search');
+const input = document.querySelector('#search');
+let cityName = '';
 
-const setQuery = (evt) => {
-    if (evt.keyCode == 13) {
-        getResults(searchbox.value);
-        console.log(searchbox.value);
+input.addEventListener('keypress', function(e) {
+    if (e.keyCode === 13) {
+        const inputValue = input.value;
+        cityName = inputValue;
+        const API_URL_FORECAST = `https://api.openweathermap.org/data/2.5/forecast?q=${cityName}&units=metric&APPID=${API_KEY}`;
+        const API_URL_TODAY = `https://api.openweathermap.org/data/2.5/weather?q=${cityName}&units=metric&APPID=${API_KEY}`;
+        getWeatherToday(API_URL_TODAY);
+        getWeatherForecast(API_URL_FORECAST);
+
     }
-}
+});
 
-searchbox.addEventListener('keypress', setQuery);
 
-function getResults(query) {
-    fetch(`${weatherbase}weather?q=${query}&units=metric&appid=${API_KEY}`)
-        .then((response) => {
-            return response.json();
-        })
-        .then((currentWeather) => {
-            console.log(currentWeather);
-            upDateWeather(currentWeather);
 
-        });
-}
+const getWeatherToday = (url) => {
+        fetch(url)
+            .then((response) => {
+                return response.json();
+            })
+            .then((currentWeather) => {
+                console.log(currentWeather);
+                upDateWeather(currentWeather);
+
+            });
+    }
+    //Get today's weather andupdate temperature, weathertype and icon
 const upDateWeather = (currentWeather) => {
-    console.log(currentWeather);
-    //update temperature, weathertype and icon
     let city = document.getElementById('name');
     city.innerHTML = `${currentWeather.name}, ${currentWeather.sys.country}`;
     document.getElementById('degrees').innerHTML = `${currentWeather.main.temp.toFixed(1)} °C`;
@@ -45,26 +48,8 @@ const upDateWeather = (currentWeather) => {
     const sunsetTimeString = sunsetTime.toLocaleTimeString('sv-SE', { timestyle: 'long', hour: "2-digit", minute: "2-digit" });
     document.getElementById('sunset').innerHTML += sunsetTimeString;
 
-    setBackground(description);
 }
 
-const setBackground = (description) => {
-    const wrapper = document.getElementById('wrapper');
-
-    if (description === 'Clear') {
-        wrapper.style.backgroundImage = 'url(./img/clear.jpg)';
-    } else if (description === 'Clouds') {
-        wrapper.style.backgroundImage = 'url(./img/clouds.jpg)';
-    } else if (description === 'Rain' || description === 'Drizzle') {
-        wrapper.style.backgroundImage = 'url(./img/rain.jpg)';
-    } else if (description === 'Thunderstorm') {
-        wrapper.style.backgroundImage = 'url(./img/thunder.jpg)';
-    } else if (description === 'Snow') {
-        wrapper.style.backgroundImage = 'url(./assets/snow.jpg)';
-    } else if (description === 'Mist' || description === 'Fog') {
-        wrapper.style.backgroundImage = 'url(./img/fog.jpg)';
-    }
-};
 
 //get the name of the day
 const weekDays = () => {
@@ -72,7 +57,7 @@ const weekDays = () => {
     const d = new Date();
     const dayName = days[d.getDay()];
     return dayName;
-}
+};
 
 const date = document.getElementById('day')
 date.innerHTML = weekDays();
@@ -83,26 +68,28 @@ const today = () => {
     const current_datetime = new Date()
     const formatted_date = `${current_datetime.getDate()} ${months[current_datetime.getMonth()]} ${current_datetime.getFullYear()}`;
     return `  ${formatted_date}`;
-}
+};
 
 date.innerHTML += today();
 
+//get five days forecast
+const getWeatherForecast = (url) => {
+    fetch(url)
+        .then((response) => {
+            return response.json();
+        })
+        .then((forecastArray) => {
+            const filteredArray = forecastArray.list.filter(item => item.dt_txt.includes('12:00'));
+            console.log(filteredArray);
 
-fetch(forecastUrl)
-    .then((response) => {
-        return response.json();
-    })
-    .then((forecastArray) => {
-        const filteredArray = forecastArray.list.filter(item => item.dt_txt.includes('12:00'));
-        console.log(filteredArray);
+            const forecast = filteredArray.map((forecast) => {
+                const day = (new Date(forecast.dt * 1000)).toLocaleDateString("en-US", { weekday: "long" })
+                const temperature = (forecast.main.temp).toFixed(1);
+                const weatherType = forecast.weather[0].description
 
-        const forecast = filteredArray.map((forecast) => {
-            const day = (new  Date(forecast.dt  *  1000)).toLocaleDateString("en-US",   {  weekday:   "long" })
-            const temperature = (forecast.main.temp).toFixed(1);
-            const weatherType = forecast.weather[0].description
-
-            document.getElementById('days').innerHTML += `<p>${day} &nbsp</p>`;
-            document.getElementById('temp').innerHTML += `<p>${temperature} °C  &nbsp &nbsp</p>`;
-            document.getElementById('description').innerHTML += `<p>${weatherType}</p>`;
+                document.getElementById('days').innerHTML += `<p>${day} &nbsp</p>`;
+                document.getElementById('temp').innerHTML += `<p>${temperature} °C  &nbsp &nbsp</p>`;
+                document.getElementById('description').innerHTML += `<p>${weatherType}</p>`;
+            });
         });
-    });
+}
