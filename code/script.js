ded17504abbfc6e17fd3e0d22773dd294e581e1c
Annika Melin <melin.anki>
2020-09-29 20:09:10 +0200

    updated and cleaned functions

--- code/script.js
+++ code/script.js
@@ -9,7 +9,6 @@ const sunriseElement = document.getElementById("sunrise")
 const sunsetElement = document.getElementById("sunset")
 const forecastTableElement = document.getElementById("forecastTable")
 
-
 fetch(weatherUrl)
   .then(response => {
     return response.json()
@@ -17,15 +16,13 @@ fetch(weatherUrl)
   .then(json => {
     const weather = {
       city: json.name,
-      temp: json.main.temp,
-      iconId: json.weather[0].icon,
-      description: json.weather[0].description,
-      sunrise: json.sys.sunrise,
-      sunset: json.sys.sunset
+      temp: Math.round(json.main.temp),
+      icon: getIconUrl(json.weather[0].icon),
+      description: json.weather[0].description.charAt(0).toUpperCase() + json.weather[0].description.slice(1),
+      sunrise: convertToTime(json.sys.sunrise),
+      sunset: convertToTime(json.sys.sunset)
     }
-
-    const modifiedWeather = modifyWeather(weather)
-    displayWeather(modifiedWeather)
+    displayWeather(weather)
   })
   .catch(error => {
     console.log("Error fetching weather data", error)
@@ -38,21 +35,12 @@ fetch(forecastUrl)
   })
   .then(json => {
     const filteredForecasts = json.list.filter(item => item.dt_txt.includes("12:00"))
-
     const forecasts = filteredForecasts.map(item => {
-      const day = item.dt
-      const iconId = item.weather[0].icon
-      const temp = item.main.temp
-      return {
-        day, iconId, temp
-      }
+      const day = convertToDay(item.dt)
+      const icon = getIconUrl(item.weather[0].icon)
+      const temp = Math.round(item.main.temp)
+      return { day, icon, temp }
     })
-
-    // const modifiedForecasts = forecasts.forEach(forecast => {
-    //   modifyForecast(forecast)
-    // })
-    // displayForecasts(modifiedForecasts)
-
     displayForecast(forecasts)
   })
   .catch(error => {
@@ -60,34 +48,14 @@ fetch(forecastUrl)
     // alert("Forecast data error")
   })
 
-const modifyWeather = weather => {
-  const city = weather.city
-  const temp = Math.round(weather.temp)
-  const iconUrl = getIconUrl(weather.iconId)
-  const description = weather.description.charAt(0).toUpperCase() + weather.description.slice(1)
-
-  const time = convertTime(weather.sunrise, weather.sunset)
-  const sunrise = time.sunriseTime
-  const sunset = time.sunsetTime
-
-  return { city, temp, iconUrl, description, sunrise, sunset }
-}
-
 const displayWeather = weather => {
   cityElement.innerHTML = weather.city
   tempElement.innerHTML = `${weather.temp}°`
-  iconElement.innerHTML = `<img src='${weather.iconUrl}'>`
+  iconElement.innerHTML = `<img src='${weather.icon}'>`
   descriptionElement.innerHTML = weather.description
   sunriseElement.innerHTML = `Sunrise: ${weather.sunrise} `
   sunsetElement.innerHTML = `Sunset: ${weather.sunset} `
 }
-
-// const modifyForecast = forecast => {
-//   const day = handleDay(forecast.day)
-//   const iconUrl = getIconUrl(forecast.iconId)
-//   const temp = Math.round(forecast.temp)
-//   return { day, iconUrl, temp }
-// }
 
 const displayForecast = forecasts => {
   forecasts.forEach((forecast) => {
@@ -96,37 +64,17 @@ const displayForecast = forecasts => {
 }
 
 const generateHTML = forecast => {
-  const day = convertDay(forecast.day)
-
-  const iconUrl = getIconUrl(forecast.iconId)
-  const temp = Math.round(forecast.temp)
-
   let forecastHTML = ""
   forecastHTML += `<li class="forecast-row">`
-  forecastHTML += `<p>${day}</p> `
-  forecastHTML += `<div><img src='${iconUrl}'></div > `
-  forecastHTML += `<p class="temp">${temp}°</p > `
+  forecastHTML += `<p>${forecast.day}</p> `
+  forecastHTML += `<div><img src='${forecast.icon}'></div > `
+  forecastHTML += `<p class="temp">${forecast.temp}°</p > `
   forecastHTML += `</li> `
-  return (forecastHTML)
+  return forecastHTML
 }
 
-const convertTime = (sunrise, sunset) => {
-  const sunriseDate = new Date(sunrise * 1000)
-  const sunsetDate = new Date(sunset * 1000)
-  const options = { hour: "2-digit", minute: "2-digit" }
-  const sunriseTime = sunriseDate.toLocaleTimeString([], options)
-  const sunsetTime = sunsetDate.toLocaleTimeString([], options)
-  return time = { sunriseTime, sunsetTime }
-}
+const convertToTime = timeStamp => (new Date(timeStamp * 1000)).toLocaleTimeString([], options = { hour: "2-digit", minute: "2-digit" })
 
-const convertDay = day => {
-  const forecastDate = new Date(day * 1000)
-  const options = { weekday: "short" }
-  const forecastDay = forecastDate.toLocaleString("en-GB", options)
-  return forecastDay
-}
+const convertToDay = timeStamp => (new Date(timeStamp * 1000)).toLocaleString("en-GB", options = { weekday: "short" })
 
-const getIconUrl = iconId => {
-  const iconUrl = `https://openweathermap.org/img/wn/${iconId}@2x.png`
-  return iconUrl
-}
+const getIconUrl = iconId => `https://openweathermap.org/img/wn/${iconId}@2x.png`
