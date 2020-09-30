d2f411a9af9a85efbbfa02c52baa2031caee95c0
Annika Melin <melin.anki>
2020-09-30 05:53:24 +0200

    cleaned js code

--- code/script.js
+++ code/script.js
@@ -17,10 +17,10 @@ fetch(weatherUrl)
     const weather = {
       city: json.name,
       temp: Math.round(json.main.temp),
-      icon: getIconUrl(json.weather[0].icon),
+      icon: `https://openweathermap.org/img/wn/${json.weather[0].icon}@2x.png`,
       description: json.weather[0].description.charAt(0).toUpperCase() + json.weather[0].description.slice(1),
-      sunrise: convertToTime(json.sys.sunrise),
-      sunset: convertToTime(json.sys.sunset)
+      sunrise: (new Date(json.sys.sunrise * 1000)).toLocaleTimeString([], options = { hour: "2-digit", minute: "2-digit" }),
+      sunset: (new Date(json.sys.sunset * 1000)).toLocaleTimeString([], options = { hour: "2-digit", minute: "2-digit" })
     }
     displayWeather(weather)
   })
@@ -34,14 +34,15 @@ fetch(forecastUrl)
     return response.json()
   })
   .then(json => {
-    const filteredForecasts = json.list.filter(item => item.dt_txt.includes("12:00"))
-    const forecasts = filteredForecasts.map(item => {
-      const day = convertToDay(item.dt)
-      const icon = getIconUrl(item.weather[0].icon)
+    const forecasts = json.list.filter(item => item.dt_txt.includes("12:00")).map(item => {
+      const day = (new Date(item.dt * 1000)).toLocaleString("en-GB", options = { weekday: "short" })
+      const icon = `https://openweathermap.org/img/wn/${item.weather[0].icon}@2x.png`
       const temp = Math.round(item.main.temp)
       return { day, icon, temp }
     })
-    displayForecast(forecasts)
+    forecasts.forEach(forecast => {
+      forecastTableElement.innerHTML += displayForecast(forecast)
+    })
   })
   .catch(error => {
     console.log("Error fetching forecast data", error)
@@ -57,24 +58,12 @@ const displayWeather = weather => {
   sunsetElement.innerHTML = `Sunset: ${weather.sunset} `
 }
 
-const displayForecast = forecasts => {
-  forecasts.forEach((forecast) => {
-    forecastTableElement.innerHTML += generateHTML(forecast)
-  })
-}
-
-const generateHTML = forecast => {
+const displayForecast = forecast => {
   let forecastHTML = ""
   forecastHTML += `<li class="forecast-row">`
   forecastHTML += `<p>${forecast.day}</p> `
-  forecastHTML += `<div><img src='${forecast.icon}'></div > `
+  forecastHTML += `<img src='${forecast.icon}'>`
   forecastHTML += `<p class="temp">${forecast.temp}°</p > `
   forecastHTML += `</li> `
   return forecastHTML
 }
-
-const convertToTime = timeStamp => (new Date(timeStamp * 1000)).toLocaleTimeString([], options = { hour: "2-digit", minute: "2-digit" })
-
-const convertToDay = timeStamp => (new Date(timeStamp * 1000)).toLocaleString("en-GB", options = { weekday: "short" })
-
-const getIconUrl = iconId => `https://openweathermap.org/img/wn/${iconId}@2x.png`
