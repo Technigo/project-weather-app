2a1fc49cc33d8beaa0e574371eb1a943c5e59cbc
elisa blomberg <elisa.blomberg@outlook.com>
2020-10-04 12:12:34 +0200

    Deleted a console.log in the JS-file

--- code/weather.js
+++ code/weather.js
@@ -57,7 +57,6 @@ fetch(apiCurrentWeather).then((response) => {
 const night = () => {
     const currentTime = new Date().getHours();
     if (currentTime >= 22 || currentTime <= 04) {
-        console.log(currentTime)
         document.getElementById('nightIcon').src = './icons/iconfinder_03_moon_sleepy_night_emoticon_weather_smiley_3375686.svg';
         weatherText.innerHTML +="<h2>Hello there Night Owl! Can't sleep? Cathing stars? Dancing?</h2>";
         var element = document.getElementById("weatherIconParent");
