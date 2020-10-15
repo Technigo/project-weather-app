d75d93964f7a9eafe4ea1b12eaf65beaedd3c02a
Olof Lindahl <olindahl@gmail.com>
2020-10-15 17:04:45 +0200

    Change requests fixed

--- code/weather.js
+++ code/weather.js
@@ -1,50 +1,55 @@
 //get location when page is loading
 
-window.onload = () =>  getLocation(); 
+window.onload = () => getLocation();
 
 //get location through browser
 
 const getLocation = () => {
   if (navigator.geolocation) {
     navigator.geolocation.getCurrentPosition(getCity);
-  } else { 
+  } else {
     console.log("Geolocation is not supported by this browser.");
   }
-
-}
+};
 
 //change city by user input
 
 const changeCity = () => {
-    let city = document.getElementById("city-label").value 
-    getLatLong(city)
-}
+  let city = document.getElementById("city-label").value;
+  getLatLong(city);
+};
 
 //this function is just used for getting the city name (reverse geolocation) to show to the user when using "current position"
 
 const getCity = (position) => {
-    const lat = position.coords.latitude
-    const lon = position.coords.longitude
-    fetch(`https://api.openweathermap.org/data/2.5/forecast?lat=${lat}&lon=${lon}&appid=7d01b328e34c450986cb7faef032a771`)
-    .then(response => response.json())
-        .then(forecast => {
-            document.getElementById("city").innerHTML = forecast.city.name
-            fetchForecast(lat, lon)
-    });
-}
+  const lat = position.coords.latitude;
+  const lon = position.coords.longitude;
+  fetch(
+    `https://api.openweathermap.org/data/2.5/forecast?lat=${lat}&lon=${lon}&appid=7d01b328e34c450986cb7faef032a771`
+  )
+    .then((response) => response.json())
+    .then((forecast) => {
+      document.getElementById("city").innerHTML = forecast.city.name;
+      fetchForecast(lat, lon);
+    })
+    .catch((err) => console.error(err));
+};
 
 //get coordinates when user change city (using One Call API which only takes coordinates as parameter)
 
 const getLatLong = (city) => {
-    fetch(`https://api.openweathermap.org/data/2.5/weather?q=${city},Sweden&units=metric&APPID=7d01b328e34c450986cb7faef032a771`)
-    .then(response => response.json())
-        .then(forecast => {
-            const lat = forecast.coord.lat
-            const lon = forecast.coord.lon
-            document.getElementById("city").innerHTML = forecast.name
-            fetchForecast(lat, lon)
-    });
-}
+  fetch(
+    `https://api.openweathermap.org/data/2.5/weather?q=${city},Sweden&units=metric&APPID=7d01b328e34c450986cb7faef032a771`
+  )
+    .then((response) => response.json())
+    .then((forecast) => {
+      const lat = forecast.coord.lat;
+      const lon = forecast.coord.lon;
+      document.getElementById("city").innerHTML = forecast.name;
+      fetchForecast(lat, lon);
+    })
+    .catch((err) => console.error(err));
+};
 
 //Main function for fetching forecast data
 
@@ -52,32 +57,22 @@ const fetchForecast = (lat, lon) => {
   return fetch(
     `https://api.openweathermap.org/data/2.5/onecall?lat=${lat}&lon=${lon}&%20exclude=current,minutely,hourly&appid=7d01b328e34c450986cb7faef032a771&units=metric`
   )
-    .then((response) => {
-      return response.json();
-    })
+    .then((response) => response.json())
+    .catch((err) => console.error(err))
     .then((json) => {
       generateForecast(json);
     });
 };
 
 const getDayOfWeek = (dayOfWeek) => {
-    return isNaN(dayOfWeek)
-      ? null
-      : [
-          "Sun",
-          "Mon",
-          "Tue",
-          "Wed",
-          "Thu",
-          "Fri",
-          "Sat",
-        ][dayOfWeek];
-  };
+  return isNaN(dayOfWeek)
+    ? null
+    : ["Sun", "Mon", "Tue", "Wed", "Thu", "Fri", "Sat"][dayOfWeek];
+};
 
-//main function for generating the HTML 
+//main function for generating the HTML
 
-  const generateForecast = (forecast) => {
-  
+const generateForecast = (forecast) => {
   //get current temp
   const currentTemp = document.getElementById("current");
   currentTemp.innerHTML = `${Math.round(forecast.current.temp)}&#176`;
@@ -87,7 +82,7 @@ const getDayOfWeek = (dayOfWeek) => {
   currentIcon.src = `${forecast.current.weather[0].icon}@2x.png`;
 
   //get current weather
-  const currentWeather = document.getElementById("current-weather")
+  const currentWeather = document.getElementById("current-weather");
   currentWeather.innerHTML = forecast.current.weather[0].description;
 
   //get sunrise/sunset
@@ -113,16 +108,28 @@ const getDayOfWeek = (dayOfWeek) => {
   //Get new array with forecast to map in new array
   const slicedForecast = forecast.daily.slice(0, 5);
   //get array for HTML elements to loop through
-  const daysInForecast = document.getElementsByClassName("day")
+  const daysInForecast = document.getElementsByClassName("day");
 
   //loop through each day and set HTML
   slicedForecast.forEach((day, index) => {
-    daysInForecast[index].querySelector(".next-day").innerHTML = `${getDayOfWeek(new Date(day.dt * 1000).getDay())}`;
-    daysInForecast[index].querySelector(".day-temp").innerHTML = `<span class="wind-dir" style="transform: rotate(${day.wind_deg}deg)">&#8593</span>`;
-    daysInForecast[index].querySelector(".day-temp").innerHTML += `<span class="tablet">${Math.round(day.wind_speed)} m/s</span>`;
-    daysInForecast[index].querySelector(".day-temp").innerHTML += `<img src='${day.weather[0].icon}@2x.png'>   `;
-    daysInForecast[index].querySelector(".day-temp").innerHTML += `<span>${Math.round(day.temp.min)}&#176 / ${Math.round(day.temp.max)}&#176</span>`;
-  })
-}
-
-
+    daysInForecast[index].querySelector(
+      ".next-day"
+    ).innerHTML = `${getDayOfWeek(new Date(day.dt * 1000).getDay())}`;
+    daysInForecast[index].querySelector(
+      ".day-temp"
+    ).innerHTML = `<span class="wind-dir" style="transform: rotate(${day.wind_deg}deg)">&#8593</span>`;
+    daysInForecast[index].querySelector(
+      ".day-temp"
+    ).innerHTML += `<span class="data-text, tablet">${Math.round(
+      day.wind_speed
+    )} m/s</span>`;
+    daysInForecast[index].querySelector(
+      ".day-temp"
+    ).innerHTML += `<img src='${day.weather[0].icon}@2x.png'>   `;
+    daysInForecast[index].querySelector(
+      ".day-temp"
+    ).innerHTML += `<span class="data-text">${Math.round(
+      day.temp.min
+    )}&#176 / ${Math.round(day.temp.max)}&#176</span>`;
+  });
+};
