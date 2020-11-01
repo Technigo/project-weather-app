67e4e7eac2ae1e4bd5d21a5c614168ebc71570f6
caroline birgersson <caroline@birgerochblom.se>
2020-11-01 20:34:06 +0100

    changes after change request

--- code/weather.js
+++ code/weather.js
@@ -1,76 +1,83 @@
 //Weather from API
-const apiWeatherToday = 'https://api.openweathermap.org/data/2.5/weather?q=Stockholm,Sweden&units=metric&APPID=e33f1cc192401277e601a6aed3a82800'
-const apiForecast = 'https://api.openweathermap.org/data/2.5/forecast?q=Stockholm,Sweden&units=metric&APPID=e33f1cc192401277e601a6aed3a82800'
+const apiWeatherToday =
+  "https://api.openweathermap.org/data/2.5/weather?q=Stockholm,Sweden&units=metric&APPID=e33f1cc192401277e601a6aed3a82800";
+const apiForecast =
+  "https://api.openweathermap.org/data/2.5/forecast?q=Stockholm,Sweden&units=metric&APPID=e33f1cc192401277e601a6aed3a82800";
 
 //Catching id's from html
-const weatherDescription = document.getElementById('description')
-const weatherTemperature = document.getElementById('temperature')
-const weatherIcon = document.getElementById('icon')
-const weatherLocation = document.getElementById('location')
-const weatherSunrise = document.getElementById('sunrise')
-const weatherSunset = document.getElementById('sunset')
+const weatherDescription = document.getElementById("description");
+const weatherTemperature = document.getElementById("temperature");
+const weatherIcon = document.getElementById("icon");
+const weatherLocation = document.getElementById("location");
+const weatherSunrise = document.getElementById("sunrise");
+const weatherSunset = document.getElementById("sunset");
 
 // Fetch function
 const fetchWeather = () => {
   fetch(apiWeatherToday)
     .then((response) => {
-      return response.json()
-   })
-// Weather description + temp
-  .then((weather) => {
-    weatherLocation.innerHTML = weather.name;
-    weatherDescription.innerHTML = weather.weather[0].description
-    weatherTemperature.innerHTML = Math.round(weather.main.temp)
-
-// Sunrise
-  const weatherSunrise = () => {
-    const dateSunrise = new Date(weather.sys.sunrise * 1000);
-    const timeSunrise = dateSunrise.toLocaleTimeString([], {
-      hour: '2-digit',
-      minute: '2-digit',
+      return response.json();
     })
-    document.getElementById('sunrise').innerHTML = timeSunrise
-  }
-  weatherSunrise()
+    // Weather description + temp
+    .then((weather) => {
+      weatherLocation.innerHTML = weather.name;
+      weatherDescription.innerHTML = weather.weather[0].description;
+      weatherTemperature.innerHTML = Math.round(weather.main.temp);
 
-// Sunset
-  const weatherSunset = () => {
-    const dateSunset = new Date(weather.sys.sunset * 1000);
-    const timeSunset = dateSunset.toLocaleTimeString([], {
-      hour: '2-digit',
-      minute: '2-digit',
-    })
-    document.getElementById('sunset').innerHTML = timeSunset
-    }
-  weatherSunset()
- })
- 
-}
+      // Sunrise
+      const weatherSunrise = () => {
+        const dateSunrise = new Date(weather.sys.sunrise * 1000);
+        const timeSunrise = dateSunrise.toLocaleTimeString([], {
+          hour: "2-digit",
+          minute: "2-digit",
+        });
+        document.getElementById("sunrise").innerHTML = timeSunrise;
+      };
+      weatherSunrise();
+
+      // Sunset
+      const weatherSunset = () => {
+        const dateSunset = new Date(weather.sys.sunset * 1000);
+        const timeSunset = dateSunset.toLocaleTimeString([], {
+          hour: "2-digit",
+          minute: "2-digit",
+        });
+        document.getElementById("sunset").innerHTML = timeSunset;
+      };
+      weatherSunset();
+    });
+};
 
 fetchWeather();
 
 // Date catcher
 const todaysDate = new Date();
-  const dateFormatted = todaysDate.toLocaleDateString("en-EN", {year: 'numeric', month: 'short', day: 'numeric'})
-  document.getElementById("date").innerHTML = dateFormatted;
+const dateFormatted = todaysDate.toLocaleDateString("en-EN", {
+  year: "numeric",
+  month: "short",
+  day: "numeric",
+});
+document.getElementById("date").innerHTML = dateFormatted;
 
 // Forecast for the next 5 days
 fetch(apiForecast)
-  .then((Response) => { 
-    return Response.json()
+  .then((response) => {
+    return response.json();
   })
-  .then ((json) => {
-    const filteredForecast = json.list.filter(item =>
+  .then((json) => {
+    const filteredForecast = json.list.filter((item) =>
       item.dt_txt.includes("12:00")
-  )
+    );
 
-  filteredForecast.forEach(day => {
-    const date = new Date(day.dt * 1000)
-    const dayName = date.toLocaleDateString("en-EN", { weekday: "short" })
-    const dayTemp = day.main.temp;
-    const weekTemp = dayTemp.toFixed(0.1)
+    filteredForecast.forEach((day) => {
+      const date = new Date(day.dt * 1000);
+      const dayName = date.toLocaleDateString("en-EN", { weekday: "short" });
+      const dayTemp = day.main.temp;
+      const weekTemp = dayTemp.toFixed(0.1);
 
-    document.getElementById('weekday').innerHTML += `<p>${dayName.toLowerCase()}</p>`
-    document.getElementById('weekTemp').innerHTML += `<p>${weekTemp}°C</p>`
-  })
-})
+      document.getElementById(
+        "weekday"
+      ).innerHTML += `<p>${dayName.toLowerCase()}</p>`;
+      document.getElementById("weekTemp").innerHTML += `<p>${weekTemp}°C</p>`;
+    });
+  });
