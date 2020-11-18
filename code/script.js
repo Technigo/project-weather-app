ba7650addcc3aeb16ac2c898e9ebc7eb68a0949a
JOHANNA REXIN <johanna.rexin@gmail.com>
2020-11-18 10:32:18 +0100

    Latest version

--- code/script.js
+++ code/script.js
@@ -5,8 +5,7 @@ const apiFiveDaysForecast = 'https://api.openweathermap.org/data/2.5/forecast?q=
 //Weather Variables
 const city = document.getElementById('city');
 const weatherDescription = document.getElementById('weatherDescription');
-// const sunrise = document.getElementById('sunrise');
-// const sunset = document.getElementById('sunset');
+
 const fiveDays = document.getElementById('fiveDays');
 const temperatures = document.getElementById('temperatures');
 
@@ -19,13 +18,9 @@ fetch(apiStockholm)
     city.innerHTML = weatherObject.name;
     weatherDescription.innerHTML = weatherObject.weather[0].description.toUpperCase()
     temperatures.innerHTML = weatherObject.main.temp.toFixed(0.5)
-    
-    // I've tried to solve the sunrise/sunset to show hhh:mm but didn't suceed. 
-    // Have googled, asked in group chat and read every question I could find on Stack without understanding.
-    // Would very much like to get some help to understand this one.
 
 const weatherSunrise = () => {
-    const dateSunrise = new Date(json.sys.sunrise * 1000);
+    const dateSunrise = new Date(weatherObject.sys.sunrise * 1000);
     const timeSunrise = dateSunrise.toLocaleTimeString([], {
       hour: '2-digit',
       minute: '2-digit',
@@ -33,16 +28,32 @@ const weatherSunrise = () => {
   document.getElementById('sunrise').innerHTML = timeSunrise;
   };
     weatherSunrise();
-})
 
-// Don't know why I can't get the forecast "apifiveDaysForecast" to work. 
-// I believe I have done it in the exactly same way as with "apiStockholm".
+
+const weatherSunset = () => {
+  const dateSunset = new Date(weatherObject.sys.sunset * 1000);
+  const timeSunset = dateSunset.toLocaleTimeString([], {
+    hour: '2-digit',
+    minute: '2-digit',
+  });
+document.getElementById('sunset').innerHTML = timeSunset;
+};
+  weatherSunset();
+})
 
 fetch(apiFiveDaysForecast)
-    .then((response) => {
-        return response.json();
-})
+  .then((response) => {
+    return response.json();    
+  })
 
-// // Five days forecast
+  .then((weatherObject) => {  
+    console.log(weatherObject)
+    const filteredWeek = weatherObject.list.filter(item => item.dt_txt.includes('12:00'));
+    console.log(filteredWeek)
+    filteredWeek.forEach(weekday => {
 
-const filteredWeek = json.list.filter(item => item.dt_txt.includes('12:00'));
+      const temperature = weekday.main.temp;
+      const date = new Date(weekday.main.dt_text);
+
+    })
+  })
