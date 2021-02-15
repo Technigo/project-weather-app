1d507c8f41bd0b4efed68c488d2d7f366e54a791
Your Name <isabellamarkskog@live.se>
2021-02-15 20:21:32 +0100

    Updated the structure for the forecast fetch (step 2)

--- script.js
+++ script.js
@@ -1,4 +1,5 @@
 const apiUrl = 'http://api.openweathermap.org/data/2.5/weather?q=Kuala%20Lumpur&units=metric&appid=5527a65cb469b1aa0aae3017c8ea2460'
+const apiUrlForecast = 'http://api.openweathermap.org/data/2.5/forecast?q=Kuala%20Lumpur&units=metric&appid=5527a65cb469b1aa0aae3017c8ea2460'
 
 const summary = document.getElementById('summary')
 const mainInformation = document.getElementById('mainInformation')
@@ -12,19 +13,29 @@ fetch(apiUrl)
 
 })
 .then((json) => {
-  console.log(json)
+  // console.log(json)
 
   // SUMMARY
-  // 1. WEATHER (weather --> main/description)
+  // 1. WEATHER (weather --> description)
   // 2. TEMPERATURE (main --> temp) 
   // 3. SUNRISE (sys --> sunrise)
   // 4. SUNSET (sys --> sunset)
 
   //MAIN INFORMATION
+  // 1. LOCATION (name)
+  // SVG + descriptive text that changes depending on the weather
 
-  
+})
 
-  //FORECAST
 
+//FORECAST - own fetch function
+// Weather next 5 days + the same time
+fetch(apiUrlForecast)
+.then((response) => {
+  return response.json()
+
+})
+.then((json) => {
+  console.log(json)
 
 })
