5a8595ac1483db9dd4066830793dc421e08e1777
Maria Sparre <you@example.com>
2021-02-16 23:52:16 +0100

    icon fetch for weather working

--- code/app.js
+++ code/app.js
@@ -1,15 +1,19 @@
-const weatherUrl = "https://api.openweathermap.org/data/2.5/weather?q=Stockholm,Sweden&units=metric&APPID=f463a96f9ee6b3233c3a141a391ac3cf"
+const todaysWeatherUrl = "https://api.openweathermap.org/data/2.5/weather?q=Stockholm,Sweden&units=metric&APPID=f463a96f9ee6b3233c3a141a391ac3cf"
+const fiveDayForecastStockholm = "https://api.openweathermap.org/data/2.5/forecast?q=Stockholm,Sweden&appid=f463a96f9ee6b3233c3a141a391ac3cf"
+const fiveDayForecastBarcelona = "https://api.openweathermap.org/data/2.5/forecast?q=Barcelona,Spain&appid=f463a96f9ee6b3233c3a141a391ac3cf"
 const mainTemperature = document.getElementById('main-temperature');
 const cityName = document.getElementById('city-name');
 const sunrise = document.getElementById('sunrise');
 const sunset = document.getElementById('sunset');
-
+const fiveDayForecast = document.getElementById('five-day-forecast');
+const weatherDescription = document.getElementById('weather-description');
+const weatherIcon = document.getElementById('weather-icon');
 //api.openweathermap.org/data/2.5/weather?q={city name}&appid={f463a96f9ee6b3233c3a141a391ac3cf}const weatherUrl = "https://api.openweathermap.org/data/2.5/weather?q=Stockholm,Sweden&units=metric&APPID=f463a96f9ee6b3233c3a141a391ac3cf"
 
 //api.openweathermap.org/data/2.5/weather?q={city name}&appid={f463a96f9ee6b3233c3a141a391ac3cf}
 
 //Main fetch for Stockoholm weather
-fetch(weatherUrl)
+fetch(todaysWeatherUrl)
 
   .then((response) => {
       return response.json();
@@ -20,13 +24,48 @@ fetch(weatherUrl)
     mainTemperature.innerHTML = `${json.main.temp} ºC`;
     sunrise.innerHTML = `Sunrise: ${json.sunrise}`;
     sunset.innerHTML = `Sunset: ${json.sunset}`;
-    const sunriseValue = json.sys.sunrise; //Sunrise and Sunset times in UNIX
-    const sunsetValue = json.sys.sunset;
-    /* Multiply by 1000 because the data is given to us in UNIX which is in seconds, but Javascript uses milliseconds. */
-    const sun = new Date(sunriseValue * 1000);
-    const set = new Date(sunsetValue * 1000);
-    const sunriseHour = sun.toLocaleTimeString('en-US', {hour: '2-digit', minute:'2-digit', hour12: false,});
-    const sunsetHour = set.toLocaleTimeString('en-US', {hour: '2-digit', minute:'2-digit', hour12: false,});
-    sunrise.innerHTML = `Sunrise: ${sunriseHour}`;
-    sunset.innerHTML = `Sunset: ${sunsetHour}`;
+      const sunriseValue = json.sys.sunrise; //Sunrise and Sunset times in UNIX
+      const sunsetValue = json.sys.sunset;
+      /* Multiply by 1000 because the data is given to us in UNIX which is in seconds, but Javascript uses milliseconds. */
+      const sun = new Date(sunriseValue * 1000);
+      const set = new Date(sunsetValue * 1000);
+      const sunriseHour = sun.toLocaleTimeString('en-US', {hour: '2-digit', minute:'2-digit', hour12: false,});
+      const sunsetHour = set.toLocaleTimeString('en-US', {hour: '2-digit', minute:'2-digit', hour12: false,});
+      sunrise.innerHTML = `Sunrise: ${sunriseHour}`;
+      sunset.innerHTML = `Sunset: ${sunsetHour}`;
+      //weatherDescription.innerHTML +=`${json.weather[0].description}`;
+      weatherIcon.innerHTML += 
+      `<img src="https://openweathermap.org/img/wn/${json.weather[0].icon}@2x.png" alt="${json.weather[0].description} icon" />`
   })
+
+    // fetch(todaysWeatherUrl)
+
+  // .then((response) => {
+  //     return response.json();
+  // })
+  // .then((json) => {
+  //   weatherDescription.innerHTML =`${json.weather.description}`;
+  //   weatherIcon.innerHTML = `${json.weather.icon}`;
+  // })
+  //New fetch for 5 days forecast
+
+//   fetch(fiveDayForecastStockholm)
+//   .then((response) => {
+//     return response.json();
+// })
+//   .then((json) => {
+//     const filteredForecast = json.list.filter(item => item.dt_txt.includes('12:00'))
+//     filteredForecast.forEach((json.list) => {
+//     fiveDayForecast.innerHTML += forecast.innerHTML
+//     }
+//   })
+
+
+
+//playing around with getting the 5 days 
+
+// 
+//   const firstDayList = json.list.filter(item => item.dt_txt.includes("12:00"))
+//   //Whenever you call the API, just create an array of dateStrings and then, (where list is the array returned from the API and dateStringList is the array of dateStrings) var firstDayList = list.filter(item -> item.dt_text.includes(dateStringList[0]) 
+
+
