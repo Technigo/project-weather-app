c19811567639e3097c15701bac2ebc5b1ffa2da0
Your Name <isabellamarkskog@live.se>
2021-02-16 21:28:43 +0100

    Fetched data for the forecast section, added some innerHTML to the JS code

--- script.js
+++ script.js
@@ -1,11 +1,17 @@
+//API variables
 const apiUrl = 'http://api.openweathermap.org/data/2.5/weather?q=Kuala%20Lumpur&units=metric&appid=5527a65cb469b1aa0aae3017c8ea2460'
 const apiUrlForecast = 'http://api.openweathermap.org/data/2.5/forecast?q=Kuala%20Lumpur&units=metric&appid=5527a65cb469b1aa0aae3017c8ea2460'
+
 
 const summary = document.getElementById('summary')
 const mainInformation = document.getElementById('mainInformation')
 const icon = document.getElementById('icon')
 const infoText = document.getElementById('infoText')
 const forecast = document.getElementById('forecast')
+
+//Global variables
+let temperature
+
 
 fetch(apiUrl)
 .then((response) => {
@@ -16,24 +22,35 @@ fetch(apiUrl)
   // SUMMARY
   // 1. WEATHER (weather --> description)
   json.weather.map((weather) => {
-    console.log(weather.description)    //sätt en innerHTML
+    // console.log(weather.description)    //sätt en innerHTML
+    summary.innerHTML += `
+      <p>${weather.description}</p>`
   })
 
   // 2. TEMPERATURE (main --> temp) 
   // console.log(json.main.temp)  
-  let temperature = json.main.temp
-  console.log(temperature.toFixed(0))
+  temperature = json.main.temp
+  // console.log(temperature.toFixed(0))
+  summary.innerHTML += `
+    <p>${temperature.toFixed(0)}</p>`
 
   // 3. SUNRISE (sys --> sunrise)
-  console.log(json.sys.sunrise)
+  // console.log(json.sys.sunrise)
+  summary.innerHTML += `
+  <p>${json.sys.sunrise}</p>`
 
   // 4. SUNSET (sys --> sunset)
-  console.log(json.sys.sunset)
+  // console.log(json.sys.sunset)
+  summary.innerHTML += `
+  <p>${json.sys.sunset}</p>`
+
 
   //MAIN INFORMATION
-  // 1. LOCATION (name)
-  console.log(json.name)
   // SVG + descriptive text that changes depending on the weather
+  // 1. LOCATION (name)
+  // console.log(json.name)
+  mainInformation.innerHTML += `
+  <p>${json.name}</p>`
 
 })
 
@@ -47,5 +64,21 @@ fetch(apiUrlForecast)
 })
 .then((json) => {
   //// console.log(json)
+  //Data types: day, date, temperature, weather (description)
+  const filteredForecast = json.list.filter(item => item.dt_txt.includes('12:00'))
+  console.log(filteredForecast)
+
+  filteredForecast.map((temp) => {
+    console.log(temp.main.temp.toFixed(0))    
+  })
+
+  // WEATHER
+  // const weatherDesc = filteredForecast.filter(item => item.weather.description)
+  // console.log(weatherDesc)
+  // filteredForecast.map((weather) => {
+
+  //   // console.log(weather.weather.description)
+    
+  // })
 
 })
