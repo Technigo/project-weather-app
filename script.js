6ed34318ca16093aa96ee0722bb0be2d1135f737
Sara Carlstein <carlstein.sara@gmail.com>
2021-02-16 13:35:53 +0100

    step 4 done

--- script.js
+++ script.js
@@ -11,6 +11,9 @@ fetch (`https://api.openweathermap.org/data/2.5/weather?q=${cityName}&units=metr
         
         let cityTemp = data.main.temp.toFixed(1)
         let sunRise = new Date(data.sys.sunrise * 1000)
+        let sunSet =new Date(data.sys.sunset * 1000)
+
+        console.log(sunSet)
 
         const hourAndMinutes = (i) =>{
             if (i < 10) {
@@ -19,17 +22,20 @@ fetch (`https://api.openweathermap.org/data/2.5/weather?q=${cityName}&units=metr
             return i;
         }
         
-        let hour = hourAndMinutes(sunRise.getHours())
-        let minutes = hourAndMinutes(sunRise.getMinutes())
-        console.log(hour, minutes)
+        let hourSunRise = hourAndMinutes(sunRise.getHours())
+        let minutesSunRise = hourAndMinutes(sunRise.getMinutes())
+        // console.log(hour, minutes)
 
+        let hourSunSet = hourAndMinutes(sunSet.getHours())
+        let minutesSunSet = hourAndMinutes(sunSet.getMinutes())
 
         city.innerHTML += `<p> ${data.name}</p>`
         city.innerHTML += `<p> ${cityTemp}</p>`
         city.innerHTML += `<p> ${data.weather[0].description}</p>`
         // console.log(data.weather[0].description)
-        city.innerHTML += `<p> ${sunRise}</p>`
-        city.innerHTML += `<p>${hour}:${minutes}</p>`
+        // city.innerHTML += `<p> ${sunRise}</p>`
+        city.innerHTML += `<p>${hourSunRise}:${minutesSunRise}</p>`
+        city.innerHTML += `<p>${hourSunSet}:${minutesSunSet}</p>`
         // console.log(sunRise[0])
         // console.log(sunRise.getHours())
         // console.log(sunRise.getMinutes())
