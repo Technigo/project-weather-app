3f4edacd7f99b7ef0a6cef9b605704dea3427bbe
“[ida <[ida.t.sylven@gmail.com]>
2021-02-16 17:30:28 +0100

    Added a 2nd API URL and started to import data for a 5-day forecast. Added some bacis flexboxes to layout

--- script.js
+++ script.js
@@ -1,10 +1,13 @@
 const apiUrl = 'http://api.openweathermap.org/data/2.5/weather?q=Stockholm,Sweden&units=metric&APPID=800ddea444fa8b1532e3492f09d4432d'
+const apiUrlForecast = 'https://api.openweathermap.org/data/2.5/forecast?q=Stockholm,Sweden&units=metric&APPID=800ddea444fa8b1532e3492f09d4432d'
 const city = document.getElementById('city')
 const currentTemp = document.getElementById('currentTemp')
 const feelsLikeTemp = document.getElementById('feelsLikeTemp')
 const weatherDescrValue = document.getElementById('weatherDescription')
 const sunrise = document.getElementById('sunrise')
 const sunset = document.getElementById('sunset')
+const currentWeatherIcon = document.getElementById('currentWeatherIcon')
+
 
 fetch(apiUrl)
     .then((response) => {
@@ -31,4 +34,23 @@ fetch(apiUrl)
         console.log(date)
         sunrise.innerHTML += date.toString()
 
+        let iconValue = `${json.weather[0].icon}`
+        console.log(iconValue)
+        currentWeatherIcon.innerHTML = `<img id="currentWeatherIcon" src="http://openweathermap.org/img/wn/${iconValue}.png" />`
+    })
+
+fetch(apiUrlForecast)
+    .then((response) => {
+        return response.json()
+    })
+    .then((json) => {
+        console.log(json.cod)
+        const weekdayOne = document.getElementById('dayOne')
+        let iconValue2 = `${json.list[0].weather[0].icon}`
+        console.log(iconValue2)
+        weekdayOne.innerHTML = `
+            <p>Mon</p>
+            <img src="http://openweathermap.org/img/wn/${iconValue2}.png" />
+            <p>Max ${json.list[0].main.temp_max}°/ Min ${json.list[0].main.temp_min} °C</p>
+        `
     })
