5fce77d04040b8ffa7ff19796dbe7eb21d2084d8
Sara Carlstein <carlstein.sara@gmail.com>
2021-02-16 14:36:34 +0100

    step 5

--- script.js
+++ script.js
@@ -1,7 +1,12 @@
 // const weatherAPI = 'https://api.openweathermap.org/data/2.5/weather?q=Stockholm,Sweden&units=metric&APPID=b875a4e82ea7797a6ff76c50f02a0b8b'
 let cityName = "Stockholm" 
-
-const city = document.getElementById('city')
+// const city = document.getElementById('city')
+// const dayOne = document.getElementById('day-one')
+// const dayTwo = document.getElementById('day-two')
+// const dayThree = document.getElementById('day-three')
+// const dayFour = document.getElementById('day-four')
+// const dayFive = document.getElementById('day-five')
+const allDays = document.getElementById('all-days')
 
 
 fetch (`https://api.openweathermap.org/data/2.5/weather?q=${cityName}&units=metric&APPID=b875a4e82ea7797a6ff76c50f02a0b8b`)
@@ -15,6 +20,7 @@ fetch (`https://api.openweathermap.org/data/2.5/weather?q=${cityName}&units=metr
 
         console.log(sunSet)
 
+        //Adds a 0 
         const hourAndMinutes = (i) =>{
             if (i < 10) {
                 i = "0" + i;
@@ -36,19 +42,21 @@ fetch (`https://api.openweathermap.org/data/2.5/weather?q=${cityName}&units=metr
         // city.innerHTML += `<p> ${sunRise}</p>`
         city.innerHTML += `<p>${hourSunRise}:${minutesSunRise}</p>`
         city.innerHTML += `<p>${hourSunSet}:${minutesSunSet}</p>`
-        // console.log(sunRise[0])
-        // console.log(sunRise.getHours())
-        // console.log(sunRise.getMinutes())
-        // console.log(hour, minutes)
-        // console.log(hourAndMinutes(hour))
-        // console.log(hourAndMinutes(minutes))
+       
     })
 
-    //city name
-    //temprature (rounded 1 decimal)
-    //type of weather
-    
-    //sunrise and sunset
-    //time
-
     //forcast 5 days
+
+    fetch(`https://api.openweathermap.org/data/2.5/forecast?q=${cityName},Sweden&units=metric&APPID=b875a4e82ea7797a6ff76c50f02a0b8b`)
+        .then((response) => {
+            return response.json();
+        }).then((data) => {
+            const filteredForecast = data.list.filter(item => item.dt_txt.includes('12:00'))
+    
+            console.log(filteredForecast)
+                filteredForecast.forEach((day) => {
+                    allDays.innerHTML += `<div> Temperature: ${day.main.temp} Feels like: ${day.main.feels_like}</div>`
+            })
+            
+          
+        })
