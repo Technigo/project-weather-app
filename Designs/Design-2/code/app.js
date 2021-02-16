614ed577710de5beeb5922c3d51758c6a30d8eb8
Ann-Sofi Jönsson <annsofi.jonsson@hotmail.se>
2021-02-16 20:19:11 +0100

    made the fecth'es in to functions. Made the temperature without decimals. Created function to print out weekdays from API.  Started with css

--- Designs/Design-2/code/app.js
+++ Designs/Design-2/code/app.js
@@ -6,6 +6,13 @@ const forcast = document.getElementById('forcast');
 
 //let unix
 
+let returnWeekDay = (date) => { 
+  let daysInWeek = ["Sunday", "Monday","Tuesday", "Wednesday","Thursday","Friday","Saturday"];
+  let inputDate = new Date(date);
+  return daysInWeek[inputDate.getDay()];
+}
+
+const fetchTodaysWeather = () => {
 fetch(weatherUrl)
   .then((response) => {
     return response.json()
@@ -17,38 +24,53 @@ fetch(weatherUrl)
     let sunrise = new Date(sunriseAPI*1000).toLocaleTimeString([], {timeStyle: 'short'});
     let sunset = new Date(sunsetAPI*1000).toLocaleTimeString([], {timeStyle: 'short'});
 
-    weatherSection.innerHTML = `
-    <h2>${json.name}</h2>
-    <h3>${json.main.temp} °C</h2>  
-    <p>The humidity is ${json.main.humidity} %</p>
-    <p>Today: ${json.weather[0].description} </p>
-    <p>The sunrise is: ${sunrise}</p>
-    <p>The sunset is: ${sunset}</p>
-    `
+    let tempToday = `${json.main.temp}`
+    let tempTodayRounded = Math.round(tempToday)
 
-    //let temp = `${json.main.temp}` //Försöker få temp med bara en decimal. 
-    // temp= temp.toFixed(1)
-    // console.log(temp)
+    weatherSection.innerHTML = `
+    <div class="today-temp">
+      <div class="today">
+        <h2>${tempTodayRounded} °C</h2>  
+        <h1>${json.name}</h1>
+        <h3>${json.weather[0].description} </h3>
+      </div>
+      <div class="weather-icon">
+        <img src="./Group36.png" alt="sun">
+      </div>
+    </div>
+    <div class="sun-hours">
+      <p>Sunrise: ${sunrise}</p>
+      <p>Sunset: ${sunset}</p>
+    </div> 
+    `
   })
+}
   
+const fetch5DayForcast = () => {
 fetch(weatherForcastUrl)
   .then((response) => {
     return response.json()
   })
   .then((json) => {
-    const filteredForecast = json.list.filter(item => item.dt_txt.includes('12:00')) //Vill få ut temp och typ av väder från alla 5 dagar. Göra en forEach.. hur? 
+    const filteredForecast = json.list.filter(item => item.dt_txt.includes('12:00'))
     console.log(filteredForecast);
     
-    // Math.round
-    // forcast.innerHTML = `
-    // <p>Temp at 12 tomorrow: ${filteredForecast[0].main.temp} °C</p>
-    // <p>Temp at 12 day after tomorrow: ${filteredForecast[1].main.temp} °C</p>   
-    // `
+   
 
     filteredForecast.forEach((filteredForecast) => {
-      forcast.innerHTML += `<p> ${filteredForecast.main.temp} </p>`
+      let dayInWeek = returnWeekDay(filteredForecast.dt_txt);
+      let temp5Days = `${filteredForecast.main.temp}`
+      let temp5DaysRounded = Math.round(temp5Days)
+      forcast.innerHTML += `
+      <div class="five-days">
+      <p> ${dayInWeek}:</p> <p> ${temp5DaysRounded} </p>
+      </div>
+      `
     })
   })
+};
 
-//Vill ha en decimal på temp på forcast
   
+
+fetchTodaysWeather(); 
+fetch5DayForcast();
