31ccfb8b858bf2281ef0d90bf4e2f32cc7602544
Your Name <isabellamarkskog@live.se>
2021-02-17 21:25:22 +0100

    Converted sunrise and sunset number to readable time. Added temp and weather description innerHTML. Added day and date converted innerHTML.

--- script.js
+++ script.js
@@ -2,57 +2,55 @@
 const apiUrl = 'http://api.openweathermap.org/data/2.5/weather?q=Kuala%20Lumpur&units=metric&appid=5527a65cb469b1aa0aae3017c8ea2460'
 const apiUrlForecast = 'http://api.openweathermap.org/data/2.5/forecast?q=Kuala%20Lumpur&units=metric&appid=5527a65cb469b1aa0aae3017c8ea2460'
 
-
+//DOM selectors
 const summary = document.getElementById('summary')
 const mainInformation = document.getElementById('mainInformation')
 const icon = document.getElementById('icon')
 const infoText = document.getElementById('infoText')
-const forecast = document.getElementById('forecast')
+const forecastContainer = document.getElementById('forecastContainer')
 
 //Global variables
 let temperature
 
 
 fetch(apiUrl)
-.then((response) => {
-  return response.json()
-
-})
-.then((json) => {
-  // SUMMARY
-  // 1. WEATHER (weather --> description)
-  json.weather.map((weather) => {
-    // console.log(weather.description)    //sätt en innerHTML
-    summary.innerHTML += `
-      <p>${weather.description}</p>`
+  .then((response) => {
+    return response.json()
   })
+  .then((json) => {
+    // SUMMARY
+    // 1. WEATHER 
+    json.weather.map((weather) => {
+      summary.innerHTML += `
+        <p>${weather.description}</p>`
+    })
 
-  // 2. TEMPERATURE (main --> temp) 
-  // console.log(json.main.temp)  
-  temperature = json.main.temp
-  // console.log(temperature.toFixed(0))
-  summary.innerHTML += `
-    <p>${temperature.toFixed(0)}</p>`
+    // 2. TEMPERATURE 
+    temperature = json.main.temp
+    summary.innerHTML += `
+      <p>${temperature.toFixed(0)}</p>`
 
-  // 3. SUNRISE (sys --> sunrise)
-  // console.log(json.sys.sunrise)
-  summary.innerHTML += `
-  <p>${json.sys.sunrise}</p>`
+    // 3. SUNRISE 
+    const sunriseDate = new Date((json.sys.sunrise + json.timezone) * 1000)
+    summary.innerHTML += `
+    <p>${sunriseDate.getHours()}:${sunriseDate.getMinutes()}</p>`
 
-  // 4. SUNSET (sys --> sunset)
-  // console.log(json.sys.sunset)
-  summary.innerHTML += `
-  <p>${json.sys.sunset}</p>`
+    // 4. SUNSET 
+    const sunsetDate = new Date((json.sys.sunset + json.timezone) * 1000)
+    summary.innerHTML += `
+    <p>${sunsetDate.getHours()}:${sunsetDate.getMinutes()}</p>`
 
 
-  //MAIN INFORMATION
-  // SVG + descriptive text that changes depending on the weather
-  // 1. LOCATION (name)
-  // console.log(json.name)
-  mainInformation.innerHTML += `
-  <p>${json.name}</p>`
-
-})
+    //MAIN INFORMATION
+    // SVG + descriptive text that changes depending on the weather
+    // 1. LOCATION 
+    mainInformation.innerHTML += `
+    <h2>${json.name}</h2>`
+  })
+  .catch(err => {
+    console.error(err)
+    summary.innerHTML = 'Error! ' + err
+  })
 
 
 //FORECAST - own fetch function
@@ -60,19 +58,27 @@ fetch(apiUrl)
 fetch(apiUrlForecast)
 .then((response) => {
   return response.json()
-
 })
 .then((json) => {
-  //// console.log(json)
   //Data types: day, date, temperature, weather (description)
+  const timezone = json.city.timezone
   const filteredForecast = json.list.filter(item => item.dt_txt.includes('12:00'))
-  console.log(filteredForecast)
 
   // TEMPERATURE & WEATHER
   filteredForecast.map((forecast) => {
-    console.log(forecast.main.temp.toFixed(0))
-    console.log(forecast.weather[0].description) //hårdkodat, skriv kommentar senare
-    console.log(forecast.dt)
+    forecastContainer.innerHTML += `
+    <p>${forecast.main.temp.toFixed(0)}</p>
+    `
+
+    //hårdkodat, skriv kommentar senare
+    forecastContainer.innerHTML += `
+    <p>${forecast.weather[0].description}</p>           
+    `
+
+    const forecastDate = new Date((forecast.dt + timezone) * 1000)
+    forecastContainer.innerHTML += `
+    ${forecastDate.toLocaleDateString('en-US', {weekday: 'short', month: 'numeric', day: 'numeric'})}
+    `
   })
 
 
