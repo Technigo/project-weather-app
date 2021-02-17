a37f07a9a6b042a76ea25233aa0ab27534fcbe10
Åsa Hildebrand <asahildebrand@yahoo.com>
2021-02-17 10:26:54 +0100

    Added some sections, Id's and classes in HTML. Beginning to style with CSS.

--- script.js
+++ script.js
@@ -1,10 +1,10 @@
-const container = document.getElementById("container")
 const cityName = document.getElementById("city-name")
 const temperature = document.getElementById("temperature")
 const weather = document.getElementById("weather")
 const sunriseTime = document.getElementById("sunrise-time")
 const sunsetTime = document.getElementById("sunset-time")
 const forecast = document.getElementById("forecast")
+const todaysWeather = document.getElementById("todays-weather")
 
 fetch ("http://api.openweathermap.org/data/2.5/weather?q=Stockholm,Sweden&units=metric&APPID=cd2e520714b56e5fd7e92da11e1db7f3")
 .then((response) => {
@@ -12,8 +12,9 @@ fetch ("http://api.openweathermap.org/data/2.5/weather?q=Stockholm,Sweden&units=
 })
 .then((json) => {
     cityName.innerHTML=json.name
-    temperature.innerHTML=Math.round(json.main.temp * 10) / 10
-    weather.innerHTML=json.weather[0].description
+    todaysWeather.innerHTML=`${weather} | ${temperature}`
+    temperature=Math.round(json.main.temp * 10) / 10
+    weather=json.weather[0].description
     sunriseTime.innerHTML=json.sys.sunrise
     sunsetTime.innerHTML=json.sys.sunset
     console.log(json)
@@ -23,7 +24,9 @@ fetch ("https://api.openweathermap.org/data/2.5/forecast?q=Stockholm,Sweden&unit
 .then((response) => {
     return response.json()
 })
+
 .then((json) => {
-    forecast.innerHTML=json.list[0].main.temp_max
-    console.log(json)
+    for (let i = 0; i < 5; i++) {
+        forecast.innerHTML+=`<p>${json.list[i].main.temp_max}</p>`
+    console.log(json)}
 })
