da5cc589f2cd24509f5f1866840c7ad859b79adb
Caroline Byström <frivridnadagar@gmail.com>
2021-02-17 16:13:53 +0100

    add temp och feels like, add snow icon

--- code/script.js
+++ code/script.js
@@ -1,8 +1,9 @@
 //console.log('hej hej')
 const StockholmAPI = 'http://api.openweathermap.org/data/2.5/weather?q=Stockholm,Sweden&units=metric&APPID=1f53b6ca8e6cbcf1c51848ca6c257778'
 const StockholmForecastAPI = 'https://api.openweathermap.org/data/2.5/forecast?q=Stockholm,Sweden&units=metric&APPID=1f53b6ca8e6cbcf1c51848ca6c257778'
-const cityName = document.getElementById('cityName')
+const cityName = document.getElementById('cityName') //TA BORT om vi aldrig använder den
 const weatherContainer = document.getElementById('weatherContainer')
+const forecastContainer = document.getElementById('forecastContainer')
 const sydneyApi = 'https://api.openweathermap.org/data/2.5/weather?q=Sydney&appid=2430dd2cc5de1b93bed8a082b8dda9c5'
 const chicagoApi = 'https://api.openweathermap.org/data/2.5/weather?q=Chicago&appid=2430dd2cc5de1b93bed8a082b8dda9c5'
 const hongKongApi = 'https://api.openweathermap.org/data/2.5/weather?q=Johannesburg&appid=2430dd2cc5de1b93bed8a082b8dda9c5'
@@ -23,46 +24,61 @@ fetch(StockholmAPI)
     .then((data) => {
 //      console.log(data);   //take away later?
     
-    const num = data.main.temp
-    const n = Math.round(num * 10) / 10
-
+    const temp = data.main.temp
+    const tempRounded = Math.round(temp * 10) / 10 //eller toFixed()
+    const date = new Date((data.dt) * 1000); //amount of milliseconds passed since January 1st 1970
+    const sunriseDate = new Date((data.sys.sunrise) * 1000);
+    const sunsetDate = new Date((data.sys.sunset) * 1000);
+    
       weatherContainer.innerHTML += `<div>
         <h1>Today's weather in: ${data.name}<h1/>
-        <h2>${n}</h2>
+        <h2>${tempRounded}°</h2>
         <h3>${data.weather[0].description}</h3>
-        
+        <h3>${data.name}</h3>                
+        <h5>Date: ${date.toLocaleDateString('sv-SE', {weekday: 'long', year: 'numeric', month: 'long', day: 'numeric'})}</h5>
+        <h5>Time: ${date.getHours()}:${date.getMinutes()}</h5>
+        <h5>Sunrise: ${sunriseDate.getHours()}:${sunriseDate.getMinutes()}</h5>
+        <h5>Sunset: ${sunsetDate.getHours()}:${sunsetDate.getMinutes()}</h5>
         </div>`
     }) 
-/*
-var number = 12.3456789
-var rounded = Math.round(number * 10) / 10
-
-function myFunction() {
-  var num = 5.56789;
-  var n = num.toFixed(2);
-  document.getElementById("demo").innerHTML = n;
-}
-*/
 
 fetch(StockholmForecastAPI)
   .then((response) => {
-
     return response.json()
   })
 
-  .then((json) => {
-    //console.log(json)
-  const filteredForecast = json.list.filter(item => item.dt_txt.includes('12:00'))
+  .then((data) => {
+    //console.log(data)
+  const filteredForecast = data.list.filter(item => item.dt_txt.includes('12:00'))
     console.log(filteredForecast)
     filteredForecast.forEach((day) => {
-        let min = day.main.temp_min
-        let max = day.main.temp_max //change these two to fells like and temp instead? No difference btw max and min from this API. 
-        console.log(min, max)
+        let temp = day.main.temp
+        let feelsLike = day.main.feels_like //change these two to fells like and temp instead? No difference btw max and min from this API. 
+        const image = feelsLike 
+        if (feelsLike =< 0) {
+          'Group16.png'
+        } else if (feelsLike <= 5) {
+          'Group34.png'
+        } else if (feelsLike > 5) {
+          'Group36.png'
+        } else (feelsLike > 15) { //kan tas bort senare, stå endast else
+          'Group37.png'
+        }
+
+        console.log(temp, feelsLike)
 
         let nextDays = new Date(day.dt_txt)
         let weekdayInteger = nextDays.getDay()
         console.log(day.dt_txt)
         console.log(weekday[weekdayInteger])
+
+        forecastContainer.innerHTML += `<div>
+        <h3>${weekday[weekdayInteger]}<h3>
+        <h3>Temp: ${temp}°</h3>
+        <h3>Feels like: ${image} ${feelsLike}°</h3>
+        </div>
+        `
+
     })
   })
 
