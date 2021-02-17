05c1939dfa29120355560ef6b97491adadd5caf1
Julia Ros <70485921+julialeonieros@users.noreply.github.com>
2021-02-17 20:54:56 +0100

    Changed array for 5-day forecast so it also prints out weekday. Added catch for errors.

--- code/script.js
+++ code/script.js
@@ -6,7 +6,7 @@ const weeksWeather = document.getElementById('weeks-weather')
 const cityHeader = document.getElementById('city')
 const sunrise = document.getElementById('sunrise')
 const sunset = document.getElementById('sunset')
-const weekdaysContainer = document.getElementById('weekdays-container')
+// const weekdaysContainer = document.getElementById('weekdays-container')
 
 fetch(apiUrlToday)
   .then((response) => {
@@ -18,27 +18,38 @@ fetch(apiUrlToday)
   })
   .then((data) => {
 
+    const sunrise = new Date (data.sys.sunrise * 1000).toLocaleTimeString('sv-SE', {
+        hour: '2-digit', minute: '2-digit',
+      })
+
+    const sunset = new Date (data.sys.sunset * 1000).toLocaleTimeString('sv-SE', {
+      hour: '2-digit', minute: '2-digit',
+    })
+
     todaysWeatherSmall.innerHTML = `
-      <p>${data.weather[0].description} | ${Math.round(data.main.temp * 10) / 10} °C (feels like ${Math.round(data.main.feels_like * 10) / 10})</p>
+      <p>${data.weather[0].description} | ${data.main.temp.toFixed(1)} °C (feels like ${data.main.feels_like.toFixed(1)})</p>
+      <p>sunrise: ${sunrise}</p>
+      <p>sunset: ${sunset}</p>
     `
-    sunrise.innerHTML = `
-      ${new Date (data.sys.sunrise * 1000).toLocaleTimeString('sv-SE', {
-      hour: '2-digit',
-      minute: '2-digit',
-      })}
-    `
-    sunset.innerHTML = `
-      ${new Date (data.sys.sunset * 1000).toLocaleTimeString('sv-SE', {
-      hour: '2-digit',
-      minute: '2-digit',
-      })}
-    `
+    // sunrise.innerHTML = `
+    //   ${new Date (data.sys.sunrise * 1000).toLocaleTimeString('sv-SE', {
+    //   hour: '2-digit',
+    //   minute: '2-digit',
+    //   })}
+    // `
+    // sunset.innerHTML = `
+    //   ${new Date (data.sys.sunset * 1000).toLocaleTimeString('sv-SE', {
+    //   hour: '2-digit',
+    //   minute: '2-digit',
+    //   })}
+    // `
+    
     cityHeader.innerHTML = `
       ${data.name}
     `
   })
   .catch(error => {
-    weeksWeather.innerHTML = `
+    todaysWeather.innerHTML = `
     <h1>${error}</h1>
     `
   })
@@ -52,13 +63,15 @@ fetch(apiUrlFiveDays)
     }
   })
   .then((data) => {
-    let weekday
 
     const filteredForecast = data.list.filter(day => day.dt_txt.includes('12:00'))
     filteredForecast.forEach(day => {
-      weekday = Math.round(day.main.temp_min * 10) / 10
-      console.log(weekday)
+      const forecastDay = new Date (day.dt_txt)
+      weeksWeather.innerHTML += `
+      <div class='weekdays-container'><div class='day'>${forecastDay.toLocaleString('en-us', {weekday: 'short'})}</div><div class='temp-container'>${day.main.temp.toFixed(0)} &#176C</div></div>
+    ` 
     })
+
   })
   .catch(error => {
     weeksWeather.innerHTML = `
