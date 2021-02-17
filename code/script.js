51ea8d4acaca896156675fa9b70fa7e7044daa20
Caroline Byström <frivridnadagar@gmail.com>
2021-02-17 22:17:55 +0100

    add icon to Feels like forecast, and Stockholm

--- code/script.js
+++ code/script.js
@@ -1,14 +1,16 @@
 //console.log('hej hej')
 const StockholmAPI = 'http://api.openweathermap.org/data/2.5/weather?q=Stockholm,Sweden&units=metric&APPID=1f53b6ca8e6cbcf1c51848ca6c257778'
 const StockholmForecastAPI = 'https://api.openweathermap.org/data/2.5/forecast?q=Stockholm,Sweden&units=metric&APPID=1f53b6ca8e6cbcf1c51848ca6c257778'
-const cityName = document.getElementById('cityName') //TA BORT om vi aldrig använder den
-const weatherContainer = document.getElementById('weatherContainer')
-const forecastContainer = document.getElementById('forecastContainer')
 const sydneyApi = 'https://api.openweathermap.org/data/2.5/weather?q=Sydney&appid=2430dd2cc5de1b93bed8a082b8dda9c5'
 const chicagoApi = 'https://api.openweathermap.org/data/2.5/weather?q=Chicago&appid=2430dd2cc5de1b93bed8a082b8dda9c5'
 const hongKongApi = 'https://api.openweathermap.org/data/2.5/weather?q=Johannesburg&appid=2430dd2cc5de1b93bed8a082b8dda9c5'
 const johannesburgApi = 'https://api.openweathermap.org/data/2.5/weather?q=HongKong&appid=2430dd2cc5de1b93bed8a082b8dda9c5'
 const quitoApi = 'https://api.openweathermap.org/data/2.5/weather?q=Quito&appid=2430dd2cc5de1b93bed8a082b8dda9c5'
+
+const cityName = document.getElementById('cityName') //TA BORT om vi aldrig använder den!
+const weatherContainer = document.getElementById('weatherContainer')
+const forecastContainer = document.getElementById('forecastContainer')
+
 
 console.log('API fetch starting')
 
@@ -29,12 +31,13 @@ fetch(StockholmAPI)
     const date = new Date((data.dt) * 1000); //amount of milliseconds passed since January 1st 1970
     const sunriseDate = new Date((data.sys.sunrise) * 1000);
     const sunsetDate = new Date((data.sys.sunset) * 1000);
-    
+    const icon = 'http://openweathermap.org/img/wn/' + data.weather[0].icon +'@2x.png'
+
+    //ta bort Today's weather in: ?? 
       weatherContainer.innerHTML += `<div>
-        <h1>Today's weather in: ${data.name}<h1/>
-        <h2>${tempRounded}°</h2>
-        <h3>${data.weather[0].description}</h3>
-        <h3>${data.name}</h3>                
+        <h1>${tempRounded}° <img src="${icon}"<h1/>
+        <h2>Today's weather in: ${data.name}</h2> 
+        <h3>${data.weather[0].description}</h3>         
         <h5>Date: ${date.toLocaleDateString('sv-SE', {weekday: 'long', year: 'numeric', month: 'long', day: 'numeric'})}</h5>
         <h5>Time: ${date.getHours()}:${date.getMinutes()}</h5>
         <h5>Sunrise: ${sunriseDate.getHours()}:${sunriseDate.getMinutes()}</h5>
@@ -52,21 +55,15 @@ fetch(StockholmForecastAPI)
   const filteredForecast = data.list.filter(item => item.dt_txt.includes('12:00'))
     console.log(filteredForecast)
     filteredForecast.forEach((day) => {
-        let temp = day.main.temp //avrunda till heltal
-        let feelsLike = day.main.feels_like //avrunda till heltal
-        
-        let image = feelsLike () => { //eller const
-          if (feelsLike =< 0) { 
-            'snow.png'
-          } else if (feelsLike <= 5) { //kan en skriva grader så här?
-            'Group34.png'
-          } else if (feelsLike > 5) {
-            'Group36.png'
-          } else (feelsLike > 15) { //kan tas bort sen, stå endast else
-            'Group37.png'
-        }
-        } 
-        console.log(temp, feelsLike)
+        let temp = day.main.temp
+        let tempRounded = Math.round(temp)
+
+        let feelsLike = day.main.feels_like
+        let feelsLikeRounded = Math.round(feelsLike)
+
+        let icon = 'http://openweathermap.org/img/wn/' + day.weather[0].icon +'@2x.png' // http://openweathermap.org/img/wn/10d@2x.png
+
+        console.log(tempRounded, feelsLikeRounded)
 
         let nextDays = new Date(day.dt_txt)
         let weekdayInteger = nextDays.getDay()
@@ -75,8 +72,8 @@ fetch(StockholmForecastAPI)
 
         forecastContainer.innerHTML += `<div>
         <h3>${weekday[weekdayInteger]}<h3>
-        <h3>Temp: ${temp}°</h3>
-        <h3>Feels like: ${image} ${feelsLike}°</h3>
+        <h3>Temp: ${tempRounded}°</h3>
+        <h3>Feels like: <img src="${icon}"> ${feelsLikeRounded}°</h3>
         </div>
         `
     })
