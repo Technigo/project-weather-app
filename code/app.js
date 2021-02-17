2a27dfb276f55b39ce3bef91db35f48cfec555db
Bellsibub <isabella.billgren@gmail.com>
2021-02-17 09:37:17 +0100

    sunrise&sunset data added, formatted

--- code/app.js
+++ code/app.js
@@ -14,12 +14,13 @@ const fetchWeatherToday = () => {
   fetch(`${API_URL}weather?q=${CITY}&units=metric&appid=${API_KEY}`)
     .then((response) => response.json())
     .then((data) => {
+      const sunTimes = formatTime([data.sys.sunrise, data.sys.sunset]);
       let weatherData = {
         temp: data.main.temp.toFixed(1),
         type: data.weather[0].description,
         city: CITY,
-        sunrise: "",
-        sunset: "",
+        sunrise: sunTimes[0],
+        sunset: sunTimes[1],
       };
       weatherToday.innerHTML = getWeatherToday(weatherData);
       // DEBUG: remove on submission
@@ -27,6 +28,16 @@ const fetchWeatherToday = () => {
       console.log(weatherData);
     })
     .catch((err) => console.log(`Error was thrown: ${err.message}`));
+};
+
+const formatTime = (times) => {
+  let formattedTimes = [];
+  times.forEach((time) => {
+    const date = new Date(time * 1000).toTimeString();
+    const dateStrings = date.split(":");
+    formattedTimes.push(`${dateStrings[0]}:${dateStrings[1]}`);
+  });
+  return formattedTimes;
 };
 
 /* EXECUTE PAGE LOAD FUNCTIONS */
