c14749a7c980af655f46f42c3ce8278ff5cdaaf3
vladjnbykov <67795638+vladjnbykov@users.noreply.github.com>
2021-02-17 23:37:29 +0100

    dynamic weather icons, multiple cities

--- code/app.js
+++ code/app.js
@@ -1,13 +1,17 @@
 const dtable = document.getElementById('dynamic');
-
-
+const selectCity = document.getElementById('selectCity');
+//const URL =  "https://api.openweathermap.org/data/2.5/forecast?q=Stockholm,Sweden&units=metric&APPID=1b672b9c637e28dafe516793b1e9bf96"
+// const today = "https://api.openweathermap.org/data/2.5/weather?q=Stockholm,Sweden&units=metric&APPID=1b672b9c637e28dafe516793b1e9bf96"  
+// const URL5d = "https://api.openweathermap.org/data/2.5/forecast?q=Stockholm,Sweden&units=metric&APPID=1b672b9c637e28dafe516793b1e9bf96"
 // Global variable
 let weather, daysWeek, comboArray,
-dWeek,
- tempPoints
+dWeek, value, tempPoints, 
+NEW_1URL = "https://api.openweathermap.org/data/2.5/weather?q=Stockholm,Sweden&units=metric&APPID=1b672b9c637e28dafe516793b1e9bf96",
+NEW_5URL = "https://api.openweathermap.org/data/2.5/forecast?q=Stockholm,Sweden&units=metric&APPID=1b672b9c637e28dafe516793b1e9bf96"
 
 // convert datetime from API to hours and min
 const hrsMinConverter = (param) => {
+  
     const date = new Date(param * 1000)
     const hours = date.getHours()
     const min = date.getMinutes()
@@ -26,25 +30,33 @@ const dayMonthConverter = (param) => {
 
 // convert datatime from API to day of the week
 const dayWeekConverter = (param) => {
-  const date = new Date(param * 1000)
+  const date = new Date((param)* 1000)
   const dayOfWeek = date.getDay()
   return dayOfWeek
 }
 
 /************ TODAY WEATHER FORECAST ***************/
 const weatherData = () => {
-  fetch("https://api.openweathermap.org/data/2.5/weather?q=Stockholm,Sweden&units=metric&APPID=1b672b9c637e28dafe516793b1e9bf96")
+  console.log(NEW_1URL)
+  fetch(NEW_1URL)
   .then((response) => {
     return response.json().then((json) => {
       weather = json
       console.log(weather)
       console.log("today", dayWeekConverter(weather.dt))
-      city.innerHTML= weather.name
-      dayCurrent.innerHTML += dayMonthConverter(weather.dt)
-      timeCurrent.innerHTML += hrsMinConverter(weather.dt)
-      sky.innerHTML += weather.weather[0].main
-      temperature.innerHTML += `${Math.round(weather.main.temp)}C` 
-      feelsTemp.innerHTML += `${Math.round(weather.main.feels_like)}C`
+      city.innerHTML += weather.name
+      dayCurrent.innerHTML+= `Today: ${dayMonthConverter(weather.dt)}`
+      timeCurrent.innerHTML += `Time: ${hrsMinConverter(weather.dt)}`
+      sky.innerHTML += `
+        <tr>
+        <td> ${weather.weather[0].main}</td>
+        <td><img src="https://openweathermap.org/img/wn/${weather.weather[0].icon}@2x.png"/></td>
+        </tr>  
+        `
+
+
+      temperature.innerHTML += `Temperature: ${Math.round(weather.main.temp)}C` 
+      feelsTemp.innerHTML+= `Feels like: ${Math.round(weather.main.feels_like)}C`
       sunrise.innerHTML += hrsMinConverter(weather.sys.sunrise)
       sunset.innerHTML += hrsMinConverter(weather.sys.sunset)
     });
@@ -54,25 +66,21 @@ weatherData();
 
 /********** 5 DAYS FORECAST ***********/
 const weatherForecast = () => {
-  fetch("https://api.openweathermap.org/data/2.5/forecast?q=Stockholm,Sweden&units=metric&APPID=1b672b9c637e28dafe516793b1e9bf96")
+  
+  fetch(NEW_5URL)
   .then((response) => {
     return response.json().then((json) => {
       const filteredForecast = json.list.filter(item => item.dt_txt.includes('12:00'))
       console.log("filtered data",filteredForecast)
-      
       filteredForecast.map((elem) => {
-        //console.log(elem.dt)
-        //console.log(dayWeekConverter(elem.dt))
         dw= dayWeekConverter(elem.dt)  
-        //console.log("dw", dw)
-        //console.log(dayWeekConverter(elem.dt), elem.main.feels_like)
+        
         if (dw === 0) {
-          
           dtable.innerHTML += `
           <tr>
             <td>Sun ${Math.round(elem.main.feels_like)}C</td>
             <td>${elem.weather[0].description}</td>
-
+            <td><img src="https://openweathermap.org/img/wn/${elem.weather[0].icon}@2x.png"></td>
           </tr>
           `
         } else if (dw === 1) {
@@ -80,7 +88,7 @@ const weatherForecast = () => {
           <tr>
             <td>Mon ${Math.round(elem.main.feels_like)}C</td>
             <td>${elem.weather[0].description}</td>
-
+            <td><img src="https://openweathermap.org/img/wn/${elem.weather[0].icon}@2x.png"></td>
           </tr>
           `
         } else if (dw === 2) {
@@ -88,7 +96,7 @@ const weatherForecast = () => {
           <tr>
             <td>Tue ${Math.round(elem.main.feels_like)}C</td>
             <td>${elem.weather[0].description}</td>
-
+            <td><img src="https://openweathermap.org/img/wn/${elem.weather[0].icon}@2x.png"></td>
           </tr>
           `
         } else if (dw === 3) {
@@ -96,7 +104,7 @@ const weatherForecast = () => {
           <tr>
             <td>Wed ${Math.round(elem.main.feels_like)}C</td>
             <td>${elem.weather[0].description}</td>
-
+            <td><img src="https://openweathermap.org/img/wn/${elem.weather[0].icon}@2x.png"></td>
           </tr>
           `
         } else if (dw === 4) {
@@ -104,7 +112,7 @@ const weatherForecast = () => {
           <tr>
             <td>Thu ${Math.round(elem.main.feels_like)}C</td>
             <td>${elem.weather[0].description}</td>
-
+            <td><img src="https://openweathermap.org/img/wn/${elem.weather[0].icon}@2x.png"></td>
           </tr>
           `
         } else if (dw === 5) {
@@ -112,7 +120,7 @@ const weatherForecast = () => {
           <tr>
             <td>Fri ${Math.round(elem.main.feels_like)}C</td>
             <td>${elem.weather[0].description}</td>
-
+            <td><img src="https://openweathermap.org/img/wn/${elem.weather[0].icon}@2x.png"></td>
             </tr>
             `
         } else if (dw === 6) {
@@ -120,7 +128,7 @@ const weatherForecast = () => {
           <tr>
             <td>Sat ${Math.round(elem.main.feels_like)}C</td>
             <td>${elem.weather[0].description}</td>
-
+            <td><img src="https://openweathermap.org/img/wn/${elem.weather[0].icon}@2x.png"></td>
           </tr>
           `
         }
@@ -128,12 +136,14 @@ const weatherForecast = () => {
     })
   })
 }
-
 weatherForecast()
 
 /*************************WEATHER GRAPH *******************/
+
+//fetch("https://api.openweathermap.org/data/2.5/forecast?q=Stockholm,Sweden&units=metric&APPID=1b672b9c637e28dafe516793b1e9bf96")
+//
 const tempGraph = () => {
-  fetch("https://api.openweathermap.org/data/2.5/forecast?q=Stockholm,Sweden&units=metric&APPID=1b672b9c637e28dafe516793b1e9bf96")
+  fetch(NEW_5URL)
   .then((response) => {
     return response.json().then((json) => {
       const filteredForecast = json.list.filter(item => item.dt_txt.includes('12:00'))
@@ -189,4 +199,31 @@ const tempGraph = () => {
 })
 }
 tempGraph()
+
+const myCity = () => {
+
+  
+  const value = selectCity.options[selectCity.selectedIndex].value
+
+  console.log("the selected city", value)
+  NEW_1URL = `https://api.openweathermap.org/data/2.5/weather?q=${value}&units=metric&APPID=1b672b9c637e28dafe516793b1e9bf96`
+  NEW_5URL = `https://api.openweathermap.org/data/2.5/forecast?q=${value}&units=metric&APPID=1b672b9c637e28dafe516793b1e9bf96`
+  
+  weatherData()
+  weatherForecast()
+  tempGraph()
+
+}
+
+selectCity.addEventListener("change", () => {
+  city.innerHTML = ""
+  dayCurrent.innerHTML = ""
+  timeCurrent.innerHTML = ""
+  sky.innerHTML = ""
+  temperature.innerHTML = ""
+  feelsTemp.innerHTML = ""
+  dtable.innerHTML = ""
+  myCity(selectCity.value)
+})
+
 
