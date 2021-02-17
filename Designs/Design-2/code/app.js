71807f571f2b7a35754596498f71cd2f090cd987
Ann-Sofi Jönsson <annsofi.jonsson@hotmail.se>
2021-02-17 16:05:42 +0100

    added weather icons, did more styling, started with media queries

--- Designs/Design-2/code/app.js
+++ Designs/Design-2/code/app.js
@@ -6,8 +6,6 @@ const forcast = document.getElementById('forcast');
 
 
 
-//let unix
-
 let returnWeekDay = (date) => { 
   let daysInWeek = ["Sunday", "Monday","Tuesday", "Wednesday","Thursday","Friday","Saturday"];
   let inputDate = new Date(date);
@@ -17,27 +15,33 @@ let returnWeekDay = (date) => {
 const fetchTodaysWeather = () => {
 fetch(weatherUrl)
   .then((response) => {
+    if (response.ok) {
     return response.json()
+    } else {
+      throw "Something went wrong";
+    }
   })
   .then((json)=> {
     // console.log(json)
+    let tempToday = `${json.main.temp}`
+    let tempTodayRounded = Math.round(tempToday)
+    let cityName = json.name;
+    let weatherDescription = json.weather[0].description;
+    let iconId = json.weather[0].icon;
     let sunriseAPI = `${json.sys.sunrise}`
     let sunsetAPI = `${json.sys.sunset}`
     let sunrise = new Date(sunriseAPI*1000).toLocaleTimeString([], {timeStyle: 'short'});
     let sunset = new Date(sunsetAPI*1000).toLocaleTimeString([], {timeStyle: 'short'});
 
-    let tempToday = `${json.main.temp}`
-    let tempTodayRounded = Math.round(tempToday)
-
     weatherSection.innerHTML = `
     <div class="today-temp">
       <div class="today">
         <h2>${tempTodayRounded} °C</h2>  
-        <h1>${json.name}</h1>
-        <h3>${json.weather[0].description} </h3>
+        <h1>${cityName}</h1>
+        <h3>${weatherDescription} </h3>
       </div>
       <div class="weather-icon">
-        <img src="./Group36.png" alt="sun">
+        <img src="http://openweathermap.org/img/wn/${iconId}@2x.png">
       </div>
     </div>
     <div class="sun-hours">
@@ -46,30 +50,41 @@ fetch(weatherUrl)
     </div> 
     `
   })
+  .catch(error => {
+    weatherSection.innerHTML = `${error}`
+  })
 }
   
 const fetch5DayForcast = () => {
 fetch(weatherForcastUrl)
   .then((response) => {
+    if (response.ok) {
     return response.json()
+    } else {
+      throw "Something went wrong";
+    }
   })
   .then((json) => {
     const filteredForecast = json.list.filter(item => item.dt_txt.includes('12:00'))
     console.log(filteredForecast);
-    
-   
 
     filteredForecast.forEach((filteredForecast) => {
       let dayInWeek = returnWeekDay(filteredForecast.dt_txt);
       let temp5Days = `${filteredForecast.main.temp}`
       let temp5DaysRounded = Math.round(temp5Days)
+      let iconId = filteredForecast.weather[0].icon;
       forcast.innerHTML += `
       <div class="five-days">
-      <p> ${dayInWeek}:</p> <p> ${temp5DaysRounded} </p>
+        <p> ${dayInWeek}:</p>
+        <img src="http://openweathermap.org/img/wn/${iconId}@2x.png">
+        <p> ${temp5DaysRounded}°C </p>
       </div>
       `
     })
   })
+    .catch(error => {
+      filteredForecast.innerHTML = `${error}`
+    })
 };
 
   
