513c130e2d4d1a05601e3fdc785dc6a6077e57ef
Sara Carlstein <carlstein.sara@gmail.com>
2021-02-17 22:41:10 +0100

    a lot of css stuff added

--- script.js
+++ script.js
@@ -1,15 +1,15 @@
 // const weatherAPI = 'https://api.openweathermap.org/data/2.5/weather?q=Stockholm,Sweden&units=metric&APPID=b875a4e82ea7797a6ff76c50f02a0b8b'
 let cityName = "Stockholm" 
-// const city = document.getElementById('city')
-// const dayOne = document.getElementById('day-one')
-// const dayTwo = document.getElementById('day-two')
-// const dayThree = document.getElementById('day-three')
-// const dayFour = document.getElementById('day-four')
-// const dayFive = document.getElementById('day-five')
+const city = document.getElementById('city')
+const showSunRise = document.getElementById('sunRise')
+const showSunSet = document.getElementById('sunSet')
 const allDays = document.getElementById('all-days')
-const dayOrNight = document.getElementById('dayOrNight')
-
-
+const weatherDescDiv = document.getElementById('weather-container__info')
+const currentWeather = document.getElementById('currentWeather')
+const body = document.getElementById('body')
+const icons = document.getElementById('icons')
+const weekday = document.getElementById('weekday')
+const temp = document.getElementById('temp')
 
 
 fetch (`https://api.openweathermap.org/data/2.5/weather?q=${cityName}&units=metric&APPID=b875a4e82ea7797a6ff76c50f02a0b8b`)
@@ -38,24 +38,29 @@ fetch (`https://api.openweathermap.org/data/2.5/weather?q=${cityName}&units=metr
         const changeBackground = () => {
             const time = (new Date(data.dt * 1000).getHours());
                 if (time >= 7 && time <= 17) {
-                dayOrNight.style.backgroundImage = "url('./Designs/icons/day.jpg')";
+                    weatherDescDiv.classList.add('day')
+                    body.classList.add('day-time')
+                    console.log("day")
                 } else if (time >= 18) {
-                    dayOrNight.style.backgroundImage = "url('./Designs/icons/night.jpg')";
+                    console.log("night")
+                    weatherDescDiv.classList.add('night')
+                    body.classList.add('night-time')
                 } else {
-                    dayOrNight.style.backgroundImage = "url('./Designs/icons/night.jpg')"; 
+                    weatherDescDiv.classList.add('night')
+                    body.classList.add('night-time')
                 }
             }
         changeBackground()  
         
         console.log(new Date(data.dt * 1000).getHours(),new Date(data.dt * 1000).getMinutes())
        
+        
+        city.innerHTML += `<p class="current-city"> ${data.name}</p>`
+        city.innerHTML += `<p class="current-temp"> ${cityTemp}</p>`
+        currentWeather.innerHTML += `<p> ${data.weather[0].description}</p><img src="./Designs/icons/snowy.svg"/>`
 
-        city.innerHTML += `<p> ${data.name}</p>`
-        city.innerHTML += `<p> ${cityTemp}</p>`
-        city.innerHTML += `<p> ${data.weather[0].description}</p><img src="./Designs/icons/snowy.svg"/>`
-
-        city.innerHTML += `<p>${hourSunRise}:${minutesSunRise}</p><img src="./Designs/icons/sunrise.svg"/>`
-        city.innerHTML += `<p>${hourSunSet}:${minutesSunSet}</p><img src="./Designs/icons/sunset.svg"/>`
+        showSunRise.innerHTML += `<p>${hourSunRise}:${minutesSunRise}</p><img src="./Designs/icons/sunrise.svg"/>`
+        showSunSet.innerHTML += `<p>${hourSunSet}:${minutesSunSet}</p><img src="./Designs/icons/sunset.svg"/>`
        
     })
 
@@ -68,9 +73,9 @@ fetch (`https://api.openweathermap.org/data/2.5/weather?q=${cityName}&units=metr
             const filteredForecast = data.list.filter(item => item.dt_txt.includes('12:00'))
             
                 filteredForecast.forEach((day) => {
-                    allDays.innerHTML += `<div> Temperature: ${day.main.temp} Feels like: ${day.main.feels_like}</div>`
+                    allDays.innerHTML += `<div class="weekday"> <p>Monday</p></div>`
                     if (day.weather[0].id >= 200 && day.weather[0].id <= 299) {
-                        allDays.innerHTML += `<img src="./Designs/icons/thunderstorm.svg"/>`
+                        allDays.innerHTML += `<div class="icon"><img src="./Designs/icons/thunderstorm.svg"/></div>`
                     } else if (day.weather[0].id >= 300 && day.weather[0].id <= 399) {
                         allDays.innerHTML += `<img src="./Designs/icons/drizzle.svg"/>`
                     } else if (day.weather[0].id >= 500 && day.weather[0].id <= 599) {
@@ -82,8 +87,10 @@ fetch (`https://api.openweathermap.org/data/2.5/weather?q=${cityName}&units=metr
                     } else if (day.weather[0].id == 800 ) {
                         allDays.innerHTML += `<img src="./Designs/icons/sunny.svg"/>`
                     } else if (day.weather[0].id >= 801 && day.weather[0].id <= 899) {
-                        allDays.innerHTML += `<img src="./Designs/icons/cloudy.svg"/>`
+                        allDays.innerHTML += `<img src="./Designs/icons/cloudy.svg"/>`   
                     }
+                    
+                    allDays.innerHTML += `<div class="forcast-temp"> <p>${day.main.temp} Feels like: ${day.main.feels_like} </p></div>`
             })
         })
           
