e4605f1e6189abb5f0216a43c52aca62033743a2
“[ida <[ida.t.sylven@gmail.com]>
2021-02-18 10:54:27 +0100

    Revised flexboxes to forecast section. Started the detailed styling of main section

--- script.js
+++ script.js
@@ -16,15 +16,13 @@ fetch(apiUrl)
     .then((json) => {
         city.innerHTML = json.name
 
-        let currentTempValue = json.main.temp
-        currentTempValue = currentTempValue.toFixed(1)
-        currentTemp.innerHTML += `${currentTempValue}°c`
+        let currentTempValue = (json.main.temp).toFixed(1)
+        currentTemp.innerHTML += currentTempValue
 
         weatherDescrValue.innerHTML = json.weather[0].description
 
-        let feelsLikeTempValue = json.main.feels_like
-        feelsLikeTempValue = feelsLikeTempValue.toFixed(1)
-        feelsLikeTemp.innerHTML += `${feelsLikeTempValue}°c`
+        let feelsLikeTempValue = (json.main.feels_like).toFixed(1)
+        feelsLikeTemp.innerHTML += feelsLikeTempValue
 
         let sunriseValue = new Date((json.sys.sunrise) * 1000)
         sunrise.innerHTML += `0${sunriseValue.getHours()}:${sunriseValue.getMinutes()}`
@@ -34,8 +32,7 @@ fetch(apiUrl)
 
         let iconValue = json.weather[0].icon
         console.log(iconValue)
-        currentWeatherIcon.innerHTML = ` <img id = "currentWeatherIcon"
-            src = "http://openweathermap.org/img/wn/${iconValue}.png"/> `
+        currentWeatherIcon.innerHTML = ` <img src="http://openweathermap.org/img/wn/${iconValue}.png"/> `
     })
 
 fetch(apiUrlForecast)
@@ -54,14 +51,16 @@ fetch(apiUrlForecast)
             const forecastTemp = (filteredForecast[index].main.temp).toFixed(1)
             const feelsLikeTempForecast = (filteredForecast[index].main.feels_like.toFixed(1))
 
-            console.log('hej', index)
-
             weeklyForecastContainer.innerHTML += `
-            <div class="weekday">
-                <p>${weekDay.toLocaleDateString('en-US', {weekday: 'short'})}</p>
-                <img src = "http://openweathermap.org/img/wn/${iconValue2}.png"/>
-                <p>${forecastTemp} °C</p>
-                <p>Feels like ${feelsLikeTempForecast} °C</p>
+            <div class="weekday-forecast-div">
+                <div class="weekday">
+                    <p>${weekDay.toLocaleDateString('en-US', {weekday: 'short'})}</p>
+                </div>
+                <div class="icons-and-temp">
+                    <img class="forecast-icons" src="http://openweathermap.org/img/wn/${iconValue2}.png"/>
+                    <p>${forecastTemp} °/</p>
+                    <p class="feels-like-temp-text">Feels like ${feelsLikeTempForecast} °C</p>
+                </div>
             </div>
         `
         })
