b915351258d0dd1581294d41a5bdbaf28849c8aa
liaback <felicia.liaback@gmail.com>
2021-02-18 16:38:46 +0100

    add .catch

--- code/script.js
+++ code/script.js
@@ -1,47 +1,63 @@
-// URL
-const apiDailyWeather = 'https://api.openweathermap.org/data/2.5/weather?q=Stockholm,Sweden&units=metric&APPID=2ecf28ca2c29fa0578cb610c6c66c223'
-const apiWeeklyWeather = 'https://api.openweathermap.org/data/2.5/forecast?q=Stockholm,Sweden&units=metric&APPID=2ecf28ca2c29fa0578cb610c6c66c223'
+// URL (rename to URL and capitals)
+const URLDAILYWEATHER = 'https://api.openweathermap.org/data/2.5/weather?q=Stockholm,Sweden&units=metric&APPID=2ecf28ca2c29fa0578cb610c6c66c223'
+const URLWEEKLYWEATHER = 'https://api.openweathermap.org/data/2.5/forecast?q=Stockholm,Sweden&units=metric&APPID=2ecf28ca2c29fa0578cb610c6c66c223'
+
 // All the DOM selectors stored as short variables
+const containerDailyWeather = document.getElementById('containerDailyWeather')
+const dailyTemp = document.getElementById('dailyTemp') 
+const sunrise = document.getElementById('sunrise')
+const sunset = document.getElementById('sunset')
 const city = document.getElementById('location')
 const mainWeather = document.getElementById('mainWeather')
 
-// fetch daily weather
-fetch (apiDailyWeather)
+// fetch URL for daily weather (can make it shorter - one line)
+fetch (URLDAILYWEATHER)
+    // recive the response and unpack it
     .then((response) => {
         return response.json()
     })
+    // recive the unpacked response and do something with the data
     .then((data) => {
-        console.log("main api", data)
-        //console.log(data.main.temp)
+        //console.log("daily URL", data)
 
-        // variable to round temperature
+        // round up todays temperature, no decimal
         const temp = data.main.temp
-        const roundedTemp = temp.toFixed() // no decimal since () is empty
-        //console.log(roundedTemp)
+        const roundedTemp = temp.toFixed()
+        dailyTemp.innerHTML = `Todays temperature: ${roundedTemp}`
 
         // sunrise and sunset
+        const sunriseHour = new Date((data.sys.sunrise)*1000).toLocaleTimeString('se-SE', {hour:'numeric', minute: 'numeric'})
+            
+            sunrise.innerHTML = `Sunrise: ${sunriseHour}`
         
+        const sunsetHour = new Date(data.sys.sunset*1000).toLocaleTimeString('se-SE', {hour:'numeric', minute: 'numeric'})
+            sunset.innerHTML = `Sunset: ${sunsetHour}`
 
         // get todays weather in weatehr-array using map to iterate and find right object and then return the value
         const fetchedWeather = data.weather.map(
             (weatherData) => {
-            //console.log(weatherData.main)
             return weatherData.main
             })
-
-        // add function to get the time right here?
-
-            //inner HTML for main forcast section
-            city.innerHTML = `The wather in ${data.name}:`
+            city.innerHTML = `The weather in + ${data.name}:`
             mainWeather.innerHTML = `${fetchedWeather}`
-            dailyForecast.innerHTML = `${roundedTemp}`
     })
 
-    // fetch weekly weather
-    fetch (apiWeeklyWeather)
+    //.cath(error => {}) always something we should do. Add if else to first .then() depending on what to return
+    .catch( err => {
+        //console.error(err)
+        containerDailyWeather.innerHTML = 'error!' + err
+    })    
+    .finally(() => console.log('Request finished'))
+    //.finally(() => console.log(Request finished)) always executed, dont have to do it this week.
+
+    // fetch URL for weekly forecast
+    fetch (URLWEEKLYWEATHER)
         .then((response) => {
             return response.json()
         })
         .then((data) => {
-            console.log("weekly api",data)
+            console.log("weekly URL",data)
+            //copied from step 4
+            //const filteredForcast = data.list.filter(item => item.dt_txt.includes('12:00'))
+            //console.log(filteredForcast)
         })
