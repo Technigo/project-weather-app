be42ff3fa3fd5f77f4e343ca407aae6e67819ac3
Laima <Laima@jefimss-mbp.mynet>
2021-02-18 15:25:34 +0100

    backgroud images change

--- code/index.js
+++ code/index.js
@@ -6,6 +6,8 @@ let sunrise = document.getElementById('sunrise')
 let sunriseAndSunset = document.getElementById('sunrise-and-sunset')
 let shortForecast = document.getElementById('three-day-forecast')
 let longForecast = document.getElementById('five-day-forecast')
+let icons = document.getElementById('icons')
+let backgroundImage = document.getElementById('img-background')
 
 // Functions
 let time = (unix) => {
@@ -41,8 +43,6 @@ fetch(APIurl)
         temperature.innerHTML = `${(weatherData.main.temp).toFixed(1)}&#8451`
         typeOfWeather.innerHTML = `${weatherData.weather[0].description}`
 
-
-
         let sunriseTime = time(weatherData.sys.sunrise) /*calling the time function with each sunrise and sunset time*/
         let sunsetTime = time(weatherData.sys.sunset)
 
@@ -50,6 +50,29 @@ fetch(APIurl)
         <h4>Sunrise: ${sunriseTime}</h4>
         <h4>Sunset: ${sunsetTime}</h4>
         `
+            // weatherData.main.temp = 30 /*use for testing of if-string since temperature is almost always below 0 now :)*/
+        if (weatherData.main.temp < 0) {
+            backgroundImage.innerHTML += `
+            <img id="img-background" class="img-background" alt="" src="/assets/foggy.png">
+            `
+        } else if (weatherData.main.temp > 0 && weatherData.main.temp < 3) {
+            backgroundImage.innerHTML += `
+            <img id="img-background" class="img-background" alt="" src="/assets/windy.png">
+            `
+        } else if (weatherData.main.temp > 3 && weatherData.main.temp < 10) {
+            backgroundImage.innerHTML += `
+            <img id="img-background" class="img-background" alt="" src="/assets/clear.png">
+            `
+        } else if (weatherData.main.temp >= 10 && weatherData.main.temp <= 20) {
+            backgroundImage.innerHTML += `
+            <img id="img-background" class="img-background" alt="" src="/assets/rainy.png">
+            `
+        } else {
+            backgroundImage.innerHTML += `
+            <img id="img-background" class="img-background" alt="" src="/assets/sunny3.png">
+            `
+
+        }
     })
 
 fetch(forecastAPIUrl)
@@ -59,26 +82,38 @@ fetch(forecastAPIUrl)
     .then((forecastData) => {
         const filteredForecast = forecastData.list.filter(item => item.dt_txt.includes('12:00')) /*function taken from hint-section to filter out forecasts from 12:00 PM*/
         const threeDayForecast = filteredForecast.slice(0, 3) //Forecast for three days (mobile view)
-            // const weatherStatus = filteredForecast.list.filter(status => status.weather.name)
-            // const weatherStatus = filteredForecast.forEach(forecastData.list[0].weather[0].main)
+
+        // let forecastDescriptions = filteredForecast.map(forecastObject => { /*a function to create a new array of the description for the forecast*/
+        //     return forecastObject.weather[0].main
+        // })
+        // console.log(forecastDescriptions)
+
+        // forecastDescriptions.forEach(element => {
+        //     if (element === 'Clouds') {
+        //         icons.innerHTML += `
+        //         src='./assets/clouds.png'
+        //         `
+        //         cloudImg = `<img  `
+
+        //     }
+        //     console.log(element)
+        // })
 
 
-        filteredForecast.forEach((object) => {
-                return object.weather
-            })
-            // const weatherStatus = forecastData.weather.map((value) => {
-            //     return value.main
-            // })
 
-        console.log(filteredForecast)
+
+
 
         /*a forEach that interates through the filtered array extracting the min and max temp of the comming 5 days*/
         threeDayForecast.forEach((forecastSingle) => {
-            shortForecast.innerHTML += `
+
+
+            const weatherIconStatus =
+                shortForecast.innerHTML += `
                 <div class="forecast">
                 <div class="forecast-elements">
                 <p class="days-of-week">${daysOfWeek(forecastSingle.dt)}</p>
-                <img class="icons" src="/assets/snowflake.png" alt="">
+                <img class="icons" id="icons" src="/assets/clouds.png">
                 <p class="forecast-temp">${(forecastSingle.main.temp_max).toFixed(0)} &#8451 </p>
                 </div>
                 </div>
@@ -89,7 +124,7 @@ fetch(forecastAPIUrl)
                     <div class="forecast">
                     <div class="forecast-elements">
                     <p class="days-of-week">${daysOfWeek(forecastSingle.dt)}</p>
-                    <img class="icons" src="/assets/snowflake.png" alt="">
+                    <img class="icons" src="/assets/snow.png" alt="">
                     <p class="forecast-temp">${(forecastSingle.main.temp_max).toFixed(0)} &#8451 </p>
                     </div>
                     </div>
@@ -97,22 +132,3 @@ fetch(forecastAPIUrl)
         })
 
     })
-
-
-
-
-
-
-//mood.innerHTML += `<h2>Light a fire and get cosy. ${data.name} is looking grey today.</h2>`
-// const weatherMessage = () => {
-//     if (weatherDescription[0] === "Clouds") {
-//         console.log('working')
-//         currentWeather.innerHTML += `<img src="./noun_Cloud_1188486.svg">`
-//         mood.innerHTML += `<h2>Light a fire and get cosy. ${data.name} is looking grey today.</h2>`
-//     } else if (weatherDescription === "Rain") {
-//         mood.innerHTML += `<img src="./noun_Cloud_1188486.svg">`
-//         mood.innerHTML += `<h2>Light a fire and get cosy. ${data.name} is looking grey today.</h2>`
-//     }
-// }
-// weatherMessage()
-// })
