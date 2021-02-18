9f4d8334b575b93eb57cd31e79ef06e60abd7dbb
jess-stack <72085164+jess-stack@users.noreply.github.com>
2021-02-18 19:31:27 +0100

    changed icon and created function for date to work in ios

--- code/script.js
+++ code/script.js
@@ -46,9 +46,10 @@ const getWeatherData = (data) => {
 
 // Function for forecast
 const getForecastData = (data) => {
+  const getDate = date => new Date(date.replace(' ', 'T')).toLocaleDateString('en-US', {weekday: 'long'});
   const filteredForecast = data.list.filter(item => item.dt_txt.includes('12:00'));
   filteredForecast.forEach((forecastItem) => {
-    const loopOverWeek = new Date(forecastItem.dt_txt + 'Z').toLocaleDateString('en-US', {weekday: 'long'});
+    const loopOverWeek = getDate(forecastItem.dt_txt);
     const loopOverTemp = Math.floor(forecastItem.main.temp);
     const icon = `https://openweathermap.org/img/wn/${forecastItem.weather[0].icon}@2x.png`;
     forecast.innerHTML +=  `
