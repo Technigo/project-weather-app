2471327ca71e37a9752be0f9f7afcfab7e7a9c97
jess-stack <72085164+jess-stack@users.noreply.github.com>
2021-02-18 11:10:14 +0100

    Fixed styling and weather icons. Added background change for day and night

--- code/script.js
+++ code/script.js
@@ -8,13 +8,14 @@ const description = document.getElementById("description")
 const forecast = document.getElementById("forecast")
 const icon = document.getElementById("icon")
 const bigIcon = document.getElementById("big-icon")
+const today = document.getElementById("today")
 
 // Global variables
-const API_KEY = "d54b10c260730aa99d10c1f676d759e6"
+const API_KEY = "d54b10c260730aa99d10c1f676d759e6";
 let city = "Stockholm"
 
 const sunTime = (time) => {
-    let fixedTime = new Date(time * 1000).toLocaleTimeString('sv-SE', {
+    const fixedTime = new Date(time * 1000).toLocaleTimeString('sv-SE', {
         hour: "2-digit",
         minute: "2-digit",
         hour12: false
@@ -22,40 +23,47 @@ const sunTime = (time) => {
     return fixedTime;
 };
 
+const getWeatherData = (data) => {
+    const temp = Math.floor(data.main.temp);
+    const condition = data.weather[0].description;
+    const icon = `https://openweathermap.org/img/wn/${data.weather[0].icon}@2x.png`;
+    const sunriseText = sunTime(data.sys.sunrise);
+    const sunsetText = sunTime(data.sys.sunset);
+    bigIcon.innerHTML = `<img src="${icon}"/>`;
+    shortDescription.innerText = `${condition}`;   
+    temperature.innerHTML = `${temp} <span class="celsius">°C</span>`;
+    sunrise.innerText = `sunrise: ${sunriseText}`;
+    sunset.innerText = `sunset: ${sunsetText}`;
+    let changeBackground = new Date().getHours();
+    console.log(changeBackground);
+        if (changeBackground > 20 && changeBackground < 6) {
+            today.style.background = "linear-gradient(to bottom, #222350, #313263, #404176, #4f518a, #5f619f)"
+        } 
+}
+
 fetch(`http://api.openweathermap.org/data/2.5/weather?q=Stockholm,Sweden&units=metric&APPID=${API_KEY}`)
     .then((response) => {
         return response.json();
     })
     .then((data) => {
-        const temp = Math.floor(data.main.temp)  
-        const condition = data.weather[0].description
-        const icon = `https://s3-us-west-2.amazonaws.com/s.cdpn.io/162656/${data.weather[0].icon}.svg`
-        const sunriseText = sunTime(data.sys.sunrise);
-        const sunsetText = sunTime(data.sys.sunset);
-        bigIcon.innerHTML = `<img class="big-image" id="big-icon" src="${icon}"/>`
-        shortDescription.innerText = `${condition}`   
-        temperature.innerHTML = `${temp} <span class="celsius">°C</span>`
-        sunrise.innerText = `Sunrise: ${sunriseText}`
-        sunset.innerText = `Sunset: ${sunsetText}`
-        console.log(temp) 
-    })
+        getWeatherData(data);
+    });
 
     
-let weekdays = ["Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday"];
+const weekdays = ["Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday"];
 
 fetch(`https://api.openweathermap.org/data/2.5/forecast?q=Stockholm,Sweden&units=metric&APPID=${API_KEY}`)
     .then((response) => {
         return response.json();
     })
     .then((data) => {
-        const filteredForecast = data.list.filter(item => item.dt_txt.includes('12:00'))
+        console.log(data);
+        const filteredForecast = data.list.filter(item => item.dt_txt.includes('12:00'));
         filteredForecast.forEach((forecastItem) => {
-            let loopOverWeek = weekdays[(new Date(forecastItem.dt_txt).getDay())]
-            let loopOverTemp = Math.floor(forecastItem.main.temp);
-            let icon = `https://s3-us-west-2.amazonaws.com/s.cdpn.io/162656/${forecastItem.weather[0].icon}.svg`;
+            const loopOverWeek = weekdays[(new Date(forecastItem.dt_txt).getDay())];
+            const loopOverTemp = Math.floor(forecastItem.main.temp);
+            const icon = `https://openweathermap.org/img/wn/${forecastItem.weather[0].icon}@2x.png`;
 
-            console.log(bigIcon);
-            
             forecast.innerHTML +=  `
             <div>
                 <p>${loopOverWeek}</p>
