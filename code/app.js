aee05350fc8f61e34b6d67ee3c3028a2b6f2c3fa
Linnéa Borealis <70851837+Polkapetunian@users.noreply.github.com>
2021-02-19 09:36:59 +0100

    Added other icons

--- code/app.js
+++ code/app.js
@@ -1,48 +1,102 @@
-// Global Variables
-let cityNameStockholm = document.getElementById("cityNameStockholm");
-let stockholmSunrise = document.getElementById("stockholmSunrise");
-let stockholmSunset = document.getElementById("stockholmSunset");
-let stockholmTemperature = document.getElementById("stockholmTemperature");
-let cityNameCapeTown = document.getElementById("cityNameCapeTown");
-let capeTownSunrise = document.getElementById("capeTownSunrise");
-let capeTownSunset = document.getElementById("capeTownSunset");
-let capeTownTemperature = document.getElementById("capeTownTemperature");
+//Variables
+const weatherSection = document.getElementById('weatherSection');
+const forecast = document.getElementById('forecast');
 
-//APIs
-const CAPE_TOWN = 'http://api.openweathermap.org/data/2.5/weather?id=3369157&units=metric&appid=3b3dfc4920c50815af4eaabe044bdc31';
-const STOCKHOLM = 'http://api.openweathermap.org/data/2.5/weather?id=2673730&units=metric&appid=3b3dfc4920c50815af4eaabe044bdc31';
-const weatherCapeTown = () => {
-  fetch(CAPE_TOWN)
-    .then((response) => {
-      return response.json();
+//Cape Town Weather API
+const capeTownForecast = 'https://api.openweathermap.org/data/2.5/forecast?id=3369157&units=metric&appid=3b3dfc4920c50815af4eaabe044bdc31';
+const capeTownWeather = 'https://api.openweathermap.org/data/2.5/weather?id=3369157&units=metric&appid=3b3dfc4920c50815af4eaabe044bdc31';
+
+
+let returnWeekDay = (date) => { 
+  let daysInWeek = ["Sunday", "Monday","Tuesday", "Wednesday","Thursday","Friday","Saturday"];
+  let inputDate = new Date(date.replace(' ', 'T'));
+  return daysInWeek[inputDate.getDay()];
+}
+
+// Fetch Cape Town current weather
+const fetchTodaysWeather = () => {
+fetch(capeTownWeather)
+  .then((response) => {
+    if (response.ok) {
+    return response.json()
+    } else {
+      throw "Error";
+    }
+  })
+  .then((json)=> {
+    let tempToday = `${json.main.temp}`
+    let tempTodayRounded = Math.round(tempToday)
+    let cityName = json.name;
+    let weatherDescription = json.weather[0].description;
+    let iconID = json.weather[0].icon;
+    let sunriseAPI = `${json.sys.sunrise}`
+    let sunsetAPI = `${json.sys.sunset}`
+    let sunrise = new Date(sunriseAPI*1000).toLocaleTimeString([], {timeStyle: 'short'});
+    let sunset = new Date(sunsetAPI*1000).toLocaleTimeString([], {timeStyle: 'short'});
+    
+    weatherSection.innerHTML = `
+    <div class="current-weather">
+      <div class="hamburger-container">
+        <img class="hamburger-menu" src="hamburger.svg">
+      </div>
+      <div class="current-temp-and-icon">
+        <h2 class="current-temp">${tempTodayRounded}</h2>
+        <p class="celsius">°C</p> 
+        <div class="weather-icon">
+        <img class="current-icon" src="./icons/${iconID}.svg">
+      </div>
+    </div>
+      <div>
+        <h1 class="city-name">${cityName}</h1>  
+        <h3 class="weather-description">${weatherDescription} </h3>
+      </div>
+    </div>
+    <div class="sunrise-sunset">
+      <p>Sunrise: ${sunrise}</p>
+      <p>Sunset: ${sunset}</p>
+    </div> 
+    `
+  })
+  .catch(error => {
+    weatherSection.innerHTML = `${error}`
+  })
+}
+  
+
+// Fetch Cape Town 5 Day Forecast
+const fetch5DayForecast = () => {
+fetch(capeTownForecast)
+  .then((response) => {
+    if (response.ok) {
+    return response.json()
+    } else {
+      throw "Error";
+    }
+  })
+  .then((json) => {
+    const filteredForecast = json.list.filter(item => item.dt_txt.includes('12:00'))
+    console.log(filteredForecast);
+
+    filteredForecast.forEach((filteredForecast) => {
+      let dayInWeek = returnWeekDay(filteredForecast.dt_txt);
+      let temp5Days = `${filteredForecast.main.temp}`
+      let temp5DaysRounded = Math.round(temp5Days)
+      let iconID = filteredForecast.weather[0].icon;
+      forecast.innerHTML += `
+      <div class="five-day-forecast">
+        <p> ${dayInWeek}:</p>
+        <img class="forecast-icon" src="./icons/${iconID}.svg">
+        <p> ${temp5DaysRounded}°C </p>
+      </div>
+      `
     })
-    .then((json) => {
-      console.log(json);
-
-      cityNameCapeTown.innerHTML = json.name;
-      capeTownTemperature.innerHTML = `${json.main.temp}℃`;
-      capeTownSunriseDate = new Date(json.sys.sunrise * 1000);
-      capeTownSunsetDate = new Date(json.sys.sunset * 1000);
-      capeTownSunrise.innerHTML = `Sunrise:${capeTownSunriseDate.getHours()}:${capeTownSunriseDate.getMinutes()}`; //change timezone if coder not in Cape Town
-      capeTownSunset.innerHTML = `Sunset:${capeTownSunsetDate.getHours()}:${capeTownSunsetDate.getMinutes()}` //change timezone if coder not in Cape Town
-    });
+  })
+  .catch(error => {
+    filteredForecast.innerHTML = `${error}`
+  })
 };
-weatherCapeTown();
 
-const weatherStockholm = () => {
-  fetch(STOCKHOLM)
-    .then((response) => {
-      return response.json();
-    })
-    .then((json) => {
-      console.log(json);
+  
 
-      cityNameStockholm.innerHTML = json.name;
-      stockholmTemperature.innerHTML = `${json.main.temp}℃`;
-      stockholmSunriseDate = new Date((json.sys.sunrise + json.timezone) * 1000); //take away (+ json.timezone) if coder not in Cape Town
-      stockholmSunsetDate = new Date((json.sys.sunset + json.timezone) * 1000); //take away (+ json.timezone) if coder not in Cape Town
-      stockholmSunrise.innerHTML = `Sunrise:${stockholmSunriseDate.getHours()}:${stockholmSunriseDate.getMinutes()}`;
-      stockholmSunset.innerHTML = `Sunset:${stockholmSunsetDate.getHours()}:${stockholmSunsetDate.getMinutes()}`
-    });
-};
-weatherStockholm();
+fetchTodaysWeather(); 
+fetch5DayForecast();
