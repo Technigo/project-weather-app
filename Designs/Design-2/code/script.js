83891e7d4c110211222aedb4a6d3620fa711bb23
Irina <jekabsone.irina@gmail.com>
2021-02-19 20:16:57 +0100

    README and dotted underline in forecast section to change the color

--- Designs/Design-2/code/script.js
+++ Designs/Design-2/code/script.js
@@ -1,5 +1,5 @@
-const CURRENT_WEATHER_URL = "http://api.openweathermap.org/data/2.5/weather?q=Helsinki&units=metric&appid=9aeed004b4643ec679ac5d430384d994"
-const FORECAST_URL = "http://api.openweathermap.org/data/2.5/forecast?q=Helsinki&units=metric&appid=9aeed004b4643ec679ac5d430384d994"
+const CURRENT_WEATHER_URL = "https://api.openweathermap.org/data/2.5/weather?q=Helsinki&units=metric&appid=9aeed004b4643ec679ac5d430384d994"
+const FORECAST_URL = "https://api.openweathermap.org/data/2.5/forecast?q=Helsinki&units=metric&appid=9aeed004b4643ec679ac5d430384d994"
 
 const cityName = document.getElementById("cityName")
 const currentTemperature = document.getElementById("currentTemperature")
@@ -32,6 +32,7 @@ const fetchCurrentWeather = () => {
             const cloudIcon = "../icons/noun_Cloud_1188486.svg"
             const sunglassesIcon = "../icons/noun_Sunglasses_2055147.svg"
             const umbrellaIcon = "../icons/noun_Umbrella_2030530.svg"
+            let container = document.getElementById("container");
             
             //Showing temperature, sunrise, sunset and city name
             currentTemperature.innerHTML = `<p>${data.weather[0].main} | ${data.main.temp.toFixed(0)}&deg;<p>` 
@@ -39,27 +40,27 @@ const fetchCurrentWeather = () => {
             sunset.innerHTML += `<p>Sunset ${sunsetDate.getHours()}:${sunsetDate.getMinutes()}<p>`
             cityName.innerHTML += `<h1>${data.name}<h1>`
 
-            //Showing proper icon and text depending on weatherDescription
+            //Showing proper icon, colors and text depending on weatherDescription
             if (weatherDescription === "clear sky"){
                 icon.innerHTML += `<img src=${sunglassesIcon}></img>`
                 weatherMessage.innerHTML += `<h2>Get your sunnies on! ${data.name} is looking rather great today.</h2>`
-                
-                let container = document.getElementById("container");
-                let forecast = document.getElementById("forecast");
                 container.style.backgroundColor = "#F7E9B9";
                 container.style.color = "#2A5510";
-                //Change the color for border bottom!!!
+                forecastInformation.style.textDecorationColor = "#2A5510"
 
             } else if (weatherDescription === "thunderstorm" || "rain" || "shower rain" || "snow"){
                 icon.innerHTML += `<img src=${umbrellaIcon}> </img>`
                 weatherMessage.innerHTML += `<h2>Don't forget your umbrella. It's wet in ${data.name} today.</h2>`
                 container.style.backgroundColor = "#A3DEF7";
                 container.style.color = "#164A68";
+                forecastInformation.style.textDecorationColor = "#164A68"
+
             } else {
                 icon.innerHTML += `<img src=${cloudIcon}> </img>`
                 weatherMessage.innerHTML += `<h2>Light a fire, get cosy ${data.name} is looking grey today.</h2>`
                 container.style.backgroundColor = "#F47775";
                 container.style.color = "#F4F7F8";
+                forecastInformation.style.textDecorationColor = "#F4F7F8"
             }        
         })
         // Method to catch errors during fetch()
@@ -91,11 +92,11 @@ const fetchForecastWeather = () => {
                 const now = new Date();
                 // Compare the forecast's day with the day right now
                 const isTodaysForecast = date.getDay() === now.getDay();
-                const weekdays = ["mon", "tue", "wed", "thu", "fri", "sat", "sun"]
+                const weekdays = ["sun", "mon", "tue", "wed", "thu", "fri", "sat"]
                 let dayOfTheWeek = weekdays[date.getDay()]
                 // We don't want to include this forecast if it is for today
                     if(!isTodaysForecast){
-                        //Is there a better way to create a space in the text?
+                        //We tried to create the space with <span>, but then we couldn´t get the underline work properly, so we left &nbsp; 
                         forecastInformation.innerHTML += `<h3 id="forecast" class="forecast">${dayOfTheWeek}&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
                         &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
                         ${data.main.temp.toFixed(0)}&deg;</h3>`
--- README.md
+++ README.md
@@ -1,13 +1,21 @@
 # Weather App
 
-Replace this readme with your own information about your project.
-
-Start by briefly describing the assignment in a sentence or two. Keep it short and to the point.
+Our project for this week was to work with pair programming and create a simple weather app with mobile first approach, also responsive for tablet and desktop, that shows today's weather information for one city, and a 5-day forecast using a weather API.
 
 ## The problem
 
-Describe how you approached to problem, and what tools and techniques you used to solve it. How did you plan? What technologies did you use? If you had more time, what would be next?
+We decided to replicate on of the provided designs and chose Helsinki as the city to show weather information about.
+We were concentrating step by step on blue level requirements, making sure that both of as understand the code. And we have also reached few goals from the red level. We made the colors and icons to change based on the weather information. It was done by using an if statement.
+
+First of all we created basic HTML structure with ids and classes. To reflect information about the current weather and the foreacast we used two API endpoints and made them as constant urls at the begining of the JavaScript file. Then we used two fetch() functions – one to fetch information about the current weather and another one for the forecast. To present data on our web app we injected values into the DOM from the JSON file which comes from the API. 
+
+Since the API gives the forecast on the next 5 days for every third hour, we used filter() function to filter the forecast list array to only get the information from 12:00 each day. And by using forEach() method we looped through that array to present a day and temperature information on our web app. And we also used Date() object to present date and time information in a way we want it to be.
+
+Once we had all information needed on our web app, we styled it in CSS to look like one of the provided designs.
+
+To solve problems, we used console.log a lot, we logged every part of the code in console to see which parts are not working properly, and we also used dev tools to see where the problem in the code is. And we googled a lot as well, for example, to find how to get the temperature to 1 decimal. And we managed to solve most of the struggles this way. We also asked question on Stack Overflow and during the Q&A session.
+
 
 ## View it live
 
-Every project should be deployed somewhere. Be sure to include the link to the deployed project so that the viewer can click around and see what it's all about.
+Link to published site:
