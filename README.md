fffe21a4175c4684d46bfa8062cee081edbbd4c6
Sara Carlstein <carlstein.sara@gmail.com>
2021-02-19 13:11:36 +0100

    changes in readme

--- README.md
+++ README.md
@@ -1,13 +1,11 @@
 # Weather App
 
-Replace this readme with your own information about your project.
-
-Start by briefly describing the assignment in a sentence or two. Keep it short and to the point.
+Programming together in pairs to build a weather dashboard that tells today's weather and temperature using a public weather API.
 
 ## The problem
 
-Describe how you approached to problem, and what tools and techniques you used to solve it. How did you plan? What technologies did you use? If you had more time, what would be next?
+Fetching real time data through API end points. Problem we encountered included fetching data from the API's to display the information. Making it responsive on website, mobile and tablet. Tools, team work, discussions, google, stackOverflow. If we had more time we would add multiple cities and more styling in CSS. 
 
 ## View it live
 
-Every project should be deployed somewhere. Be sure to include the link to the deployed project so that the viewer can click around and see what it's all about.
+The project can be viewed live here: https://inspiring-archimedes-330d55.netlify.app/
--- script.js
+++ script.js
@@ -1,5 +1,3 @@
-// const weatherAPI = 'https://api.openweathermap.org/data/2.5/weather?q=Stockholm,Sweden&units=metric&APPID=b875a4e82ea7797a6ff76c50f02a0b8b'
-let cityName = "Stockholm" 
 const city = document.getElementById('city')
 const showSunRise = document.getElementById('sunRise')
 const showSunSet = document.getElementById('sunSet')
@@ -10,16 +8,17 @@ const body = document.getElementById('body')
 const icons = document.getElementById('icons')
 const weekday = document.getElementById('weekday')
 const temp = document.getElementById('temp')
-const dayOfTheWeek = ["Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday"];
 
+//Global variables 
+const dayOfTheWeek = ["Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday"]
 let minMax, currentDate, minTemp, maxTemp
+let cityName = "Stockholm" 
 
-
+//current weather
 fetch (`https://api.openweathermap.org/data/2.5/weather?q=${cityName}&units=metric&APPID=b875a4e82ea7797a6ff76c50f02a0b8b`)
     .then((response) => {
-        return response.json();
+        return response.json()
     }).then ((data) => {
-        
         
         let cityTemp = data.main.temp.toFixed(1)
         let sunRise = new Date(data.sys.sunrise * 1000)
@@ -43,13 +42,11 @@ fetch (`https://api.openweathermap.org/data/2.5/weather?q=${cityName}&units=metr
 
         //changing background and image depending on time of day
         const changeBackground = () => {
-            const time = (new Date(data.dt * 1000).getHours());
+            const time = (new Date(data.dt * 1000).getHours())
                 if (time >= 7 && time <= 17) {
                     weatherDescDiv.classList.add('day')
                     body.classList.add('day-time')
-                    console.log("day")
                 } else if (time >= 18) {
-                    console.log("night")
                     weatherDescDiv.classList.add('night')
                     body.classList.add('night-time')
                 } else {
@@ -58,8 +55,6 @@ fetch (`https://api.openweathermap.org/data/2.5/weather?q=${cityName}&units=metr
                 }
             }
         changeBackground()  
-        
-        console.log(new Date(data.dt * 1000).getDay())
        
         //fetching city name and current temprature and adding in html
         city.innerHTML += `<p class="current-city"> ${data.name}</p>`
@@ -83,70 +78,68 @@ fetch (`https://api.openweathermap.org/data/2.5/weather?q=${cityName}&units=metr
             } else if (data.weather[0].id >= 801 && data.weather[0].id <= 899) {
                 currentWeather.innerHTML += `<img src="./Designs/icons/cloudy.svg"/>`   
             }
-    }
+        }
             showSunRise.innerHTML += `<p>${hourSunRise}:${minutesSunRise}</p><img src="./Designs/icons/sunrise.svg"/>`
             showSunSet.innerHTML += `<p>${hourSunSet}:${minutesSunSet}</p><img src="./Designs/icons/sunset.svg"/>`
             weatherDescription(); 
-        })
+    })
 
-    //forcast 5 days
-    fetch(`https://api.openweathermap.org/data/2.5/forecast?q=${cityName},Sweden&units=metric&APPID=b875a4e82ea7797a6ff76c50f02a0b8b`)
-        .then((response) => {
-            return response.json();
-        }).then((data) => {
-            
-            //forecast including 5 days
-            const filteredForecast = data.list.filter(item => item.dt_txt.includes('12:00'))
+//forcast 5 days
+fetch(`https://api.openweathermap.org/data/2.5/forecast?q=${cityName},Sweden&units=metric&APPID=b875a4e82ea7797a6ff76c50f02a0b8b`)
+    .then((response) => {
+        return response.json();
+    }).then((data) => {
+        
+        //forecast including 5 days
+        const filteredForecast = data.list.filter(item => item.dt_txt.includes('12:00'))
 
-            // finding out min and max temprature for each day
-            const updateMinMaxTemps = () => {
+        // finding out min and max temprature for each day
+        const updateMinMaxTemps = () => {
             minMax = {};
             data.list.forEach((day) => {
-            currentDate = day.dt_txt.split(" ")[0];
+                currentDate = day.dt_txt.split(" ")[0];
 
-            if (minMax[currentDate]) {
-                if (day.main.temp_min < minMax[currentDate].minTemp) {
-                    minMax[currentDate].minTemp = day.main.temp_min;
+                if (minMax[currentDate]) {
+                    if (day.main.temp_min < minMax[currentDate].minTemp) {
+                        minMax[currentDate].minTemp = day.main.temp_min;
+                    }
+                    if (day.main.temp_max > minMax[currentDate].maxTemp) {
+                        minMax[currentDate].maxTemp = day.main.temp_max;
+                    }
+                } else {
+                    minMax[currentDate] = { 
+                        minTemp: day.main.temp_min, 
+                        maxTemp: day.main.temp_max,
+                    }
                 }
-                if (day.main.temp_max > minMax[currentDate].maxTemp) {
-                    minMax[currentDate].maxTemp = day.main.temp_max;
-                }
-            } else {
-                minMax[currentDate] = { 
-                    minTemp: day.main.temp_min, 
-                    maxTemp: day.main.temp_max,
-                }
-            }
-        }) 
-
-        return minMax
-
+            }) 
+            return minMax
         }
 
         updateMinMaxTemps()
-            
-        //fetching the forecast for the next five days 
-            filteredForecast.forEach((day) => {
-                currentDate = day.dt_txt.split(" ")[0];
-                const dateIndex = (new Date(day.dt * 1000)).getDay();
-        //changing icons description and min/max temp depending on the conditions
-                allDays.innerHTML += `<div class="weekday"><p>${dayOfTheWeek[dateIndex]}</p></div>`
-                if (day.weather[0].id >= 200 && day.weather[0].id <= 299) {
-                    allDays.innerHTML += `<img src="./Designs/icons/thunderstorm.svg"/>`
-                } else if (day.weather[0].id >= 300 && day.weather[0].id <= 399) {
-                    allDays.innerHTML += `<img src="./Designs/icons/drizzle.svg"/>`
-                } else if (day.weather[0].id >= 500 && day.weather[0].id <= 599) {
-                    allDays.innerHTML += `<img src="./Designs/icons/rainy.svg"/>`
-                } else if (day.weather[0].id >= 600 && day.weather[0].id <= 699) {
-                    allDays.innerHTML += `<img src="./Designs/icons/snowy.svg"/>`
-                } else if (day.weather[0].id >= 700 && day.weather[0].id <= 799) {
-                    allDays.innerHTML += `<img src="./Designs/icons/atmosphare.svg"/>`
-                } else if (day.weather[0].id == 800 ) {
-                    allDays.innerHTML += `<img src="./Designs/icons/sunny.svg"/>`
-                } else if (day.weather[0].id >= 801 && day.weather[0].id <= 899) {
-                    allDays.innerHTML += `<img src="./Designs/icons/cloudy.svg"/>`   
-                }
-                allDays.innerHTML += `<div class="forcast-temp"> <p>${minMax[currentDate].minTemp.toFixed(0)} &#8451 / ${minMax[currentDate].maxTemp.toFixed(0)} &#8451</p></div>`
-            })
+        
+        //data  for the next five days 
+        filteredForecast.forEach((day) => {
+            currentDate = day.dt_txt.split(" ")[0];
+            const dateIndex = (new Date(day.dt * 1000)).getDay();
+            //changing icons description and min/max temp depending on the conditions
+            allDays.innerHTML += `<div class="weekday"><p>${dayOfTheWeek[dateIndex]}</p></div>`
+            if (day.weather[0].id >= 200 && day.weather[0].id <= 299) {
+                allDays.innerHTML += `<img src="./Designs/icons/thunderstorm.svg"/>`
+            } else if (day.weather[0].id >= 300 && day.weather[0].id <= 399) {
+                allDays.innerHTML += `<img src="./Designs/icons/drizzle.svg"/>`
+            } else if (day.weather[0].id >= 500 && day.weather[0].id <= 599) {
+                allDays.innerHTML += `<img src="./Designs/icons/rainy.svg"/>`
+            } else if (day.weather[0].id >= 600 && day.weather[0].id <= 699) {
+                allDays.innerHTML += `<img src="./Designs/icons/snowy.svg"/>`
+            } else if (day.weather[0].id >= 700 && day.weather[0].id <= 799) {
+                allDays.innerHTML += `<img src="./Designs/icons/atmosphare.svg"/>`
+            } else if (day.weather[0].id == 800 ) {
+                allDays.innerHTML += `<img src="./Designs/icons/sunny.svg"/>`
+            } else if (day.weather[0].id >= 801 && day.weather[0].id <= 899) {
+                allDays.innerHTML += `<img src="./Designs/icons/cloudy.svg"/>`   
+            }
+            allDays.innerHTML += `<div class="forcast-temp"> <p>${minMax[currentDate].minTemp.toFixed(0)} &#8451 / ${minMax[currentDate].maxTemp.toFixed(0)} &#8451</p></div>`
         })
-          
+    })
+        
