0522366d78ce7756004f1df5276cd504715eb354
“[ida <[ida.t.sylven@gmail.com]>
2021-02-19 16:25:08 +0100

    Added the  README section

--- README.md
+++ README.md
@@ -1,13 +1,15 @@
 # Weather App
 
-Replace this readme with your own information about your project.
-
-Start by briefly describing the assignment in a sentence or two. Keep it short and to the point.
+This is the front end boot camp assignment number 6 creating a weather app by fetching data from an API. Part of the practice was also to implement a given design layout. This project was done as a pair-programming assignment.  
 
 ## The problem
 
-Describe how you approached to problem, and what tools and techniques you used to solve it. How did you plan? What technologies did you use? If you had more time, what would be next?
+We have created our code entirely from scratch. We started out by analysing the brief in general, and getting ourselves going with the actual fetch from the weather API in JavaScript. After that we worked our way through the project by creating all necessary functions alongside the general structure of the HTML. It was a lot of fun doing our very first fetch!
+
+The second part of the assignment was so try to do a design replica of a given design sketch. We really embraced this part of the project as a good practice for upcoming projects working as front end developers in the future, being given a set design to put to life. Some of the features have been modified by us, as modest suggestion for improvement from a user perspective.
+
+If we had more time would have liked for the weekdays to be printed in full (e.g "Monday" instead of "Mon"). The website is responsive to various devices, but we would like to fine tune the layouts in Tablet and Desktop view. It would also be fun to add forecasts for additional cities. 
 
 ## View it live
 
-Every project should be deployed somewhere. Be sure to include the link to the deployed project so that the viewer can click around and see what it's all about.
+To view the site live, please follow this link: 
--- script.js
+++ script.js
@@ -1,55 +1,52 @@
 const apiUrl = 'http://api.openweathermap.org/data/2.5/weather?q=Stockholm,Sweden&units=metric&APPID=800ddea444fa8b1532e3492f09d4432d'
 const apiUrlForecast = 'https://api.openweathermap.org/data/2.5/forecast?q=Stockholm,Sweden&units=metric&APPID=800ddea444fa8b1532e3492f09d4432d'
-const city = document.getElementById('city')
-const currentTemp = document.getElementById('currentTemp')
-const feelsLikeTemp = document.getElementById('feelsLikeTemp')
-const weatherDescrValue = document.getElementById('weatherDescription')
-const sunrise = document.getElementById('sunrise')
-const sunset = document.getElementById('sunset')
-const currentWeatherIcon = document.getElementById('currentWeatherIcon')
-
 
 fetch(apiUrl)
     .then((response) => {
         return response.json()
     })
     .then((json) => {
+        const city = document.getElementById('city')
+        const currentTemp = document.getElementById('currentTemp')
+        const feelsLikeTemp = document.getElementById('feelsLikeTemp')
+        const weatherDescrValue = document.getElementById('weatherDescription')
+        const sunrise = document.getElementById('sunrise')
+        const sunset = document.getElementById('sunset')
+        const currentWeatherIcon = document.getElementById('currentWeatherIcon')
+
+        // All data content from API for the main section
         city.innerHTML = json.name
 
-        let currentTempValue = (json.main.temp).toFixed(1)
+        const currentTempValue = (json.main.temp).toFixed(1)
         currentTemp.innerHTML += currentTempValue
 
         weatherDescrValue.innerHTML = json.weather[0].description
 
-        let feelsLikeTempValue = (json.main.feels_like).toFixed(1)
+        const feelsLikeTempValue = (json.main.feels_like).toFixed(1)
         feelsLikeTemp.innerHTML += feelsLikeTempValue
 
-        let sunriseValue = new Date((json.sys.sunrise) * 1000)
+        const sunriseValue = new Date((json.sys.sunrise) * 1000)
         sunrise.innerHTML += `0${sunriseValue.getHours()}:${sunriseValue.getMinutes()}`
-        console.log(sunriseValue)
 
-        let sunsetValue = new Date((json.sys.sunset) * 1000)
+        const sunsetValue = new Date((json.sys.sunset) * 1000)
         sunset.innerHTML += `${sunsetValue.getHours()}:${sunsetValue.getMinutes()}`
 
+        // Changes the background image in main section to day or night, based on sunrise and sunset.
         const mainContainerChange = ((sunriseValue, sunsetValue) => {
 
             const mainContainer = document.getElementById('main')
-            let currentTime = new Date()
-            console.log(currentTime)
-            console.log(sunsetValue)
+            const currentTime = new Date()
 
             if (currentTime >= sunriseValue && currentTime < sunsetValue) {
                 mainContainer.classList.add('main-day')
             } else {
                 mainContainer.classList.add('main-night')
-                console.log('else')
             }
         })
 
         mainContainerChange(sunriseValue, sunsetValue)
 
-        let iconValue = json.weather[0].icon
-        console.log(iconValue)
+        const iconValue = json.weather[0].icon
         currentWeatherIcon.innerHTML += ` <img class="current-weather-icon" src="http://openweathermap.org/img/wn/${iconValue}.png"/> `
     })
 
@@ -59,16 +56,16 @@ fetch(apiUrlForecast)
     })
     .then((json) => {
         const filteredForecast = json.list.filter(day => day.dt_txt.includes('12:00'))
-        console.log(filteredForecast)
-
         const weeklyForecastContainer = document.getElementById('weeklyForecast')
 
+        // All data content from API for the forecast section
         filteredForecast.forEach((day, index) => {
-            let iconValue2 = filteredForecast[index].weather[0].icon
-            let weekDay = new Date((filteredForecast[index].dt) * 1000)
+            const iconValue2 = filteredForecast[index].weather[0].icon
+            const weekDay = new Date((filteredForecast[index].dt) * 1000)
             const forecastTemp = (filteredForecast[index].main.temp).toFixed(1)
             const feelsLikeTempForecast = (filteredForecast[index].main.feels_like.toFixed(1))
 
+            // Inserts the HTML structure and the values for the forecast section
             weeklyForecastContainer.innerHTML += `
             <div class="weekday-forecast-div">
                 <div class="weekday">
