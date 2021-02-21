68048ee667e48025f9630ccb0948e99e7304b445
Maria Sparre <you@example.com>
2021-02-21 01:43:33 +0100

    images added for different media

--- README.md
+++ README.md
@@ -1,13 +1,13 @@
 # Weather App
 
-Replace this readme with your own information about your project.
-
-Start by briefly describing the assignment in a sentence or two. Keep it short and to the point.
+This project's goal is to build a simple weather web app that shows today's weather and temperature, and a 5-day forecast using the Open Weather API 🌤
 
 ## The problem
 
-Describe how you approached to problem, and what tools and techniques you used to solve it. How did you plan? What technologies did you use? If you had more time, what would be next?
+We have used JavScript's fetch() method to get all the data we need from the Open Weather API 🌦 This is our first time working with an API, so we learned about understanding its documentation and the different parameters in its URL.
+Learned about promises and how to extract the data from JSON object 💪
+We also learned about styling in javascript and dared to try animation effects for the icons and background.
 
 ## View it live
 
-Every project should be deployed somewhere. Be sure to include the link to the deployed project so that the viewer can click around and see what it's all about.
+Want to see how the weather is today in Stockholm? You can take a look at our weather app here: https://pedantic-northcutt-25e910.netlify.app
--- code/app.js
+++ code/app.js
@@ -1,8 +1,7 @@
 const todaysWeatherUrl = "https://api.openweathermap.org/data/2.5/weather?q=Stockholm,Sweden&units=metric&APPID=f463a96f9ee6b3233c3a141a391ac3cf"
-//const todaysWeatherUrlBA = "https://api.openweathermap.org/data/2.5/weather?q=BsAs,Argentina&units=metric&APPID=96757b909ab14fd81c8ce5bb9ee9ad53"
+const todaysWeatherBar = "https://api.openweathermap.org/data/2.5/weather?q=Barcelona,Spain&units=metric&appid=96757b909ab14fd81c8ce5bb9ee9ad53"
+const todaysWeatherUrlBA = "http://api.openweathermap.org/data/2.5/weather?q=Buenos%20Aires,Argentina&units=metric&appid=96757b909ab14fd81c8ce5bb9ee9ad53"
 const fiveDayForecastStockholm = "https://api.openweathermap.org/data/2.5/forecast?q=Stockholm,Sweden&units=metric&appid=f463a96f9ee6b3233c3a141a391ac3cf"
-// const fiveDayForecastBarcelona = "https://api.openweathermap.org/data/2.5/forecast?q=Barcelona,Spain&units=metric&appid=f463a96f9ee6b3233c3a141a391ac3cf"
-// const fiveDayForecastBuenosAires = "https://api.openweathermap.org/data/2.5/forecast?q=BsAs,Argentina&units=metric&APPID=96757b909ab14fd81c8ce5bb9ee9ad53"
 const mainTemperature = document.getElementById('main-temperature');
 const cityName = document.getElementById('city-name');
 const sunrise = document.getElementById('sunrise');
@@ -12,13 +11,13 @@ const weatherDescription = document.getElementById('weather-description');
 const weatherIcon = document.getElementById('weather-icon');
 const weatherFeelsLike = document.getElementById('weather-feels-like');
 
-//Main fetch for Stockoholm weather
+
+//Stockoholm current weather 
 fetch(todaysWeatherUrl)
   .then((response) => {
       return response.json();
   })
   .then((json) => {
-    console.log(json);
     cityName.innerHTML =`
       <h2>${json.name}</h2>
       `;
@@ -26,7 +25,7 @@ fetch(todaysWeatherUrl)
       <h3>${json.main.temp.toFixed(1)} ºC</h3>
       `;
     weatherIcon.innerHTML += `
-      <img src="https://openweathermap.org/img/wn/${json.weather[0].icon}@2x.png" height="150px" alt="${json.weather[0].description} icon" />
+      <img class="main-icon" src="https://openweathermap.org/img/wn/${json.weather[0].icon}@2x.png" height="150px" alt="${json.weather[0].description} icon" />
       `;
     weatherDescription.innerHTML += `
       ${json.weather[0].description}
@@ -52,28 +51,26 @@ fetch(todaysWeatherUrl)
         `; 
     const weatherId = json.weather[0].id;
       console.log(weatherId)
-      if (weatherId <= 232){ //thunderstorm
-       document.getElementById('container').style.background = 'linear-gradient(315deg, #0cbaba 0%, #380036 74%)'; 
-      } else if (weatherId <= 321) { //drizzle
-          document.getElementById('container').style.background = 'linear-gradient(315deg, #2a2a72 0%, #009ffd 74%)'; 
-      } else if (weatherId <= 531) { //rain
-          document.getElementById('container').style.background = 'linear-gradient(315deg, #f8ceec 0%, #a88beb 74%)'; 
-      } else if (weatherId <= 622) { //snow
-          document.getElementById('container').style.background = 'linear-gradient(315deg, #b8c6db 0%, #f5f7fa 74%)'; 
-      } else if (weatherId <=781 ) { //atmosphere
-          document.getElementById('five-day-forecast').style.background = 'linear-gradient(-45deg, #a7db9c, #d3aaba, #81c6df, #a5e5d6)'; 
-      } else if (weatherId === 800) { //clear
-          document.getElementById('container').style.background = 'linear-gradient(-45deg, #a7db9c, #d3aaba, #81c6df, #a5e5d6)'; 
-      } else  { //clouds
-          document.getElementById('container').style.background = 'linear-gradient(-45deg, #28b10d, #e73c7e, #23a6d5, #23d5ab)'
-      }
+      if (weatherId <= 232){ 
+       document.getElementById('container').style.background = 'linear-gradient(-45deg, #c2b6b6 0%, #576574 74%)';
+      } else if (weatherId <= 531) { 
+          document.getElementById('container').style.background = 'linear-gradient(-45deg, #f5f7fa, #c3cfe2)';    
+      } else if (weatherId <= 622) { 
+          document.getElementById('container').style.background = 'linear-gradient(-45deg, #b8c6db 0%, #f5f7fa 74%)'; 
+      } else if (weatherId <=781 ) { 
+          document.getElementById('container').style.background = 'linear-gradient(-45deg,#a7db9c, #d3aaba, #81c6df, #a5e5d6)'; 
+      } else if (weatherId === 800) { 
+          document.getElementById('container').style.background = 'linear-gradient(-45deg, #a3bded, #6991c7)'; 
+      } else  { 
+          document.getElementById('container').style.background = 'linear-gradient(-45deg, #a1c4fd, #c2e9fb)';
+      }   
   })
   .catch(error => {
     container.innerHTML = error;
   })
   .finally(() => console.log('Finished!'));
 
-//Five day forecast
+//Five day Stockholm forecast
 fetch(fiveDayForecastStockholm)
   .then((response) => {
     return response.json();
@@ -87,19 +84,61 @@ fetch(fiveDayForecastStockholm)
       let weekday = 
         (new Date(item.dt * 1000)).toLocaleDateString("en-US", { weekday: "short" })
       let icon = `
-        <img src=https://openweathermap.org/img/wn/${item.weather[0].icon}@2x.png></img>
+        <img class="forecast-icon" src=https://openweathermap.org/img/wn/${item.weather[0].icon}@2x.png></img>
         `;
-    fiveDayForecast.innerHTML += `
-      <div class="forecast>
-      <span>${weekday}</span> 
-      <span>${temperature}ºC</span>
-      <span>${icon}</span>
-      </div>
-      `; 
-    });
+      fiveDayForecast.innerHTML += `
+        <p class="forecast">
+          <span>${weekday}</span> 
+          <span>${icon}</span>
+          <span>${temperature}ºC</span>
+        </p>
+        `; 
+    }); 
   });
 
-  
+ 
+  //Barcelona current weather
+  fetch(todaysWeatherBar)
+  .then((response) => {
+    return response.json();
+  })
+  .then((json) => {
+    console.log(json);
+    barcelonaName.innerHTML =`
+      <h2>${json.name}</h2>
+      `;
+      barcelonaTemperature.innerHTML = `
+      <h3>${json.main.temp.toFixed(1)} ºC</h3>
+      `;
+      barcelonaIcon.innerHTML += `
+      <img class="main-icon" src="https://openweathermap.org/img/wn/${json.weather[0].icon}@2x.png" alt="${json.weather[0].description} icon" />
+      `;
+  })
+  .catch(error => {
+    barcelona.innerHTML = error;
+  })
+  .finally(() => console.log('Finished!'));
+
+  //Buenos Aires current weather
+  fetch(todaysWeatherUrlBA)
+  .then((response) => {
+    return response.json();
+  })
+  .then((json) => {
+    bsAsName.innerHTML =`
+      <h2>${json.name}</h2>
+      `;
+    bsAsTemperature.innerHTML = `
+      <h3>${json.main.temp.toFixed(1)} ºC</h3>
+      `;
+    bsAsIcon.innerHTML += `
+      <img class="main-icon" src="https://openweathermap.org/img/wn/${json.weather[0].icon}@2x.png" alt="${json.weather[0].description} icon" />
+      `;
+  })
+  .catch(error => {
+    bsAs.innerHTML= error;
+  })
+  .finally(() => console.log('Finished!'));
 
 
 
