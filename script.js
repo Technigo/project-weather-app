de25761f618379e8a3404c5929712a432bacb423
pdetli <74938586+pdetli@users.noreply.github.com>
2021-09-20 22:07:10 +0200

    step 2 done

--- script.js
+++ script.js
@@ -3,8 +3,6 @@ const WEATHER_API =
   "https://api.openweathermap.org/data/2.5/weather?q=Stockholm,Sweden&units=metric&APPID=96cb0f55d34310e596ed4792c7800540";
 const weatherContainer = document.getElementById("weather");
 
-let wData = "";
-
 const fetchData = () => {
   fetch(WEATHER_API)
     .then((res) => {
@@ -14,9 +12,11 @@ const fetchData = () => {
       console.log("This is data", data);
       console.log("This is data.main.temp", data.main.temp);
 
+      const decimal = Math.round(data.main.temp * 10) / 10;
+
       weatherContainer.innerHTML = `
           <h2> City: ${data.name}</h2>
-          <h3> Hello, the tempature is ${data.main.temp}</h3>
+          <h3> Hello, the tempature is ${decimal}&#176 Celsius </h3>
           <h3> Type of weather: ${data.weather[0].description}</h3>
         `;
     })
@@ -29,9 +29,3 @@ const fetchData = () => {
 };
 
 fetchData();
-console.log(data);
-// console.log("this is the wData", wData);
-
-// weatherContainer.innerHTML = `
-//   <h3> Hello, the tempature is ${wData}</h3>
-// `;
