0bf3e5cadb8cdd087881987fa4dfbf868f0e3587
Gustav Frid <gustav.frid@gmail.com>
2021-09-20 21:37:11 +0200

    Printing todays date, temp and weekdays day+ tamp
    Co-authored-by: Elsa Bjelkedal <Elsa-Johanna@users.noreply.github.com>
    Co-authored-by: Lisa Blomé <annaester@users.noreply.github.com>
    Co-authored-by: Idautterstrom <Idautterstrom@users.noreply.github.com>

--- Designs/script.js
+++ Designs/script.js
@@ -1,7 +1,18 @@
-const API_URL = 'https://api.openweathermap.org/data/2.5/weather?q=Stockholm,Sweden&units=metric&APPID=e0b091865cb13799f8ef15c4fb40d2a9';
+const API_URL = 'https://api.openweathermap.org/data/2.5/onecall?lat=59.3326&lon=18.0649&exclude=minutely&units=metric&appid=e0b091865cb13799f8ef15c4fb40d2a9'; //'https://api.openweathermap.org/data/2.5/weather?q=Stockholm,Sweden&units=metric&APPID=e0b091865cb13799f8ef15c4fb40d2a9';
 const weatherApp = document.getElementById('weatherApp');
+const weekDays = ['Sun', 'Mon', 'Tue', 'Wed', 'Thu', 'Fri', 'Sat'];
 
 fetch(API_URL)
 	.then((res) => res.json())
-	.then((data) => console.log('data', data))
+	.then((data) => {
+		const date = new Date(data.current.dt * 1000);
+		weatherApp.innerHTML += `<p>Current date: ${date}</p> <p>Current location ${data.timezone}</p> <p>Todays weather  ${data.current.temp} </p>`;
+		console.log('data', data);
+
+		data.daily.slice(1).forEach((day) => {
+			let weekDay = new Date(day.dt * 1000);
+			weekDay = weekDay.getDay();
+			weatherApp.innerHTML += `<p>${weekDays[weekDay]} ${weekDay} 🌈${day.temp.day}</p>`;
+		});
+	})
 	.catch((error) => console.log('error', error));
