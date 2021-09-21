b1d9f7682c6e0f0500376d321f8a484cd3d1338b
Gustav Frid <gustav.frid@gmail.com>
2021-09-21 21:37:15 +0200

    Forecast feature
    Co-authored-by: Emelie <intehon@users.noreply.github.com>
    Co-authored-by: Elsa Bjelkedal <Elsa-Johanna@users.noreply.github.com>
    Co-authored-by: Lisa Blomé <annaester@users.noreply.github.com>

--- script.js
+++ script.js
@@ -1,6 +1,6 @@
 const API_KEY = 'e0b091865cb13799f8ef15c4fb40d2a9';
 const API_URL = `https://api.openweathermap.org/data/2.5/weather?q=Stockholm,Sweden&units=metric&APPID=${API_KEY}`;
-//forecast five days: https://api.openweathermap.org/data/2.5/forecast?q=Stockholm,Sweden&units=metric&APPID=e0b091865cb13799f8ef15c4fb40d2a9
+const API_URL_FORECAST = `https://api.openweathermap.org/data/2.5/forecast?q=Stockholm,Sweden&units=metric&APPID=${API_KEY}`;
 
 const weatherApp = document.getElementById('weatherApp');
 const weekDays = ['Sun', 'Mon', 'Tue', 'Wed', 'Thu', 'Fri', 'Sat'];
@@ -22,5 +22,28 @@ fetch(API_URL)
 		// 	weekDay = weekDay.getDay();
 		// 	weatherApp.innerHTML += `<p>${weekDays[weekDay]} ${weekDay} 🌈${day.temp.day}</p>`;
 		// });
+	})
+	.catch((error) => console.log('error', error));
+
+fetch(API_URL_FORECAST)
+	.then((res) => res.json())
+	.then((data) => {
+		const date = new Date(data.dt * 1000);
+		const filteredForecast = data.list.filter((item) => item.dt_txt.includes('12:00'));
+		console.log(filteredForecast);
+		filteredForecast.forEach((day) => {
+			let weekDay = new Date(day.dt * 1000);
+			weekDay = weekDay.getDay();
+			weatherApp.innerHTML += `<p>${weekDays[weekDay]} 🌈${Math.round(day.main.temp * 10) / 10}</p>`;
+		});
+
+		// weatherApp.innerHTML += `
+		// <p>Current date: ${date}</p>
+		// <p>Current location: ${data.name}</p>
+		// <p>Todays temperature: ${Math.round(data.main.temp * 10) / 10}</p>
+		// <p>Todays weather: ${data.weather[0].description}</p>
+		// `;
+
+		console.log('data', data);
 	})
 	.catch((error) => console.log('error', error));
