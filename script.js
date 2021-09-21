7ef0503290a201e520bd1bccee785a086af40403
annathunberg <83536062+annathunberg@users.noreply.github.com>
2021-09-21 19:41:55 +0200

    removed sunrise and sunset to redo in new branch

--- script.js
+++ script.js
@@ -6,26 +6,14 @@ const weatherContainer = document.getElementById('weather-container')
 fetch(API_URL)
 .then((response) => response.json())
 .then((data) => {
-// This function adds a zero in case the time units consist of only one digit 
-    const addZero = sec => {
-        if (sec < 10) {
-            sec = '0' + sec;
-        }
-        return sec
-    }
 
-    let sunriseTime = new Date(data.sys.sunrise)
-    let sunsetTime = new Date(data.sys.sunset)
-    let formattedHourSunrise = addZero(sunriseTime.getHours()) + ':' + addZero(sunriseTime.getMinutes()) + ':' + addZero(sunriseTime.getSeconds()) 
-    let formattedHourSunset = addZero(sunsetTime.getHours()) + ':' +
-    addZero(sunsetTime.getMinutes()) + ':' + addZero(sunsetTime.getSeconds()) 
 
     weatherContainer.innerHTML += `
     <h1>City: ${data.name}</h1>
     <h2>Temperature: ${data.main.temp.toFixed(1)} C°</h2>
     <h2>Type of weather: ${data.weather[0].description}</h2>
-    <h2>Sunrise: ${formattedHourSunrise}</h2>
-    <h2>Sunset: ${formattedHourSunset}</h2>
+    <h2>Sunrise: </h2>
+    <h2>Sunset: </h2>
     `
 })
 .catch((error) => console.error(error))
