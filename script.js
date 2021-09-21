9bf98ce21ccd02b9abf5c597f3b5cc3a31819aa9
Kaliine <80845418+Kaliine@users.noreply.github.com>
2021-09-21 18:37:41 +0200

    done sunrise and sunset

--- script.js
+++ script.js
@@ -1,6 +1,9 @@
 const cityName = document.getElementById('city')
 const temperature = document.getElementById('temperature')
 const typeOfWeather = document.getElementById('typeOfWeather')
+const sunrise = document.getElementById('sunrise')
+const sunset = document.getElementById('sunset')
+
 
 const API_LINK = 'https://api.openweathermap.org/data/2.5/weather?q=Stockholm,Sweden&units=metric&APPID=147b874875d53e0e9f84cbacd0567b99'
     fetch(API_LINK)
@@ -11,6 +14,25 @@ const API_LINK = 'https://api.openweathermap.org/data/2.5/weather?q=Stockholm,Sw
         cityName.innerHTML = json.name
         temperature.innerHTML = json.main.temp.toFixed(1)
         typeOfWeather.innerHTML = json.weather[0].description
-        console.log(json)
+        const sunriseConvert = new Date((json.sys.sunrise) * 1000);
+        const sunriseTime = sunriseConvert.toLocaleTimeString([], {
+            hour: "2-digit",
+            minute: "2-digit",
+          });
+      
+          sunrise.innerHTML = `Sunrise: ${sunriseTime}`;
+
+          const sunsetConvert = new Date((json.sys.sunset) * 1000);
+          const sunsetTime = sunsetConvert.toLocaleTimeString([], {
+              hour: "2-digit",
+              minute: "2-digit",
+            });
+      
+            sunset.innerHTML = `Sunset: ${sunsetTime}`;
+
+          console.log(json)
     })
+
+    
+    
 
