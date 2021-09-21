9d67f3a69a3aede4595f326b4d89891b311d150f
Maria Thomasson <81348201+MariaThomasson@users.noreply.github.com>
2021-09-21 14:32:21 +0200

    done with forecast min-max temp

--- script.js
+++ script.js
@@ -3,6 +3,7 @@ const temperature = document.getElementById('temperature')
 const typeOfWeather = document.getElementById('typeOfWeather')
 const minTemp = document.getElementById('min-temp')
 const maxTemp = document.getElementById('max-temp') 
+const bottomSection = document.getElementById('bottom-section')
 
 const API_LINK = 'https://api.openweathermap.org/data/2.5/weather?q=Stockholm,Sweden&units=metric&APPID=147b874875d53e0e9f84cbacd0567b99'
     fetch(API_LINK)
@@ -23,12 +24,19 @@ const API_LINK_FORECAST = 'https://api.openweathermap.org/data/2.5/forecast?q=St
     })
     .then((json) => {
         const filteredForecast = json.list.filter(item => item.dt_txt.includes('12:00'))
-        console.log(filteredForecast)
-        filteredForecast.forEach((element) => {
-            minTemp.innerHTML = element.main.temp_min
-            maxTemp.innerHTML = element.main.temp_max
-            console.log(element)
+
+        const weekDays = ["Sunday","Monday","Tuesday","Wednesday","Thursday","Friday","Saturday",];
+
+        filteredForecast.forEach((day) => {
+            const date = new Date(day.dt * 1000) //converted numbers to right format, in this case Weekdays.
+            
+            bottomSection.innerHTML += `<p>${weekDays[date.getDay()]}</p> 
+            <p>Min: ${day.main.temp_min.toFixed(1)}°C</p> 
+            <p>Max: ${day.main.temp_max.toFixed(1)}°C</p> `
+            
+ 
         })
+
 
         console.log(json)
     })
