9bdee14969d01e8512189a110c297363fc466213
Jsfrulle <Jessicamhermansson@gmail.com>
2021-09-21 12:29:03 +0200

    forecast

--- Designs/script.js
+++ Designs/script.js
@@ -1,17 +1,42 @@
-const weatherData = document.getElementById('weatherdata')
+const weatherData = document.getElementById("weatherdata");
 
 const API_URL =
 	"https://api.openweathermap.org/data/2.5/weather?q=stockholm,Sweden&units=metric&APPID=5caaaf25021b2d7aa4d206126b6a3351";
 
+const API_FORCAST =
+	"https://api.openweathermap.org/data/2.5/forecast?q=stockholm,Sweden&units=metric&APPID=5caaaf25021b2d7aa4d206126b6a3351";
+
 fetch(API_URL)
-  .then((response) => response.json())
-  .then((data) => {
-    console.log(data)
-    weatherData.innerHTML = `
+	.then((response) => response.json())
+	.then((data) => {
+		console.log(data);
+		weatherData.innerHTML = `
+	
       <p>City: ${data.name}</p>
       <p>Temp: ${data.main.temp.toFixed(1)}°C</p>  
       <p>Weather: ${data.weather[0].description}</p>
-    `// toFixed(1) rounds the temp to one decimal
-  })
-  .catch((error) => console.error('Error: ', error))
-  .finally(() => console.log('Request done'))    
+
+
+    `; // toFixed(1) rounds the temp to one decimal
+	})
+	.catch((error) => console.error("Error: ", error))
+	.finally(() => console.log("Request done"));
+
+fetch(API_FORCAST)
+	.then((res) => res.json())
+	.then((forecast) => {
+		const forecastDay = forecast.list.filter((day) =>
+			day.dt_txt.includes("12:00")
+		);
+
+		forecastDay.forEach((day) => {
+			weatherData.innerHTML += `
+      <h3>${day.dt_txt} </h3>
+      <p>Temp: ${day.main.temp.toFixed(1)}°C</p> 
+	  <p>Feels like: ${day.main.feels_like.toFixed(1)}°C</p> 
+      <p>Weather: ${day.weather[0].description}</p>
+    `;
+		});
+	})
+	.catch((error) => console.error("Error: ", error))
+	.finally(() => console.log("Request done"));
