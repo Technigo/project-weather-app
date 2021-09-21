d2eb08c26ba410ed7afbb162f4a4f57404d870fa
Naushin <malik.naushin@gmail.com>
2021-09-21 21:07:51 +0200

    fetch the weather forecast and displaying it

--- script.js
+++ script.js
@@ -1,7 +1,15 @@
-// const API_URL = 'api.openweathermap.org/data/2.5/weather?q=London,uk&APPID=00ceff8163f7cba27d66b6501ce70e06'
+//for weather forecast API URL
+const FORECAST_API_URL = 'https://api.openweathermap.org/data/2.5/forecast?q=London,uk&units=metric&APPID=00ceff8163f7cba27d66b6501ce70e06'
+
+//Dom selector
 const test = document.getElementById('test')
+
+//global variables
 let temperature 
 let roundTemp
+ 
+//fetch function
+
 fetch('https://api.openweathermap.org/data/2.5/weather?q=London,uk&APPID=00ceff8163f7cba27d66b6501ce70e06')
 .then((response) => {
     return response.json()
@@ -22,4 +30,29 @@ fetch('https://api.openweathermap.org/data/2.5/weather?q=London,uk&APPID=00ceff8
 .catch((error) => console.error(error))
 .then(() => console.log('Request finished'));
 
-console.log ("just for checking that branch forecast is behaving the way it should be")
+//for weather forecast fetch function
+fetch(FORECAST_API_URL)
+  .then((res) => {
+    return res.json()
+  })
+  .then((data) => {
+    console.log('Forecast DATA', data)
+    const filteredForecast = data.list.filter(item => item.dt_txt.includes('12:00'))
+    console.log('filtered forecast', filteredForecast)
+    filteredForecast.forEach(item => {
+      
+      test.innerHTML +=`
+      <div class= "forecast">
+        <p class= "forecast-date">
+         ${item.dt_txt}
+        </p>
+        <p class= "forecast-temp">${item.main.temp} </p>
+      </div>
+    `
+
+    })
+    
+  })
+  .catch((error) => console.error(error))
+  .then(() => console.log('Request finished'));
+
