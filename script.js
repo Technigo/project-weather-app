558210e9eba45a16ce1cc5747b52603aa87070ee
Никитина Юля <nik.iuliiasw@gmail.com>
2021-09-21 15:42:40 +0200

    finished step 3

--- script.js
+++ script.js
@@ -1,6 +1,8 @@
 const API_URL = 'https://api.openweathermap.org/data/2.5/weather?q=Stockholm,Sweden&units=metric&APPID=7678391e67f390dcfc1cc2681209fd22'
+const API_URL_FORECAST = 'https://api.openweathermap.org/data/2.5/forecast?q=Stockholm,Sweden&units=metric&APPID=7678391e67f390dcfc1cc2681209fd22'
 
 const weatherContainer = document.getElementById("weather-container");
+
 
 const fetchWeather = () => {
     fetch(API_URL)
@@ -15,3 +17,24 @@ const fetchWeather = () => {
 }
 
 fetchWeather();
+
+const fetchWeatherForecast = () => {
+    fetch(API_URL_FORECAST)
+        .then(response => response.json())
+        .then(data => {
+            console.log(data)
+            const tempForecast = data.list.filter((item) => item.dt_txt.includes('12:00')) // array
+
+            const tempForecastFiveDays = tempForecast.map(listItem => {
+                console.log(listItem.main.temp)
+                const dateVariable = new Date(listItem.dt * 1000).getDay() // gives us 2 as today is tuesday
+                const arrayOfWeekdays = ["Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday"]
+                const weekdayName = arrayOfWeekdays[dateVariable] //arrayofWeekdays[2]
+                console.log(weekdayName)
+                console.log(dateVariable)
+                return weatherContainer.innerHTML += `<h1> ${weekdayName}: ${listItem.main.temp}</h1>`
+
+            })
+        })
+}
+fetchWeatherForecast()
