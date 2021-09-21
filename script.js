2876918d9aec4827388b8af6fa262dca5622a275
Lena Simma <84474447+lenisili@users.noreply.github.com>
2021-09-21 16:20:18 +0200

    convert time sunrise sunset

--- script.js
+++ script.js
@@ -2,6 +2,7 @@
 const test = document.getElementById('test')
 let temperature 
 let roundTemp
+
 fetch('https://api.openweathermap.org/data/2.5/weather?q=London,uk&APPID=00ceff8163f7cba27d66b6501ce70e06')
 .then((response) => {
     return response.json()
@@ -18,6 +19,19 @@ fetch('https://api.openweathermap.org/data/2.5/weather?q=London,uk&APPID=00ceff8
     `
     test.innerHTML += `${data.weather[0].description}
     `
+    let sunrise = convertTime(data.sys.sunrise)
+    test.innerHTML += `The sun rises at (sunrise)
+    `
+    let sunset = convertTime(data.sys.sunset)
+    test.innerHTML += `The sun sets at ${sunset}
+    `
 })
-.catch((error) => console.error(error))
-    .then(() => console.log('Request finished'));
+
+
+function convertTime(unixTime){
+  let date = new Date(unixTime * 1000)
+  let hours = date.getHours()
+  let minutes = "0" + date.getMinutes()
+  let time = hours + ":" + minutes.substr(-2)
+  console.log(time)
+};
