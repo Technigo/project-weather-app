cc44e38d437d3090f0aa331e2e1224a8d80a6633
Naushin <malik.naushin@gmail.com>
2021-09-21 22:43:42 +0200

    changed the format of date

--- script.js
+++ script.js
@@ -36,15 +36,17 @@ fetch(FORECAST_API_URL)
     return res.json()
   })
   .then((data) => {
-    console.log('Forecast DATA', data)
-    const filteredForecast = data.list.filter(item => item.dt_txt.includes('12:00'))
+    console.log('Forecast DATA', data) // just for testing purpose have to delete 
+    const filteredForecast = data.list.filter(item => item.dt_txt.includes('00:00:00'))
     console.log('filtered forecast', filteredForecast)
     filteredForecast.forEach(item => {
-      
+      //calling function to convert date with 
+      let forecastDate = convertDate(item.dt)
+
       test.innerHTML +=`
       <div class= "forecast">
         <p class= "forecast-date">
-         ${item.dt_txt}
+         ${forecastDate}
         </p>
         <p class= "forecast-temp">${item.main.temp} </p>
       </div>
@@ -56,3 +58,45 @@ fetch(FORECAST_API_URL)
   .catch((error) => console.error(error))
   .then(() => console.log('Request finished'));
 
+//----- functions for forecast-----//
+
+
+// function to find min and max temperature of the day
+const createForecast = (data) => {
+  let fiveDayForecast = {}
+  
+  data.list.forEach((item) => {
+    
+
+
+
+
+  })
+
+
+
+  
+
+
+
+
+}
+// converting date format
+function convertDate (fetchedDate)  {
+  const week = ['Sunday', 'Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday']
+  const month = ['JAN', 'FEB','MAR', 'APR', 'MAY','JUN', 'JUL', 'AUG', 'SEP', 'OCT', 'NOV', 'DEC' ]
+  const date = new Date(fetchedDate * 1000)
+  //converting month
+  let newMonth = date.getMonth()
+  let whatMonth = month[newMonth]
+  console.log(whatMonth)
+  //converting day
+  let newDay = date.getDay()
+  let whatDay = week[newDay]
+  console.log(whatDay)
+  //constructing the required format
+  let formattedDate = ` ${whatDay} ${date.getDate()} ${whatMonth}`
+  //returning the formatted date
+  return formattedDate 
+
+}
