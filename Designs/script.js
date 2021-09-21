a34ef78d2b4c2544f18bb704ca9fdf345ad01821
Birgit Nehrwein <nehrwein@posteo.de>
2021-09-21 14:56:43 +0200

    added sunrise and sunset in time format: 07:45

--- Designs/script.js
+++ Designs/script.js
@@ -7,10 +7,21 @@ fetch(API_URL)
   .then((response) => response.json())
   .then((data) => {
     console.log(data)
+
+    let sunrise = data.sys.sunrise;
+    sunrise = new Date(sunrise*1000);
+    sunrise = sunrise.toLocaleTimeString([], {timeStyle: 'short'});
+    
+    let sunset = data.sys.sunset;
+    sunset = new Date(sunset*1000);
+    sunset = sunset.toLocaleTimeString([], {timeStyle: 'short'});
+
     weatherData.innerHTML = `
       <p>City: ${data.name}</p>
       <p>Temp: ${data.main.temp.toFixed(1)}°C</p>  
       <p>Weather: ${data.weather[0].description}</p>
+      <p>Sunrise: ${sunrise}</p>
+      <p>Sunset: ${sunset}</p>
     `// toFixed(1) rounds the temp to one decimal
   })
   .catch((error) => console.error('Error: ', error))
