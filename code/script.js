97d97e9b55d8e93058fe54bc643f61bafcb92003
Johanna Mannestål <johannamannestal@Johannas-MacBook-Air.local>
2021-09-21 12:22:41 +0200

    screating layout in js

--- code/script.js
+++ code/script.js
@@ -1,1 +1,15 @@
-console.log("hello tiger weather");
+// Main placeholder for the whole weather app
+const wrapperContainer = document.getElementById("wrapper-container");
+// https://developer.mozilla.org/en-US/docs/Web/API/Document/createElement
+// Main container for todays weather forcast
+const weatherContainer = document.createElement("section");
+// https://developer.mozilla.org/en-US/docs/Web/API/Element/prepend
+wrapperContainer.prepend(weatherContainer);
+// Assigning a class to HTLM element
+weatherContainer.className = "weather-container";
+// Container for next 5 days weather forcast
+const weekdayContainer = document.createElement("section");
+// https://developer.mozilla.org/en-US/docs/Web/API/Element/append
+wrapperContainer.append(weekdayContainer);
+// Assigning a class to HTLM element
+weekdayContainer.className = "weekday-container";
