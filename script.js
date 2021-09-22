9aeccfeb70932a5533e35c4eed138f1a1cf0af3b
Elsa-Johanna <elsa.bjelkedal@gmail.com>
2021-09-22 21:39:34 +0200

    added more styleing to cloudy, rain and clear

--- script.js
+++ script.js
@@ -9,52 +9,59 @@ fetch(API_URL)
   .then((res) => res.json())
   .then((data) => {
     //HÄR VILL VI SÄTTA CLASS PÅ VÄDER, GRADER, SOLUPPGÅNG/SOLNEDGÅNG
-    const date = new Date(data.dt * 1000);
     const sunrise = new Date(data.sys.sunrise * 1000);
     const sunset = new Date(data.sys.sunset * 1000);
     const weatherTypes = data.weather[0].main;
     weatherApp.innerHTML += `
 			<section class="topsite">
-      <p class="toptext">${weatherTypes} | ${
+      <p class="toptext">${weatherTypes.toLowerCase()} | ${
       Math.round(data.main.temp * 10) / 10
     }&#176;</p>
-      <p class="toptext">Sunrise ${sunrise.getHours()}:${sunrise.getMinutes()}</p>
-      <p class="toptext">Sunset ${sunset.getHours()}:${sunset.getMinutes()}</p>
+      <p class="toptext">sunrise ${sunrise.getHours()}:${sunrise.getMinutes()}</p>
+      <p class="toptext">sunset ${sunset.getHours()}:${sunset.getMinutes()}</p>
 			</section>`;
     console.log("data current", data);
 
     if (weatherTypes === "Clear") {
       //här borde vi kunna skriva en weatherApp.style.backgroundcolor = #F7E9B9 style.color = #2A5510
-      weatherApp.innerHTML += `<h2 class="description">Get your sunnies on. Stockholm is looking rather great today.</h2>`;
+      weatherApp.innerHTML += `<img class="icon" src="./Designs/Design-2/icons/noun_Sunglasses_2055147.svg">
+      <h2 class="description">Get your sunnies on. Stockholm is looking rather great today.</h2>`;
+      weatherApp.style.color = "#2A5510";
+      weatherApp.parentNode.style.backgroundColor = "#F7E9B9";
     } else if (weatherTypes === "Clouds") {
-      weatherApp.innerHTML += `<h2 class="description">Light a fire and get cosy. Stockholm is looking grey today.</h2>`;
+      weatherApp.innerHTML += `<img class="icon" src="./Designs/Design-2/icons/noun_Cloud_1188486.svg"><h2 class="description">Light a fire and get cosy. Stockholm is looking grey today.</h2>`;
+      weatherApp.style.color = "#F47775";
+      weatherApp.parentNode.style.backgroundColor = "#F4F7F8";
     } else if (weatherTypes === "Rain") {
-      weatherApp.innerHTML += `<h2 class="description">Don't forget your umbrella! It's wet in Stockholm today!</h2>`;
+      weatherApp.innerHTML += `<img class="icon" src="./Designs/Design-2/icons/noun_Umbrella_2030530.svg"><h2 class="description">Don't forget your umbrella! It's wet in Stockholm today!</h2>`;
+      weatherApp.style.color = "#164A68";
+      weatherApp.parentNode.style.backgroundColor = "#A3DEF7";
     }
   })
-
   .catch((error) => console.log("error", error));
 
 fetch(API_URL_FORECAST)
   .then((res) => res.json())
   .then((data) => {
-    const date = new Date(data.dt * 1000);
     const filteredForecast = data.list.filter((item) =>
       item.dt_txt.includes("12:00")
     );
     console.log(filteredForecast);
-    filteredForecast.slice(1).forEach((day) => {
+    filteredForecast.forEach((day) => {
       let weekDay = new Date(day.dt * 1000);
       weekDay = weekDay.getDay();
-      weatherApp.innerHTML += `<section class="forecast"><p class="forecastDay">${
-        weekDays[weekDay]
-      }</p><p class="forecastTemp"> ${
-        Math.round(day.main.temp * 10) / 10
-      }&#176;</p></section>`;
+      weatherApp.innerHTML += `
+      <section class="forecast">
+        <p class="forecastDay">${weekDays[weekDay]}</p>
+        <p class="forecastTemp"> ${Math.round(day.main.temp * 1) / 1}&#176;</p>
+      </section>`;
     });
 
     console.log("data forecast", data);
   })
   .catch((error) => console.log("error", error));
+
+//weatherApp.style.color = "#2A5510"
+//weatherApp.style.backgroundColor = "#F7E9B9"
 
 //   vi behöver fixa ikoner som ska vara ovanför h2an, färgtema tll varje väder samt fixa prickarna för att ha under kommande fyra dagar
