ad53936ff4bbcbcbbed493cd7e55da83f1ba657c
Darya <darya.lapata@gmail.com>
2021-09-22 13:07:12 +0200

    created styling branch

--- script.js
+++ script.js
@@ -1,6 +1,7 @@
 const cityName = document.getElementById('city')
 const temperature = document.getElementById('temperature')
 const typeOfWeather = document.getElementById('typeOfWeather')
+const mainIcon = document.getElementById('main-icon')
 
 const API_LINK = 'https://api.openweathermap.org/data/2.5/weather?q=Stockholm,Sweden&units=metric&APPID=147b874875d53e0e9f84cbacd0567b99'
     fetch(API_LINK)
@@ -9,8 +10,11 @@ const API_LINK = 'https://api.openweathermap.org/data/2.5/weather?q=Stockholm,Sw
     })
     .then((json) => {
         cityName.innerHTML = json.name
-        temperature.innerHTML = json.main.temp.toFixed(1)
+        mainIcon.src = `./`
+        temperature.innerHTML = `${json.main.temp.toFixed(1)} °C`
         typeOfWeather.innerHTML = json.weather[0].description
         console.log(json)
     })
 
+
+    
