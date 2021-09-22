73096705d7621f0cdf1a54abecd7f89cb28c5441
Idautterstrom <ida.utterstrom@hotmail.com>
2021-09-22 14:53:45 +0200

    added styling

--- script.js
+++ script.js
@@ -3,26 +3,31 @@ const API_URL = `https://api.openweathermap.org/data/2.5/weather?q=Stockholm,Swe
 const API_URL_FORECAST = `https://api.openweathermap.org/data/2.5/forecast?q=Stockholm,Sweden&units=metric&APPID=${API_KEY}`;
 
 const weatherApp = document.getElementById('weatherApp');
-const weekDays = ['Sun', 'Mon', 'Tue', 'Wed', 'Thu', 'Fri', 'Sat'];
+const weekDays = ['sun', 'mon', 'tue', 'wed', 'thu', 'fri', 'sat'];
 
 fetch(API_URL)
 	.then((res) => res.json())
 	.then((data) => {
 		const date = new Date(data.dt * 1000);
+		const sunrise = new Date(data.sys.sunrise * 1000);
+		const sunset = new Date(data.sys.sunset * 1000);
+		const weatherTypes = data.weather[0].main
 		weatherApp.innerHTML += `
-			<p>Current date: ${date}</p>
-			<p>Current location: ${data.name}</p>
-			<p>Todays temperature: ${Math.round(data.main.temp * 10) / 10}</p>
-			<p>Todays weather: ${data.weather[0].description}</p>
+			<p>${weatherTypes} | ${Math.round(data.main.temp * 10) / 10}&#176;</p>
+			<p>sunrise ${sunrise.getHours()}:${sunrise.getMinutes()}</p>
+			<p>sunset ${sunset.getHours()}:${sunset.getMinutes()}</p>
 			`;
-		console.log('data', data);
+		console.log('data current', data);
 
-		// data.daily.slice(1).forEach((day) => {
-		// 	let weekDay = new Date(day.dt * 1000);
-		// 	weekDay = weekDay.getDay();
-		// 	weatherApp.innerHTML += `<p>${weekDays[weekDay]} ${weekDay} 🌈${day.temp.day}</p>`;
-		// });
-	})
+			if (weatherTypes === "Clear") {
+				weatherApp.innerHTML += `<p>Don't forget your umbrella! It's wet in Stockholm today!</p>`
+			} else if (weatherTypes === "Cloudy") {
+				weatherApp.innerHTML += `<p>Don't forget your umbrella! It's wet in Stockholm today!</p>`
+			} else if (weatherTypes === "Cloudy") {
+				weatherApp.innerHTML += `<p>Don't forget your umbrella! It's wet in Stockholm today!</p>`
+			} 
+		})
+		
 	.catch((error) => console.log('error', error));
 
 fetch(API_URL_FORECAST)
@@ -31,19 +36,13 @@ fetch(API_URL_FORECAST)
 		const date = new Date(data.dt * 1000);
 		const filteredForecast = data.list.filter((item) => item.dt_txt.includes('12:00'));
 		console.log(filteredForecast);
-		filteredForecast.forEach((day) => {
+		filteredForecast.slice(1).forEach((day) => {
 			let weekDay = new Date(day.dt * 1000);
 			weekDay = weekDay.getDay();
-			weatherApp.innerHTML += `<p>${weekDays[weekDay]} 🌈${Math.round(day.main.temp * 10) / 10}</p>`;
+			weatherApp.innerHTML += `<p>${weekDays[weekDay]} ${Math.round(day.main.temp * 10) / 10}&#176;</p>`;
 		});
 
-		// weatherApp.innerHTML += `
-		// <p>Current date: ${date}</p>
-		// <p>Current location: ${data.name}</p>
-		// <p>Todays temperature: ${Math.round(data.main.temp * 10) / 10}</p>
-		// <p>Todays weather: ${data.weather[0].description}</p>
-		// `;
-
-		console.log('data', data);
+		console.log('data forecast', data);
 	})
 	.catch((error) => console.log('error', error));
+	
