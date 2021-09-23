f7e164a68922106fc9cea34984d8e6e789c126ee
annathunberg <83536062+annathunberg@users.noreply.github.com>
2021-09-23 20:19:21 +0200

    combined copied.js and script.js

--- script.js
+++ script.js
@@ -1,9 +1,15 @@
 const API_URL = 'https://api.openweathermap.org/data/2.5/weather?q=Gothenburg,Sweden&units=metric&APPID=affe19113e10ebc0685623d229879d1f'
 
+const API_URL_5DAY =
+  "https://api.openweathermap.org/data/2.5/forecast?q=Gothenburg,Sweden&units=metric&APPID=affe19113e10ebc0685623d229879d1f";
+
+
 const sunsetSunrise = document.getElementById('sunset-sunrise')
 const weatherContainer = document.getElementById('weather-container')
 const text = document.getElementById('text')
+const forecastContainer = document.getElementById("forecast-container");
 
+//Sunrise, sunset feature
 fetch(API_URL)
 .then((response) => response.json())
 .then((data) => {
@@ -30,7 +36,7 @@ fetch(API_URL)
     <h2>Temperature: ${data.main.temp.toFixed(1)} C°</h2>
     <h2>Type of weather: ${data.weather[0].description}</h2>`
 
-    // If else statement med de olika ikonerna
+    // If else statement with different weather-icons
     text.innerHTML += `
     <img src="./Designs/Design-2/icons/noun_Cloud_1188486.svg" alt="cloud icon">
     <img src="./Designs/Design-2/icons/noun_Sunglasses_2055147.svg" alt="sunglasses icon">
@@ -43,3 +49,48 @@ fetch(API_URL)
 })
 
 .catch((error) => console.error(error))
+
+//Function for turning a date to a string short weekday
+const getWeekDay = (data) => {
+    const dateDay = new Date(data * 1000); //Timestamp to milliseconds
+    return dateDay.toLocaleDateString("en-GB", {
+        //Setting how to show day
+        weekday: "short",
+    });
+};
+
+//5 day forecast feature
+fetch(API_URL_5DAY)
+  .then((response) => response.json())
+  .then((data) => {
+    console.log("DATA!", data); //REMOVE
+    const filteredForecast = data.list.filter((item) =>
+      item.dt_txt.includes("12:00")
+    );
+    console.log("FILTERED FORECAST!", filteredForecast); //REMOVE
+
+    forecastContainer.innerHTML += `
+    <table>
+    <tr>
+      <td>${getWeekDay(filteredForecast[0].dt)}</td>
+      <td class="degrees">${filteredForecast[0].main.temp.toFixed(1)} C°</td>
+    </tr>
+    <tr>
+     <td>${getWeekDay(filteredForecast[1].dt)}</td>
+     <td class="degrees">${filteredForecast[1].main.temp.toFixed(1)} C°</td> 
+    </tr>
+    <tr>
+     <td>${getWeekDay(filteredForecast[2].dt)}</td>
+     <td class="degrees">${filteredForecast[2].main.temp.toFixed(1)} C°</td>
+    </tr>
+    <tr>
+     <td>${getWeekDay(filteredForecast[3].dt)}</td>
+     <td class="degrees">${filteredForecast[3].main.temp.toFixed(1)} C°</td>
+    </tr>
+    <tr>
+     <td>${getWeekDay(filteredForecast[4].dt)}</td>
+     <td class="degrees">${filteredForecast[4].main.temp.toFixed(1)} C°</td>
+    </tr>
+    </table>
+      `;
+  })
