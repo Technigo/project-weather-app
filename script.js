b971922f8f08ae1b53ac5dfad4cd3fb5d653f092
katiewu1 <83988618+katiewu1@users.noreply.github.com>
2021-09-23 14:50:28 +0200

    animation, footer, finished the mobile view

--- script.js
+++ script.js
@@ -5,108 +5,102 @@ const API_FORECAST =
 const cityName = document.getElementById('name');
 const temp = document.getElementById('temp');
 const description = document.getElementById('description');
-const humidity = document.getElementById("humidity");
-const forecast = document.getElementById('forecast');
-const displayWeekdays = document.getElementById('displayWeekdays');
-const sunTime = document.getElementById("suntimes");
-const body = document.getElementById('body')
-const image = document.getElementById('image')
-const dayZero = document.getElementById('dayZero')
-const dayOne = document.getElementById('dayOne')
-const dayTwo = document.getElementById('dayTwo')
-const dayThree = document.getElementById('dayThree')
-const dayFour = document.getElementById('dayFour')
-const td = document.getElementsByTagName('td')
+const humidity = document.getElementById('humidity');
+const sunTime = document.getElementById('suntimes');
+const body = document.getElementById('body');
+const image = document.getElementById('image');
+const dayZero = document.getElementById('dayZero');
+const dayOne = document.getElementById('dayOne');
+const dayTwo = document.getElementById('dayTwo');
+const dayThree = document.getElementById('dayThree');
+const dayFour = document.getElementById('dayFour');
 
 fetch(API_URL)
   .then((response) => {
     return response.json();
   })
   .then((json) => {
-    const tempDecimal = json.main.temp.toFixed(0); 
+    const tempDecimal = json.main.temp.toFixed(0);
     description.innerHTML += `
-    <p>${json.weather[0].description} | ${tempDecimal}°C</p>`;
+    <p>${json.weather[0].description} | ${tempDecimal}°</p>`;
 
-    humidity.innerHTML =`
+    humidity.innerHTML = `
     <p>${json.main.humidity}% relative humidity</p>`;
 
     const rise = new Date(json.sys.sunrise * 1000); // new Date() shows todays date. The json.sys.sunrise gets the time for the sunrise in ms x 1000 to get a whole second
     const up = rise.toLocaleTimeString([], {
       // returns the date object as a string, using local (timezone) conventions
-      hour: "2-digit", // show the time as 00:00 hour/minute
-      minute: "2-digit",
+      hour: '2-digit', // show the time as 00:00 hour/minute
+      minute: '2-digit',
     });
-    sunTime.innerHTML = `<p>Sunrise ${up}</p>`;
+    sunTime.innerHTML = `<p>sunrise ${up}</p>`;
 
     const set = new Date(json.sys.sunset * 1000);
     const down = set.toLocaleTimeString([], {
-      hour: "2-digit",
-      minute: "2-digit",
+      hour: '2-digit',
+      minute: '2-digit',
     });
-    sunTime.innerHTML += `<p>Sunset ${down}</p>`;
-    
+    sunTime.innerHTML += `<p>sunset ${down}</p>`;
+
+    // json.weather[0].main = 'Clouds';
+
     if (json.weather[0].main === 'Clear') {
-      body.style.background = '#F7E9B9'
-      body.style.color = '#2A5510'
-      image.innerHTML = `<img src="./Designs/Design-2/icons/noun_Sunglasses_2055147.svg"/>`
-      cityName.innerHTML += `Get your sunnies on. ${json.name} is looking rather great today.`
-  } else if(json.weather[0].main === 'Rain') {
-      body.style.background = '#A3DEF7'
-      body.style.color = '#164A68'
-      image.innerHTML = `<img src="./Designs/Design-2/icons/noun_Umbrella_2030530.svg"/>`
-      cityName.innerHTML += `Don’t forget your umbrella. It’s wet in ${json.name} today.`
+      body.style.background = '#F7E9B9';
+      body.style.color = '#2A5510';
+      image.innerHTML = `<img src="./Designs/Design-2/icons/noun_Sunglasses_2055147.svg"/>`;
+      cityName.innerHTML += `Get your sunnies on. ${json.name} is looking rather great today.`;
+    } else if (json.weather[0].main === 'Rain') {
+      body.style.background = '#A3DEF7';
+      body.style.color = '#164A68';
+      image.innerHTML = `<img src="./Designs/Design-2/icons/noun_Umbrella_2030530.svg"/>`;
+      cityName.innerHTML += `Don’t forget your umbrella. It’s wet in ${json.name} today.`;
       // td.style.borderBottom += '4px dotted #164A68'
-  } else {
-      body.style.background = '#F4F7F8'
-      body.style.color = '#F47775'
-      image.innerHTML = `<img src="./Designs/Design-2/icons/noun_Cloud_1188486.svg"/>`
-      cityName.innerHTML += `Light a fire and get cosy. ${json.name} is looking grey today.`
-  }
+    } else {
+      body.style.background = '#F4F7F8';
+      body.style.color = '#F47775';
+      image.innerHTML = `<img src="./Designs/Design-2/icons/noun_Cloud_1188486.svg"/>`;
+      cityName.innerHTML += `Light a fire and get cosy. ${json.name} is looking grey today.`;
+    }
   });
-  
+
 fetch(API_FORECAST)
   .then((response) => {
     return response.json();
   })
   .then((json) => {
-    const filteredForecast = json.list.filter(item => item.dt_txt.includes('12:00'))
+    const filteredForecast = json.list.filter((item) =>
+      item.dt_txt.includes('12:00')
+    );
 
     // filteredForecast.forEach((_, index) => {
     //   const tempOfTheDay = filteredForecast[index].main.temp.toFixed(0)
     //   const descriptNow = filteredForecast[index].weather[0].description
-     
+
     //   forecast.innerHTML += `
     //   <p>${tempOfTheDay}°C ${descriptNow}</p>`
     // })
 
-    document.querySelectorAll('.forecast-description').forEach((description, index) => {
-      const descriptNow = filteredForecast[index].weather[0].description
-     
-      description.innerHTML += `
-      <p>${descriptNow}</p>`
-    })
+    document
+      .querySelectorAll('.forecast-description')
+      .forEach((description, index) => {
+        const descriptNow = filteredForecast[index].weather[0].description;
+
+        description.innerHTML += `
+      ${descriptNow}`;
+      });
 
     document.querySelectorAll('.temperature').forEach((temp, index) => {
-      const tempOfTheDay = filteredForecast[index].main.temp.toFixed(0)
-          
-      temp.innerHTML += `
-      <p>${tempOfTheDay}°C</p>`
-    })
+      const tempOfTheDay = filteredForecast[index].main.temp.toFixed(0);
 
+      temp.innerHTML += `
+      ${tempOfTheDay}°`;
+    });
 
     // display the name of the weekday for the next five days forecast
 
     // set up an array with the names of each weekday.
     // So weekday[0] = 'Sunday'; weekday[1] = 'Monday' and so on.
-    const weekday = [
-      'sun',
-      'mon',
-      'tue',
-      'wed',
-      'thu',
-      'fri',
-      'sat',
-    ];
+    const weekday = ['sun', 'mon', 'tue', 'wed', 'thu', 'fri', 'sat'];
 
     // local variable where we can store the names of the weekdays
     const weekdaysNamesArray = [];
@@ -128,22 +122,22 @@ fetch(API_FORECAST)
     // });
 
     dayZero.innerHTML += `
-    Today: ${weekday[weekdaysNamesArray[0]]}
-    `
+    ${weekday[weekdaysNamesArray[0]]}
+    `;
 
     dayOne.innerHTML += `
-    Tomorrow: ${weekday[weekdaysNamesArray[1]]}
-    `
+    ${weekday[weekdaysNamesArray[1]]}
+    `;
 
     dayTwo.innerHTML += `
     ${weekday[weekdaysNamesArray[2]]}
-    `
+    `;
 
     dayThree.innerHTML += `
     ${weekday[weekdaysNamesArray[3]]}
-    `
+    `;
 
     dayFour.innerHTML += `
     ${weekday[weekdaysNamesArray[4]]}
-    `
+    `;
   });
