ba7481b5a3e4bba596de1327c11063bf9b182281
Lena Simma <84474447+lenisili@users.noreply.github.com>
2021-09-23 15:53:38 +0200

    Update script.js

--- script.js
+++ script.js
@@ -1,24 +1,4 @@
-// const API_URL = 'api.openweathermap.org/data/2.5/weather?q=London,uk&APPID=00ceff8163f7cba27d66b6501ce70e06'
-const test = document.getElementById('test')
-let temperature 
-let roundTemp
 
-fetch('https://api.openweathermap.org/data/2.5/weather?q=London,uk&APPID=00ceff8163f7cba27d66b6501ce70e06')
-.then((response) => {
-    return response.json()
-  })
-.then((data) => {
-    // today's forecast 
-    console.log('DATA', data)
-    test.innerHTML += `
-    ${data.name}
-    `
-    temperature =  (data.main.temp) - 273.15
-    roundTemp = temperature.toFixed(1)
-    test.innerHTML += `The temperature is ${roundTemp}
-    `
-    test.innerHTML += `${data.weather[0].description}
-    `
     function convertTime(unixTime){
       let date = new Date(unixTime * 1000)
       let hours = date.getHours()
