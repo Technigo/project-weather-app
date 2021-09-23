4d3efafb0ee56bd4355ceb6308ace51fbec09f95
molbimien <43146046+molbimien@users.noreply.github.com>
2021-09-23 21:39:38 +0200

    added styling according to the design 2 sketches

--- script.js
+++ script.js
@@ -10,17 +10,17 @@ const weatherContainer = document.getElementById('weather-container')
 const messageContainer = document.getElementById('message-container')
 const forecastContainer = document.getElementById('forecast-container')
 
-fetch(WEATHER_API_URL)
-    .then(res => res.json()) 
-    .then(weatherData => {
-        console.log('WEATHERDATA!!!', weatherData);
-        //Math.round(25.62*10)/10)
-        //Math.round(256.2)/10
-        //256/10
-        //25.6
-        weatherContainer.innerHTML += /* html */ `
-            <p>Name: ${weatherData.name}</p>
-            <p>Temperature: ${Math.round(weatherData.main.temp * 10)/10}</p>
-            <p>Description: ${weatherData.weather[0].description}</p>
-        `        
-    })
+// fetch(WEATHER_API_URL)
+//     .then(res => res.json()) 
+//     .then(weatherData => {
+//         console.log('WEATHERDATA!!!', weatherData);
+//         //Math.round(25.62*10)/10)
+//         //Math.round(256.2)/10
+//         //256/10
+//         //25.6
+//         weatherContainer.innerHTML += /* html */ `
+//             <p>Name: ${weatherData.name}</p>
+//             <p>Temperature: ${Math.round(weatherData.main.temp * 10)/10}</p>
+//             <p>Description: ${weatherData.weather[0].description}</p>
+//         `        
+//     })
