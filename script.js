ed23d39d36a675f90c16890b99014ddc9674ff61
katiewu1 <83988618+katiewu1@users.noreply.github.com>
2021-09-24 22:32:29 +0200

    deleted unnecessary files and class names. Put the images in the images-folder. Did some more comments

--- script.js
+++ script.js
@@ -8,13 +8,13 @@ const API_ZAGREB =
   'https://api.openweathermap.org/data/2.5/weather?q=Zagreb,%20Croatia&units=metric&APPID=c51f401bafb99cdf6e4b149c98e89cc3';
 const API_FORECAST =
   'https://api.openweathermap.org/data/2.5/forecast?q=Stockholm,Sweden&units=metric&APPID=c51f401bafb99cdf6e4b149c98e89cc3';
-const cityName = document.getElementById('name');
-const temp = document.getElementById('temp');
+
+const body = document.getElementById('body');
 const description = document.getElementById('description');
 const humidity = document.getElementById('humidity');
 const sunTime = document.getElementById('suntimes');
-const body = document.getElementById('body');
 const image = document.getElementById('image');
+const cityName = document.getElementById('name');
 const dayZero = document.getElementById('dayZero');
 const dayOne = document.getElementById('dayOne');
 const dayTwo = document.getElementById('dayTwo');
@@ -27,14 +27,17 @@ fetch(API_URL)
     return response.json();
   })
   .then((json) => {
-    const tempDecimal = json.main.temp.toFixed(0);
-    description.innerHTML += `
-    <p>${json.weather[0].description} | ${tempDecimal}°</p>`;
+    const currentTemp = json.main.temp.toFixed(0);
+    // display current description and temperature
+    description.innerHTML = `
+    <p>${json.weather[0].description} | ${currentTemp}°</p>`;
 
+    //display current humidity
     humidity.innerHTML = `
     <p>${json.main.humidity}% relative humidity</p>`;
-      // new Date() shows todays date. The json.sys.sunrise gets the time for the 
-      // sunrise in ms x 1000 to get a whole second
+
+    // new Date() shows todays date. The json.sys.sunrise gets the time for the
+    // sunrise in ms x 1000 to get a whole second
     const rise = new Date(json.sys.sunrise * 1000);
     const up = rise.toLocaleTimeString([], {
       // returns the date object as a string, using local (timezone) conventions
@@ -49,21 +52,22 @@ fetch(API_URL)
       minute: '2-digit',
     });
     sunTime.innerHTML += `<p>sunset ${down}</p>`;
+
     // Theme changes based on weather
     if (json.weather[0].main === 'Clear') {
       body.style.background = '#F7E9B9';
       body.style.color = '#2A5510';
-      image.innerHTML = `<img src="./Designs/Design-2/icons/noun_Sunglasses_2055147.svg"/>`;
+      image.innerHTML = `<img src="./Designs/Design-2/icons/noun_Sunglasses_2055147.svg" alt=" sunglasses icon"/>`;
       cityName.innerHTML += `Get your sunnies on. ${json.name} is looking rather great today.`;
     } else if (json.weather[0].main === 'Rain') {
       body.style.background = '#A3DEF7';
       body.style.color = '#164A68';
-      image.innerHTML = `<img src="./Designs/Design-2/icons/noun_Umbrella_2030530.svg"/>`;
+      image.innerHTML = `<img src="./Designs/Design-2/icons/noun_Umbrella_2030530.svg" alt="umbrella icon"/>`;
       cityName.innerHTML += `Don’t forget your umbrella. It’s wet in ${json.name} today.`;
-     } else {
+    } else {
       body.style.background = '#F4F7F8';
       body.style.color = '#F47775';
-      image.innerHTML = `<img src="./Designs/Design-2/icons/noun_Cloud_1188486.svg"/>`;
+      image.innerHTML = `<img src="./Designs/Design-2/icons/noun_Cloud_1188486.svg" alt="cloud icon"/>`;
       cityName.innerHTML += `Light a fire and get cosy. ${json.name} is looking grey today.`;
     }
   });
@@ -73,19 +77,22 @@ fetch(API_FORECAST)
     return response.json();
   })
   .then((json) => {
+    // use the filter to just include the data from 12:00 of each day
     const filteredForecast = json.list.filter((item) =>
       item.dt_txt.includes('12:00')
     );
 
+    // find all CSS selectors 'forecast-description' and for each of them display the description
     document
       .querySelectorAll('.forecast-description')
       .forEach((description, index) => {
         const descriptNow = filteredForecast[index].weather[0].description;
 
         description.innerHTML += `
-      ${descriptNow}`;
+        ${descriptNow}`;
       });
 
+    // find all CSS selectors 'temperature' and for each of them display the temperature
     document.querySelectorAll('.temperature').forEach((temp, index) => {
       const tempOfTheDay = filteredForecast[index].main.temp.toFixed(0);
 
@@ -93,21 +100,22 @@ fetch(API_FORECAST)
       ${tempOfTheDay}°C`;
     });
 
-    // display the name of the weekday for the next five days forecast
+    // an array where we store the names. weekday[0]='sun', weekday[1]='mon' and so on
     const weekday = ['sun', 'mon', 'tue', 'wed', 'thu', 'fri', 'sat'];
 
-    // local variable where we can store the names of the weekdays
+    // local variable where we can store the weekdays from the forecast data
     const weekdaysNamesArray = [];
 
     filteredForecast.forEach((_, index) => {
       // for all of the five arrays in 'filteredForecast' we want to get the Date and Time
-      // we can get the data from 'filteredForecast[index].dt_txt'
+      // we can get the data from 'filteredForecast[index].dt' and multiply it with 1000
       // store each data in a variable named 'dateAndTime'
       const dateAndTime = new Date(filteredForecast[index].dt * 1000);
       // store the data that define which weekday it is in an array named 'weekdaysNamesArray'
       weekdaysNamesArray[index] = dateAndTime.getDay();
     });
 
+    // display the days in another way (otherwise we can also apply querySelectorAll and forEach here too)
     dayZero.innerHTML += `
     ${weekday[weekdaysNamesArray[0]]}
     `;
@@ -128,12 +136,13 @@ fetch(API_FORECAST)
     ${weekday[weekdaysNamesArray[4]]}
     `;
   });
-  
+
 fetch(API_ROME)
   .then((response) => {
     return response.json();
   })
   .then((json) => {
+    // display the city name and temperature with the function 'generateCity'
     cityContainer.innerHTML += generateCity(json);
   });
 let generateCity = (city) => {
@@ -141,15 +150,21 @@ let generateCity = (city) => {
   const cityTemp = city.main.temp;
 
   let cityHTML = '';
-  cityHTML += `<div class="cityCard"><p>${cityName} ${cityTemp.toFixed()}°C</p><img class="city-photo" src='./images/${cityName}.jpg'></div>`;
+  cityHTML += `
+  <div class="cityCard">
+    <p>${cityName} ${cityTemp.toFixed()}°C</p>
+    <img class="city-photo" src="./images/${cityName}.jpg" alt="city photo" />
+  </div>`;
 
   return cityHTML;
 };
+
 fetch(API_HURGHADA)
   .then((response) => {
     return response.json();
   })
   .then((json) => {
+    // display the city name and temperature with the function 'generateCity'
     cityContainer.innerHTML += generateCity(json);
   });
 fetch(API_ZAGREB)
@@ -157,5 +172,6 @@ fetch(API_ZAGREB)
     return response.json();
   })
   .then((json) => {
+    // display the city name and temperature with the function 'generateCity'
     cityContainer.innerHTML += generateCity(json);
   });
