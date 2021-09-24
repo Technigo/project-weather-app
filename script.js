3c92596aa48d53bcec40d4062254857d89c6c193
sofiawillebrand <82705594+sofiawillebrand@users.noreply.github.com>
2021-09-24 13:17:46 +0200

    add classes for styling

--- script.js
+++ script.js
@@ -6,6 +6,7 @@ const WEATHER_API_URL = 'https://api.openweathermap.org/data/2.5/weather?q=Stock
 const FORECAST_API_URL = 'https://api.openweathermap.org/data/2.5/forecast?q=Stockholm,Sweden&units=metric&APPID=96fe044f50a395c3e9ed78deeb27089f'
 // Calls 5 day weather forecast data for Stockholm, Sweden using the api key: 96fe044f50a395c3e9ed78deeb27089f
 
+const body = document.body
 const weatherContainer = document.getElementById('weather-container')
 const messageContainer = document.getElementById('message-container')
 const forecastContainer = document.getElementById('forecast-container')
@@ -18,7 +19,7 @@ fetch(WEATHER_API_URL)
         // multiplied by 1000 so that the argument is in milliseconds, not seconds.
         const timeSunrise = new Date(weatherData.sys.sunrise*1000)
         const timeSunset = new Date(weatherData.sys.sunset*1000)
-        const weather = 'Thunderstorm' // weatherData.weather[0].main
+        const weather = 'Clouds' // weatherData.weather[0].main
 
         // adds html to the section weather-container.
         // to get the time to display as 00:00 and not 0:0 we add a zero to the beginning of the time
@@ -35,36 +36,42 @@ fetch(WEATHER_API_URL)
         `
         // depending on weather different html and icons are added
         if(weather === 'Clouds'){
+            body.classList.add('cloudy')
             messageContainer.innerHTML = /*html*/`
                 <img src="./Designs/Design-2/icons/noun_Cloud_1188486.svg" />
                 <p>Light a fire and get cosy. ${weatherData.name} is looking grey today.</p>
             `
         }
         else if (weather === 'Clear'){
+            body.classList.add('sunny')
             messageContainer.innerHTML = /*html*/`
                 <img src="./Designs/Design-2/icons/noun_Sunglasses_2055147.svg" />
                 <p>Get your sunnies on. ${weatherData.name} is looking rather great today.</p>
             `
         }
         else if (weather === 'Snow'){
+            body.classList.add('rainy')
             messageContainer.innerHTML = /*html*/`
                 <i class="far fa-snowflake fa-5x"></i>
                 <p>${weatherData.name} is chilly today... Ho ho ho, here comes the ${weather.toLowerCase()}!</p>
             `
         }
         else if (weather === 'Rain'){
+            body.classList.add('rainy')
             messageContainer.innerHTML = /*html*/`
                 <img src="./Designs/Design-2/icons/noun_Umbrella_2030530.svg" />
                 <p>Don't forget your umbrella. It's wet in ${weatherData.name} today.</p>
             `
         }
         else if (weather === 'Drizzle'){
+            body.classList.add('rainy')
             messageContainer.innerHTML = /*html*/`
                 <i class="fas fa-cloud-rain fa-5x"></i>
                 <p>It's a ${weather.toLowerCase()} in ${weatherData.name} today, tricky weather for gremlins!</p>
             `
         }
         else if (weather === 'Thunderstorm'){
+            body.classList.add('rainy')
             messageContainer.innerHTML = /*html*/`
                 <i class="fas fa-poo-storm fa-5x"></i>
                 <p>Oh no, there is a ${weather.toLowerCase()} in ${weatherData.name}! Don't stand next to a tall tree</p>
