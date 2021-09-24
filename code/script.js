f064b254f51277d6d3d0f1e084f98bd681300df1
Johanna Mannestål <johannamannestal@Johannas-MacBook-Air.local>
2021-09-24 14:26:48 +0200

    feature freeze

--- code/script.js
+++ code/script.js
@@ -1,22 +1,26 @@
 // Global variables
 const todaysWeather = document.getElementById("weatherContainer");
 const fiveDayForecastContainer = document.getElementById("fiveDayForecast");
-const API_WEATHER = "https://api.openweathermap.org/data/2.5/weather?q=Stockholm,Sweden&units=metric&APPID=f9773f2491f9348664665c65e8d966c3";
-const API_FIVE_DAY_FORECAST = "https://api.openweathermap.org/data/2.5/forecast?q=Stockholm,Sweden&units=metric&APPID=f9773f2491f9348664665c65e8d966c3";
+const API_WEATHER =
+  "https://api.openweathermap.org/data/2.5/weather?q=stockholm,Sweden&units=metric&APPID=f9773f2491f9348664665c65e8d966c3";
+const API_FIVE_DAY_FORECAST =
+  "https://api.openweathermap.org/data/2.5/forecast?q=stockholm,Sweden&units=metric&APPID=f9773f2491f9348664665c65e8d966c3";
 
-const currentWeatherPictureContainer = document.getElementById("currentWeatherPictureContainer");
+const currentWeatherPictureContainer = document.getElementById(
+  "currentWeatherPictureContainer"
+);
 currentWeatherPictureContainer.className = "current-weather-picture-container";
 
 const cityName = document.getElementById("cityName");
-cityName.className = "city-name";
+//cityName.className = "city-name";
 const currentTemperature = document.getElementById("currentTemperature");
-currentTemperature.className = "current-temperature";
+//currentTemperature.className = "current-temperature";
 const currentWeatherStatus = document.getElementById("currentWeatherStatus");
-currentWeatherStatus.className = "current-weather-status";
+//currentWeatherStatus.className = "current-weather-status";
 const sunrise = document.getElementById("sunrise");
-sunrise.className = "sunrise";
+// sunrise.className = "sunrise";
 const sunset = document.getElementById("sunset");
-sunset.className = "sunset";
+// sunset.className = "sunset";
 
 // Object with all emoji
 const emojiObject = {
@@ -37,7 +41,9 @@ const staticImg = {
 };
 
 const fetchWeather = () => {
-  fetch("https://api.openweathermap.org/data/2.5/weather?q=Stockholm,Sweden&units=metric&APPID=f9773f2491f9348664665c65e8d966c3")
+  fetch(
+    "https://api.openweathermap.org/data/2.5/weather?q=stockholm,Sweden&units=metric&APPID=f9773f2491f9348664665c65e8d966c3"
+  )
     .then((response) => response.json())
     .then((data) => {
       const todaysTemp = data.main.temp_max.toFixed(0);
@@ -71,10 +77,21 @@ const fetchSunriseSunset = () => {
     .then((response) => response.json())
     .then((data) => {
       console.log("Data from sunrise/sunset:", data);
-      const sunriseDate = new Date((data.sys.sunrise + data.timezone + new Date().getTimezoneOffset() * 60) * 1000);
-      const sunriseTime = sunriseDate.getHours() + ":" + sunriseDate.getMinutes();
+      const sunriseDate = new Date(
+        (data.sys.sunrise +
+          data.timezone +
+          new Date().getTimezoneOffset() * 60) *
+          1000
+      );
+      const sunriseTime =
+        sunriseDate.getHours() + ":" + sunriseDate.getMinutes();
 
-      const sunsetDate = new Date((data.sys.sunset + data.timezone + new Date().getTimezoneOffset() * 60) * 1000);
+      const sunsetDate = new Date(
+        (data.sys.sunset +
+          data.timezone +
+          new Date().getTimezoneOffset() * 60) *
+          1000
+      );
       const sunsetTime = sunsetDate.getHours() + ":" + sunsetDate.getMinutes();
       sunrise.innerHTML += `
       <img class="sun-img"src="/Designs/Design-1/assets/sunrise32.png"/> ${sunriseTime}
@@ -93,20 +110,30 @@ const fetchForecast = () => {
     .then((data) => {
       console.log("New Forecast JSON:", data);
 
-      let filteredFiveDays = data.list.filter((item) => item.dt_txt.includes("12:00"));
+      let filteredFiveDays = data.list.filter((item) =>
+        item.dt_txt.includes("09:00")
+      );
       console.log(filteredFiveDays);
-      let filteredFiveNights = data.list.filter((item) => item.dt_txt.includes("21:00"));
+      // let filteredFiveNights = data.list.filter((item) =>
+      //   item.dt_txt.includes("21:00")
+      // );
 
       filteredFiveDays.forEach((item) => {
         fiveDayForecastContainer.innerHTML += `
         <div class="weekday-row">
-        <div class="forecast-content" id="week-day">${new Date(item.dt_txt).toLocaleDateString("en-US", {
+        <div class="forecast-content" id="week-day">${new Date(
+          item.dt_txt
+        ).toLocaleDateString("en-US", {
           weekday: "short",
         })}
         </div>
-        <div class="forecast-content" id="emoji"> <img src = "${emojiObject[item.weather[0].main]}"></div>
+        <div class="forecast-content" id="emoji"> <img src = "${
+          emojiObject[item.weather[0].main]
+        }"></div>
         
-        <div class="forecast-content" id="temperature">${item.main.temp_max.toFixed(0)}°C</div>
+        <div class="forecast-content" id="temperature">${item.main.temp_max.toFixed(
+          1
+        )}°C</div>
         </div>`;
       });
     })
