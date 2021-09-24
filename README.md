39f2b8ee537440d457b8532e09c1f72c2e8acde5
Kara Howes <karahowes@Karas-MacBook-Air.local>
2021-09-24 12:27:49 +0200

    changed city to paris, added new icons

--- README.md
+++ README.md
@@ -1,13 +1,14 @@
 # Weather App
 
-Replace this readme with your own information about your project.
-
-Start by briefly describing the assignment in a sentence or two. Keep it short and to the point.
+This is a weather web app that shows today's weather and temperature in London, and a 5-day forecast for London using a weather API. This was alos a practise how to work in a dev team.
 
 ## The problem
 
-Describe how you approached to problem, and what tools and techniques you used to solve it. How did you plan? What technologies did you use? If you had more time, what would be next?
+There were serveral issues along the way. The group had to collaborate and in order to do that we had to meet several times which could be kind of tricky.
+We used a weather API https://api.openweathermap.org to fetch real live data in javascript.
+
+If we had more time we would dsplay more data and add some more CSS-styling.
 
 ## View it live
 
-Every project should be deployed somewhere. Be sure to include the link to the deployed project so that the viewer can click around and see what it's all about.
+https://dreamy-minsky-cb23fa.netlify.app/
--- script.js
+++ script.js
@@ -1,5 +1,6 @@
 //for weather forecast API URL
-const FORECAST_API_URL = 'https://api.openweathermap.org/data/2.5/forecast?q=London,uk&units=metric&APPID=00ceff8163f7cba27d66b6501ce70e06'
+const FORECAST_API_URL = 'https://api.openweathermap.org/data/2.5/forecast?q=Paris,fr&units=metric&APPID=00ceff8163f7cba27d66b6501ce70e06'
+const CURRENT_API_URL = 'https://api.openweathermap.org/data/2.5/weather?q=Paris,fr&APPID=00ceff8163f7cba27d66b6501ce70e06'
 
 //Dom selector
 const forecastContainer = document.getElementById('forecast')
@@ -9,12 +10,12 @@ const headerBackground = document.getElementById('header')
 //global variables
 let temperature 
 let roundTemp
+
 //for forecast 
 let fiveDayForecast = {}  
 
 //fetch function
-// TEST!
-fetch('https://api.openweathermap.org/data/2.5/weather?q=London,uk&APPID=00ceff8163f7cba27d66b6501ce70e06')
+fetch(CURRENT_API_URL)
 .then((response) => {
     return response.json()
   })
@@ -27,6 +28,7 @@ fetch('https://api.openweathermap.org/data/2.5/weather?q=London,uk&APPID=00ceff8
   const description = toTitleCase(data.weather[0].description)
   let sunrise = convertTime(data.sys.sunrise)
   let sunset = convertTime(data.sys.sunset)
+
   currentWeather.innerHTML += `
     <div class= "wrapper">
       <h1 class = "temperature" >
@@ -63,8 +65,6 @@ fetch('https://api.openweathermap.org/data/2.5/weather?q=London,uk&APPID=00ceff8
 .catch((error) => console.error(error))
 .then(() => console.log('Request finished'));
 
-//END TEST
-
 //for weather forecast fetch function
 fetch(FORECAST_API_URL)
   .then((res) => {
@@ -74,21 +74,23 @@ fetch(FORECAST_API_URL)
     console.log('Forecast DATA', data) // just for testing purpose 
    
     createForecast(data) //calling CreateForecast function to create forecast for next five days with minimum temperature, maximum temperature and formatted date
-    console.log (typeof(fiveDayForecast)) //for test purpose
-    Object.values(fiveDayForecast).forEach(item => {  // in order to iterate each value of object as forEach method does not work on object directly (https://flexiple.com/loop-through-object-javascript/)
-     const  {date, weatherIcon, minTemp, maxTemp}= item  
+    
+    let forecastArray = Object.values(fiveDayForecast)
+    forecastArray.shift()
+    forecastArray.forEach(item => {  // in order to iterate each value of object as forEach method does not work on object directly (https://flexiple.com/loop-through-object-javascript/)
+    const  {date, weatherIcon, minTemp, maxTemp}= item  
       forecastContainer.innerHTML +=`
-      <div class= "js-row">
-        <span> 
-         ${date} 
-        </span>
-        <span>
-        <img src="${weatherIcon}"/>
-        </span>
-        <span>
-        ${minTemp.toFixed(0)}&#8451 / ${maxTemp.toFixed(0)}&#8451
-        </span>
-      </div>
+        <div class= "js-row">
+          <span> 
+            ${date} 
+          </span>
+          <span>
+            <img src="${weatherIcon}"/>
+          </span>
+          <span>
+            ${minTemp.toFixed(0)}&#8451 / ${maxTemp.toFixed(0)}&#8451
+          </span>
+        </div>
     `
     })
     
@@ -101,12 +103,12 @@ fetch(FORECAST_API_URL)
 
 // function to create object of forecast with min, max temperature and formatted date
 const createForecast = (data) => {
-  
-  
+    
   data.list.forEach((item) => {  //iterating the 'list' item of data
     const currentDate = item.dt_txt.split(" ")[0]  //creating a variable by splitting  item.dt_txt and returning the date part
-    console.log(currentDate)
+    
     if (fiveDayForecast[currentDate]) { //checking if an object with key as currentDate is created, if true compare the value of min and max temperature and set values accordingly
+      
       if (item.main.temp_min < fiveDayForecast[currentDate].minTemp) {
         fiveDayForecast[currentDate].minTemp = item.main.temp_min
         console.log[fiveDayForecast[currentDate].minTemp]
@@ -114,10 +116,12 @@ const createForecast = (data) => {
       if (item.main.temp_max > fiveDayForecast[currentDate].maxTemp) {
         fiveDayForecast[currentDate].maxTemp = item.main.temp_max
       }
+
     } else {  //else create an object with currentDate as key
         let forecastDate = convertDate(item.dt) //calling convertDate function for formatting the date
         let DescriptionImage = weatherDescriptionImage(item.weather[0].description) 
         fiveDayForecast[currentDate] = {
+          
           "date": forecastDate,
           "weatherIcon":DescriptionImage,
           "minTemp": item.main.temp_min,
@@ -129,77 +133,96 @@ const createForecast = (data) => {
   })
 
 }
+
 // converting date format
-function convertDate (fetchedDate)  {
+function convertDate(fetchedDate) {
+  
   const week = ['Sun', 'Mon', 'Tue', 'Wed', 'Thu', 'Fri', 'Sat']
-  const month = ['JAN', 'FEB','MAR', 'APR', 'MAY','JUN', 'JUL', 'AUG', 'SEP', 'OCT', 'NOV', 'DEC' ]
   const date = new Date(fetchedDate * 1000)
-  //converting month
-  let newMonth = date.getMonth()
-  let whatMonth = month[newMonth]
-  console.log(whatMonth)
+
   //converting day
   let newDay = date.getDay()
   let whatDay = week[newDay]
-  console.log(whatDay)
+
   //constructing the required format
-  //let formattedDate = ` ${whatDay} ${date.getDate()} ${whatMonth}`
-   let formattedDate = ` ${whatDay}`
+  let formattedDate = ` ${whatDay}`
+
   //returning the formatted date
   return formattedDate 
 
 }
 const weatherDescriptionImage = (desc) => {
   
-  if (desc === 'clear sky') { return './Designs/Design-1/assets/Group38.png';
-  } else if (desc === 'few clouds') {
-    return './Designs/Design-1/assets/Group34.png';
+  if (desc === 'clear sky') {
+    return './Designs/Design-1/assets/Group38.png'
+  
+  } else if (desc === 'few clouds') { 
+    return './Designs/Design-1/assets/Group34.png'
+  
   } else if (desc === 'scattered clouds') {
-    return './Designs/Design-1/assets/Group16.png';
+    return './Designs/Design-1/assets/Group16.png'
+  
   } else if (desc === 'broken clouds') {
-    return './Designs/Design-1/assets/Group16.png';
+    return './Designs/Design-1/assets/Group16.png'
+
   } else if (desc === 'shower rain') {
-    return './Designs/Design-1/assets/rain.png';
+    return './Designs/Design-1/assets/Group16.png'
+
   } else if (desc === 'rain') {
-    return './Designs/Design-1/assets/rain.png';
+    return './Designs/Design-1/assets/Group16.png'
+
   } else if (desc === 'thunderstorm') {
-    return './Designs/Design-1/assets/Group16.png';
+    return './Designs/Design-1/assets/Group16.png'
+
   } else if (desc === 'snow') {
-    return './Designs/Design-1/assets/Group16.png';
+    return './Designs/Design-1/assets/Group16.png'
+
   } else {
-    return './Designs/Design-1/assets/Group16.png';
+    return './Designs/Design-1/assets/Group16.png'
   }
 }
 
 const bigWeatherIcons = (desc) => {
   
   if (desc === 'clear sky') {
-    headerBackground.style.backgroundImage = "linear-gradient(#ade8f4,#caf0f8)"
-    return './Designs/Design-1/assets/sun-icon.png';
+    headerBackground.style.backgroundImage = "linear-gradient(#8b8fff,#e3e4ff)"
+    return './Designs/Design-1/assets/sun-icon.png'
+
   } else if (desc === 'few clouds') {
     headerBackground.style.backgroundImage = "linear-gradient(#ade8f4,#caf0f8)"
-    return './Designs/Design-1/assets/Group34.png';
+    return './Designs/Design-1/assets/sun_cloud.png'
+  
+  } else if (desc === 'overcast clouds') {
+    headerBackground.style.backgroundImage = "linear-gradient(#6c757d, #adb5bd)"
+    return './Designs/Design-1/assets/cloud.png'
+
   } else if (desc === 'scattered clouds') {
-    headerBackground.style.backgroundImage = "linear-gradient(#ade8f4,#caf0f8)"
-    return './Designs/Design-1/assets/sun-cloud2.png';
+    headerBackground.style.backgroundImage = "linear-gradient(#6c757d, #adb5bd)"
+    return './Designs/Design-1/assets/sun_cloud.png'
+
   } else if (desc === 'broken clouds') {
-    headerBackground.style.backgroundImage = "linear-gradient(#333,#caf0f8)"
-    return './Designs/Design-1/assets/cloud.png';
+    headerBackground.style.backgroundImage = "linear-gradient(#6c757d, #adb5bd)"
+    return './Designs/Design-1/assets/cloud.png'
+
   } else if (desc === 'shower rain') {
-    headerBackground.style.backgroundImage = "linear-gradient(#ade8f4,#caf0f8)"
-    return './Designs/Design-1/assets/rain.png';
+    headerBackground.style.backgroundImage = "linear-gradient(#87A1B6,#E1E7EB)"
+    return './Designs/Design-1/assets/rain.png'
+
   } else if (desc === 'rain') {
-    headerBackground.style.backgroundImage = "linear-gradient(#ade8f4,#caf0f8)"
-    return './Designs/Design-1/assets/rain.png';
+    headerBackground.style.backgroundImage = "linear-gradient(#87A1B6,#E1E7EB)"
+    return './Designs/Design-1/assets/rain.png'
+
   } else if (desc === 'thunderstorm') {
-    headerBackground.style.backgroundImage = "linear-gradient(#ade8f4,#caf0f8)"
-    return './Designs/Design-1/assets/NA.png';
+    headerBackground.style.backgroundImage = "linear-gradient(#87A1B6,#E1E7EB)"
+    return './Designs/Design-1/assets/NA.png'
+
   } else if (desc === 'snow') {
     headerBackground.style.backgroundImage = "linear-gradient(#ade8f4,#caf0f8)"
-    return './Designs/Design-1/assets/NA.png';
+    return './Designs/Design-1/assets/NA.png'
+
   } else {
     headerBackground.style.backgroundImage = "linear-gradient(#ade8f4,#caf0f8)"
-    return './Designs/Design-1/assets/NA.png';
+    return './Designs/Design-1/assets/NA.png'
   }
 }
 
@@ -210,7 +233,7 @@ const toTitleCase= (str)=> {
     function(txt) {
       return txt.charAt(0).toUpperCase() + txt.substr(1).toLowerCase();
     }
-  );
+  )
 }
 
 function convertTime(unixTime){
@@ -219,7 +242,5 @@ function convertTime(unixTime){
   let minutes = "0" + date.getMinutes()
   let time = hours + ":" + minutes.substr(-2)
   return time
-  //console.log(time)
 }
-
 
