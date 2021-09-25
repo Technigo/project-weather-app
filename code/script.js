4c8982cc3ced0bd4ef69780c9a7ca54fcd51602f
Jenny Quach <quach.jen@gmail.com>
2021-09-25 23:05:25 +0200

    removed console logs and all ;

--- code/script.js
+++ code/script.js
@@ -1,11 +1,11 @@
-const weatherData = document.getElementById("weatherdata");
-const weatherToday = document.getElementById("weather-today");
-const weatherForecast = document.getElementById("weather-forecast");
+const weatherData = document.getElementById("weatherdata")
+const weatherToday = document.getElementById("weather-today")
+const weatherForecast = document.getElementById("weather-forecast")
 
 const API_URL =
-  "https://api.openweathermap.org/data/2.5/weather?q=cityname&units=metric&APPID=5caaaf25021b2d7aa4d206126b6a3351";
+  "https://api.openweathermap.org/data/2.5/weather?q=cityname&units=metric&APPID=5caaaf25021b2d7aa4d206126b6a3351"
 const API_Forecast =
-  "https://api.openweathermap.org/data/2.5/forecast?q=cityname&units=metric&APPID=5caaaf25021b2d7aa4d206126b6a3351";
+  "https://api.openweathermap.org/data/2.5/forecast?q=cityname&units=metric&APPID=5caaaf25021b2d7aa4d206126b6a3351"
 const getData = () => {
   //made global function so we can call from different areas in the code
 
@@ -13,13 +13,13 @@ const getData = () => {
     .then((response) => response.json())
     .then((data) => {
       //users local timezone offset
-      let usersTimeZoneOffset = new Date().getTimezoneOffset() * 60;
+      let usersTimeZoneOffset = new Date().getTimezoneOffset() * 60
 
       //create variables for localtime, sunset and sunrise in current weather city
       //with users timezineoffset to handle ex CET
-      let localTimeNow = new Date(Date.now() + (data.timezone * 1000) + (usersTimeZoneOffset * 1000));
-      let localTimeSunrise = new Date((data.sys.sunrise * 1000) + (data.timezone * 1000) + (usersTimeZoneOffset * 1000));
-      let localTimeSunset = new Date((data.sys.sunset * 1000) + (data.timezone * 1000) + (usersTimeZoneOffset * 1000));
+      let localTimeNow = new Date(Date.now() + (data.timezone * 1000) + (usersTimeZoneOffset * 1000))
+      let localTimeSunrise = new Date((data.sys.sunrise * 1000) + (data.timezone * 1000) + (usersTimeZoneOffset * 1000))
+      let localTimeSunset = new Date((data.sys.sunset * 1000) + (data.timezone * 1000) + (usersTimeZoneOffset * 1000))
 
       weatherToday.innerHTML = /* html */ `
       <div class="weather-container-div">
@@ -37,123 +37,117 @@ const getData = () => {
       </span>
           </div>
       </div>
-      `;
+      `
       // toFixed(1) rounds the temp to one decimal
       // time
 
-      let isNight = localTimeNow > localTimeSunset || localTimeNow < localTimeSunrise;
-      temp = data.main.temp;
-      var mobil = window.matchMedia("(max-width: 769px)");
+      let isNight = localTimeNow > localTimeSunset || localTimeNow < localTimeSunrise
+      temp = data.main.temp
+      const mobil = window.matchMedia("(max-width: 769px)")
 
       if (isNight) {
-        document.body.classList.add('night');
-        document.body.classList.remove('day');
+        document.body.classList.add('night')
+        document.body.classList.remove('day')
       }
       else {
-        document.body.classList.add('day');
-        document.body.classList.remove('night');
+        document.body.classList.add('day')
+        document.body.classList.remove('night')
       }
 
       if (mobil.matches) {
-        const nav = document.getElementById("nav");
+        const nav = document.getElementById("nav")
 
         if (isNight) {
-          nav.style.background = "var(--nightphone)";
-          weatherToday.style.backgroundImage = "var(--imgn)";
-          weatherToday.style.backgroundSize = "cover";
-          weatherToday.style.color = "white";
+          nav.style.background = "var(--nightphone)"
+          weatherToday.style.backgroundImage = "var(--imgn)"
+          weatherToday.style.backgroundSize = "cover"
+          weatherToday.style.color = "white"
         } else {
-          nav.style.background = "var(--dayphone)";
-          weatherToday.style.backgroundImage = "var(--imgd)";
-          weatherToday.style.backgroundSize = "cover";
-          weatherToday.style.color = "black";
+          nav.style.background = "var(--dayphone)"
+          weatherToday.style.backgroundImage = "var(--imgd)"
+          weatherToday.style.backgroundSize = "cover"
+          weatherToday.style.color = "black"
         }
       } else {
         if (temp >= 25 && temp <= 65) {
           if (isNight) {
-            weatherToday.style.background = "var(--hotnight)";
-            weatherToday.style.color = "var(--textcolornight)";
+            weatherToday.style.background = "var(--hotnight)"
+            weatherToday.style.color = "var(--textcolornight)"
           } else {
-            weatherToday.style.background = "var(--hot)";
+            weatherToday.style.background = "var(--hot)"
           }
         }
         if (temp >= 0 && temp <= 24) {
           if (isNight) {
-            weatherToday.style.background = "var(--moderatenight)";
+            weatherToday.style.background = "var(--moderatenight)"
           } else {
-            weatherToday.style.background = "var(--moderate)";
+            weatherToday.style.background = "var(--moderate)"
           }
         }
 
         if (temp >= -40 && temp <= -1) {
           if (isNight) {
-            weatherToday.style.background = "var(--coldnight)";
+            weatherToday.style.background = "var(--coldnight)"
           } else {
-            weatherToday.style.background = "var(--cold)";
+            weatherToday.style.background = "var(--cold)"
           }
         }
       }
     })
     .catch((error) => console.error("Error: ", error))
-    .finally(() => console.log("Request done"));
+    .finally(() => console.log("Request done"))
 
   fetch(API_Forecast.replace("cityname", currentCity))
     .then((res) => res.json())
     .then((forecast) => {
       const filteredForecast = forecast.list.filter((day) =>
         day.dt_txt.includes("12:00")
-      );
-      weatherForecast.innerHTML = "";
+      )
+      weatherForecast.innerHTML = ""
 
       filteredForecast.forEach((day) => {
-        const date = new Date(day.dt * 1000);
-        const dayName = date.toLocaleDateString("en-US", { weekday: "long" });
-        const weekTemp = day.main.temp.toFixed(0);
+        const date = new Date(day.dt * 1000)
+        const dayName = date.toLocaleDateString("en-US", { weekday: "long" })
+        const weekTemp = day.main.temp.toFixed(0)
 
         weatherForecast.innerHTML += `<p id = "smaller-text" > ${dayName}</p>
         <p id="smaller-text">🌡️${weekTemp}°C</p>
         <p id="smaller-text">Feels like ${day.main.feels_like.toFixed(1)}°C</p>
         <img id="day-night-icon" src="https://openweathermap.org/img/wn/${day.weather[0].icon
-          }@2x.png"/>`;
-      });
-    });
-};
+          }@2x.png"/>`
+      })
+    })
+}
 
 //Forecast is showing the upcoming 5 days
 const handleWeatherApiResponse = (forecastForCity) => {
-  console.log(forecastForCity);
   weatherToday.innerHTML = `
-  < p > City: ${forecastForCity.name}</p >
+  <p>City: ${forecastForCity.name}</p>
   <p>Temp: ${forecastForCity.main.temp.toFixed(1)}°C</p>
-  <p>Weather: ${forecastForCity.weather[0].description}</p>
-`;
-};
+  <p>Weather: ${forecastForCity.weather[0].description}</p>`
+}
 
 //Activating the Hamburger Menu to select another city
 const onCityChanged = (event) => {
-  document.querySelector(".menu-btn").checked = false;
-  console.log(event.target.dataset.cityname);
-  currentCity = event.target.dataset.cityname;
-  getData();
-};
+  document.querySelector(".menu-btn").checked = false
+  currentCity = event.target.dataset.cityname
+  getData()
+}
 
 //Setting the default city
 const getDefaultCity = () => {
-  let cities = document.querySelectorAll("[data-cityname]");
-  return cities[0].dataset.cityname; //dataset is an object with all data attributes inside https://www.w3schools.com/tags/att_global_data.asp
-};
+  let cities = document.querySelectorAll("[data-cityname]")
+  return cities[0].dataset.cityname //dataset is an object with all data attributes inside https://www.w3schools.com/tags/att_global_data.asp
+}
 
 //choose a city in the Hamburger Menu on click
 const initializeCitySelector = () => {
-  console.log("begin initializeCitySelector");
-  let cities = document.querySelectorAll("[data-cityname]");
+  let cities = document.querySelectorAll("[data-cityname]")
   cities.forEach((element) => {
-    console.log("adding event listener");
-    element.addEventListener("click", onCityChanged);
-  });
-  console.log("finished initializeCitySelector");
-};
+    element.addEventListener("click", onCityChanged)
+  })
+}
 
-initializeCitySelector();
-let currentCity = getDefaultCity(); //a function to get default city
-getData();
+initializeCitySelector()
+let currentCity = getDefaultCity() //a function to get default city
+getData()
