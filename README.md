d3576fe7b3bc4683b404f74dbf6dd32e510bb0d1
pdetli <74938586+pdetli@users.noreply.github.com>
2021-09-26 21:31:22 +0200

    bg images and icons updated & readme added

--- README.md
+++ README.md
@@ -1,13 +1,16 @@
 # Weather App
 
-Replace this readme with your own information about your project.
-
-Start by briefly describing the assignment in a sentence or two. Keep it short and to the point.
+Project to create a weather app with API data that show main weather for today and forecast weather for upcoming days.
 
 ## The problem
 
-Describe how you approached to problem, and what tools and techniques you used to solve it. How did you plan? What technologies did you use? If you had more time, what would be next?
+We approached the project as a team first planning on how we wanted to structure the app layout, did some first code on a master branch. Then we split up
+in two teams on feature branches for main weather and forecast. After that we merged the branches togheter and finalized it with more addons afterwards.
+
+We used API:s, addeventlisteners, functions, dom-selectors, if/else conditionals etc...
+
+Some difficulties with the structure when it came to planning as a team. If we had more time we would've liked to plan even more and add Geo Location and more fun stuff.
 
 ## View it live
 
-Every project should be deployed somewhere. Be sure to include the link to the deployed project so that the viewer can click around and see what it's all about.
+https://weather-app-by-team-fox.netlify.app/
--- script.js
+++ script.js
@@ -1,18 +1,30 @@
-console.log("Hello world!");
-const API_KEY = "96cb0f55d34310e596ed4792c7800540"
 
+const API_KEY = "96cb0f55d34310e596ed4792c7800540" //Token api key
+
+// -------------------- API weather&forecast links for each city------------
+const API_STOCKHOLM_W = `https://api.openweathermap.org/data/2.5/weather?q=Stockholm&units=metric&APPID=${API_KEY}`
+const API_STOCKHOLM_F = `https://api.openweathermap.org/data/2.5/forecast?q=Stockholm,Sweden&units=metric&APPID=${API_KEY}`
+const API_BARCELONA_W = `https://api.openweathermap.org/data/2.5/weather?q=Barcelona&units=metric&APPID=${API_KEY}`
+const API_BARCELONA_F = `https://api.openweathermap.org/data/2.5/forecast?q=Barcelona,Spain&units=metric&APPID=${API_KEY}`
+const API_PARIS_W = `https://api.openweathermap.org/data/2.5/weather?q=Paris&units=metric&APPID=${API_KEY}`
+const API_PARIS_F = `https://api.openweathermap.org/data/2.5/forecast?q=Paris,France&units=metric&APPID=${API_KEY}`
+
+// -------------------- DOMs --------------------
 const weatherContainer = document.getElementById("weather");
 const forecastContainer = document.getElementById("forecast");
 const sunriseContainer = document.getElementById("sunrise");
 const sunsetContainer = document.getElementById("sunset");
 const mainContainer = document.getElementById('mainWeather')
+const weatherIconContainer = document.getElementById('weatherIcon')
 const cityButton = document.getElementById('cityBtn')
 const body = document.getElementById('body')
+// -------------------- DOMs end --------------------
 
-// console.log("This is the first API", API_CITY)
+// -------------------- Fetch weather section -------------------- 
+// Fetch for Stockholm weather
 const fetchStockholm = () => {
-  fetch(`https://api.openweathermap.org/data/2.5/weather?q=Stockholm&units=metric&APPID=${API_KEY}`)
-    .then((res) => {
+  fetch(API_STOCKHOLM_W)
+      .then((res) => {
       return res.json();
     })
     .then((data) => {
@@ -20,11 +32,12 @@ const fetchStockholm = () => {
     })
 };
 
-fetchStockholm();
+fetchStockholm(); //Loads Stockholm as first city
 
+// Fetch for Stockholm forecast
 const fetchForecastStockholm = () => {
-  fetch(`https://api.openweathermap.org/data/2.5/forecast?q=Stockholm,Sweden&units=metric&APPID=${API_KEY}`)
-    .then((res) => {
+  fetch(API_STOCKHOLM_F)
+  .then((res) => {
       return res.json();
     })
     .then((data) => {
@@ -32,11 +45,12 @@ const fetchForecastStockholm = () => {
     })
 };
 
-fetchForecastStockholm();
+fetchForecastStockholm(); //Loads Stockholm as first city
 
+// Fetch for Barcelona weather
 const fetchBarcelona = () => {
-  fetch(`https://api.openweathermap.org/data/2.5/weather?q=Barcelona&units=metric&APPID=${API_KEY}`)
-    .then((res) => {
+  fetch(API_BARCELONA_W)
+  .then((res) => {
       return res.json();
     })
     .then((data) => {
@@ -44,9 +58,10 @@ const fetchBarcelona = () => {
     })
 };
 
+// Fetch for Barcelona forecast
 const fetchForecastBarcelona = () => {
-  fetch(`https://api.openweathermap.org/data/2.5/forecast?q=Barcelona,Spain&units=metric&APPID=${API_KEY}`)
-    .then((res) => {
+  fetch(API_BARCELONA_F)
+  .then((res) => {
       return res.json();
     })
     .then((data) => {
@@ -54,8 +69,9 @@ const fetchForecastBarcelona = () => {
     })
 };
 
+// Fetch for Paris weather
 const fetchParis = () => {
-  fetch(`https://api.openweathermap.org/data/2.5/weather?q=Paris&units=metric&APPID=${API_KEY}`)
+  fetch(API_PARIS_W)
     .then((res) => {
       return res.json();
     })
@@ -64,62 +80,70 @@ const fetchParis = () => {
     })
 };
 
+// Fetch for Paris forecast
 const fetchForecastParis = () => {
-  fetch(`https://api.openweathermap.org/data/2.5/forecast?q=Paris,France&units=metric&APPID=${API_KEY}`)
-    .then((res) => {
+  fetch(API_BARCELONA_F)
+  .then((res) => {
       return res.json();
     })
     .then((data) => {
       getForecastData(data)     
     })
 };
+// -------------------- Fetch section end ----------------------------
 
+
+// -------------------- Function for main weather -------------------- 
 const getWeatherData = (data) => {
-  console.log("This is data", data);
-  console.log("This is data.main.temp", data.main.temp);
-
   const decimal = (data.main.temp).toFixed(1) // 
 
   // a variable for main images depending on the weather comes from JSON
   const idForMainImage = data.weather[0].main
   // to change the bg-image depending on the data weather
-  mainContainer.style.backgroundImage = `url(./Designs/Design-1/mainImages/${idForMainImage}.png)`
+  weatherIconContainer.innerHTML = `
+  <img class="weather-icon-img" src="./Designs/Design-1/icons/${idForMainImage}.svg">
+  `
 
   weatherContainer.innerHTML = `
+      <p>Todays weather</p>
       <h1>${decimal}°C</h1>
       <h2>${data.name}</h2>
-      <h3>${data.weather[0].description}</h3>          
+      <h3>${data.weather[0].description}</h3>  
     `;
 
-    if (decimal >= 20) {
-      body.style.background = "orange"
-    }
-    else if (decimal < 20 && decimal > 10){
-      body.style.background = "blue"
-    }
-    else {
-      body.style.background = "white"
-    }
+  //------------------IF we want to change background depending on temp, not used atm---
+    /*      if (decimal >= 20) {
+       body.style.background = "#fff7ed"
+     }
+     else if (decimal < 20 && decimal > 10){
+       body.style.background = "#edfaff"
+     }
+     else {
+      body.style.background = "#f0f0f0"
+    } */
+  //------------------------- Background color temp end---------------------------------
 
-
-    const sunriseData = new Date(data.sys.sunrise * 1000) //Converts UNIX/EPOCH time to readable human time
-    const sunsetData = new Date(data.sys.sunset * 1000) //Converts UNIX/EPOCH time to readable human time
+    // This part takes the current sunrise / sunset times and converts it to HH:MM
+    const sunriseData = new Date(data.sys.sunrise * 1000) //Converts UNIX/EPOCH time to readable human time, makes is millisec
+    const sunsetData = new Date(data.sys.sunset * 1000) //Converts UNIX/EPOCH time to readable human time, makes is millisec
     const sunriseString = sunriseData.toLocaleTimeString('en-SE', {hour: '2-digit', minute:'2-digit'}) //toLocaleTimeString reduces data to only show hh:mm
     const sunsetString = sunsetData.toLocaleTimeString('en-SE', {hour: '2-digit', minute:'2-digit'}) //toLocaleTimeString reduces data to only show hh:mm
 
-    console.log(sunriseData, sunsetData)
-
+    //prints HH:MM for sunrise/sunset with icon
     sunriseContainer.innerHTML = `
-    <h3>Sunrise ${sunriseString}</h3>`
+    <img class="sun-icon" src="./Designs/Design-1/mainImages/up.png"> 
+    <h3>${sunriseString}</h3>`
     sunsetContainer.innerHTML = `
-    <h3>Sunset ${sunsetString}</h3>`
-
+    <img class="sun-icon" src="./Designs/Design-1/mainImages/down.png">
+    <h3>${sunsetString}</h3>`
 }
+// -------------------- Main weather function end ----------------------------------
 
+
+// -------------------- Function for forecast weather ------------------------------ 
   const getForecastData = (data) => {
   // display the next 5 days  with filtered info (once a day at 12:00) 
   const filteredForecast = data.list.filter(item => item.dt_txt.includes('12:00'))
-  console.log("THIS IS THE filter", filteredForecast)
 
   // forEach 5 days info for each days in the filtered array
   filteredForecast.forEach((object) => {
@@ -129,29 +153,44 @@ const getWeatherData = (data) => {
     // multiplied with 1000 to make it visibale both in safari and chrome //
     const date = new Date(object.dt * 1000); // full date with day month and year
     const days = date.toLocaleDateString('en-SE', { weekday: 'long' }); // display the day with the name only Tuesday,Wednesday,Monday etc.. 
-    console.log("the next day is", days) // the next day is wednesday etc. 
-
-
-    console.log("the next day is", object.weather[0].main)
 
     // 5 days name & temperature & description of the weather
     const idForIcons = object.weather[0].main
     forecastContainer.innerHTML += ` 
     <div class="forecast-days">
       <div>
-      <h4 class="day"> ${days} </h4>
+        <h4 class="day"> ${days} </h4>
       </div>
       <div class="iconandtemp">
-      <img id="iconID" class="icons" alt="" src="./Designs/Design-1/icons/${idForIcons}.png">
-      <h4 class="temp"> ${decimal}°C</h4>
+        <img id="iconID" class="icons" alt="" src="./Designs/Design-1/icons/${idForIcons}.svg">
+        <h4 class="temp"> ${decimal}°C</h4>
       </div>
     </div>
     `;
   })
 }
+// -------------------- Forecast weather function end ----------------------------
 
+
+// -------------------- Day / Night background change depending on time ---------- 
+const time = new Date()
+const timeHr = time.getHours() // Converts time to only hours
+
+if (timeHr > 7 && timeHr <= 19){
+  mainContainer.style.backgroundImage = `url(./Designs/Design-1/mainImages/day.png)` // Changes background image to day
+}
+else if (timeHr > 19){
+  mainContainer.style.backgroundImage = `url(./Designs/Design-1/mainImages/night.png)` // Changes background image to night
+}
+else{
+  mainContainer.style.backgroundImage = `url(./Designs/Design-1/mainImages/night.png)` // Changes background image to night
+}
+// -------------------- Day / Night background end ------------------------------- 
+
+
+// -------------------- City changer -------------------------------------------
 let cityClick = 0;
-
+// Function for changing city
 cityButton.addEventListener('click', () => {
   forecastContainer.innerHTML = ``;
   if (cityClick === 0){
@@ -169,39 +208,5 @@ cityButton.addEventListener('click', () => {
     fetchForecastStockholm()
     cityClick = 0;
   }
-
-
-  
-  /* console.log(API_CITY) */
 })
-
-//TODO LIST ------
-/* 
-fix the button for a city loop between barca-sthlm  --- tea ✅
-footer 
-add comments
-clean the console logs
-css cleaning for media queries or bg images?
-check html
-delete the branches 
-add picture for clear
-
-Fix hot/cold background for mobile/tablet.. or maybe just go 1 color
-Add 1 more city?          ---- team ✅
-More Main pictures  ----clear/clouds/drizzle/rain/snow/thunderstorm/ xx night/partly-cloudly(=not weather)xx (openweathermap.org/weather-conditions)
-the .GIF icon
-
-Icons for sunset & sunrise
-Animation on todays weather
-STYLING.
-Get the description to start with a capital letter?
-
-
-BONUS!
-Add more Data
-GeoLocation...... long shot!
-
-
-Show more citys in Desktop
-
-*/
+//-------------------- City changer end ----------------------------------------
