5101f9b810a67d484f154221f76068e895d975aa
Michael Dohmann Chang <michaelchang@MacBook-Pro.lan>
2022-02-14 15:08:10 +0100

    testing merge

--- script.js
+++ script.js
@@ -5,10 +5,7 @@ const middleSection = document.getElementById('middleSection')
 // const weatherQuip = document.createElement('h1');
 const bottomSection = document.getElementById('bottomSection')
 
-<<<<<<< HEAD
-=======
 // Testing Testing
->>>>>>> sunrise_sunset
 fetch('https://api.openweathermap.org/data/2.5/weather?q=Gothenburg,Sweden&units=metric&APPID=1d70a07080ab5151e3f54886ea0d8389')
   .then((res) => res.json())
   .then((data) => {
