6d5eacbc1737f5af4e07eb4ad576715ab8f8d2bd
Tiina <tiina_liukkonen@hotmail.com>
2022-02-16 12:00:26 +0200

    added sunrise and sunset times

--- Designs/code/script.js
+++ Designs/code/script.js
@@ -13,11 +13,18 @@ fetch('https://api.openweathermap.org/data/2.5/weather?q=Rovaniemi,Finland&units
     .then((json) => {
         console.log(json)
         const roundedTemp = Math.round(json.main.temp * 10) / 10
+        let sunriseTime = new Date(1644992567*1000)
+        let sunriseHours = sunriseTime.getHours()
+        let sunriseMinutes = sunriseTime.getMinutes()
+        let sunsetTime = new Date(1645022395*1000)
+        let sunsetHours = sunsetTime.getHours()
+        let sunsetMinutes = sunsetTime.getMinutes()
+
         currentWeather.innerHTML += `
         <h1 class="main-temp">${roundedTemp} <span class="celsius">&#8451;</span></h1>
         <h2 class="city-name">${json.name}</h2>
         <p class="weather-type">${json.weather[0].main}</p>
-        <div class="rise-set"></div>
+        <div class="rise-set">Sunrise ${sunriseHours}:${sunriseMinutes} Sunset ${sunsetHours}:${sunsetMinutes}</div>
          `
 
     })
