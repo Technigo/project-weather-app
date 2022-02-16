770588b2de055161c9811f817072636f28f22646
Nadia Lefebvre <95084122+nadialefebvre@users.noreply.github.com>
2022-02-16 11:09:54 +0100

    removed useless commented out lines to avoid confusion

--- script.js
+++ script.js
@@ -1,11 +1,10 @@
 // this is the API variable for the main weather 
-const API_WEATHER = 'https://api.openweathermap.org/data/2.5/weather?q=Stockholm,Sweden&units=metric&APPID=f1099e35a6194bceb628758a90cd792b'
-// this is the API variable for the 5 day forecast
+const API_WEATHER = 'https://api.openweathermap.org/data/2.5/weather?q=LasVegas,USA&units=metric&APPID=f1099e35a6194bceb628758a90cd792b'
+// this is the API variable for the 5 days forecast
 const API_FORECAST = 'https://api.openweathermap.org/data/2.5/forecast?q=Stockholm,Sweden&units=metric&APPID=292db829ecfe6c13a596b799caf9289f'
 
-// these 3 are the DOM selectors 
-const actualWeather = document.getElementById('actualWeather')
-const actualTemperature = document.getElementById('actualTemperature')
+// DOM selectors 
+const todaysWeather = document.getElementById('todaysWeather')
 const todaysAdvice = document.getElementById('todaysAdvice')
 
 // we fetch the main weather and with the help of json we display it
@@ -15,23 +14,22 @@ fetch(API_WEATHER)
     // we say what we want to be done with the response  
     .then((json) => {
         // weather and temp are injected in HTML, we also rounded the value to show no decimal
-        actualWeather.innerHTML = `${json.weather[0].description}`
-        // with one decimal: actualTemperature.innerHTML = `${Math.round(json.main.temp * 10) / 10}`        
-        actualTemperature.innerHTML = `${Math.round(json.main.temp)}°`
-
+        // with one decimal: actualTemperature.innerHTML = `${Math.round(json.main.temp * 10) / 10}`
+        todaysWeather.innerHTML += `
+        <p class="actual-temperature">${json.weather[0].description} | ${Math.round(json.main.temp)}°</p>
+        `
         const sunset = new Date(json.sys.sunset * 1000)
         const sunrise = new Date(json.sys.sunrise * 1000)
-
         todaysWeather.innerHTML += `
             <p class="sunrise" id="sunrise">sunrise ${sunrise.getHours() < 10
                 ? "0" + sunrise.getHours() : sunrise.getHours()}.${sunrise.getMinutes()}</p>
             <p class="sunset" id="sunset">sunset ${sunset.getHours() < 10
                 ? "0" + sunset.getHours() : sunset.getHours()}.${sunset.getMinutes()}</p>
-        `  
+        `
 
         // today's advice is injected in HTML
         // conditional is quite simply done: cloudy style if "cloud" is in description, sunny style if "clear", rainy for the rest.
-        if (json.weather[0].description.includes('clear')) {
+        if (json.weather[0].main.includes('Clear')) {
             document.body.style.backgroundColor = '#F7E9B9'
             document.body.style.color = '#2A5510'
             todaysAdvice.innerHTML = `
@@ -40,7 +38,7 @@ fetch(API_WEATHER)
                 <span class="city" id="city">Get your sunnies on. ${json.name} is looking rather great today.</span>
             </h1>
         `
-        } else if (json.weather[0].description.includes('cloud')) {
+        } else if (json.weather[0].main.includes('Cloud')) {
             document.body.style.backgroundColor = '#F4F7F8'
             document.body.style.color = '#F47775'
             todaysAdvice.innerHTML = `
@@ -61,8 +59,7 @@ fetch(API_WEATHER)
         }
     })
 
-    // added the forecast part from the forecast branch to check the styling for this section
-    fetch(API_FORECAST)
+fetch(API_FORECAST)
     .then((response) => response.json())
     .then((json) => {
         const filteredForecast = json.list.filter(item => item.dt_txt.includes('12:00'))
@@ -70,11 +67,9 @@ fetch(API_WEATHER)
             const options1 = { weekday: 'short' }
             // forecast is injected in HTML, we also rounded the value to show no decimal
             // with one decimal: ${Math.round(day.main.temp * 10) / 10}
-            // changed the h3 for p tag here as well so they are not bold
-
-    forecast.innerHTML +=
-        `<div class="day-container" id="firstDay">
+            forecast.innerHTML +=
+                `<div class="day-container" id="firstDay">
             <p class="forecastDay"><span>${new Intl.DateTimeFormat('en-GB', options1).format(day.dt * 1000).toLowerCase()}</span><span>${Math.round(day.main.temp)}°</span></p>
         </div>`
-})
+        })
     })
