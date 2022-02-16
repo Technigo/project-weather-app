380a910001a1e93c482466901ec0ff916b0b4b0a
savannah-hayes <79738821+savannah-hayes@users.noreply.github.com>
2022-02-16 11:40:08 +0100

    Display sunrise and sunset time

--- script.js
+++ script.js
@@ -5,13 +5,49 @@ const icon = document.getElementById("icon");
 const message = document.getElementById("message");
 const forecastWrapper = document.getElementById("forecastWrapper");
 
+
 const fetchData = () => {
   const API_URL = "https://api.openweathermap.org/data/2.5/weather?q=stockholm&units=metric&appid=7908c37d2eaed12abeb790e5b0154ee9"
-    fetch(API_URL)
-    .then((res) => res.json()) 
+  fetch(API_URL)
+    .then((res) => res.json())
     .then(data => {
-        weather.innerHTML = data.weather[0].description
-    }) 
- }
+      displayData(data)
+    })
+}
+
+const displayData = (data) => {
+  const temperature = Math.round(data.main.temp)
+  const description = data.weather[0].description
+  const sunrisedata = new Date(data.sys.sunrise * 1000)
+  const sunsetdata = new Date(data.sys.sunset * 1000)
+
+  let sunriseHours = sunrisedata.getHours();
+  let sunriseMinutes = sunrisedata.getMinutes()
+
+  let sunsetHours = sunsetdata.getHours();
+  let sunsetMinutes = sunsetdata.getMinutes()
+
+  if (sunriseHours < 10) {
+    sunriseHours = `0${sunriseHours}`
+  } 
+   
+  if (sunriseMinutes < 10) {
+    sunriseMinutes = `0${sunriseMinutes} `
+  }
+
+  if (sunsetHours < 10) {
+    sunsetHours = `0${sunsetHours}`
+  } 
+   
+  if (sunsetMinutes < 10) {
+    sunsetMinutes = `0${sunsetMinutes} `
+  }
+
+  weather.innerHTML = `${description} | ${temperature}`
+  sunrise.innerHTML = `Sunrise: ${sunriseHours}.${sunriseMinutes}`
+  sunset.innerHTML = `Sunset: ${sunsetHours}.${sunsetMinutes}`
+  console.log(data)
+}
+
  fetchData() 
  
