43ea2a52de44dc5a8aa006823a80b07b092956fc
Sherin Susan Thomas <sherinsusanthomas95@gmail.com>
2022-02-16 18:49:35 +0100

    updated js with icons

--- script.js
+++ script.js
@@ -12,15 +12,26 @@ const API_Weather_URL =
   "https://api.openweathermap.org/data/2.5/forecast?q=Stockholm,Sweden&units=metric&APPID=856500266ed2a8bc92cf454b0800d15c";
 const timeInHr = new Date().getHours();
 
-//>>>>>>> sherin
 fetch(API_URL) //this is when we send something to BE
   .then((res) => res.json()) //this is when we receive the data from BE
   .then((data) => {
     console.log("data", data);
     weatherContainer.innerHTML = ` <h1 class="today" id="today">Today</h1> 
     <h1 class="temperature" id="temperature">${data.main.temp}°C</h1>
-        <h2 class="city" id="city">${data.name} </h2>
-        <h2  class="weather-description" id="weatherDescription">${data.weather[0].description}</h2>`;
+        <h2 class="city" id="city">${data.name} </h2>`;
+
+    // weather icons
+    if (data.weather[0].main === "Clouds") {
+      weatherContainer.innerHTML += `  <h2  class="weather-description" id="weatherDescription"><img class="clouds-icon" src="./images/clouds.png">  ${data.weather[0].description}</h2>`;
+    } else if (data.weather[0].main === "Rain") {
+      weatherContainer.innerHTML += `<h2  class="weather-description" id="weatherDescription"><img class="rain-icon" src="./images/rain.png">  ${data.weather[0].description}</h2>`;
+    } else if (data.weather[0].main === "Clear") {
+      weatherContainer.innerHTML += `<h2  class="weather-description" id="weatherDescription"><img class="sun-icon" src="./images/sun.png">  ${data.weather[0].description}</h2>`;
+    } else if (data.weather[0].main === "Snow") {
+      weatherContainer.innerHTML += `<h2  class="weather-description" id="weatherDescription"><img class="snow-icon" src="./images/clouds.png">  ${data.weather[0].description}</h2>`;
+    } else {
+      weatherContainer.innerHTML += `<h2  class="weather-description" id="weatherDescription"><img class="other-icon" src="./images/clouds.png">  ${data.weather[0].description}</h2>`;
+    }
 
     /* sunrise & sunset */
     const sunriseSec = data.sys.sunrise;
@@ -28,11 +39,11 @@ fetch(API_URL) //this is when we send something to BE
     const sunrise = convertUTCToSunTime(sunriseSec);
     const sunset = convertUTCToSunTime(sunsetSec);
     sunContainer.innerHTML = `
-     
-    <h3 class="sunrise" id="sunRise" > <img class="sun-icon" src="./images/sunrise.png">  ${sunrise} </h3> 
    
-    <h3 class="sunset" id="sunSet">  <img class="sun-icon" src="./images/sunset.png">${sunset}</h3>
- `;
+    <h3 class="sunrise" id="sunRise" >   ${sunrise} </h3> 
+    <img class="sunrise-icon" src="./images/sunrise.png">
+    <h3 class="sunset" id="sunSet">  ${sunset}</h3>
+    <img class="sunset-icon" src="./images/sunset.png">`;
   });
 
 function convertUTCToSunTime(UTCsec) {
@@ -61,10 +72,17 @@ function convertUTCToSunTime(UTCsec) {
     });
   });*/
 // setting bg Image based on day/night
-if (timeInHr >= 6 && timeInHr <= 18) {
+if (timeInHr >= 6 && timeInHr <= 17) {
   mainContainer.style.backgroundImage = `url(./images/day.jpg)`;
   mainContainer.style.backgroundSize = "cover";
-} else if (timeInHr < 6 && timeInHr > 18) {
+}
+if (timeInHr >= 18) {
   mainContainer.style.backgroundImage = `url(./images/night.jpg)`;
   mainContainer.style.backgroundSize = "cover";
+  mainContainer.style.color = "white";
+}
+if (timeInHr <= 5) {
+  mainContainer.style.backgroundImage = `url(./images/night.jpg)`;
+  mainContainer.style.backgroundSize = "cover";
+  mainContainer.style.color = "white";
 }
