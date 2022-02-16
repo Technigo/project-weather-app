20b104ab5cd7bea0c00e9d2a4643dbe88964264a
eyahya-khan <eyahyakhan@gmail.com>
2022-02-16 16:17:16 +0100

    added forecast

--- script.js
+++ script.js
@@ -23,53 +23,6 @@ fetch(url)
 })
 
 
-// Object with all emoji
-const emojiObject = {
-    Clouds: "./assets/cloud.png",
-  
-    Wind: "./assets/wind.png",
-  
-    Clear: "./assets/sun.png",
-  
-    Rain: "./assets/rain.png",
-  
-    Snow: "./assets/snow.png",
-  };
-
-//weather forecast next 5 days
-const weatherForecast = document.getElementById('weatherForecast')
-
-const urlForecast = 'https://api.openweathermap.org/data/2.5/forecast?q=Stockholm,Sweden&units=metric&APPID=5877e9b20cbe99b1b6637d0e4bb81238'
-
-    const fetchForecast = () => {
-        fetch(urlForecast)
-          .then((response) => response.json())
-          .then((data) => {
-            //console.log("New Forecast JSON:", data);
-      
-            let filteredFiveDays = data.list.filter((item) =>
-              item.dt_txt.includes("12:00")
-              );
-             // console.log(filteredFiveDays)
-              
-            filteredFiveDays.forEach((item) => {
-                weatherForecast.innerHTML += `
-                <div class="weatherForecastDays">
-                <div class="weatherForecastRow">
-                <p>${new Date(item.dt_txt).toLocaleDateString("en-US", {weekday: "short",})} </p>
-                <p><img src = "${emojiObject[item.weather[0].main]}"></p>
-                <p>${item.main.temp_max.toFixed(1)}°C/${item.main.temp_min.toFixed(1)}°C</p>
-                </div>
-              </div>`;
-            });
-          })
-          .catch((error) => console.error(error));
-      };
-      
-      fetchForecast();
-
-
-
 
 
       //weather forecast for 5 days  
