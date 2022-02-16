625f39a294200275c906c9f24ab3766296d0cf33
Josefin Eliasson <91781497+Josse79@users.noreply.github.com>
2022-02-16 14:37:07 +0100

    Added sunrise_sunset section

--- script.js
+++ script.js
@@ -13,6 +13,7 @@ fetch('https://api.openweathermap.org/data/2.5/weather?q=Gothenburg,Sweden&units
     console.log(data.sys.sunrise);
     console.log(data.sys.sunset);
     console.log(data.weather[0].main);
+    console.log(data.dt);
     const weatherDescription = data.weather[0].description
     if (data.weather[0].main.includes("Clear")) {
       body.classList.add("sunny")
@@ -33,6 +34,30 @@ fetch('https://api.openweathermap.org/data/2.5/weather?q=Gothenburg,Sweden&units
     <p>${todayTemp}°</p>
     <p>${weatherDescription}</p>
     `
+    //SUNRISE_SUNSET section
+    //Converting sys sunrise in API from seconds to milliseconds  - then to local SE time HH:MM
+    const sunriseTime = new Date(data.sys.sunrise * 1000).toLocaleTimeString('sv-SE', {
+      hour: '2-digit', 
+      minute: '2-digit'
+    })
+    
+    //Tried to apply sunriseTime to different time zones - returned the whole date string
+    // const sunriseTime = new Date((data.sys.sunrise + new Date().getTimezoneOffset()
+    //  * 60 + data.timezone) * 1000).toTimeString('sv-SE', {
+    //   hour: '2-digit', 
+    //   minute: '2-digit'
+    // })
+
+    //Converting sys sunset in API from seconds to milliseconds  - then to local SE time HH:MM
+    const sunsetTime = new Date(data.sys.sunset * 1000).toLocaleTimeString('sv-SE', {
+      hour: '2-digit', 
+      minute: '2-digit'
+    })
+    
+    //Adding sunriseTime + SunsetTime into correct HTML ID of topsection
+     topSection.innerHTML =`
+     <p>sunrise ${sunriseTime}</p>
+     <p>sunset ${sunsetTime}</p>`
   });
 
 // weather[0].main: Clear = Sunny, Clouds = Cloud, Rain = Rain
