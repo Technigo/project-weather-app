7a91c27abfb14b08df833e540053fcd3ccac4d57
Kristiina Kolu <kristiinakolu@Maxs-MacBook-Air.local>
2022-02-16 12:02:55 +0200

    added stuff to js file

--- Designs/code/script.js
+++ Designs/code/script.js
@@ -1,10 +1,14 @@
 // Decide on class and id names for elements
 // DOM selectors
 const currentWeather = document.getElementById('currentWeather')
-const upcomingWeather = document.getElementById('upcomingWeather')
+//const upcomingWeather = document.getElementById('upcomingWeather')
 
 
 //const APP_ID = '94506b4af0e0a236471b8ee0da3c2281'
+
+//local variables:
+let today = new Date() //this stores the current date and time 
+console.log(today)
 
 fetch('https://api.openweathermap.org/data/2.5/weather?q=Rovaniemi,Finland&units=metric&APPID=94506b4af0e0a236471b8ee0da3c2281')
     .then((response) => {
@@ -22,4 +26,25 @@ fetch('https://api.openweathermap.org/data/2.5/weather?q=Rovaniemi,Finland&units
 
     })
 
-   
+ //we decided to take the max temperature of noon and then min temperature of the midnight,
+ //so then we have min and max for the whole day   
+fetch('https://api.openweathermap.org/data/2.5/forecast?q=Stockholm,Sweden&units=metric&APPID=94506b4af0e0a236471b8ee0da3c2281')   
+    .then((response) => {
+    return response.json()
+    })
+    .then((forecastdata) => {
+    console.log(forecastdata)
+    //const filteredForecast = forecastdata.list.filter(item => item.dt_txt.includes('12:00', '00:00')) 
+    //console.log(filteredForecast)
+    }) 
+//some notes here:
+    //const day1 = array[0]
+    //const day2 = array[1]
+
+
+    //list.main.temp_max
+    //list.main.temp_min
+
+
+
+
