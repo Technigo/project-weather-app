0d05bf1fbbc9804f64651aed20c9f7b298d72466
Katarina Elgerus <katarina.elgerus@liv.se>
2022-02-16 16:24:37 +0100

    header api fixed

--- code/script.js
+++ code/script.js
@@ -4,7 +4,7 @@ const dailyWeather = document.getElementById('dailyWeather')
 const greeting = document.getElementById('greeting')
 const forecast = document.getElementById('forecast')
 
- const API_URLD = ('http://api.openweathermap.org/data/2.5/weather?q=Berlin,de&APPID=d423e9bdbd74d4fcdd3804322b8767eb')
+ const API_URLD = ('https://api.openweathermap.org/data/2.5/weather?q=Berlin,de&appid=d423e9bdbd74d4fcdd3804322b8767eb')
  const API_URLF = ('https://api.openweathermap.org/data/2.5/forecast?q=Berlin,de&APPID=d423e9bdbd74d4fcdd3804322b8767eb')
     // First API - gives us Daily weather
     fetch(API_URLD)
@@ -13,10 +13,30 @@ const forecast = document.getElementById('forecast')
     .then((json) => {
         // console.log('data', data)
         console.log(json.main.temp)
-        console.log(json.weather[0].main)
-        console.log(json.sys.sunrise)
-        console.log(json.sys.sunset)
+
+        const celcius= Math.floor((json.main.temp - 273.15)) // convert kelvin to celcius
+      
+     
+
+        dailyWeather.innerHTML= `
+        <p>${json.weather[0].main} | ${celcius} </p> 
+        <p>Sunrise ${json.sys.sunrise}</p>
+        <p>Sunset ${json.sys.sunset}</p>`  
+        
+
+        // const currentSunrise = new Date(
+		// 	(json.sys.sunrise * 1000 + json.sys.timezone + new Date().getTimezoneOffset())
+		// ).toTimeString()
+        // console.log(currentSunrise)
+
+		//dailyWeather.innerHTML = currentSunrise
+
+
+
+
     })
+
+
 
     // Second API - gives us 5 days forcast
     fetch(API_URLF)
