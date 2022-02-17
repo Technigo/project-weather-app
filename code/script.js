874834dad24f9f3d5d3098c338b7cf78eb0612b2
Rawisara Saman <rawiisara@gmail.com>
2022-02-17 18:26:23 +0700

    added multi-city functions and display design that matches weather condition

--- code/script.js
+++ code/script.js
@@ -1,37 +1,9 @@
 
-
-// this will be removed if to-do idea is implemented
 const stockholmUrl = 'https://api.openweathermap.org/data/2.5/onecall?lat=59.33&lon=18.06&units=metric&exclude=minutely,hourly,alerts&appid=99271fdaf78d63e5bf35004e02e4e29d'
-
-// TO-DO IDEA: select city to be displayed from a drop down menu
-// Create a select city options menu
-// Give each option (city) a value equal to its coordinates in the exact format the api url needs
-    // ex. for Stockholm value = "lat=59.33&lon=18.06"
-    // ex. for Seattle value = "lat=47.60&lon=-122.33"
-// Declare a function that is called with a select menu event listener (ie with "change")
-    // use the selected city's value to populate the url with its coordinates
-    // ex. let url =  `https://api.openweathermap.org/data/2.5/onecall?${value}&units=metric&exclude=minutely,hourly,alerts&appid=99271fdaf78d63e5bf35004e02e4e29d`
-    // call a function that includes fetch which takes url as an argument
-        // requires moving everything fetch is doing into a function
-        // everything else continues per usual after that
-
-// fetch current and daily forecast for Stockholm, link to documentation: https://openweathermap.org/api/one-call-api
-fetch(stockholmUrl)
-.then((result) => {
-    return result.json(); // returns a promise that something will happen
-})
-.then((data) => {
-
-    let currentWeather = formatCurrentWeather(data); // calls function with the data the promise delivers
-    updateCurrentWeatherDisplay(currentWeather); // updates display when data is ready
-
-    let forecast = formatForecast(data); // calls function with the data the promise delivers
-    updateForecastDisplay(forecast); // updates display when data is ready
-
-    })
-    .catch((error) => {
-        console.log(error); // shows if something went wrong (ex. API key, json formatted incorrectly, etc)
-    });
+const bangkokUrl = 'https://api.openweathermap.org/data/2.5/onecall?lat=13.73&lon=100.31&units=metric&exclude=minutely,hourly,alerts&appid=99271fdaf78d63e5bf35004e02e4e29d'
+const seattleUrl = 'https://api.openweathermap.org/data/2.5/onecall?lat=47.60&lon=-122.33&units=metric&exclude=minutely,hourly,alerts&appid=99271fdaf78d63e5bf35004e02e4e29d'
+const torontoUrl = 'https://api.openweathermap.org/data/2.5/onecall?lat=43.65&lon=-79.34&units=metric&exclude=minutely,hourly,alerts&appid=99271fdaf78d63e5bf35004e02e4e29d'
+const cityOptions= document.getElementById('city')
 
 // populate currentWeather object with data from the current weather API
 const formatCurrentWeather = (data) => {
@@ -95,11 +67,34 @@ const formatForecast = (data) => {
     return forecast;
 };
 
+// fetch(stockholmUrl)
+//     .then((result) => {
+//         return result.json(); // returns a promise that something will happen
+//     })
+//     .then((data) => {
+
+//     let currentWeather = formatCurrentWeather(data); // calls function with the data the promise delivers
+//     updateCurrentWeatherDisplay(currentWeather); // updates display when data is ready
+
+//     let forecast = formatForecast(data); // calls function with the data the promise delivers
+//     updateForecastDisplay(forecast); // updates display when data is ready
+
+//     })
+//     .catch((error) => {
+//         console.log(error); // shows if something went wrong (ex. API key, json formatted incorrectly, etc)
+//     });
 
 /* ------------------------ START FROM HERE ------------------------*/
 const currentWeatherCondition = document.getElementById('currentWeatherCondition')
 const currentWeatherSunrise = document.getElementById('currentWeatherSunrise')
 const currentWeatherSunset = document.getElementById('currentWeatherSunset')
+const iconCloud = document.getElementById('iconCloud')
+const iconUmbrella = document.getElementById('iconUmbrella')
+const iconGlasses = document.getElementById('iconGlasses')
+const styleChange = document.getElementsByTagName('body')
+const fontColor = document.querySelectorAll('p')
+const currentWeatherHeaderWrapper = document.getElementById('currentWeatherHeaderWrapper')
+const headerMessage = document.getElementById('currentWeatherHeader')
 
 // this function is called when the data is available 
 const updateCurrentWeatherDisplay = (currentWeather) => {
@@ -108,7 +103,119 @@ const updateCurrentWeatherDisplay = (currentWeather) => {
     currentWeatherCondition.innerHTML = `${currentWeather.condition} | ${currentWeather.temp}`
     currentWeatherSunrise.innerHTML = `sunrise ${currentWeather.sunrise}`
     currentWeatherSunset.innerHTML = `sunset ${currentWeather.sunset}`
+
+    if (currentWeather.condition === 'Clouds') {
+        iconCloud.style.display = 'block'
+        iconUmbrella.style.display = 'none'
+        document.body.style.backgroundColor = "#F4F7F8"
+        currentWeatherCondition.style.color = "#F47775"
+        currentWeatherSunrise.style.color = "#F47775"
+        currentWeatherSunset.style.color = "#F47775"
+        currentWeatherHeader.style.color =  "#F47775"
+        headerMessage.innerText = "It's cloudy"
+    } else if (currentWeather.condition === 'Rain') {
+        iconCloud.style.display = 'none'
+        iconUmbrella.style.display = 'block'
+        document.body.style.backgroundColor = "#A3DEF7"
+        currentWeatherCondition.style.color = "#164A68"
+        currentWeatherSunrise.style.color = "#164A68"
+        currentWeatherSunset.style.color = "#164A68"
+        currentWeatherHeader.style.color =  "#164A68"
+        headerMessage.innerText = "Don't forget your umbrella :)"
+        
+    } else if (currentWeather.condition === 'Clear'){
+        iconGlasses.style.display = 'block'
+        iconCloud.style.display = 'none'
+        iconUmbrella.style.display = 'none'
+        headerMessage.innerText = "Time to put on a nice pair of sunglasses!"
+    } else {
+        console.log("I have no icons left! Hope it doesn't snow!!!")
+    }
 }
+
+
+
+// Cities 
+
+const stockholm = () => {
+    fetch(stockholmUrl)
+        .then((result) => {
+            return result.json(); // returns a promise that something will happen
+        })
+        .then((data) => {
+    
+        let currentWeather = formatCurrentWeather(data); // calls function with the data the promise delivers
+        updateCurrentWeatherDisplay(currentWeather); // updates display when data is ready
+    
+        let forecast = formatForecast(data); // calls function with the data the promise delivers
+        updateForecastDisplay(forecast); // updates display when data is ready
+    
+        })
+        .catch((error) => {
+            console.log(error); // shows if something went wrong (ex. API key, json formatted incorrectly, etc)
+        });
+    }
+
+const bangkok = () => {
+    fetch(bangkokUrl)
+    .then((result) => {
+        return result.json()
+    })
+  .then((data) => {
+
+    let currentWeather = formatCurrentWeather(data); // calls function with the data the promise delivers
+    updateCurrentWeatherDisplay(currentWeather); // updates display when data is ready
+
+    let forecast = formatForecast(data); // calls function with the data the promise delivers
+    updateForecastDisplay(forecast); // updates display when data is ready
+
+    })
+}
+
+const seattle= () => {
+    fetch(seattleUrl)
+    .then((result) => {
+        return result.json()
+    })
+  .then((data) => {
+
+    let currentWeather = formatCurrentWeather(data); // calls function with the data the promise delivers
+    updateCurrentWeatherDisplay(currentWeather); // updates display when data is ready
+
+    let forecast = formatForecast(data); // calls function with the data the promise delivers
+    updateForecastDisplay(forecast); // updates display when data is ready
+
+    })
+}
+
+
+const toronto= () => {
+    fetch(torontoUrl)
+    .then((result) => {
+        return result.json()
+    })
+  .then((data) => {
+
+    let currentWeather = formatCurrentWeather(data); // calls function with the data the promise delivers
+    updateCurrentWeatherDisplay(currentWeather); // updates display when data is ready
+
+    let forecast = formatForecast(data); // calls function with the data the promise delivers
+    updateForecastDisplay(forecast); // updates display when data is ready
+
+    })
+}
+
+cityOptions.addEventListener('change', () => { 
+    if(cityOptions.value == "bangkok") {
+        bangkok()
+    } else if (cityOptions.value == "seattle") {
+        seattle()
+    } else if (cityOptions.value == "toronto"){
+       toronto()
+    } else {
+        stockholm()
+    }
+} ) 
 
 // this function is called when the data is available 
 const updateForecastDisplay = (forecast) => {
