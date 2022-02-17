df6c1b52e11eedd1aab94af92a18b84c9fd9762b
Kristiina Kolu <kristiinakolu@Maxs-MacBook-Air.local>
2022-02-17 18:54:51 +0200

    styling the page and cleaning code

--- Designs/code/script.js
+++ Designs/code/script.js
@@ -2,8 +2,8 @@
 // DOM selectors
 
 const currentWeather = document.getElementById('currentWeather')
-//const upcomingWeather = document.getElementById('upcomingWeather')
-
+const currentWeatherWrapper = document.getElementById('current-weather-wrapper')
+const upcomingWeather = document.getElementById('upcomingWeather')
 const searchForm = document.getElementById("search-form")
 const cityInput = document.getElementById("search-input")
 
@@ -14,6 +14,8 @@ const cityInput = document.getElementById("search-input")
 let today = new Date().toLocaleDateString('en', {weekday: 'short'})
 //console.log('today',today) 
 let city = 'Helsinki'
+let mainIcon = ""
+
 
 const fetchWeather = (city) =>{
 let weatherURL = `https://api.openweathermap.org/data/2.5/weather?q=${city}&units=metric&APPID=94506b4af0e0a236471b8ee0da3c2281`
@@ -33,27 +35,79 @@ let forecastURL = `https://api.openweathermap.org/data/2.5/forecast?q=${city}&un
             currentWeather.innerHTML = ``
             upcomingWeather.innerHTML = 'Ups, no city with that name, try again!'
         }
-        else{
+        else {
         console.log(json)
-        const roundedTemp = Math.round(json.main.temp * 10) / 10
-        let sunriseTime = new Date(1644992567*1000)
-        let sunriseHours = sunriseTime.getHours()
-        let sunriseMinutes =  sunriseTime.getMinutes()
-        let sunsetTime = new Date(1645022395*1000)
-        let sunsetHours = sunsetTime.getHours()
-        let sunsetMinutes =  sunsetTime.getMinutes()
+        const roundedTemp = 10
+        //Math.round(json.main.temp * 10) / 10
+        // This is showing the local time for sunrise transformed into a 2-digit form for hours and minutes.
+        let sunriseTime = new Date((json.sys.sunrise + json.timezone + new Date().getTimezoneOffset() * 60) * 1000).toLocaleTimeString([], {
+          hour: "2-digit",
+          minute: "2-digit",
+        });
     
+        // This is showing the local time for sunrise transformed into a 2-digit form for hours and minutes.
+        let sunsetTime = new Date(
+          (json.sys.sunset + json.timezone + new Date().getTimezoneOffset() * 60) * 1000).toLocaleTimeString([], {
+          hour: "2-digit",
+          minute: "2-digit",
+        });
 
-        currentWeather.innerHTML += `
-        <h1 class="main-temp">${roundedTemp} <span class="celsius">&#8451;</span></h1>
-        <h2 class="city-name">${json.name}</h2>
-        <p class="weather-type">${json.weather[0].main}</p>
-        <div class="rise-set">Sunrise ${sunriseHours}:${sunriseMinutes} Sunset ${sunsetHours}:${sunsetMinutes}</div>
-         `
+        // This variable stores the current weather
+        const mainWeather = json.weather[0].main;
 
+        // This function shows the weather icons that are changing according to the current weather
+        const showMainWeatherIcon = () => {
+          if (mainWeather === `Snow` ) {
+            mainIcon = `<img src="./assets/snow.png"/>`
+          } else if (mainWeather === `Rain` || mainWeather === `Drizzle`) {
+            mainIcon = `<img src="./assets/rain.png" />`
+          } else if (mainWeather === `Thunderstorm`) {
+            mainIcon = `<img src="./assets/thunderstorm.png" />`
+          } else if (mainWeather === `Clear`) {
+            mainIcon = `<img src="./assets/clear.png" />`
+          } else if (mainWeather === `Clouds`) {
+            mainIcon = `<img src="./assets/cloudy.png" />`
+          } else if (mainWeather === `Squall`) {
+            mainIcon = `<img src="./assets/squall.png" />`
+          } else if (mainWeather === `Tornado`) {
+            mainIcon = `<img src="./assets/tornado.png" />`
+          } else if (mainWeather === `Fog` || mainWeather === `Mist` || mainWeather === `Haze`) {
+            mainIcon = `<img src="./assets/fog.png" />`
+          } else if (mainWeather === `Dust` || mainWeather === `Sand` || mainWeather === `Smoke` || mainWeather === `Ash`) {
+            mainIcon = `<img src="./assets/dust.png" />`
+          }
+        };
+        showMainWeatherIcon();
+
+        const changeTempBackground = () => {
+            if (roundedTemp < 5) {
+                currentWeatherWrapper.style.background =
+                'linear-gradient(50deg, #663399 0%, #b9bfff 50%, #22277A 100%)'
+            } else if (roundedTemp > 5 && roundedTemp < 19) {
+                currentWeatherWrapper.style.background =
+                'linear-gradient(50deg, #1c9bf6e8 0%, #d8d8d8 60%, #1c9cf6e8 100%)'
+            } else {
+                currentWeatherWrapper.style.background =
+                'linear-gradient(50deg, #F6412D 0%, #FFF682 60%, #FF5607 100%)'
+            } 
         }
-    })
-//This is fetching the 5-day forecast
+        changeTempBackground()
+
+        // This is showing the current weather for the location.
+        currentWeather.innerHTML += `
+            <div class="main-icon-container">
+              <div class = "main-icon"> ${mainIcon}</div>
+            </div>
+            <h1 class="main-temp">${roundedTemp} <span class="celsius">&#8451;</span></h1>
+            <h2 class="city-name">${json.name}</h2>
+            <p class="weather-type">${mainWeather}</p>
+            <div class="rise-set">
+              <p class="sunrise">Sunrise ${sunriseTime}</p>
+              <p class="sunset">Sunset ${sunsetTime}</p>
+            </div>
+            `;
+            //DOR and KRI part here (inside of else)
+            //This is fetching the 5-day forecast
     fetch(forecastURL)   
     .then((response) => {
     return response.json()
@@ -89,21 +143,32 @@ let forecastURL = `https://api.openweathermap.org/data/2.5/forecast?q=${city}&un
         //here comes if statement 
         
         if (weatherType === 'Snow'){
-            typeImg = 'snow.png'
-        }
-        else if (weatherType === 'Clouds'){
-            typeImg = 'cloudy.png'
-        }
-        else if (weatherType === 'Clear'){
-            typeImg = 'sunny.png'
+            typeImg = 'snow2.png'
         }
         else if (weatherType === 'Rain'){
-            typeImg = 'rain.png'
+            typeImg = 'rain2.png'
         }
-        else {
-            typeImg = 'partly.png'
+        else if (weatherType === 'Thunderstorm'){
+            typeImg = 'thunderstorm2.png'
         }
-     
+        else if (weatherType === 'Clear'){
+          typeImg = 'clear2.png'
+        }
+        else if (weatherType === 'Clouds'){
+          typeImg = 'cloudy2.png'
+        }
+        else if (weatherType === 'Squall'){
+          typeImg = 'squall2.png'
+        }
+        else if (weatherType === 'Tornado'){
+          typeImg = 'tornado2.png'
+        }
+        else if (weatherType === `Fog` || weatherType === `Mist` || weatherType === `Haze`){
+          typeImg = 'fog2.png'
+        }
+        else if (mainWeather === `Dust` || mainWeather === `Sand` || mainWeather === `Smoke` || mainWeather === `Ash`){
+          typeImg = 'dust2.png'
+        }
 
         //to make temperatures rounded to one decimal
         let roundedWeekMaxTemp = Math.round(filteredForecastNoon[day].main.temp_max)
@@ -117,7 +182,14 @@ let forecastURL = `https://api.openweathermap.org/data/2.5/forecast?q=${city}&un
     }
 
     })
+            
+        }//else ends here
+
+      });//this is ending then.json
 }
+
+
+
 //invokes the fetchWeather function when page loaded
 fetchWeather(city)
 
@@ -125,6 +197,7 @@ fetchWeather(city)
 /*when you search a city the evenlistener activates with submit and it will 
 refresh upcomingWeather.innerHTML and currentWeather.innerHTML to empty first and then 
 it takes the input value as a city argument to the fetchWeather function*/
+
 searchForm.addEventListener("submit", (e) => {
     e.preventDefault();
     upcomingWeather.innerHTML = ``
@@ -132,102 +205,9 @@ searchForm.addEventListener("submit", (e) => {
     city = cityInput.value;
     console.log('city:', city)
     fetchWeather(city)
-
-    
   })
   
+
  
 
-const currentWeather = document.getElementById("currentWeather");
-const upcomingWeather = document.getElementById("upcomingWeather");
-const currentWeatherWrapper = document.getElementById('current-weather-wrapper')
 
-//const APP_ID = '94506b4af0e0a236471b8ee0da3c2281'
-
-// Global variable
-let mainIcon = "";
-
-fetch(
-  "https://api.openweathermap.org/data/2.5/weather?q=Rovaniemi,Finland&units=metric&APPID=94506b4af0e0a236471b8ee0da3c2281"
-)
-  .then((response) => {
-    return response.json();
-  })
-
-  .then((json) => {
-    console.log(json);
-    // This is showing the current temperature rounded to an integer.
-    const roundedTemp = 10
-    //Math.round(json.main.temp * 10) / 10;
-    // This is showing local time for sunrise transformed into 2-digit form for hours and minutes.
-    let sunriseTime = new Date(
-      (json.sys.sunrise + json.timezone + new Date().getTimezoneOffset() * 60) *
-        1000
-    ).toLocaleTimeString([], {
-      hour: "2-digit",
-      minute: "2-digit",
-    });
-
-    // This is showing local time for sunrise transformed into 2-digit form for hours and minutes.
-    let sunsetTime = new Date(
-      (json.sys.sunset + json.timezone + new Date().getTimezoneOffset() * 60) *
-        1000
-    ).toLocaleTimeString([], {
-      hour: "2-digit",
-      minute: "2-digit",
-    });
-
-    // This is the weather condition icons that are showing depending on the weather
-    const mainWeather = json.weather[0].main;
-
-    const showMainWeatherIcon = () => {
-      if (mainWeather === `Snow` ) {
-        mainIcon = `<img src="./assets/snow.png"/>`
-      } else if (mainWeather === `Rain` || mainWeather === `Drizzle`) {
-        mainIcon = `<img src="./assets/rain.png" />`
-      } else if (mainWeather === `Thunderstorm`) {
-        mainIcon = `<img src="./assets/thunderstorm.png" />`
-      } else if (mainWeather === `Clear`) {
-        mainIcon = `<img src="./assets/clear.png" />`
-      } else if (mainWeather === `Clouds`) {
-        mainIcon = `<img src="./assets/cloudy.png" />`
-      } else if (mainWeather === `Squall`) {
-        mainIcon = `<img src="./assets/squall.png" />`
-      } else if (mainWeather === `Tornado`) {
-        mainIcon = `<img src="./assets/tornado.png" />`
-      } else if (mainWeather === `Fog` || mainWeather === `Mist` || mainWeather === `Haze`) {
-        mainIcon = `<img src="./assets/fog.png" />`
-      } else if (mainWeather === `Dust` || mainWeather === `Sand` || mainWeather === `Smoke` || mainWeather === `Ash`) {
-        mainIcon = `<img src="./assets/dust.png" />`
-      }
-
-    };
-    showMainWeatherIcon();
-    const changeTempBackground = () => {
-        if (roundedTemp < 5) {
-            currentWeatherWrapper.style.background =
-            'linear-gradient(50deg, #663399 0%, #b9bfff 50%, #22277A 100%)'
-        } else if (roundedTemp > 5 && roundedTemp < 19) {
-            currentWeatherWrapper.style.background =
-            'linear-gradient(50deg, #1c9bf6e8 0%, #d8d8d8 60%, #1c9cf6e8 100%)'
-        } else {
-            currentWeatherWrapper.style.background =
-            'linear-gradient(50deg, #F6412D 0%, #FFF682 60%, #FF5607 100%)'
-        } 
-    }
-    changeTempBackground()
-
-    // This is showing the current weather for the location.
-    currentWeather.innerHTML += `
-        <div class="main-icon-container">
-          <div class = "main-icon"> ${mainIcon}</div>
-        </div>
-        <h1 class="main-temp">${roundedTemp} <span class="celsius">&#8451;</span></h1>
-        <h2 class="city-name">${json.name}</h2>
-        <p class="weather-type">${mainWeather}</p>
-        <div class="rise-set">
-          <p class="sunrise">Sunrise ${sunriseTime}</p>
-          <p class="sunset">Sunset ${sunsetTime}</p>
-        </div>
-         `;
-  });
