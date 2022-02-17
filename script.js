007fa9cb3075334094860aa10461604a177198ad
Sherin Susan Thomas <sherinsusanthomas95@gmail.com>
2022-02-17 11:51:04 +0100

    updated js

--- script.js
+++ script.js
@@ -15,46 +15,44 @@ const timeInHr = new Date().getHours();
 fetch(API_URL) //this is when we send something to BE
   .then((res) => res.json()) //this is when we receive the data from BE
   .then((data) => {
-    /* sunrise & sunset */
-    const sunriseSec = data.sys.sunrise;
-    const sunsetSec = data.sys.sunset;
-    const sunrise = convertUTCToSunTime(sunriseSec, data.timezone);
-    const sunset = convertUTCToSunTime(sunsetSec, data.timezone);
-    sunContainer.innerHTML = `
-     <h3 class="sunrise" id="sunRise" >${sunrise} </h3>`;
     console.log("data", data);
-    const weatherIcon = data.weather[0].icon;
     weatherContainer.innerHTML = ` <h1 class="today" id="today">Today</h1> 
     <h1 class="temperature" id="temperature">${data.main.temp}°C</h1>
         <h2 class="city" id="city">${data.name} </h2>
-        <h2  class="weather-description" id="weatherDescription"> ${data.weather[0].icon} ${data.weather[0].description}  </h2>`;
-    // weather icons
-    if (data.weather[0].main === "Clouds") {
-      weatherContainer.innerHTML += `  <h2  class="weather-description" id="weatherDescription"><img class="clouds-icon" src="./images/clouds.png">  ${data.weather[0].description}</h2>`;
-    } else if (data.weather[0].main === "Rain") {
-      weatherContainer.innerHTML += `<h2  class="weather-description" id="weatherDescription"><img class="rain-icon" src="./images/rain.png">  ${data.weather[0].description}</h2>`;
-    } else if (data.weather[0].main === "Clear") {
-      weatherContainer.innerHTML += `<h2  class="weather-description" id="weatherDescription"><img class="sun-icon" src="./images/sun.png">  ${data.weather[0].description}</h2>`;
-    } else if (data.weather[0].main === "Snow") {
-      weatherContainer.innerHTML += `<h2  class="weather-description" id="weatherDescription"><img class="snow-icon" src="./images/clouds.png">  ${data.weather[0].description}</h2>`;
-    } else {
-      weatherContainer.innerHTML += `<h2  class="weather-description" id="weatherDescription"><img class="other-icon" src="./images/clouds.png">  ${data.weather[0].description}</h2>`;
-    }
+        <h2 class="city" id="city">${data.weather[0].description} </h2>`;
 
-    // setting bg Image based on day/night
-    if (timeInHr >= 6 && timeInHr <= 17) {
-      mainContainer.style.backgroundImage = `url(./images/day.jpg)`;
-      mainContainer.style.backgroundSize = "cover";
-    } else if (timeInHr >= 18) {
-      mainContainer.style.backgroundImage = `url(./images/night.jpg)`;
-      mainContainer.style.backgroundSize = "cover";
-      mainContainer.style.color = "white";
-    } else {
-      mainContainer.style.backgroundImage = `url(./images/night.jpg)`;
-      mainContainer.style.backgroundSize = "cover";
-      mainContainer.style.color = "white";
-    }
+    /* sunrise & sunset */
+    const sunriseSec = data.sys.sunrise;
+    const sunsetSec = data.sys.sunset;
+    const sunrise = convertUTCToSunTime(sunriseSec);
+    const sunset = convertUTCToSunTime(sunsetSec);
+    sunContainer.innerHTML = `
+   
+    <h3 class="sunrise" id="sunRise" >   ${sunrise} </h3> 
+    <img class="sunrise-icon" src="./images/sunrise.png">
+    <h3 class="sunset" id="sunSet">  ${sunset}</h3>
+    <img class="sunset-icon" src="./images/sunset.png">`;
   });
+function convertUTCToSunTime(UTCsec) {
+  const UTCstring = new Date(UTCsec * 1000).toString();
+  const timeWithSec = UTCstring.split(" ")[4];
+  const timeWithoutSec = timeWithSec.slice(0, -3);
+  return timeWithoutSec; //07:38
+}
+
+// setting bg Image based on day/night
+if (timeInHr >= 6 && timeInHr <= 17) {
+  mainContainer.style.backgroundImage = `url(./images/day.jpg)`;
+  mainContainer.style.backgroundSize = "cover";
+} else if (timeInHr >= 18) {
+  mainContainer.style.backgroundImage = `url(./images/night.jpg)`;
+  mainContainer.style.backgroundSize = "cover";
+  mainContainer.style.color = "white";
+} else {
+  mainContainer.style.backgroundImage = `url(./images/night.jpg)`;
+  mainContainer.style.backgroundSize = "cover";
+  mainContainer.style.color = "white";
+}
 
 fetch(API_Weather_URL)
   .then((res) => res.json())
