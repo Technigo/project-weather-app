b3dba8f2f3f7576e5246a85c78626ed64537dd5e
Dorothea-B <barre.dorothea@gmail.com>
2022-02-17 10:12:00 +0100

    if/else statements for weather icons, adjusted to prevent today´s weather from showing

--- Designs/code/script.js
+++ Designs/code/script.js
@@ -8,7 +8,8 @@ const upcomingWeather = document.getElementById('upcomingWeather')
 
 //local variables
 let today = new Date().toLocaleDateString('en', {weekday: 'short'})
-console.log(today) 
+//console.log('today',today) 
+      
 
 //Today's temperature, city, weather type, sunrise and sunset
 fetch('https://api.openweathermap.org/data/2.5/weather?q=Rovaniemi,Finland&units=metric&APPID=94506b4af0e0a236471b8ee0da3c2281')
@@ -27,7 +28,7 @@ fetch('https://api.openweathermap.org/data/2.5/weather?q=Rovaniemi,Finland&units
 
     })
 //This is fetching the 5-day forecast
-    fetch('https://api.openweathermap.org/data/2.5/forecast?q=Stockholm,Sweden&units=metric&APPID=94506b4af0e0a236471b8ee0da3c2281')   
+    fetch('https://api.openweathermap.org/data/2.5/forecast?q=Rovaniemi,Finland&units=metric&APPID=94506b4af0e0a236471b8ee0da3c2281')   
     .then((response) => {
     return response.json()
     })
@@ -39,6 +40,8 @@ fetch('https://api.openweathermap.org/data/2.5/weather?q=Rovaniemi,Finland&units
     const filteredForecastNoon = forecastdata.list.filter(item => item.dt_txt.includes('12:00')) 
     const filteredForecastMidnight = forecastdata.list.filter(item => item.dt_txt.includes('00:00:00')) 
     
+        
+
     //console.log to test
     console.log('noon', filteredForecastNoon)
     console.log('midnight', filteredForecastMidnight)
@@ -48,13 +51,43 @@ fetch('https://api.openweathermap.org/data/2.5/weather?q=Rovaniemi,Finland&units
         let weekDay = filteredForecastNoon[day].dt_txt
         //printing the short versin of the weekday (e.g Mon,Tue,Wed,Thu,Fri,Sat,Sun)
         let shortWeekday = new Date(weekDay).toLocaleDateString('en', {weekday: 'short'})
+        
+        
+        //here if statement if shortWeekDay does not equels to today, then executes following:
+        if (shortWeekday !== today) {
+        //weatherType storage the value of forecast days's weather type
+        let weatherType = filteredForecastNoon[day].weather[0].main
+        console.log(weatherType)
+        //if statements what weather image we should use for forecasted days:
+        //here comes if statement 
+        
+        if (weatherType === 'Snow'){
+            typeImg = 'snow.png'
+        }
+        else if (weatherType === 'Clouds'){
+            typeImg = 'cloudy.png'
+        }
+        else if (weatherType === 'Clear'){
+            typeImg = 'sunny.png'
+        }
+        else if (weatherType === 'Rain'){
+            typeImg = 'rain.png'
+        }
+        else{
+            typeImg = 'partly.png'
+        }
+     
+
         //to make temperatures rounded to one decimal
         let roundedWeekMaxTemp = Math.round(filteredForecastNoon[day].main.temp_max *10) / 10
         let roundedWeekMinTemp = Math.round(filteredForecastMidnight[day].main.temp_min *10) / 10
-        upcomingWeather.innerHTML += `<div>${shortWeekday} ${roundedWeekMaxTemp}<span class="celsius">&#8451;</span> / ${roundedWeekMinTemp} <span class="celsius">&#8451;</span></div>`
-
+        upcomingWeather.innerHTML += `<div>${shortWeekday}<img class="small-weather-icons" src="./assets/${typeImg}"> ${roundedWeekMaxTemp}<span class="celsius">&#8451;</span> / ${roundedWeekMinTemp} <span class="celsius">&#8451;</span></div>`
+        }    
     }
-           
+
 })
+
+
+
 
 
