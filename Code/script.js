89c9caa92a61f3afd9471a15aa8b9d88e90dbb3a
Petra Eriksson <eriksson.petra@gmail.com>
2022-09-06 15:37:47 +0200

    got API to work and have featched a couple of values. Finished step 1 and step 2 in instructions

--- Code/script.js
+++ Code/script.js
@@ -1,18 +1,33 @@
 
 
 const container = document.getElementById('todaySummary')
+const mainWeather = document.getElementById('mainWeather') 
+const weeklyWeather = document.getElementById('weeklyForcastWrapper')
+const dailyForcast = document.getElementById('dailyForcastRow')
+const day = document.getElementById('day')
+const temp = document.getElementById('temp')
 
-fetch('http://api.openweathermap.org/data/2.5/weather?q=Stockholm,Sweden&units=metric&APPID=e7b3f1559413c473c7155cc145c88519')
+fetch('http://api.openweathermap.org/data/2.5/weather?q=Stockholm,Sweden&units=metric&APPID=64d2a624607147029ae4574d21f5c6d9')
     .then((response) => {
         return response.json()
     })
     .then((data) => {
-        console.log(data)
-    })
+      
+        mainWeather.innerHTML = `<h1>Todays temperature in ${data.name} is ${data.main.temp} and it's ${data.weather[0].description} today.</h1>`
+        console.log(data)    
+      })
     .catch((err) =>{ //ERROR function. We pass in a function as a parameter in the function, just like the then function.
       console.log(err)
     })
 
+    //the city name
+    //the temperature (rounded to 1 decimal place)
+    //and what type of weather it is (the "description" in the JSON)
+
+
+
+
+    
 
     //https://api.openweathermap.org/data/2.5/weather?lat={LAT}&lon={LON}&appid={API KEY}
 
