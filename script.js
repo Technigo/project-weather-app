6e82337fe944513e63bd27815477d0917c910d71
Amanda Elvkull <108291356+AmandaElvkull@users.noreply.github.com>
2022-09-06 13:44:30 +0200

    add ground of forcaste table and function for date- string short.

--- script.js
+++ script.js
@@ -1,5 +1,6 @@
 // Api 
 const API = 'https://api.openweathermap.org/data/2.5/weather?q=Amsterdam,Netherlands&units=metric&appid=26500228b15aa40fc0617041c68bf843'
+const apiForeCast = 'https://api.openweathermap.org/data/2.5/forecast?q=Amsterdam,Netherlands&units=metric&appid=26500228b15aa40fc0617041c68bf843'
 
 // Global DOM selectors
 
@@ -28,6 +29,42 @@ const nameCity = json.name
  container.innerHTML=`<h1> ${currentDescription} | ${temperature} ${nameCity} </h1>`
 
 })
+
+// turning the date to a string short weekday. 
+const weekDay = (data) => {
+    const currentDate = new Date (data * 1000); // set to millisec. 
+    return currentDate.toLocaleDateString('en-GB', {
+        weekday:'short',
+    });
+};
+
+
+//ForeCast table for 5 days - fetch API from new api URL 
+fetch(apiForeCast)
+.then((response) => response.json())
+.then((json) => {
+
+// new variable for filter the table and choose the same time everyday. 
+    const filterTable = json.list.filter((item) =>
+    item.dt_txt.includes('12:00')
+  );
+  
+  // printed out forcast. filterTable [x] changes depends of which day. 
+  forCast.innerHTML += `
+  <div class="day">${weekDay(filterTable[0].dt)}</div>
+  <div class="temp"> ${filterTable[0].main.temp.toFixed(0)}</div>
+  <div class="day">${weekDay(filterTable[1].dt)}</div>
+  <div class="temp"> ${filterTable[1].main.temp.toFixed(0)}</div>
+  <div class="day">${weekDay(filterTable[2].dt)}</div>
+  <div class="temp"> ${filterTable[2].main.temp.toFixed(0)}</div>
+  <div class="day">${weekDay(filterTable[3].dt)}</div>
+  <div class="temp"> ${filterTable[3].main.temp.toFixed(0)}</div>
+  <div class="day">${weekDay(filterTable[4].dt)}</div>
+  <div class="temp"> ${filterTable[4].main.temp.toFixed(0)}</div>
+ 
+  `
+})
+
 
 
 
