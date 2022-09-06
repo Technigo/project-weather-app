9af0d4b28c8ecd4ce4d2d5af8c097603d6cb25da
Tina Bruce <tinabruce42@hotmail.com>
2022-09-06 16:49:39 +0200

    added time formating function

--- code/script.js
+++ code/script.js
@@ -13,16 +13,40 @@ fetch('https://api.openweathermap.org/data/2.5/weather?q=Stockholm,Sweden&units=
     // API sunrise and sunset times are given as UNIX time: the number of seconds that have elapsed since 00:00:00 UTC on 1 January 1970
     let sunrise = new Date(json.sys.sunrise * 1000); //multiply by 1000 to get milliseconds, as Unix timestamp in in seconds while JS Date() uses milliseconds
     let sunset = new Date(json.sys.sunset * 1000);
+    let currentTime;
+
+    // Function to add a 0 before any single digit hour or minute
+    const formatTime = (sunEvent) => {
+        let hours = sunEvent.getHours()
+        let minutes = sunEvent.getMinutes()
+        
+        // Object that stores the hour and minutes 
+        currentTime = {
+            hours: hours,
+            minutes: minutes
+        }
+
+        // If needed, a 0 is added infront of the hours and minutes
+        if (hours < 10){
+            currentTime.hours = '0' + hours
+        }
+        if (minutes < 10){
+            currentTime.minutes = '0' + minutes
+        }
+        
+        return currentTime
+    }
     
     summary.innerHTML = `
         <h1>${json.name}</h1>
         ${json.weather[0].description} |
-        ${Math.round(json.main.temp)}°C 
-        <p>sunrise ${sunrise.getHours()}:${sunrise.getMinutes()}</p>
-        <p>sunset ${sunset.getHours()}:${sunset.getMinutes()} </p>
+        ${Math.round(json.main.temp)}° 
+        <p>sunrise ${formatTime(sunrise).hours}.${formatTime(sunrise).minutes}</p>  
+        <p>sunset ${formatTime(sunset).hours}.${formatTime(sunset).minutes}</p>
         ` 
     })
  
+    // ${sunrise.getHours()}.${sunrise.getMinutes()}
     //<p>${new Date()}</p>  Gives current date normally
     //<p>${Date.now()}</p>  Gives current date in milliseconds
 
