8e10f780fd6962eb3af08449efdb5f61b77c684d
Tina Bruce <tinabruce42@hotmail.com>
2022-09-06 15:54:00 +0200

    sunrise and sunset showing in app

--- code/script.js
+++ code/script.js
@@ -8,14 +8,22 @@ fetch('https://api.openweathermap.org/data/2.5/weather?q=Stockholm,Sweden&units=
     })
 
     .then((json) => {
-    console.log(json)
+    console.log(json) // remove later
+
+    // API sunrise and sunset times are given as UNIX time: the number of seconds that have elapsed since 00:00:00 UTC on 1 January 1970
+    let sunrise = new Date(json.sys.sunrise * 1000); //multiply by 1000 to get milliseconds, as Unix timestamp in in seconds while JS Date() uses milliseconds
+    let sunset = new Date(json.sys.sunset * 1000);
+    
     summary.innerHTML = `
         <h1>${json.name}</h1>
         ${json.weather[0].description} |
         ${Math.round(json.main.temp)}°C 
-        <p>sunrise ${json.sys.sunrise}</p>
-        <p>sunset ${json.sys.sunset}</p>
+        <p>sunrise ${sunrise.getHours()}:${sunrise.getMinutes()}</p>
+        <p>sunset ${sunset.getHours()}:${sunset.getMinutes()} </p>
         ` 
     })
  
+    //<p>${new Date()}</p>  Gives current date normally
+    //<p>${Date.now()}</p>  Gives current date in milliseconds
+
     
