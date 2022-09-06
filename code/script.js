99e0678c75f8d83236ff78a9cc14d94b34162a7e
emilbergvik <emilbergvik@gmail.com>
2022-09-07 00:38:11 +0200

    forecast temp working

--- code/script.js
+++ code/script.js
@@ -15,4 +15,28 @@ fetch('https://api.openweathermap.org/data/2.5/weather?q=Stockholm,Sweden&units=
     ` 
     })
  
+    fetch('https://api.openweathermap.org/data/2.5/forecast?q=Stockholm,Sweden&units=metric&APPID=a8803210b888f640e92f889b4be6e93f')
+    .then((response) => {
+    return response.json()
+    })
     
+    
+    .then((json) => {
+    console.log(json)
+    const filteredForecast = json.list.filter(item => item.dt_txt.includes('15:00'))
+    forecast.innerHTML =
+    `
+    ${Math.round(filteredForecast[0].main.temp)}°C
+
+    ${Math.round(filteredForecast[1].main.temp)}°C
+
+    ${Math.round(filteredForecast[2].main.temp)}°C
+
+    ${Math.round(filteredForecast[3].main.temp)}°C
+
+    ${Math.round(filteredForecast[4].main.temp)}°C
+    
+    `
+    
+    
+    })
