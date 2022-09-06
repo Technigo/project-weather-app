3585ff5e6691545638b199f5afd8d6e6c5636c63
Sailornina <sylcardozo.sc@gmail.com>
2022-09-06 12:01:30 +0200

    reformat code

--- Designs/code/script.js
+++ Designs/code/script.js
@@ -7,36 +7,36 @@ const description = document.getElementById('description');
 
 const weatherInfo = () => {
     fetch('https://api.openweathermap.org/data/2.5/weather?q=Stockholm,Sweden&units=metric&APPID=7899d890f36cbd5ef29eba2a205b5409')
-    .then(response => {
-        return response.json() //This always looks like this.
+        .then(response => {
+            return response.json() //This always looks like this.
 
-    })
-    .then((json) => {
-        console.log(json.name);
-        console.log(json.main.temp);
-        console.log(json.weather[0].description);
-        console.log(json);
-
-        const cityValue = json.name;
-        console.log(cityValue);
-        cityName.innerHTML += ` <td id="cityName2">${json.name}</td>`;
-        temp.innerHTML += ` <td id="temp">${json.main.temp}</td>`;
-        description.innerHTML += ` <td id="description">${json.weather[0].description}</td>`;
         })
-    }
+        .then((json) => {
+            console.log(json.name);
+            console.log(json.main.temp);
+            console.log(json.weather[0].description);
+            console.log(json);
+
+            const cityValue = json.name;
+            console.log(cityValue);
+            cityName.innerHTML += ` <td id="cityName2">${json.name}</td>`;
+            temp.innerHTML += ` <td id="temp">${json.main.temp}</td>`;
+            description.innerHTML += ` <td id="description">${json.weather[0].description}</td>`;
+        })
+}
 
 
-    weatherInfo();
+weatherInfo();
 
-    const fillWatherTable = () => {
-        cityName.innerHTML += `<td id="cityName2">${json.name}</td>`;
-/*         temp.innerHTML;
-        description.innerHTML; */
+const fillWatherTable = () => {
+    cityName.innerHTML += `<td id="cityName2">${json.name}</td>`;
+    /*         temp.innerHTML;
+            description.innerHTML; */
 
-    }
+}
 
 
-    fillWatherTable();
+fillWatherTable();
 
 //City name
 //temperature
