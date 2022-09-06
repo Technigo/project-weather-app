3dc233dbbdf4e7b11e1a3f43aeb00e5e3a2e3038
Tina Bruce <tinabruce42@hotmail.com>
2022-09-06 10:00:22 +0200

    steps 1-2

---
+++ code/script.js
@@ -0,0 +1,18 @@
+const summary = document.getElementById("summary");
+const main = document.getElementById("main");
+const forecast = document.getElementById("forecast");
+
+fetch('https://api.openweathermap.org/data/2.5/weather?q=Stockholm,Sweden&units=metric&APPID=a8803210b888f640e92f889b4be6e93f')
+    .then((response) => {
+    return response.json()
+    })
+
+    .then((json) => {
+    console.log(json)
+    summary.innerHTML = `<h1>${json.name}</h1>
+    ${json.weather[0].description}
+    ${Math.round(json.main.temp)}°C
+    ` 
+    })
+ 
+    
