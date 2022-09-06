67d0880eff4851ea0da688812801abba7e020304
Mine Mine Mine <MasterNina@NBs-MacBook-Pro.local>
2022-09-06 22:01:48 +0200

    step1 step2, done before branching

--- code/script.js
+++ code/script.js
@@ -1,14 +1,25 @@
 
-const fetchWeatherCity = () => {
-    fetch("https://api.openweathermap.org/data/2.5/weather?q=Stockholm,Sweden&units=metric&APPID=072c88c2b7f1a1c7fb7704f9f847b690")
+const todaysWeather = document.getElementById('todays-weather');
+const weatherCity = document.getElementById('weather-city');
+
+
+
+const fetchWeatherData = () => {
+    fetch("https://api.openweathermap.org/data/2.5/weather?q=Gothenburg,Sweden&units=metric&APPID=072c88c2b7f1a1c7fb7704f9f847b690")
       .then((response) => {
         return response.json();
       })
       .then((json) => {
-        const weatherCity = json;
-        console.log(weatherCity);
+        const weatherData = json;
+        console.log(weatherData);
+
+        weatherCity.innerHTML = weatherData.name; 
+        todaysWeather.innerHTML = `${weatherData.weather[0].description} | ${weatherData.main.temp.toFixed(1)}° `;
+        //Check that it rounds corret way when the temp change - OK!!
+
+        });
+       
+      };
+ 
   
-      });
-  };
-  
-  fetchWeatherCity();
+  fetchWeatherData();
