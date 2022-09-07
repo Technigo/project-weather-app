f48038bc1a61c51179b3bb76a3a81843cfddb7f6
David Ballester <dballesterfont@gmail.com>
2022-09-07 20:52:19 +0200

    Temp - City - Description --> Working / Correct time sunset and sunrise

--- code/script.js
+++ code/script.js
@@ -5,25 +5,44 @@ const infoSunPosition = document.getElementById('infoSunPosition');
  */
 const infoWrapper = document.getElementById('infoWrapper');
 const forecastWrapper = document.getElementById('forecastWrapper')
+const icon = document.getElementById('data-result-icon')
 
-fetch('http://api.openweathermap.org/data/2.5/weather?q=Stockholm,Sweden&units=metric&APPID=9fcd313d36f7f6461ce56606e7e35c9c')
+
+fetch('https://api.openweathermap.org/data/2.5/forecast?q=Stockholm,Sweden&units=metric&APPID=8802f8b4b2d622931613aace44be57ae')
 .then((response) => {
     return response.json() 
 })
 .then((json) => {
     console.log(json)
+
+    let unixSunriseTime = json.city.sunrise
+    let unixSunsetTime = json.city.sunset
+    const sunriseTime = new Date(unixSunriseTime * 1000)
+    const sunsetTime = new Date(unixSunsetTime * 1000)
+    
     infoWrapper.innerHTML += `
-    <div class="info-temp" id="infoTemp">${json.main.temp.toFixed(0)}</div> 
-    <div class="info-city" id="infoCity">${json.name}</div> 
-    <div class="info-sky" id="infoSky">${json.weather[0].description}</div>
-    <div class="info-sun-position" id="infoSunPosition">${json.sys.sunrise}</div>
-    ` // DAVID - Behöver ändra till klockslag 
+    <div class="info-temp" id="infoTemp">
+        <p>${json.list[0].main.temp.toFixed(0)}°C</p>
+    </div>
+    <div class="info-city" id="infoCity">
+        ${json.city.name}
+    </div> 
+    <div class="info-sky" id="infoSky">
+        <p>${json.list[0].weather[0].description}</p>
+        <img class="weather-icon" id="weatherIcon" src="http://openweathermap.org/img/wn/${json.list[0].weather[0].icon}.png" alt="weather-icon">
+    </div>
+    <div class="info-sun-position" id="infoSunPosition">
+        <p>sunrise ${sunriseTime.getHours()}:${sunriseTime.getMinutes()} </p>
+        <p>sunset ${sunsetTime.getHours()}:${sunsetTime.getMinutes()}</p>
+    </div>
+    `
 })
 .catch((err) => {
     console.log('caught error', err)
 });
 
-console.log('test')
+/* ${icon.src = "http://openweathermap.org/img/wn/${json.list[0].weather[0].icon}.png"  */
+
 //Sarah
 //const forecast = () => {
     //fetch
