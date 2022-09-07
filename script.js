47fe3fd5399b88b9bafa1ca4c7ed0463527f5992
jessnmo <jessicamo92@gmail.com>
2022-09-07 16:13:10 +0200

    added city input form

--- script.js
+++ script.js
@@ -1,3 +1,8 @@
+
+
+
+
+
 //trying fetch to see if connection between HTML is working, we can choose another API later
 fetch ('https://api.openweathermap.org/data/2.5/weather?q=Stockholm,Sweden&units=metric&APPID=399ae731e6b36c8272a3566b6ed57e5c')
     .then((response) => {
