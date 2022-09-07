0c1c370bd071b462d49393551bc84ade40b6bc81
Caroline Söderström <carolinesoderstrom@Carolines-MacBook-Air.local>
2022-09-07 16:24:44 +0200

    sunrise sunset added forecast started

--- script.js
+++ script.js
@@ -1,4 +1,6 @@
+const API_KEY = "15c9c7801fe68566167373f16cf7590a"
 const API = "https://api.openweathermap.org/data/2.5/weather?q=Stockholm,Sweden&units=metric&APPID=15c9c7801fe68566167373f16cf7590a";
+const API_FORECAST = "https://api.openweathermap.org/data/2.5/forecast?q=Stockholm,Sweden&units=metric&APPID=15c9c7801fe68566167373f16cf7590a"
 
 const weather = document.getElementById("weather");
 const city = document.getElementById("city");
@@ -6,18 +8,7 @@ const description = document.getElementById("description");
 const temperature = document.getElementById("temperature");
 const sunrise = document.getElementById("sunrise");
 const sunset = document.getElementById("sunset");
-const weekday = document.getElementById("weekday");
-
-// const sunrise = new Date(json.sys.sunrise * 1000); 
-// const sunset = new Date(json.sys.sunset *1000);
-
-// // declare new variable to show only hh:mm
-// const sunriseShort = sunrise.toLocaleTimeString([], {timeStyle: 'short'}) 
-// const sunsetShort = sunset.toLocaleTimeString([], {timeStyle: 'short'})
-
-
-// const sunriseNewDate = new Date(data.sys.sunrise * 1000); 
-// const sunriseTime = sunriseNewDate.toLocaleTimeString('en-US', {hour: '2-digit', minute: '2-digit', hour12: false})
+const fiveDays = document.getElementById("fiveDays");
 
 fetch (API)
 .then((response) => {
@@ -28,12 +19,73 @@ fetch (API)
  city.innerHTML = json.name
  description.innerHTML = json.weather[0].description
  temperature.innerHTML = json.main.temp.toFixed(1)
- sunrise.innerHTML = json.sys.sunrise
- sunset.innerHTML = json.sys.sunset
+ //console.log(json) 
 
- console.log(json) 
+//Sunrise  --> from numbers to date
+const weatherSunrise = () => {
+    const dateSunrise = new Date(json.sys.sunrise * 1000);
+    const timeSunrise = dateSunrise.toLocaleTimeString([], {
+      hour: '2-digit',
+      minute: '2-digit',
+    });
+    sunrise.innerHTML = timeSunrise;
+  };
+  weatherSunrise();
+
+//Sunset --> from numbers to date
+  const weatherSunset = () => {
+    const dateSunset = new Date(json.sys.sunset * 1000);
+    const timeSunset = dateSunset.toLocaleTimeString([], {
+      hour: '2-digit',
+      minute: '2-digit',
+    });
+    sunset.innerHTML = timeSunset;
+  };
+  weatherSunset();
+
 })
-.catch((error) =>
-console.error("There has been a problem with your fetch operation:", error)
-);
 
+// Five day forecast section
+
+fetch(API_FORECAST)
+    .then((response) => {
+    return response.json()
+   })
+   
+   .then((fiveForecastjson) => {
+
+// fetch the data from the API. Then if you console.log the json
+// you'll see that we only care about the array called list.
+
+const filteredForecast = fiveForecastjson.list.filter(item => item.dt_txt.includes('12:00'))
+// filteredForecast is now an array with only the data from 12:00 each day.
+
+const weekdays = [
+    "sunday",
+    "monday",
+    "tuesday",
+    "wednesday",
+    "thursday",
+    "friday",
+    "saturday",
+    "sunday",
+  ];
+
+filteredForecast.forEach((day) => {
+    const date = new Date(day.dt * 1000);
+    let weekdayNumber= date.getDay();
+    let roundedTemperature = day
+    console.log(roundedTemperature);
+
+    fiveDays.innerHTML += ` 
+    <div class="daily-forecast"> 
+    <p> ${weekdays[weekdayNumber]}</p> 
+    <p> ${roundedTemperature}°C </p>
+    </div> 
+    `;
+})
+});
+
+// .catch((error) =>
+// console.error("There has been a problem with your fetch operation:", error)
+// );
