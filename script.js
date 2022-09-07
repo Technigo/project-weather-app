26512d05c9105341fde8a939afa87701a9860b46
katirum <kartikaprw@gmail.com>
2022-09-07 09:20:10 +0200

    Co-authored-by: Linnéa Wahlström <LinneaWa@users.noreply.github.com>

--- script.js
+++ script.js
@@ -8,8 +8,12 @@ fetch('https://api.openweathermap.org/data/2.5/weather?q=Stockholm,Sweden&units=
         return response.json()
     })
     .then((json) =>{
+        let decimal = json.main.temp
+        let number = Math.round(decimal * 10) / 10;
+        let fixed = number.toFixed(1)
         city.innerHTML= (`City: ${json.name}`);
-        temperature.innerHTML= (`Temperature: ${Math.round(json.main.temp)}`); /*made the rounded decimal works*/
+        temperature.innerHTML+= (`Temperature: ${fixed}°C`);
+        /*made the rounded decimal works*/
         description.innerHTML= (`${json.weather[0].description}`) /*made it work by targeting the weather index zero, and the description within that index zero GREAT JOB!!!*/
     });
 
