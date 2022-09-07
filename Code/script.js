62d15bda8c851d82ecc554b7f3cca5cedf27b65a
Petra Eriksson <eriksson.petra@gmail.com>
2022-09-07 11:53:06 +0200

    Added a api for forcast, made to const out of the api, fetching day and temp for each day

--- Code/script.js
+++ Code/script.js
@@ -6,32 +6,64 @@ const weeklyWeather = document.getElementById('weeklyForcastWrapper')
 const dailyForcast = document.getElementById('dailyForcastRow')
 const day = document.getElementById('day')
 const temp = document.getElementById('temp')
-const temp2 = document.getElementById('temp2') //ta bort
+const ApiWeather = 'http://api.openweathermap.org/data/2.5/weather?q=Stockholm,Sweden&units=metric&APPID=64d2a624607147029ae4574d21f5c6d9'
+// this is the API variable for the 5 days forecast
+const ApiForcast = 'https://api.openweathermap.org/data/2.5/forecast?q=Stockholm,Sweden&units=metric&APPID=4c7a468589eea9cb94d5053a081d05ba'
 
-//HEJ funkar detta
 
 
-fetch('http://api.openweathermap.org/data/2.5/weather?q=Stockholm,Sweden&units=metric&APPID=64d2a624607147029ae4574d21f5c6d9')
+/// An object catching the weekday and turning it into a string//
+
+const days = ["Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday"];
+const d = new Date();
+let weekDay = days[d.getDay()];
+//console.log ('Today is ' + weekDay)
+
+fetch(ApiWeather)
     .then((response) => {
         return response.json()
     })
     .then((data) => {
 
         mainWeather.innerHTML = `<h1>Todays temperature in ${data.name} is ${data.main.temp} and it's ${data.weather[0].description} today.</h1>`
-        console.log(data)    
+        //console.log(data)    
       })
     .catch((err) =>{ //ERROR function. We pass in a function as a parameter in the function, just like the then function.
       console.log(err)
     })
 
-    //the city name
-    //the temperature (rounded to 1 decimal place)
-    //and what type of weather it is (the "description" in the JSON)
 
+    fetch(ApiForcast)
+    .then((response) => {
+        return response.json()
+    })
+    .then((dataforcast) => {
 
+        // day.innerHTML = `Works? ${dataforcast.cnt} and ${dataforcast.cod} `
+        // console.log(dataforcast)
+        //day.innerHTML = `${weekDay}`
 
+        
+          const filteredForecast = dataforcast.list.filter(item => item.dt_txt.includes('12:00'))
+          //console.log(filteredForecast)
+          filteredForecast.forEach((day) => {
+          const options1 = { weekday: 'long' }
+          // const options2 = { weekday: 'short' }
+          //     // forecast is injected in HTML, we also rounded the value to show no decimal
+          //     // with one decimal: ${Math.round(day.main.temp * 10) / 10}
+          //     // adds the weekdays in two ways, short and long format, example mon or monday
+          
+          
+          dailyForcast.innerHTML +=`<span>${new Intl.DateTimeFormat('en-GB', options1).format(day.dt * 1000).toLowerCase()}</span>
+          <span>${day.weather[0].description}</span>
+          <span>${Math.round(day.main.temp)}°</span>`
+        })
+      })
 
-    
+.catch((err) =>{ //ERROR function. We pass in a function as a parameter in the function, just like the then function.
+console.log(err)
+ })
+
 
     //https://api.openweathermap.org/data/2.5/weather?lat={LAT}&lon={LON}&appid={API KEY}
 
