f6d2109646b579424b61eb8afceb6cc1a81baa97
Julia Ostedt <julia.ostedt@gmail.com>
2022-09-07 12:04:46 +0200

    structured html, fetched current weather

--- code/script.js
+++ code/script.js
@@ -1,15 +1,27 @@
+// DOM selectors stored as short variables
 const city = document.getElementById("city");
 const currentWeather = document.getElementById("currentWeather");
+const currentTemp = document.getElementById("currentTemp");
 
-fetch(
-  "https://api.openweathermap.org/data/2.5/weather?q=Stockholm,Sweden&units=metric&APPID=1ecbebf1161e80b656c352a8c659aec8"
-)
+// Global variables
+let URLforecast =
+  "https://api.openweathermap.org/data/2.5/forecast?q=Stockholm,Sweden&units=metric&APPID=8802f8b4b2d622931613aace44be57ae";
+
+// Fetch
+fetch(URLforecast)
   .then((res) => {
-    console.log(res);
     return res.json();
   })
   .then((data) => {
-    city.innerHTML = data.name;
-    currentWeather.innerHTML = data.main;
     console.log(data);
+    city.innerHTML += `City: ${data.city.name}`;
+    currentWeather.innerHTML += `Current weather: ${data.list[0].weather[0].description} `;
+    currentTemp.innerHTML += `Temperature: ${data.list[0].main.temp.toFixed(
+      1
+    )}<sup>°C</sup>`; // toFixed(1) rounds temperature to one decimal
+    // Stackoverflow re: <sup> solution: https://stackoverflow.com/c/technigo/questions/750
   });
+
+// All the event listeners
+
+// Stackoverflow asked question: https://stackoverflow.com/c/technigo/questions/4001
