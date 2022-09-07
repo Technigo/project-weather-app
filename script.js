f4ad29762c949895184bf0579ea936ea21d0661c
Jessika Lind <jessika@MacBook-Air-som-tillhor-Jessika.local>
2022-09-07 16:42:10 +0200

    API working for Mon-Fri temp forecast

--- script.js
+++ script.js
@@ -7,9 +7,8 @@ const city = document.getElementById('city')
 const clearOrClody = document.getElementById('clearOrCloudy')
 const sunriseSunset = document.getElementById('sunriseSunset')
 const weatherWeek = document.getElementById('weatherWeek')
-const weekday = document.getElementById('weekday')
 
-fetch('https://api.openweathermap.org/data/2.5/weather?q=Stockholm,Sweden&units=metric&APPID=98bd2fbedad0f13ae05ed8e49698fda1')
+/*fetch('https://api.openweathermap.org/data/2.5/weather?q=Stockholm,Sweden&units=metric&APPID=98bd2fbedad0f13ae05ed8e49698fda1')
     .then((response) => {
         return response.json()
     })
@@ -17,6 +16,36 @@ fetch('https://api.openweathermap.org/data/2.5/weather?q=Stockholm,Sweden&units=
         console.log(json)
         city.innerHTML += json.name
     })
+    .catch((err) => {
+        console.log('caught error', err)
+    })*/
+
+
+//Array displaying the days of the week
+const dayOfTheWeek = ['Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday']
+for (let i = 0; i < dayOfTheWeek.length; i++) { // a for-loop function allowing me to loop through the items in the array. 
+    console.log(dayOfTheWeek[i])
+}
+    
+fetch('https://api.openweathermap.org/data/2.5/forecast?q=Stockholm,Sweden&units=metric&APPID=98bd2fbedad0f13ae05ed8e49698fda1')
+    .then((response) => {
+        return response.json()
+    })
+    .then ((data) => {
+        //console.log(data)
+        const filteredForecast = data.list.filter(item => item.dt_txt.includes('12:00')) //here we have made an array where only the weather at 12.00 will show. The array contains 5 objects (Mon-Fri) of which themselves are arrays. 
+        console.log(filteredForecast)
+
+        // This loop prints the days of the weeks and the temperatures belonging to each day. 
+       for (let i=0; i < filteredForecast.length; i++) {
+        console.log(i)
+        weatherWeek.innerHTML += 
+            `<div class = "weekday">
+                <p>${dayOfTheWeek[i]} ${filteredForecast[i].main.temp.toFixed(0)} °C</p> 
+            </div>`
+        }
+    })
+       
     .catch((err) => {
         console.log('caught error', err)
     })
