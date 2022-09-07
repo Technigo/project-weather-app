830199728be68543512861ecbcf4466aca6aed79
fannycardell <fannycardell@gmail.com>
2022-09-07 15:31:45 +0200

    first trial for sunrise

--- script.js
+++ script.js
@@ -19,6 +19,28 @@ fetch("http://api.openweathermap.org/data/2.5/weather?q=Stockholm,Sweden&units=m
         <p>temperature ${(json.main.temp).toFixed(1)}, feels like ${json.main.feels_like.toFixed(1)}, min temperature ${json.main.temp_min.toFixed(1)}, max temperature ${json.main.temp_max.toFixed(1)} </p>
         `
         })
+
+        const sunrise = new Date(json.sys.sunrise);
+        const sunset = new Date(json.sys.sunset);
+            console.log(sunrise.getHours) 
+            
+        //const sunrise = new Date();
+        //sunrise.setUTCHours(8);
+
+        //setUTCHours(hoursValue, minutesValue)
+
+        //An integer between 0 and 23, representing the hour. 
+        //An integer between 0 and 59, representing the minutes.
+
+        /*const setUTCHours = json.sys
+        setUTCHours((sunrise) => {
+            console.log(sunrise)
+        }
+        */
+
     })
 
-    //here is a test for git branch
+
+//sys: {type: 1, id: 1788, country: 'SE', sunrise: 1662523067, sunset: 1662572058} 
+//timezone: 7200 visibility: 10000
+
