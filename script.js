7439f9c6fef691f7b643ceda5a33c65661f6c5bd
katirum <kartikaprw@gmail.com>
2022-09-07 14:46:20 +0200

    Edit styling

--- script.js
+++ script.js
@@ -16,8 +16,8 @@ fetch('https://api.openweathermap.org/data/2.5/weather?q=Stockholm,Sweden&units=
         let decimal = json.main.temp
         let number = Math.round(decimal * 10) / 10;
         let fixed = number.toFixed(1)
-        city.innerHTML= (`City: ${json.name}`);
-        temperature.innerHTML+= (`Temperature: ${fixed}°C`);
+        city.innerHTML= (`${json.name}`);
+        temperature.innerHTML= (`${fixed}°C`);
         /*made the rounded decimal works*/
         description.innerHTML= (`${json.weather[0].description}`) /*made it work by targeting the weather index zero, and the description within that index zero GREAT JOB!!!*/
         const sunriseConvert = new Date((json.sys.sunrise) * 1000);
@@ -51,25 +51,65 @@ fetch ('https://api.openweathermap.org/data/2.5/forecast?q=Stockholm,Sweden&unit
     .then ((json) => {
         const filteredForecast = json.list.filter(item => item.dt_txt.includes('12:00'))
         forecast.innerHTML += `
+        <div id = "dayOne">
         <div class="days">${getDay(filteredForecast[1].dt)}</div>
         <div class="dayDescription">${filteredForecast[1].weather[0].description}</div>
         <div class="temp">Temp: ${filteredForecast[1].main.temp.toFixed(1)}°</div>
         <div class="feelsLike">Feels like:${filteredForecast[1].main.feels_like.toFixed(1)}°</div>
+        </div>
 
+        <div id = "dayTwo">
         <div class="days">${getDay(filteredForecast[2].dt)}</div>
-        <div class="temp">${filteredForecast[2].main.temp.toFixed(1)}°</div>
+        <div class="dayDescription">${filteredForecast[2].weather[0].description}</div>
         <div class="temp">Temp: ${filteredForecast[2].main.temp.toFixed(1)}°</div>
         <div class="feelsLike">Feels like:${filteredForecast[2].main.feels_like.toFixed(1)}°</div>
+        </div>
 
+        <div id = "dayThree">
         <div class="days">${getDay(filteredForecast[3].dt)}</div>
-        <div class="temp">${filteredForecast[3].main.temp.toFixed(1)}°</div>
+        <div class="dayDescription">${filteredForecast[3].weather[0].description}</div>
         <div class="temp">Temp: ${filteredForecast[3].main.temp.toFixed(1)}°</div>
         <div class="feelsLike">Feels like:${filteredForecast[3].main.feels_like.toFixed(1)}°</div>
+        </div>
 
+        <div id = "dayFour">
         <div class="days">${getDay(filteredForecast[4].dt)}</div>
-        <div class="temp">${filteredForecast[4].main.temp.toFixed(1)}°</div>
+        <div class="dayDescription">${filteredForecast[4].weather[0].description}</div>
         <div class="temp">Temp: ${filteredForecast[4].main.temp.toFixed(1)}°</div>
         <div class="feelsLike">Feels like:${filteredForecast[4].main.feels_like.toFixed(1)}°</div>
-
+        </div>
         `
     })
+
+    const currentTime = new Date().getHours();
+    if(document.body){
+        if(7 <= currentTime && currentTime < 8){
+            document.body.background = "./images/try.jpg";
+            
+        }else {
+            document.body.background = 	"./images/try2.jpg";
+            document.body.style.color = "#FFFFFF";
+            document.getElementById("dayOne").style.backgroundColor = "#F0FFFF";
+        }
+    }
+
+
+    /*<html>
+<head>
+    <title></title>
+</head>
+<body>
+
+</body>
+<script type="text/javascript">
+var currentTime = new Date().getHours();
+if (document.body) {
+    if (7 <= currentTime && currentTime < 8) {
+        document.body.background = "http://itsnotch.com/tumblr/images/daytime_bg.jpg";
+    }
+    else {
+        document.body.background = "http://itsnotch.com/tumblr/images/nighttime_bg.jpg";
+    }
+}
+</script>
+</html> */
