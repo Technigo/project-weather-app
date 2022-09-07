95aad69ef728179ebf406cfd3dcdfc59d87cd026
Linnea Ajger <linnea.ajger@gmail.com>
2022-09-07 09:42:02 +0200

    updated with weather description

--- script.js
+++ script.js
@@ -8,15 +8,21 @@ fetch("http://api.openweathermap.org/data/2.5/weather?q=Stockholm,Sweden&units=m
 
     .then((json) => {
         console.log(json)
+
+        const weathers = json.weather
+        weathers.forEach((weather, index) => {
+            console.log(weather.description)
+
         region.innerHTML = `
         <h1> ${json.name} </h1>
-        <p>Temperature ${(json.main.temp).toFixed(1)}, feels like ${json.main.feels_like.toFixed(1)}, min temperature ${json.main.temp_min.toFixed(1)}, max temperature ${json.main.temp_max.toFixed(1)} </p>
+        <p>${weather.description}</p>
+        <p>temperature ${(json.main.temp).toFixed(1)}, feels like ${json.main.feels_like.toFixed(1)}, min temperature ${json.main.temp_min.toFixed(1)}, max temperature ${json.main.temp_max.toFixed(1)} </p>
         `
-        console.log(json.name)
+        })
     })
 
     
 
 //- the city name OK
-// - the temperature (rounded to 1 decimal place)
-// - and what type of weather it is (the "description" in the JSON)
+// - the temperature (rounded to 1 decimal place) OK
+// - and what type of weather it is (the "description" in the JSON) OK
