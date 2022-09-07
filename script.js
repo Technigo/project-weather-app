fde5011c04782bf1e684e0ae0fd95e78e3d8f40c
joelohman <joel.ohman@entryevent.se>
2022-09-07 19:35:27 +0200

    Sunrise/sunset and weather forecast from API

--- script.js
+++ script.js
@@ -11,45 +11,58 @@ const sectionForecast = document.getElementById('forecast');
 fetch(API_TODAY)
   .then(response => response.json())
   .then(data => {
-    console.log(data);
+    console.log('today data:', data);
+    console.log('weather main:', data.weather[0].main);
+
+    const sunrise = new Date(data.sys.sunrise * 1000)
+      .toLocaleTimeString()
+      .slice(0, 5);
+    const sunset = new Date(data.sys.sunset * 1000)
+      .toLocaleTimeString()
+      .slice(0, 5);
+
+    let imageSrc, weatherDescription;
+
+    if (data.weather[0].main === 'Clear') {
+      imageSrc = 'noun_Sunglasses_2055147.svg';
+      weatherDescription = `Get your sunnies on. ${data.name} is looking rather great today.`;
+    } else if (data.weather[0].main === 'Rain') {
+      imageSrc = 'noun_Umbrella_2030530.svg';
+      weatherDescription = `Don't forget your umbrella. It's wet in ${data.name} today`;
+    } else {
+      imageSrc = 'noun_Cloud_1188486.svg';
+      weatherDescription = `Light a fire and get cosy. ${data.name} is looking grey today.`;
+    }
+
     sectionToday.innerHTML = `
-    <p class="today-temp">clear | 23°</p>
-    <p class="sunrise">sunrise 08.00</p>
-    <p class="sunset">sunset 22.30</p>
-    <img class="weather-icon" />
-    <h1 class="today-title">Get your sunnies on. ${data.name} is looking rather great today.</h1>
+    <p class="today-temp">${
+      data.weather[0].description
+    } | ${data.main.temp.toFixed(1)}°</p>
+    <p class="sunrise">sunrise ${sunrise}</p>
+    <p class="sunset">sunset ${sunset}</p>
+    <img class="weather-icon"
+    src="/Designs/Design-2/icons/${imageSrc}"
+  />
+    <h1 class="today-title">${weatherDescription}</h1>
     `;
   });
 
 fetch(API_FORECAST)
   .then(response => response.json())
   .then(data => {
-    console.log(data);
-    sectionForecast.innerHTML = `
-    <div class="forecast-day-container">
-     <p class="forecast-day">mon</p>
-     <p class="forecast-day-temp">23°</p>
-    </div>
-    <div class="forecast-day-container">
-     <p class="forecast-day">tue</p>
-     <p class="forecast-day-temp">20°</p>
-    </div>
-    <div class="forecast-day-container">
-     <p class="forecast-day">wed</p>
-     <p class="forecast-day-temp">16°</p>
-    </div>
-    <div class="forecast-day-container">
-     <p class="forecast-day">thu</p>
-     <p class="forecast-day-temp">25°</p>
-    </div>
-    <div class="forecast-day-container">
-     <p class="forecast-day">fri</p>
-     <p class="forecast-day-temp">27°</p>
-    </div>
-    <div class="forecast-day-container">
-     <p class="forecast-day">sat</p>
-     <p class="forecast-day-temp">23°</p>
-    </div>
+    console.log('forecast data:', data);
 
-    `;
+    const forecastDays = data.list.filter(day => day.dt_txt.includes('12:00'));
+    console.log(forecastDays);
+    sectionForecast.innerHTML = '';
+    forecastDays.forEach(day => {
+      const date = new Date(day.dt * 1000);
+      day.weekday = String(date).slice(0, 3);
+      sectionForecast.innerHTML += `
+      <div class="forecast-day-container">
+       <p class="forecast-day">${day.weekday}</p>
+       <p class="forecast-day-temp">${day.main.temp.toFixed(1)}°</p>
+      </div>
+      `;
+    });
   });
