fa0e869f67f2f2277fe2a56a4190c85df6bb18c9
Julia Ostedt <julia.ostedt@gmail.com>
2022-09-07 07:38:57 +0200

    displayed data from the json

--- code/script.js
+++ code/script.js
@@ -1,7 +1,15 @@
-fetch("https://api.openweathermap.org/data/2.5/weather?q=Stockholm,Sweden&units=metric&APPID=1ecbebf1161e80b656c352a8c659aec8")
-.then((response) => {
-    return response.json()
-})
-.then ((json) => {
-    console.log(json)
-})
+const city = document.getElementById("city");
+const currentWeather = document.getElementById("currentWeather");
+
+fetch(
+  "https://api.openweathermap.org/data/2.5/weather?q=Stockholm,Sweden&units=metric&APPID=1ecbebf1161e80b656c352a8c659aec8"
+)
+  .then((res) => {
+    console.log(res);
+    return res.json();
+  })
+  .then((data) => {
+    city.innerHTML = data.name;
+    currentWeather.innerHTML = data.main;
+    console.log(data);
+  });
