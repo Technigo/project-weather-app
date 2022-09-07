b02816a36277083a26df38879dc3003cee02bf37
Elin Segelöv <elin.segelov@gmail.com>
2022-09-07 11:01:26 +0200

    made adjustments to new endpoint

--- script.js
+++ script.js
@@ -4,23 +4,22 @@ const cityWeather = document.getElementById('weather-placeholder');
 const sunriseTime = document.getElementById('sunrise-time');
 const sunsetTime = document.getElementById('sunset-time');
 
+//api.openweathermap.org/data/2.5/forecast?lat={lat}&lon={lon}&appid={API key}
 
-
-fetch('https://api.openweathermap.org/data/2.5/weather?q=Stockholm,Sweden&units=metric&APPID=8802f8b4b2d622931613aace44be57ae')
+fetch('https://api.openweathermap.org/data/2.5/forecast?q=Stockholm,Sweden&units=metric&APPID=8802f8b4b2d622931613aace44be57ae')
     .then(response => {
         return response.json()
     }) 
     .then((json) => {
         console.log(json);
-        cityName.innerHTML = json.name;
-        cityTemp.innerHTML = (Math.round(json.main.temp)).toFixed(1);
-        cityWeather.innerHTML = json.weather[0].description;
-        const sunriseStart = new Date(json.sys.sunrise);
-        const sunsetStart = new Date(json.sys.sunset);
+        cityName.innerHTML = json.city.name;
+        cityTemp.innerHTML = json.list[0].main.temp.toFixed(1);
+        cityWeather.innerHTML = json.list[0].weather[0].description;
+        const sunriseStart = new Date(json.city.sunrise);
+        const sunsetStart = new Date(json.city.sunset);
         //https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/Date/toLocaleTimeString
         sunriseTime.innerHTML = sunriseStart.toLocaleTimeString([], {hour:'2-digit', minute: '2-digit'});
         sunsetTime.innerHTML = sunsetStart.toLocaleTimeString([], {hour:'2-digit', minute: '2-digit'});
-
     } )
     
     .catch((error) => {
