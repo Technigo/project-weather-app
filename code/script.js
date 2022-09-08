d8778c5ea9f5acb7290f1a7ee3f72c604fa503e1
Sneezan <ceciliafraanlund@gmail.com>
2022-09-08 13:21:13 +0200

    search bar function complete and working

--- code/script.js
+++ code/script.js
@@ -1,13 +1,20 @@
+// burger menu
+const closeMenu = document.querySelector(".closeMenu");
+const burger = document.querySelector(".burger");
+const sideMenu = document.querySelector(".sideMenu");
+const inputLocation = document.getElementById("inputLocation");
+const submitBtn = document.getElementById("submitBtn");
+const searchForm = document.getElementById("searchForm");
+
+// city and temps
 const currentCity = document.getElementById("city");
 const liveTemperature = document.getElementById("temperature");
 const weatherDescription = document.getElementById("description");
 const dayForecast = document.getElementById("weatherForecast");
 const daysOfTheWeek = ["sun", "mon", "tue", "wed", "thu", "fri", "sat"];
+const weatherStats = document.getElementById("todays-weather");
 const currentSunrise = document.getElementById("todaysWeatherSunrise");
 const currentSunset = document.getElementById("todaysWeatherSunset");
-const closeMenu = document.querySelector(".closeMenu");
-const burger = document.querySelector(".burger");
-const sideMenu = document.querySelector(".sideMenu");
 
 // show is added to how burger menu appears
 const show = () => {
@@ -21,69 +28,81 @@ const close = () => {
 };
 
 
-//This works! But how do we change the city!!! 
-let city = "Stockholm"
-const apiNow =
-`https://api.openweathermap.org/data/2.5/weather?q=${city}&units=metric&APPID=ba408ec4b2f7f251f2dd0044bd3e07f2`;
-const apiForecast =
-`https://api.openweathermap.org/data/2.5/forecast?q=${city}&units=metric&APPID=ba408ec4b2f7f251f2dd0044bd3e07f2`;
+const getWeather = (city) => {
+  console.log('fetching weather data for ', city)
+  const apiNow =
+  `https://api.openweathermap.org/data/2.5/weather?q=${city}&units=metric&APPID=ba408ec4b2f7f251f2dd0044bd3e07f2`;
+  const apiForecast =
+  `https://api.openweathermap.org/data/2.5/forecast?q=${city}&units=metric&APPID=ba408ec4b2f7f251f2dd0044bd3e07f2`;
 
-fetch(apiNow)
-  .then((response) => {
-    return response.json();
-  })
-  .then((json) => {
-    currentCity.innerHTML = json.name;
-    liveTemperature.innerHTML = json.main.temp.toFixed(1);
-    weatherDescription.innerHTML = json.weather[0].description;
+  console.log('using urls:', { apiNow, apiForecast})
 
-    // Sunrise and sunset //
-    const timestampSunrise = json.sys.sunrise;
-    const timestampSunset = json.sys.sunset;
+  fetch(apiNow)
+    .then((response) => response.json())
+    .then((json) => {
 
-    let sunrise = new Date(timestampSunrise * 1000);
-    let sunriseTime = sunrise.toLocaleTimeString([], { timeStyle: "short" });
-    currentSunrise.innerHTML = `${sunriseTime}`; // prints in HTML
+      currentCity.innerHTML = json.name;
+      liveTemperature.innerHTML = json.main.temp.toFixed(1);
+      weatherDescription.innerHTML = json.weather[0].description;
+      
+      // Sunrise and sunset //
+      const timestampSunrise = json.sys.sunrise;
+      const timestampSunset = json.sys.sunset;
 
-    let sunset = new Date(timestampSunset * 1000);
-    let sunsetTime = sunset.toLocaleTimeString([], { timeStyle: "short" });
-    currentSunset.innerHTML = `${sunsetTime}`; // prints in HTML
-  
-    console.log(json);
-  })
-  .catch((error) =>
-    console.error("There has been a problem with your fetch operation:", error)
-  );
+      let sunrise = new Date(timestampSunrise * 1000);
+      let sunriseTime = sunrise.toLocaleTimeString([], { timeStyle: "short" });
+      currentSunrise.innerHTML = `${sunriseTime}`; // prints in HTML
 
+      let sunset = new Date(timestampSunset * 1000);
+      let sunsetTime = sunset.toLocaleTimeString([], { timeStyle: "short" });
+      currentSunset.innerHTML = `${sunsetTime}`; // prints in HTML
 
-fetch(apiForecast)
-  .then((response) => {
-    return response.json();
-  })
-  .then((dayForecast) => {
-    const filteredForecast = dayForecast.list.filter((item) =>
-      // @TODO maybe change to a better time here? see this for visualisation https://jsoncrack.com/editor?fbclid=IwAR2ZSGA26fdIHECi0-ISKwEsHs8BuZlb8bCS_-3O1j_0drQRkNIdzvK7fE0
-      item.dt_txt.includes("12:00")
+    })
+    
+    .catch((error) =>
+      console.error("There has been a problem with your fetch operation:", error)
     );
-    filteredForecast.forEach((dayForecast) => {
-      const forecastDate = new Date(dayForecast.dt * 1000);
-      const dayOfTheWeek = forecastDate.getDay();
-      weatherForecast.innerHTML += `
-      <div class="weather-forecast-entry">
-      <span>${daysOfTheWeek[dayOfTheWeek]}</span>
-      <span>${dayForecast.main.temp.toFixed(0)}°</span>
-      </div>
-      `;
+
+
+  fetch(apiForecast)
+    .then((response) => {
+      return response.json();
+    })
+    .then((dayForecast) => {
+      const filteredForecast = dayForecast.list.filter((item) =>
+        // @TODO maybe change to a better time here? see this for visualisation https://jsoncrack.com/editor?fbclid=IwAR2ZSGA26fdIHECi0-ISKwEsHs8BuZlb8bCS_-3O1j_0drQRkNIdzvK7fE0
+        item.dt_txt.includes("12:00")
+      );
+      filteredForecast.forEach((dayForecast) => {
+        const forecastDate = new Date(dayForecast.dt * 1000);
+        const dayOfTheWeek = forecastDate.getDay();
+        weatherForecast.innerHTML += `
+        <div class="weather-forecast-entry">
+        <span>${daysOfTheWeek[dayOfTheWeek]}</span>
+        <span>${dayForecast.main.temp.toFixed(0)}°</span>
+        </div>
+        `;
+      });
     });
-  });
+}
+getWeather("Barcelona");
 
-// 
 
 
+// Event listeners 
+searchForm.addEventListener("submit", (e) => {  //when pressend enter it sends
+  console.log('form submitted')
+  e.preventDefault();
+  weatherStats.innerHTML;
+  weatherForecast.innerHTML= ``;
+  city = inputLocation.value;
+  console.log('change city to', city)
+  getWeather(city);
+  
+});
 burger.addEventListener("click", show);
 closeMenu.addEventListener("click", close);
 searchForm.addEventListener("submit", close);
-
 
 
 /*
