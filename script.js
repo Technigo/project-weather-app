9ecab3617a521528c8e290149b98ef36b646d27f
Jessika Lind <jessika@MacBook-Air-som-tillhor-Jessika.local>
2022-09-08 08:56:20 +0200

    feels-like temp added + some styling

--- script.js
+++ script.js
@@ -7,8 +7,10 @@ const city = document.getElementById('city')
 const clearOrClody = document.getElementById('clearOrCloudy')
 const sunriseSunset = document.getElementById('sunriseSunset')
 const weatherWeek = document.getElementById('weatherWeek')
+const rainOrSun = document.getElementById('rainOrSun')
+const weekday = document.getElementById('weekday')
 
-/*fetch('https://api.openweathermap.org/data/2.5/weather?q=Stockholm,Sweden&units=metric&APPID=98bd2fbedad0f13ae05ed8e49698fda1')
+fetch('https://api.openweathermap.org/data/2.5/weather?q=Stockholm,Sweden&units=metric&APPID=98bd2fbedad0f13ae05ed8e49698fda1')
     .then((response) => {
         return response.json()
     })
@@ -18,43 +20,62 @@ const weatherWeek = document.getElementById('weatherWeek')
     })
     .catch((err) => {
         console.log('caught error', err)
-    })*/
+    })
 
 
 //Array displaying the days of the week
 const dayOfTheWeek = ['Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday']
 for (let i = 0; i < dayOfTheWeek.length; i++) { // a for-loop function allowing me to loop through the items in the array. 
-    console.log(dayOfTheWeek[i])
+    //console.log(dayOfTheWeek[i])
 }
 
-
-    
+//Fetching the API for the weekly forecast
 fetch('https://api.openweathermap.org/data/2.5/forecast?q=Stockholm,Sweden&units=metric&APPID=98bd2fbedad0f13ae05ed8e49698fda1')
     .then((response) => {
         return response.json()
     })
+
     .then ((data) => {
-        //console.log(data)
+        // console.log(data)
         const filteredForecast = data.list.filter(item => item.dt_txt.includes('12:00')) //here we have made an array where only the weather at 12.00 will show. The array contains 5 objects (Mon-Fri) of which themselves are arrays. 
         console.log(filteredForecast)
 
         // This loop prints the days of the weeks and the temperatures belonging to each day. 
        for (let i=0; i < filteredForecast.length; i++) {
+        //console.log(i)
         
-      
-
-        console.log(i)
+        const dailyTemp = filteredForecast[i].main.temp.toFixed(0)
+        const currentWeather = filteredForecast[i].weather[0].main
+        const feelsLike = filteredForecast[i].main.feels_like.toFixed(0)
+        
+        const showWeatherIcon = () => {
+            if (currentWeather === "Clear") {
+                weatherWeek.innerHTML += `<div☀️</div>`
+              } 
+              
+              else if (currentWeather === "Rain") {
+              weatherWeek.innerHTML += `<div>🌧</div>`
+              } 
+              
+              else {
+                weatherWeek.innerHTML += `<div>☁️</div>`
+              }
+        }
+        showWeatherIcon()
+         //varför kan jag inte lägga in detta
+        
         weatherWeek.innerHTML += 
-            `<div class = "weekday">
+            `<div class = "weekday id="weekday">
                 <p class = "day"> ${dayOfTheWeek[i]} </p>
-                <p class = "day-temp"> ${filteredForecast[i].main.temp.toFixed(0)}°C </p> 
-                <p class = "rain-or-sun"> ${filteredForecast[i].weather[0].main}
+                <p class="rain-or-sun" id="rainOrSun"> ${currentWeather} </p>
+                <p class = "day-temp"> ${dailyTemp}°C </p>     
+                <p class = "feels-like"> Feels like: ${feelsLike}°C</p>
             </div>`
+             //Tanken är att Clouds/rain ska ersättas av emojin som i dagsläget ligger utanför div:en. 
         } 
-
-        
-        })
+    })
 
     .catch((err) => {
         console.log('caught error', err)
     })
+      
