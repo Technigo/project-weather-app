13687b18c71fc8ff6fd345500c9b4415366ee422
fannycardell <fannycardell@gmail.com>
2022-09-08 12:19:21 +0200

    added sunrise and sunset

--- script.js
+++ script.js
@@ -1,5 +1,6 @@
 const region = document.getElementById('region')
-
+const sunrise = document.getElementById('sunrise-time')
+const sunset = document.getElementById('sunset-time')
 
 fetch("http://api.openweathermap.org/data/2.5/weather?q=Stockholm,Sweden&units=metric&APPID=fb125bc213d8ee5c4a432b3a2b24aecf")
     .then((response) => {
@@ -18,17 +19,18 @@ fetch("http://api.openweathermap.org/data/2.5/weather?q=Stockholm,Sweden&units=m
         <p>${weather.description}</p>
         <p>temperature ${(json.main.temp).toFixed(1)}, feels like ${json.main.feels_like.toFixed(1)}, min temperature ${json.main.temp_min.toFixed(1)}, max temperature ${json.main.temp_max.toFixed(1)} </p>
         `
-        })
-
-        const sunrise = new Date(json.sys.sunrise);
-        const sunset = new Date(json.sys.sunset);
-            console.log(sunrise.getHours) 
+        sunrise.innerHTML = new Date(json.sys.sunrise *1000);
+        //sunrise.setUTCHours(8)
+        sunset.innerHTML = new Date(json.sys.sunset *1000);
+        //sunset.setUTCHours(8)
+            console.log(sunrise, sunset) 
+        
             
-        //const sunrise = new Date();
-        //sunrise.setUTCHours(8);
+        //sys: {type: 1, id: 1788, country: 'SE', sunrise: 1662523067, sunset: 1662572058} 
+        //timezone: 7200 visibility: 10000
+        //;
 
-        //setUTCHours(hoursValue, minutesValue)
-
+        //setUTCHours(hoursValue, minutesValue) Hur definerar jag variablerna?
         //An integer between 0 and 23, representing the hour. 
         //An integer between 0 and 59, representing the minutes.
 
@@ -38,9 +40,10 @@ fetch("http://api.openweathermap.org/data/2.5/weather?q=Stockholm,Sweden&units=m
         }
         */
 
+        })
+
     })
 
 
-//sys: {type: 1, id: 1788, country: 'SE', sunrise: 1662523067, sunset: 1662572058} 
-//timezone: 7200 visibility: 10000
+
 
