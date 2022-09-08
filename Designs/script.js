273618ecd21a4446bcd4fc5b24ba687cb6c34282
Kristin <kristin@iMac.local>
2022-09-08 17:26:58 +0200

    started with CSS

--- Designs/script.js
+++ Designs/script.js
@@ -1,7 +1,7 @@
 const currentWeatherURL = "https://api.openweathermap.org/data/2.5/weather?q=Stockholm,Sweden&units=metric&APPID=072a011cef8c3eb73f98d70ebc36f439";
 const forecastWeatherURL = "https://api.openweathermap.org/data/2.5/forecast?q=Stockholm,Sweden&units=metric&APPID=072a011cef8c3eb73f98d70ebc36f439";
 
-const sunriseSunset = document.getElementById("SunriseSunset");
+const sunriseSunset = document.getElementById("sunriseSunset");
 
 const city = document.getElementById("city");
 
@@ -17,18 +17,30 @@ fetch(currentWeatherURL)
     console.log(cityWeatherData)
 
     const weatherDescription = cityWeatherData.weather[0].description;
-    sunriseSunset.innerHTML += `<p>Description: ${weatherDescription}</p>`
+    sunriseSunset.innerHTML += `
+    <div class="description">
+    <p>Description: ${weatherDescription}</p>
+    </div>
+    `
     
 
     const unixTimestampSunrise = cityWeatherData.sys.sunrise    //Declare variable for the time of sunrise/sunset
     const sunriseCalc = new Date(unixTimestampSunrise * 1000)   //To get sunrise/sunset time in hours:minutes:seconds
     const sunriseTime = sunriseCalc.toLocaleTimeString([], { timeStyle: 'short' }) //Declare new variable to show only hh:mm
-    sunriseSunset.innerHTML += `<p>Sunrise: ${sunriseTime}</p>`
+    sunriseSunset.innerHTML += `
+    <div class="sunrise">
+    <p>Sunrise: ${sunriseTime}</p>
+    </div>
+    `
 
     const unixTimestampSunset = cityWeatherData.sys.sunset
     const sunsetCalc = new Date(unixTimestampSunset * 1000)
     const sunsetTime = sunsetCalc.toLocaleTimeString([], { timeStyle: 'short' })
-    sunriseSunset.innerHTML += `<p>Sunset: ${sunsetTime}</p>`
+    sunriseSunset.innerHTML += `
+    <div class="sunset">
+    <p>Sunset: ${sunsetTime}</p>
+    </div>
+    `
 
     city.innerHTML = cityWeatherData.name;
 
