c7c862f2ead2f7a7c8002fcd4858baa7207ff971
KajaWilbik <wilbik.kaja@gmail.com>
2022-09-08 09:53:54 +0200

    assigned variables JS

--- script.js
+++ script.js
@@ -1,7 +1,8 @@
-
-
-
-
+const cityInput = document.getElementById("city-input");
+const geo = document.getElementById("geo")
+const todaysWeather = document.getElementById("todaysWeather")
+const message = document.getElementById("message")
+const weatherForecast = document.getElementById("weather-forecast")
 
 //trying fetch to see if connection between HTML is working, we can choose another API later
 fetch ('https://api.openweathermap.org/data/2.5/weather?q=Stockholm,Sweden&units=metric&APPID=399ae731e6b36c8272a3566b6ed57e5c')
