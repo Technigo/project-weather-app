13bd2f63480926159e052a824f7ca82d6cc59540
Tara78 <70331037+Tara78@users.noreply.github.com>
2022-09-08 16:24:57 +0200

    surnise

--- app.js
+++ app.js
@@ -3,14 +3,11 @@ const container = document.getElementById('container');
 const btnSearchCity= document.getElementById('btn-searchCity');
 
 const today = new Date()
-var date = (today.getMonth() + 1) + '-' + today.getDate();
-var time = today.getHours() + ":" + today.getMinutes();
-var CurrentDateTime = date + ' ' + time;
-
-
+const date = (today.getMonth() + 1) + '-' + today.getDate();
+const time = today.getHours() + ":" + today.getMinutes();
+const CurrentDateTime = date + ' ' + time;
 const sunrise= document.getElementById('sunrise')
 const sunset= document.getElementById('sunset')
-
 
 btnSearchCity.addEventListener('click', ()=>{
   const city = document.getElementById('search').value;
@@ -21,10 +18,9 @@ btnSearchCity.addEventListener('click', ()=>{
   .then(function (response) {
     return response.json();
   })
-  .then(function (data) {
-
+  .then(function (data) {    
     container.innerHTML = `
-    <p>${time}</p>
+    <h2>${time}</h2>
       <h1>${data.city.name}</h1>
        <h1>${Math.round(data.list[0].main.temp_kf.toFixed(1) * 9 / 5 + 32)} ${'&#8451;'}</h1>
       <h3>${data.list[0].weather[0].main}</h3>
@@ -32,31 +28,22 @@ btnSearchCity.addEventListener('click', ()=>{
       <p> Sunset: ${data.city.sunset}</p>
      
       `
-      
-
-      // const sunrise = new Date(json.sys.sunrise * 1000);
-// const sunset = new Date(json.sys.sunset * 1000);
-
-    // const sunriseTime = sunrise.toLocaleTimeString([], { timeStyle: 'short' })
-    // const sunsetTime = sunset.toLocaleTimeString([], { timeStyle: 'short' })
-
   console.log(data)
+
     const degree = Math.round(data.list[0].main.temp_kf.toFixed(1));
     // const result= (degree-32) * (5/9) ;
     // const result = (degree-32)/1.8;
     const result= degree * 9/5 +32;
     console.log(result)
 
-
-    // const sunriseTime = sunrise.toLocaleTimeString([], { timeStyle: 'short' })
-
-    // const suRise= (data.city.sunriseTime.sys)
-    // console.log(sunriseTime)
-
     // Temp as a string with 1 decimal pointed;
 //  const temp = data.main.temp.toFixed(1);
   // console.log(Math.round(data.main.temp * 10) / 10);
   })
+
+  const gethours = function(){
+
+  }
 
 })
 
