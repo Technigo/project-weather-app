710bad3b6d1908bcad238c55287540133b0a638c
Archkrull <102250838+Archkrull@users.noreply.github.com>
2022-09-09 11:31:27 +0200

    finished forecast festure

--- code/script.js
+++ code/script.js
@@ -17,7 +17,7 @@ fetch("https://api.openweathermap.org/data/2.5/weather?q=Stockholm,Sweden&units=
 //Get the days for the forecast weather
 const getDay = (weekday) => {
     const dates = new Date(weekday * 1000); 
-    return dates.toLocaleDateString('en', {weekday: 'long'}); 
+    return dates.toLocaleDateString('en', {weekday: 'long', day:'numeric'}); 
     }
 
     
@@ -27,37 +27,33 @@ const fetchWeatherForecast = () => {
         return response.json()
     })
         .then((json) => {
-        const filteredForecast = json.list.filter(item => item.dt_txt.includes('12:00') && ('00:00'))  
-        forecast.innerHTML = `
+        const filteredForecast = json.list.filter(item => item.dt_txt.includes('12:00'))  
+        forecast.innerHTML += `
         <div id = "dayOne">
-            <div class="day">${getDay(filteredForecast[0].dt)}</div>
-            <div class="description">${filteredForecast[0].weather[0].description}</div>
-            <div class="temp">${filteredForecast[0].main.temp_min.toFixed(1)}</div>
-            <div class="temp">${filteredForecast[0].main.temp_max.toFixed(1)}</div>
+            <div class="day">${getDay(filteredForecast[1].dt)}</div>
+            <div class="description">${filteredForecast[1].weather[0].description}</div>
+            <div class="temp">${filteredForecast[1].main.temp.toFixed(1)}</div>
 
         </div>
 
         <div id = "dayTwo">
-            <div class="day">${getDay(filteredForecast[1].dt)}</div>
-            <div class="description">${filteredForecast[1].weather[0].description}</div>
-            <div class="temp">${filteredForecast[1].main.temp_min.toFixed(1)}</div>
-            <div class="temp">${filteredForecast[1].main.temp_max.toFixed(1)}</div>
+            <div class="day">${getDay(filteredForecast[2].dt)}</div>
+            <div class="description">${filteredForecast[2].weather[0].description}</div>
+            <div class="temp">${filteredForecast[2].main.temp.toFixed(1)}</div>
 
         </div>
         
         <div id = "dayThree">
-            <div class="day">${getDay(filteredForecast[2].dt)}</div>
-            <div class="description">${filteredForecast[2].weather[0].description}</div>
-            <div class="temp">${filteredForecast[2].main.temp_min.toFixed(1)}</div>
-            <div class="temp">${filteredForecast[2].main.temp_max.toFixed(1)}</div>
+            <div class="day">${getDay(filteredForecast[3].dt)}</div>
+            <div class="description">${filteredForecast[3].weather[0].description}</div>
+            <div class="temp">${filteredForecast[3].main.temp.toFixed(1)}</div>
 
         </div>
         
         <div id = "dayFour">
-            <div class="day">${getDay(filteredForecast[3].dt)}</div>
-            <div class="description">${filteredForecast[3].weather[0].description}</div>
-            <div class="temp">${filteredForecast[3].main.temp_min.toFixed(1)}</div>
-            <div class="temp">${filteredForecast[3].main.temp_max.toFixed(1)}</div>
+            <div class="day">${getDay(filteredForecast[4].dt)}</div>
+            <div class="description">${filteredForecast[4].weather[0].description}</div>
+            <div class="temp">${filteredForecast[4].main.temp.toFixed(1)}</div>
 
         </div>`
         
@@ -67,40 +63,5 @@ const fetchWeatherForecast = () => {
 
 fetchWeatherForecast ()
 
-    /* .then((json) => {
-        forecast.innerHTML =  ''
-        const filteredForecast = json.list.filter((forecastDay) => {
-            return forecastDay.dt_txt.includes('12:00')
-        })
-        filteredForecast.forEach((forecastDay) => {
-            forecast.innerHTML += `
-            <div class="forecast-grid">
-                <div class="days">
-                    <h3>${getWeekday(forecastDay.dt_txt)}</h3>
-                </div>
-                <div class="weather-type">
-                    <img src="assets/${weatherIcon(forecastDay.weather[0].main)}">
-                </div>
-                <div class="min-temp">
-                    <h3>${Math.round(forecastDay.main.temp_min)}°C</h3>
-                </div>
-                <div class="max-temp">
-                    <h3>${Math.round(forecastDay.main.temp_max)}°C</h3>
-                </div>
-            </div>`
-        })
-
- */
     
-    /* .then((json) => {
-        const filteredForecast = json.list.filter(item =>
-          item.dt_txt.includes("12:00")
-        );
-        json.forEach((forecast) => {
-          filteredForecast.innerHTML += generatedHTMLForWeatherForecast(forecast);
-        }); */
-     /*  }); */
-/*     }); 
-  }
-    fetchWeatherForecast() */
-   // https://api.openweathermap.org/data/2.5/forecast?lat=59.3326&lon=18.0649&appid=d5df19d6f5f0e0c58e9f1a6d07022e47
+ 
