55432b21427973bf9fc4c9fb3aacd9d7048dce50
Jacob Lie <jacob_lie@hotmail.com>
2022-09-09 12:19:11 +0200

    added wind direction and speed

--- Designs/Design-2/app.js
+++ Designs/Design-2/app.js
@@ -1,95 +1,47 @@
-const apiKey = 'e30ad481a3fcc72a6217c4f6edfa883d'
-const lat = '59.3307'
-const lon = '18.0579'
+//GLOBAL VARIABLES
 const city = 'Stockholm, Sweden'
+const apiKey = 'e30ad481a3fcc72a6217c4f6edfa883d'
 const urlWeather = `https://api.openweathermap.org/data/2.5/weather?q=${city}&units=metric&APPID=${apiKey}`
 const urlForecast = `https://api.openweathermap.org/data/2.5/forecast?q=${city}&units=metric&APPID=${apiKey}`
-const urlForecast7Days = `https://api.openweathermap.org/data/2.5/forecast/daily?lat=${lat}&lon=${lon}&cnt=7&APPID=${apiKey}`
-
-function addContentForWeekdays() {
-    const dayNames = ['Sun', 'Mon', 'Tue', 'Wed', 'Thur', 'Fri', 'Sat'];
-    const weekdays = document.getElementById('weekdays')
-
-    fetch(urlForecast)
-        .then((res) => {
-            return res.json()
-        })
-        .then((res) => {
-            const days = res.list.filter(item => {
-                if(item.dt_txt.endsWith('12:00:00')) {
-                    if(
-                        item.dt_txt.startsWith(
-                            new Date().toLocaleDateString('sv')
-                            
-                        )
-                    ){
-                        return false
-                    }
-                    return true
-
-                }
-                return false
-            })
-
-            
-            // Cleaner version with map
-            weekdays.innerHTML = days.map((day) => {
-                const dateObject = new Date(day.dt_txt) // Get JS date object
-                const dayIndex = dateObject.getDay() // The number of the day in a week
-
-                return `<div class="row">
-                    <p>${dayNames[dayIndex]}</p>
-                    <p>${Math.round(day.main.temp)}</p>
-                </div>`
-            }).join('')
-        })
-}
-
-addContentForWeekdays()
+const weatherSlogan = document.getElementById('weather-slogan')
+const img = document.getElementById('weather-img')
+const todaysWeather = document.getElementById('todays-weather')
+const windSpeed = document.getElementById('wind-speed')
+const windDirection = document.getElementById('wind-direction')
 
 
-// function functionName(yourArgument) {
-//     console.log(yourArgument);
-// } 
 
-// const funtionName = (yourArgument) => {
-//     console.log(yourArgument);
-// } 
+const windDirectionConverter = (degrees) => {
+        let val = Math.floor((degrees / 22.5) + 0.5);
+        let arr = ["N", "NNE", "NE", "ENE", "E", "ESE", "SE", "SSE", "S", "SSW", "SW", "WSW", "W", "WNW", "NW", "NNW"];
+        return arr[(val % 16)];
+    }
 
-// (yourArgument) => {
-//     console.log(yourArgument);
-// } 
 
+// This function generates all the info we need for today's weather forecast and sunrise/sunset.
 fetch(urlWeather)
         .then((res) => {
             return res.json()
         })
         .then((res) => {
            console.log(res);
-           const weatherSlogun = document.getElementById('weather-slogun')
-           const img = document.getElementById('weather-img')
-            const todaysWeather = document.getElementById('todays-weather')
-            const todaysSunrise = document.getElementById('todays-sunrise')
-            const todaysSunset = document.getElementById('todays-sunset')
-            
-            todaysWeather.innerHTML =`${res.weather[0].main} | ${Math.round(res.main.temp)}&#8451;`
-            console.log(todaysWeather);
+           todaysWeather.innerHTML =`${res.weather[0].main} | ${Math.round(res.main.temp)}&#8451;`
+           console.log(todaysWeather);
 
-            function getTimeFromDate(datetime) {
+           function getTimeFromDate(datetime) {
                 const date = new Date(datetime * 1000)
                 return `${date.getHours().toString().padStart(2, '0')}:${date.getMinutes().toString().padStart(2, '0')}`
             }
 
-            todaysSunrise.innerHTML = `Sunrise ${getTimeFromDate(res.sys.sunrise)} `
-            
-            todaysSunset.innerHTML = `Sunset ${getTimeFromDate(res.sys.sunset)} ` 
+            let windDirectionCardinal = windDirectionConverter((res.wind.deg));
+            windSpeed.innerHTML = `Wind speed | ${(res.wind.speed)} m/s`            
+            windDirection.innerHTML = `Wind direction | ${(windDirectionCardinal)} ` 
 
+// Changes the CSS appearance of the page depending on today's weather forecast.
             const root = document.querySelector(':root')
-             //shows different weather types
-            res.weather[0].main = 'Snow'
     
            if(res.weather[0].main === 'Clouds')  {
-               weatherSlogun.innerHTML =`Light a fire and get cozy, ${res.name} is looking gray today`;
+               weatherSlogan.innerHTML =`Light a fire and get cozy, ${res.name} is looking gray today`;
                img.setAttribute('src', 'icons/cloud.svg')
                root.style.setProperty('--bg-color', '#f3f7f8')
                root.style.setProperty('--font-color', '#ff7476')
@@ -97,7 +49,7 @@ fetch(urlWeather)
 
            }
            if(res.weather[0].main === 'Clear') {
-            weatherSlogun.innerHTML = `Get your sunnies on, ${res.name} is looking might fine today`;
+            weatherSlogan.innerHTML = `Get your sunnies on, ${res.name} is looking mighty fine today`;
             img.setAttribute('src', 'icons/sunglasses.svg')
             root.style.setProperty('--bg-color', '#f7e9bd')
             root.style.setProperty('--font-color', '#2c541b')
@@ -105,7 +57,7 @@ fetch(urlWeather)
            }
 
            if(res.weather[0].main === 'Rain') {
-           weatherSlogun.innerHTML =`Don't forget your Umbrella, it's wet in ${res.name} today`;
+           weatherSlogan.innerHTML =`Don't forget your Umbrella, it's wet in ${res.name} today`;
            img.setAttribute('src', 'icons/umbrella.svg') 
            root.style.setProperty('--bg-color', '#b3e9fa')
            root.style.setProperty('--font-color', '#004b67')
@@ -113,10 +65,50 @@ fetch(urlWeather)
            }
 
            if(res.weather[0].main === 'Snow') {
-            weatherSlogun.innerHTML =`Wrap up warm, ${res.name} is looking very white today`;
+            weatherSlogan.innerHTML =`Wrap up warm, ${res.name} is looking very white today`;
             img.setAttribute('src', 'icons/snowflake.svg') 
             root.style.setProperty('--bg-color', '#ffffff')
             root.style.setProperty('--font-color', '#7fd2fe')
             root.style.setProperty('--border-color', '#90d1fa7a')
             }
         })
+
+
+    //This function generates the 5-day forecast based on the urlForecast API. It fetches the midday forecast for each of the 5 days from the API.
+        function addContentForWeekdays() {
+            const dayNames = ['Sun', 'Mon', 'Tue', 'Wed', 'Thur', 'Fri', 'Sat'];
+            const weekdays = document.getElementById('weekdays')
+        
+            fetch(urlForecast)
+                .then((res) => {
+                    return res.json()
+                })
+                .then((res) => {
+                    const days = res.list.filter(item => {
+                        if(item.dt_txt.endsWith('12:00:00')) {
+                            if(
+                                item.dt_txt.startsWith(
+                                    new Date().toLocaleDateString('sv')
+                                    
+                                )
+                            ){
+                                return false
+                            }
+                            return true
+                        }
+                        return false
+                    })
+                        
+                    weekdays.innerHTML = days.map((day) => {
+                        const dateObject = new Date(day.dt_txt) // Get JS date object
+                        const dayIndex = dateObject.getDay() // The number of the day in a week
+        
+                        return `<div class="row">
+                            <p>${dayNames[dayIndex]}</p>
+                            <p>${Math.round(day.main.temp)}&#8451</p>
+                        </div>`
+                    }).join('')
+                })
+        }
+        
+        addContentForWeekdays()
