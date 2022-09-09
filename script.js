7940903e28fde30aeedd1dee67f2d57e62e071a2
Linnea Ajger <linnea.ajger@gmail.com>
2022-09-09 20:33:49 +0200

    added switch statements and toggle functions to the different weathers categories

--- script.js
+++ script.js
@@ -1,5 +1,12 @@
-const region = document.getElementById('region')
-const weekdays = ["Sun","Mon","Tue","Wed","Thu","Fri","Sat"];
+const header = document.getElementById('header')
+const weatherDescription = document.getElementById('weatherDescription')
+const fiveDaysForecast = document.getElementById('fiveDaysForecast')
+const weatherImg = document.getElementById('weatherImg')
+const container = document.getElementById('container')
+const weekdays = ["sun","mon","tue","wed","thu","fri","sat"];
+
+let containerToggle = document.querySelector('.container')
+
 
 fetch("http://api.openweathermap.org/data/2.5/weather?q=Stockholm,Sweden&units=metric&APPID=fb125bc213d8ee5c4a432b3a2b24aecf")
     .then((response) => {
@@ -11,14 +18,50 @@ fetch("http://api.openweathermap.org/data/2.5/weather?q=Stockholm,Sweden&units=m
         const weathers = json.weather
         weathers.map((weather) => {
 
-        region.innerHTML = `
-        <h1> ${json.name} </h1>
-        <h3> Today </h3>
-        <p>${weather.description}</p>
-        <p>temperature ${(json.main.temp).toFixed(0)}°</p>
+        header.innerHTML = `
+        <h3>${weather.description} | ${(json.main.temp).toFixed(0)}°</h3>
         `
-        })
+
+        //different actions depending on weather
+        switch(weather.main) {
+            case 'Clouds':  // if (x === 'value1')
+                console.log('cloudy');
+                weatherImg.innerHTML += `
+                <img src="./Designs/Design-2/icons/noun_Cloud_1188486.svg" alt="">`
+                weatherDescription.innerHTML += `
+                <h1>The sky is 50 shades of grey in ${json.name}.  </h1>
+                `;
+                containerToggle.classList.toggle('container-cloudy');
+                console.log(weatherToggle)
+            break;
+        
+            case 'Rain':  // if (x === 'value2')
+                console.log('rainy')
+                console.log(weather.description)
+                weatherImg.innerHTML += `
+                <img src="./Designs/Design-2/icons/noun_Umbrella_2030530.svg" alt="">`       
+                weatherDescription.innerHTML += `
+                <h1>Drip drop. It's raining in ${json.name}. Don't forget your umbrella.  </h1>
+                `
+                containerToggle.classList.toggle('container-rainy');
+                console.log(containerToggle)
+            break;
+        
+            default:
+                console.log('sunny')
+                weatherImg.innerHTML += `
+                <img src="./Designs/Design-2/icons/noun_Sunglasses_2055147.svg" alt="">`
+                weatherDescription.innerHTML += `
+                <h1>The sky is clear as korvspad in ${json.name}. </h1>
+                </div>
+                `
+                containerToggle.classList.toggle('container-clear');
+                console.log(containerToggle)
+            break;
+        }
+
     })
+ })
 
 //WEATHER-FORECAST FEATURE 
 // Show a forecast for the next 5 days. You can choose how to display the forecast 
@@ -46,14 +89,15 @@ fetch("https://api.openweathermap.org/data/2.5/forecast?q=Stockholm,Sweden&units
                 console.log(weekdays[weekday]);
 
 
-                region.innerHTML += `
+                fiveDaysForecast.innerHTML += `
                 <div id="forecastSection" class="forecast-section">
+                
                     <div id="weekdaysection" class="weekday-section">
                          <h3> ${weekdays[weekday]} </h3>
                     </div>
                     
                     <div id="temperature" class="temperature">
-                    <p>${day.weather[0].description} ${(day.main.temp).toFixed(0)}°</p>
+                    <h3>${(day.main.temp).toFixed(0)}°</h3>
                     </div>
 
                 </div>
