9820e0614d85822176815bc21b3045360b2be602
Elin Segelöv <elin.segelov@gmail.com>
2022-09-09 11:39:31 +0200

    styling adjustments,e.g grid on wekly forcast

--- script.js
+++ script.js
@@ -23,7 +23,7 @@ fetch(
     cityTemp.innerHTML = json.list[0].main.temp.toFixed(1);
     cityWeather.innerHTML = json.list[0].weather[0].description;
     const sunriseStart = new Date(json.city.sunrise * 1000);
-    const sunsetStart = new Date(json.city.sunset * 1000); //här hämtas fel data
+    const sunsetStart = new Date(json.city.sunset * 1000); 
 
     sunriseTime.innerHTML = sunriseStart.toLocaleTimeString([], {
       hour: "2-digit",
@@ -35,44 +35,54 @@ fetch(
     });
 
     //Actual weather & background picture
+    //clouds
     if (json.list[0].weather[0].main.includes("Clouds")) {
-      hero.style.backgroundImage = `linear-gradient(rgba(255,255,255,0.5), rgba(255,255,255,0.5)), url('images/cloud.jpg')`;
-      todaysIcon.innerHTML = `<img src=\'images/cloud-icon.png'>
-            <h1>It is rather gray today in ${json.city.name}.</h1>`;
+      hero.style.backgroundImage = `linear-gradient(rgba(255,255,255,0.5), rgba(255,255,255,0.5)), url('images/bg-cloud-sun.jpg')`;
+      todaysIcon.innerHTML = 
+        `<img src=\'images/Clouds.png'>
+        <h3>It is rather gray today in ${json.city.name}.</h3>`;
+    //rain
     } else if (json.list[0].weather[0].main.includes("Rain")) {
-      hero.style.backgroundImage = `linear-gradient(rgba(255,255,255,0.5), rgba(255,255,255,0.5)), url('images/rain.jpeg')`;
-      todaysIcon.innerHTML = `<img src=\'images/rain-icon.png'>
-            <h1>Don't forget you umbrella. It's raining in ${json.city.name}.</h1>`;
+      hero.style.backgroundImage = `linear-gradient(rgba(255,255,255,0.5), rgba(255,255,255,0.5)), url('images/bg-rain.jpeg')`;
+      todaysIcon.innerHTML = 
+        `<img src=\'images/Rain.png'>
+        <h3>Don't forget you umbrella. It's raining in ${json.city.name}.</h3>`;
+    //clear
     } else if (json.list[0].weather[0].main.includes("Clear")) {
-      hero.style.backgroundImage = `linear-gradient(rgba(255,255,255,0.5), rgba(255,255,255,0.5)), url('images/clear.png')`;
-      todaysIcon.innerHTML = `<img src=\'images/sun-icon.png'>
-            <h1>The sun is shining in ${json.city.name} today.</h1>`;
+      hero.style.backgroundImage = `linear-gradient(rgba(255,255,255,0.5), rgba(255,255,255,0.5)), url('images/bg-clear.png')`;
+      todaysIcon.innerHTML = 
+        `<img src=\'images/Clear.png'>
+        <h3>The sky is clear in ${json.city.name} today.</h3>`;
+    //snow      
     } else if (json.list[0].weather[0].main.includes("Snow")) {
-      hero.style.backgroundImage = `linear-gradient(rgba(255,255,255,0.5), rgba(255,255,255,0.5)), url('images/snow.jpeg')`;
-      todaysIcon.innerHTML = `<img src=\'images/snow-icon.png'>
-            <h1>It is snowing in ${json.city.name}.</h1>`;
+      hero.style.backgroundImage = `linear-gradient(rgba(255,255,255,0.5), rgba(255,255,255,0.5)), url('images/bg-snow.jpeg')`;
+      todaysIcon.innerHTML = 
+        `<img src=\'images/Snow.png'>
+        <h3>It is snowing in ${json.city.name}.</h3>`;
+    //other
     } else {
       hero.style.backgroundImage = `linear-gradient(rgba(255,255,255,0.5), rgba(255,255,255,0.5)), url('images/rain.jpeg')`;
-      todaysIcon.innerHTML = `<img src=\'images/cloud-sun-icon.png'>
-            <h1>It is a cloudy ${json.city.name} now.</h1>`;
+      todaysIcon.innerHTML = 
+        `<img src=\'images/cloud-sun-icon.png'>
+        <h3>It is a cloudy ${json.city.name} now.</h3>`;
     }
 
     weeklyWeather = json.list.filter((item) => item.dt_txt.includes("12:00"));
     console.log(weeklyWeather);
-    weeklyTemp = weeklyWeather.map((day) => {
+/*     weeklyWeather = weeklyWeather.map((day) => {
       let date = new Date(day.dt * 1000);
       let nameOfDay = date.toLocaleDateString("en-Us", { weekday: "long" });
       let dailyTemperature = day.main.temp.toFixed(1);
 
       if (day.weather[0].main.includes("Clouds")) {
-        dailyIcon = `<img src=\'images/cloud-icon.png'>`;
+        dailyIcon = `<img src=\'images/icon-cloud.png'>`;
       } else if (day.weather[0].main.includes("Rain")) {
-        dailyIcon = `<img src=\'images/rain-icon.png'>`;
+        dailyIcon = `<img src=\'images/icon-rain.png'>`;
       } else if (day.weather[0].main.includes("Clear")) {
-        dailyIcon = `<img src=\'images/sun-icon.png'>`;
+        dailyIcon = `<img src=\'images/icon-sun.png'>`;
       } else if (day.weather[0].main.includes("Snow")) {
       } else {
-        dailyIcon = `<img src=\'images/snow-icon.png'>`;
+        dailyIcon = `<img src=\'images/icon-snow.png'>`;
       }
 
       console.log(weeklyTemp);
@@ -83,7 +93,22 @@ fetch(
            <span>${dailyTemperature}°C</span>
            </li>
            `);
-    });
+    }); */
+    weeklyWeather = weeklyWeather.map((day) => {
+        let date = new Date(day.dt * 1000);
+        let nameOfDay = date.toLocaleDateString('en-Us', {weekday: 'long'});
+        let dailyTemperature = day.main.temp.toFixed(1);
+        dailyIcon = `<img src=\'images/${day.weather[0].main}.png'>`;
+        return (
+            weeklyTemp.innerHTML +=`
+            <li>
+                <p>${nameOfDay}</p>
+                <p class="daily-icon">${dailyIcon}</p>
+                <p class="daily-temperature">${dailyTemperature}°C</p>
+            </li>
+       `)   
+    })
+
   })
 
   .catch((error) => {
@@ -91,17 +116,3 @@ fetch(
   });
 
 
-/*         weeklyTemp = weeklyWeather.map((day) => {
-            let date = new Date(day.dt * 1000);
-            let nameOfDay = date.toLocaleDateString('en-Us', {weekday: 'long'});
-            let dailyTemperature = day.main.temp.toFixed(1);
-            dailyIcon = `<img src=\'images/${day.weather[0].main}.png'>`;
-            return (
-                weeklyTemp.innerHTML +=`
-                <li>
-                    <span>${nameOfDay}</span>
-                    <span>${dailyIcon}</span>
-                    <span>${dailyTemperature}°C</span>
-                </li>
-           `)   
-        }) */
