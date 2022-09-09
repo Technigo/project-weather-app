4dd66bcb7ca1934ef07393b82f4966fbda969682
Kneedler <sarahklindelow@gmail.com>
2022-09-09 09:33:50 +0200

    master+

--- README.md
+++ README.md
@@ -1,13 +1,13 @@
 # Weather App
-
-Replace this readme with your own information about your project.
-
-Start by briefly describing the assignment in a sentence or two. Keep it short and to the point.
+This weeks project was to build a simple weather dashboard that tells today's weather and temperature and a 5-day forecast using a weather API.
+To build a simple 
 
 ## The problem
 
-Describe how you approached to problem, and what tools and techniques you used to solve it. How did you plan? What technologies did you use? If you had more time, what would be next?
+We worked together at first and then in branshes and learnd how to do that. We also used public API that we fetched and filterd the information throu JSON. We dorked with date() and how to convert the respons in to display the correct time. We had problem with adding a zero to the left of the timestamp, when we were using a getMinutes(). For this we created a function with an if statement. At first we also hade some trouble understanding the filter function, but when we understood it we could use it too loop throu te response and get the information we wanted. 
+
+We would add a search function or more citys and style the page more to change color or photo based on the weather. 
 
 ## View it live
+https://idyllic-queijadas-6e6760.netlify.app/
 
-Every project should be deployed somewhere. Be sure to include the link to the deployed project so that the viewer can click around and see what it's all about.
--- code/script.js
+++ code/script.js
@@ -1,66 +1,58 @@
-/* const infoTemp = document.getElementById('infoTemp');
-const infoCity = document.getElementById('infoCity');
-const infoSky = document.getElementById('infoSky');
-const infoSunPosition = document.getElementById('infoSunPosition');
- */
+// Variables that point to selected DOM elements
 const infoWrapper = document.getElementById('infoWrapper');
 const forecastWrapper = document.getElementById('forecastWrapper')
 const icon = document.getElementById('data-result-icon')
 
-
+//the fetch from the API
 fetch('https://api.openweathermap.org/data/2.5/forecast?q=Stockholm,Sweden&units=metric&APPID=8802f8b4b2d622931613aace44be57ae')
     .then((response) => {
         return response.json() 
     })
     .then((json) => {
         console.log(json)
-
+//Sunrise and sunset
         let unixSunriseTime = json.city.sunrise
         let unixSunsetTime = json.city.sunset
         const sunriseTime = new Date(unixSunriseTime * 1000)
         const sunsetTime = new Date(unixSunsetTime * 1000)
         
+        //
         infoWrapper.innerHTML += `
         <img class="menu" src="https://img.icons8.com/color/48/000000/menu--v1.png"/>
         <div class="info-row">
-        <div class="info-temp" id="infoTemp">
-            <h1>${json.list[0].main.temp.toFixed(0)}</h1>
+            <div class="info-temp" id="infoTemp">
+                <h1>${json.list[0].main.temp.toFixed(0)}</h1>
+            </div>
+            <div class="info-sky" id="infoSky">
+                <p>${json.list[0].weather[0].description}</p>
+                <img class="weather-icon" id="weatherIcon" 
+                src="http://openweathermap.org/img/wn/${json.list[0].weather[0].icon}.png" alt="weather-icon">
+            </div>
+            <div class="info-city" id="infoCity">${json.city.name}</div> 
+            <div class="info-sun-position" id="infoSunPosition">
+                <p class="text">Sunrise at ${sunriseTime.getHours()}:${sunriseTime.getMinutes()}</p>
+                <p class="text">Sunset at ${sunsetTime.getHours()}:${sunsetTime.getMinutes()}</p>
+            </div>
         </div>
-        <div class="info-sky" id="infoSky">
-            <p>${json.list[0].weather[0].description}</p>
-            <img class="weather-icon" id="weatherIcon" src="http://openweathermap.org/img/wn/${json.list[0].weather[0].icon}.png" alt="weather-icon">
-        </div>
-        <div class="info-city" id="infoCity">
-            ${json.city.name}
-        </div> 
-        <div class="info-sun-position" id="infoSunPosition">
-            <p class="text">Sunrise at ${sunriseTime.getHours()}:${sunriseTime.getMinutes()}</p>
-            <p class="text">Sunset at ${sunsetTime.getHours()}:${sunsetTime.getMinutes()}</p>
-        </div>
-        </div>
-        
-        `
-      
+        `;
+      // five day forecast 
         let filteredForecast = json.list.filter(item => item.dt_txt.includes('12:00'));
         filteredForecast.forEach(item => {
             forecastWrapper.innerHTML += `
             <div class="forecast-row">
-            <div class="for-day-temp"> ${new Date(item.dt * 1000).toLocaleDateString("en-US", {
-                weekday: "short",
-              })}</div>
-            <img class="weather-icon" id="weatherIcon" src="http://openweathermap.org/img/wn/${item.weather[0].icon}.png" alt="weather-icon">
-            <div class="for-day-temp"> ${item.main.temp.toFixed(0)}˚C</div>
-            <div class="desktop-info"> Feels like ${item.main.feels_like.toFixed(0)}˚C</div>
-            <div class="desktop-info"> Humidity ${item.main.humidity.toFixed(0)}%</div>
-            <div class="desktop-info"> Wind speed ${item.wind.speed} m/s</div>
-            
+                <div class="for-day-temp"> ${new Date(item.dt * 1000).toLocaleDateString("en-US", {
+                weekday: "short",})}</div>
+                <img class="weather-icon" id="weatherIcon" 
+                src="http://openweathermap.org/img/wn/${item.weather[0].icon}.png" alt="weather-icon">
+                <div class="for-day-temp"> ${item.main.temp.toFixed(0)}˚C</div>
+                <div class="desktop-info"> Feels like ${item.main.feels_like.toFixed(0)}˚C</div>
+                <div class="desktop-info"> Humidity ${item.main.humidity.toFixed(0)}%</div>
+                <div class="desktop-info"> Wind speed ${item.wind.speed} m/s</div>
             </div>
-            `
+            `;
         })
     })
+// if there is an error, this will catch it
 .catch((err) => {
     console.log('caught error', err)
 });
-
-
-// ${item.main.temp_min.toFixed(0)
