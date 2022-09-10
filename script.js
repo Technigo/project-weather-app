60e0709e7c9874c31e6d6904fd44a45fcc2effad
Jessika Lind <jessika@MacBook-Air-som-tillhor-Jessika.local>
2022-09-10 16:33:26 +0200

    responsive design added + time fixed

--- script.js
+++ script.js
@@ -32,21 +32,21 @@ const containerSunset = document.getElementById("sunset")
 
         //Sunrise and sunset //*1000 because it is milliseconds
         let sunrise = json.city.sunrise;
-        let sunriseDate = new Date(sunrise * 1000).toLocaleString().split(", ").slice(0).join(", ") //get the slice-method to work
+        let sunriseDate = new Date(sunrise * 1000).toLocaleTimeString()
 
-        containerSunrise.innerHTML = `<h2> Sunrise: ${sunriseDate} </h2>`
+        containerSunrise.innerHTML = `<h2> sunrise ${sunriseDate} </h2>`
     
         let sunset = json.city.sunset;
-        let sunsetDate = new Date(sunset*1000).toLocaleString().split(", ").slice(0).join(", ") //get the slice-method to work
+        let sunsetDate = new Date(sunset*1000).toLocaleTimeString()
 
-        containerSunset.innerHTML = `<h2> Sunset: ${sunsetDate} </h2>`
+        containerSunset.innerHTML = `<h2> sunset ${sunsetDate} </h2>`
 
       const changeBackground = (() => {
         if (currentWeather === "Clouds") {
-            header.style.backgroundImage = `url(https://images.pexels.com/photos/96622/pexels-photo-96622.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1)`
+            header.style.backgroundImage = `url(https://images.pexels.com/photos/414659/pexels-photo-414659.jpeg?auto=compress&cs=tinysrgb&w=800)`
         
         } else if (currentWeather === "Rain") {
-            header.style.backgroundImage = `url(https://images.pexels.com/photos/96622/pexels-photo-96622.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1)`
+            header.style.backgroundImage = `url(https://images.pexels.com/photos/2259232/pexels-photo-2259232.jpeg?auto=compress&cs=tinysrgb&w=800)`
         
         } else {
             header.style.backgroundImage = `url(https://images.pexels.com/photos/96622/pexels-photo-96622.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1)`
@@ -57,8 +57,6 @@ const containerSunset = document.getElementById("sunset")
     })
 
     
-
-
 //Fetching the API for the weekly forecast
 fetch('https://api.openweathermap.org/data/2.5/forecast?q=Stockholm,Sweden&units=metric&APPID=98bd2fbedad0f13ae05ed8e49698fda1')
     .then((response) => {
