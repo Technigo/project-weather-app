7455bdb9424b4624b33c3c9c6d8b830d9963ad15
jessnmo <jessicamo92@gmail.com>
2022-09-10 18:14:26 +0200

    last commit

--- script.js
+++ script.js
@@ -4,9 +4,9 @@ const todaysWeather = document.getElementById("todaysWeather")
 const message = document.getElementById("message")
 const weatherForecast = document.getElementById("weather-forecast")
 const searchDisplay = document.getElementById("search-display");
-//const forecastMessage = document.getElementById("id");
 const cityName = document.getElementById("city-name");
 const searchBtn = document.getElementById("search-button");
+const bodyContainer = document.querySelector(".body-container");
 
  //trying fetch to see if connection between HTML is working, we can choose another API later
 //--------DATA FETCH FROM API FOR TODAY'S FORECAST-----------
@@ -43,28 +43,24 @@ fetch ('https://api.openweathermap.org/data/2.5/weather?q=Stockholm,Sweden&units
     const dailyMessage = () => {
         if(weather === "Clear") {
             icon.src = "./Designs/Design-2/icons/noun_Sunglasses_2055147.svg"
-            message.innerHTML =  `
-                <h1> Get your sunnies on. Stockholm is looking rather great today! </h1>
-        `}
+            message.innerHTML =  `<h1> Get your sunnies on. Stockholm is looking rather great today! </h1>`
+            bodyContainer.classList.add("clear");
+        }
         else if (weather === "Rain") {
             icon.src = "./Designs/Design-2/icons/noun_Umbrella_2030530.svg"
-            message.innerHTML = `
-                <h2> Don't forger your umbrella! Stockholm is wet today! </h2>`
-        }
-        else {
+            message.innerHTML = `<h1> Don't forget your umbrella! Stockholm is wet today! </h1>`
+            bodyContainer.classList.add('rain');
+        } else {
             icon.src = "./Designs/Design-2/icons/noun_Cloud_1188486.svg"
-            message.innerHTML =  `
-                <h2> Light a fire and get cosy. Stockholm is looking great today. </h2>`
-        }
+            message.innerHTML =  `<h1> Light a fire and get cosy. Stockholm is looking grey today. </h1>`
+            bodyContainer.classList.add('clouds');
+        } 
         //console.log("message", dailyMessage)
     }
      dailyMessage()
 });
 
 
-
-
-let weeklyTemp;
 
 /*----- dynamic city input TO CHECK------
 //the city name + error display when input a city name 
