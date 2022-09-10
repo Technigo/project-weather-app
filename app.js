1d8214ddc8ff6fa792cd0c3d7294cb00c7662b6d
Agnes Ahlman <ahlman.agnes@gmail.com>
2022-09-10 11:48:30 +0200

    some css

--- app.js
+++ app.js
@@ -2,27 +2,31 @@ const apiKey = '8ba8157c8f9c786166631ade41fce81c';
 const container = document.getElementById('container');
 const forecast = document.getElementById('forecast');
 const btnSearchCity = document.getElementById('btn-searchCity');
-const allInfo= document.querySelector('allInfo');
-const cityName= document.getElementById('city')
-const degree= document.getElementById('degree')
-const weather= document.getElementById('weather')
-const sunrise= document.getElementById('sunrise')
-const sunset = document.getElementById('sunset')
-const search= document.getElementById('search')
-const timeInBackground = document.getElementById('time')
+const allInfo = document.querySelector('allInfo');
+const cityName = document.getElementById('city');
+const degree = document.getElementById('degree');
+const weather = document.getElementById('weather');
+const sunrise = document.getElementById('sunrise');
+const sunset = document.getElementById('sunset');
+const search = document.getElementById('search');
+const timeInBackground = document.getElementById('time');
 
-const day= document.getElementById('day')
-const navBar= document.getElementById('navBar');
+const day = document.getElementById('day');
+const navBar = document.getElementById('navBar');
 
-const date= new Date()
-let now = new Date().toLocaleDateString('en-us', { weekday: "long", month: "long", day: "numeric" });
-console.log(now)
+const date = new Date();
+let now = new Date().toLocaleDateString('en-us', {
+  weekday: 'long',
+  month: 'long',
+  day: 'numeric',
+});
+console.log(now);
 
-const time = date.getHours() + ":" + date.getMinutes();
-console.log(time)
+const time = date.toLocaleTimeString().substring(0, 5); //date.getHours() + ':' + date.getMinutes();
+console.log(time);
 
 day.innerHTML = `Today is: ${now}`;
-timeInBackground.innerHTML = time
+timeInBackground.innerHTML = time;
 
 btnSearchCity.addEventListener('click', () => {
   const city = document.getElementById('search').value;
@@ -34,43 +38,46 @@ btnSearchCity.addEventListener('click', () => {
     })
     .then(function (data) {
       // hour.innerHTML=time;
-      cityName.innerHTML= data.city.name;
-      degree.innerHTML = `<h2>${Math.round(data.list[0].main.temp.toFixed(1))} ${'&#8451;'}</h2>`
+      cityName.innerHTML = data.city.name;
+      degree.innerHTML = `<h2>${Math.round(
+        data.list[0].main.temp.toFixed(1)
+      )} ${'&#8451;'}</h2>`;
 
       weather.innerHTML = data.list[0].weather[0].main;
-      sunrise.innerHTML = new Date(data.city.sunrise * 1000).toLocaleTimeString();
+      sunrise.innerHTML = new Date(
+        data.city.sunrise * 1000
+      ).toLocaleTimeString();
       sunset.innerHTML = new Date(data.city.sunset * 1000).toLocaleTimeString();
 
-      search.value = " ";
-   
-    const weatherAppearance = data.list[0].weather[0].main
-    const background = document.querySelector('.wrapper')
-    const appBackgroud= () => {
-    background.style.backgroundSize = "cover";
-    background.style.height = "700px";
-    background.style.repeat = "no-repeat";
-    if(weatherAppearance ==='Rain'){
-      console.log('It is raining')
-      background.style.backgroundImage = "url('https://st.depositphotos.com/1013195/1395/i/450/depositphotos_13958901-stock-photo-rainy-day.jpg') ";
-    }
-    else if(weatherAppearance === 'Clear'){
-      background.style.backgroundImage = "url('https://wallpaperaccess.com/sunny-day')";
-    }
-    else if(weatherAppearance === 'Clouds'){
-      background.style.backgroundImage = "url('https://images.unsplash.com/photo-1419833173245-f59e1b93f9ee?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxzZWFyY2h8MTF8fGNvbHVkeSUyMHNreXxlbnwwfHwwfHw%3D&auto=format&fit=crop&w=500&q=60')"
-    }
-    else if (weatherAppearance === 'Snow'){
-      background.style.backgroundImage = "url('https://cdn.pixabay.com/photo/2014/12/02/22/05/snowflakes-554635__340.jpg')"
-    }
+      search.value = ' ';
 
-    }
-    appBackgroud()
+      const weatherAppearance = data.list[0].weather[0].main;
+      const background = document.querySelector('.wrapper');
+      const appBackgroud = () => {
+        background.style.backgroundSize = 'cover';
+        background.style.height = '1200px';
+        background.style.repeat = 'no-repeat';
+        if (weatherAppearance === 'Rain') {
+          console.log('It is raining');
+          background.style.backgroundImage =
+            "url('https://st.depositphotos.com/1013195/1395/i/450/depositphotos_13958901-stock-photo-rainy-day.jpg') ";
+        } else if (weatherAppearance === 'Clear') {
+          background.style.backgroundImage =
+            "url('https://wallpaperaccess.com/sunny-day')";
+        } else if (weatherAppearance === 'Clouds') {
+          background.style.backgroundImage =
+            "url('https://images.unsplash.com/photo-1419833173245-f59e1b93f9ee?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxzZWFyY2h8MTF8fGNvbHVkeSUyMHNreXxlbnwwfHwwfHw%3D&auto=format&fit=crop&w=500&q=60')";
+        } else if (weatherAppearance === 'Snow') {
+          background.style.backgroundImage =
+            "url('https://cdn.pixabay.com/photo/2014/12/02/22/05/snowflakes-554635__340.jpg')";
+        }
+      };
+      appBackgroud();
 
-       btnSearchCity.value = "  ";
+      btnSearchCity.value = '  ';
 
       // 5 days forecast
       const filteredForecast = data.list.filter((item) => item.dt);
-
       const filteredTemp = data.list.filter((item) =>
         item.dt_txt.includes('12:00')
       );
@@ -103,9 +110,7 @@ btnSearchCity.addEventListener('click', () => {
         `;
       });
     });
-
-    })
-})
+});
 
 // Switch to get the icons images to the right condition
 
