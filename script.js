0d80fd9bc19518336798924c2a48f657eae673dc
Jessika Lind <jessika@MacBook-Air-som-tillhor-Jessika.local>
2022-09-11 14:55:10 +0200

    additional changes

--- script.js
+++ script.js
@@ -13,9 +13,6 @@ const containerSunrise = document.getElementById("sunrise")
 const containerSunset = document.getElementById("sunset")
 const menuBtn = document.getElementById("menuIcon")
 
-
-
-
     fetch('https://api.openweathermap.org/data/2.5/forecast?q=Stockholm,Sweden&units=metric&APPID=98bd2fbedad0f13ae05ed8e49698fda1')
     .then((response) => {
         return response.json()
@@ -47,10 +44,10 @@ const menuBtn = document.getElementById("menuIcon")
 
       const changeBackground = (() => {
         if (currentWeather === "Clouds") {
-            header.style.backgroundImage = `url(https://images.pexels.com/photos/209845/pexels-photo-209845.jpeg?auto=compress&cs=tinysrgb&w=1500)`
+            header.style.backgroundImage = `url(https://images.pexels.com/photos/209845/pexels-photo-209845.jpeg?auto=compress&cs=tinysrgb&w=1700)`
         
         } else if (currentWeather === "Rain") {
-            header.style.backgroundImage = `url(https://images.pexels.com/photos/2259232/pexels-photo-2259232.jpeg?auto=compress&cs=tinysrgb&w=1500)`
+            header.style.backgroundImage = `url(https://images.pexels.com/photos/2259232/pexels-photo-2259232.jpeg?auto=compress&cs=tinysrgb&w=1700)`
         
         } else {
             header.style.backgroundImage = `url(https://images.pexels.com/photos/96622/pexels-photo-96622.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1)`
@@ -81,7 +78,7 @@ fetch('https://api.openweathermap.org/data/2.5/forecast?q=Stockholm,Sweden&units
         const feelsLike = filteredForecast[i].main.feels_like.toFixed(0)
         let day
           day = new Date(filteredForecast[i].dt * 1000).toLocaleDateString("en-US", {weekday: 'short'})
-          //console.log(day)
+          console.log(day)
         
         weatherWeek.innerHTML += 
             `<div class = "weekday id="weekday">
