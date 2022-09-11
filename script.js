7579f1638606fb51a94fded2c8b10f25444bd747
Linnea Ajger <linnea.ajger@gmail.com>
2022-09-11 22:37:38 +0200

    updated https link

--- script.js
+++ script.js
@@ -7,7 +7,7 @@ const weekdays = ["sun","mon","tue","wed","thu","fri","sat"];
 
 let containerToggle = document.querySelector('.container')
 
-fetch("http://api.openweathermap.org/data/2.5/weather?q=Stockholm,Sweden&units=metric&APPID=fb125bc213d8ee5c4a432b3a2b24aecf")
+fetch("https://api.openweathermap.org/data/2.5/weather?q=Stockholm,Sweden&units=metric&APPID=fb125bc213d8ee5c4a432b3a2b24aecf")
     .then((response) => {
         return response.json()
     })
