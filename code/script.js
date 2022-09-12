b165deea2b69ad4f8a06f650ffff72347e442765
a-well <amandaemkwellington@gmail.com>
2022-09-12 09:35:38 +0200

    Fix mobile height

--- code/script.js
+++ code/script.js
@@ -238,3 +238,8 @@ document.onkeydown = function (evt) {
     close();
   }
 };
+
+// First we get the viewport height and we multiple it by 1% to get a value for a vh unit
+let vh = window.innerHeight * 0.01;
+// Then we set the value in the --vh custom property to the root of the document
+document.documentElement.style.setProperty("--vh", `${vh}px`);
