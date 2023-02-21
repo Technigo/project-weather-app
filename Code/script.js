d2dc706276e66697022c771560898303b1953ffe
fannystenberg <ffannystenberg@gmail.com>
2023-02-21 15:33:05 +0100

    fixed script link + added basic css to see layout

--- Code/script.js
+++ Code/script.js
@@ -1,10 +1,15 @@
- const container = document.querySelector('.weather-prognosis')
+ // All the DOM selectors stored as variables
+ const header = document.getElementById("head")
+ const aboutWeather = document.querySelector(".about-weather")
+ const weatherProg = document.querySelector(".weather-prog")
 
+ // Fetching the data from the API
  fetch("https://api.openweathermap.org/data/2.5/weather?q=Stockholm,Sweden&units=metric&APPID=8d66acab5dd718723a370e1b64f22f8c")
   .then((response) => {
      console.log(response);
      return response.json();
-  })
+   })
   .then ((data) => {
      console.log(data);
-  });
+     return data;
+   });
