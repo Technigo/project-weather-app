df16f0973e333bded2b16a4e814b1112a5997753
Vio Szabo <vio@vioszabo.se>
2023-02-21 11:57:37 +0100

    Co-authored-by: Malin Skill <MalinSkill@users.noreply.github.com>

---
+++ script.js
@@ -0,0 +1,16 @@
+const topSection = document.getElementById('topSection')
+const main = document.getElementById('main')
+const degrees = document.getElementById('degrees')
+const city = document.getElementById('city')
+const condition = document.getElementById('condition')
+
+fetch('https://api.openweathermap.org/data/2.5/weather?q=Reykjavik&appid=fa2755c779ce094fc80f2fa365eea704&units=metric')
+.then((response) => {
+    return response.json()
+})
+.then((json) => {
+    degrees.innerHTML = `${json.main.temp.toFixed(1)}°C`;
+    city.innerHTML = json.name;
+    condition.innerHTML = json.weather[0].description;
+    })
+    console.log(json)
