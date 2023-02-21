ad1a8086a0fa5e8c74add1055c5bb5e2d95f7a5d
Malin <malin@MacBook-Pro.local>
2023-02-21 13:38:43 +0100

    added dom selector for sunriseSunset and button

--- script.js
+++ script.js
@@ -3,6 +3,8 @@ const main = document.getElementById('main')
 const degrees = document.getElementById('degrees')
 const city = document.getElementById('city')
 const condition = document.getElementById('condition')
+const sunriseSunset = document.getElementById('sunriseSunset')
+const button = document.getElementById('button')
 
 fetch('https://api.openweathermap.org/data/2.5/weather?q=Reykjavik&appid=fa2755c779ce094fc80f2fa365eea704&units=metric')
 .then((response) => {
@@ -14,3 +16,41 @@ fetch('https://api.openweathermap.org/data/2.5/weather?q=Reykjavik&appid=fa2755c
     condition.innerHTML = json.weather[0].description;
     })
     console.log(json)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
