fc55b0a6fe038b2fe1d80cd5d02e0288fef0c801
YlvaKarlsson <ylva_87@hotmail.com>
2023-02-21 17:15:48 +0100

    Added some comments to help from todays Live Session

--- script.js
+++ script.js
@@ -10,10 +10,15 @@ const sunset = document.getElementById('sunset')
 
 
 // This is the API key for OpenWeatherMap.org - Stockholm (YK)
+// From Live-session: STEP 1/fetch "I have a present for you to fetch" (YK)
+//.then = STEP 2 "I'm giving you the present but you don't know what's inside yet" (YK)
+//.then = STEP 3 "I'm giving you the present so now you can take a look what's inside it" (YK)
 fetch('https://api.openweathermap.org/data/2.5/weather?q=Stockholm,Sweden&units=metric&APPID=bcc357d81ce23673e3a8e92322d840f2')
+    //.then = Step 2 "I'm giving you the present but you don't know what's inside yet" (YK)
     .then((response) => {
         return response.json()
     })
+    //.then = Step 3 "I'm giving you the present so now you can take a look what's inside it" (YK)
     .then((json) => {
         weatherContainer.innerHTML = `<h1>This is the weather in ${json.name}</h1>`
         console.log(json)
