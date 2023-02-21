85d824a3a8c27d3078f28621778c2e9c17227659
malvinamaria <mmpolak@protonmail.com>
2023-02-21 10:58:49 +0100

    fetching some data

--- script.js
+++ script.js
@@ -1,6 +1,14 @@
 const container = document.getElementById('card')
 
-fetch('http://api.openweathermap.org/data/2.5/weather?q=Stockholm,Sweden&units=metric&APPID=ddf98774bfc0041a16a7d95948e68934')
+fetch("https://api.openweathermap.org/data/2.5/weather?q=Stockholm,Sweden&units=metric&APPID=14156af54d10ce159e1b521416adb87f")
     .then((response)=> {
         return response.json()
+    })
+    .then((json) => {
+        console.log(json)
+        container.innerHTML = `
+        <h1> Check the weather in ${json.name} </h1>
+        <h2> The visibility is ${json.visibility} </h2>
+        <h3> The temperature is ${json.main.temp}</h3>
+        `
     })
