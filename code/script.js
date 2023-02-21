8b75b258582e560eafbe5dc3e7511dd15cd3f253
Nora Terbocs <nora.terbocs@gmail.com>
2023-02-21 13:46:01 +0100

    Fetched forecast data

--- code/script.js
+++ code/script.js
@@ -1,24 +1,38 @@
 const currentWeather = document.getElementById("current-weather");
 const forecast = document.getElementById("forecast");
 
-const getWeatherData = () => {
+// current weather details
+const getCurrentWeatherData = () => {
   fetch(
     `http://api.openweathermap.org/data/2.5/weather?q=Stockholm,Sweden&units=metric&APPID=f60c361b4571fb70c85f29bbd856c13f`
   )
     .then((response) => {
       return response.json();
     })
-    .then((json) => {
-      console.log(json);
-
-      // current weather details
-      document.getElementById("temperature").textContent = json.main.temp;
-      document.getElementById("city").textContent = json.name;
+    .then((data) => {
+      console.log(data);
+      document.getElementById("temperature").textContent = data.main.temp;
+      document.getElementById("city").textContent = data.name;
       document.getElementById("weather-type").textContent =
-        json.weather[0].description;
-      document.getElementById("sunrise").textContent = json.sys.sunrise;
-      document.getElementById("sunset").textContent = json.sys.sunset;
+        data.weather[0].description;
+      document.getElementById("sunrise").textContent = data.sys.sunrise;
+      document.getElementById("sunset").textContent = data.sys.sunset;
     });
 };
 
-getWeatherData();
+getCurrentWeatherData();
+
+//forecast
+const getForecastWeatherData = () => {
+  fetch(
+    `https://api.openweathermap.org/data/2.5/forecast?q=Stockholm,Sweden&units=metric&appid=f60c361b4571fb70c85f29bbd856c13f`
+  )
+    .then((response) => {
+      return response.json();
+    })
+    .then((data) => {
+      console.log(data);
+    });
+};
+
+getForecastWeatherData();
