41b86487e321c9e57ba1d0b4dda964633bf088de
AntoniaGranit <antonia.granit@gmail.com>
2023-02-21 16:12:57 +0200

    added DOM element selectors

--- script.js
+++ script.js
@@ -1,4 +1,10 @@
 // http://api.openweathermap.org/data/2.5/weather?q=Stockholm,Sweden&units=metric&APPID=775b6d69e24fcb088a070bee66d05057
+const weatherHeader = document.getElementById("weather-header");
+const mainSection = document.getElementById("main-section");
+const weatherIcon = document.getElementById("weather-icon");
+const weatherText = document.getElementById("weather-text");
+const weekdays = document.getElementById("weekdays");
+const temperature = document.getElementById("temperature");
 
 fetch(
   "http://api.openweathermap.org/data/2.5/weather?q=Stockholm,Sweden&units=metric&APPID=775b6d69e24fcb088a070bee66d05057"
