f8d34c4af4d94b83269ca38b8533ba399268a03b
Emilia Saberski <emilia@saberski.com>
2023-02-22 12:34:08 +0100

    made if-statements depending on weather

--- script.js
+++ script.js
@@ -8,16 +8,43 @@ fetch ('https://api.openweathermap.org/data/2.5/weather?q=Stockholm,Sweden&units
 
 .then ((data) => {
     console.log (data)
-    weatherWrapper.innerHTML += `
-    <div id="city-name" class="city-name"> ${data.name}</div>`
+   
+ /*    weatherWrapper.innerHTML += `
+    <div id="city-name" class="city-name"> ${data.name}</div>` */
 
     data.weather.forEach((weatherDescription) => { //.weather comes from the API - it is an object name
-        weatherWrapper.innerHTML +=
-        `${weatherDescription.description}`
+    
+    if (weatherDescription.main === 'Clear') {
+        weatherWrapper.innerHTML = `
+        <img src="./img/sunglasses.svg" alt="sun-glasses icon"></img>
+        <p> Get your sunnies on. ${data.name} is looking rather great today.
+        </p>`;
+    } 
+    else if (weatherDescription.main === 'Clouds') {
+        weatherWrapper.innerHTML = `
+        <img src="./img/cloud.svg" alt="cloud icon"></img>
+        <p> Light a fire and get cosy. ${data.name} looks grey today.
+        </p>`;
+    } 
+    else if (weatherDescription.main === 'Rain' | "Drizzle") {
+        weatherWrapper.innerHTML = `
+        <img src="./img/rain.svg" alt="rain icon"></img>
+        <p> Don’t forget your umbrella. It’s wet in ${data.name} today.
+        </p>`;
+    } 
+    else if (weatherDescription.main === 'Snow') {
+        weatherWrapper.innerHTML = `
+        <img src="./img/snow.svg" alt="snow icon"></img>
+        <p> Light a fire and get cosy. ${data.name} looks snowy today.
+        </p>`;
+    } 
+    else {
+        weatherWrapper.innerHTML =
+        `<p> Go to the window and find out! </p>`
+    }
     })
     const roundedTemp = Math.round(data.main.temp * 10)/10;
     //console.log(roundedTemp)
     weatherWrapper.innerHTML +=
     `<div id='main-temp' class='main-temp'>${roundedTemp} Celcius</div>`
-
 })
