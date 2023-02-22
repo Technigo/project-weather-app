ec88c2fda57be247447a77561358340a838abe47
camcron <camillacronqvist@hotmail.com>
2023-02-22 21:49:36 +0100

    Added Javascript to show different info from the API's

--- script.js
+++ script.js
@@ -1,26 +1,64 @@
-const container = document.getElementById('weatherOne')
+const container = document.getElementById('weatherMain')
 
-fetch('https://api.openweathermap.org/data/2.5/weather?q=sydney,&units=metric&APPID=156328eec9b7853e6ecd35c030202c4c') //Change to our weather API-key
+
+fetch('https://api.openweathermap.org/data/2.5/weather?q=Sydney,AU&units=metric&APPID=156328eec9b7853e6ecd35c030202c4c')
     .then((response) => {
         console.log(response)
         return response.json()
     })
+
+
+// Name of the city showing
     .then((json) => { 
-        const tempDecimal = json.main.temp.toFixed(1) //Change to (0) for no decimals
-            container.innerHTML = `<p>${json.name} is the city</p>`
-            container.innerHTML += `<p>${tempDecimal} °C is the temperature </p>`
-            container.innerHTML += `<p>${json.weather[0].description} is the weather </p>`
+        container.innerHTML = `<p>${json.name}</p>`
+
+
+
+// Current temperature shown in Celsius with one decimal
+        const tempFormatted = json.main.temp.toFixed(1) //Change to (0) for no decimals
+            container.innerHTML += `<p>The temperature is ${tempFormatted} °C</p>`
+
+
+
+// Current weather description
+        container.innerHTML += `<p>Current weather is ${json.weather[0].description}</p>`
+
+
+
+// Sunset and sunrise (in local time Sydney, Australia)
+        const sunrise = json.sys.sunrise;
+        let sunriseTime = new Date(sunrise*1000).toLocaleTimeString('sv-SE', {hour: '2-digit', minute: '2-digit', timeZone: 'Australia/Sydney'});//This code shows the time as SE-time (ex 08:40/22:40)
+     
+            container.innerHTML += `<p>
+            <img src="/icons8-sunrise-64.png" class="logo-sunrise" alt="Logo of sunrise">
+            The sun rises at ${sunriseTime}
+            </p>`
+
+
+        const sunset = json.sys.sunset;
+        let sunsetTime = new Date(sunset*1000).toLocaleTimeString('eng-US', {hour: '2-digit', minute: '2-digit',timeZone: 'Australia/Sydney'});////This code shows the time as eng-US (ex 08:40 AM/08:40 PM)
+            container.innerHTML += `<p>
+            <img src="/icons8-sunset-64.png" class="logo-sunset" alt="Logo of sunset">
+            The sun sets at ${sunsetTime}
+            </p>`
+    });
+
+
+
+// Forecast for the next 5 days
+fetch('https://api.openweathermap.org/data/2.5/forecast?q=Sydney,Australia&units=metric&appid=156328eec9b7853e6ecd35c030202c4c')
+    .then((response) => {
+        console.log(response)
+        return response.json()
     })
-    /*
-     Take a look at this, should the getElementById be here?
-     container.innerHTML = `<h1>There are ${json.number} people in space right now</h1>`
 
-     json.weather.forEach((person) => {
-       container.innerHTML += `<p>${person.name} is on the ${person.craft}</p>`
-     })
-     */
+    .then((json) => {
+        const filteredForecast = json.list.filter((item) => item.dt_txt.includes('12:00'))
+        console.log(filteredForecast)
 
-/*
-     json.name((weather) => {
-        container.innerHTML += `<p>${json.name} is the name of the city</p>`
-        console.log(json)*/
+        filteredForecast.forEach((dailyForecast) => {
+            const forecastDay = new Date(dailyForecast.dt*1000);
+            const day = new Intl.DateTimeFormat('en-US', { weekday: 'long' }).format(forecastDay); //This changes the index of the date to the day of the week
+                container.innerHTML += `<p>The weather on ${day} is ${dailyForecast.weather[0].description}</p>`
+        });
+    });
