f29e4c5359ccdfcee8d0f5e9bef3fb64a8199297
mvfrid <104832335+mvfrid@users.noreply.github.com01>
2023-02-22 14:58:51 +0100

    got the sunrise and sunset from unix to date

--- script.js
+++ script.js
@@ -11,10 +11,20 @@ fetch('https://api.openweathermap.org/data/2.5/weather?q=Stockholm&units=metric&
         console.log(round)
 
         console.log(json.weather[0].description)
-        
+
+        const sunriseUnix = json.sys.sunrise;                   // Unix timestamp
+        const sunriseTime = new Date(sunriseUnix * 1000);       //Gives us the time in "human" form (as a date), mult. by 1000 to get it in ms.
+        const sunriseShort = sunriseTime.toLocaleTimeString([], { timeStyle: 'short' });        //Transforms it into just the Hour/minutes and AM/PM. Select the short variant to get the time with minutes and not seconds.
+        const sunsetTime = new Date(json.sys.sunset * 1000);
+        const sunsetShort = sunsetTime.toLocaleTimeString([], { timeStyle: 'short' }); 
+
         testy.innerHTML = `<p>City: ${json.name}</p>`;
         testy.innerHTML += `<p>Temperature: ${round} °C</p>`;
         testy.innerHTML += `<p>Weather: ${json.weather[0].description}</p>`;
+
+
+        testy.innerHTML += `<p>sunrise: ${sunriseShort}</p>`;
+        testy.innerHTML += `<p>sunset: ${sunsetShort}</p>`;
     })
     .catch((err) => {
         console.log(`error caught:`, err)
