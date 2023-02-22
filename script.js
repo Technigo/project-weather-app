86d44832e457f174648d0600aeb8ee76bc6f9e26
Annika Lindberg <annika.edit.lindberg@gmail.com>
2023-02-22 11:11:12 +0100

    added gifs in folder

---
+++ script.js
@@ -0,0 +1,11 @@
+// Matildas Key
+"https://api.openweathermap.org/data/2.5/weather?q=Stockholm,Sweden&units=metric&APPID=7309e4a5829fafe809df835ad95f18ea";
+
+// Annikas Key:
+"http://api.openweathermap.org/data/2.5/weather?q=Stockholm,Sweden&units=metric&APPID=d37e49016232d41da09ab7080df2faa7";
+
+const container document.getElementById('container')
+const headerWeather document.getElementById('header-weather')
+const symbolImage document.getElementById('symbol')
+const weatherText document.getElementById('weatherText')
+const forecast document.getElementById('forecastContainer')
