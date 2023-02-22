55dfdf80b5fa0ef9ee3651f9b9deb370b6e8d310
mvfrid <104832335+mvfrid@users.noreply.github.com01>
2023-02-22 20:59:39 +0100

    some fixes

--- script.js
+++ script.js
@@ -1,5 +1,16 @@
 const testy = document.getElementById('testy');
+const sunriseText = document.getElementById('sunriseText');
+const sunsetText = document.getElementById('sunsetText');
 
+
+//Variables we can use later to automate API-fethcing:
+const apiKey = 'c480de5f69ca98d1993a4dae3213642e';
+let city = 'Stockholm';
+// Use: `https://api.openweathermap.org/data/2.5/weather?q=${city}&units=metric&APPID=${apiKey}`
+
+
+
+//Our testing fetch:
 fetch('https://api.openweathermap.org/data/2.5/weather?q=Stockholm&units=metric&APPID=c480de5f69ca98d1993a4dae3213642e')
     .then((response) => {
         return response.json()
@@ -7,27 +18,43 @@ fetch('https://api.openweathermap.org/data/2.5/weather?q=Stockholm&units=metric&
     .then((json) => {
         console.log(`json:`, json)
 
+        // Store the rounded number in a variable called "round"
         let round = Math.round(json.main.temp * 10 ) / 10;
-        console.log(round)
-
-        console.log(json.weather[0].description)
-
-        const sunriseUnix = json.sys.sunrise;                   // Unix timestamp
-        const sunriseTime = new Date(sunriseUnix * 1000);       //Gives us the time in "human" form (as a date), mult. by 1000 to get it in ms.
-        const sunriseShort = sunriseTime.toLocaleTimeString([], { timeStyle: 'short' });        //Transforms it into just the Hour/minutes and AM/PM. Select the short variant to get the time with minutes and not seconds.
-        const sunsetTime = new Date(json.sys.sunset * 1000);
-        const sunsetShort = sunsetTime.toLocaleTimeString([], { timeStyle: 'short' }); 
 
         testy.innerHTML = `<p>City: ${json.name}</p>`;
         testy.innerHTML += `<p>Temperature: ${round} °C</p>`;
         testy.innerHTML += `<p>Weather: ${json.weather[0].description}</p>`;
-
-
-        testy.innerHTML += `<p>sunrise: ${sunriseShort}</p>`;
-        testy.innerHTML += `<p>sunset: ${sunsetShort}</p>`;
     })
     .catch((err) => {
         console.log(`error caught:`, err)
     })
 
    
+
+
+    const getSunriseSunsetData = () => {
+        fetch(`https://api.openweathermap.org/data/2.5/weather?q=${city}&units=metric&APPID=${apiKey}`)
+        .then((response) => {
+            return response.json()
+        })
+        .then((json) => {
+            //console.log(`json:`, json)
+
+            //const sunriseUnix = json.sys.sunrise;                   // Unix timestamp
+            const sunriseTime = new Date(json.sys.sunrise * 1000);       //Gives us the time in "human" form (as a date), mult. by 1000 to get it in ms.
+            const sunriseShort = sunriseTime.toLocaleTimeString([], { timeStyle: 'short' }).replace("AM", "").replace("PM", "");        //Transforms it into just the Hour/minutes and AM/PM. Select the short variant to get the time with minutes and not seconds.
+            const sunsetTime = new Date(json.sys.sunset * 1000);
+            const sunsetShort = sunsetTime.toLocaleTimeString([], { timeStyle: 'short' }).replace("AM", "").replace("PM", "");          //Tar bort AM och PM, kolla om det finns bättre sätt.
+            
+            //Modifying the HTML based on our input:
+            sunriseText.innerHTML += `<p>sunrise</p>
+                                        <p class="time-data">${sunriseShort}</p>`;
+            sunsetText.innerHTML += `<p>sunset</p>
+                                    <p class="time-data">${sunsetShort}</p>`;
+        })
+        .catch((err) => {
+            console.log(`error caught:`, err)
+        })
+    }
+
+    getSunriseSunsetData();
