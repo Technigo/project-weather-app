315a579eca3bdc53483268f179291de5f1ced54e
Andreas Axelsson <pererikandreas@gmail.com>
2023-02-22 10:59:26 +0100

    fist add HTML,CSS,DOMSELECTION

---
+++ script.js
@@ -0,0 +1,8 @@
+//////////////////////// DOM Selection /////////////////////////////
+
+const forcastRightNow = document.getElementById('forcastRightNow')
+const casualWeatherBox = document.getElementById('casualWeatherBox')
+const weekdays = document.getElementById('weekdays')
+
+////////////////////////////////////////////////////////////////////
+
