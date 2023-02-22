4e831ea03f2f396a31fd55ce0f3d633e529f51c7
Vera Sjunnesson <Vera@admins-mbp.home>
2023-02-22 12:38:41 +0100

    Filtered data from weather api for weather forecast functions

--- script.js
+++ script.js
@@ -1,4 +1,11 @@
 const testy = document.getElementById('testy');
+const weatherForecast = document.getElementById('weatherForecast')
+const forecastWeekdays = document.getElementById('forecastWeekdays')
+const forecastDescription = document.getElementById('forecastDescription')
+const forecastTemp = document.getElementById('forecastTemp')
+const forecastFeelsLike = document.getElementById('forecastFeelsLike')
+
+
 
 fetch('https://api.openweathermap.org/data/2.5/weather?q=Stockholm&units=metric&APPID=c480de5f69ca98d1993a4dae3213642e')
     .then((response) => {
@@ -20,4 +27,32 @@ fetch('https://api.openweathermap.org/data/2.5/weather?q=Stockholm&units=metric&
         console.log(`error caught:`, err)
     })
 
-   
+
+const weatherForecastData = () => {
+    fetch('https://api.openweathermap.org/data/2.5/forecast?q=Stockholm,Sweden&units=metric&APPID=c480de5f69ca98d1993a4dae3213642e')
+        .then((forecastResponse) => {
+            return forecastResponse.json();
+        })
+        .then((result) => {
+            const todaysDate = new Date().toString().split(' ')[0]; //Todays date ine text form
+            console.log(todaysDate)
+            
+            const filterData = result.list.filter(weatherDay => weatherDay.dt_txt.includes('12:00')); //Filters out the data at 12:00 every day
+            console.log(filterData)
+            
+            filterData.forEach(date => { 
+            const weekDay = new Date(date.dt * 1000).toString().split(' ')[0]; //All the five days dates' convertet from numbers to text
+                if (weekDay !== todaysDate) {
+                    forecastWeekdays.innerHTML += `<p>${weekDay}</p>`
+                    forecastDescription.innerHTML += `<p>${date.weather[0].description}</p>`
+                    forecastTemp.innerHTML += `<p>${date.main.temp.toFixed(0)}°</p>`
+                    forecastFeelsLike.innerHTML += `<p>${date.main.feels_like.toFixed(0)}°</p>`
+                }
+            }); 
+        })
+}
+
+weatherForecastData();
+ 
+
+
