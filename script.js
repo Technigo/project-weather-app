4954d525fe8a19a8f47c75d17266f72cad51f9dc
Annika Lindberg <annika.edit.lindberg@gmail.com>
2023-02-22 16:22:57 +0100

    added 5day forecast and todays weather in header

--- script.js
+++ script.js
@@ -1,38 +1,95 @@
-// Matildas Key
-"https://api.openweathermap.org/data/2.5/weather?q=Stockholm,Sweden&units=metric&APPID=7309e4a5829fafe809df835ad95f18ea";
-
 // Annikas Key:
 "http://api.openweathermap.org/data/2.5/weather?q=Stockholm,Sweden&units=metric&APPID=d37e49016232d41da09ab7080df2faa7";
 
 const main = document.getElementById("main");
-const todaysForecast = document.getElementById("todaysForecast");
+const todaysHeaderForecast = document.getElementById("todaysHeaderForecast");
 const symbolImage = document.getElementById("symbol");
-const weatherText = document.getElementById("weatherText");
+const textForecast = document.getElementById("textForecast");
 const weeklyForecast = document.getElementById("weeklyForecast");
 
+// This makes the URL easier to read in the code
 let urlWeather =
-  "http://api.openweathermap.org/data/2.5/weather?q=Stockholm,Sweden&units=metric&APPID=d37e49016232d41da09ab7080df2faa7";
+  "http://api.openweathermap.org/data/2.5/weather?q=Kiruna,Sweden&units=metric&APPID=d37e49016232d41da09ab7080df2faa7";
 let urlForecast =
-  "https://api.openweathermap.org/data/2.5/forecast?q=Stockholm,Sweden&units=metric&APPID=d37e49016232d41da09ab7080df2faa7";
+  "https://api.openweathermap.org/data/2.5/forecast?q=Kiruna,Sweden&units=metric&APPID=d37e49016232d41da09ab7080df2faa7";
 
-//this guy makes the URL easier to read
 fetch(urlWeather)
-  // Fetches the data from the API into our dayly weather description
+  // Fetches the data from the API into our daily weather description
   .then((response) => {
     return response.json();
   })
-  .then((data) => {
-    console.log(data.weather[0].description);
+  .then((wData) => {
+    //console.log(wData.weather[0].description);
 
-    todaysForecast.innerHTML = `<p>The weather is ${
-      data.weather[0].description
-    } in ${data.name}. 
-    The current temperature is ${Math.round(data.main.temp)}</p>
-    <p> Sunrise: ${data.sys.sunrise}</p>`;
+    // Using math.round in the template literal for temperature to make it an equal number.
+    todaysHeaderForecast.innerHTML = `<p>The weather is ${
+      wData.weather[0].description
+    } in ${wData.name}.</p> <p>
+    The current temperature is ${Math.round(
+      wData.main.temp
+    )}</p> <p> Feels like ${Math.round(wData.main.feels_like)}</p>
+    <p>Sunrise: ${wData.sys.sunrise}</p> 
+    <p>Sunset: ${wData.sys.sunset}</p> `;
+
+    let todaysWeatherMain = wData.weather[0].main;
+    //console.log("Today's main weather test", todaysWeatherMain);
+    // Create conditional statement that inserts symbol and weather description depending on the weather (ex: "Clouds")
+    if (todaysWeatherMain === "Clouds") {
+      textForecast.innerHTML = `
+      <img src="Designs/Design-1/assets/Group16.png">
+    <p>Oj det är moln idag</p>`;
+    } else if (todaysWeatherMain === "Snow") {
+      `<img src=""
+      <p>Today it's snowing!</p>`;
+    } else {
+      `<img src=""
+      <p>Okänt väder idag</p>`;
+    }
+    //osv
   });
-//using math.round in the template literal for temperature to make it an equal number.
 
-// (Math.round(data.main.temp)))
+// 5-day forecast
+fetch(urlForecast)
+  .then((response) => {
+    return response.json();
+  })
+  .then((fData) => {
+    console.log("test fData");
 
-//the city name
-//the temperature
+    // filteredForecast is now an array with only the data from 12:00 each day.
+    const filteredForecast = fData.list.filter((item) =>
+      item.dt_txt.includes("12:00")
+    );
+    console.log(filteredForecast);
+
+    // Make a variable that is a function that stores the date and converts it to the name of the weekday
+    // This is a function that converts the date (dt) from the API from milliseconds into name of the weekday
+    const weekday = (data) => {
+      const currentDate = new Date(data * 1000); // sets to millisec.
+      return currentDate.toLocaleDateString("en-GB", {
+        weekday: "short",
+      });
+    };
+
+    weeklyForecast.innerHTML = ` 
+  <div class="day-temp">
+    <div class="day">${weekday(filteredForecast[0].dt)}</div>
+    <div class="temp">${Math.round(filteredForecast[0].main.temp)}°</div>
+  </div>
+  <div class="day-temp">
+    <div class="day">${weekday(filteredForecast[1].dt)}</div>
+    <div class="temp">${Math.round(filteredForecast[1].main.temp)}°</div>
+  </div>
+  <div class="day-temp">
+    <div class="day">${weekday(filteredForecast[2].dt)}</div>
+    <div class="temp">${Math.round(filteredForecast[2].main.temp)}°</div>
+  </div>
+  <div class="day-temp">
+    <div class="day">${weekday(filteredForecast[3].dt)}</div>
+    <div class="temp">${Math.round(filteredForecast[3].main.temp)}°</div>
+  </div>
+  <div class="day-temp">
+    <div class="day">${weekday(filteredForecast[4].dt)}</div>
+    <div class="temp">${Math.round(filteredForecast[4].main.temp)}°</div>
+  </div>`;
+  });
--- slask.js
+++ slask.js
@@ -15,3 +15,27 @@ var seconds = "0" + date.getSeconds();
 var formattedTime = hours + ":" + minutes.substr(-2) + ":" + seconds.substr(-2);
 
 console.log(formattedTime);
+
+const sunriseUnix = json.sys.sunrise; // object based on the unix timestamp for sunrise
+let daterise = new Date(sunriseUnix * 1000); // multiplied by 1000 so that it is in milliseconds, not seconds
+let hoursrise = daterise.getHours(); // hours part from the unix timestamp
+let minutesrise = "0" + daterise.getMinutes(); //minutes part from the unix timestamp
+let formattedTimeSunrise = hoursrise + ":" + minutesrise.substr(-2); //creates the timestamp in hour and minute-form
+const sunsetUnix = json.sys.sunset; // same thing but with sunset
+let dateset = new Date(sunsetUnix * 1000);
+let hoursset = dateset.getHours();
+let minutesset = "0" + dateset.getMinutes();
+let formattedTimeSunset = hoursset + ":" + minutesset.substr(-2);
+
+//Forecast
+const weekday = (data) => {
+  const currentDate = new Date(data * 1000); // sets to millisec.
+  return currentDate.toLocaleDateString("en-GB", { weekday: "short" });
+};
+// fetch the data from the API. Then if you console.log the json
+// you'll see that we only care about the array called list.
+
+const filteredForecast = json.list.filter((item) =>
+  item.dt_txt.includes("12:00")
+);
+// filteredForecast is now an array with only the data from 12:00 each day.
