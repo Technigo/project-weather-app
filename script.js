03246bcee36f29e340eb99d32682ec505f4b0ad0
Emilia G. Anundi <emilia-lundmark@hotmail.com>
2023-02-22 18:28:48 +0100

    Features

--- script.js
+++ script.js
@@ -13,22 +13,28 @@ const sunset = document.getElementById('sunset')
 // From Live-session: STEP 1/fetch "I have a present for you to fetch" (YK)
 //.then = STEP 2 "I'm giving you the present but you don't know what's inside yet" (YK)
 //.then = STEP 3 "I'm giving you the present so now you can take a look what's inside it" (YK)
-fetch('https://api.openweathermap.org/data/2.5/weather?q=Stockholm,Sweden&units=metric&APPID=bcc357d81ce23673e3a8e92322d840f2')
+fetch('https://api.openweathermap.org/data/2.5/weather?q=Piteå,Sweden&units=metric&APPID=bcc357d81ce23673e3a8e92322d840f2')
     //.then = Step 2 "I'm giving you the present but you don't know what's inside yet" (YK)
     .then((response) => {
         return response.json()
     })
     //.then = Step 3 "I'm giving you the present so now you can take a look what's inside it" (YK)
     .then((json) => {
-        city.innerHTML = `<h1>This is the weather in ${json.name}</h1>`
+        city.innerHTML = `<h1>${json.name}</h1>`
         console.log(json) //Works this far, object loading on to console log. (EGA)
 
-        //Added the temperature with some help from another students code (EGA)
-        temperature.innerHTML = `<h3>The temperature is ${
-            Math.round(json.main.temp * 10) / 10
-          }°C in ${json.name}</h3>`;
+        
+        temperature.innerHTML = `<h2>The temperature is ${Math.round(json.main.temp * 10) / 10}°C</h2>`;
+        condition.innerHTML = `<h2>with ${json.weather[0].description}</h2>`;
+        
+        sunrise.textContent=`Sunrise: ${new Date(data.sys.sunrise * 1000).toLocaleTimeString([], {
+            hour: "2-digit",
+            minute: "2-digit",
+          })}`
 
-        condition.innerHTML = `<h2>The weather is ${json.weather[0].description}</h2>`;
-       
+        .catch((error) => {
+            console.log(error);
+          });
+
     })
     
