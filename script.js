347129f96c21a1a96d9902371fe841f6add6b6fe
Bridget Mailley <bmailley42@gmail.com>
2023-02-22 14:58:18 +0100

    created Bridget branch, worked on sunrise and sunset

--- script.js
+++ script.js
@@ -11,13 +11,25 @@ fetch ('https://api.openweathermap.org/data/2.5/weather?q=Stockholm,Sweden&units
     weatherWrapper.innerHTML += `
     <div id="city-name" class="city-name"> ${data.name}</div>`
 
-    data.weather.forEach((weatherDescription) => { //.weather comes from the API - it is an object name
-        weatherWrapper.innerHTML +=
-        `${weatherDescription.description}`
-    })
+    // data.weather.forEach((weatherDescription) => { //.weather comes from the API - it is an object name
+    //     weatherWrapper.innerHTML +=
+    //     `${weatherDescription.description}`
+    // })
+    
+    const mainWeather = (data.weather[0].main);
+    //console.log(mainWeather)
     const roundedTemp = Math.round(data.main.temp * 10)/10;
     //console.log(roundedTemp)
-    weatherWrapper.innerHTML +=
-    `<div id='main-temp' class='main-temp'>${roundedTemp} Celcius</div>`
+    const sunriseDateAndTime = new Date(data.sys.sunrise * 1000); //*1000 makes it correct date and time, but don't know why
+    let hoursrise = sunriseDateAndTime.getHours();
+    let minutesrise = sunriseDateAndTime.getMinutes(); 
+    console.log(sunriseDateAndTime);
+    console.log(hoursrise);
+    console.log(minutesrise);
 
+    weatherWrapper.innerHTML +=
+    `<p><span id='weather-description' class = 'weather-description'>${mainWeather}</span> | <span id='main-temp' class='main-temp'>${roundedTemp}°C</span>
+    <p><span id='sunrise' class = 'sunrise'>Sunrise:</span><p>
+    <p><span id='sunset' class = 'sunset'>Sunset:</span><p>
+     `
 })
