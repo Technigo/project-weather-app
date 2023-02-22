60a0bde260b7eb90d2d382d34889abff1e362359
Bridget Mailley <bmailley42@gmail.com>
2023-02-22 11:23:10 +0100

    set up API and first three data points
    Co-authored-by: Emilia <emiliasaberski@users.noreply.github.com>

--- script.js
+++ script.js
@@ -1,1 +1,23 @@
-console.log('linked')
+const weatherWrapper  = document.getElementById ('weather-wrapper')
+
+fetch ('https://api.openweathermap.org/data/2.5/weather?q=Stockholm,Sweden&units=metric&APPID=d9838d8630b03a974ed368611cffd256')
+
+.then ((response) => {
+    return response.json()
+})
+
+.then ((data) => {
+    console.log (data)
+    weatherWrapper.innerHTML += `
+    <div id="city-name" class="city-name"> ${data.name}</div>`
+
+    data.weather.forEach((weatherDescription) => { //.weather comes from the API - it is an object name
+        weatherWrapper.innerHTML +=
+        `${weatherDescription.description}`
+    })
+    const roundedTemp = Math.round(data.main.temp * 10)/10;
+    //console.log(roundedTemp)
+    weatherWrapper.innerHTML +=
+    `<div id='main-temp' class='main-temp'>${roundedTemp} Celcius</div>`
+
+})
