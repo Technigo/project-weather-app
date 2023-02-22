2a127506670218ecbd0e2e8a7271be559482219a
Emma Engvall <emma.engvall@ica.se>
2023-02-22 17:10:12 +0100

    added comments in JS-file

--- Designs/Design-2/script.js
+++ Designs/Design-2/script.js
@@ -1,5 +1,8 @@
+//Variables that point to selected DOM elements
 const container = document.getElementById('weather')
 
+
+//Fetching data from API
 fetch('http://api.openweathermap.org/data/2.5/weather?q=Gothenburg,Sweden&units=metric&APPID=06edb4af2e0738583f1bc67674449140')
     .then ((response) => {
         return response.json()
@@ -9,7 +12,18 @@ fetch('http://api.openweathermap.org/data/2.5/weather?q=Gothenburg,Sweden&units=
         container.innerHTML = `<h1>The weather in ${json.name} is ${json.timezone} today.</h1>`
         /*Change from timezone*/
 
-        /*json.weather.array.forEach((city) => {
+        /*json.weather.array.forEach((zz) => {
             container.innerHTML += `<p>${} is in ${}</p>`
         });*/
     })
+
+    
+//Display weather.main, weather.description (type of weather) 
+//Display main.temp (current temperature in city XX)
+//Display sys.sunrise (time of sunrise)
+//Display sys.sunset (time of sunset)
+
+//Display img (type depending on weather)
+//Display text (type depending on weather)
+
+//Five day forecast
