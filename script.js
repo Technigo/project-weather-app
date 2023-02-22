464be27de014fb20bd9eef8c551b3631c19fd72e
Amanda <amanda.sl747@gmail.com>
2023-02-22 10:52:55 +0100

    weather-forecast script

--- script.js
+++ script.js
@@ -18,3 +18,47 @@ fetch(
       Math.round(json.main.temp * 10) / 10
     }°C in ${json.name}</h3>`;
   });
+
+// Five day weather forecast
+
+const fiveDayApiUrl =
+  "https://api.openweathermap.org/data/2.5/forecast?lat=55.6059&lon=13.0007&units=metric&appid=7916e2ff30e82c8f4b79258c3235d9c2";
+
+const weatherFiveDays = document.getElementById("weather-five-days");
+
+const fiveDayForecast = () => {
+  fetch(fiveDayApiUrl)
+    .then((response) => {
+      return response.json();
+    })
+
+    .then((fiveDayArray) => {
+      const filteredForecast = fiveDayArray.list.filter((item) =>
+        item.dt_txt.includes("12:00")
+      );
+
+      filteredForecast.forEach((eachDay) => {
+        weatherFiveDays.innerHTML += generateHTMLForForecast(eachDay);
+      });
+    });
+};
+
+fiveDayForecast();
+
+// A function that retrieve and convert data from the API in a readable format.
+function generateHTMLForForecast(day) {
+  const weekdayUnix = day.dt;
+  const weekdayLong = new Date(weekdayUnix * 1000);
+  const weekdayName = weekdayLong
+    .toLocaleDateString("en-US", { weekday: "long" })
+    .toLowerCase();
+  const dayTemp = `${Math.round(day.main.temp * 10) / 10}`;
+
+  // And creates HTML code that is returned
+  let fiveDayForecastHTML = "";
+  fiveDayForecastHTML += `<div class="weather-five-days">`;
+  fiveDayForecastHTML += `<p class="weekday-name">${weekdayName}</p>`;
+  fiveDayForecastHTML += `<p class="day-temp">${dayTemp} °C</p>`;
+  fiveDayForecastHTML += `</div>`;
+  return fiveDayForecastHTML;
+}
