ed64f123bc0e0be55f62de800e3f3004b946c143
Ed Code <edvard.ekstrom@gmail.com>
2023-02-22 20:16:41 +0100

    added sunrise/sunset

--- script.js
+++ script.js
@@ -1,10 +1,11 @@
 let weatherLink =
   "https://api.openweathermap.org/data/2.5/weather?q=Malm%C3%B6,Sweden&units=metric&APPID=";
 
-let weatherDescriptionHtml = document.getElementById("weatherDescription");
-let cityNameHtml = document.getElementById("cityName");
-let currentTemperatureHtml = document.getElementById("currentTemperature");
-let mediumTemperature;
+let weatherDescriptionDiv = document.getElementById("weatherDescription");
+let cityNameDiv = document.getElementById("cityName");
+let currentTemperatureDiv = document.getElementById("currentTemperature");
+let sunriseDiv = document.getElementById("sunrise");
+let sunsetDiv = document.getElementById("sunset");
 
 const fetchWeather = () => {
   fetch(
@@ -14,13 +15,28 @@ const fetchWeather = () => {
       return response.json();
     })
     .then((data) => {
-      // console.log(data);
+      //console.log(data);
       let weatherDescription = data.weather[0].description;
       let cityName = data.name;
-      let currentTemperature = data.main.temp;
-      weatherDescriptionHtml.innerHTML = `${weatherDescription}`;
-      cityNameHtml.innerHTML = `${cityName}`;
-      currentTemperatureHtml.innerHTML = `${currentTemperature}`;
+      let currentTemperature = data.main.temp; // fix to only show one number
+
+      let fetchedSunrise = data.sys.sunrise;
+      let sunriseTime = new Date(fetchedSunrise * 1000);
+      let sunriseHours = sunriseTime.getHours();
+      let sunriseMinutes = sunriseTime.getMinutes();
+      let renderedSunrise = "0" + sunriseHours + "." + sunriseMinutes; // add zero if minutes are only one digit
+
+      let fetchedSunset = data.sys.sunset;
+      let sunsetTime = new Date(fetchedSunset * 1000);
+      let sunsetHours = sunsetTime.getHours();
+      let sunsetMinutes = sunsetTime.getMinutes();
+      let renderedSunset = sunsetHours + "." + sunsetMinutes; // add zero if minutes are only one digit
+
+      weatherDescriptionDiv.innerHTML = `${weatherDescription}`;
+      cityNameDiv.innerHTML = `${cityName}`;
+      currentTemperatureDiv.innerHTML = `${currentTemperature}`;
+      sunriseDiv.innerHTML = `${renderedSunrise}`;
+      sunsetDiv.innerHTML = `${renderedSunset}`;
     })
     .catch((error) => {
       console.log(error);
