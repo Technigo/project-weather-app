459bbe6e3e162882399d20c94392bfd5d98e9995
AntoniaGranit <antonia.granit@gmail.com>
2023-02-22 11:39:52 +0200

    add weather and location info

--- script.js
+++ script.js
@@ -9,11 +9,18 @@ const temperature = document.getElementById("temperature");
 fetch(
   "http://api.openweathermap.org/data/2.5/weather?q=Stockholm,Sweden&units=metric&APPID=775b6d69e24fcb088a070bee66d05057"
 )
-  .then((present) => {
-    console.log(present);
-    return present.json();
+  .then((response) => {
+    console.log(response);
+    return response.json();
   })
-  .then((unwrappedPresent) => {
-    console.log(unwrappedPresent);
-    return unwrappedPresent;
+  .then((json) => {
+    console.log(json);
+    console.log(json.main.temp);
+    const currentTemp = json.main.temp;
+    const roundedUpTemp = Math.round(currentTemp);
+    const description = json.weather[0].description;
+    console.log(description);
+    weatherHeader.innerHTML = `<p>City name: ${json.name}</p>
+    <p>Temperature: ${roundedUpTemp}</p>
+    <p>Weather: ${description}</p>`;
   });
