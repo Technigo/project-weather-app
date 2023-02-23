3f70f0031eef3168031f476f2af390e58659fc67
malvinamaria <51817058+malvinamaria@users.noreply.github.com>
2023-02-23 10:26:26 +0100

    adding some styling to the main paige (#2)
    * add some styling
    
    * added geo location
    
    * last changes

--- script.js
+++ script.js
@@ -1,34 +1,39 @@
-const container = document.getElementById('card')
+
+const container = document.getElementById('city');
 const forecastDate = document.getElementById('forecastDate')
 const forecastTemp = document.getElementById('forecastTemp')
 const forecastFeels = document.getElementById('forecastFeels')
 const forecastMinMax = document.getElementById('forecastMinMax')
 
 
-fetch("https://api.openweathermap.org/data/2.5/weather?q=Stockholm,Sweden&units=metric&APPID=14156af54d10ce159e1b521416adb87f")
-    .then((response)=> {
-        return response.json()
-    })
+// Get the user's current location
+navigator.geolocation.getCurrentPosition((position) => {
+  const { latitude, longitude } = position.coords;
+
+  // Fetch the current weather data for the user's location
+  fetch(`https://api.openweathermap.org/data/2.5/weather?lat=${latitude}&lon=${longitude}&units=metric&APPID=ddf98774bfc0041a16a7d95948e68934`)
+    .then((response) => response.json())
     .then((json) => {
-        console.log(json)
-        // Convert Unix timestamps to date objects
-        const sunrise = new Date(json.sys.sunrise * 1000);
-        const sunset = new Date(json.sys.sunset * 1000);
-        
-        container.innerHTML = `
+      // Convert Unix timestamps to date objects
+      const sunrise = new Date(json.sys.sunrise * 1000);
+      const sunset = new Date(json.sys.sunset * 1000);
+
+      // Update the HTML content with the current weather data for the user's location
+      container.innerHTML = `
         <h1> ${json.name} </h1>
         <h2> ${(Math.round(json.main.temp))}°C</h2>
-        <h3> ${json.weather[0].description}</h3>
-        <h4> sunrise ${sunrise.toLocaleTimeString()} sunset ${sunset.toLocaleTimeString()}  </h4>
-        `
-    })
+        <h3> ${json.weather[0].main}</h3>
+        <h4> 🌅 sunrise ${sunrise.toLocaleTimeString()}</h4>
+        <h4> 🌇 sunset ${sunset.toLocaleTimeString()}</h4>
+      `;
+    });
 
-    fetch("https://api.openweathermap.org/data/2.5/forecast?q=Stockholm,Sweden&units=metric&APPID=ddf98774bfc0041a16a7d95948e68934")
-        .then((response) => {
-            return response.json()
-        })
-        .then ((json) => {
-            console.log(json)
+  // Fetch the weather forecast data for the user's location
+  fetch(`https://api.openweathermap.org/data/2.5/forecast?lat=${latitude}&lon=${longitude}&units=metric&APPID=ddf98774bfc0041a16a7d95948e68934`)
+    .then((response) => response.json())
+    .then((json) => {
+      // Filter the forecast data to only show the information for 12:00 PM each day
+      const filteredForecast = json.list.filter((item) => item.dt_txt.includes('12:00'));
 
              const filteredForecast = json.list.filter(item => item.dt_txt.includes('12:00'))
             console.log(filteredForecast)
