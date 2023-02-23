dddaf587a189873ec184586685ca7d681836486e
Jonas Jakobson <jonas.jakobson@gmail.com>
2023-02-23 10:06:33 +0100

    Changed styling, added weather classes

--- script.js
+++ script.js
@@ -4,18 +4,36 @@ const dailyTextMsg = document.getElementById("dailyText");
 const dayFive = document.getElementById("fiveDays");
 
 
-//api for stockholm
-const apiLisbon = "http://api.openweathermap.org/data/2.5/weather?q=Lisbon,Portugal&units=metric&APPID=4f9ca5d3e70c95a041bc513ac8b31ff8"
 
-fetch(apiLisbon)
-.then(response => {
-    return response.json()
-})
-.then( dataLis => {
-    console.log(dataLis);  //We want to save this data in variable for later us
 
-//can we do this shorter and easier
-const sunriseTime = dataLis.sys.sunrise;
+//api key
+const apiKey = "4f9ca5d3e70c95a041bc513ac8b31ff8"
+
+
+//Lisbon coordinates
+const latitude = 38.7167
+const longitude = -9.1333
+
+const lisbonURL = `http://api.openweathermap.org/data/2.5/weather?q=Lisbon,Portugal&units=metric&APPID=${apiKey}`;
+const fiveDayURL = `http://api.openweathermap.org/data/2.5/forecast?lat=${latitude}&lon=${longitude}&units=metric&appid=${apiKey}`
+
+
+//This code fetches the data, turns it into json and passes it to other functions
+fetch(lisbonURL)
+  .then(response => response.json())
+  .then(dataLis => {
+    currentTemp(dataLis);      //functions for later use
+    checkWeather(dataLis);
+  
+  })
+  //.catch(error => console.error("errror is:" + error));
+
+
+//Sunrise/sunset-function
+const currentTemp = (dataLis) => {
+  //console.log(`Just nu är det ${data.main.temp} grader varmt`);
+ 
+ const sunriseTime = dataLis.sys.sunrise;
 const sunsetTime = dataLis.sys.sunset;
 
 const sunriseDate = new Date(sunriseTime * 1000);
@@ -27,13 +45,11 @@ const sunriseTimeString =sunriseDate.toLocaleTimeString();
 const sunsetTimeString =sunsetDate.toLocaleTimeString();
 console.log("Sunrise: " + sunriseTimeString)
 console.log("Sunset: " + sunsetTimeString)
-
-//weather today starts here
-
-    today.innerHTML = `
+ 
+ 
+  today.innerHTML = `
     <p>${dataLis.weather[0].description} | ${Math.round(dataLis.main.temp)}°C</p>
    
-
     <div>
         <p class="sunrises" id="sunrise"> 
          Sunrise ${hours} </p>
@@ -43,78 +59,79 @@ console.log("Sunset: " + sunsetTimeString)
         <div>
         <p class="sunsets" id="sunset"> 
         Sunset ${hoursSunset} 
-        </p>`
+        </p> </div>`
+
+}
 
 
-});
+//Checks weather type (cloudy, clear, rainy)
+const checkWeather = (dataLis) => {
+    console.log(dataLis)
+  console.log(dataLis.weather[0].main);
 
-//Fetching city name and changing weather (image/text)
-fetch(apiLisbon)
-.then(response => {
-    return response.json()
-})
-.then( dataLis => {
-    console.log(dataLis.weather[0].main);
-
-    if (dataLis.weather[0].main === 'Clouds'){
-
-dailyTextMsg.innerHTML=   `<div id="cloud" class="cloudy">
+  if (dataLis.weather[0].main === 'Clouds'){
+    dailyTextMsg.innerHTML=   `<div id="cloud" class="cloudy">
 <img src="/Designs/Design-2/icons/noun_Cloud_1188486.svg" alt="">
     <p>oh oooo Its cloudy in ${dataLis.name} right now</p>
 </div>`
 }
-else if (dataLis.weather[0].main === 'Clear'){
-    `
-    <div id="sunGlasses" class="sunglasses">
+
+    else if (dataLis.weather[0].main === 'Clear'){
+      dailyTextMsg.innerHTML=  `<div id="sunGlasses" class="sunglasses">
       <img src="/Designs/Design-2/icons/noun_Sunglasses_2055147.svg" alt="">
         
       <p>it's sunny on ${dataLis.name} right now</p>
     </div>
     `
-
-}
-else  {
-    `
-<div id="umbrella" class="umbrellas">
+    } 
+    else if (dataLis.weather[0].main === 'Fog'){
+     dailyTextMsg.innerHTML=    
+     `<div id="fog" class="fog">
   <img src="/Designs/Design-2/icons/noun_Umbrella_2030530.svg" alt="">
-  <p>Raindropps on ${dataLis.name} right now</p>
-</div>
-`
-}
-
-
-});
-
-
-/*//Array holding all weather messages
-const weatherMessages = [
-    `<div id="cloud" class="cloudy">
-<img src="/Designs/Design-2/icons/noun_Cloud_1188486.svg" alt="">
-    <p>oh oooo Its cloudy in ${dataLis.name} right now</p>
-</div>`,
-
-`
-<div id="sunGlasses" class="sunglasses">
-  <img src="/Designs/Design-2/icons/noun_Sunglasses_2055147.svg" alt="">
+  <p>Fog on ${dataLis.name} right now</p>
+</div>`
+    }
     
-  <p>it's sunny on ${dataLis.name} right now</p>
-</div>
-`,
-`
-<div id="umbrella" class="umbrellas">
+    else {
+      
+     dailyTextMsg.innerHTML=  `<div id="umbrella" class="umbrellas">
   <img src="/Designs/Design-2/icons/noun_Umbrella_2030530.svg" alt="">
   <p>Raindropps on ${dataLis.name} right now</p>
-</div>
-`,
-
-]
-
+</div>`
+    };
+};
 
 
-console.log( weatherMessages[0])*/ 
+//Five day forecast needs new API-call 
+//fixa i css så de hamnar i linje med varandra
+fetch(fiveDayURL)
+.then(response => response.json())
+.then(fiveDayData => {
+  ///includes  data from everyday at 12.00 hours 
+
+//Returns seven days
+const filteredData = fiveDayData.list.filter(data => data.dt_txt.includes('12:00:00'));
+//This part slices first five days
+const sliceData= filteredData.slice(0, 5);
+
+console.log(sliceData);
 
 
+//function to show the data in html
+sliceData.forEach(data => {
+const day = new Date (data.dt_txt).toLocaleString('en-US', {weekday: 'short'});
+const temp = Math.round(data.main.temp);
+
+//this should be left in branch styling
+dayFive.innerHTML += `
+       <div class= "border"> 
+       <p class = "day">${day}</p>
+    <p class = " temp">${temp}°C</p> 
+        </div>
+      `;
 
 
+})
 
-
+  }
+  )
