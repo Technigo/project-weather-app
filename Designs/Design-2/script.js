53eb569c625b71f72d2ab3e14ef9db4cbee980d6
Emma Engvall <emma.engvall@ica.se>
2023-02-23 09:49:51 +0100

    added divs in html and added dom elements in js

--- Designs/Design-2/script.js
+++ Designs/Design-2/script.js
@@ -1,6 +1,13 @@
 //Variables that point to selected DOM elements
-const container = document.getElementById('weather')
+const container = document.getElementById("loading")
+const header = document.getElementById("header")
+const body = document.body
+const text = document.getElementById("#text")
+const img = document.getElementById(".img")
+const date = document.getElementById("date")
 
+//Global variable
+let todayWeather
 
 //Fetching data from API
 fetch('http://api.openweathermap.org/data/2.5/weather?q=Gothenburg,Sweden&units=metric&APPID=06edb4af2e0738583f1bc67674449140')
@@ -17,7 +24,6 @@ fetch('http://api.openweathermap.org/data/2.5/weather?q=Gothenburg,Sweden&units=
         });*/
     })
 
-//Add a promise?    
     
 //Display weather.main, weather.description (type of weather) 
 //Display main.temp (current temperature in city XX)
@@ -26,6 +32,18 @@ fetch('http://api.openweathermap.org/data/2.5/weather?q=Gothenburg,Sweden&units=
 
 //Display img (type depending on weather)
 //Display text (type depending on weather)
+
+/*If weather === "clear", "sunny" ....
+	show <p> "Get your sunnies on. ${json.city} is looking rather great today."</p>
+	show img. "noun_Sunglasses..."
+
+  else if weather === "rain",....
+	show <p> "Don't forget your umbrella. It's wet in ${json.city} today."</p>
+	show img. "noun_Umbrella..."
+
+  else weather === "cloudy",...
+      show <p> "Light a candle and get cosy. ${json.city} is looking grey today."</p>
+	show img. "noun_Cloud...*/
 
 //Five day forecast
 
