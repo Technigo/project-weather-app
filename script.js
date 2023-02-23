5c2812767d08246a146ca1a9a3c6c40ecd726034
khleoth <leothunell@gmail.com>
2023-02-23 11:34:59 +0100

    static CSS done

--- script.js
+++ script.js
@@ -6,13 +6,15 @@ const cityName = document.getElementById("city")
 const cloudReport = document.getElementById("cloudReport")
 const sunrise = document.getElementById("sunrise")
 const sunset = document.getElementById("sunset")
-const cloudImage = document.getElementById("cloudImage")
-const description = document.getElementById("description")
+const weatherImage = document.getElementById("weatherImage")
+const middleSection = document.getElementById("middleSection")
 const day1 = document.getElementById("day+1")
 const day2 = document.getElementById("day+2")
 const day3 = document.getElementById("day+3")
 const day4 = document.getElementById("day+4")
 const day5 = document.getElementById("day+5")
+
+// change so that API-key is inside a variable instead straight into the functions
 
 //functions here
 
@@ -29,7 +31,7 @@ const fetchWeatherAPI = () => {
             console.log(json)
 
             cityName.innerHTML = json.name;
-            cloudReport.innerHTML = `${json.weather[0].description} | ${Number(json.main.temp).toFixed(1)}°`;
+            cloudReport.innerHTML = `${json.weather[0].description} | ${Number(json.main.temp).toFixed(1)} C°`;
 
             let timeSunrise = new Date(json.sys.sunrise * 1000)
             let timeSunset = new Date(json.sys.sunset * 1000)
@@ -61,23 +63,25 @@ const fetchWeekdaysAPI = () => {
             const filteredForecast = json.list.filter(item => item.dt_txt.includes('12:00'))
 
             dayTime = new Date(filteredForecast[0].dt_txt)
-            day1.innerHTML = roundDecimal(filteredForecast[0].main.temp) +"-----------------" + dayTime.toLocaleString('en-US', options)
+            day1.innerHTML = `<span>${dayTime.toLocaleString('en-US', options)}</span>` + `<span>${roundDecimal(filteredForecast[0].main.temp)}C°</span>`
 
-            
             dayTime = new Date(filteredForecast[1].dt_txt)
-            day2.innerHTML = roundDecimal(filteredForecast[1].main.temp) +"-----------------" + dayTime.toLocaleString('en-US', options)
+            day2.innerHTML = `<span>${dayTime.toLocaleString('en-US', options)}</span>` + `<span>${roundDecimal(filteredForecast[1].main.temp)}C°</span>`
 
             dayTime = new Date(filteredForecast[2].dt_txt)
-            day3.innerHTML = roundDecimal(filteredForecast[2].main.temp) +"-----------------" + dayTime.toLocaleString('en-US', options)
+            day3.innerHTML = `<span>${dayTime.toLocaleString('en-US', options)}</span>` + `<span>${roundDecimal(filteredForecast[2].main.temp)}C°</span>`
 
-            
             dayTime = new Date(filteredForecast[3].dt_txt)
-            day4.innerHTML = roundDecimal(filteredForecast[3].main.temp) +"-----------------" + dayTime.toLocaleString('en-US', options)
+            day4.innerHTML = `<span>${dayTime.toLocaleString('en-US', options)}</span>` + `<span>${roundDecimal(filteredForecast[3].main.temp)}C°</span>`
 
-            
             dayTime = new Date(filteredForecast[4].dt_txt)
-            day5.innerHTML = roundDecimal(filteredForecast[4].main.temp) +"-----------------" + dayTime.toLocaleString('en-US', options)
+            day5.innerHTML = `<span>${dayTime.toLocaleString('en-US', options)}</span>` + `<span>${roundDecimal(filteredForecast[4].main.temp)}C°</span>`
 
+            /* dayTime = new Date(filteredForecast[5].dt_txt)
+            day5.innerHTML = `<span>${dayTime.toLocaleString('en-US', options)}</span>` + `<span>${roundDecimal(filteredForecast[5].main.temp)}C°</span>`
+
+            dayTime = new Date(filteredForecast[6].dt_txt)
+            day5.innerHTML = `<span>${dayTime.toLocaleString('en-US', options)}</span>` + `<span>${roundDecimal(filteredForecast[6].main.temp)}C°</span>` */
             
             //let dayTime = new Date(filteredForecast[0].dt_txt)
             console.log(dayTime.toLocaleString('en-US', options));
