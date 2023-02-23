68dbb9b3f6cbaa7cd17274c23ca8aeb3231e2cea
cpz-manifold <carol.pinzon.ardila@gmail.com>
2023-02-23 11:42:56 +0100

    forecast and phrase

--- Designs/weather.js
+++ Designs/weather.js
@@ -8,7 +8,7 @@ const forecastTable = document.getElementById('forecast-table');
 //global variables
 let defaultCity = "Stockhoml";
 const maxDayDisplay = 5;
-const cod = "93834bb23b2a9e80836d0a5415cc4a72"
+const cod = "7309e4a5829fafe809df835ad95f18ea"
 
 
 const WeatherData = () => {
@@ -20,9 +20,14 @@ const WeatherData = () => {
       console.log(json);
       // Setting basic information on the app and rounded numbers to first decimal.
       name.innerHTML = json.name;
-      tempMax.innerHTML = Number(json.main.temp_max).toFixed(1);
-      tempMin.innerHTML = Number(json.main.temp_min).toFixed(1);
+      tempMax.innerHTML =  Math.round(json.main.temp_max);
+      tempMin.innerHTML =  Math.round(json.main.temp_min);
       description.innerHTML = json.weather[0].description;
+
+
+      console.log("HERE")
+      console.log(json.weather[0].description)
+      setWeatherMessage(json.weather[0].description)
     })
     .catch((error) => {
       console.error(error);
@@ -32,23 +37,31 @@ const WeatherData = () => {
       // Here we will inject the json data into the
       // html table related to the id = forecast-table
       displayForecast()
+
+
 };
 
+//https://api.openweathermap.org/data/2.5/onecall?lat=59.3326&lon=18.0649&exclude=hourly,minutely&appid=${cod}
+
 const displayForecast = () => {
-  fetch(`https://api.openweathermap.org/data/2.5/forecast?q=Stockholm,Sweden&units=metric&APPID=${cod}`)
+  //fetch(`https://api.openweathermap.org/data/2.5/onecall?q=Stockholm,Sweden&units=metric&APPID=${cod}`)
+  fetch(`https://api.openweathermap.org/data/2.5/onecall?lat=33.44&lon=-94.04&exclude=hourly,minutely&units=metric&appid=${cod}`)
   .then((response) => {
     return response.json();
   })
   .then((json) => {
     // in this variable we will make sure it display only 5 days
     let forecastList; 
+    console.log(json)
+    console.log(typeof(json.daily))
+    console.log(json.daily[0])
   
-    if (json.list.length > maxDayDisplay) {
+    if (json.daily.length > maxDayDisplay) {
       // if it has more than 5 days slice the rest
-      forecastList = json.list.slice(0, maxDayDisplay)
+      forecastList = json.daily.slice(0, maxDayDisplay)
     } else {
       // if it has 5 or less display them
-      forecastList = json.list
+      forecastList = json.daily
     }
 
   // here we loop through every day of the list and inject the info on the html
@@ -56,10 +69,13 @@ const displayForecast = () => {
       // Getting data to be displayed on each row.
       // convert the weird data number in a human readable date
       const date = new Date(day.dt * 1000)
-      let dayName = date.toLocaleDateString("en-US", {weekday: "short"})
+      let dayName = date.toLocaleDateString("en-EN", {weekday: "short"})
+     
+      console.log(date)
+      console.log(dayName)
      // round the decimals of the temperature
-      const minTemp = Math.round(day.main.temp_min)
-      const maxTemp = Math.round(day.main.temp_max)
+      const minTemp = Math.round(day.temp.min)
+      const maxTemp = Math.round(day.temp.max)
 
       // Setting the data into the HTML table.
       forecastTable.innerHTML += `
@@ -70,8 +86,6 @@ const displayForecast = () => {
       `  
     })
 
-
-
   })
   .catch((error) => {
     console.error(error);
@@ -80,3 +94,61 @@ const displayForecast = () => {
 }
 
 WeatherData();
+
+//  phrase that change depending on the weather 
+const setWeatherMessage = (weather) => {
+  if    (weather.includes("thunderstorm")) {
+    description.innerHTML = `
+    <h1> today we are having ${weather} ..... </h1>`
+
+
+  }
+
+  else if (weather.includes("drizzle")) {
+    description.innerHTML = `
+    <h1> today we are having ${weather} ..... </h1>`
+
+
+    
+  }
+
+  else if (weather.includes("rain")) {
+    description.innerHTML = `
+    <h1> today we are having ${weather} ..... </h1>`
+
+    
+
+  }
+
+  else if (weather.includes("snow")) {
+    description.innerHTML = `
+    <h1> today we are having ${weather} ..... </h1>`
+
+
+  }
+
+  else if (weather.includes("atmosphere")) {
+    description.innerHTML = `
+    <h1>  today we are having  ${weather}.... </h1>`
+
+  }
+
+
+  else if (weather.includes("clear")) {
+    description.innerHTML = `
+    <h1> today we are having ${weather} ..... </h1>`
+
+
+  }
+
+
+  else if  (weather.includes("clouds")){
+    description.innerHTML = `
+    <h1> today we are having ${weather} .....</h1>`
+
+
+  }
+
+
+}
+
