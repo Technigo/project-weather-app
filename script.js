197d75db6679dbedca2f17e2286982e8a8dd6c0b
Vera Sjunnesson <Vera@admins-mbp.home>
2023-02-24 09:42:00 +0100

    tried some stuff out

--- script.js
+++ script.js
@@ -1,37 +1,43 @@
 const testy = document.getElementById('testy');
 const weatherForecast = document.getElementById('weatherForecast')
 const forecastWeekdays = document.getElementById('forecastWeekdays')
+const forecastIcon = document.getElementById('forecastIcon')
 const forecastDescription = document.getElementById('forecastDescription')
 const forecastTemp = document.getElementById('forecastTemp')
 const forecastFeelsLike = document.getElementById('forecastFeelsLike')
 const sunriseText = document.getElementById('sunriseText');
 const sunsetText = document.getElementById('sunsetText');
-
+const freezing = document.getElementById('freezing-cold') 
+const cold = document.getElementById('cold')
+const mediumCold = document.getElementById('medium-cold')
+const warm = document.getElementById('warm')
+const hot = document.getElementById('hot')
+const mainIcon = document.getElementById('mainIcon')
 
 //Variables we can use later to automate API-fethcing:
 const apiKey = 'c480de5f69ca98d1993a4dae3213642e';
 let city = 'Stockholm';
 // Use: `https://api.openweathermap.org/data/2.5/weather?q=${city}&units=metric&APPID=${apiKey}`
 
-//Our testing fetch:
+
 fetch('https://api.openweathermap.org/data/2.5/weather?q=Stockholm&units=metric&APPID=c480de5f69ca98d1993a4dae3213642e')
     .then((response) => {
         return response.json()
     })
     .then((json) => {
         console.log(`json:`, json)
-
+        let { icon } = json.weather[0];
         // Store the rounded number in a variable called "round"
         let round = Math.round(json.main.temp * 10 ) / 10;
 
         testy.innerHTML = `<p>City: ${json.name}</p>`;
         testy.innerHTML += `<p>Temperature: ${round} °C</p>`;
         testy.innerHTML += `<p>Weather: ${json.weather[0].description}</p>`;
+        mainIcon.innerHTML += `<img src="http://openweathermap.org/img/wn/${icon}@2x.png" alt="weather icon" class="weather-icons">`
     })
     .catch((err) => {
         console.log(`error caught:`, err)
     })
-
 
 const getSunriseSunsetData = () => {
     fetch(`https://api.openweathermap.org/data/2.5/weather?q=${city}&units=metric&APPID=${apiKey}`)
@@ -62,7 +68,7 @@ getSunriseSunsetData();
 
 
 const weatherForecastData = () => {
-    fetch('https://api.openweathermap.org/data/2.5/forecast?q=Stockholm,Sweden&units=metric&APPID=c480de5f69ca98d1993a4dae3213642e')
+    fetch(`https://api.openweathermap.org/data/2.5/forecast?q=${city}&units=metric&APPID=${apiKey}`)
         .then((forecastResponse) => {
             return forecastResponse.json();
         })
@@ -76,8 +82,9 @@ const weatherForecastData = () => {
             filterData.forEach(date => { 
             const weekDay = new Date(date.dt * 1000).toString().split(' ')[0]; //All the five days dates' convertet from numbers to text
                 if (weekDay !== todaysDate) {
+                    let {icon} = date.weather[0];
                     forecastWeekdays.innerHTML += `<p>${weekDay}</p>`
-                    forecastDescription.innerHTML += `<p>${date.weather[0].description}</p>`
+                    forecastIcon.innerHTML += `<img src="http://openweathermap.org/img/wn/${icon}@2x.png" alt="weather icon" class="weather-icons">`
                     forecastTemp.innerHTML += `<p>${date.main.temp.toFixed(0)}°</p>`
                     forecastFeelsLike.innerHTML += `<p>${date.main.feels_like.toFixed(0)}°</p>`
                 }
@@ -89,7 +96,29 @@ weatherForecastData();
  
 //Learn how to get symbols from the api
 //Learn how to get a search word to show an image from unsplash
-   
 
 
-    
+
+const gardientWarmCold = () => {
+    fetch(`https://api.openweathermap.org/data/2.5/weather?q=${city}&units=metric&APPID=${apiKey}`)
+        .then((response) => {
+        return response.json();
+        })
+        .then((data) => {
+        console.log(data.main.temp.toFixed(0))
+            const value = (data.main.temp.toFixed(0))
+            if (value >= -20 && value <= -10 ) {
+                cold.media = '';   
+            } if (value > -10 && value <= 0) {
+                cold.media = ''; 
+            } if (value > 0.00 && value <= 10.00) {
+                cold.media = '';    
+            } if (value > 10.00 && value <= 25.00) {
+                cold.media = ''; 
+            } else {
+                cold.media = ''; 
+            }         
+})
+}
+
+gardientWarmCold()
