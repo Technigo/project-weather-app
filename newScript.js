5dbc9f70433eca3888ad53b019697d16267ab651
Irupé Pozo <irupe.pozo.graviz@gmail.com>
2023-02-24 12:29:24 +0100

    hand in version

--- newScript.js
+++ newScript.js
@@ -10,55 +10,30 @@ const apiKey = "4f9ca5d3e70c95a041bc513ac8b31ff8"
 
 
 //Lisbon coordinates
-const latitude = 38.7167
-const longitude = -9.1333
+
 let city = "Lisbon"; //Default city
 
 
-let lisbonURL = `http://api.openweathermap.org/data/2.5/weather?q=${city}&units=metric&appid=6dc9ca16706cabb0c8c9d20011825ab1`;
-const fiveDayURL = `http://api.openweathermap.org/data/2.5/forecast?lat=${latitude}&lon=${longitude}&units=metric&appid=${apiKey}`
-
-//http://api.openweathermap.org/data/2.5/weather?q=Lisbon,Portugal&units=metric&APPID=${apiKey}
-
-// http://api.openweathermap.org/data/2.5/forecast?q=Motala,Sweden&units=metric&appid=${apiKey}
-
-//geo possition
-const successCallback = (position) => {
-  console.log(position);
-};
-
-const errorCallback = () => {
-
-  //bodySelector.innerHTML += "<p>Location not found</p>"
-};
-
-//navigator.geolocation.getCurrentPosition(successCallback, errorCallback);
-
-//this is for te search and find other cities, make it shorter
-
-
-
-
-      
-
+let cityURL = `http://api.openweathermap.org/data/2.5/weather?q=${city}&units=metric&appid=6dc9ca16706cabb0c8c9d20011825ab1`;
+const fiveDayURL = `http://api.openweathermap.org/data/2.5/forecast?q=${city}&units=metric&appid=${apiKey}`
 
 //This code fetches the data, turns it into json and passes it to other functions
-fetch(lisbonURL)
+fetch(cityURL)
   .then(response => response.json())
-  .then(dataLis => {
-    currentTemp(dataLis);      //functions for later use
-    checkWeather(dataLis);
+  .then(dataForecast => {
+    currentTemp(dataForecast);      //functions for later use
+    checkWeather(dataForecast);
   
-  })
-  //.catch(error => console.error("errror is:" + error));
+  }) 
+  .catch(error => console.error("errror is:" + error));
 
 
 //Sunrise/sunset-function
-const currentTemp = (dataLis) => {
-  //console.log(`Just nu är det ${data.main.temp} grader varmt`);
+const currentTemp = (dataForecast) => {
 
- const sunriseTime = dataLis.sys.sunrise;
-const sunsetTime = dataLis.sys.sunset;
+
+ const sunriseTime = dataForecast.sys.sunrise;
+const sunsetTime = dataForecast.sys.sunset;
 
 const sunriseDate = new Date(sunriseTime * 1000);
 const hours = sunriseDate.getHours() + ":" + sunriseDate.getMinutes();
@@ -70,11 +45,11 @@ const sunsetTimeString =sunsetDate.toLocaleTimeString();
 console.log("Sunrise: " + sunriseTimeString)
 console.log("Sunset: " + sunsetTimeString)
 
-const toDay = new Date (dataLis.dt * 1000).toLocaleString('en-US', {weekday: 'long', timeZone: 'Europe/Lisbon'}); //add timezone (local?)
+const toDay = new Date (dataForecast.dt * 1000).toLocaleString('en-US', {weekday: 'long', timeZone: 'Europe/Lisbon'}); 
  
   today.innerHTML = `
   <h3>  ${toDay}</h3>
-    <p>${dataLis.weather[0].description} | ${Math.round(dataLis.main.temp)}°C</p>
+    <p>${dataForecast.weather[0].description} | ${Math.round(dataForecast.main.temp)}°C</p>
    
 
     <div>
@@ -90,36 +65,35 @@ const toDay = new Date (dataLis.dt * 1000).toLocaleString('en-US', {weekday: 'lo
 
 }
 
-
 //Checks weather type (cloudy, clear, rainy)
-const checkWeather = (dataLis) => {
+const checkWeather = (dataForecast) => {
   
-  console.log(dataLis.weather[0].main);
+  console.log(dataForecast.weather[0].main);
 
-  if (dataLis.weather[0].main === 'Clouds'){
+  if (dataForecast.weather[0].main === 'Clouds'){
     bodySelector.classList.add('clouds');
     dailyTextMsg.innerHTML=   `<div id="cloud">
 <img src="/Designs/Design-2/icons/noun_Cloud_1188486.svg" alt="">
 
-    <p>oh oooo Its cloudy in ${dataLis.name} right now</p>
+    <p>It's cloudy in ${dataForecast.name} right now</p>
 </div>`;
 }
 
-    else if (dataLis.weather[0].main === 'Clear'){
+    else if (dataForecast.weather[0].main === 'Clear'){
       bodySelector.classList.add('sunny');
       dailyTextMsg.innerHTML=  `<div id="sunGlasses" class="sunglasses">
       <img src="/Designs/Design-2/icons/noun_Sunglasses_2055147.svg" alt="">
         
-      <p>it's sunny on ${dataLis.name} right now</p>
+      <p>It's sunny on ${dataForecast.name} right now</p>
     </div>
     `
     } 
-    else if (dataLis.weather[0].main === 'Fog'){
+    else if (dataForecast.weather[0].main === 'Fog'){
       bodySelector.classList.add('foggy');
      dailyTextMsg.innerHTML=    
      `<div id="fog" class="fog">
      <img src="/Designs/Design-2/icons/icons8-fog-64.png" alt="fog">
-  <p>Fog on ${dataLis.name} right now</p>
+  <p>It's foggy in ${dataForecast.name} right now</p>
 </div>`
     }
     
@@ -127,27 +101,27 @@ const checkWeather = (dataLis) => {
       bodySelector.classList.add('rain');
      dailyTextMsg.innerHTML=  `<div id="umbrella" class="umbrellas">
   <img src="/Designs/Design-2/icons/noun_Umbrella_2030530.svg" alt="">
-  <p>Raindropps on ${dataLis.name} right now</p>
+  <p>It's raining in ${dataForecast.name} right now</p>
 </div>`
     };
 };
 
 
-//Five day forecast needs new API-call 
-//fixa i css så de hamnar i linje med varandra
 fetch(fiveDayURL)
 .then(response => response.json())
 .then(fiveDayData => {
   ///includes  data from everyday at 12.00 hours 
   console.log(fiveDayData)
+  fiveDayForecast(fiveDayData)
+}) 
 
 //Returns seven days
+const fiveDayForecast = (fiveDayData) => {
+//Every time function runs, old forecast is deleted
+dayFive.innerHTML = "";
 const filteredData = fiveDayData.list.filter(data => data.dt_txt.includes('12:00:00'));
 //This part slices first five days
 const sliceData= filteredData.slice(0, 5);
-
-console.log(sliceData);
-
 
 //function to show the data in html
 sliceData.forEach(data => {
@@ -161,25 +135,43 @@ dayFive.innerHTML += `
         <p class="temperature">${temp}°C</p>
         </div>
       `;
+})};
 
 
-})
+const form = document.getElementById("mainForm");
+const cityInput = document.getElementById("citysearch");
 
-  }
-  )
+let userCity = "";
 
-  const form = document.getElementById("mainForm");
-  const cityInput = document.getElementById("citysearch");
+form.addEventListener("submit", (event) => {
+  event.preventDefault();
+  userCity = cityInput.value.trim();
 
-  let userCity = ""
-  
-  form.addEventListener ("submit", (event) => {
-    event.preventDefault();
-    userCity = cityInput.value  //.trim();
+  fetch(`http://api.openweathermap.org/data/2.5/weather?q=${userCity}&units=metric&appid=6dc9ca16706cabb0c8c9d20011825ab1`)
+    .then(response => response.json())
+    .then(data => {
+     
+      checkWeather(data);
+      currentTemp(data);
+    })
+    .catch(error => {
+    
+      console.error(error);
+    });
+    fetch(`http://api.openweathermap.org/data/2.5/forecast?q=${userCity}&units=metric&appid=6dc9ca16706cabb0c8c9d20011825ab1`)
+    .then(response => response.json())
+    .then(data => {
+    fiveDayForecast(data);
+      
+    })
+    .catch(error => {
+   
+      console.error(error);
+    });
+});
 
- lisbonURL = `http://api.openweathermap.org/data/2.5/weather?q=${userCity}&units=metric&appid=6dc9ca16706cabb0c8c9d20011825ab1`;
-  
-  })
 
-  
-  //console.log("This is " + userCity)
+
+
+
+
