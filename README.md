260d6809c7f66d867f883d61a037275a839fff11
Oscar Sindemark <120760711+OscarSindemark@users.noreply.github.com>
2023-02-24 01:31:43 -0800

    added weather icon, fixed README (#4)

--- README.md
+++ README.md
@@ -4,7 +4,11 @@ This project is a weather dashboard that provides current weather and temperatur
 
 ## The problem
 
-The project involves working with branches on GitHub and practicing git commands. It also involves using fetch() and promises in JavaScript, along with Date() objects. The goal of this project is to provide a practical example of using these concepts to build a useful web application.
+
+Oscar- If/else if statements will be the death of me. I spent a lot of time searching a lot of sites for info but in the end, chatting with the animal team was the best way to solve the issue. I couldn't get the weather icons to show up for all of the days, but maybe in the future!
+
+Malvina- The project involves working with branches on GitHub and practicing git commands. It also involves using fetch() and promises in JavaScript, along with Date() objects. The goal of this project is to provide a practical example of using these concepts to build a useful web application.
+
 
 ## View it live
 
--- script.js
+++ script.js
@@ -4,7 +4,7 @@ const forecastDate = document.getElementById('forecastDate')
 const forecastTemp = document.getElementById('forecastTemp')
 const forecastFeels = document.getElementById('forecastFeels')
 const forecastMinMax = document.getElementById('forecastMinMax')
-
+const showWeatherIcons = document.getElementById('weatherIcon')
 
 // Get the user's current location
 navigator.geolocation.getCurrentPosition((position) => {
@@ -23,13 +23,10 @@ navigator.geolocation.getCurrentPosition((position) => {
         <h1> ${json.name} </h1>
         <h2> ${(Math.round(json.main.temp))}°C</h2>
         <h3> ${json.weather[0].main}</h3>
-<<<<<<< HEAD
-        <h4> 🌅 sunrise ${sunrise.toLocaleTimeString([], { hour: 'numeric', minute: 'numeric', hour12: true })}</h4>
-        <h4> 🌇 sunset ${sunset.toLocaleTimeString([], { hour: 'numeric', minute: 'numeric', hour12: true })}</h4>
-=======
+
         <h4> <img src="./images/sunrise.png" alt="Sunrise"> ${sunrise.toLocaleTimeString([], { hour: 'numeric', minute: 'numeric', hour12: true })}</h4>
         <h4> <img src="./images/sunset.png" alt="Sunset"> ${sunset.toLocaleTimeString([], { hour: 'numeric', minute: 'numeric', hour12: true })}</h4>
->>>>>>> play-with-styling
+
       `;
     });
 
@@ -38,18 +35,36 @@ navigator.geolocation.getCurrentPosition((position) => {
     .then((response) => response.json())
     .then((json) => {
       // Filter the forecast data to only show the information for 12:00 PM each day
-      const filteredForecast = json.list.filter((item) => item.dt_txt.includes('12:00'));
 
             console.log(filteredForecast)
             filteredForecast.forEach((weeklyForecast) => {
                 forecastTemp.innerHTML += `<span>${weeklyForecast.main.temp.toFixed(0)}\u00B0C</span> `
                 forecastFeels.innerHTML += `<span>${weeklyForecast.main.feels_like.toFixed(0)}\u00B0C</span> `
                 forecastMinMax.innerHTML += `<span>${weeklyForecast.main.temp_min.toFixed(0)}\u00B0C/${weeklyForecast.main.temp_max.toFixed(0)}\u00B0C</span> `
+            const conditions = (weeklyForecast.weather[0].main);
+               const showWeatherIcons = () => { 
+                
+                  if (conditions === "Clear") {
+                    document.getElementById("gifIcon").src = "./images/sun-weather.gif";
+                  } else if (conditions === "Rain") {
+                    document.getElementById("gifIcon").src = "./images/rainy-weather.gif";
+                  } else if (conditions === "Snow") {
+                    document.getElementById("gifIcon").src = "./images/snow-storm-weather.gif";
+                  }
+                  else {
+                    document.getElementById("gifIcon").src = "./images/cloudy-weather.gif";
+                  }
+                  
+                  };  
+                  showWeatherIcons(conditions)    
             })
             filteredForecast.forEach((day) => {
                 const date = new Date(day.dt * 1000)
                 let dayName = date.toLocaleDateString("en-US", {weekday: "short"})
                 forecastDate.innerHTML += `<span>${dayName}</span>`
                 })
+              })
+               
+                 
              })
-            })
+
