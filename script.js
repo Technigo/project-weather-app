ec12ee7462c24d6c2d2f94285f9dc8672afdd92b
Maja Zimnoch <majamargareta@Majas-MacBook-Air.local>
2023-02-25 14:16:58 +0100

    built hamburger menu and fix the feather function

--- script.js
+++ script.js
@@ -10,6 +10,23 @@ let changeCityMalmo = document.getElementById("cityMalmo");
 let changeCityStockholm = document.getElementById("cityStockholm");
 let mainWeatherPicture = document.getElementById("mainWeatherPicture");
 
+/* hamburger part */
+const hamburger = document.querySelector(".hamburger");
+const navMenu = document.querySelector(".nav-menu");
+
+hamburger.addEventListener("click", () => {
+  hamburger.classList.toggle("active");
+  navMenu.classList.toggle("active");
+})
+
+document.querySelectorAll(".nav-link").forEach(n => n.
+addEventListener("click", () => {
+  hamburger.classList.remove("active");
+  navMenu.classList.remove("active");
+}))
+
+/* hamburger part */
+
 const fetchCurrentWeather = (currentCity) => {
   let currentWeatherLink = `https://api.openweathermap.org/data/2.5/weather?q=${currentCity},Sweden&units=metric&APPID=d73aa5f2cfee2a35632856b10b30a458`;
 
@@ -22,6 +39,7 @@ const fetchCurrentWeather = (currentCity) => {
       renderCurrentWeather(data);
       renderSunriseSunset(data);
       currentTime(data);
+      updateWeatherPicture(data);
     })
     .catch((error) => {
       console.log(error);
@@ -118,6 +136,44 @@ const renderWeeklyWeather = (data) => {
   });
 };
 
+
+
+
+
+
+
+const updateWeatherPicture = (data) => {
+
+  // background changes depending on the weather type
+  let mainWeather = data.weather[0].main;
+  console.log(updateWeatherPicture)
+
+  if (mainWeather === "Clear") {
+    mainWeatherPicture.innerHTML = `
+    <img id="" class="" src="/assets/day-sunny.jpg">`
+
+  } else if (mainWeather === "Thunderstorm") {
+    mainWeatherPicture.innerHTML = `
+    <img id="" class="" src="/assets/night.jpg"/>`
+
+  } else if (mainWeather === "Drizzle" | "Rain") {
+    mainWeatherPicture.innerHTML = `
+    <img id="" class="" src="/assets/night.jpg"/>`
+
+  } else if (mainWeather === "Snow") {
+    mainWeatherPicture.innerHTML = `
+    <img id="" class="" src="/assets/snow.jpg">`
+
+  } else if (mainWeather === "Clouds") {
+  mainWeatherPicture.innerHTML = `
+  <img id="" class="" src="./assets/cloudy.jpg"/>`
+
+  } else {
+  mainWeatherPicture.innerHTML = `
+  <img id="" class="" src="./assets/day.jpg"/>`
+}
+};
+
 fetchCurrentWeather(currentCity);
 fetchWeeklyWeather(currentCity);
 
@@ -129,55 +185,5 @@ changeCityStockholm.addEventListener("click", () => {
   fetchCurrentWeather("Stockholm");
   fetchWeeklyWeather("Stockholm");
 });
-
-const updateWeatherPicture = (data) => {
-
-  // background changes depending on the weather type
-  let weatherImage = data.weather[0].main;
-  
-  
-  if (weatherImage === "Clear") {
-    // https://openweathermap.org/weather-conditions The types should match the main types from this web?
-    mainWeatherPicture.innerHTML = `
-    <img id="" class="" src="/assets/sun.jpg">`
-  } else if (weatherImage === "Thunderstorm") {
-    mainWeatherPicture.innerHTML = `
-    <img id="" class="" src=""/>`
-  } else if (weatherImage === "Drizzle") {
-    mainWeatherPicture.innerHTML = `
-    <img id="" class="" src="./assets/sun.jpg"/>`
-  } else if (weatherImage === "Rain") {
-    mainWeatherPicture.innerHTML = `
-    <img id="" class="" src="./assets/sun.jpg"/>`
-  } else if (weatherImage === "Snow") {
-    mainWeatherPicture.innerHTML = `
-    <img id="" class="" src="/assets/cloudy.jpg">`
-} else if (weatherImage === "Clouds") {
-  mainWeatherPicture.innerHTML = `
-  <img id="" class="" src="./assets/sun.jpg"/>`
-} else {
-  mainWeatherPicture.innerHTML = `
-  <img id="" class="" src="./assets/sun.jpg"/>`
-}
-}
-
-
-const time = new Date().toLocaleString([], {
-  
-})
-// Background change
-const dayToNight = () => {
-  if (time < sunrise && time > sunset) {
-    mainWeather.style.background = `
-    
-    `;
-  }
-};
-
-dayToNight();
-
-
-fetchWeather();
-
 
 
