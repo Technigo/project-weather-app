fdfceac5b02f49b7be4472a923cbb719d9609a1b
Johanna Leonsson <johanna@leonsson.se>
2023-02-26 19:55:28 +0100

    fixed weather description and comments in js and cleaned up styling in css

--- script.js
+++ script.js
@@ -2,6 +2,7 @@ const sunrise = document.getElementById("sunrise");
 const fiveDaysForecast = document.getElementById("fiveDaysForecast");
 const sunriseTime = document.getElementById("sunrise-time");
 const sunsetTime = document.getElementById("sunset-time");
+const weatherContainer = document.getElementById("weatherDescription");
 
 //making the API to a variable instead to make the code easier to read
 const API_today =
@@ -21,10 +22,7 @@ fetch(API_today)
     console.log(round);
 
     console.log(json.weather[0].description);
-
-   // sunrise.innerHTML += `<p>City: ${json.name}</p>`;
-    sunrise.innerHTML += `<p>Temp: ${round} °C</p>`;
-   // sunrise.innerHTML += `<p>Weather: ${json.weather[0].description}</p>`;
+    sunrise.innerHTML += `<p>Temp ${round} °C  |  ${json.weather[0].description}</p>`;
   });
 
 //Variable allowing Stockholm to be the city.
@@ -50,10 +48,8 @@ const getSunData = () => {
       });
 
       //HTML modifier.
-      sunriseText.innerHTML += `
-                                <p class="time-data">Sunrise ${sunriseShort}</p>`;
-      sunsetText.innerHTML += `
-                                <p class="time-data">Sunset ${sunsetShort}</p>`;
+      sunriseText.innerHTML += `<p class="time-data">Sunrise ${sunriseShort}</p>`;
+      sunsetText.innerHTML += `<p class="time-data">Sunset ${sunsetShort}</p>`;
     })
     //Collecting errors.
     .catch((err) => {
@@ -63,25 +59,69 @@ const getSunData = () => {
 //Invoke function.
 getSunData();
 
-// Väder i Stockholm kommande fem dagar
+
+//Weather description of today 
+const change = (json) => {
+  if (json.weather[0].main === "Clouds") {
+    weatherContainer.innerHTML = `
+      <img src="/Designs/Design-2/icons/noun_Cloud_1188486.svg" alt="cloud"/>
+      <h1>Light a fire and get cosy. ${json.name} is looking grey today.</h1>
+    `;
+    document.body.style.backgroundColor = "#F4F7F8";
+    document.body.style.color = "#F47775";
+  } else if (json.weather[0].main === "Rain") {
+    weatherContainer.innerHTML = `
+      <img src="/Designs/Design-2/icons/noun_Umbrella_1188485.svg" alt="umbrella"/>
+      <h1>Don't forget your umbrella. It's wet in ${json.name} today.</h1>
+    `;
+    document.body.style.backgroundColor = "#A3DEF7";
+    document.body.style.color = "#164A68";
+  } else if (json.weather[0].main === "Sunny") {
+    weatherContainer.innerHTML = `
+      <img src="/Designs/Design-2/icons/noun_Sunglasses_2055147.svg" alt="sunglasses"/>
+      <h1>Get your sunnies on. ${json.name} is looking rather great today.</h1>
+    `;
+    document.body.style.backgroundColor = "#F7E9B9";
+    document.body.style.color = "#2A5510";
+  } else {
+    weatherContainer.innerHTML = `    
+        <h1> Nothing exceptional with the weather in ${json.name} right now 🤷</h1>`;
+      document.body.style.backgroundColor = "#f8ede5";
+      document.body.style.color = "#616667";
+  }
+};
+
+fetch(API_today)
+  .then((response) => {
+    return response.json();
+  })
+  .then((json) => {
+    change(json);
+  })
+  .catch((err) => {
+    console.log(`error caught:`, err);
+  });
+
+
+//Weather in Stockholm for five days
 const fiveDayForecast = () => {
-  fetch(API_forecast) //forecast-delen av API
+  fetch(API_forecast) //fetch the forecast-api
     .then((response) => {
       return response.json();
     })
     .then((data) => {
       const filterForecast = data.list.filter((item) =>
-        item.dt_txt.includes("00:00:00")
+        item.dt_txt.includes("12:00")
       );
 
       filterForecast.forEach((item) => {
         const date = new Date(item.dt * 1000);
-        const weatherTemperature = item.main.temp.toFixed(1); // 1 decimaltal
+        const weatherTemperature = item.main.temp.toFixed(1); //1 decimal number
 
         fiveDaysForecast.innerHTML += `
           <div class="week-days"> 
             <p>${new Date(date).toLocaleDateString("en", {
-              weekday: "short", //Short = Skriver bara ut de tre första bokstäverna i dagarna. Tex Fre för Fredag.
+              weekday: "short", //Short = Only prints the first three letters of the days.
             })}
             </p>
             <p>${weatherTemperature}°</p> 
@@ -90,4 +130,4 @@ const fiveDayForecast = () => {
       });
     });
 };
-fiveDayForecast(); //Kallar på funktionen som heter fiveDayForecast
+fiveDayForecast();//Invoke function
