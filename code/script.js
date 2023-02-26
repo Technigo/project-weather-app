d9129ab01f0dd293299a8bb220d139b0f6ae2698
Alexander-Gabor <gaboralexanderalexgmail.com>
2023-02-26 22:36:34 +0100

    Recommended items updated, wind-speed added, & CSS updated

--- code/script.js
+++ code/script.js
@@ -5,7 +5,7 @@ const forecast = document.getElementById("forecast-section");
 const weatherBody = document.getElementById("weather-body");
 
 let weatherResults;
-let city = "Gothenburg" //Default city
+let city = "Gothenburg"; //Default city
 
 const fetchingWeather = () => {
   fetch(
@@ -18,56 +18,62 @@ const fetchingWeather = () => {
       console.log(json);
       weatherResults = json;
 
-      //Setting the different alternatives for the "city-weather" section
-      weatherDescription(weatherResults.weather[0].main,weatherResults.name)
-    })
-    const weatherDescription = (localWeatherToday, city) => {   
-      if (localWeatherToday === "Clear") {
-        cityWeather.innerHTML = `<div class= "Clear">
+      weatherDescription(weatherResults.weather[0].main, weatherResults.name);
+    });
+  //Setting the different alternatives for the "city-weather" section
+  const weatherDescription = (localWeatherToday, city) => {
+    if (localWeatherToday === "Clear") {
+      cityWeather.innerHTML = `<div class= "Clear">
                   <img class= "sunny" src="images/sun-glasses.png"/>
-                  <h1> Sunglass-up. ${city} is shining and so are you.</h1>
+                  <h1> Sunglass-up. ${city} is shining and so are you!</h1>
                     <div class= "todays-item">
                       <p>Recommended clothing item:</p>
-                      <img class= "item" src="images/keps.png"/>
+                      <div class= "items-wrapper">
+                        <img class= "item" src="images/keps.png"/> <img class= "item" src="images/tshirt.png"/>
+                      </div>
                     </div>
                   </div>`;
-        document.body.style.backgroundColor = "#F7E9B9";
-        document.body.style.color = "#2A5510";
-        citySelector.style.backgroundColor = "#F7E9B9";
-        citySelector.style.color = "#2A5510";
-      } else if (localWeatherToday === "Rain") {
-        cityWeather.innerHTML = `<div class= "rain">
+      document.body.style.backgroundColor = "#F7E9B9";
+      document.body.style.color = "#2A5510";
+      citySelector.style.backgroundColor = "#F7E9B9";
+      citySelector.style.color = "#2A5510";
+    } else if (localWeatherToday === "Rain") {
+      cityWeather.innerHTML = `<div class= "rain">
                   <img class= "rain" src="images/umbrella.png"/>
                   <h1> Fetch that umbrella. ${city} is crying today.</h1>
                     <div class= "todays-item">
                       <p>Recommended clothing item:</p>
-                      <img class= "item" src="images/beanie.png"/>
+                      <div class= "items-wrapper">
+                        <img class= "item" src="images/umbrellaitem.png"/> <img class= "item" src="images/raincoat.png"/> <img class= "item" src="images/raingloves.png"/>
+                      </div>
                     </div>
                   </div>`;
-        document.body.style.backgroundColor = "#A3DEF7";
-        document.body.style.color = "#164A68";
-        citySelector.style.backgroundColor = "#A3DEF7";
-        citySelector.style.color = "#164A68";
-      } else {
-        cityWeather.innerHTML = `<div class= "clouds">
+      document.body.style.backgroundColor = "#A3DEF7";
+      document.body.style.color = "#164A68";
+      citySelector.style.backgroundColor = "#A3DEF7";
+      citySelector.style.color = "#164A68";
+    } else {
+      cityWeather.innerHTML = `<div class= "clouds">
                   <img class= "rain" src="images/clouds.png"/>
                   <h1> Cuddle up! The ${city} sky is grey today.</h1>
                     <div class= "todays-item">
-                      <p>Recommended clothing item:</p>
-                      <img class= "item" src="images/gloves.png"/>
+                      <p>Recommended clothing items:</p>
+                      <div class= "items-wrapper">
+                        <img class= "item" src="images/gloves.png"/> <img class= "item" src="images/scarf.png"/> <img class= "item" src="images/beanie.png"/>
+                      </div>
                     </div>
                   </div>`;
-        document.body.style.backgroundColor = "#FBF4F4";
-        document.body.style.color = "#F47775";
-        citySelector.style.backgroundColor = "#FBF4F4";
-        citySelector.style.color = "#F47775";
-      }
-      console.log(localWeatherToday);
+      document.body.style.backgroundColor = "#FBF4F4";
+      document.body.style.color = "#F47775";
+      citySelector.style.backgroundColor = "#FBF4F4";
+      citySelector.style.color = "#F47775";
+    }
+    console.log(localWeatherToday);
 
-      console.log(weatherResults);
-      todaysWeather.innerHTML = `<p> ${
-        weatherResults.weather[0].description
-      } | ${Math.round(weatherResults.main.temp * 10) / 10}° </p>
+    console.log(weatherResults);
+    todaysWeather.innerHTML = `<p> ${weatherResults.weather[0].description} | ${
+      Math.round(weatherResults.main.temp * 10) / 10
+    }° </p>
       <p>
       sunrise ${new Date(weatherResults.sys.sunrise * 1000).toLocaleTimeString(
         [],
@@ -76,8 +82,11 @@ const fetchingWeather = () => {
       <p>sunset ${new Date(weatherResults.sys.sunset * 1000).toLocaleTimeString(
         [],
         { hour: "2-digit", minute: "2-digit" }
-      )}</p>`;
-    };
+      )}</p>
+      <img id= "wind" src="images/windspeed.png"/> <p>wind-speed | ${
+        weatherResults.wind.speed
+      }</p>`;
+  };
 };
 
 // Calling the fetching function for the first 2 sections, today's weather and city's weather
@@ -150,13 +159,13 @@ const fetchingWeatherForecast = () => {
     });
 };
 
-const selectCity = () =>{
-  city = citySelector.options[citySelector.selectedIndex].value
-  fetchingWeather()
+const selectCity = () => {
+  city = citySelector.options[citySelector.selectedIndex].value;
+  fetchingWeather();
   fetchingWeatherForecast();
-}
+};
 
 // Calling the fetching function for forecast
 fetchingWeatherForecast();
 
-citySelector.addEventListener("change", selectCity)
+citySelector.addEventListener("change", selectCity);
