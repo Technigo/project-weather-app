67bc3d2d1295f311ae653752591d06e8d0cef658
Your Name alexandrameija@hotmail.com <alexandrameija@hotmail.com.com>
2023-09-10 11:12:12 +0200

    -m

--- design/script.js
+++ design/script.js
@@ -1,11 +1,12 @@
-const container = document.getElementById()
+const container = document.getElementById('sthweather')
 
 fetch('https://api.openweathermap.org/data/2.5/weather?q=Stockholm,Sweden&units=metric&APPID=8fa7c461aec946fde31f330992fce9d6')
   .then((response) => {
     return response.json();
   })
   .then((json) => {
-    container.innerHTML = `<h1>This is the ${json.weather[0].description} in Stockholm right now</h1>`;
+    const cityName = json.name;
+    container.innerHTML = `<h1>Here's the weather in ${cityName} right now</h1>`;
 
     json.weather.forEach((param) => {
       container.innerHTML += `<p>${param.description}</p>`;
