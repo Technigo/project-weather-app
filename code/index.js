1b16b2431b65d2f83a60fc5f35d882b9ce21b721
Minadarabi <minaadabirii@gmail.com>
2023-09-12 03:23:02 -0700

    first commit

---
+++ code/index.js
@@ -0,0 +1,26 @@
+// Dom Section
+const tempElement = document.getElementById("temp")
+
+
+fetch("https://api.openweathermap.org/data/2.5/weather?q=Stockholm,Sweden&units=metric&APPID=64dc0a4bc655c3566178e4cae018559e")
+
+.then((response)=>{
+    return response.json()
+})
+.then ((data) =>{
+    console.log(data)
+    console.log(data.name)
+    console.log(data.main.temp)
+    console.log(data.wind.speed)
+    console.log(data.weather[0].description)
+    tempElement.innerHTML = data.main.temp;
+
+    // tempElement.appendChild(temp);
+
+    // const tempElement = document.getElementById("temp");
+    // tempElement.innerHTML = data.main.temp 
+})
+.catch ((err) => {
+    console.log(err)
+})
+
