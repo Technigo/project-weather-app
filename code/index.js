fc9fabdc869d0b46add5213485890505cb1087dd
Minadarabi <minaadabirii@gmail.com>
2023-09-12 08:27:07 -0700

    fixed fetch

--- code/index.js
+++ code/index.js
@@ -1,25 +1,37 @@
 // Dom Section
-const tempElement = document.getElementById("temp")
+const tempElement = document.getElementById("temp");
 
 
-fetch("https://api.openweathermap.org/data/2.5/weather?q=Stockholm,Sweden&units=metric&APPID=64dc0a4bc655c3566178e4cae018559e")
+const baseUrl = "https://api.openweathermap.org/data/2.5/weather?";
+const apiKey = "64dc0a4bc655c3566178e4cae018559e";
+const lat = "Stockholm";
+const lon = "Sweden";
+const URL = `${baseUrl}q=${lat},${lon}&units=metric&APPID=${apiKey}`
 
-.then((response)=>{
-    return response.json()
-})
-.then ((data) =>{
-    console.log(data)
+
+
+
+const fetchWeatherAsync = async () =>{
+  const response = await fetch(URL).catch((err) => console.log("my ERROR" , err));
+  const data = await response.json()
+  console.log(data)
+  updateHTML(data)
+}
+fetchWeatherAsync()
+
+// headers
+// const URLToAnotherAPI = "http"
+// const options = { method: GET headers:{ ApiKey:"" ApiHost:""}, body: JSON.stringfy(data)}
+// fetch(URLToAnotherAPI , options)
+
+const updateHTML = (data) =>{
+  console.log(data)
     console.log(data.name)
     console.log(data.main.temp)
     console.log(data.wind.speed)
     console.log(data.weather[0].description)
-    tempElement.innerHTML = data.main.temp;
+    tempElement.innerText = data.main.temp;
 
-    
 
-   
-})
-.catch ((err) => {
-    console.log(err)
-})
+}
 
