a3eac2a954afc560276ca80fe5abec7f6a0a0e39
BeckieMorton <beckam11@gmail.com>
2023-09-12 20:39:19 +1000

    added comments for structure in js file

--- code/script.js
+++ code/script.js
@@ -1,3 +1,20 @@
+//--------DOM selectors stored as short variables-------------//
+
+
+
+
+
+
+//---------------- Global Variables -------------------------//
+
+
+
+
+
+//----------- Functions after this comment -----------------//
+
+
+
 // Making an API request using fetch
 fetch('https://api.example.com/data')
     .then(response => response.json())
@@ -13,3 +30,7 @@ fetch('https://api.example.com/data')
 
 // For example, to get the current weather in Stockholm, you can use the URL below. Remember to replace YOUR_API_KEY with the API key //you copied from your dashboard.
 // https://api.openweathermap.org/data/2.5/weather?q=Stockholm,Sweden&units=metric&APPID=YOUR_API_KEY
+
+
+
+//-------------------- All Event Listeners --------------------//
