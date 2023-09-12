34cae485c980a06c7e9c3df3f7770684093c7ddb
MikoZhu <huadanzhu01@gmail.com>
2023-09-12 10:41:06 +0200

    finish the sunrise and sunset

--- script.js
+++ script.js
@@ -7,12 +7,26 @@ const todayWeather = () => {
     })
     .then((data) => {
       console.log(data);
-      const city = data.name;
-      console.log(city); //Stockholm
-      const weather = data.weather[0].description;
-      console.log(weather); //Clouds
-      const temp = Math.round(data.main.temp * 10) / 10;
-      console.log(temp);
+      const city = data.name; //Stockholm
+      const weather = data.weather[0].description; //Clouds
+      const temp = Math.round(data.main.temp * 10) / 10; //the temperature data to one decimal point, *100)/100 two decimal point
+      //Declare variable for the time of sunrise/sunset
+      const unixSunrise = data.sys.sunrise;
+      const unixSunset = data.sys.sunset;
+      //To get sunrise/sunset time in hours:minutes:seconds
+      const sunrise = new Date(unixSunrise * 1000);
+      const sunset = new Date(unixSunset * 1000);
+
+      //Declare new variable to show only hh:mm
+      const sunriseTime = sunrise.toLocaleTimeString([], {
+        timeStyle: "short",
+      });
+      const sunsetTime = sunset.toLocaleTimeString([], {
+        timeStyle: "short",
+      });
+      // print out the result
+      console.log(sunriseTime);
+      console.log(sunsetTime);
     })
     .catch((error) => {
       console.log("caught error", error);
