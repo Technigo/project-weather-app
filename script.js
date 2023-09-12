cff6f9ca453dca8d9d6e5828e65df0e7a09929e7
Emmy Dieden <emmydieden@Emmys-MacBook-Pro.local>
2023-09-12 14:13:14 +0200

    rounded numbers

--- script.js
+++ script.js
@@ -43,11 +43,11 @@ fetch('https://api.openweathermap.org/data/2.5/forecast?q=Stockholm,Sweden&units
     console.log(json.city.name)
 
      // Filter the list for elements with a timestamp of 12:00:00
-     const elements12 = json.list.filter((el) => el.dt_txt.includes("12:00:00"));
+     const weatherAt12 = json.list.filter((el) => el.dt_txt.includes("12:00:00"));
     //elements12 är en array med 5 objekt. Innehåller all väderinfo kl 12, 5 olika dagar
-     console.log(elements12)
+     console.log(weatherAt12)
 
-     const dates_text = elements12.map((el) => el.dt_txt)
+     const dates_text = weatherAt12.map((el) => el.dt_txt)
      // dates_text är en array med 5 strings: ex; '2023-09-12 12:00:00'
      console.log(dates_text)
 
@@ -68,20 +68,21 @@ fetch('https://api.openweathermap.org/data/2.5/forecast?q=Stockholm,Sweden&units
      let day7 = weekday[DayNumber[6]];
      //Här skrivs day som ex Weekday[2] = Tuesday utifrån arrayn 'DayNumber'
 
-     console.log(day1, day2)
-     
 
      // Extract temperature values for the filtered elements
-     const temperaturesAt12 = elements12.map((el) => el.main.temp);
+     const temperaturesAt12 = weatherAt12.map((el) => Math.round(el.main.temp))
+    
+     const feelsLike = weatherAt12.map((el) => Math.round(el.main.feels_like))
  
      console.log(`tempat12 ${temperaturesAt12}`);
+     console.log(feelsLike)
 
      forecastSection.innerHTML += `
-        ${day1}: ${temperaturesAt12[0]}°C
-     <br>${day2}: ${temperaturesAt12[1]}°C
-     <br>${day3}: ${temperaturesAt12[2]}°C
-     <br>${day4}: ${temperaturesAt12[3]}°C
-     <br>${day5}: ${temperaturesAt12[4]}°C `
+         ${day1}: ${temperaturesAt12[0]}°C, ${feelsLike[0]}°C
+     <br>${day2}: ${temperaturesAt12[1]}°C, ${feelsLike[1]}°C
+     <br>${day3}: ${temperaturesAt12[2]}°C, ${feelsLike[2]}°C
+     <br>${day4}: ${temperaturesAt12[3]}°C, ${feelsLike[3]}°C
+     <br>${day5}: ${temperaturesAt12[4]}°C, ${feelsLike[4]}°C `
    
      
     })
