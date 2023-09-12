b80a1814a6e3af8276522083d21f8510debc36d2
BeckieMorton <beckam11@gmail.com>
2023-09-12 20:28:26 +1000

    added code folder with html and js with fetch

---
+++ code/script.js
@@ -0,0 +1,15 @@
+// Making an API request using fetch
+fetch('https://api.example.com/data')
+    .then(response => response.json())
+    .then(data => {
+        // Process the API response data
+        console.log(data)
+    })
+    .catch(error => {
+        // Handle any errors that occurred during the API request
+        console.error('Error:', error)
+    })
+
+
+// For example, to get the current weather in Stockholm, you can use the URL below. Remember to replace YOUR_API_KEY with the API key //you copied from your dashboard.
+// https://api.openweathermap.org/data/2.5/weather?q=Stockholm,Sweden&units=metric&APPID=YOUR_API_KEY
