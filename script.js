945fac472defda866a07c4f745f4f33db570777d
Idah Collin <idah_collin@hotmail.com>
2023-09-12 10:11:49 +0200

    test feature

--- script.js
+++ script.js
@@ -6,28 +6,28 @@ const weatherType = document.getElementById('weatherType')
 const fetchWeather = () => {
 
     fetch('https://api.openweathermap.org/data/2.5/weather?q=Stockholm,Sweden&units=metric&APPID=fd582670436692008725c351eb4985b0')
-    .then((response) => {
-        return response.json()
-    })
-    .then((json) => {
-        console.log(json)
-        console.log(json.name)
-        console.log((json.main.temp).toFixed(1))
-        console.log(json.weather[0].description) //här kanske vi får ändra! om det kommer fler objekt i arrayen.
-        cityName.innerHTML = `STAD: ${json.name}`
-        temperature.innerHTML =`TEMPERATUREN ÄR: ${(json.main.temp).toFixed(1)}`
-        weatherType.innerHTML =`BESKRIVNING: ${json.weather[0].description}`
-        //jennys del sunrise sunset
+        .then((response) => {
+            return response.json()
+        })
+        .then((json) => {
+            console.log(json)
+            console.log(json.name)
+            console.log((json.main.temp).toFixed(1))
+            console.log(json.weather[0].description) //här kanske vi får ändra! om det kommer fler objekt i arrayen.
+            cityName.innerHTML = `STAD: ${json.name}`
+            temperature.innerHTML = `TEMPERATUREN ÄR: ${(json.main.temp).toFixed(1)}`
+            weatherType.innerHTML = `BESKRIVNING: ${json.weather[0].description}`
+            //jennys del sunrise sunset
 
 
 
-        //den andra delen weather forecast
-    })
+            //den andra delen weather forecast hej hej
+        })
 
 
 
 
 
-    
+
 }
 fetchWeather()
