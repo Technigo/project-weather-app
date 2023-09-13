8faae501be81afe78667d5cbd77e679433bb051d
Sheryll-Ann <shey.pc@gmail.com>
2023-09-13 20:44:42 +0200

    attempt-input values in table- 1whole colum or row

--- script.js
+++ script.js
@@ -3,8 +3,23 @@ const container = document.getElementById('weather');
 const cityInput = document.getElementById('city-input');
 const searchButton = document.getElementById('search-button');
 const swipeButton = document.getElementById('swipe-button');
+const weatherTableRow = document.getElementById('forecast-tr')
 
 const apiKey = '30497ceff63316bea65ec674ac0ba4c7';
+
+//Reusable functions:
+//Convert a Unix timestamp into "hour:min" format
+const formattedTime = (timestamp) => {
+    sunStatusDate = new Date(timestamp * 1000);
+    const hours = sunStatusDate.getHours();
+    const minutes = sunStatusDate.getMinutes();
+    const time = `
+    ${hours.toString().padStart(2, '0')}:${minutes.toString().padStart(2, '0')}`;
+    return (time)
+}
+
+
+//----------------------  Part1  ------------------------------------------
 //Fetch current data for when entering the page
 getWeatherData = (city) => {
 
@@ -14,25 +29,16 @@ getWeatherData = (city) => {
         .then((currentWeatherJson) => {
             console.log(currentWeatherJson);
 
-            //Convert a Unix timestamp into "hour:min" format
-            const formattedTime = (timestamp) => {
-                sunStatusDate = new Date(timestamp * 1000);
-                const hours = sunStatusDate.getHours();
-                const minutes = sunStatusDate.getMinutes();
-                const time = `
-                ${hours.toString().padStart(2, '0')}:${minutes.toString().padStart(2, '0')}`;
-                return (time)
-            }
-
             // Display current weather information for the entered city
             currentCity.innerHTML += `
             <h1>${currentWeatherJson.main.temp.toFixed(1)}°C </h1>
             `;
             currentCity.innerHTML += `
-            `
-            currentCity.innerHTML += `
             <h2> ${currentWeatherJson.name}</h2>
             `;
+            currentCity.innerHTML += `
+            <h2>${formattedTime(currentWeatherJson.dt)}</h2>
+            `
             currentCity.innerHTML += `
             <p> ${currentWeatherJson.weather[0].description}</p>
             `;
@@ -49,6 +55,7 @@ getWeatherData = (city) => {
         });
 }
 
+//--------------------Part 2  ---------------------------------
 weeklyForecast = (city) => {
     // Fetch current weather data for the entered city
     // Fetch 5-day weather forecast for the entered city
@@ -60,26 +67,33 @@ weeklyForecast = (city) => {
             console.log(forecastJson);
 
             // Display the 5-day weather forecast for the entered city 
-            container.innerHTML = ''; //clear content 
+            weatherTableRow.innerHTML = ''; //clear content 
 
             const processedDates = []; //keep track of displayed dates just like with the dogs!!
-
+            // weatherTable.innerHTML += `<tbody>`
+            // weatherTableRow.innerHTML += `<tr>`
             forecastJson.list.map((forecast) => {
                 const dateTime = new Date(forecast.dt * 1000); // Convert timestamp from seconds to ms adapted to Javascript
                 const date = dateTime.toLocaleDateString(); // Formated date accordingly to the user's locale
 
                 // Display date, weather description, and temperature in Celsius
                 if (!processedDates.includes(date)) {//if the date is not in the processedDates array
-                    container.innerHTML += `<p>Date: ${date}</p>`;
-                    container.innerHTML += `<img src="https://openweathermap.org/img/wn/${forecast.weather[0].icon}@2x.png">`;
-                    container.innerHTML += `<p>${forecast.weather[0].description}</p>`;
-                    container.innerHTML += `<p>Temperature: ${forecast.main.temp} °C</p>`;
-                    container.innerHTML += `<p>Wind: ${forecast.wind.speed} m/s</p>`;
-                    container.innerHTML += '<hr>'; // Add a horizontal line for separation
+                    weatherTableRow.innerHTML += `
+                    <td>${date}</td>`;
+                    weatherTableRow.innerHTML += `
+                    <td> <img src="https://openweathermap.org/img/wn/${forecast.weather[0].icon}@2x.png"> </td>`;
+                    // container.innerHTML += `<p>${forecast.weather[0].description}</p>`;
+                    weatherTableRow.innerHTML += `
+                    <td>${forecast.main.temp.toFixed(1)} °C</td>`;
+                    weatherTableRow.innerHTML += `
+                    <td>${forecast.wind.speed} m/s</td>`;
 
                     processedDates.push(date); // push method to array
                 }
             });
+            // weatherTableRow.innerHTML += '</tr>';
+            // weatherTable.innerHTML += '</tbody>';
+
         })
         .catch((error) => {
             console.error('Error', error)
