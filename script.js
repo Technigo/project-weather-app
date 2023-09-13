e5aa42cce30e6d332dcae8dc2e4fb54240c7cd3f
hali <hali.olad@icloud.com>
2023-09-13 14:35:59 +0200

    step 1

---
+++ script.js
@@ -0,0 +1,8 @@
+const city = document.getElementById('') //skriva in id när HTML är klar
+const weather = document.getElementById('')
+const temperature = document.getElementById('')
+
+const API_KEY = "b881032f7a405f3e6e05ebbfb98e3e49"
+
+
+const url = "https://api.openweathermap.org/data/2.5/weather?q=Stockholm,Sweden&units=metric&APPID=YOUR_API_KEY"
