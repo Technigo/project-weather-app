d65ca41baa51bfff1932e03a04ea35a1060cd74d
Andrej <72137459+andrkavr@users.noreply.github.com>
2023-09-13 17:08:01 +0200

    lean up code a bit. Feature weatherForecast completed.

--- script.js
+++ script.js
@@ -6,68 +6,13 @@ const forecast = document.querySelector(".forecast");
 const API_KEY = "64856650e6321cbb411769554b46b8ad";
 // Reserve API KEY = "421db630ea3e3aeb0cb64db6a500c27b"
 
-const url =
-  "https://api.openweathermap.org/data/2.5/weather?q=Stockholm,Sweden&units=metric&APPID";
+let cityName = "Orebro";
+const url = `https://api.openweathermap.org/data/2.5/weather?q=${cityName}&units=metric&APPID=${API_KEY}`;
 
-const API_CALL = `${url}=${API_KEY}`;
+const API_CALL = `${url}`;
 
-// fetch(`${API_CALL}`)
-//   .then((response) => response.json())
-//   .then((data) => {
-//     console.log(data);
-//     let localTime = new Date((data.dt + data.timezone) * 1000);
-//     console.log(localTime.toUTCString());
-//     city.innerHTML = `${data.name}`;
-//     weather.innerHTML = `${data.weather[0].description}`;
-//     temperature.innerHTML = `${data.main.temp}`;
-//   });
-
-// const forecast = () => {
-//   let cityName = "Tokyo";
-
-//   //Functionality for searching for a specific city's weather
-//   /* --------------------------------------------------
-//     const citySearch = `https://api.openweathermap.org/data/2.5/weather?q=${cityName}&appid=${API_KEY}`;
-//          --------------------------------------------------*/
-//   fetch(
-//     `https://api.openweathermap.org/data/2.5/forecast?q=${cityName}&units=metric&appid=${API_KEY}`
-//   )
-//     .then((response) => response.json())
-//     .then((data) => {
-//       const localTimeZone = data.city.timezone;
-//       console.log(data);
-//       let lat = data.city.coord.lat;
-//       let lon = data.city.coord.lon;
-//       const part = "current,minutely,hourly,alerts";
-//       fetch(
-//         `https://api.openweathermap.org/data/2.5/onecall?lat=${lat}&lon=${lon}&units=metric&cnt=6&exclude=${part}&appid=${API_KEY}`
-//       )
-//         .then((response) => response.json())
-//         .then((data) => {
-//           let localTime = new Date((data.daily.dt + localTimeZone) * 1000);
-//           console.log(data);
-//           // console.log(localTime);
-//           // console.log(`The coming week's weather in ${cityName}:`);
-//           //working display of daily dates
-//           data.daily.forEach((day) => {
-//             let converted = new Date(day.dt * 1000);
-//             let dayMax = day.temp.max.toFixed(0);
-//             let dayMin = day.temp.min.toFixed(0);
-//             let weekday = converted.toUTCString().substring(0, 3);
-//             // console.log(converted.toUTCString());
-//             // console.log(`${weekday} ${dayMax}°C / ${dayMin}°C`);
-//           });
-//         });
-//     });
-// };
-// forecast();
-
-const forecast2 = () => {
-  let cityName = "Tokyo";
-
-  fetch(
-    `https://api.openweathermap.org/data/2.5/weather?q=${cityName}&units=metric&APPID=${API_KEY}`
-  )
+const apiForecast = () => {
+  fetch(API_CALL)
     .then((response) => response.json())
     .then((data) => {
       console.log(data);
@@ -75,6 +20,10 @@ const forecast2 = () => {
       let lon = data.coord.lon;
       let localTime = new Date((data.dt + data.timezone) * 1000);
       let subbedTime = localTime.toUTCString().substring(17, 22);
+      city.innerHTML = `${data.name}`;
+
+      weather.innerHTML = `${data.weather[0].description}`;
+      temperature.innerHTML = `${data.main.temp.toFixed(1)}`;
       console.log(`Local Time is: ${subbedTime}`);
       console.log(localTime.toUTCString());
       const part = "current,minutely,hourly,alerts";
@@ -84,10 +33,6 @@ const forecast2 = () => {
         .then((response) => response.json())
         .then((data) => {
           console.log(data);
-          // console.log(localTime);
-          // console.log(`The coming week's weather in ${cityName}:`);
-          //working display of daily dates
-
           //Counter for days received from array
           let counter = 0;
 
@@ -104,7 +49,7 @@ const forecast2 = () => {
                       <div>
                       <p>${weekday} </p>
                       <p>${dayMax}°C / ${dayMin}°C </p>
-                      <p>${dailyWeather} </p> <img src="http://openweathermap.org/img/wn/${iconLink}@2x.png" alt="weather icon">
+                      <p>${dailyWeather} </p> <img src="http://openweathermap.org/img/wn/${iconLink}@2x.png" alt="weather-icon">
                       </div>`;
               console.log(
                 `${weekday} ${dayMax}°C / ${dayMin}°C  ${dailyWeather}`
@@ -112,21 +57,7 @@ const forecast2 = () => {
               counter++;
             }
           });
-
-          //   data.daily.forEach((day) => {
-          //     let converted = new Date(day.dt * 1000);
-          //     let dayMax = day.temp.max.toFixed(0);
-          //     let dayMin = day.temp.min.toFixed(0);
-          //     let weekday = converted.toUTCString().substring(0, 3);
-          //     // console.log(converted.toUTCString());
-          //     if (counter < 5) {
-          //       console.log(`${weekday} ${dayMax}°C / ${dayMin}°C`);
-          //       counter++;
-          //     }
-          //   });
         });
     });
 };
-forecast2();
-// .throw((error) => console.log(error));
-//.toFixed(1)
+apiForecast();
