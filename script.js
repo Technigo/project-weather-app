99959d4740006ed6ba8f27a4f3744aeb461b9a4d
FridafridaL <fridus.lindskog@gmail.com>
2023-09-13 10:26:12 +0200

    step2

--- script.js
+++ script.js
@@ -1,4 +1,7 @@
 const container = document.getElementById(`weather`);
+const hamburgerMenu = document.getElementById("hamburger-menu");
+const navMenu = document.getElementById("nav-menu");
+const weatherIcon = document.getElementById("weather-icon");
 
 fetch(
   "https://api.openweathermap.org/data/2.5/weather?q=Stockholm,Sweden&units=metric&APPID=b0bce0ead37d18acc13ad506864e75ac"
@@ -12,9 +15,38 @@ fetch(
     const roundedTemperature = parseFloat(json.main.temp).toFixed(1);
     //const temperature = Math.round(json.main.temp);
     container.innerHTML += `
-    <h1>${roundedTemperature}°C</h1>
+    <h1>${roundedTemperature} °C</h1>
     <h2>${json.name}</h2>
+    <p>${json.weather[0].description}</p>
     
     
     `;
   });
+/*
+const fetchWeatherData = () => {
+  // ... your fetch and data processing code ...
+
+  // Use the weather description to set the weather icon
+  const iconFileName = weatherIcons[currentWeatherDescription];
+  if (iconFileName) {
+    // Set the src attribute with the relative path to the image
+    weatherIcon.src = `images/${iconFileName}`;
+    weatherIcon.alt = currentWeatherDescription;
+  } else {
+    // If the weather description is not in the mapping, use a default icon
+    weatherIcon.src = "images/default.png";
+    weatherIcon.alt = "Default Icon";
+  }
+};
+
+// Invoke the fetchWeatherData function to fetch and display weather data
+fetchWeatherData();
+*/
+hamburgerMenu.addEventListener("click", function () {
+  console.log(`hamburger menu`);
+  if (navMenu.style.display === "block") {
+    navMenu.style.display = "none";
+  } else {
+    navMenu.style.display = "block";
+  }
+});
