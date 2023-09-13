d199b0aaf861800e35a5769448129eec7a654942
Carolina Luna <kroluna@gmail.com>
2023-09-13 15:55:16 +0200

    sunrise sunset unfinish

--- script.js
+++ script.js
@@ -3,8 +3,10 @@ const hamburgerMenu = document.getElementById("hamburger-menu");
 const navMenu = document.getElementById("nav-menu");
 const weatherIcon = document.getElementById("weather-icon");
 
+
+
 fetch(
-  "https://api.openweathermap.org/data/2.5/weather?q=Stockholm,Sweden&units=metric&APPID=b0bce0ead37d18acc13ad506864e75ac"
+  "https://api.openweathermap.org/data/2.5/weather?q=Stockholm,Sweden&units=metric&APPID=168451996f01476589314aaee8750993"
 )
   .then((response) => {
     return response.json();
@@ -17,6 +19,7 @@ fetch(
     container.innerHTML += `
     <h1>${roundedTemperature} °C</h1>
     <h2>${json.name}</h2>
+    <p>Sunrise: ${json.sunriseTime}, Sunset: ${json.sunsetTime}</p>
     <p>${json.weather[0].description}</p>
     <p>${json.weather[0].icon}<p/>
     `;
@@ -50,3 +53,41 @@ hamburgerMenu.addEventListener("click", function () {
     navMenu.style.display = "block";
   }
 });
+
+
+fetch("https://api.openweathermap.org/data/2.5/weather?q=Stockholm,Sweden&units=metric&APPID=168451996f01476589314aaee8750993")
+  .then(response => response.json())
+  .then((json) => {
+    /*if(json.sys) {
+      displaySunriseSunset(json.sys.sunrise, json.sys.sunset);
+    } else {
+      console.error('sys property not found in the response:', json);
+    }
+  })
+  .catch(error => console.error('Error:', error));*/
+
+  const displaySunriseSunset = (sunriseTimestamp, sunsetTimestamp) => {
+    const sunriseDate = new Date(sunriseTimestamp * 1000);
+    const sunsetDate = new Date(sunsetTimestamp * 1000);
+  
+    const sunriseTime = `${sunriseDate.getHours()}:${sunriseDate.getMinutes().toString().padStart(2, '0')}`;
+    const sunsetTime = `${sunsetDate.getHours()}:${sunsetDate.getMinutes().toString().padStart(2, '0')}`;
+  
+    
+    console.log(`Sunrise: ${sunriseTime}, Sunset: ${sunsetTime}`);
+
+    document.getElementById('sunrise-time').textContent = sunriseTime;
+    document.getElementById('sunset-time').textContent = sunsetTime;
+  }
+
+
+
+
+
+/*fetch("https://api.openweathermap.org/data/2.5/weather?q=Stockholm,Sweden&units=metric&APPID=168451996f01476589314aaee8750993")
+.then(response => response.json())
+.then((json) => {
+  displaySunriseSunset(json.sys.sunrise, json.sys.sunset);
+  
+})
+.catch(error => console.error('Error:', error));*/
