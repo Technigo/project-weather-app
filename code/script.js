a83d0c4ea6eb934679645278daedb2a07c53dd07
Sakura Tanaka <sito6496t@google.com>
2023-09-13 22:43:19 +0900

    initial commit

---
+++ code/script.js
@@ -0,0 +1,10 @@
+"use strict";
+
+/* 
+Here is my plan for this app.
+1, async -> fetch api
+2, function  
+-> slider and dots
+-> input/ serach/ localstorage
+-> modal window  delete/ add/ clear all/ 
+*/
