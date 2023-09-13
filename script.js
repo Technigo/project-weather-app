2cd72a91022cca61d56bf11f9df9e37d79d66538
MikoZhu <huadanzhu01@gmail.com>
2023-09-13 09:07:00 +0200

    style the top future -background circle

--- script.js
+++ script.js
@@ -1,15 +1,29 @@
+const mainContainer = document.getElementById("main-container");
+const cityName = document.getElementById("city-name");
+const weatherDescription = document.getElementById("descriptionWeather");
+const mainTemp = document.getElementById("mainTemp");
+const sunriseText = document.getElementById("sunrise");
+const sunsetText = document.getElementById("sunset");
+const topBackground = document.getElementById("topBackground");
+const weatherImage = document.getElementById("weatherImage");
+console.log(weatherImage);
+// define a variable here
+let city = "Stockholm";
 const todayWeather = () => {
   fetch(
-    "https://api.openweathermap.org/data/2.5/weather?q=stockholm,sweden&units=metric&APPID=aa3656bfb4f1c6ee11a76a4ba390afe7"
+    `https://api.openweathermap.org/data/2.5/weather?q=${city}&units=metric&APPID=aa3656bfb4f1c6ee11a76a4ba390afe7`
   )
     .then((response) => {
       return response.json();
     })
     .then((data) => {
       console.log(data);
-      const city = data.name; //Stockholm
+      const city = data.name; //city name
+      cityName.innerText = `${city}`;
       const weather = data.weather[0].description; //Clouds
+      weatherDescription.innerText = `${weather}`;
       const temp = Math.round(data.main.temp * 10) / 10; //the temperature data to one decimal point, *100)/100 two decimal point
+      mainTemp.innerText = `${temp.toFixed(0)}`;
       //Declare variable for the time of sunrise/sunset
       const unixSunrise = data.sys.sunrise;
       const unixSunset = data.sys.sunset;
@@ -27,6 +41,17 @@ const todayWeather = () => {
       // print out the result
       console.log(sunriseTime);
       console.log(sunsetTime);
+      sunriseText.innerText = `${sunriseTime}`;
+      sunsetText.innerText = `${sunsetTime}`;
+
+      // future warm or cold image here
+      const currentTemp = temp.toFixed(0);
+      console.log(currentTemp);
+      if (currentTemp <= "10") {
+        weatherImage.src = "./asserts/cold.jpg";
+      } else {
+        weatherImage.src = "./asserts/warm.jpg";
+      }
     })
     .catch((error) => {
       console.log("caught error", error);
