b595fa735e33af2b377eb0eda7c5fffefc6b5a6a
Amanda Rambert Hammarström <amanda.rambert@gmail.com>
2023-09-13 19:26:51 +0200

    Made the first API call

--- script.js
+++ script.js
@@ -1,1 +1,10 @@
-// fetch("https://api.openweathermap.org/data/2.5/weather?q=Stockholm,Sweden&units=metric&APPID=6cf9113cc039128887bea41cc4117942")
+//fetch("https://api.openweathermap.org/data/2.5/weather?q=Stockholm,Sweden&units=metric&APPID=6cf9113cc039128887bea41cc4117942")
+
+const getWeather = async () => {
+    const response = await fetch("https://api.openweathermap.org/data/2.5/weather?q=Stockholm,Sweden&units=metric&APPID=966881349119fc14f6f3831c44ff9b53");
+
+    const data = await response.json();
+
+    console.log(data)
+}
+getWeather();
