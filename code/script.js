16eade3ab627fabe4ad35bac938a3af083faa351
Julia Holm <juliaholm@julias-mbp.home>
2023-09-13 20:24:13 +0200

    added a little style to see better

--- code/script.js
+++ code/script.js
@@ -19,40 +19,41 @@
 //const URL = ${BASE_URL}${word}${API_KEY};
 //console.log(URL);
 
-const currentWeather = document.getElementById("currentWeather")
-const tempContainer = document.getElementById("temperature")
-const city = document.getElementById("city")
-const time = document.getElementById("time")
-const weather = document.getElementById("weather")
-const sunrise = document.getElementById("sunrise")
-const sunset = document.getElementById("sunset")
-const weatherForecast = document.getElementById("weatherForecast")
-
+const currentWeather = document.getElementById("currentWeather");
+const tempContainer = document.getElementById("temperature");
+const city = document.getElementById("city");
+const time = document.getElementById("time");
+const weather = document.getElementById("weather");
+const sunrise = document.getElementById("sunrise");
+const sunset = document.getElementById("sunset");
+const weatherForecast = document.getElementById("weatherForecast");
 
 const fetchStockholmWeather = () => {
-    fetch("https://api.openweathermap.org/data/2.5/weather?q=Stockholm,Sweden&units=metric&appid=5660c7e2a75e2c204e4b057312e71c93")
-        .then((response) => {
-            return response.json()
-        })
-        .then((json) => {
-            console.log(json)
+  fetch(
+    "https://api.openweathermap.org/data/2.5/weather?q=Stockholm,Sweden&units=metric&appid=5660c7e2a75e2c204e4b057312e71c93"
+  )
+    .then((response) => {
+      return response.json();
+    })
+    .then((json) => {
+      console.log(json);
 
-            const temperature = (json.main.temp);
-            const roundedTemperature = temperature.toFixed(1); // This will round to one decimal place
-            console.log(roundedTemperature);
+      const temperature = json.main.temp;
+      const roundedTemperature = temperature.toFixed(1); // This will round to one decimal place
+      console.log(roundedTemperature);
 
-            tempContainer.innerHTML = `${roundedTemperature}`
-            city.innerHTML = `${json.name}`
-            //time.innerHTML = `${}`
+      tempContainer.innerHTML = `${roundedTemperature}`;
+      city.innerHTML = `${json.name}`;
+      //time.innerHTML = `${}`
 
-            const weatherDescription = (json.weather[0].description)
-            weather.innerHTML = `${weatherDescription}`
-            //sunrise.innerHTML = `${}`
-            //sunset.innerHTML = `${}`
-        })
-}
+      const weatherDescription = json.weather[0].description;
+      weather.innerHTML = `${weatherDescription}`;
+      //sunrise.innerHTML = `${}`
+      //sunset.innerHTML = `${}`
+    });
+};
 
-fetchStockholmWeather()
+fetchStockholmWeather();
 
 // const fiveDayForecast = () => {
 //     fetch("https://api.openweathermap.org/data/2.5/forecast?q=stockholm,se&units=metric&appid=5660c7e2a75e2c204e4b057312e71c93")
@@ -66,48 +67,51 @@ fetchStockholmWeather()
 //         }
 
 const fiveDayForecast = () => {
-     fetch("https://api.openweathermap.org/data/2.5/forecast?q=stockholm,se&units=metric&appid=5660c7e2a75e2c204e4b057312e71c93")
-         .then((response) => {
-             return response.json()
-         })
-         .then((json) => {
-             const filteredData = json.list.filter((item) => {
-                 // Check if the time is 12:00:00 (noon)
-                 return item.dt_txt.includes("12:00:00")
-             })
+  fetch(
+    "https://api.openweathermap.org/data/2.5/forecast?q=stockholm,se&units=metric&appid=5660c7e2a75e2c204e4b057312e71c93"
+  )
+    .then((response) => {
+      return response.json();
+    })
+    .then((json) => {
+      const filteredData = json.list.filter((item) => {
+        // Check if the time is 12:00:00 (noon)
+        return item.dt_txt.includes("12:00:00");
+      });
 
-             console.log(filteredData);
-             // You can now use filteredData to display the weather at 12:00 PM every day
-             showWeatherData(filteredData)
-         })
-         .catch((error) => console.log("Error ⛔", error))
- }
- fiveDayForecast()
+      console.log(filteredData);
+      // You can now use filteredData to display the weather at 12:00 PM every day
+      showWeatherData(filteredData);
+    })
+    .catch((error) => console.log("Error ⛔", error));
+};
+fiveDayForecast();
 
- const showWeatherData = (filteredData) => {
-     let otherDayForecast = ''
-     
-console.log(otherDayForecast)
-     filteredData.forEach((day, idx) => {
-         if (idx >=1 && idx <=4) {
-            const { main, wind_speed, weather } = day
-            const { temp } = main
-            const icon = weather[0].icon
+const showWeatherData = (filteredData) => {
+  let otherDayForecast = "";
 
-            otherDayForecast += `
- <div class="day-${idx + 1}" id="day-${idx + 1}">
-      <div class="day">${(new Date(day.dt * 1000)).toLocaleDateString('en-US', { weekday: 'short' })}</div>
+  console.log(otherDayForecast);
+  filteredData.forEach((day, idx) => {
+    if (idx >= 1 && idx <= 4) {
+      const { main, wind_speed, weather } = day;
+      const { temp } = main;
+      const icon = weather[0].icon;
+
+      otherDayForecast += `
+ <div class="day" id="day">
+      <div class="day">${new Date(day.dt * 1000).toLocaleDateString("en-US", {
+        weekday: "short",
+      })}</div>
       <img src="https://openweathermap.org/img/wn/${icon}@2x.png">
       <div class="temp">${temp}&#176;C</div>
       <div class="wind">${wind_speed} m/s</div>
   </div>
- `
-     }
-     })
+ `;
+    }
+  });
 
-     weatherForecast.innerHTML = otherDayForecast
- }
-
+  weatherForecast.innerHTML = otherDayForecast;
+};
 
 //     weatherForecast.innerHTML =
 //         `<div class="today" id="today">
