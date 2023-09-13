85703ffc266bbb2d5841abf2ccc769a7c69430ff
Amanda Rambert Hammarström <amanda.rambert@gmail.com>
2023-09-13 19:59:03 +0200

    Visualised data and added an error message

--- script.js
+++ script.js
@@ -1,10 +1,17 @@
-//fetch("https://api.openweathermap.org/data/2.5/weather?q=Stockholm,Sweden&units=metric&APPID=6cf9113cc039128887bea41cc4117942")
-
+// Fetching weather data and converted to json.
 const getWeather = async () => {
-    const response = await fetch("https://api.openweathermap.org/data/2.5/weather?q=Stockholm,Sweden&units=metric&APPID=966881349119fc14f6f3831c44ff9b53");
+    try {
+        const response = await fetch("https://api.openweathermap.org/data/2.5/weather?q=Stockholm,Sweden&units=metric&APPID=966881349119fc14f6f3831c44ff9b53", { method: 'GET' });
 
-    const data = await response.json();
+        const data = await response.json();
 
-    console.log(data)
+        console.log(data)
+
+        document.getElementById("city-name").innerText = data.name;
+        document.getElementById("temperature").innerText = data.main.temp;
+        document.getElementById("weather").innerText = data.weather[0].main;
+    } catch {
+        console.error("Could not get weather data");
+    }
 }
 getWeather();
