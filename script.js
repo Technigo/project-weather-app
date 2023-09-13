ff28c206d8b9b43fbc239c2323b5e6fc163c0a7a
Frida Kristiansson <frida.kristiansson@gmail.com>
2023-09-13 10:54:56 +0200

    We have added a weathericon for the forecast by inserting an image using innerHTML. We also added the windspeed in the forecast. We created an if-else statement to make sure only the coming days were added to the forecast (not today). The corresponding days of the week were added to the forecast

--- script.js
+++ script.js
@@ -3,14 +3,15 @@ const weatherApp = document.getElementById('weatherApp')
 const sunRiseSet = document.getElementById('sunRiseSet')
 const weatherForecast = document.getElementById('weatherForecast')
 
+//create a search string
 const appID = "d8d8bd8fc9a245def8c2bd16cb32ba83"
 const units = "metric"
 const baseURL = "https://api.openweathermap.org/data/2.5/"
 
-//create a search string
 const searchString = (searchTerm, searchCity) => {
     return (`${baseURL}${searchTerm}?q=${searchCity}&units=${units}&APPID=${appID}`)
 }
+
 
 fetch(searchString("weather", "Stockholm,Sweden"))
     .then((response) => {
@@ -19,7 +20,6 @@ fetch(searchString("weather", "Stockholm,Sweden"))
         let cityName = json.name
         let temperature = Math.round(json.main.temp*10)/10
         let weatherDescription = json.weather[0].description
-        let windSpeed = Math.round(json.wind.speed*10)/10
 
         let sunrise = new Date (json.sys.sunrise * 1000)
         const sunriseTime = sunrise.toLocaleTimeString([], { timeStyle: 'short' })
@@ -34,9 +34,6 @@ fetch(searchString("weather", "Stockholm,Sweden"))
 
         sunRiseSet.innerHTML +=`<p>sunrise ${sunriseTime}</p>`
         sunRiseSet.innerHTML +=`<p>sunset ${sunsetTime}</p>`
-
-        weatherForecast.innerHTML +=`<p>${windSpeed} m/s</p>`
-        
     })
 
 fetch(searchString("forecast", "Stockholm,Sweden"))
@@ -44,12 +41,37 @@ fetch(searchString("forecast", "Stockholm,Sweden"))
     .then((json) => {
 
         const noonWeather = json.list.filter((item) => item.dt_txt.includes("12:00:00"))
-        
+
+        const weekDays = ["Sunday", "Monday", "Tuesday","Wednesday", "Thursday", "Friday", "Saturday"]
+        let today = new Date().toDateString()
+
         noonWeather.forEach((item) => {
-            let temperature = Math.round(item.main.temp *10)/10
-            weatherForecast.innerHTML += `<p>${temperature}°C</p>`
+            let day = new Date(item.dt_txt)
+            let windSpeed = item.wind.speed
+
+            if (day.toDateString() === today) {}
+            else {
+                let dayOfWeek = weekDays[day.getDay()]
+                weatherForecast.innerHTML += `<p>${dayOfWeek}</p>`
+                
+                weatherIcon = createIcon(item.weather[0].icon)
+                weatherForecast.innerHTML += `<img src="${weatherIcon}", alt="${item.weather.description}"></img>`
+
+                let temperature = Math.round(item.main.temp *10)/10
+                weatherForecast.innerHTML += `<p>${temperature}°C</p>`
+
+                weatherForecast.innerHTML +=`<p>${windSpeed} m/s</p>`
+            }
         })
 })
+
+const createIcon= (iconID) => {
+    let base_URL = `https://openweathermap.org/img/wn/`
+    let icon = iconID
+    let end_URL = `@2x.png`
+
+    return (base_URL+icon+end_URL)
+}
 
 
 //Function for current time
@@ -63,10 +85,9 @@ const currentTime = () => {
         if (minutes < 10) {
             minutes = `0${minutes}`
         }
-    
+
         weatherContainer.innerHTML += `<p>Time: ${hours}:${minutes}</p>`
 }
-
 
 /*
 const formatDay = (timestamp) => {
