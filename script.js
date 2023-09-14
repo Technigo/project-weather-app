67fe309cbafb85325e9fc86edffaccf8b88b7cf6
Amanda Rambert Hammarström <amanda.rambert@gmail.com>
2023-09-14 16:58:37 +0200

    created a functon that gets current weather and forecast

--- script.js
+++ script.js
@@ -1,17 +1,32 @@
+//
+const getWeatherData = async () => {
+    const forecastResponse = await fetch("https://api.openweathermap.org/data/2.5/forecast?q=Stockholm,Sweden&units=metric&APPID=966881349119fc14f6f3831c44ff9b53", { method: 'GET' });
+
+    const weatherResponse = await fetch("https://api.openweathermap.org/data/2.5/weather?q=Stockholm,Sweden&units=metric&APPID=966881349119fc14f6f3831c44ff9b53", { method: 'GET' });
+
+    const weatherData = await weatherResponse.json();
+    //console.log(weatherData)
+
+    const forecastData = await forecastResponse.json();
+    //console.log(forecastData)
+
+    return {
+        city: weatherData.name,
+        weatherDescription: weatherData.weather[0].description,
+        sunrise: forecastData.city.sunrise,
+        sunset: forecastData.city.sunset,
+        temperature: weatherData.main.temp,
+        forecast: []
+    }
+}
+
 // Fetching weather data and converted to json.
 const getWeather = async () => {
     try {
-        const response = await fetch("https://api.openweathermap.org/data/2.5/forecast?q=Stockholm,Sweden&units=metric&APPID=966881349119fc14f6f3831c44ff9b53", { method: 'GET' });
-
-        const data = await response.json();
-
+        const data = await getWeatherData();
         console.log(data)
-
-        document.getElementById("city-name").innerText = data.name;
-        document.getElementById("temperature").innerText = data.main.temp;
-        document.getElementById("weather").innerText = data.weather[0].main;
-    } catch {
-        console.error("Could not get weather data");
+    } catch (error) {
+        console.error(error);
     }
 }
 getWeather();
