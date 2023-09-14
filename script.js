5c290e63809e2ecb352fb0c75f1cd8a33fd2fc9d
Inna Kokic <Inna@Innas-MacBook-Pro.local>
2023-09-14 18:15:07 +0200

    Added icons and started making rows

--- script.js
+++ script.js
@@ -6,13 +6,19 @@ const sunSection = document.getElementById('sun')
 const forecastSection = document.getElementById('forecast')
 const sunrise = document.getElementById('sunrise')
 const sunset = document.getElementById('sunset')
+const forecastOne = document.getElementById('forecast-1')
+const forecastTwo = document.getElementById('forecast-2')
+const forecastThree = document.getElementById('forecast-3')
+const forecastFour = document.getElementById('forecast-4')
+const forecastFive = document.getElementById('forecast-5')
+const forecastLabel = document.getElementById('forecast-label')
 
 const BASE_URL= 'https://api.openweathermap.org/data/2.5/'
 const API_KEY = 'bc487ba1fa4b42fcfb85443237a7774e'
 
 const URL_WEATHER = 'weather'
 const URL_FORECAST = 'forecast'
-const cityQuery = 'Tokyo,Japan'
+const cityQuery = 'Stockholm,Sweden'
 
 //------Fetching today's weather-----
 const fetchWeather = () => {
@@ -28,8 +34,6 @@ fetch(`${BASE_URL}${URL_WEATHER}?q=${cityQuery}&units=metric&APPID=${API_KEY}`)
     calculateSunset(json)
  })
 }
-
-
 
 const getBasicWeatherInfo = (json) => {
     const cityName = ` ${json.name}`
@@ -72,8 +76,6 @@ const insertSunset = (localtimeSunset) => {
 }
 
 
-
-
 //----Fetching for 5 days forecast
 const fetchForecast = () => {
 fetch(`${BASE_URL}${URL_FORECAST}?q=${cityQuery}&units=metric&APPID=${API_KEY}`)
@@ -82,76 +84,105 @@ fetch(`${BASE_URL}${URL_FORECAST}?q=${cityQuery}&units=metric&APPID=${API_KEY}`)
 })
 .then((json) => {
     console.log (json)
-    console.log(json.city.name)
-
-    json.list.forEach((element)=> {
-     //console.log(element.main.temp)
-    }) 
-
-
-     // Filter the list for elements with a timestamp of 12:00:00
-     const weatherAt12 = json.list.filter((el) => el.dt_txt.includes("12:00:00"));
-    //elements12 är en array med 5 objekt. Innehåller all väderinfo kl 12, 5 olika dagar
-     console.log(weatherAt12)
-
-     const dates_text = weatherAt12.map((el) => el.dt_txt)
-     // dates_text är en array med 5 strings: ex; '2023-09-12 12:00:00'
-     console.log(dates_text)
-
-     const DayNumber = dates_text.map((el)=>{
-        return new Date(el).getDay()
-     })
-     // dayNumber är en string: [2, 3, 4, 5, 6], representerar dagarna i nummerform
-     console.log(DayNumber)
-
-     const weekday = ["Sun","Mon","Tue","Wed","Thu","Fri","Sat"];
-
-     let day1 = weekday[DayNumber[0]];
-     let day2 = weekday[DayNumber[1]];
-     let day3 = weekday[DayNumber[2]];
-     let day4 = weekday[DayNumber[3]];
-     let day5 = weekday[DayNumber[4]];
-    
-     //Här skrivs day som ex Weekday[2] = Tuesday utifrån arrayn 'DayNumber'
-
-
-     // Extract temperature values for the filtered elements
-     const temperaturesAt12 = weatherAt12.map((el) => Math.round(el.main.temp))
-    
-     const feelsLike = weatherAt12.map((el) => Math.round(el.main.feels_like))
- 
-     console.log(`tempat12 ${temperaturesAt12}`);
-     console.log(feelsLike)
-
-     forecastSection.innerHTML += `
-     <div class='forecast-column'>
-     <p>Day</p>
-     <p>${day1}:</p>
-     <p>${day2}:</p>
-     <p>${day3}:</p>
-     <p>${day4}:</p>
-     <p>${day5}:</p>
-     </div>
-   
-     <div class='forecast-column'>
-     <p>Temp</p>
-     <p>${temperaturesAt12[0]}°C,</p>
-     <p>${temperaturesAt12[1]}°C,</p>
-     <p>${temperaturesAt12[2]}°C,</p>
-     <p>${temperaturesAt12[3]}°C,</p>
-     <p>${temperaturesAt12[4]}°C,</p>
-      </div> 
-
-    <div class='forecast-column'>
-    <p>Feels like</p>
-    <p>${feelsLike[0]}°C</p>
-    <p>${feelsLike[1]}°C</p>
-    <p>${feelsLike[2]}°C</p>
-    <p>${feelsLike[3]}°C</p>
-    <p>${feelsLike[4]}°C</p>
-    </div> `
+    getWeatherAt12(json)
     })
  }
+
+
+ const getWeatherAt12 = (json) => {
+    const weatherAt12 = json.list.filter((el) => el.dt_txt.includes("12:00:00")); //Filtering for timestamps
+    gettingDays(weatherAt12)
+    gettingTemperatures(weatherAt12)
+    gettingFeelsLike(weatherAt12)
+    gettingIcon(weatherAt12)
+}
+
+const gettingDays = (weatherAt12) => {
+    const dates_text = weatherAt12.map((el) => el.dt_txt) //getting a new array with dates and time
+    const DayNumber = dates_text.map((el)=>{ //Converting daynames to number
+       return new Date(el).getDay()
+    })
+    const weekday = ["Sun","Mon","Tue","Wed","Thu","Fri","Sat"];
+
+    let day1 = weekday[DayNumber[0]];
+    let day2 = weekday[DayNumber[1]];
+    let day3 = weekday[DayNumber[2]];
+    let day4 = weekday[DayNumber[3]];
+    let day5 = weekday[DayNumber[4]];
+    // forecastSection.innerHTML += `
+    //  <div class='forecast-column'>
+    //  <p>Day</p>
+    //  <p>${day1}:</p>
+    //  <p>${day2}:</p>
+    //  <p>${day3}:</p>
+    //  <p>${day4}:</p>
+    //  <p>${day5}:</p>
+    //  </div>`
+
+    forecastOne.innerHTML += `
+    ${day1}°C
+    `
+    forecastTwo.innerHTML += `
+    ${day2}°C
+    `
+   
+}
+
+const gettingIcon = (weatherAt12) => {
+    console.log(weatherAt12)
+    const iconsAt12 = weatherAt12.map((el)=> el.weather.map((el)=> el.icon))
+    console.log(iconsAt12)
+    // forecastSection.innerHTML += `
+    // <div class='forecast-column'>
+    //  <p>Temp</p>
+    //  <img src =https://openweathermap.org/img/wn/${iconsAt12[0]}@2x.png></p>
+    //  <img src =https://openweathermap.org/img/wn/${iconsAt12[1]}@2x.png></p>
+    //  <img src =https://openweathermap.org/img/wn/${iconsAt12[2]}@2x.png></p>
+    //  <img src =https://openweathermap.org/img/wn/${iconsAt12[3]}@2x.png></p>
+    //  <img src =https://openweathermap.org/img/wn/${iconsAt12[4]}@2x.png></p>
+    //   </div> `
+    
+    forecastOne.innerHTML += `
+    <img src =https://openweathermap.org/img/wn/${iconsAt12[0]}@2x.png>
+    `
+    
+}
+
+const gettingTemperatures = (weatherAt12) => {
+    const temperaturesAt12 = weatherAt12.map((el) => Math.round(el.main.temp))
+
+    // forecastSection.innerHTML += `
+    // <div class='forecast-column'>
+    //  <p>Temp</p>
+    //  <p>${temperaturesAt12[0]}°C,</p>
+    //  <p>${temperaturesAt12[1]}°C,</p>
+    //  <p>${temperaturesAt12[2]}°C,</p>
+    //  <p>${temperaturesAt12[3]}°C,</p>
+    //  <p>${temperaturesAt12[4]}°C,</p>
+    //   </div> `
+
+    forecastOne.innerHTML += `
+    ${temperaturesAt12[0]}°C
+    `
+}
+
+const gettingFeelsLike = (weatherAt12) => {
+    const feelsLike = weatherAt12.map((el) => Math.round(el.main.feels_like))
+    // forecastSection.innerHTML += `
+    // <div class='forecast-column'>
+    // <p>Feels like</p>
+    // <p>${feelsLike[0]}°C</p>
+    // <p>${feelsLike[1]}°C</p>
+    // <p>${feelsLike[2]}°C</p>
+    // <p>${feelsLike[3]}°C</p>
+    // <p>${feelsLike[4]}°C</p>
+    // </div> `
+    forecastOne.innerHTML += `
+    ${feelsLike[0]}°C`
+
+}
+
+
 
 
 //----fetching json------
