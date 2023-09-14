f22fe0f89050f645df5a8641ab9d0f5bb69d3350
Maria Pettersson <32590851+mariateresepettersson@users.noreply.github.com>
2023-09-14 17:21:03 +0100

    Added themes based on weather condition
    It's not perfect and needs to be adjusted but the base code is there. need to do small adjustments still to attend all weather conditions. I thought it would be good to keep to 3 conditions, sunny, cold, cloudy

--- script.js
+++ script.js
@@ -6,7 +6,7 @@ const apiKey = '8fa7c461aec946fde31f330992fce9d6';
 
 function fetchWeatherDataByCoordinates(latitude, longitude) {
   const apiUrl = `https://api.openweathermap.org/data/2.5/weather?lat=${latitude}&lon=${longitude}&units=metric&appid=${apiKey}`;
-  
+
   fetch(apiUrl)
     .then((response) => {
       if (!response.ok) {
@@ -21,18 +21,18 @@ function fetchWeatherDataByCoordinates(latitude, longitude) {
       const sunsetTimestamp = json.sys.sunset * 1000;
       const sunriseTimestamp = json.sys.sunrise * 1000;
       const feelsLike = json.main.feels_like.toFixed(1);
-  
+
       // Create Date objects for sunset and sunrise times
       const sunset = new Date(sunsetTimestamp);
       const sunrise = new Date(sunriseTimestamp);
       const sunsetTime = `${sunset.getHours()}:${sunset.getMinutes()}`;
       const sunriseTime = `${sunrise.getHours()}:${sunrise.getMinutes()}`;
       const currentDayOfWeek = new Date().getDay();
-  
+
       function generateWeatherMessage(description) {
         let message = "";
         let iconUrl = "";
-  
+
         if (description.includes("rain")) {
           message = `Don't forget your umbrella! ${cityName} is looking kind of rainy today!`;
           iconUrl = 'https://maxst.icons8.com/vue-static/landings/animated-icons/icons/rainy-weather/rainy-weather.json';
@@ -41,7 +41,7 @@ function fetchWeatherDataByCoordinates(latitude, longitude) {
           iconUrl = 'https://maxst.icons8.com/vue-static/landings/animated-icons/icons/cloudy-weather/cloudy-weather.json';
         } else if (description.includes("clear sky")) {
           message = `Clear vision ahead. ${cityName} get ready for that BBQ.`;
-          iconUrl = 'https://maxst.icons8.com/vue-static/landings/animated-icons/icons/sun-weather/sun-weather.json'; 
+          iconUrl = 'https://maxst.icons8.com/vue-static/landings/animated-icons/icons/sun-weather/sun-weather.json';
         } else if (description.includes("drizzle")) {
           message = `Better wear a jacket as ${cityName} isn't it's happiest today.`;
           iconUrl = 'https://maxst.icons8.com/vue-static/landings/animated-icons/icons/rain-cloud-weather/rain-cloud-weather.json';
@@ -50,21 +50,24 @@ function fetchWeatherDataByCoordinates(latitude, longitude) {
           iconUrl = 'https://maxst.icons8.com/vue-static/landings/animated-icons/icons/stormy-weather/stormy-weather.json';
         } else if (description.includes("snow")) {
           message = `Get ready for the ${cityName} snowball championship!`;
-          iconUrl = 'https://maxst.icons8.com/vue-static/landings/animated-icons/icons/snow-storm-weather/snow-storm-weather.json'; 
+          iconUrl = 'https://maxst.icons8.com/vue-static/landings/animated-icons/icons/snow-storm-weather/snow-storm-weather.json';
         } else {
           message = `${cityName} is looking a bit unstable today. Weather conditions may vary.`;
           iconUrl = 'https://maxst.icons8.com/vue-static/landings/animated-icons/icons/temperature-weather/temperature-weather.json';
         }
-  
+
         return { message, iconUrl };
       }
-  
+
       // Generate the weather message and get the icon URL based on the weather description
       const { message, iconUrl } = generateWeatherMessage(weatherDescription);
-  
+
+      // Set the weather theme based on weather conditions
+      setWeatherTheme(weatherDescription);
+
       // Display the weather message
       messageContainer.innerHTML = `<h1>${message}</h1>`;
-  
+
       container.innerHTML = `
         <h3>${weekdays[currentDayOfWeek]}</h3>
         <p>${weatherDescription} | ${temperature}°</p>
@@ -72,6 +75,25 @@ function fetchWeatherDataByCoordinates(latitude, longitude) {
         <p>sunrise: ${sunriseTime}</p> 
         <p>sunset: ${sunsetTime}</p> 
       `;
+
+      function setWeatherTheme(weatherDescription) {
+        const body = document.body; // Get the <body> element
+
+        if (weatherDescription.includes("cold")) {
+          body.style.backgroundColor = '#c0c0c0'; // Gray background for cold weather
+          body.style.color = '#000'; // Black text color
+        } else if (weatherDescription.includes("clear sky")) {
+          body.style.backgroundColor = '#fff'; // White background for sunny weather
+          body.style.color = '#ff9900'; // Orange text color
+        } else if (weatherDescription.includes("cloud")) {
+          body.style.backgroundColor = '#e0e0e0'; // Light gray background for cloudy weather
+          body.style.color = '#333'; // Dark gray text color
+        } else {
+          // Handle other weather conditions or set a default theme
+          body.style.backgroundColor = '#fff'; // Default background color
+          body.style.color = '#000'; // Default text color
+        }
+      }
 
       // Load and play the Lottie animation based on the icon URL
       const weatherAnimation = document.getElementById("weather-animation");
@@ -138,11 +160,11 @@ function fetchWeatherData() {
           const eveningItem = forecastItems.find((item) => item.dt_txt.includes("21:00"));
 
 
-         if (morningItem && eveningItem) {
-              const morningTemperature = morningItem.main.temp.toFixed(1); // Temp at 9 am
-              const eveningTemperature = eveningItem.main.temp.toFixed(1); // Temp at 9 pm
-              const morningFeelsLike = morningItem.main.feels_like.toFixed(1); // Feels like at 9 am
-              const eveningFeelsLike = eveningItem.main.feels_like.toFixed(1); //Feels like at 9 pm
+          if (morningItem && eveningItem) {
+            const morningTemperature = morningItem.main.temp.toFixed(1); // Temp at 9 am
+            const eveningTemperature = eveningItem.main.temp.toFixed(1); // Temp at 9 pm
+            const morningFeelsLike = morningItem.main.feels_like.toFixed(1); // Feels like at 9 am
+            const eveningFeelsLike = eveningItem.main.feels_like.toFixed(1); //Feels like at 9 pm
             const morningDescription = morningItem.weather[0].description; // Weather description at 9 am
             const eveningDescription = eveningItem.weather[0].description; // Weather description at 9 pm
             const morningHumidity = morningItem.main.humidity; // Humidity at 9am
