7c69fda64449b23ddcf5ea47bd7ff3581464e0c0
stenlisuryadinata <elim_berkat@yahoo.co.id>
2023-09-15 00:43:55 +0700

    Update script.js
    Update script.js
    
    add clock function

--- design/code/script.js
+++ design/code/script.js
@@ -100,5 +100,22 @@ const updateHTMLforecast = (json) => {
     }
 };
 
+function updateClock() {
+    const clockElement = document.getElementById('clock');
+    const now = new Date();
+    const hours = now.getHours().toString().padStart(2, '0');
+    const minutes = now.getMinutes().toString().padStart(2, '0');
+    const seconds = now.getSeconds().toString().padStart(2, '0');
+    const timeString = `${hours}:${minutes}:${seconds}`;
+    clockElement.textContent = timeString;
+}
+
+// Update the clock every second
+setInterval(updateClock, 1000);
+
+// Initial update
+updateClock();
+
+
 //-------------------- All Event Listeners --------------------//
 //if we make a button to scroll through to other cities????
