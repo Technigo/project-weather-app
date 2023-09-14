a3b37a8c4963bc467a83a4fb5f774fd3ca9b68b9
hali <hali.olad@icloud.com>
2023-09-14 11:35:36 +0200

    css

--- script.js
+++ script.js
@@ -1,13 +1,13 @@
 const city = document.getElementById('city')
 const weather = document.getElementById('weather')
 const temperature = document.getElementById('teperature')
-const forecast = document.getElementById('') //skriva när html är klar
-const sunrise = document.getElementById('')
-const sunset =
+const forecast = document.getElementById('forcast')
+const sunrise = document.getElementById('sunrise')
+const sunset = document.getElementById('sunset')
 
 const API_KEY = "b881032f7a405f3e6e05ebbfb98e3e49"
 
-
+let cityName
 const url = "https://api.openweathermap.org/data/2.5/weather?q=Stockholm,Sweden&units=metric&APPID=YOUR_API_KEY"
 
 const fetchWeather = () => {
