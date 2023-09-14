fcd1e0e623a06610c83d3137a551fd69cfa8dd20
EvelynDelCarmen <hey@evelyndelcarmen.com>
2023-09-14 13:05:01 +0200

    css added

--- script.js
+++ script.js
@@ -8,6 +8,7 @@ const API_KEY = "b881032f7a405f3e6e05ebbfb98e3e49"
 const url = "https://api.openweathermap.org/data/2.5/weather?q=Stockholm,Sweden&units=metric&APPID=YOUR_API_KEY"
 
 const fetchWeather = () => {
+    console.log(fetchWeather)
     fetch(url)
         .then((respons) => respons.json())
         .then((data) => {
