cd3f8013a7189f46221544c306282111ab37d76c
Amanda Rambert Hammarström <amanda.rambert@gmail.com>
2023-09-15 17:50:58 +0200

    Made UI design included responsivness

--- script.js
+++ script.js
@@ -1,7 +1,7 @@
 // Fetching weather data and converted to json.
 const getWeather = async () => {
     try {
-        const response = await fetch("https://api.openweathermap.org/data/2.5/forecast?q=Stockholm,Sweden&units=metric&APPID=966881349119fc14f6f3831c44ff9b53", { method: 'GET' });
+        const response = await fetch("https://api.openweathermap.org/data/2.5/weather?q=Stockholm,Sweden&units=metric&APPID=966881349119fc14f6f3831c44ff9b53", { method: 'GET' });
 
         const data = await response.json();
 
