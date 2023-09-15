61977eb535b689163b2e1ff0e67871c687137cbe
jforjanice <jjanice.cheng@gmail.com>
2023-09-15 11:21:12 +0200

    updated css

--- code/index.js
+++ code/index.js
@@ -94,7 +94,7 @@ const imagesDescriptionMain = {
 };
 
 
-
+// search bar
 searchBar.addEventListener("click", () => {
   if (searchWrapper.style.display === "none") {
     searchBar.style.display = "none";
@@ -110,7 +110,7 @@ searchDelete.addEventListener("click", () => {
   }
 })
 
-
+// fetch Data from API
 const fetchWeatherAsync = async () => {
   const URL = `${baseUrl}q=${cityQuery}&units=metric&APPID=${apiKey}`
   const response = await fetch(URL).catch((err) => console.log("my ERROR", err));
@@ -161,19 +161,19 @@ const fetchForecastAsync = async () => {
     return new Date(forecast.dt_txt).getHours() === 12
   })
   console.log(filteredForecast)
-  iconsForecast.forEach((icon , index)=>{
+  iconsForecast.forEach((icon, index) => {
     const iconNum = filteredForecast[index].weather[0].icon;
 
     icon.src = `https://openweathermap.org/img/wn/${iconNum}@2x.png`
 
   });
 
-  tempsForecast.forEach((temp , index)=>{
+  tempsForecast.forEach((temp, index) => {
     const temperture = filteredForecast[index].main.temp;
-    temp.innerText =`${Math.floor(temperture)} °C` 
+    temp.innerText = `${Math.floor(temperture)} °C`
   });
 
-  windForecast.forEach((wind , index)=>{
+  windForecast.forEach((wind, index) => {
     const windSpeed = filteredForecast[index].wind.speed;
 
     wind.innerText = `${Math.floor(windSpeed)} m/s`
@@ -194,9 +194,9 @@ const updateHTML = (data) => {
 
   // sunrise and sunset time
 
-  let newSunrise = new Date((sunriseMilli + timeZone + (new Date().getTimezoneOffset() * 60)) * 1000).toLocaleTimeString([], { timeStyle: 'short' ,  hour12: false });
+  let newSunrise = new Date((sunriseMilli + timeZone + (new Date().getTimezoneOffset() * 60)) * 1000).toLocaleTimeString([], { timeStyle: 'short', hour12: false });
 
-  let newSunset = new Date((sunsetMilli + timeZone + (new Date().getTimezoneOffset() * 60)) * 1000).toLocaleTimeString([], { timeStyle: 'short' ,hour12: false});
+  let newSunset = new Date((sunsetMilli + timeZone + (new Date().getTimezoneOffset() * 60)) * 1000).toLocaleTimeString([], { timeStyle: 'short', hour12: false });
 
 
   // console.log(newSunrise);
@@ -205,7 +205,7 @@ const updateHTML = (data) => {
   sunrise.innerText = newSunrise
   sunset.innerText = newSunset
 
-  tempElement.innerText = `${Math.floor(data.main.temp)}°C`;
+  tempElement.innerText = `${Math.floor(data.main.temp)}`;
   windMain.innerText = `${Math.floor(data.wind.speed)} m/s`;
   cityName.innerText = data.name;
   descriptionEl.innerText = data.weather[0].description;
@@ -218,49 +218,49 @@ const updateHTML = (data) => {
 
 
 
-timeToday.innerText = newTime;
+  timeToday.innerText = newTime;
 
-// images 
+  // images 
 
-const weatherInfo = data.weather[0].main;
-const isDaytime = newTime >= newSunrise && newTime < newSunset;
-let backgroundImage;
+  const weatherInfo = data.weather[0].main;
+  const isDaytime = newTime >= newSunrise && newTime < newSunset;
+  let backgroundImage;
 
 
-if(weatherInfo === "Clear"){
-  backgroundImage = isDaytime ? imagesDescriptionMain.Clear.dayImage : imagesDescriptionMain.Clear.nightImage;
+  if (weatherInfo === "Clear") {
+    backgroundImage = isDaytime ? imagesDescriptionMain.Clear.dayImage : imagesDescriptionMain.Clear.nightImage;
 
-}else if(weatherInfo == "Clouds"){
-  backgroundImage = isDaytime ? imagesDescriptionMain.Clouds.dayImage : imagesDescriptionMain.Clouds.nightImage;
+  } else if (weatherInfo == "Clouds") {
+    backgroundImage = isDaytime ? imagesDescriptionMain.Clouds.dayImage : imagesDescriptionMain.Clouds.nightImage;
 
-}else if (weatherInfo === "Tornado" || weatherInfo === "Squall" || weatherInfo === "Ash" || weatherInfo === "Dust" || weatherInfo === "Sand" || weatherInfo === "Fog" || weatherInfo === "Haze" || weatherInfo === "Smoke" || weatherInfo === "Mist") {
-  backgroundImage = isDaytime ? imagesDescriptionMain[weatherInfo].dayImage : imagesDescriptionMain[weatherInfo].nightImage;
+  } else if (weatherInfo === "Tornado" || weatherInfo === "Squall" || weatherInfo === "Ash" || weatherInfo === "Dust" || weatherInfo === "Sand" || weatherInfo === "Fog" || weatherInfo === "Haze" || weatherInfo === "Smoke" || weatherInfo === "Mist") {
+    backgroundImage = isDaytime ? imagesDescriptionMain[weatherInfo].dayImage : imagesDescriptionMain[weatherInfo].nightImage;
 
-} else if (weatherInfo === "Snow") {
-  backgroundImage = isDaytime ? imagesDescriptionMain.Snow.dayImage : imagesDescriptionMain.Snow.nightImage;
+  } else if (weatherInfo === "Snow") {
+    backgroundImage = isDaytime ? imagesDescriptionMain.Snow.dayImage : imagesDescriptionMain.Snow.nightImage;
 
-} else if (weatherInfo === "Rain") {
-  backgroundImage = isDaytime ? imagesDescriptionMain.Rain.dayImage : imagesDescriptionMain.Rain.nightImage;
+  } else if (weatherInfo === "Rain") {
+    backgroundImage = isDaytime ? imagesDescriptionMain.Rain.dayImage : imagesDescriptionMain.Rain.nightImage;
 
-} else if (weatherInfo === "Drizzle") {
-  backgroundImage = isDaytime ? imagesDescriptionMain.Drizzle.dayImage : imagesDescriptionMain.Drizzle.nightImage;
+  } else if (weatherInfo === "Drizzle") {
+    backgroundImage = isDaytime ? imagesDescriptionMain.Drizzle.dayImage : imagesDescriptionMain.Drizzle.nightImage;
 
-} else if (weatherInfo === "Thunderstorm") {
-  backgroundImage = isDaytime ? imagesDescriptionMain.Thunderstorm.dayImage : imagesDescriptionMain.Thunderstorm.nightImage;
-}
-mainSection.style.backgroundImage = `url(${backgroundImage})`;
+  } else if (weatherInfo === "Thunderstorm") {
+    backgroundImage = isDaytime ? imagesDescriptionMain.Thunderstorm.dayImage : imagesDescriptionMain.Thunderstorm.nightImage;
+  }
+  mainSection.style.backgroundImage = `url(${backgroundImage})`;
 
-// max-min-temperture
-maxTemperture.innerText =`Maximum: ${Math.floor(data.main.temp_max)} °C`;
-minTemperture.innerText = `Minumum: ${Math.floor(data.main.temp_min)} °C`;
+  // max-min-temperture
+  maxTemperture.innerText = `Max: ${Math.floor(data.main.temp_max)} °C`;
+  minTemperture.innerText = `Min: ${Math.floor(data.main.temp_min)} °C`;
 
-// search city
+  // search city
 
-searchBtn.addEventListener("click" , () => {
-  cityQuery = inputEl.value;
-  fetchWeatherAsync()
-  fetchForecastAsync()
- })
+  searchBtn.addEventListener("click", () => {
+    cityQuery = inputEl.value;
+    fetchWeatherAsync()
+    fetchForecastAsync()
+  })
 
 }
 
