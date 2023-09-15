6bc4df9cef4e50928cdca1d081297e1c285a3152
Your Name <you@example.com>
2023-09-15 09:03:19 +0200

    branch done

--- script.js
+++ script.js
@@ -3,6 +3,7 @@ const weatherDescription = document.getElementById('weatherDescription')
 const container = document.getElementById('container')
 const forecastItems = document.getElementById('forecastItems')
 
+const fetchWeather = () => { 
 fetch("https://api.openweathermap.org/data/2.5/weather?q=Stockholm,Sweden&units=metric&APPID=fd582670436692008725c351eb4985b0")
     .then((response) => {
         return response.json()
@@ -11,7 +12,7 @@ fetch("https://api.openweathermap.org/data/2.5/weather?q=Stockholm,Sweden&units=
     .then((json) => {
 
         const sunRise = new Date (json.sys.sunrise * 1000);
-        //making the time variables into strings to display the correct time format
+        //making the time variables into strings to display the sunrise and sunset times into a string with hours and minutes in a "HH:MM" format
         let sunRiseHoursAndMinutes = String(sunRise.getHours()).padStart(2, '0') + ':' + String(sunRise.getMinutes()).padStart(2, '0');
         console.log(sunRise.getHours(), sunRise.getMinutes()); 
 
@@ -21,34 +22,44 @@ fetch("https://api.openweathermap.org/data/2.5/weather?q=Stockholm,Sweden&units=
 
         const weathers = json.weather
         weathers.map((weather) => {
-
+        //This code block uses the map function to iterate over the elements in the weathers array. The weather variable represents each element (an object) in the array, containing information about the weather conditions.
+        //Inside the map callback function, the code updates the content of the header element based on the weather data including description, temperature and sunset and sunrise time.
         header.innerHTML = `
         <h3>${weather.description} | ${(json.main.temp).toFixed(0)}°</h3>
         <h3>sunrise ${sunRiseHoursAndMinutes}</h3>
         <h3>sunset ${sunSetHoursAndMinutes}</h3>
         `
 
-        //different actions depending on weather
-        console.log('hej')
+        //switch statement to perform different actions depending on the main property of the weather object (depending on weather description). 
         switch(weather.main) {
-            case 'Clouds':  // if (x === 'value1')
+            case 'Clouds':  
                 console.log('cloudy');
                 weatherDescription.innerHTML += `
-                <h1>The sky is grey in ${json.name}. </h1>`;
+                <h1>The sky is grey in ${json.name}. </h1>
+                `;   
             break;
         
-            case 'Rain':  // if (x === 'value2')
+            case 'Rain':  
                 console.log('rainy')
-                console.log(weather.description)       
+                console.log(weather.description)    
                 weatherDescription.innerHTML += `
-                <h1>It's raining in ${json.name}.  </h1>
+                <h1>It's raining in ${json.name}. It's okay to stay inside!  </h1>
                 `
-            
-            
+            break;
+        
+            default:
+                console.log('sunny')
+                weatherDescription.innerHTML += `
+                <h1>The sky is crispy and clear in ${json.name}. </h1>
+                </div>
+                `
+            break;
         }
 
     })
  })
+}
+fetchWeather();
 
 
 
