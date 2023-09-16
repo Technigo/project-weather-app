e707db8f11b732cd1ba9d1b7574ecf881523982a
mirelcac <141237662+mirelcac@users.noreply.github.com>
2023-09-16 15:31:54 +0200

    forecastFetched

--- index.js
+++ index.js
@@ -7,17 +7,28 @@ const temperature = document.getElementById(`Temperature`)
 const todaysAdvice = document.getElementById(`todaysAdvice`)
 const sunriseAndTime = document.getElementById(`sunriseAndTime`)
 const description = document.getElementById(`description`)
+const weeklyForecast = document.getElementsByClassName(`weeklyForecast`)
 
-//GLOBAL VARIABLES//
+//GLOBAL VARIABLES - WEATHER//
 const BASE_URL = "https://api.openweathermap.org/data/2.5/weather"
 const stadenMin = "Stockholm,Sweden"
 const API_KEY = "964a15302a76eed8fe2ddd899c2fb441"
 
 const URL = `${BASE_URL}?q=${stadenMin}&units=metric&APPID=${API_KEY}` // erlik 3 første const?
-//FULL URL FRA OPENWEATHERMAP (API) "https://api.openweathermap.org/data/2.5/weather?q=Stockholm,Sweden&units=metric&APPID=964a15302a76eed8fe2ddd899c2fb441"
+//FULL URL FRA OPENWEATHERMAP - WEATHER (API) "https://api.openweathermap.org/data/2.5/weather?q=Stockholm,Sweden&units=metric&APPID=964a15302a76eed8fe2ddd899c2fb441"
 
-console.log(URL)
+//GLOBAL VARIABLES - 5DAY FORECAST/
+const BASE_URL_FOR = "https://api.openweathermap.org/data/2.5/forecast"
+const stadenMinFor = "Stockholm,Sweden"
+const API_KEY_FOR = "964a15302a76eed8fe2ddd899c2fb441"
 
+const URLFOR = `${BASE_URL_FOR}?q=${stadenMinFor}&units=metric&APPID=${API_KEY_FOR}` // erlik 3 første const?
+//FULL URL FRA OPENWEATHERMAP - FORECAST (API) "https://api.openweathermap.org/data/2.5/forecast?q=Stockholm,Sweden&units=metric&APPID=964a15302a76eed8fe2ddd899c2fb441"
+
+console.log(URL) //console.log the url for weather API
+console.log(URLFOR) //console.log the url for 5day forecast API
+
+//FUNCTION - FETCH WEATHER DATA//
 function fetchWeatherData() {
     fetch(URL)
         .then((response) => {
@@ -44,6 +55,7 @@ function fetchWeatherData() {
 •	the temperature (rounded to 1 decimal place) - DONE, BUT 2 dec
 •	and what type of weather it is (the "description" in the JSON) - DONE*/
 
+//DISPLAY DATA FETCHED IN THE fetchWeatherData FUNCTION ON THE WEBSITE//
 //Data here shows to data feched in function fetchWeatherData()
 function updateWeatherUI(data) {
     //temperature
@@ -81,4 +93,48 @@ function updateWeatherUI(data) {
 
 //brances added - need to commit? Did it just in case
 
-window.addEventListener("load", fetchWeatherData);
+/*Show a forecast for the next 5 days. You can choose how to display the forecast - perhaps you want to show the min and max temperature for each day, or perhaps you want to show the temperature from the middle of the day,
+The API gives us the next 5 days but for every third hour. So a good idea could be to only use the weather data from the same time every day. You can filter the forecast list array to only get the info from 12:00 each day for example.
+Read the endpoint documentation for the forecast.*/
+
+//FUNCTION FETCH 5DAY FORECAST DATA//
+function fetchForecastData() {
+    fetch(URLFOR)
+        .then((response) => {
+            if (!response.ok) {
+                throw new Error('Network response was not ok');
+            }
+            return response.json();
+        })
+        .then((forecastData) => {
+            // Handle the data here
+            updateForecast(forecastData);
+            console.log(forecastData)
+        })
+        .catch((error) => {
+            console.error(`Error fetching data: ${error.message}`);
+            console.log(error)
+        });
+}// (forecastData)= forecast data from API
+
+//UpdateForecast Function TO DISPLAY DATA FETCHED IN THE fetchForecastData FUNCTION ON THE WEBSITE//
+//forecastData here shows to data feched in function fetchForecastData()
+function updateForecast(forecastData) {
+    const weeklyForecast = document.getElementsByClassName(`weeklyForecast`);
+    weeklyForecast.innerHTML = ``;
+    const filteredForecast = forecastData.list.filter(item =>
+        item.dt_txt.includes('12:00')
+    );
+}
+
+
+
+
+
+
+
+
+
+//EVENT LISTENERS//
+window.addEventListener("load", fetchWeatherData); //update weather information when site loads
+window.addEventListener("load", fetchForecastData); //update 5day forecast information when site loads
