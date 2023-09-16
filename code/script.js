ec9db4d703f5463a115e3bb46edbd0b3b247214f
Lisa Dahlkar <lisadahlkar@macbook-pro.home>
2023-09-16 12:37:26 +0200

    added searchbar

--- code/script.js
+++ code/script.js
@@ -1,10 +1,6 @@
 
-let city = "Stockholm" //Default city
+//API: https://api.openweathermap.org/data/2.5/weather?q=Stockholm,Sweden&units=metric&APPID=d75b73cbadec04610cd0103495fdb88c
 
-const fetchingWeather = () => {
-    fetch(`https://api.openweathermap.org/data/2.5/weather?q=Stockholm,Sweden&units=metric&APPID=d75b73cbadec04610cd0103495fdb88c`
-    )
-        .then((response) => {
-            return response.json()
-        })
-}
+const body = document.getElementById('body')
+
+fetch('https://api.openweathermap.org/data/2.5/weather?q=Stockholm,Sweden&units=metric&APPID=d75b73cbadec04610cd0103495fdb88c')
