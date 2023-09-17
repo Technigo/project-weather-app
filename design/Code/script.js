1084cb6114935b1272517868ff654c26ce5992bd
Breanna Sand <breanna.sand8@gmail.com>
2023-09-17 11:56:26 +0200

    list and textSnippet

--- design/Code/script.js
+++ design/Code/script.js
@@ -2,13 +2,14 @@
 const weather = document.getElementById("weather")
 const temperature = document.getElementById("temperature")
 const city = document.getElementById("city")
+const sunrise = document.getElementById("sunrise")
+const sunset = document.getElementById("sunset")
+const weeklyForecast = document.getElementById("weeklyForecast")
+const textSnippet = document.getElementById("textContainer")
 
 const MY_API_KEY = "b0f999409176c1b94b357122dd7da817"
 const BASE_URL = "https://api.openweathermap.org/data/2.5/weather?q=Stockholm,Sweden&units=metric&APPID="
 const URL = `${BASE_URL}${MY_API_KEY}`
-
-
-
 
 const fetchWeather = () => {
     fetch(URL)
@@ -23,8 +24,23 @@ const fetchWeather = () => {
 const updateHTML = (data) => {
 
     weather.innerText = data.weather[0].description
-    temperature.innerText = data.main.temp
+    temperature.innerHTML = `<h2> ${(data.main.temp).toFixed(0)}°</h2>`
     city.innerText = data.name
+
+
+    sunrise.innerHTML = `<h2> Sunrise ${data.sys.sunrise} </h2>`
+    sunset.innerHTML = `<h2> Sunset ${data.sys.sunset} </h2>`
+
+    //invoke function for changing text snippet here
+    //textSnippet(city, weather)
 }
+
+//create a function for displaying text snippet specific to the weather using if statement
+//const textSnippet = (city, weather) => {
+//    if (weather = "cloudy"){
+textSnippet.
+}
+//}
+
 
 fetchWeather()
