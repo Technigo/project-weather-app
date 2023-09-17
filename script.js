d6ea76b3daad9f0e3389224518634df93d308b2f
Frida Kristiansson <frida.kristiansson@gmail.com>
2023-09-17 08:42:59 +0200

    Changed the code so current time was the local time of the city searched. Also adjusted the day night or dusk function to compare the time to local time

--- script.js
+++ script.js
@@ -1,4 +1,4 @@
-//Containers for the DOM elements
+//CONTAINERS for the DOM elements
 const weatherContainer = document.getElementById('currentWeather')
 const featureImage = document.getElementById('featureImage')
 const weatherApp = document.getElementById('weatherApp')
@@ -11,8 +11,8 @@ const weatherForecast = document.getElementById('weatherForecast')
 const searchMenuBtn = document.getElementById('searchMenuBtn');
 const closeSearchMenu = document.getElementById('closeSearchMenu');
 const searchBtn = document.getElementById('searchBtn');
+const searchToggler = document.getElementById('search-toggler')
 const inputField = document.getElementById('inputField');
-//const switchFavoriteCity = document.getElementById('switchBtn');
 
 //Create a search string
 const appID = "d8d8bd8fc9a245def8c2bd16cb32ba83"
@@ -22,15 +22,43 @@ const searchString = (searchTerm, searchCity) => {
     return (`${baseURL}${searchTerm}?q=${searchCity}&units=${units}&APPID=${appID}`)
 }
 
-const todaysWeatherFeature = async (city) =>{
-    //Fetch the API from Open Weather construct the current weather
-   await fetch(searchString("weather", city))
+//Creates the weather forecast and current weather with a city name as a parameter. Calls an API using fetch-then functions.
+const todaysWeatherFeature = (city) =>{
+
+    //Fetch the API from Open Weather to construct the current weather
+    fetch(searchString("weather", city))
     .then((response) => {
         return response.json()})
     .then((json) => {
+        
+        let temperature = Math.round(json.main.temp)
         let cityName = json.name
-        let temperature = Math.round(json.main.temp)                                       
+
+        currentTemperature.innerHTML = `<p><div class ="tempNumber">${temperature}</div> <div class="degrees">°C</div></p>`
+        cityTimeDesc.innerHTML += `<h1 class="cityFont">${cityName}</h1>`
+
         let weatherDescription = json.weather[0].description
+        let weatherIcon = createIcon(json.weather[0].icon)
+
+        cityTimeDesc.innerHTML +=`
+        <div class="weatherDescriptionIcon">  
+        <p class="weatherDescription">${weatherDescription}</p> 
+        <img class="weatherIcon" src = "${weatherIcon}", alt = "${weatherDescription}", width = "50px">
+        </img>
+        </div>`
+
+        const currentLocalTime = new Date((json.dt + json.timezone) * 1000)
+        const timeNow = currentLocalTime.toLocaleTimeString([], {timeStyle: 'short', timeZone: 'UTC'})
+
+        cityTimeDesc.innerHTML += `<p>${timeNow}</p>`
+
+        let sunrise = new Date ((json.sys.sunrise + json.timezone) * 1000)
+        let sunset = new Date ((json.sys.sunset + json.timezone) * 1000)
+        const sunriseTime = sunrise.toLocaleTimeString([], { timeStyle: 'short', timeZone: 'UTC'})
+        const sunsetTime = sunset.toLocaleTimeString([], { timeStyle: 'short', timeZone: 'UTC'})
+    
+        sunRiseSet.innerHTML +=`<p>sunrise ${sunriseTime}</p>`
+        sunRiseSet.innerHTML +=`<p>sunset ${sunsetTime}</p>`
 
         let weatherImg = json.weather[0].main
         switch (weatherImg) {
@@ -57,53 +85,36 @@ const todaysWeatherFeature = async (city) =>{
                 break
         }
 
-        let sunrise = new Date (json.sys.sunrise * 1000)
-        const sunriseTime = sunrise.toLocaleTimeString([], { timeStyle: 'short' })
-        let sunset = new Date (json.sys.sunset * 1000)
-        const sunsetTime = sunset.toLocaleTimeString([], { timeStyle: 'short' })
-
-        currentTemperature.innerHTML = `<p><div class ="tempNumber">${temperature}</div> <div class="degrees">°C</div></p>`
-        cityTimeDesc.innerHTML += `<h1 class="cityFont">${cityName}</h1>`
-        let timeNow = currentTime()
-        cityTimeDesc.innerHTML += `<p>${timeNow}</p>`
-    
-        dayNightOrDusk(sunriseTime, sunsetTime)
-
-        let weatherIcon = createIcon(json.weather[0].icon)
-        cityTimeDesc.innerHTML +=`
-        <div class="weatherDescriptionIcon">  
-        <p class="weatherDescription">${weatherDescription}</p> 
-        <img class="weatherIcon" src = "${weatherIcon}", alt = "${weatherDescription}", width = "50px">
-        </img>
-        </div>`
-
-        sunRiseSet.innerHTML +=`<p>sunrise ${sunriseTime}</p>`
-        sunRiseSet.innerHTML +=`<p>sunset ${sunsetTime}</p>`
+        dayNightOrDusk(sunriseTime, sunsetTime, currentLocalTime)
     })
 
 //Fetch the API from Open Weather and construct a forecast for the coming 4-5 days
 fetch(searchString("forecast", city))
     .then((response) => (response.json()))
     .then((json) => {
+
         const noonWeather = json.list.filter((item) => item.dt_txt.includes("12:00:00"))
 
         let today = new Date().toDateString()
         let iterationNum = 1
 
+        const weekDays = ["Sun", "Mon", "Tue", "Wed", "Thu", "Fri", "Sat"]
+
         noonWeather.forEach((item) => {
+
             let day = new Date(item.dt_txt)
-            let windSpeed = item.wind.speed
 
             let weatherIcon = createIcon(item.weather[0].icon)
             let weatherDescription = item.weather.description
 
             let temperature = Math.round(item.main.temp *10)/10
+            let windSpeed = item.wind.speed
 
+            //If first forecast is for today, don't log it in forecast. Otherwise add all info to innerHTML
             if (day.toDateString() === today) {}
             else {
                 const forecastRow = document.getElementById(`forecastRow${iterationNum}`)
 
-                const weekDays = ["Sun", "Mon", "Tue", "Wed", "Thu", "Fri", "Sat"]
                 let dayOfWeek = weekDays[day.getDay()]
                 forecastRow.innerHTML += `<p>${dayOfWeek}</p>`
 
@@ -118,22 +129,9 @@ fetch(searchString("forecast", city))
     })
 }
 
-//Function for current time
-const currentTime = () => {
-    let date = new Date()
-    let hours = date.getHours()
-        if (hours < 10) {
-            hours = `0${hours}`
-        }
-    let minutes = date.getMinutes()
-        if (minutes < 10) {
-            minutes = `0${minutes}`
-        }
-    return `Time: ${hours}:${minutes}`
-}
-
 //Creating the icon's cource URL
 const createIcon= (iconID) => {
+
     let base_URL = `https://openweathermap.org/img/wn/`
     let icon = iconID
     let end_URL = `@2x.png`
@@ -141,27 +139,26 @@ const createIcon= (iconID) => {
     return base_URL+icon+end_URL
 }
 
+//Toggles between open and closed search field when you click the magnifying glass or the cross
 const toggleSearchField = () => {
-    //This just controls the toggling between opening and closing the search field
-    const searchToggler = document.getElementById('search-toggler')
+    
     searchToggler.classList.toggle('hidden')
     closeSearchMenu.classList.toggle('hidden')
     searchMenuBtn.classList.toggle('hidden')
 }
 
+//When searching for a new city. Saves the input as a variable and runs todaysWeatherFeature with this input. Clears the previous weather forecast
 const searchFunction = () => {
-    //This is for storing the user input from the search and pushing it into our fetching weather function later on
-    
+
     let searchedCity = inputField.value
 
-        //Use the city searched and inject into ""fetchWeather""" function:
-        //weather.fetchWeather(searchedCity); //Skriv om till den vi använder
         todaysWeatherFeature(searchedCity)
 
-        //Clears field & hides the input field:
+        //Clears field & hides the input field
         inputField.value = ""
+        searchToggler.classList.toggle('hidden')
 
-        //Reset the weather forecast:
+        //Resets the weather forecast
         cityTimeDesc.innerHTML = ""
         sunRiseSet.innerHTML = ""
         forecastRow1.innerHTML =""
@@ -171,14 +168,18 @@ const searchFunction = () => {
         forecastRow5.innerHTML =""
 }
 
+//Changes the backgroundimage
 const changeBackground = (imgURL) => {
+
     featureImage.style.backgroundImage = 'url(img/' + imgURL + '.jpg)'
 }
 
-const dayNightOrDusk = (sunriseTime, sunsetTime) => {
-    let date = new Date()
-    let hours = date.getHours()
-    if (hours < sunriseTime.substring(0,2) || hours > sunsetTime.substring(0,2)) {
+//Changes the backgroungimage if it's night or evening. Takes the current time, sunrise and sunset as parameters
+const dayNightOrDusk = (sunriseTime, sunsetTime, timeNow) => {
+
+    let hours = timeNow.toLocaleTimeString([],{hour: '2-digit', timeZone: 'UTC'})
+
+    if (hours < sunriseTime.substring(0,2) || hours >= sunsetTime.substring(0,2)) {
         changeBackground('night')
     }
     else if (hours > (sunsetTime.substring(0,2) - 1)) {
@@ -186,15 +187,20 @@ const dayNightOrDusk = (sunriseTime, sunsetTime) => {
     }
 }
 
+
 //START
 todaysWeatherFeature("Stockholm, Sweden")
 
+
 //EVENT LISTENERS
+
 //Eventlistener to toggle search field:
 searchMenuBtn.addEventListener('click', toggleSearchField)
 closeSearchMenu.addEventListener('click', toggleSearchField)
+
 //Eventlistener to search through enter key also
 searchBtn.addEventListener('click', searchFunction)
+
 //Eventlistener to search through enter key also
 inputField.addEventListener('keyup', function (event) {
     if (event.key == "Enter") {
