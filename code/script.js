d9b595ff9546c862c2a864c44a9aea617c068159
Isa Robertini <robertiniisa@gmail.com>
2023-09-17 20:05:26 +0200

    latest

--- code/script.js
+++ code/script.js
@@ -28,6 +28,10 @@ const cities = [
         image: 'https://images.unsplash.com/photo-1585425422110-bb85558b2293?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=1935&q=80'
     }
 ];
+const weekDays = [
+    'Sun', 'Mon', 'Tues', 'Wed', 'Thurs', 'Fri', 'Sat'
+]
+
 
 
 let selectedCity = 0;
@@ -106,7 +110,7 @@ const weeklyForecast = (cityName) => {
             const today = new Date();
             const todaysDate = today.toLocaleDateString()
             const processedDates = [];
-            processedDates.push(todaysDate);
+            processedDates.push(todaysDate); //includes today's date in the array from beginning
 
 
             //Create processedDates array
