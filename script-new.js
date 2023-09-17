821a532a185e73299864dec24396a21d0da538ca
Emmy Jansson <emmybengtsson@live.se>
2023-09-17 14:27:41 +0200

    forecast info

--- script-new.js
+++ script-new.js
@@ -18,6 +18,7 @@ if (isDayTime) {
   document.body.className = "day";
   console.log("daytime");
 } else {
+  document.body.className = "night";
   console.log("nighttime");
 }
 
@@ -26,6 +27,7 @@ fetch(searchString("weather", "Zurich, Switzerland"))
   .then((response) => response.json())
   .then((json) => {
     let weatherIcon = json.weather[0].icon;
+    setWeatherIcon(weatherIcon);
     let temperature = json.main.temp;
     let cityName = json.name;
     let weatherCondition = json.weather[0].description;
@@ -35,6 +37,7 @@ fetch(searchString("weather", "Zurich, Switzerland"))
     let sunset = new Date(json.sys.sunset * 1000);
     const sunsetTime = sunset.toLocaleTimeString([], { timeStyle: "short" });
 
+    console.log("Weather Icon Code:", weatherIcon);
     console.log(`city` + cityName);
     console.log(weatherIcon);
     console.log(weatherCondition);
@@ -44,9 +47,9 @@ fetch(searchString("weather", "Zurich, Switzerland"))
     console.log(sunsetTime);
 
     weatherContainer.innerHTML = `
-      <h1>${temperature} °C</h1>
+       <img src = "${weatherIcon}", width = "75px"></img>
+       <h1>${temperature} °C</h1>
       <p>${cityName}</p>
-      <p>${weatherIcon}</p>
       <p>${weatherCondition}</p>
       <p>Feels like: ${feelsLike} °C</p>
     `;
@@ -69,13 +72,6 @@ fetch(searchString("forecast", "Zurich, Switzerland"))
     console.log(today);
     let iteration = 1;
     console.log(iteration);
-
-    // ...
-
-    // Select the container for forecast elements
-    const forecastContainer = document.getElementById("forecastContainer");
-
-    // ...
 
     // Inside the fetch for weather forecast
     dailyWeather.forEach((item) => {
@@ -112,23 +108,16 @@ fetch(searchString("forecast", "Zurich, Switzerland"))
       const forecastElement = document.createElement("div");
       forecast.className += "forecast";
       forecast.innerHTML += `<p>${dayOfWeek}</p>`;
-      forecast.innerHTML += `<p>${dailyWeatherIcon}</p>`;
+      forecast.innerHTML += `<img src = "${dailyWeatherIcon}", width = "60px"></img>`;
       forecast.innerHTML += `<p>${dailyWeatherConditions}</p>`;
       forecast.innerHTML += ` <p>${dailyTemperature}°C</p>`;
       forecast.innerHTML += `<p>Feels like: ${dailyFeelsLike}</p>`;
     }
   });
 
-//Creating the icon
-const createIcon = () => {
-  let base_URL = `https://openweathermap.org/img/wn/`;
-  let icon = weatherIcon;
-  let end_URL = `@2x.png`;
-
-  return base_URL + icon + end_URL;
-};
-
-// Create an img element for the icon and set its src attribute
-const iconElement = document.createElement("img");
-iconElement.src = iconURL;
-iconElement.alt = "Weather Icon";
+// Function to create and set the weather icon
+function setWeatherIcon(iconCode) {
+  let iconURL = `http://openweathermap.org/img/wn/${iconCode}.png`;
+  const weatherIconElement = document.getElementById("weatherIcon");
+  weatherIconElement.src = iconURL;
+}
