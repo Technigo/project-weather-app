2e3d5c1285caaaa62ac39644d264aab209399a4e
Breanna Sand <breanna.sand8@gmail.com>
2023-09-17 10:54:37 +0200

    innerText and Dom for city and temperature

--- design/Code/script.js
+++ design/Code/script.js
@@ -1,13 +1,13 @@
 //select from data variable the values you want to inject into the DOM
-const forecast = document.getElementById("forecast")
+const weather = document.getElementById("weather")
+const temperature = document.getElementById("temperature")
+const city = document.getElementById("city")
 
 const MY_API_KEY = "b0f999409176c1b94b357122dd7da817"
 const BASE_URL = "https://api.openweathermap.org/data/2.5/weather?q=Stockholm,Sweden&units=metric&APPID="
 const URL = `${BASE_URL}${MY_API_KEY}`
 
-const sunrise = ""
-const sunset = ""
-const arrayForecast = ""
+
 
 
 const fetchWeather = () => {
@@ -15,13 +15,16 @@ const fetchWeather = () => {
         .then((response) => response.json())
         .then((data) => {
             console.log(data)
-            forecast.innerText = data.weather[0].description
-
-
+            updateHTML(data)
         })
         .catch((error) => console.log("Error X", error))
 }
 
+const updateHTML = (data) => {
 
+    weather.innerText = data.weather[0].description
+    temperature.innerText = data.main.temp
+    city.innerText = data.name
+}
 
 fetchWeather()
