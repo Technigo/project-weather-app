6b07d769a859f460a1a7c15a1a07bfd08cee7b0b
Vanessa Arruda <adm.vanessasouza@gmail.com>
2023-10-10 16:55:06 +0200

    fetch weather data and log response

--- script.js
+++ script.js
@@ -1,5 +1,6 @@
+//FETCH API
 
-const URL = "https://api.openweathermap.org/data/2.5/weather?q=Stockholm,Sweden&units=metric&APPID=231ff309be8ceb223aff125da6bf7bb2";
+const URL = 'https://api.openweathermap.org/data/2.5/weather?q=Stockholm,Sweden&units=metric&APPID=231ff309be8ceb223aff125da6bf7bb2';
 let weatherData = [];
 
 fetch(URL)
@@ -7,6 +8,9 @@ fetch(URL)
         return response.json();
     })
     .then((json) => {
-        console.log(json.results);
-        weatherData = json.results;
+        weatherData = json;
+        console.log(weatherData);
+    })
+    .catch((err) => {
+        console.log('caught error', err);
     });
