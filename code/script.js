a8159d75140c7f77133d1c6cb14e017516f6dc9d
petrame <63057558+petrame@users.noreply.github.com>
2023-10-11 13:35:01 +0200

    New feature: Search by location

--- code/script.js
+++ code/script.js
@@ -1,19 +1,6 @@
 // Get API_KEY from separate file
 import { API_KEY } from "./api_key.js";
 
-let currentTown = "Örebro";
-
-// Store API URL's
-const weatherApiUrl = `https://api.openweathermap.org/data/2.5/weather?q=${currentTown},Sweden&units=metric&APPID=${API_KEY}`;
-const forecastApiUrl = `https://api.openweathermap.org/data/2.5/forecast?q=${currentTown},Sweden&units=metric&APPID=${API_KEY}`;
-
-// Get current time and date
-const currentDate = new Date().toLocaleDateString();
-const currentTime = new Date().toLocaleTimeString("sv-SE", {
-  hour: "2-digit",
-  minute: "2-digit",
-});
-
 // Declare variables
 const backgroundColor = document.getElementById("backgroundColor");
 const date = document.getElementById("date");
@@ -23,125 +10,148 @@ const weatherDescription = document.getElementById("weatherDescription");
 const weatherImage = document.getElementById("weatherImage");
 const sunrise = document.getElementById("sunrise");
 const sunset = document.getElementById("sunset");
+const locationInput = document.getElementById("searchField");
+
+// Get current time and date
+const currentDate = new Date().toLocaleDateString();
+const currentTime = new Date().toLocaleTimeString("sv-SE", {
+  hour: "2-digit",
+  minute: "2-digit",
+});
 
 // Set current date and time
 date.innerHTML = `${currentDate} ${currentTime}`;
 
-// Get todays weather
-fetch(weatherApiUrl)
-  .then((response) => {
-    return response.json();
-  })
-  .then((json) => {
-    temperature.innerHTML = json.main.temp.toFixed();
-    city.innerHTML = json.name;
-    weatherDescription.innerHTML = json.weather[0].main;
+// Set initial location
+let currentLocation = "Örebro";
 
-    // Get weather icon name from API
-    const weatherIcon = json.weather[0].icon;
-    weatherImage.src = `https://openweathermap.org/img/wn/${weatherIcon}@2x.png`;
+// Store API URL's
+let weatherApiUrl = `https://api.openweathermap.org/data/2.5/weather?q=${currentLocation},Sweden&units=metric&APPID=${API_KEY}`;
+let forecastApiUrl = `https://api.openweathermap.org/data/2.5/forecast?q=${currentLocation},Sweden&units=metric&APPID=${API_KEY}`;
 
-    const sunriseValue = new Date(json.sys.sunrise * 1000).toLocaleTimeString(
-      "sv-SE",
-      {
-        hour: "2-digit",
-        minute: "2-digit",
-      }
-    );
-    sunrise.innerHTML = "Sunrise " + sunriseValue;
+// Updates the URL:s when searching for a new location
+const changeLocation = () => {
+  currentLocation = locationInput.value;
+  weatherApiUrl = `https://api.openweathermap.org/data/2.5/weather?q=${currentLocation},Sweden&units=metric&APPID=${API_KEY}`;
+  forecastApiUrl = `https://api.openweathermap.org/data/2.5/forecast?q=${currentLocation},Sweden&units=metric&APPID=${API_KEY}`;
+  locationInput.value = "";
+  fetchAPI();
+};
+locationInput.addEventListener("change", changeLocation);
 
-    const sunsetValue = new Date(json.sys.sunset * 1000).toLocaleTimeString(
-      "sv-SE",
-      {
-        hour: "2-digit",
-        minute: "2-digit",
-      }
-    );
-    sunset.innerHTML = "Sunset " + sunsetValue;
+const fetchAPI = () => {
+  // Get todays weather
+  fetch(weatherApiUrl)
+    .then((response) => {
+      return response.json();
+    })
+    .then((json) => {
+      temperature.innerHTML = json.main.temp.toFixed();
+      city.innerHTML = json.name;
+      weatherDescription.innerHTML = json.weather[0].main;
 
-    // Toggle class for night mode if sun is down
-    if (currentTime < sunriseValue || currentDate > sunsetValue) {
-      backgroundColor.classList.add("night");
-    } else {
-      backgroundColor.classList.remove("night");
-    }
-  })
-  .catch((err) => {
-    console.log("Caught error:", err);
-  });
+      // Get weather icon name from API
+      const weatherIcon = json.weather[0].icon;
+      weatherImage.src = `https://openweathermap.org/img/wn/${weatherIcon}@2x.png`;
 
-// Get forecast for the next five days
-fetch(forecastApiUrl)
-  .then((response) => {
-    return response.json();
-  })
-  .then((json) => {
-    console.log(json);
-
-    const forecast = document.getElementById("fivedayForecast");
-    const today = new Date(json.list[0].dt_txt).getDay();
-    console.log({ today });
-
-    // Create a filtered list starting from tomorrow
-    const fiveNextDays = json.list.filter(
-      (element) => new Date(element.dt_txt).getDay() !== today
-    );
-    console.log({ fiveNextDays });
-
-    let maxTemp = -1000;
-    let minTemp = 1000;
-    let weatherIcon = "";
-    // Create an array to store max and min temperature for every day
-    const fiveDayArray = [];
-
-    fiveNextDays.forEach((element) => {
-      // Get current weekday
-      const currentWeekDay = new Date(element.dt_txt).toLocaleDateString(
+      const sunriseValue = new Date(json.sys.sunrise * 1000).toLocaleTimeString(
         "sv-SE",
         {
-          weekday: "long",
+          hour: "2-digit",
+          minute: "2-digit",
         }
       );
+      sunrise.innerHTML = "Sunrise " + sunriseValue;
 
-      // If the array is empty (first iteration), add the first day in
-      if (fiveDayArray.length === 0) fiveDayArray.push(currentWeekDay);
+      const sunsetValue = new Date(json.sys.sunset * 1000).toLocaleTimeString(
+        "sv-SE",
+        {
+          hour: "2-digit",
+          minute: "2-digit",
+        }
+      );
+      sunset.innerHTML = "Sunset " + sunsetValue;
 
-      // If current weekday is in list, compere min and max temp
-      if (!fiveDayArray.includes(currentWeekDay)) {
-        // If not in list, then it's a new day - add min and max temp to array, add the new day, reset min and max temp
-        fiveDayArray.push(maxTemp);
-        fiveDayArray.push(minTemp);
-        fiveDayArray.push(weatherIcon);
-        fiveDayArray.push(currentWeekDay);
-        maxTemp = -1000;
-        minTemp = 1000;
+      // Toggle class for night mode if sun is down
+      if (currentTime < sunriseValue || currentTime > sunsetValue) {
+        backgroundColor.classList.add("night");
+      } else {
+        backgroundColor.classList.remove("night");
       }
-      if (element.main.temp_max > maxTemp) {
-        maxTemp = element.main.temp_max;
-        weatherIcon = element.weather[0].icon;
-      }
-      if (element.main.temp_min < minTemp) minTemp = element.main.temp_min;
+    })
+    .catch((err) => {
+      console.log("Caught error:", err);
     });
 
-    // Push temperature values for the last day
-    fiveDayArray.push(maxTemp);
-    fiveDayArray.push(minTemp);
-    fiveDayArray.push(weatherIcon);
+  // Get forecast for the next five days
+  fetch(forecastApiUrl)
+    .then((response) => {
+      return response.json();
+    })
+    .then((json) => {
+      const forecast = document.getElementById("fivedayForecast");
+      const today = new Date(json.list[0].dt_txt).getDay();
 
-    // Send five day forecast as HTML
-    for (let i = 0; i < 5; i++) {
-      let currentDay = [];
+      // Create a filtered list starting from tomorrow
+      const fiveNextDays = json.list.filter(
+        (element) => new Date(element.dt_txt).getDay() !== today
+      );
 
-      if (fiveDayArray.length > 4) currentDay = fiveDayArray.splice(0, 4);
-      else currentDay = fiveDayArray;
+      let maxTemp = -1000;
+      let minTemp = 1000;
+      let weatherIcon = "";
 
-      const weekDay =
-        currentDay[0].charAt(0).toUpperCase() + currentDay[0].slice(1);
-      const maxTemp = currentDay[1].toFixed();
-      const minTemp = currentDay[2].toFixed();
-      const icon = currentDay[3];
+      // Create an array to store max and min temperature for every day
+      const fiveDayArray = [];
 
-      forecast.innerHTML += `
+      fiveNextDays.forEach((element) => {
+        // Get current weekday
+        const currentWeekDay = new Date(element.dt_txt).toLocaleDateString(
+          "sv-SE",
+          {
+            weekday: "long",
+          }
+        );
+
+        // If the array is empty (first iteration), add the first day in
+        if (fiveDayArray.length === 0) fiveDayArray.push(currentWeekDay);
+
+        // If current weekday is in list, compere min and max temp
+        if (!fiveDayArray.includes(currentWeekDay)) {
+          // If not in list, then it's a new day - add min and max temp to array, add the new day, reset min and max temp
+          fiveDayArray.push(maxTemp);
+          fiveDayArray.push(minTemp);
+          fiveDayArray.push(weatherIcon);
+          fiveDayArray.push(currentWeekDay);
+          maxTemp = -1000;
+          minTemp = 1000;
+        }
+        if (element.main.temp_max > maxTemp) {
+          maxTemp = element.main.temp_max;
+          weatherIcon = element.weather[0].icon;
+        }
+        if (element.main.temp_min < minTemp) minTemp = element.main.temp_min;
+      });
+
+      // Push temperature values for the last day
+      fiveDayArray.push(maxTemp);
+      fiveDayArray.push(minTemp);
+      fiveDayArray.push(weatherIcon);
+
+      // Send five day forecast as HTML
+      for (let i = 0; i < 5; i++) {
+        let currentDay = [];
+
+        if (fiveDayArray.length > 4) currentDay = fiveDayArray.splice(0, 4);
+        else currentDay = fiveDayArray;
+
+        const weekDay =
+          currentDay[0].charAt(0).toUpperCase() + currentDay[0].slice(1);
+        const maxTemp = currentDay[1].toFixed();
+        const minTemp = currentDay[2].toFixed();
+        const icon = currentDay[3];
+
+        forecast.innerHTML += `
         <div id="dayContainer" class="day-container">
           <p id="weekday">${weekDay}</p>
           <img
@@ -157,8 +167,11 @@ fetch(forecastApiUrl)
           </div>
         </div>
       `;
-    }
-  })
-  .catch((err) => {
-    console.log("Caught error:", err);
-  });
+      }
+    })
+    .catch((err) => {
+      console.log("Caught error:", err);
+    });
+};
+
+fetchAPI();
