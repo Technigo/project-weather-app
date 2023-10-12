a60ca8efc11ee3f7dc731c1345788775126862f6
Disabledbarrel <elin.elisabeth.larsson@gmail.com>
2023-10-12 22:14:59 +0200

    elin larsson: display weather, temp, sunrise

--- script.js
+++ script.js
@@ -1,25 +1,53 @@
-const weatherContainer = document.getElementById("weather-container");
+const container = document.getElementById("container");
+const currentWeather = document.getElementById("currentWeather");
+
+// Feature: Sunrise and sunset
+function setWeatherClass(weatherClass) {
+	switch (weatherClass) {
+		case "clear":
+			container.classList.toggle("clear");
+			break;
+		case "rain":
+			container.classList.toggle("rain");
+			break;
+		case "cloudy":
+			container.classList.toggle("cloudy");
+			break;
+		default:
+			container.classList.toggle("default");
+	}
+}
 
 async function getWeather() {
 	try {
 		const response = await fetch(
 			"https://api.openweathermap.org/data/2.5/weather?q=Stockholm,Sweden&units=metric&APPID=162e87975bc70dc88548f3920f1c4fdf"
 		);
-		const weather = await response.json();
+		const weatherResponse = await response.json();
+		const weatherClass = weatherResponse.weather[0].main;
+		const currentTemperature = Math.round(weatherResponse.main.temp);
+		setWeatherClass(weatherClass.toLowerCase());
 
-		const li = document.createElement("li");
-		li.innerHTML += `
-			<div>
-				<h2>
-					${weather.name}
-				</h2>
-				<p>Temperature ${weather.main.temp.toFixed(1)}</p>
-				<p>Description: ${weather.weather[0].description}</p>
-			</div>`;
+		const weatherParagraph = document.createElement("p");
+		weatherParagraph.innerHTML += `${weatherClass} | ${currentTemperature}&#176;`;
+		currentWeather.appendChild(weatherParagraph);
 
-		weatherContainer.appendChild(li);
+		const sunrise = new Date(weatherResponse.sys.sunrise * 1000)
+			.toLocaleTimeString("sv-SE", { hour: "2-digit", minute: "2-digit" })
+			.replace(":", ".");
+		const sunriseParagraph = document.createElement("p");
+		sunriseParagraph.innerHTML += `sunrise ${sunrise}`;
+		currentWeather.appendChild(sunriseParagraph);
+
+		const sunset = new Date(weatherResponse.sys.sunset * 1000)
+			.toLocaleTimeString("sv-SE", { hour: "2-digit", minute: "2-digit" })
+			.replace(":", ".");
+		const sunsetParagraph = document.createElement("p");
+		sunsetParagraph.innerHTML += `sunset ${sunset}`;
+		currentWeather.appendChild(sunsetParagraph);
 	} catch (err) {
 		console.error(err);
+		currentWeather.innerHTML += `Could not get today's weather`;
 	}
 }
 
