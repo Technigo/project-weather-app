90803a58841856e3f44c5b20db340437baf8ef68
carolina <carolina.beatriz.cardenas@gmail.com>
2023-10-12 22:14:09 +0200

    found the API for five days need to convert dt :1697144400 to time

--- weather.js
+++ weather.js
@@ -1,12 +1,16 @@
-const url = "https://api.openweathermap.org/data/2.5/weather?q=Uppsala,Sweden&units=metric&APPID=f40f4543214ad55ead8d6ca12cb39ee0"
+const urlForOneDay = "https://api.openweathermap.org/data/2.5/weather?q=Knivsta,Sweden&units=metric&APPID=f40f4543214ad55ead8d6ca12cb39ee0"
+const urlForMoreDays ="https://api.openweathermap.org/data/2.5/forecast?q=Knivsta,Sweden&units=metric&APPID=f40f4543214ad55ead8d6ca12cb39ee0"
 const thursday = document.getElementById("thursday")
+const city = document.getElementsByClassName("get-your-sunnies")
 const fetchWeather =  async () => {
     try{
-      const response = await fetch(url);
+      const response = await fetch(urlForMoreDays);
       const data = await response.json();
       console.log(data)
-      thursday.innerHTML = data.main.temp
-    
+    //   thursday.innerHTML = Math.round(data.main.temp)+"°"
+      thursday.innerHTML = Math.round(data.list[4].main.temp)+"°"
+      const howIsTheWeather = `Get your sunnies on. ${data.name} is looking rather great today.`
+      city[0].innerHTML = howIsTheWeather
     }catch(error) {
   console.error(error)
     }
