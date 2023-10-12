2f37c34a26102166b7b578ba8e03575670f83458
Disabledbarrel <elin.elisabeth.larsson@gmail.com>
2023-10-12 19:58:05 +0200

    step 1-2 weather app

---
+++ script.js
@@ -0,0 +1,26 @@
+const weatherContainer = document.getElementById("weather-container");
+
+async function getWeather() {
+	try {
+		const response = await fetch(
+			"https://api.openweathermap.org/data/2.5/weather?q=Stockholm,Sweden&units=metric&APPID=162e87975bc70dc88548f3920f1c4fdf"
+		);
+		const weather = await response.json();
+
+		const li = document.createElement("li");
+		li.innerHTML += `
+			<div>
+				<h2>
+					${weather.name}
+				</h2>
+				<p>Temperature ${weather.main.temp.toFixed(1)}</p>
+				<p>Description: ${weather.weather[0].description}</p>
+			</div>`;
+
+		weatherContainer.appendChild(li);
+	} catch (err) {
+		console.error(err);
+	}
+}
+
+getWeather();
