1cc1228d68551cda54b6e0e977f6e4cfea39549f
Naima Malik <noma_m@hotmail.com>
2023-10-12 16:33:47 +0200

    Add sunset and sunrise feature

--- script.js
+++ script.js
@@ -3,23 +3,53 @@ const apiKey =
 
 const container = document.querySelector(".weather-container");
 
-fetch(apiKey)
-  .then((response) => response.json())
-  .then((data) => {
-    console.log(data);
-    const cityName = data.name;
-    const temperature = Math.floor(data.main.temp);
-    const description = data.weather[0].description;
+const fetchWeatherData = () => {
+  fetch(apiKey)
+    .then((response) => response.json())
+    .then((data) => {
+      console.log(data);
+      const cityName = data.name;
+      const temperature = Math.floor(data.main.temp);
+      const description = data.weather[0].description;
 
-    const heading = document.createElement("h1");
-    const temp = document.createElement("h2");
-    const weatherDescription = document.createElement("p");
+      // Sunrise and sunset times
+      const sunrise = data.sys.sunrise * 1000;
+      const sunset = data.sys.sunset * 1000;
+      const sunriseTime = new Date(sunrise);
+      const sunsetTime = new Date(sunset);
+      const sunriseHours = sunriseTime.getHours();
+      const sunriseMinutes = sunriseTime.getMinutes();
+      const sunsetHours = sunsetTime.getHours();
+      const sunsetMinutes = sunsetTime.getMinutes();
 
-    heading.textContent = cityName;
-    temp.textContent = temperature;
-    weatherDescription.textContent = description;
+      const sunriseTimeString = `${
+        sunriseHours < 10 ? "0" : ""
+      }${sunriseHours}:${sunriseMinutes < 10 ? "0" : ""}${sunriseMinutes}`;
 
-    container.appendChild(heading);
-    container.appendChild(temp);
-    container.appendChild(weatherDescription);
-  });
+      const sunsetTimeString = `${sunsetHours < 10 ? "0" : ""}${sunsetHours}:${
+        sunsetMinutes < 10 ? "0" : ""
+      }${sunsetMinutes}`;
+
+      const heading = document.createElement("h1");
+      const temp = document.createElement("h2");
+      const weatherDescription = document.createElement("p");
+      const sunriseElement = document.createElement("p");
+      const sunsetElement = document.createElement("p");
+      const divElement = document.createElement("div");
+      divElement.classList.add("sunrise-sunset");
+
+      heading.textContent = cityName;
+      temp.textContent = `${temperature}°C`;
+      weatherDescription.textContent = description;
+      sunriseElement.textContent = `Sunrise   ${sunriseTimeString}`;
+      sunsetElement.textContent = `Sunset   ${sunsetTimeString}`;
+
+      container.appendChild(temp);
+      container.appendChild(heading);
+      container.appendChild(weatherDescription);
+      container.appendChild(divElement).appendChild(sunriseElement);
+      container.appendChild(divElement).appendChild(sunsetElement);
+    });
+};
+
+fetchWeatherData();
