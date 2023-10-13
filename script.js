5afbe111cda0a5222d66b9aebc3fe2c025172ba8
Umamaheswari Thiyagarajan <umagithub23@users.noreply.github.com>
2023-10-13 19:13:23 +0200

    Step1 + step2 done together with usha

--- script.js
+++ script.js
@@ -1,26 +1,48 @@
+const cityNameElement = document.getElementById('cityNameId')
 const weatherMainElement = document.getElementById('weatherMainId')
 const temperatureElement = document.getElementById('temperatureId')
-const sunriseElement = document.getElementById('sunriseId')
-const sunsetElement = document.getElementById('sunsetId')
+const errorElement = document.getElementById("errorId")
 
 
-const fetchWeatherData = async () => {
+const DEFAULT_CITY = 'Stockholm,Sweden'
+
+const API_KEY = "29e7c1a3b2d9e92ed27e8c3d97d654cd"
+const fetchWeatherData = async (city) => {
   try {
-    let url = "https://api.openweathermap.org/data/2.5/weather?q=Stockholm,Sweden&units=metric&APPID=29e7c1a3b2d9e92ed27e8c3d97d654cd";
-
+    const url = `https://api.openweathermap.org/data/2.5/weather?q=${city}&units=metric&APPID=${API_KEY}`;
+    // https://api.openweathermap.org/data/2.5/weather?q=Stockholm,Sweden&units=metric&APPID=29e7c1a3b2d9e92ed27e8c3d97d654cd
     const responseFromApi = await fetch(url);
-    const weatherData = await responseFromApi.json();
-    console.log(weatherData)
-    // testResult.innerHTML = JSON.stringify(weatherData)
+    const responseData = await responseFromApi.json();
 
-    weatherMainElement.innerHTML = weatherData.weather[0].main
-    temperatureElement.innerHTML = weatherData.main.temp
-    sunriseElement.innerHTML = weatherData.sys.sunrise
-    sunsetElement.innerHTML = weatherData.sys.sunset
+    // Error scenario: {"cod":"404","message":"city not found"}
+    if (responseData.cod === "404") {
+      errorElement.innerHTML = responseData.message
+      return undefined
+    }
+
+    const weatherData = {
+      cityName: responseData.name,
+      description: responseData.weather[0].description,
+      temperature: responseData.main.temp.toFixed(1), // round to 1 decimal place
+    }
+
+    return weatherData
   } catch (error) {
-    console.log(error);
+    errorElement.innerHTML = JSON.stringify(error)
   }
 };
 
-fetchWeatherData()
+const displayWeatherData = async (city) => {
+  const weatherData = await fetchWeatherData(city)
+
+  if (weatherData === undefined) {
+    return
+  }
+
+  cityNameElement.innerHTML = weatherData.cityName
+  weatherMainElement.innerHTML = weatherData.description
+  temperatureElement.innerHTML = `${weatherData.temperature} °C`
+}
+
+displayWeatherData(DEFAULT_CITY)
 
