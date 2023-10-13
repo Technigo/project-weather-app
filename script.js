73ba437052eb53fd1be814cba6add20d0965e921
Disabledbarrel <elin.elisabeth.larsson@gmail.com>
2023-10-13 08:53:41 +0200

    dynamic weather and styling

--- script.js
+++ script.js
@@ -1,17 +1,46 @@
+// Constants
+const SUNNY_MESSAGE =
+	"Get your sunnies on. Stockholm is looking rather great today.";
+const RAINY_MESSAGE =
+	"Don’t forget your umbrella. It’s wet in Stockholm today.";
+const CLOUDY_MESSAGE =
+	"Light a fire and get cosy. Stockholm is looking grey today.";
+
 const container = document.getElementById("container");
 const currentWeather = document.getElementById("currentWeather");
+const weatherImg = document.getElementById("weatherImg");
+const weatherText = document.getElementById("weatherText");
 
+function setImgSrc(src) {
+	console.log("hej hej");
+	weatherImg.src = `img/${src}.svg`;
+}
 // Feature: Sunrise and sunset
+function setSunTimeParagraph(sunTime, milliSeconds) {
+	const timeInHours = new Date(milliSeconds * 1000)
+		.toLocaleTimeString("sv-SE", { hour: "2-digit", minute: "2-digit" })
+		.replace(":", ".");
+	const sunTimeParagraph = document.createElement("p");
+	sunTimeParagraph.innerHTML += `${sunTime} ${timeInHours}`;
+	currentWeather.appendChild(sunTimeParagraph);
+}
+
 function setWeatherClass(weatherClass) {
 	switch (weatherClass) {
 		case "clear":
 			container.classList.toggle("clear");
+			setImgSrc("sunglasses");
+			weatherText.innerText = `${SUNNY_MESSAGE}`;
 			break;
 		case "rain":
 			container.classList.toggle("rain");
+			setImgSrc("umbrella");
+			weatherText.innerText = `${RAINY_MESSAGE}`;
 			break;
 		case "cloudy":
 			container.classList.toggle("cloudy");
+			setImgSrc("cloud");
+			weatherText.innerText = `${CLOUDY_MESSAGE}`;
 			break;
 		default:
 			container.classList.toggle("default");
@@ -26,25 +55,14 @@ async function getWeather() {
 		const weatherResponse = await response.json();
 		const weatherClass = weatherResponse.weather[0].main;
 		const currentTemperature = Math.round(weatherResponse.main.temp);
-		setWeatherClass(weatherClass.toLowerCase());
 
 		const weatherParagraph = document.createElement("p");
 		weatherParagraph.innerHTML += `${weatherClass} | ${currentTemperature}&#176;`;
 		currentWeather.appendChild(weatherParagraph);
 
-		const sunrise = new Date(weatherResponse.sys.sunrise * 1000)
-			.toLocaleTimeString("sv-SE", { hour: "2-digit", minute: "2-digit" })
-			.replace(":", ".");
-		const sunriseParagraph = document.createElement("p");
-		sunriseParagraph.innerHTML += `sunrise ${sunrise}`;
-		currentWeather.appendChild(sunriseParagraph);
-
-		const sunset = new Date(weatherResponse.sys.sunset * 1000)
-			.toLocaleTimeString("sv-SE", { hour: "2-digit", minute: "2-digit" })
-			.replace(":", ".");
-		const sunsetParagraph = document.createElement("p");
-		sunsetParagraph.innerHTML += `sunset ${sunset}`;
-		currentWeather.appendChild(sunsetParagraph);
+		setSunTimeParagraph("sunrise", weatherResponse.sys.sunrise);
+		setSunTimeParagraph("sunset", weatherResponse.sys.sunset);
+		setWeatherClass(weatherClass.toLowerCase());
 	} catch (err) {
 		console.error(err);
 		currentWeather.innerHTML += `Could not get today's weather`;
