254d6e4489d3209112e837280c170a7ed40ecc08
samrachnaa <samrachna.ad@gmail.com>
2023-10-13 13:15:23 +0200

    fetched description of today's weather and rounded up the temperature

--- weather.js
+++ weather.js
@@ -1,4 +1,4 @@
-const url = "https://api.openweathermap.org/data/2.5/weather?q=Uppsala,Sweden&units=metric&APPID=f40f4543214ad55ead8d6ca12cb39ee0"
+const url = "https://api.openweathermap.org/data/2.5/weather?q=Stockholm,Sweden&units=metric&APPID=f40f4543214ad55ead8d6ca12cb39ee0"
 const thursday = document.getElementById("thursday")
 const sunrise = document.getElementById("sunrise")
 const sunset = document.getElementById("sunset")
@@ -12,7 +12,7 @@ const fetchWeather =  async () => {
       const response = await fetch(url);
       const data = await response.json();
       console.log(data)
-      thursday.innerHTML = data.main.temp
+      thursday.innerHTML = data.main.temp.toFixed(1)
     
     }catch(error) {
   console.error(error)
@@ -25,8 +25,8 @@ const fetchTodaysWeather =  async () => {
     const todaysResponse = await fetch(url);
     const todaysData = await todaysResponse.json();
     console.log(todaysData)
-    todaysWeather.innerHTML = todaysData.weather.description
-    todaysTemperature.innerHTML = todaysData.main.temp
+    todaysWeather.innerHTML = todaysData.weather[0].description
+    todaysTemperature.innerHTML = todaysData.main.temp.toFixed(1)
     sunrise.innerHTML = new Date(todaysData.sys.sunrise * 1000).toLocaleTimeString([], { hour: '2-digit', minute: '2-digit' })
     sunset.innerHTML = new Date(todaysData.sys.sunset * 1000).toLocaleTimeString([], { hour: '2-digit', minute: '2-digit' })
   }
