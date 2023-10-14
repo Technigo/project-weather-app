a30c4c90d45faf9cbf050a83ba95df64a957c982
psterner <psterner@hotmail.com>
2023-10-14 15:34:05 +0200

    Add eventlistener on weatherBtn to randomly select a city and displaying weather when clicking

--- js/main.js
+++ js/main.js
@@ -1,6 +1,12 @@
-import { fetchWeatherDataByCity } from "./geolocation.js";
+import { fetchWeatherDataByCity, fetchRandomWeather } from "./geolocation.js";
 
 // Intialize the app when the DOM is loaded
 document.addEventListener("DOMContentLoaded", function () {
-  // Add event listeners for weatherBtn
+  const weatherBtn = document.getElementById("btn-next-city");
+
+  // Add event listeners for weather button. Call funciton to fetch random weather data
+  weatherBtn.addEventListener("click", () => {
+    // Function that will generate different weather depending on the city;
+    fetchRandomWeather();
+  });
 });
