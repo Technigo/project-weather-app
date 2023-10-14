8b427069f7a29e5618cab22627c7a552d7470d9a
samrachnaa <samrachna.ad@gmail.com>
2023-10-15 00:02:10 +0200

    color change and icon css change

--- weather.js
+++ weather.js
@@ -1,6 +1,12 @@
-const url = "https://api.openweathermap.org/data/2.5/weather?q=Stockholm,Sweden&units=metric&APPID=f40f4543214ad55ead8d6ca12cb39ee0"
-const urlForecast ="https://api.openweathermap.org/data/2.5/forecast?q=Knivsta,Sweden&units=metric&APPID=f40f4543214ad55ead8d6ca12cb39ee0"
+let city = "Knivsta"
+
+let url ="https://api.openweathermap.org/data/2.5/weather?q=city,Sweden&units=metric&APPID=f40f4543214ad55ead8d6ca12cb39ee0"
+let newUrl = new URL(url)
+console.log(newUrl)
+newUrl = newUrl.searchParams.set("q", city);
+let urlForecast ="https://api.openweathermap.org/data/2.5/forecast?q=city,Sweden&units=metric&APPID=f40f4543214ad55ead8d6ca12cb39ee0"
 const body = document.getElementById("body")
+const icon = document.getElementById("icon")
 const sunrise = document.getElementById("sunrise")
 const sunset = document.getElementById("sunset")
 const weatherDescription = document.getElementById("weatherUpdate")
@@ -9,17 +15,16 @@ const todaysTemperature = document.getElementById("dayTemperature")
 
 const mon = [].slice.call(document.querySelectorAll(".mon"),2)
 const weekday = ["sun","mon","tue","wed","thu","fri","sat"];
-let city = "Stockholm"
 
 const pickTodaysDescription = (todaysDescription) => {
   body.classList.remove(...body.classList)
-
   if
   (todaysDescription === "Clear") {
     body.classList.add("suns-out")
     weatherDescription.innerHTML = `Get your sunnies on. ${city} is looking rather great today.`
   } 
    else if  (todaysDescription === "Clouds") {
+    icon.src = "./design/design2/icons/noun_Cloud_1188486.svg"
     body.classList.add("cloudy")
     weatherDescription.innerHTML = `Time to light a fire and get cosy. ${city} is looking grey today.`
   }
@@ -32,8 +37,18 @@ const pickTodaysDescription = (todaysDescription) => {
   }
 }
 
+const setTemperature = (fiveDays) => {
+    for (let index = 5; index < mon.length; index++) {
+        mon[index].innerHTML = Math.round(fiveDays[index - 5].main.temp) + "°";
+    }
+}
 
-
+const setDayName = (fiveDays) => {
+    for (let index = 0; index < 5; index++) {
+        const date = new Date(fiveDays[index].dt * 1000);
+        mon[index].innerHTML = weekday[date.getDay()];
+    }
+}
 const fetchWeather =  async () => {
     try{
       const response = await fetch(urlForecast);
@@ -44,39 +59,30 @@ const fetchWeather =  async () => {
         let dtTime = date.getHours();
         return time === dtTime;
         });
+        console.log(fiveDays[0].weather[0].main)
+        pickTodaysDescription(fiveDays[0].weather[0].main)
 
-    setDayName(fiveDays);    
-
-    
+    setDayName(fiveDays); 
+    console.log(fiveDays)   
     setTemperature(fiveDays);
 
     }catch(error) {
         console.error(error);
     }
-
-    function setTemperature(fiveDays) {
-        for (let index = 5; index < mon.length; index++) {
-            mon[index].innerHTML = Math.round(fiveDays[index - 5].main.temp) + "°";
-        }
-    }
-
-    function setDayName(fiveDays) {
-        for (let index = 0; index < 5; index++) {
-            const date = new Date(fiveDays[index].dt * 1000);
-            mon[index].innerHTML = weekday[date.getDay()];
-        }
-    }
 };
 
+const changeCity = () => {
+city = document.getElementById("city").value;
+urlForecast = new URL(urlForecast);
+urlForecast.searchParams.set("q", city);
 fetchWeather()
+}
 
-const fetchTodaysWeather =  async () => {
+const fetchSunriseSunset =  async () => {
   try{
     const todaysResponse = await fetch(url);
     const todaysData = await todaysResponse.json();
-    console.log(todaysData)
     todaysWeather.innerHTML = todaysData.weather[0].description
-    todaysTemperature.innerHTML = todaysData.main.temp.toFixed(1)
     sunrise.innerHTML = new Date(todaysData.sys.sunrise * 1000).toLocaleTimeString([], { hour: '2-digit', minute: '2-digit' })
     sunset.innerHTML = new Date(todaysData.sys.sunset * 1000).toLocaleTimeString([], { hour: '2-digit', minute: '2-digit' })
     
@@ -86,4 +92,7 @@ const fetchTodaysWeather =  async () => {
   console.log(error)
   }
 };
-fetchTodaysWeather()
+
+fetchWeather()
+fetchSunriseSunset()
+
