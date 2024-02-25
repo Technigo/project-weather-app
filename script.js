81e41064c4dd327a952ca1ff801421081bf142a7
Sofia Johansson <153744881+sofia32057@users.noreply.github.com>
2024-02-25 17:55:03 +0100

    Update - getting forecast max/min from entire day, instead of only noon

--- script.js
+++ script.js
@@ -1,7 +1,7 @@
 // DOM
 const weatherToday = document.getElementById("weather-today");
 const weatherForecast = document.getElementById("weather-forecast");
-const body = document.querySelector("body");
+const weatherBackground = document.querySelector(".weather-background");
 
 // Pick icon
 const pickIcon = iconId => {
@@ -37,9 +37,40 @@ const pickIcon = iconId => {
   }
 };
 
+// Change background and image if it's night
+const setNight = json => {
+  const currentTime = Date.now();
+  const sunset = convertTime(json.sys.sunset);
+  if (currentTime > sunset) {
+    weatherBackground.classList.add("night");
+    return "./design/design1/assets/moon.svg";
+  } else {
+    weatherBackground.classList.remove("night");
+    return "./design/design1/assets/sun.svg";
+  }
+};
+
 // Filter forecast
-const fiterForecast = json => {
+const fiterNoons = json => {
   return json.list.filter(obj => obj.dt_txt.includes("12:00"));
+};
+
+// Get max for entire day from forecast json
+const getMax = (day, json) => {
+  const date = convertTime(day.dt).getDate(); // Convert milliseconds to a date
+  const max = json.list
+    .filter(entry => entry.dt_txt.includes(date))
+    .sort((a, b) => a.main.temp_max > b.main.temp_max)[0];
+  return Math.floor(max.main.temp_max);
+};
+
+// Get min for entire day from forecast json
+const getMin = (day, json) => {
+  const date = convertTime(day.dt).getDate(); // Convert milliseconds to a date
+  const min = json.list
+    .filter(entry => entry.dt_txt.includes(date))
+    .sort((a, b) => b.main.temp_max > a.main.temp_max)[0];
+  return Math.floor(min.main.temp_min);
 };
 
 // convert to weekday
@@ -70,34 +101,20 @@ const toWeekday = date => {
   }
 };
 
-// Change background if it's night
-const setBackground = json => {
-  const currentTime = Date.now();
-  const sunset = json.sys.sunset * 1000;
-  if (currentTime > sunset) {
-    body.classList.add("night");
-    return "./design/design1/assets/moon.svg";
-  } else {
-    body.classList.remove("night");
-    return "./design/design1/assets/sun.svg";
-  }
-};
-
 // Convert milliseconds to readable time HH:MM
 const convertTime = milliseconds => {
-  const date = new Date(milliseconds * 1000); //The time from the API is almost at epoch...
-  const hours = date.getHours();
-  const mins = date.getMinutes();
-  return `${hours}:${mins}`;
+  return new Date(milliseconds * 1000); //The time from the API is missing zeros and is almost at epoch...
 };
 
 // function to print current weather to DOM
 const printWeather = json => {
+  const sunriseTime = convertTime(json.sys.sunrise);
+  const sunsetTime = convertTime(json.sys.sunset);
   console.log("Weather", json);
   weatherToday.innerHTML = `
   <p class="temp-current">${Math.floor(json.main.temp)}<span>°C</span></p>
   <img
-    src="${setBackground(json)}"
+    src="${setNight(json)}"
     alt="Sun is up!"
     class="weather-img" />
   <p class="city">${json.name}</p>
@@ -105,30 +122,31 @@ const printWeather = json => {
   <div class="sun">
     <div id="sunrise">
       <p class="label">sunrise</p>
-      <p class="time">${convertTime(json.sys.sunrise)}</p>
+      <p class="time">${sunriseTime.getHours()}:${sunriseTime.getMinutes()}</p>
     </div>
     <div id="sunset">
       <p class="label">sunset</p>
-      <p class="time">${convertTime(json.sys.sunset)}</p>
+      <p class="time">${sunsetTime.getHours()}:${sunsetTime.getMinutes()}</p>
+      </div></p>
     </div>
   </div>
   `;
 };
 // function to print Forecast to DOM
 const printForecast = json => {
-  console.log(json);
-  const list = fiterForecast(json);
-  console.log(list);
+  console.log("Forecast", json);
+  const list = fiterNoons(json);
+  console.log("Noons", list);
   weatherForecast.innerHTML = "";
   list.forEach(obj => {
-    let day = toWeekday(obj.dt_txt);
+    const maxTemp = getMax(obj, json);
+    const minTemp = getMin(obj, json);
+    const day = toWeekday(obj.dt_txt);
     weatherForecast.innerHTML += `
       <div class="forecast-day">
         <p class="forecast-day-label">${day}</p>
         <i class="weather-icon">${pickIcon(obj.weather[0].icon)}</i>
-        <p>${Math.floor(obj.main.temp_max)} / ${Math.floor(
-      obj.main.temp_min
-    )} °C</p>
+        <p>${maxTemp} / ${minTemp} °C</p>
       </div>
     `;
   });
