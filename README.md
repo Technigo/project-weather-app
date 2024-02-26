ad439721ebc63b4d8bd40dbe18bc3e370923dfb8
Nathalie <nathae28@hotmail.com>
2024-02-26 23:05:08 +0100

    trying to fetch API data

--- README.md
+++ README.md
@@ -4,6 +4,8 @@ Creating a weather app using the API from openweathermap.org and trying to repli
 
 ## The problem
 
+First problem that came, was the implementation of the API in the JS script. The documentation were provided, but it was still not clear to me what I have to do to make it work.
+
 Describe how you approached to problem, and what tools and techniques you used to solve it. How did you plan? What technologies did you use? If you had more time, what would be next?
 
 ## View it live
--- script.js
+++ script.js
@@ -1,11 +1,24 @@
 // DOM selectors
+const currentWeather = document.getElementById("weather-container");
 
-// Connecting weather API
+// Current weather
+const apiBaseURL = "https://api.openweathermap.org/data/2.5";
 const apiKey = "a1f68c4f65b632802ca0dd3405694457";
 let city = "Bern";
 
-const currentWeatherFeature = () => {
-  fetch(
-    `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}`,
-  );
-};
+fetch(`${apiBaseURL}/weather?q=${city}&units=metric&appid=${apiKey}`)
+  .then((response) => {
+    return response.json();
+  })
+  .then((json) => {
+    console.log(json);
+  });
+
+// Weather forecast
+fetch(`${apiBaseURL}/forecast?q=${city}&appid=${apiKey}`)
+  .then((response) => {
+    return response.json();
+  })
+  .then((json) => {
+    console.log(json);
+  });
