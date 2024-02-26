6db17e7396f9a0fc57e000b8148e704c630f5572
Tejpex <equis_angel@hotmail.com>
2024-02-26 13:24:24 +0100

    Created documents for code and connected them

---
+++ script.js
@@ -0,0 +1,10 @@
+const header = document.getElementById("header")
+const main = document.getElementById("main")
+const container = document.getElementById("container")
+
+header.innerHTML = `
+<h1>Hej</h1>
+`
+fetch ()
+
+main.innerHTML
