fe5b9beadbbbc50994607afd77d28ccf226e37ce
Tejpex <equis_angel@hotmail.com>
2024-02-26 14:48:50 +0100

    Fetched weather data

--- script.js
+++ script.js
@@ -5,6 +5,15 @@ const container = document.getElementById("container")
 header.innerHTML = `
 <h1>Hej</h1>
 `
-fetch ()
+fetch ("https://api.openweathermap.org/data/2.5/weather?q=Stockholm,Sweden&units=metric&APPID=8be7a87323d320c7bae11d84fa0a7c61")
+    .then((response) => {
+        return response.json()
+    })
+    .then((json) => {
+        main.innerHTML =
+        `There is weather in ${json.name} today.`
+    })
 
-main.innerHTML
+
+
+//API-key: 8be7a87323d320c7bae11d84fa0a7c61
