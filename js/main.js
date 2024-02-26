4c89fe631bbfe43af40a8013c5cb978625db512a
Pernilla Sterner <psterner@hotmail.com>
2024-02-26 15:08:08 +0100

    Controlling colors in main depending on weather

--- js/main.js
+++ js/main.js
@@ -1,3 +1,23 @@
+const data = {
+  forecast: "cloudy",
+  icon: "./assets/weather-icons/noun_Sunglasses_2055147.svg",
+  alt: "cloudy",
+  colorText: "#f47775",
+  backgroundColor: "#f4f7f8",
+  text: "Get your sunnies on. Stockholm is looking rather great today.",
+};
+
 const main = document.getElementById("main");
 
-const loadWeatherContent = () => {};
+const loadWeatherContent = () => {
+  // Controlling the color of main
+  main.style.backgroundColor = data.backgroundColor;
+  main.style.color = data.colorText;
+
+  main.innerHTML += `
+
+      `;
+};
+
+// Call the displayFooter function when the page is loaded
+window.onload = loadWeatherContent;
