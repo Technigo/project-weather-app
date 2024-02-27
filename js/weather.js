3194347d5ea576346636131271c0bd93fc426833
Pernilla Sterner <psterner@hotmail.com>
2024-02-27 09:41:14 +0100

    Add function that will generate the current weather HTML

--- js/weather.js
+++ js/weather.js
@@ -1,22 +1,15 @@
-import { forecastData } from "./data.js";
-import { loadWeatherContent } from "./main.js";
+// const data = {
+//   forecast: "cloudy",
+//   forecast_temp: "23",
+//   icon: "./assets/weather-icons/noun_Cloud_1188486.svg",
+//   alt: "cloudy",
+//   colorText: "#f47775",
+//   backgroundColor: "#f4f7f8",
+//   text: "Get your sunnies on. Stockholm is looking rather great today.",
+//   sunset: "22.30",
+//   sunrise: "08.00",
+// };
 
-const data = {
-  forecast: "cloudy",
-  forecast_temp: "23",
-  icon: "./assets/weather-icons/noun_Cloud_1188486.svg",
-  alt: "cloudy",
-  colorText: "#f47775",
-  backgroundColor: "#f4f7f8",
-  text: "Get your sunnies on. Stockholm is looking rather great today.",
-  sunset: "22.30",
-  sunrise: "08.00",
-};
-
-export const handleForecastData = (apiData) => {
-  console.log(apiData);
-
-  // Build a new data object with all logic
-
-  loadWeatherContent(data);
+export const getWeatherContainer = (data) => {
+  console.log(data);
 };
