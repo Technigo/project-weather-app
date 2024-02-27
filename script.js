1255bd762e946044a3745e4d7f9e2becec0193b1
Tejpex <equis_angel@hotmail.com>
2024-02-27 10:39:50 +0100

    Display temperature and weather type

--- script.js
+++ script.js
@@ -2,6 +2,10 @@ const header = document.getElementById("header")
 const main = document.getElementById("main")
 const container = document.getElementById("container")
 
+let weather = []
+let weatherObject = []
+let temperatureNow = ""
+
 header.innerHTML = `
 <h1>Hej</h1>
 `
@@ -10,10 +14,9 @@ fetch ("https://api.openweathermap.org/data/2.5/weather?q=Stockholm,Sweden&units
         return response.json()
     })
     .then((json) => {
-        main.innerHTML =
-        `There is weather in ${json.name} today.`
+        weather = json.weather
+        weatherObject = weather[0]
+        console.log (json)
+        temperatureNow = Math.round(json.main.temp)
+        main.innerHTML = `There is ${weatherObject.description} and ${temperatureNow} degrees in ${json.name} today.`;
     })
-
-
-
-//API-key: 8be7a87323d320c7bae11d84fa0a7c61
