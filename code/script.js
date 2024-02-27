a5d2b2b7054a9520febdcb9cab40cf7ea03925ed
Johanna Catalini Smith <johannacatalini@gmail.com>
2024-02-27 20:06:17 +0100

    Add API call.

---
+++ code/script.js
@@ -0,0 +1,6 @@
+const url = "https://api.openweathermap.org/data/2.5/weather?q=Stockholm,Sweden&units=metric&APPID=781cdd2e00a90d16de41361eb1c43353"
+fetch(url)
+.then(response => response.json())
+.then(forecast => {
+    console.log (forecast.weather[0].main)
+}) 
