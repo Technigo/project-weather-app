1cfd75e6b4ea834b6673fc2380d197a354db616d
Johanna Catalini Smith <johannacatalini@gmail.com>
2024-02-27 20:41:43 +0100

    add sunrise and sunset date data

--- code/script.js
+++ code/script.js
@@ -6,6 +6,12 @@ fetch(url)
     const weatherType = document.querySelector(".weatherType");
     weatherType.innerHTML = forecast.weather[0].main;
     const temperature = document.querySelector(".temperature");
-    temperature.innerHTML = forecast.main.temp
+    temperature.innerHTML = forecast.main.temp;
+    const sunrise = document.querySelector(".sunrise");
+    const sunriseDate = new Date(forecast.sys.sunrise * 1000);
+    sunrise.innerHTML = `${sunriseDate.getHours().toString().padStart(2, "0")}.${sunriseDate.getMinutes().toString().padStart(2, "0")}`;
+    const sunset = document.querySelector(".sunset");
+    const sunsetDate = new Date(forecast.sys.sunset * 1000);
+    sunset.innerHTML = `${sunsetDate.getHours().toString().padStart(2, "0")}.${sunsetDate.getMinutes().toString().padStart(2, "0")}`;
+    
   });
-
