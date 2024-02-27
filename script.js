a8f7b725f57be88e8cfd3cd5d61cec949ba06be0
Yifan-858 <133569248+Yifan-858@users.noreply.github.com>
2024-02-27 22:14:27 +0100

    udpate [script] fetch daily & weekly  weather data

--- script.js
+++ script.js
@@ -1,16 +1,63 @@
 const apiKey = "f9321b12d77c24027e5a25c9f625e63b";
-console.log("hi");
-const getWeather = () => {
-  console.log("hi");
+
+const tipText = document.querySelector(".tip-text");
+const getDailyWeather = () => {
   fetch(
-    `https://api.openweathermap.org/data/2.5/weather?q=gothenburg&unit=metric&appid=${apiKey}`
+    `https://api.openweathermap.org/data/2.5/weather?q=gothenburg&units=metric&appid=${apiKey}`
   )
     .then((response) => {
       return response.json();
     })
-    .then((json) => {
-      console.log(json);
+    .then((weatherData) => {
+      console.log(weatherData);
+      (weatherData) => {
+        displayDailyWeather(weatherData);
+      };
+    })
+    .catch((err) => {
+      console.log(err);
     });
 };
 
-getWeather();
+getDailyWeather();
+
+const displayDailyWeather = (weatherData) => {
+  const { name } = weatherData;
+  const { temp } = weatherData.main;
+};
+
+const sunriseTimestamp = 1709014431 * 1000;
+const sunsetTimestamp = 1709051787 * 1000;
+const sunriseDate = new Date(sunriseTimestamp);
+const sunsetDate = new Date(sunsetTimestamp);
+const formatTime = (date) => {
+  const hours = date.getUTCHours();
+  const minutes = date.getUTCMinutes();
+  const formattedMinutes = String(minutes).padStart(2, "0");
+
+  return `${hours}:${formattedMinutes}`;
+};
+const sunriseTime = formatTime(sunriseDate);
+const sunsetTime = formatTime(sunsetDate);
+console.log(`0${sunriseTime}`);
+console.log(`${sunsetTime}`);
+
+const getWeeklyWeather = () => {
+  fetch(
+    `https://api.openweathermap.org/data/2.5/forecast?q=gothenburg&appid=${apiKey}`
+  )
+    .then((response) => {
+      return response.json();
+    })
+    .then((weatherData) => {
+      console.log(weatherData);
+      //   (weatherData) => {
+      //     displayWeeklyWeather(weatherData);
+      //   };
+    })
+    .catch((err) => {
+      console.log(err);
+    });
+};
+
+getWeeklyWeather();
