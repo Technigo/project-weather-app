1ce86d9d65ab3fae29ca6f86730519ebb8852200
Trista Shan <trista8703@hotmail.com>
2024-02-28 22:00:07 +0200

    time not done yet, half

--- script.js
+++ script.js
@@ -1,5 +1,8 @@
 //DOM get element from HTML
 const temSunTime = document.getElementById('temperature-sun-time')
+// const temperature = document.getElementById('temperature')
+// const sunrise = document.getElementById('sunrise-time')
+// const sunset = document.getElementById('sunset-time')
 const remindImgText = document.getElementById('remind-img-text')
 const sevenDaysTemperature = document.getElementById('even-days-temperature')
 
@@ -16,9 +19,12 @@ const weatherInfo = () => {
     .then(response => response.json())
     .then(data => {
         remindTestCityName(data)
-        console.log (data)})
+        console.log (data.sys)
+        console.log(data.sys.sunrise * 1000)
+    })
 }
 weatherInfo()
+
 
 
 const remindTestCityName = (param) => {
@@ -26,24 +32,37 @@ const remindTestCityName = (param) => {
     let description = param.weather[0].description
     let temperature = Math.round(param.main.temp)
     let cityName = param.name
+
+    const sunriseTime = param.sys.sunrise * 1000
+    const sunsetTime = param.sys.sunset * 1000
+    const finlandUct = 2 * 60 * 60 * 1000 //EET = UCT + 2h
+    const finlandSunriseTime = new Date(sunriseTime + finlandUct)
+    const finlandSunsetTIme = new Date(sunsetTime + finlandUct)
+    
     if (dayWeather === 'clear') {
         temSunTime.innerHTML = `
-        <P> ${description} | ${temperature}</p>`
+        <P> ${description} | ${temperature}</p>
+        <p> Sunrise ${localeTimeString}</p>
+        <p> Sunset ${finlandSunsetTIme}</p>`
         remindImgText.innerHTML = `
         <h3 id="remind-text">Get your sunnies on. ${cityName} is looking rather great today.</h3>`
     } else if (dayWeather === 'Rain' || dayWeather === 'Drizzle') {
         temSunTime.innerHTML = `
-        <P> ${description} | ${temperature}</p>`
+        <P> ${description} | ${temperature}</p>
+        <p> Sunrise ${finlandSunriseTime}</p>
+        <p> Sunset ${finlandSunsetTIme}</p>`
         remindImgText.innerHTML = `
         <h3 id="remind-text">Don't forget your umbrella. It's wet in ${cityName} today.</h3>`
     } else if (dayWeather === 'Clouds') {
         temSunTime.innerHTML = `
-        <P> ${description} | ${temperature}</p>`
+        <P> ${description} | ${temperature}</p>
+        <p> Sunrise ${finlandSunriseTime}</p>
+        <p> Sunset ${finlandSunsetTIme}</p>`
         remindImgText.innerHTML = `
         <h3 id="remind-text">Light a fire and get cosy. ${cityName} is looking grey today.</h3>`
     } else {
         remindImgText.innerHTML = `
         <h3 id="remind-text">Oops... Something went wrong</h3>`
-    }
+    }  
 }
 
