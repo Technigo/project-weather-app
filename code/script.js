cd8b89ca2c32f182b68655531541045e31360024
SofieFerrari <sofiestrahl@gmail.com>
2024-02-28 23:05:02 +0100

    fetched city name, temp and description from api, worked on capitalizing first letter on description

--- code/script.js
+++ code/script.js
@@ -7,12 +7,37 @@ const city = "Umea, Sweden";
 */
 
 //DOM selectors
-const handleTemp = document.getElementById("temperature");
-const handleName = document.getElementById("city-name");
-const weatherDescription = document.getElementById("weather-description");
+const handleTemp = document.getElementById('temperature')
+const handleName = document.getElementById('city-name')
+const weatherDescription = document.getElementById('weather-description')
 
 //Fetching the API
-fetch(
-  "https://api.openweathermap.org/data/2.5/weather?q=Umea,Sweden&units=metric&APPID=1e48fdf267ccc8ee33c1c78150dcbab1"
-).then(response => response.json())
-    .then(data => )
+const fetchWeather = () => {
+	fetch(
+		'https://api.openweathermap.org/data/2.5/weather?q=Umea,Sweden&units=metric&APPID=1e48fdf267ccc8ee33c1c78150dcbab1'
+	)
+		.then((response) => response.json())
+		.then((data) => {
+			console.log(data)
+
+			const nameChoice = data.name
+			const tempChoice = Math.trunc(data.main.temp)
+			const weatherDescr = data.weather[0].description
+
+            console.log(weatherDescr)
+
+			handleName.innerText = nameChoice
+			handleTemp.innerText = `${tempChoice} °C`
+			weatherDescription.innerText = `${weatherDescr}`
+		})
+}
+fetchWeather()
+
+
+
+const capitalizeFirstLetter = (str) => {
+  return `${str[0].toUpperCase()}${str.slice(1)}`;
+}
+
+let myString = "codedamn";
+console.log(capitalizeFirstLetter(myString)); // Outputs: Codedamn
