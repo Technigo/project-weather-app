ca27f831d8365fda0fbd5ee4efaf329d221371bf
Tejpex <equis_angel@hotmail.com>
2024-02-28 15:50:07 +0100

    Made header show sunrise/sunset-time

--- script.js
+++ script.js
@@ -6,33 +6,50 @@ const URL_BASE = "https://api.openweathermap.org/data/2.5/weather?q=Stockholm,Sw
 const APP_KEY = "8be7a87323d320c7bae11d84fa0a7c61"
 const URL = URL_BASE+APP_KEY
 
+
+
 let city = ""
 let weatherType = ""
 let temperatureNow = ""
+let sunriseData = ""
+let sunsetData = ""
 
-header.innerHTML = `
-<h1>Hej</h1>
-`
 const fetchData = () => {
     fetch (URL)
         .then((response) => {
             return response.json()
         })
         .then((json) => {
-            console.log (json)
+            city = json.name
+            weatherType = json.weather[0].main
+            temperatureNow = json.main.temp.toFixed(1)
             printWeatherInfo (json)
+            printSunInfo (json)
         })
         .catch((error) => {
             console.log (error)
         })
 }
 
-const printWeatherInfo = (json) => {
-    city = json.name;
-    weatherType = json.weather[0].description;
-    temperatureNow = Math.round(json.main.temp);
+const printWeatherInfo = () => {
     main.innerHTML = `
-    There is ${weatherType} and ${temperatureNow} degrees in ${city} today.
+    <h1>There is ${weatherType} and ${temperatureNow} degrees in ${city} today.</h1>
+    `;
+}
+
+const printSunInfo = (json) => {
+    sunriseData = json.sys.sunrise
+    const sunriseHour = new Date(sunriseData*1000).getHours()
+    const sunriseMinute = new Date(sunriseData*1000).getMinutes();
+    const sunriseTime = `${sunriseHour}:${sunriseMinute}`
+    sunsetData = json.sys.sunset
+    const sunsetHour = new Date(sunsetData * 1000).getHours()
+    const sunsetMinute = new Date(sunsetData * 1000).getMinutes();
+    const sunsetTime = `${sunsetHour}:${sunsetMinute}`;
+    header.innerHTML = `
+    <p>${weatherType} | ${temperatureNow}°</p>
+    <p>sunrise ${sunriseTime}</p>
+    <p>sunset ${sunsetTime}</p>
     `;
 }
 
