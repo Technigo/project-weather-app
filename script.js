a529beca45042a42a1904580b07ca0735fbe7e12
Trista Shan <trista8703@hotmail.com>
2024-02-28 20:01:41 +0200

    city name, temperature and description done

--- script.js
+++ script.js
@@ -1,7 +1,46 @@
-//fetch API
+//DOM get element from HTML
+const temSunTime = document.getElementById('temperature-sun-time')
+const remindImgText = document.getElementById('remind-img-text')
+const sevenDaysTemperature = document.getElementById('even-days-temperature')
+
+//DOM create in JS
+// 1- left corner 
+
+
+
+
+
+//fetch API, weather in Helsinki
 const weatherInfo = () => {
-    fetch('https://api.openweathermap.org/data/2.5/weather?q=Stockholm,Sweden&units=metric&APPID=0a3f5beba05e6db2d5da18ddf3283c92')
+    fetch('https://api.openweathermap.org/data/2.5/weather?q=Helsinki,Finland&units=metric&appid=0a3f5beba05e6db2d5da18ddf3283c92')
     .then(response => response.json())
-    .then(data => console.log(data))
+    .then(data => {
+        remindTestCityName(data)
+        console.log (data)})
 }
 weatherInfo()
+
+
+const remindTestCityName = (param) => {
+    let dayWeather = param.weather[0].main
+    let description = param.weather[0].description
+    let temperature = Math.round(param.main.temp)
+    if (dayWeather === 'clear') {
+        temSunTime.innerHTML = `
+        <P> ${description} | ${temperature}</p>`
+        remindImgText.innerHTML = `
+        <h3 id="remind-text">Get your sunnies on. ${param.name} is looking rather great today.</h3>`
+    } else if (dayWeather === 'rain' || 'Drizzle') {
+        temSunTime.innerHTML = `
+        <P> ${description} | ${temperature}</p>`
+        remindImgText.innerHTML = `
+        <h3 id="remind-text">Don't forget your umbrella. It's wet in ${param.name} today.</h3>`
+    } else if (dayWeather === 'cloudy') {
+        temSunTime.innerHTML = `
+        <P> ${description} | ${temperature}</p>`
+        remindImgText.innerHTML = `
+        <h3 id="remind-text">Light a fire and get cosy. ${param.name} is looking grey today.</h3>`
+    } 
+    
+}
+
