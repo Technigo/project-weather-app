5cb0cc792020768da118a4082b7b122c9ba7e2e5
Johanna Catalini Smith <johannacatalini@gmail.com>
2024-02-28 15:35:59 +0100

    Reorganzing code and add comments.

--- code/script.js
+++ code/script.js
@@ -1,76 +1,84 @@
+//Declare variables that refer to HTML elements (DOM-selector)
+const weatherType = document.querySelector(".weatherType");
+const temperature = document.querySelector(".temperature");
+const sunrise = document.querySelector(".sunrise");
+const textAboutWeather = document.querySelector(".textAboutWeather");
+const sunset = document.querySelector(".sunset");
+const icon = document.querySelector(".icon");
+const days = document.querySelector(".days");
+
+//Declare (global) varibles
+
 const url =
   "https://api.openweathermap.org/data/2.5/weather?q=Stockholm,Sweden&units=metric&APPID=781cdd2e00a90d16de41361eb1c43353";
+
+const secondUrl =
+  "https://api.openweathermap.org/data/2.5/forecast?q=Stockholm,Sweden&units=metric&APPID=781cdd2e00a90d16de41361eb1c43353";
+
+//Declare funtions "hourAndminutes" and "renderWeather"
+
+//Here we turn the time into a string and set it to xx.xx (four numbers)
+//"date" is an object built in in js, so you can work with time and dates. 
+function hourAndminutes(date) {
+  return `${date.getHours().toString().padStart(2, "0")}.${date
+    .getMinutes()
+    .toString()
+    .padStart(2, "0")}`;
+}
+
+//This function renders the weather
+function renderWeather(weather) {
+  weatherType.innerHTML = weather.weather[0].main;
+
+  //math.round to remove the decimals from the degrees. "&deg" is the degree symbol.
+  temperature.innerHTML = `${Math.round(weather.main.temp)}&deg;`;
+
+  //"new.date" is the timestamp from the 1970's. "*1000 is the turn the time into milliseconds, because js want's it like that."
+  const sunriseTime = new Date(weather.sys.sunrise * 1000);
+  sunrise.innerHTML = hourAndminutes(sunriseTime);
+
+  const sunsetDate = new Date(weather.sys.sunset * 1000);
+  sunset.innerHTML = hourAndminutes(sunsetDate);
+
+  if (weather.weather[0].main == "Clouds") {
+    icon.innerHTML = `<img src="/icons/cloud.svg" />`;
+    textAboutWeather.innerHTML = `Light a fire and get cosy. Stockholm is looking grey today.`;
+  } else if (weather.weather[0].main == "Rain") {
+    icon.innerHTML = `<img src="/icons/umbrella.svg" />`;
+    textAboutWeather.innerHTML = `Don't forget your umbrella. It's wet in Stockholm today.`;
+  } else {
+    icon.innerHTML = `<img src="/icons/sunglasses.svg" />`;
+    textAboutWeather.innerHTML = `Get your sunnies on. Stockholm is looking rather great today.`;
+  }
+}
+
 fetch(url)
+  //Then&respons turns the data from url into a object
+  .then((response) => response.json())
+  .then((weather) => {
+    //Call the function to run here
+    renderWeather(weather);
+  });
+
+
+
+
+
+
+
+
+
+
+
+  
+
+fetch(secondUrl)
   .then((response) => response.json())
   .then((forecast) => {
-    const weatherType = document.querySelector(".weatherType");
-    weatherType.innerHTML = forecast.weather[0].main;
-    const temperature = document.querySelector(".temperature");
-    temperature.innerHTML = forecast.main.temp;
-    const sunrise = document.querySelector(".sunrise");
-    const sunriseDate = new Date(forecast.sys.sunrise * 1000);
-    sunrise.innerHTML = `${sunriseDate
-      .getHours()
-      .toString()
-      .padStart(2, "0")}.${sunriseDate
-      .getMinutes()
-      .toString()
-      .padStart(2, "0")}`;
-    const sunset = document.querySelector(".sunset");
-    const sunsetDate = new Date(forecast.sys.sunset * 1000);
-    sunset.innerHTML = `${sunsetDate
-      .getHours()
-      .toString()
-      .padStart(2, "0")}.${sunsetDate
-      .getMinutes()
-      .toString()
-      .padStart(2, "0")}`;
-    const textAboutWeather = document.querySelector(".textAboutWeather");
-    const icon = document.querySelector(".icon");
-    if (forecast.weather[0].main == "Clouds") {
-      icon.innerHTML = `<img src="/icons/cloud.svg" />`;
-      textAboutWeather.innerHTML = `Light a fire and get cosy. Stockholm is looking grey today.`;
-    } else if (forecast.weather[0].main == "Rain") {
-      icon.innerHTML = `<img src="/icons/umbrella.svg" />`;
-      textAboutWeather.innerHTML = `Don't forget your umbrella. It's wet in Stockholm today.`;
-    } else {
-      icon.innerHTML = `<img src="/icons/sunglasses.svg" />`;
-      textAboutWeather.innerHTML = `Get your sunnies on. Stockholm is looking rather great today.`;
+    for (const day of forecast.list) {
+      const date = new Date(day.dt * 1000);
+      days.innerHTML += `${date.toLocaleDateString("en", {
+        weekday: "short",
+      })} ${day.main.temp} `;
     }
-
   });
-
-
-
-  const secondUrl = "https://api.openweathermap.org/data/2.5/forecast?q=Stockholm,Sweden&units=metric&APPID=YOUR_API_KEY";
-  fetch(secondUrl)
-    .then((response) => response.json())
-    .then((secondForecast) => {
-      const days = document.querySelector(".days");
-    })
-
-
-  /*
- 
-
-
-  
-  return fetch(apiUrl)
-    .then((response) => response.json())
-    .catch((error) => {
-      console.error("Error:", error);
-      throw error;
-
-
-
-    // Function to render the weekly weather forecast
-    const renderWeeklyForecast = (data) => {
-    const forecastList = data.list;
-    const forecastContainer = document.getElementById("forecast-list");
-    forecastContainer.innerHTML = ""; // Clear previous forecast data
-
-
-  });
-
-
-  
