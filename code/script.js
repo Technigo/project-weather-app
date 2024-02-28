f4a52e324d518e84f349d810647a32c745726d8b
Alma Herrström <almaherrstrom@Almas-Air-4.lan>
2024-02-28 14:21:56 +0100

    Connect API and present city, temp and weather type

--- code/script.js
+++ code/script.js
@@ -8,30 +8,44 @@ const sunset = document.getElementById("sunset")
 const forecastSection = document.getElementById("forecast-section")
 const date = document.getElementById("date")
 
-////////// Get time and date and display in app //////////
+////////// Get time + date and display in app //////////
 const currentDate = new Date().toLocaleDateString()
 const currentTime = new Date().toLocaleTimeString()
 
 date.innerHTML = `${currentDate} ${currentTime}`
 
-console.log(`Current time is ${currentTime}`)
-
 ////////// API URL storing //////////
-//General URL
-const urlWeatherData = "https://api.openweathermap.org/data/2.5/weather?units=metric&q="
-//My API key
-const API_KEY = "206e380881feb4fbbb5f8d99cb75f06d"
+const BASE_URL =
+  "https://api.openweathermap.org/data/2.5/weather?units=metric&q="
+const API_KEY = "206e380881feb4fbbb5f8d99cb75f06d" //query param
+const cityName = "Stockholm" //path param
+const forecastURL =
+  "https://api.openweathermap.org/data/2.5/weather?units=metric&q="
 
+/////Display city name in app
+city.innerHTML = cityName
 
+//Function to fetch information for the current weather
 
 const fetchWeatherData = () => {
-    fetch('https://api.openweathermap.org/data/2.5/weather?q=Stockholm,Sweden&units=metric&APPID=206e380881feb4fbbb5f8d99cb75f06d')
+  fetch(`${BASE_URL}${cityName}&appid=${API_KEY}`)
     .then((response) => {
       return response.json()
     })
     .then((json) => {
-    console.log(json)
+      console.log(json)
+
+      //One decimal as per instructions
+      const temp = json.main.temp.toFixed(1)
+      //Weather description
+      const weatherDescription = json.weather[0].description
+      //Modify HTML
+      temperature.innerHTML = `${temp}`
+      weather.innerHTML = `${weatherDescription}`
+      console.log("temp:", temp)
+      console.log("weather:", weatherDescription)
+      console.log("city:", cityName)
     })
-    }
-    
-    fetchWeatherData()
+}
+
+fetchWeatherData()
