9fbcc132fa7e8e03744d9f9e6c57d7e0293335c6
Frida Svensson <fridamarisvensson@gmail.com>
2024-02-28 22:26:10 +0100

    add api key and url

--- design/design2/script.js
+++ design/design2/script.js
@@ -1,6 +1,9 @@
 const MY_API_KEY = "31320abec19306a046f96f4c46f01157"
-const BASE_URL = 
-const URL = 
+
+// URL for the Call current weather data
+const BASE_URL_CURRENT_WEATHER = "https://api.openweathermap.org/data/2.5/weather?q=Stockholm,Sweden&units=metric&APPID=YOUR_API_KEY"
+
+const URL_CURRENT_WEATHER = BAS
 
 
 
