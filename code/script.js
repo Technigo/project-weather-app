169f1c2721ff4c9e6edc5b375dc55e1aa7b7464f
fridaforser <fridaforser@gmail.com>
2024-02-29 12:12:32 +0100

    Added if/else for the different weathers and edited the text and icons

--- code/script.js
+++ code/script.js
@@ -1,24 +1,30 @@
 const today = document.getElementById ("today")
+const text = document.getElementById ("text")
 const week = document.getElementById ("week")
+const icon = document.getElementById ("icon")
+
 
 fetch("https://api.openweathermap.org/data/2.5/weather?q=Stockholm,Sweden&units=metric&APPID=725ee441189d3e16a4e4aa74b081805e")
     .then((response) => {
        return response.json()
     })
     .then ((json) => {
-        today.innerHTML = `<h3>The weather in Stockholm right now is: ${json.main.temp} degrees Celsius</h3>`
+        //Added a variable to be able to round up today's temperature
+        let temperatureRound = Math.round(json.main.temp)
+        console.log(json)
+        today.innerHTML = `<h4>
+        ${json.weather[0].main} | ${temperatureRound}°<br>
+        </h4>`
+        if (json.weather[0].main == "Clear"){
+        icon.innerHTML = `<img src="assets/noun_Sunglasses_2055147.svg"/>`
+        text.innerHTML = `<h2>Get your sunnies on. ${json.name} is looking rather great today. </h2>`
+        }
+        else if (json.weather[0].main == "Rain"){
+        icon.innerHTML = `<img src="assets/noun_Umbrella_2030530.svg"/>`
+        text.innerHTML = `<h2>Don't forget your umbrella. It's wet in ${json.name} today.</h2>`
+        }
+        else if (json.weather[0].main == "Clouds"){
+        icon.innerHTML = `<img src="assets/noun_Cloud_1188486.svg"/>`
+        text.innerHTML = `<h2>Light a fire and get cosy. ${json.name} is looking grey today. </h2>`
+        }
     })
-
-fetch("https://api.openweathermap.org/data/2.5/forecast?q=Stockholm,Sweden&units=metric&APPID=725ee441189d3e16a4e4aa74b081805e")
-    .then((response) => {
-       return response.json()
-    })
-    .then ((json) => {
-    json.list.forEach((forecast) => {
-    week.innerHTML += `<p>${forecast.main.temp_min}° / ${forecast.main.temp_max} °C</p>`
-    })
-    })
-
-
-
- 
