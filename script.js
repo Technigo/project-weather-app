2da590cf6791d40d06bba8a7948db96f0d01497f
Mai Kanetaka <maikanetaka@gmail.com>
2024-03-01 00:28:32 +0100

    Update weather and temperature

--- script.js
+++ script.js
@@ -1,54 +1,112 @@
+const weatherDescriptionAndTemperatureElement = document.getElementById(
+  "weather-description-and-temperature"
+);
+const descriptionElement = document.getElementById("description");
+const weatherConditions = {
+  sunny: {
+    id: 800,
+    class: "sunny",
+    message: "Sunny",
+  },
+  cloudy: {
+    minId: 801,
+    maxId: 899,
+    class: "cloudy",
+    message: "Cloudy",
+  },
+  rainy: {
+    minId: 500,
+    maxId: 599,
+    class: "rainy",
+    message: "Rainy",
+  },
+};
+
 //Step 1 - use fetch() to load the weather data
 fetch(
   "https://api.openweathermap.org/data/2.5/weather?q=Stockholm,Sweden&units=metric&APPID=db63b41efc78e9f2c60ec93f035d8cff"
 )
-  //Step 2 - Present some data on your web app
-  //.then()ブロックの内部でのみデータのアクセス可能
   .then((response) => response.json())
   .then((data) => {
-    //Access the elements where I want to display
-    const cityNameElement = document.getElementById("city-name");
-    const temperatureElement = document.getElementById("temperature");
-    const descriptionElement = document.getElementById("description");
-    //Update the content of above elements with the data from API
-    cityNameElement.textContent = `City Name: ${data.name}`;
-    temperatureElement.textContent = `Temperature: ${data.main.temp.toFixed(
-      1
-    )}°C`; //1は、小数点以下の桁数。もし18.37度だったら、18.3と表示
-    descriptionElement.textContent = `Weather: ${data.weather[0].description}`;
+    const weather = data.weather[0];
+    const temperature = data.main.temp.toFixed(0);
+    let condition;
 
-    //Step 3 - Features
-    //Feature - sunset and sunrise
-    /*
-1. まず読みやすい形式にデータを加工する
-- new Date(): ミリ秒を受け取って、それを日時に変換する
-- *1000: UNIXタイムスタンプは秒で測定されますが、JavaScriptのDateオブジェクトはミリ秒を期待しているので、1000を掛けて秒からミリ秒に変換
-- .toLocaleTimeString([], {hour: '2-digit', minute:'2-digit'}): Dateオブジェクトを、特定のロケール（地域や言語）に適した時間の文字列に変換
-- []: デフォルトのロケール（ブラウザやシステムの設定に基づく）を使用
-- 2-digit: 時間を2桁の数字（"03"など）
-*/
-    const sunriseTime = new Date(data.sys.sunrise * 1000).toLocaleTimeString(
-      [],
-      {
-        hour: "2-digit",
-        minute: "2-digit",
-      }
-    );
-    const sunsetTime = new Date(data.sys.sunset * 1000).toLocaleTimeString([], {
-      hour: "2-digit",
-      minute: "2-digit",
-    });
+    if (weather.id === weatherConditions.sunny.id) {
+      condition = weatherConditions.sunny;
+    } else if (
+      weather.id >= weatherConditions.cloudy.minId &&
+      weather.id <= weatherConditions.cloudy.maxId
+    ) {
+      condition = weatherConditions.cloudy;
+    } else if (
+      weather.id >= weatherConditions.rainy.minId &&
+      weather.id <= weatherConditions.rainy.maxId
+    ) {
+      condition = weatherConditions.rainy;
+    }
 
-    /*
-2. 次に、加工したデータをDOM（Document Object Model）に挿入して表示する
-- まず必要なデータを準備してから、そのデータをウェブページに表示するという流れになり、コードの読みやすさと整理がよくなる
-- データを表示するための要素（<p id="sunrise"></p>、<p id="sunset"></p>）は、事前にHTMLに用意しておく
-*/
-    document.getElementById("sunrise").textContent = `Sunrise: ${sunriseTime}`;
-    document.getElementById("sunset").textContent = `Sunset: ${sunsetTime}`;
+    if (condition) {
+      document.body.classList.add(condition.class);
+      weatherDescriptionAndTemperatureElement.textContent = `${condition.message} | ${temperature}°`;
+    } else {
+      descriptionElement.textContent = `${weather.description} | ${temperature}°`;
+    }
   })
-
   .catch((error) => console.error("There was an error!", error));
+
+//Step 2 - Present some data on your web app
+
+/*
+    if (weather.id > 800 && weather.id < 900) {
+      document.body.classList.add("cloudy");
+      weatherDescriptionAndTemperatureElement.textContent = `Cloudy | ${temperature.toFixed(
+        0
+      )}°`;
+    } else if (weather.id === 800) {
+      document.body.classList.add("sunny");
+      weatherDescriptionAndTemperatureElement.textContent = `Sunny | ${temperature.toFixed(
+        0
+      )}°`;
+    } else if (weather.id >= 500 && weather.id < 600) {
+      document.body.classList.add("rainy");
+      weatherDescriptionAndTemperatureElement.textContent = `Rainy | ${temperature.toFixed(
+        0
+      )}°`;
+    } else {
+      descriptionElement.textContent = `${
+        weather.description
+      } | ${temperature.toFixed(0)}°`;
+    }
+    */
+
+//Sunrise and Sunset
+const sunriseTime = new Date(data.sys.sunrise * 1000)
+  .toLocaleTimeString([], {
+    hour: "2-digit",
+    minute: "2-digit",
+  })
+  .replace(":", ".");
+const sunsetTime = new Date(data.sys.sunset * 1000)
+  .toLocaleTimeString([], {
+    hour: "2-digit",
+    minute: "2-digit",
+  })
+  .replace(":", ".");
+
+document.getElementById("sunrise").textContent = `Sunrise ${sunriseTime}`;
+document.getElementById("sunset").textContent = `Sunset ${sunsetTime}`;
+
+/* 
+if (weather.id > 800 && weather.id < 900) {
+  document.body.classList.add("cloudy");
+  descriptionElement.textContent = `Light a fire, get cosy. ${cityName} is looking cloudy today.`;
+} else if (weather.id === 800) {
+  document.body.classList.add("sunny");
+} else {
+  descriptionElement.textContent = `The current weather is: ${weather.description}`;
+}
+*/
 
 //Feature - weather forecast
 fetch(
@@ -72,6 +130,7 @@ fetch(
       const forecastElement = document.createElement("li");
       // リストアイテムの内容を設定
       forecastElement.textContent = `Date: ${forecast.dt_txt}, Highest Temprature: ${forecast.main.temp_max}°C, Weather: ${forecast.weather[0].description}`;
+
       // li要素をHTMLのidがforecast-listの要素の中に追加
       //.appendChild = DOM（Document Object Model）に動的に要素を追加
       document.getElementById("forecast-list").appendChild(forecastElement);
