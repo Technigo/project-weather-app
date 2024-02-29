dcd9023890221feff4132a76c8e7d0bc5d85e659
Katarina Sjölin <kat.sjolin@gmail.com>
2024-02-29 12:08:34 +0100

    added 4 days forecast incl temperature and weather description

--- script.js
+++ script.js
@@ -1,9 +1,10 @@
-const BASE_URL = "https://api.openweathermap.org/data/2.5/weather"
+const BASE_URL = "https://api.openweathermap.org/data/2.5/"
 const API_KEY = "5170e7466e6e4805797bdd1908cdd84d"
 const city = "Copenhagen,Denmark"
 
-const URL = `${BASE_URL}?q=${city}&units=metric&APPID=${API_KEY}`
-
+const URL = `${BASE_URL}weather?q=${city}&units=metric&APPID=${API_KEY}`
+const URLforecast = `${BASE_URL}forecast?q=${city}&units=metric&APPID=${API_KEY}`
+ 
 //DOM Selectors
 const mainContainer = document.getElementById("mainContainer")
 const cityName = document.getElementById("cityName")
@@ -12,6 +13,7 @@ const weatherDescription = document.getElementById("weatherDescription")
 const errorDiv = document.getElementById("error")
 const sunrise = document.getElementById("sunrise")
 const sunset = document.getElementById("sunset")
+const forecast = document.getElementById("forecast")
 
 const updateHTML = (data) => {
   const temp = data.main.temp.toFixed(1)
@@ -30,6 +32,23 @@ const updateHTML = (data) => {
   sunset.innerText += `Sunset: ${sunsetShort}`
 }
 
+const updateHTMLforecast = (json) => {
+  const filteredForecast = json.list.filter((item) =>
+    item.dt_txt.includes("12:00")
+  )
+  const tomorrow = new Date()
+  tomorrow.setDate(tomorrow.getDate() + 1)
+  for (let counter = 0; counter < 4; counter++) {
+    const day = filteredForecast[counter]
+    const date = new Date(tomorrow)
+    date.setDate(date.getDate() + counter)
+    const weekdayName = date.toLocaleDateString("en-GB", { weekday: "short" })
+    const weekdayTemp = day.main.temp.toFixed(0)
+    const weekdayDescription = day.weather[0].description
+    forecast.innerText += `${weekdayName} ${weekdayDescription} ${weekdayTemp}°C`
+  }
+};
+
 const fetchWeather = () => {
   fetch(URL)
     .then(response => response.json())
@@ -41,3 +60,15 @@ const fetchWeather = () => {
 }
 
 fetchWeather()
+
+const fetchForecast = () => {
+  fetch(URLforecast)
+    .then(response => response.json())
+    .then(data => updateHTMLforecast(data))
+    .catch(error => {
+      console.log(error)
+      errorDiv.innerText = "Something went wrong"
+    })
+}
+
+fetchForecast()
