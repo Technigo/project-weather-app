9b2847e67ca1543999111d2f0661ca993bfb8920
Lovisa <lovisa.aaberg@gmail.com>
2024-02-29 06:54:33 +0100

    Sunrise & Sunset feature

--- script.js
+++ script.js
@@ -2,34 +2,24 @@ const weatherContainer = document.getElementById("weather-container")
 const cityContainer = document.getElementById("city-container")
 
 
-const fetchWeather = () => {
-  fetch('https://api.openweathermap.org/data/2.5/weather?q=Stockholm,Sweden&units=metric&APPID=18169ba1f8859ef1f0186e26f6fac435')
-  .then((response) => {
-    return response.json()
-  })
-  .then((json) => {
-    weatherContainer.innerHTML = `
-    ${json.weather[0].description} | ${Math.floor(json.main.temp)}º
-    `
-  })
-  // Used the Math.floor to make the temp without decimals
-}
-fetchWeather()
 
-const fetchCity = () => {
+
   fetch('https://api.openweathermap.org/data/2.5/weather?q=Stockholm,Sweden&units=metric&APPID=18169ba1f8859ef1f0186e26f6fac435')
   .then((response) => {
     return response.json()
   })
   .then((json) => {
+    const sunrise = new Date(json.sys.sunrise * 1000)
+  const timeForSunrise = sunrise.toLocaleTimeString([], { hour: "2-digit", minute: "2-digit" })
+
+const sunset = new Date(json.sys.sunset * 1000)
+const timeForSunset = sunset.toLocaleTimeString([], { hour: "2-digit", minute: "2-digit" })
+
+    weatherContainer.innerHTML = `
+    ${json.weather[0].description} | ${Math.floor(json.main.temp)}º 
+    <p>Sunrise ${timeForSunrise}</p><p>Sunset ${timeForSunset}</p>
+    ` 
     cityContainer.innerHTML = `
     ${json.name}
-    `
+    ` 
   })
-}
-fetchCity()
-
-
-//clear | 23grader
-//sunrise 08.00
-//sunset 22.30
