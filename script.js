2f016431f9dbbfaa5fe68930505bbeb38a04b4dc
Nathalie <nathae28@hotmail.com>
2024-02-29 23:08:44 +0100

    Forecast with icons

--- script.js
+++ script.js
@@ -1,52 +1,24 @@
 // DOM selectors
 const currentWeather = document.getElementById("weather-container");
-const weatherForm = document.querySelector(".weatherForm");
-const cityInput = document.querySelector(".cityInput");
-const displayTemp = document.getElementById("temp");
-const displayLocation = document.getElementById("location");
-const displayCondition = document.getElementById("condition");
-const displaySunrise = document.getElementById("sunrise-time");
-const displaySunset = document.getElementById("sunset-time");
+// const weatherForm = document.querySelector(".weatherForm");
+// const displayTemp = document.getElementById("temp");
+// const displayLocation = document.getElementById("location");
+// const displayCondition = document.getElementById("condition");
+// const displaySunrise = document.getElementById("sunrise-time");
+// const displaySunset = document.getElementById("sunset-time");
 const displayForecast = document.getElementById("weather-forecast");
-
-//BUTTON
-weatherForm.addEventListener("submit", async (event) => {
-  event.preventDefault();
-  const city = cityInput.value;
-  if (city) {
-    try {
-      const weatherDataButton = await printWeather(city);
-      printWeather(weatherDataButton);
-    } catch (error) {
-      console.error(error);
-    }
-  } else {
-    displayError("Please enter a city");
-  }
-});
-
-const getWeatherData = async (city) => {};
-
-const displayError = () => {
-  const errorDisplay = document.createElement("p");
-  errorDisplay.textContent = message;
-
-  card.textContent = "";
-  card.style.display = "flex";
-  card.appendChild(errorDisplay);
-};
 
 // API
 const API_BASE_URL = "https://api.openweathermap.org/data/2.5";
 const API_KEY = "a1f68c4f65b632802ca0dd3405694457";
-// let place = "bern";
+let place = "bern";
 const URL = `${API_BASE_URL}/weather?q=${place}&units=metric&appid=${API_KEY}`;
 const FORECAST = `${API_BASE_URL}/forecast?q=${place}&units=metric&appid=${API_KEY}`;
 
 //UNIX TIMESTAMP CONVERTER
 const convertDateTime = (unixTimestamp) => {
   const date = new Date(unixTimestamp * 1000);
-  const hours = String(date.getHours()).padStart(2, "0");
+  const hours = String(date.getHours()).padStart(2, "0"); // 2 is length of digits and "0" to lead with 0
   const minutes = String(date.getMinutes()).padStart(2, "0");
   return `${hours}:${minutes}`;
 };
@@ -62,17 +34,22 @@ const printWeather = async () => {
   try {
     const weatherNow = await fetch(URL);
     const data = await weatherNow.json();
-    const currentTemp = await data.main.temp;
-    const currentLocation = await data.name;
-    const currentCondition = await data.weather[0].description;
+    const currentTemp = data.main.temp.toFixed(1);
+    const currentLocation = data.name;
+    const currentCondition = data.weather[0].description;
+    const icon = data.weather[0].icon;
     const todaySunrise = convertDateTime(data.sys.sunrise);
     const todaySunset = convertDateTime(data.sys.sunset);
+    const ICON_URL = `https://openweathermap.org/img/wn/${icon}@2x.png`;
 
-    displayTemp.innerHTML = currentTemp + "&deg;C";
-    displayLocation.innerHTML = currentLocation;
-    displayCondition.innerHTML = currentCondition;
-    displaySunrise.innerHTML = todaySunrise;
-    displaySunset.innerHTML = todaySunset;
+    currentWeather.innerHTML = `
+    <section id="current-weather">
+    <p id="temp">${currentTemp}&deg;C</p>
+    <h1 id="location">${currentLocation}</h1>
+    <p id="condition">${currentCondition} <img class="icon" src="${ICON_URL}"></p>
+    <p id="sunrise">Sunrise ${todaySunrise}</p>
+    <p id="sunset">Sunset ${todaySunset}</p>
+    </section>`;
 
     console.log("Data:", data); //NOT FORGET TO DELETE!!
   } catch (error) {
@@ -87,18 +64,28 @@ const printForecast = async () => {
     const fourDayWeather = await fetch(FORECAST);
     const json = await fourDayWeather.json();
     const forecastData = json.list.filter((item, index) => index % 8 === 0); // Filter for every 8th item, which represents once every day
+
     console.log("FORECAST JSON DATA", json);
+
+    // LOWEST & HIGHEST TEMPERATURES
+    const temperatures = forecastData.map((forecast) =>
+      forecast.main.temp.toFixed(0),
+    );
+    const lowestTemp = Math.min(...temperatures); //to get lowest temperature
+    const highestTemp = Math.max(...temperatures); //to get highest temperature
 
     forecastData.forEach((forecast, index) => {
       const forecastDate = new Date(forecast.dt * 1000);
       const weekday = getWeekdays(forecastDate);
-      const forecastTime = convertDateTime(forecast.dt);
-      const forecastTemp = forecast.main.temp;
+      const icon = forecast.weather[0].icon;
 
       const forecastElement = document.createElement("div");
       forecastElement.innerHTML = `
-      <p>${weekday}, ${forecastDate.toLocaleDateString()}: ${forecastTime}</p>
-      <p>Temperature: ${forecastTemp}&deg;C</p>`;
+      <div class="forecast-display">
+      <p>${weekday}</p>
+      <img class="icon" src="https://openweathermap.org/img/wn/${icon}@2x.png">
+      <p>${lowestTemp}&deg; / ${highestTemp}&deg;C</p>
+      </div>`;
       displayForecast.appendChild(forecastElement);
     });
   } catch (error) {
