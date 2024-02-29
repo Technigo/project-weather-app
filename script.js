0f6f40e7c101ca605642b9abb806060c9fc26ab4
Martin Jönsson <martin.t.joensson@gmail.com>
2024-02-29 20:43:19 +0100

    Got forecast to display.

--- script.js
+++ script.js
@@ -18,7 +18,8 @@ const temp = document.getElementById("temp");
 const description = document.getElementById("description");
 const sunrise = document.getElementById("sunrise");
 const sunset = document.getElementById("sunset");
-const animationButton = document.getElementById("animation-button")
+const animationButton = document.getElementById("animation-button");
+const forecast = document.getElementById("forecast");
 
 const fetchWeather = () => {
   fetch(URL)
@@ -28,23 +29,38 @@ const fetchWeather = () => {
       city.innerHTML = `<h2>${data.name}</h2>`;
       temp.innerHTML = `<h1>${Math.round(data.main.temp)}°&#x1D9C;</h1>`;
       description.innerHTML = `<h3>${data.weather[0].main}</h3>`;
-      let sunriseHoursMinutes = prettyDate2(data.sys.sunrise*1000);
-      let sunsetHoursMinutes = prettyDate2(data.sys.sunset*1000);
+      let sunriseHoursMinutes = prettyDate2(data.sys.sunrise * 1000);
+      let sunsetHoursMinutes = prettyDate2(data.sys.sunset * 1000);
 
       sunrise.innerHTML = `<h3>${sunriseHoursMinutes}</h3>`;
-      sunset.innerHTML = `<h3>${sunsetHoursMinutes}</h3>`
+      sunset.innerHTML = `<h3>${sunsetHoursMinutes}</h3>`;
     })
     .catch((error) => console.log("Caught error:", error));
 };
 fetchWeather();
+
+const checkIfNoon = (string) => {
+  return string.includes("12:00:00");
+};
 
 const fetchForecast = () => {
   fetch(FORECAST_URL)
     .then((response) => response.json())
     .then((data) => {
       console.log(data);
-      // For loop to display 4 days on weather.
-  
+      // For loop to display 4 days on weather. data.list[0].dt_text
+      console.log(data.list[0].dt_txt)
+
+      const array = data.list.filter((array) => array.dt_txt.includes("12:00:00"));
+      console.log(array);
+    
+      array.forEach((element) => {
+        forecastList.innerHTML += `<li>${element.dt_txt} IMG ${element.main.temp}</li>`;
+      });
+      const forecastArray = data.list;
+      const mappedArray = forecastArray.map(checkIfNoon);
+      console.log(mappedArray);
+
     })
     .catch((error) => console.log("Caught error:", error));
 };
@@ -56,24 +72,18 @@ const prettyDate2 = (time) => {
   return date.toLocaleTimeString(navigator.language, {
     hour: "2-digit",
     minute: "2-digit",
-    hour12: false
+    hour12: false,
   });
-}
+};
 
 //Choose Image based on weather description.
-const chooseImage = (weather) => {
-
-}
+const chooseImage = (weather) => {};
 
 // Check to see if current time is after sunset and before sunrise. Display moon.
-const checkMoon = (sunrise, sunset) => {
-
-}
+const checkMoon = (sunrise, sunset) => {};
 
 // Toggle forecast
-const toggleForecast = () => {
-  
-}
+const toggleForecast = () => {};
 
 // Eventlisteners
-animationButton.addEventListener("click", toggelForecast())
+//animationButton.addEventListener("click", toggelForecast())
