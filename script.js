a6c6f7d8f864ffef15b162d61a69c51aefbc62a9
Your Name <you@example.com>
2024-02-29 15:05:46 +0100

    ready to create branches

--- script.js
+++ script.js
@@ -13,16 +13,22 @@ const fenchWeaterData = () =>{
     console.log(data.main)
     //set main temperature
     const selectNowsTemperature = document.getElementById("nowsTemperature")
-    selectNowsTemperature.innerHTML = data.main.temp
+    const roundOneDecimal = Math.round(data.main.temp * 10)/10 //this to round to one decimal
+    selectNowsTemperature.innerHTML = roundOneDecimal
+
     //set location
     const selectLocation = document.getElementById("location")
     selectLocation.innerHTML = data.name
+
     //set description
     const selectWeatherDescription = document.getElementById("weatherDescription")
     selectWeatherDescription.innerHTML = data.weather[0].description
+    
     //set sunrise
     const selectSunraiseSunset = document.getElementById("sunraiseSunset")
-    selectSunraiseSunset.innerHTML = data.sys.sunrise    
+    const sunriseTimestamp = data.sys.sunrise * 1000
+    const sunriseDate = new Date (sunriseTimestamp)
+    selectSunraiseSunset.innerHTML = sunriseDate.toLocaleTimeString()    
   })
   .catch ((error) =>{
     console.error(`Error fetching weather data:`, error)
