40129d4a970db11a5bf4bfa3b49bc9c0ad08cf0e
Frida Svensson <fridamarisvensson@gmail.com>
2024-02-29 22:18:12 +0100

    create fetch weather function

--- design/design2/script.js
+++ design/design2/script.js
@@ -1,13 +1,25 @@
-const MY_API_KEY = "31320abec19306a046f96f4c46f01157"
+const MY_API_KEY = "31320abec19306a046f96f4c46f01157";
+const BASE_URL = "https://api.openweathermap.org/data/2.5";
 
-// URL for the Call current weather data
-const BASE_URL_CURRENT_WEATHER = "https://api.openweathermap.org/data/2.5/weather?q=Stockholm,Sweden&units=metric&APPID=YOUR_API_KEY"
+const coordinates = {
+  lat: 57.721595,
+  lon: 12.0253,
+};
 
-const URL_CURRENT_WEATHER = BAS
+const updateHTML = (data) => {
+  console.log(data);
+};
 
-
+const URL = `${BASE_URL}/weather?lat=${coordinates.lat}&lon=${coordinates.lon}&appid=${MY_API_KEY}`;
 
 const fetchWeather = () => {
-    fetch(URL)
+  fetch(URL)
+    .then((response) => response.json())
+    .then((data) => updateHTML(data))
+    .catch((error) => {
+      console.log(error);
+      errorDiv.innerText = "Something went wrong";
+    });
+};
 
-}
+fetchWeather();
