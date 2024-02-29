b881f5871311fc585dc2c548a196a452037dcc28
SofieFerrari <sofiestrahl@gmail.com>
2024-02-29 15:36:48 +0100

    fixed timezone bug and changed function name

--- code/sunrise&set.js
+++ code/sunrise&set.js
@@ -5,7 +5,7 @@ const sunset = document.getElementById('sunset')
 
 
 //Fetching the API
-const fetchWeather = () => {
+const fetchSun = () => {
 	fetch(
 		'https://api.openweathermap.org/data/2.5/weather?q=Umea,Sweden&units=metric&APPID=1e48fdf267ccc8ee33c1c78150dcbab1'
 	)
@@ -33,38 +33,4 @@ const fetchWeather = () => {
 			 
 		})
 }
-fetchWeather()
-
-
-
-const secondsSunrise = 1709185720
-const secondsSunset = 1709222075
-
-
-const formatTimeSunrise = () => {
-	const minutes = Math.floor((secondsSunrise / 60) % 60)
-	const hours = Math.floor((secondsSunrise / 60 / 60) % 24)
-
-	return [
-		hours.toString().padStart(2, '0'),
-		minutes.toString().padStart(2, '0'),
-	].join(':')
-}
-const formattedTimeSunrise = formatTimeSunrise(secondsSunrise)
-console.log("time sunrise", formattedTimeSunrise)
-
-const formatTimeSunset = () => {
-	const minutes = Math.floor((secondsSunset / 60) % 60)
-	const hours = Math.floor((secondsSunset / 60 / 60) % 24)
-
-	return [
-		hours.toString().padStart(2, '0'),
-		minutes.toString().padStart(2, '0'),
-	].join(':')
-
-}
-const formattedTimeSunset = formatTimeSunset(secondsSunset)
-console.log("time sunset", formattedTimeSunset)
-
-
-
+fetchSun()
