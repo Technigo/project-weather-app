2284c5ddec81e0783830302553e18febd4ca9702
Martin Jönsson <martin.t.joensson@gmail.com>
2024-02-29 19:08:11 +0100

    Started work on design. Put Forecast on hold for now.

--- script.js
+++ script.js
@@ -25,9 +25,13 @@ const fetchWeather = () => {
     .then((data) => {
       console.log(data);
       city.innerHTML = `<h2>${data.name}</h2>`;
-      temp.innerHTML = `<h2>${data.main.temp}</h2>`;
-      description.innerHTML = `<h2>${data.weather[0].main}</h2>`;
-      city.innerHTML = `<h2>${data.name}</h2>`;
+      temp.innerHTML = `<h1>${Math.round(data.main.temp)}°&#x1D9C;</h1>`;
+      description.innerHTML = `<h3>${data.weather[0].main}</h3>`;
+      let sunriseHoursMinutes = prettyDate2(data.sys.sunrise*1000);
+      let sunsetHoursMinutes = prettyDate2(data.sys.sunset*1000);
+
+      sunrise.innerHTML = `<h3>${sunriseHoursMinutes}</h3>`;
+      sunset.innerHTML = `<h3>${sunsetHoursMinutes}</h3>`
     })
     .catch((error) => console.log("Caught error:", error));
 };
@@ -46,3 +50,18 @@ const fetchForecast = () => {
     .catch((error) => console.log("Caught error:", error));
 };
 fetchForecast();
+
+
+const prettyDate2 = (time) => {
+  let date = new Date(parseInt(time));
+  return date.toLocaleTimeString(navigator.language, {
+    hour: "2-digit",
+    minute: "2-digit",
+    hour12: false
+  });
+}
+
+//Choose Image based on weather description.
+const chooseImage = (weather) => {
+
+}
