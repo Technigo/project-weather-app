74d095899096ab03e87498b65e1e834c4f7d03b2
Tejpex <equis_angel@hotmail.com>
2024-02-29 13:33:11 +0100

    Added switch to change main text depending on weather

--- script.js
+++ script.js
@@ -1,3 +1,4 @@
+const body = document.getElementById("body")
 const header = document.getElementById("header")
 const main = document.getElementById("main")
 const container = document.getElementById("container")
@@ -28,12 +29,38 @@ const fetchData = () => {
 }
 
 const printWeatherInfo = (json) => {
-    city = json.name;
-    weatherType = json.weather[0].description;
-    temperatureNow = Math.round(json.main.temp);
-    main.innerHTML = `
-    There is ${weatherType} and ${temperatureNow} degrees in ${city} today.
-    `;
+    city = json.name
+    weatherType = json.weather[0].main
+    temperatureNow = Math.round(json.main.temp)
+    switch (weatherType) {
+        case "Clouds":
+            body.setAttribute("class", "red-cloudy")
+            main.innerHTML = `
+            <img src="./design/design2/icons/noun_Cloud_1188486.svg" alt="Icon showing a cloud">
+            <h1>Light a fire and get cosy. ${city} is looking grey today.</h1>
+            `
+            break
+        case "Clear":
+            body.setAttribute("class", "green-yellow-sunny")
+            main.innerHTML = `
+            <img src="./design/design2/icons/noun_Sunglasses_2055147.svg" alt="Icon showing a pair of sunglasses">
+            <h1>Get your sunnies on. ${city} is looking rather great today.</h1>
+            `
+            break
+        case "Rain":
+            body.setAttribute("class", "blue-rainy");
+            main.innerHTML = `
+            <img src="./design/design2/icons/noun_Umbrella_2030530.svg" alt="Icon showing an umbrella">
+            <h1>Don't forget your umbrella. It's wet in ${city} today.</h1>
+            `
+            break
+        //Other weather types: Thunderstorm, Snow, Drizzle, Atmosphere
+        default:
+            main.innerHTML = `
+            <h1>There is definately some kind of weather in ${city} today. Look out the window and you might find out what it is.</h1>
+            `
+    }
 }
+
 
 fetchData ()
