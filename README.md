a0c441819faa505442a9064093580533effb53cd
Vittoria Matteoli <info@vittoriamatteoli.com>
2024-02-29 19:22:31 +0100

    added geolocation feature

--- README.md
+++ README.md
@@ -1,10 +1,10 @@
 # Weather App
 
-I've created a meteo app ......
+I've created a meteo app which display actual weather conditions and forecast for the following 4 days. My app use geolocation and different styling based on weather.
 
 ## The problem
 
-Describe how you approached to problem, and what tools and techniques you used to solve it. How did you plan? What technologies did you use? If you had more time, what would be next?
+Was hard to filter all the arrays and loop over them, I've mostly found solution on the web. If I had more time I would add more weather informations and handle errors in different way.
 
 ## View it live
 
--- script.js
+++ script.js
@@ -10,13 +10,16 @@ let currentDay
 let currentTime
 let API_URL = `https://api.openweathermap.org`
 let API_KEY = `1c745605f5cf52ece2c729289e47acc7`
-let nameCity = 'zurich' //default
-let country = 'CH' //default
+let lat = 47.3744489 //default
+let lon = 8.5410422 //default
+
+// need to create another HTML with the same method below ,maybe just a nice background with loading symble
 document.addEventListener('DOMContentLoaded', () => {
-  alert('APP ready!')
-  getWeather(nameCity, country)
+  body.style.backgroundColor = 'black'
+  getLocation()
 })
 
+// added in HTML select and search bar
 navContainer.innerHTML = `  <label for="favoriteCities">Choose a city :</label>
 <select id="favoriteCities">
 <option  disabled selected value="city">City</option>
@@ -28,9 +31,10 @@ navContainer.innerHTML = `  <label for="favoriteCities">Choose a city :</label>
 <input type="text" id="searchCity" placeholder="Enter city name">
 <button>Search</button>`
 
-const getWeather = (nameCity, country) => {
+// function to get the daily weather
+const getWeather = (lat, lon) => {
   fetch(
-    `${API_URL}/data/2.5/weather?q=${nameCity},${country}&units=metric&appid=${API_KEY}`
+    `${API_URL}/data/2.5/weather?lat=${lat}&lon=${lon}&units=metric&appid=${API_KEY}`
   )
     .then((response) => response.json())
     .then((data) => {
@@ -46,6 +50,7 @@ const getWeather = (nameCity, country) => {
 
       const hours = sun.getHours()
       const minutes = sun.getMinutes()
+
       // concatenate the hours and minutes for sunrire and sunset, transformed them into strings and padded with 2 decimals number , covered empty spaces with 0
       const sunrise = `${hours.toString().padStart(2, '0')}: ${minutes
         .toString()
@@ -57,8 +62,7 @@ const getWeather = (nameCity, country) => {
         .toString()
         .padStart(2, '0')} PM`
 
-      // div=actualWeather
-
+      // populate div=actualWeather
       actualWeather.innerHTML = `
       <h1>${data.name}</h1>
 
@@ -69,6 +73,7 @@ const getWeather = (nameCity, country) => {
         return (weathergroup = condition.id)
       })
 
+      // styled based on weather condition
       weatherConditions.forEach((condition) => {
         switch (true) {
           // HTML for thunderstorms
@@ -77,36 +82,42 @@ const getWeather = (nameCity, country) => {
             document.body.style.color = '#FFD43B'
             weatherDescription.innerHTML = `<i class="fa-solid fa-bolt fa-xl" style="color: #FFD43B;"></i><h3> Hold onto your hats.<br> ${data.name} is facing some stormy weather today.</h3> `
             break
+
           // HTML for drizzle
           case condition >= 300 && condition <= 321:
             document.body.style.backgroundColor = '#e6638b'
             document.body.style.color = '#63E6BE'
             weatherDescription.innerHTML = `<i class="fa-solid fa-cloud-rain fa-xl" style="color: #63E6BE;"></i><h3> Don't forget your raincoat.<br> ${data.name} is graced with a light drizzle today.</h3> `
             break
+
           // HTML for rainy days
           case condition >= 500 && condition <= 531:
             document.body.style.backgroundColor = '#BDE8FA'
             document.body.style.color = '#164A68'
             weatherDescription.innerHTML = `<img src="./assets/noun_Umbrella_2030530.svg"><h3> Don't forget your umbrella. <br>It's wet in ${data.name} today.</h3> `
             break
+
           // HTML for snowy days
           case condition >= 600 && condition <= 622:
             document.body.style.backgroundColor = '#015C92'
             document.body.style.color = '#fcfcfc'
             weatherDescription.innerHTML = `<i class="fa-regular fa-snowflake fa-xl" style="color: #fcfcfc;"></i><h3> Put on your mittens. <br>${data.name} is looking quite enchanting today with its snowfall. </h3> `
             break
+
           // HTML for foggy/mist days
           case condition >= 701 && condition <= 781:
             document.body.style.backgroundColor = '#fbfafe'
             document.body.style.color = '#B197FC'
             weatherDescription.innerHTML = `<i class="fa-solid fa-smog fa-xl" style="color: #B197FC;"></i><h3> Navigate through the mist.<br> ${data.name} is draped in a mysterious haze today. </h3> `
             break
+
           // HTML for sunny days
           case condition === 800:
             document.body.style.backgroundColor = '#F7E9B9'
             document.body.style.color = '#2A5510'
             weatherDescription.innerHTML = `<img src="./assets/noun_Sunglasses_2055147.svg"><h3> Get your sunnies on.<br> ${data.name} is looking rather great today. </h3> `
             break
+
           // HTML for cloudy days
           case condition >= 801 && condition <= 804:
             document.body.style.backgroundColor = 'white'
@@ -123,24 +134,23 @@ const getWeather = (nameCity, country) => {
     })
     .catch((error) => console.error(error))
 }
-// getWeatherZurich()
 
-const getForecast = (nameCity, country) => {
+// function to get forecast
+const getForecast = (lat, lon) => {
   fetch(
-    `${API_URL}/data/2.5/forecast?q=${nameCity},${country}&units=metric&APPID=${API_KEY}`
+    `${API_URL}/data/2.5/forecast?lat=${lat}&lon=${lon}&units=metric&APPID=${API_KEY}`
   )
     .then((response) => response.json())
     .then((data) => {
       const weekDayFromTwelve = data.list.filter((item) => {
         const dateTime = item.dt_txt
         const fullDate = new Date(dateTime)
-
         const currentTime = fullDate.getUTCHours()
-        const currentDate = fullDate.getDay()
 
         return currentTime === 11 // Filter for 11:00:00
       })
 
+      // convert the number of the date with name of the week
       const dates = weekDayFromTwelve.map((item) => {
         const dateTime = item.dt_txt
         const fullDate = new Date(dateTime)
@@ -176,6 +186,7 @@ const getForecast = (nameCity, country) => {
         return currentDay
       })
 
+      // rounded actual temperature
       actualTemp = weekDayFromTwelve.map((condition) => {
         let valueTemp = Math.round(condition.main.temp)
         return valueTemp
@@ -183,6 +194,7 @@ const getForecast = (nameCity, country) => {
 
       // Clear existing content
       forecastList.innerHTML = ''
+
       // Loop through dates and actualTemp arrays simultaneously
       for (let i = 0; i < Math.min(dates.length, actualTemp.length); i++) {
         forecastList.innerHTML += `<div class=forecast-container><li>${dates[i]} </li><span> ${actualTemp[i]} °C </span></div>`
@@ -192,11 +204,13 @@ const getForecast = (nameCity, country) => {
     .catch((error) => console.error(error))
 }
 
-getForecast(nameCity, country)
-getWeather(nameCity, country)
-//handle select city
+getForecast(lat, lon)
+getWeather(lat, lon)
+
+//handle selected city
 const handleCity = () => {
   const citySelect = document.getElementById('favoriteCities')
+
   citySelect.addEventListener('change', (event) => {
     const city = event.target.value
     getWeatherCities(city)
@@ -227,15 +241,25 @@ const getWeatherCities = (city) => {
   fetch(`${API_URL}/geo/1.0/direct?q=${city}&limit=1&appid=${API_KEY}`)
     .then((response) => response.json())
     .then((data) => {
-      console.log('Weather data for selected city:', data)
-      country = data[0].country
-      nameCity = data[0].name
-      console.log(country)
-      console.log(nameCity)
-      getForecast(nameCity, country)
-      getWeather(nameCity, country)
+      lat = data[0].lat
+      lon = data[0].lon
 
-      // Handle weather data here
+      getForecast(lat, lon)
+      getWeather(lat, lon)
     })
     .catch((error) => console.error(error))
+}
+
+// function to geolocate
+const getLocation = () => {
+  if (navigator.geolocation) {
+    navigator.geolocation.getCurrentPosition(showPosition)
+  } else {
+    body.innerHTML = 'Geolocation is not supported by this browser.'
+  }
+}
+
+const showPosition = (position) => {
+  lat = position.coords.latitude
+  lon = position.coords.longitude
 }
