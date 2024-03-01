cf7b28ab93865e61fa1057ea833459bdd89125a5
Johanna Billingskog Nyberg <johanna@billingskognyberg.se>
2024-03-01 10:44:17 +0100

    Added error message function if API doesn't load as expected.

--- script.js
+++ script.js
@@ -8,29 +8,26 @@ const forecastDay = document.getElementById("forecast-day");
 const forecastWeather = document.getElementById("forecast-weather");
 const forecastTemperature = document.getElementById("forecast-temperature");
 const forecastContainer = document.getElementById("forecast-container");
-const divider = document.getElementById("divider");
-const forecastRows = document.querySelectorAll("forecast-row");
+const errorContainer = document.getElementById("error-container");
 
 //Weather Today API
 //https://api.openweathermap.org/data/2.5/weather?q=Gagnef,Sweden&units=metric&APPID=YOUR_API_KEY
-//https://api.openweathermap.org/data/2.5/weather/?q=Gagnef,Sweden&units=metric&APPID=bf5fcdbe6629518d85ff1555a95c673f
 
 const BASE_WEATHER_URL = "https://api.openweathermap.org/data/2.5/weather";
 const API_KEY = "bf5fcdbe6629518d85ff1555a95c673f";
+// const API_KEY = "bf5fcdbe6629518d85ff1555a95c673"; // Use this to show error message
 const city = "Gagnef, Sweden";
 
+//Creating the correct API URL for todays weather data
 const weatherTodayURL = `${BASE_WEATHER_URL}?q=${city}&units=metric&APPID=${API_KEY}`;
 
-// const fetchWeatherTodayAPI = () => {
-//   fetch(weatherTodayURL)
-//     .then((response) => {
-//       return response.json();
-//     })
-//     .then((weatherTodayData) => {
-//       console.log(weatherTodayData);
-//     });
-// };
+// Error handling function
+const displayErrorMessage = (errorMessage) => {
+  // Display an error message in case of API not loading correctly
+  errorContainer.innerHTML = `<p>${errorMessage}</p>`;
+};
 
+//Fetch wather data from API
 const fetchWeatherTodayAPI = () =>
   fetch(weatherTodayURL)
     .then((response) => {
@@ -39,31 +36,15 @@ const fetchWeatherTodayAPI = () =>
     .then((weatherTodayData) => {
       // Update DOM with city name
       locationName.innerHTML = `<h4>${weatherTodayData.name}</h4>`;
-      console.log(weatherTodayData.name);
 
-      // Update DOM with today's temperature
+      //Modify temperature data to only show one decimal
       const temperatureWithOneDecimal = weatherTodayData.main.temp.toFixed(1);
+      // Update DOM with today's temperature
       todaysTemperature.innerHTML = `<h1>${temperatureWithOneDecimal}°</h1>`;
-      console.log(temperatureWithOneDecimal);
 
-      //   const dayOfWeekLowerCase = forecast.dayOfWeek.toLowerCase(); // Convert day of week to lowercase
-
-      //   // Update DOM with today's temperature
-      //   todaysTemperature.innerHTML = `<h1>| ${weatherTodayData.main.temp}°</h1>`;
-      //   console.log(weatherTodayData.main.temp);
-
-      // Update DOM with today's weather
       weatherTodayData.weather.forEach((weather) => {
-        console.log(weather);
-        console.log(weather.main);
+        // Update DOM with today's weather
         todaysWeather.innerHTML = `<h1>${weather.main}</h1>`;
-
-        //   // Update DOM with today's sunrise
-        //   todaysSunrise.innerHTML = `<h1>Sunrise ${weatherTodayData.sys.sunrise}</h1>`;
-        //   console.log(weatherTodayData.sys.sunrise);
-        //   // Update DOM with today's sunset
-        //   todaysSunset.innerHTML = `<h1>Sunset ${weatherTodayData.sys.sunset}</h1>`;
-        //   console.log(weatherTodayData.sys.sunset);
 
         // Convert Unix timestamps to milliseconds
         const sunriseTime = new Date(weatherTodayData.sys.sunrise * 1000);
@@ -85,43 +66,40 @@ const fetchWeatherTodayAPI = () =>
 
         // Update DOM with today's sunrise and sunset in lowercase and 24-hour format
         todaysSunrise.innerHTML = `<h1>${sunriseLowerCase} ${sunriseFormatted}</h1>`;
-        console.log(sunriseFormatted);
 
         todaysSunset.innerHTML = `<h1>${sunsetLowerCase} ${sunsetFormatted}</h1>`;
-        console.log(sunsetFormatted);
-
-        console.log(weatherTodayData);
       });
+    })
+    .catch((error) => {
+      // Display an error message in case of error
+      displayErrorMessage("Failed to fetch weather data.");
     });
 
 fetchWeatherTodayAPI();
 
 //Forecast API
-//https://api.openweathermap.org/data/2.5/forecast/?q=Gagnef,Sweden&units=metric&APPID=bf5fcdbe6629518d85ff1555a95c673f
+//https://api.openweathermap.org/data/2.5/forecast/?q=Gagnef,Sweden&units=metric&APPID=YOUR_API_KEY
 
 const BASE_FORECAST_URL = "https://api.openweathermap.org/data/2.5/forecast/";
 
+//Creating the correct API URL for weather forecast
 const weatherForecastURL = `${BASE_FORECAST_URL}?q=${city}&units=metric&APPID=${API_KEY}`;
 
+//Fetch forecast data from API
 const fetchWeatherForecastAPI = () =>
   fetch(weatherForecastURL)
     .then((response) => {
       return response.json();
     })
     .then((weatherForecastData) => {
-      console.log(weatherForecastData);
-      console.log(weatherForecastData.list);
-
       // Extracting dates and weather icons from the forecast data for 12:00:00 entries
-      //   const forecastDay = document.getElementById("forecast-day");
-
       const filteredWeatherData = weatherForecastData.list
-        .filter((item) => item.dt_txt.includes("12:00:00"))
+        .filter((item) => item.dt_txt.includes("12:00:00")) //Filter on 12:00:00 entries only
         .map((item) => {
           const timestamp = item.dt * 1000; // Convert seconds to milliseconds
           const date = new Date(timestamp);
           const dayOfWeek = date.toLocaleString("en-US", { weekday: "long" }); // Get day of the week
-          const weatherIconCode = item.weather[0].icon; // Get the icon code from the first weather entry
+          const weatherIconCode = item.weather[0].icon; // Get the icon code
           const iconUrl = `http://openweathermap.org/img/wn/${weatherIconCode}.png`; // Construct the icon URL
           const temperature = item.main.temp.toFixed(0); // Get temperature from main object
           return {
@@ -131,27 +109,8 @@ const fetchWeatherForecastAPI = () =>
           };
         });
 
-      console.log("Filtered Weather Data (12:00:00):", filteredWeatherData);
-
-      // Display the forecast data on the webpage -- THIS WORKS
-      //       forecastDay.innerHTML = filteredWeatherData
-      //         .map((forecast) => `<h3>${forecast.dayOfWeek}</h3>`)
-      //         .join("");
-      //       forecastWeather.innerHTML = filteredWeatherData
-      //         .map((forecast) => `<img src="${forecast.iconUrl}" alt="Weather Icon">`)
-      //         .join("");
-      //       forecastTemperature.innerHTML = filteredWeatherData
-      //         .map((forecast) => `<p>${forecast.temperature}°C</p>`)
-      //         .join("");
-      //     });
-
-      //   fetchWeatherForecastAPI()
-
-      //----------------------
-
-      // Display the forecast data on the webpage
-      //   forecastContainer.innerHTML = "";
-      filteredWeatherData.slice(0, 4).forEach((forecast) => {
+      // Display the forecast data in the application
+      filteredWeatherData.slice(1, 5).forEach((forecast) => {
         const dayOfWeekLowerCase = forecast.dayOfWeek.toLowerCase(); // Convert day of week to lowercase
         const forecastHTML = `
     <div class="forecast-row">
@@ -164,7 +123,8 @@ const fetchWeatherForecastAPI = () =>
       });
     })
     .catch((error) => {
-      console.error("Error fetching weather forecast:", error);
+      // Display an error message in case of error
+      displayErrorMessage("Failed to fetch weather data.");
     });
 
 fetchWeatherForecastAPI();
