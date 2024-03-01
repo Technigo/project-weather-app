df91ba641f3cac61dc66864c5190c17cf541798a
Your El1an3 <eliane_w@yahoo.com>
2024-03-01 11:47:22 +0100

    added more cities

--- code/script.js
+++ code/script.js
@@ -4,22 +4,22 @@ const weatherDescription = document.getElementById("weather-description");
 const fourdayForecast = document.getElementById("fourday-forecast");
 const citySelector = document.getElementById("city");
 
-//get weather data based on selected city
+//get weather data based for cities
 const getWeather = (city) => {
   fetch(
     `https://api.openweathermap.org/data/2.5/weather?q=${city}&units=metric&APPID=bac28b010cea73460ead078a7d8aa965`
   )
     .then((response) => response.json())
     .then((data) => {
-      const sunriseTime = convertTo24Hour(data.sys.sunrise);
-      const sunsetTime = convertTo24Hour(data.sys.sunset);
+      const sunriseTime = convertTo24Hour(data.sys.sunrise); //Unix timestamp (in seconds)
+      const sunsetTime = convertTo24Hour(data.sys.sunset); //Unix timestamp (in seconds)
 
       weatherToday.innerHTML = `<p>${
         data.weather[0].description
       } | ${data.main.temp.toFixed(
         1
       )}°C</p><p>sunrise ${sunriseTime}</p><p>sunset ${sunsetTime}</p>`;
-      weatherDescription.innerHTML = `<img src="icons/Cloud.svg"><h1>${data.name}</h1>`;
+      weatherDescription.innerHTML = `<img src="icons/Cloud.svg"><h1>${data.name}</h1>`; //work on icon
     });
 };
 
@@ -34,10 +34,10 @@ const getForecast = (city) => {
 
       data.list.forEach((forecast) => {
         const date = new Date(forecast.dt_txt);
-
+        //filtering for 12:00 forecast
         if (date.getHours() === 12) {
           const dayName = getDayName(date);
-
+          //If the day name doesn't already exist in the dailyForecasts object and the number of keys (representing the number of days) is less than 4, it stores the temperature for that day in the dailyForecasts object. This ensures that only forecasts for the next four days are stored.
           if (
             !dailyForecasts[dayName] &&
             Object.keys(dailyForecasts).length < 4
@@ -49,6 +49,7 @@ const getForecast = (city) => {
         }
       });
 
+      //iterates over dailyForecast and generates HTML to display forecast
       for (const dayName in dailyForecasts) {
         fourdayForecast.innerHTML += `<div class="forecast-element"><div><p>${dayName}</p></div><div><p>${dailyForecasts[
           dayName
@@ -73,18 +74,22 @@ citySelector.addEventListener("change", () => {
 
 // Function to convert time to 24-hour format
 const convertTo24Hour = (time) => {
+  //creates new Date object using the time parameter multiplied by 1000. Date expects the input to be in milliseconds, while Unix timestamps are in seconds.
   const date = new Date(time * 1000);
+
+  //extracting hours and minutes and converting to strings
+  //padStart adds leading zeros to ensure that hours/minutes always have two digits
   const hours = date.getHours().toString().padStart(2, "0");
   const minutes = date.getMinutes().toString().padStart(2, "0");
   return `${hours}:${minutes}`;
 };
 
-// Function to get abbreviated day name
+//function to get abbreviated day name
 const getDayName = (date) => {
   const days = ["sun", "mon", "tue", "wed", "thu", "fri", "sat"];
   return days[date.getDay()];
 };
 
-// Initial fetch for Zurich (or any default city)
+//initial fetch for Zurich (default)
 getWeather("Zurich");
 getForecast("Zurich");
