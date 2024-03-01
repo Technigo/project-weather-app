b1b002ec2588629e30491aa8d7cda9e649cdb283
Lovisa <lovisa.aaberg@gmail.com>
2024-03-01 21:40:07 +0100

    Changed the temp to one decimal

--- script.js
+++ script.js
@@ -1,35 +1,32 @@
-const weatherContainer = document.getElementById("weather-container")
-const cityContainer = document.getElementById("city-container")
-
+const weatherContainer = document.getElementById("weather-container");
+const cityContainer = document.getElementById("city-container");
 
 const fetchWeather = () => {
-  fetch('https://api.openweathermap.org/data/2.5/weather?q=Stockholm,Sweden&units=metric&APPID=18169ba1f8859ef1f0186e26f6fac435')
-  .then((response) => {
-    return response.json()
-  })
-  .then((json) => {
-    weatherContainer.innerHTML = `
-    ${json.weather[0].description} | ${Math.floor(json.main.temp)}º
-    `
-  })
-  // Used the Math.floor to make the temp without decimals
-}
-fetchWeather()
+  fetch(
+    "https://api.openweathermap.org/data/2.5/weather?q=Stockholm,Sweden&units=metric&APPID=18169ba1f8859ef1f0186e26f6fac435"
+  )
+    .then((response) => {
+      return response.json();
+    })
+    .then((json) => {
+      weatherContainer.innerHTML = `
+    ${json.weather[0].description} | ${json.main.temp.toFixed(1)}° 
+    `;
+    });
+};
+fetchWeather();
 
 const fetchCity = () => {
-  fetch('https://api.openweathermap.org/data/2.5/weather?q=Stockholm,Sweden&units=metric&APPID=18169ba1f8859ef1f0186e26f6fac435')
-  .then((response) => {
-    return response.json()
-  })
-  .then((json) => {
-    cityContainer.innerHTML = `
+  fetch(
+    "https://api.openweathermap.org/data/2.5/weather?q=Stockholm,Sweden&units=metric&APPID=18169ba1f8859ef1f0186e26f6fac435"
+  )
+    .then((response) => {
+      return response.json();
+    })
+    .then((json) => {
+      cityContainer.innerHTML = `
     ${json.name}
-    `
-  })
-}
-fetchCity()
-
-
-//clear | 23grader
-//sunrise 08.00
-//sunset 22.30
+    `;
+    });
+};
+fetchCity();
