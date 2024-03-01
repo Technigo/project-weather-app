d5f70c808564a3c857b414eaf2bb8fb4869735a7
SofieFerrari <sofiestrahl@gmail.com>
2024-03-01 22:05:40 +0100

    copied from sun-branch for code to work again

--- code/script.js
+++ code/script.js
@@ -1,17 +1,9 @@
-//API 
-const BASE_URL = "https://api.openweathermap.org/data/2.5/"
-const API_KEY = "1e48fdf267ccc8ee33c1c78150dcbab1"
-const city = "Umea, Sweden"
-
-const URL = `${BASE_URL}weather?q=${city}&units=metric&APPID=${API_KEY}`
 //API
 const BASE_URL = 'https://api.openweathermap.org/data/2.5/'
 const API_KEY = '1e48fdf267ccc8ee33c1c78150dcbab1'
-const city = 'Umea,Sweden'
+const city = 'Umea, Sweden'
 
 const URL = `${BASE_URL}weather?q=${city}&units=metric&APPID=${API_KEY}`
-
-//'https://api.openweathermap.org/data/2.5/weather?q=Umea,Sweden&units=metric&APPID=1e48fdf267ccc8ee33c1c78150dcbab1'
 
 //DOM selectors
 const weatherContainer = document.getElementById('weather-container')
@@ -24,31 +16,30 @@ const handleName = document.getElementById('city-name')
 const weatherDescription = document.getElementById('weather-description')
 
 //Fetching the API
+//Fetching the API
 const fetchWeather = () => {
-	fetch('https://api.openweathermap.org/data/2.5/weather?q=Umea,Sweden&units=metric&APPID=1e48fdf267ccc8ee33c1c78150dcbab1')
+	fetch(
+		'https://api.openweathermap.org/data/2.5/weather?q=Umea,Sweden&units=metric&APPID=1e48fdf267ccc8ee33c1c78150dcbab1'
+	)
 		.then((response) => response.json())
-		.then((data) => dataToHTML(data))
-		
-		.catch((error) => {
-			errorDiv.innerText = `😬`
+		.then((data) => {
+			console.log(data)
+		const nameChoice = data.name
+		const tempChoice = Math.trunc(data.main.temp)
+		const weatherDescr = data.weather[0].main
+
+		handleName.innerText = nameChoice
+		handleTemp.innerText = `${tempChoice} °C`
+		weatherDescription.innerText = `${weatherDescr}`
+		console.log('hej')
+			
 		})
 }
 fetchWeather()
-console.log(dataToHTML(data))
-
 
 //Function to get data to HTML
-const dataToHTML = (data) => {
-	const nameChoice = data.name
-	const tempChoice = Math.trunc(data.main.temp)
-	const weatherDescr = data.weather[0].main
 
-	handleName.innerText = nameChoice
-	handleTemp.innerText = `${tempChoice} °C`
-	weatherDescription.innerText = `${weatherDescr}`
-}
-dataToHTML()
-console.log('hej')
+
 
 //Function showing content on browser with .innerHTML
 const showWeather = () => {
