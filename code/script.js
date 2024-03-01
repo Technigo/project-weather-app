d94256342374bb744b7c99da3917f9974dc0b2c8
SofieFerrari <sofiestrahl@gmail.com>
2024-03-01 12:22:47 +0100

    added styling and a grid container for forecast

--- code/script.js
+++ code/script.js
@@ -1,10 +1,16 @@
 const weatherContainer = document.getElementById('weather-container')
 const imgContainer = document.getElementById('img-container')
 const cityContainer = document.getElementById('city-container')
+const forecastContainer = document.getElementById('forecast-container')
 
 const showWeather = () => {
-weatherContainer.innerHTML = `<p>hello world<p>`
-cityContainer.innerHTML = `<p>hello world<p>`
+weatherContainer.innerHTML = `
+<p>hello world<p>`
+cityContainer.innerHTML = `
+<p>hello world<p>`
+forecastContainer.innerHTML = `
+<p>mon</p>
+`
 console.log(imgContainer)
 
 }
