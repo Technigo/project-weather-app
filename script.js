f40f73f891209af33561ab2b3628eb79a624ca19
Tejpex <equis_angel@hotmail.com>
2024-03-01 10:41:58 +0100

    Fixed issue with main text over-lapping header

--- script.js
+++ script.js
@@ -1,14 +1,15 @@
+//DOM selectors
 const body = document.getElementById("body")
 const header = document.getElementById("header")
 const main = document.getElementById("main")
 const container = document.getElementById("container")
 
+//URLs
 const URL_BASE = "https://api.openweathermap.org/data/2.5/weather?q=Stockholm,Sweden&units=metric&APPID="
 const APP_KEY = "8be7a87323d320c7bae11d84fa0a7c61"
 const URL = URL_BASE+APP_KEY
 
-
-
+//Variables
 let city = ""
 let weatherType = ""
 let temperatureNow = ""
@@ -19,6 +20,7 @@ let dayForecast = ""
 let timeForecast = ""
 let temperatureForecast = ""
 
+//Fetches current weather data
 const fetchData = () => {
     fetch (URL)
         .then((response) => {
@@ -28,15 +30,43 @@ const fetchData = () => {
             city = json.name
             weatherType = json.weather[0].main
             temperatureNow = json.main.temp.toFixed(1)
-            printWeatherInfo (json)
-            printSunInfo (json)
+            printMainInfo (json)
+            printHeaderInfo (json)
         })
         .catch((error) => {
             console.log (error)
         })
 }
-
-const printWeatherInfo = (json) => {
+//Fetches forecast data
+const fetchForecast = () => {
+    fetch (`https://api.openweathermap.org/data/2.5/forecast?q=Stockholm,Sweden&units=metric&APPID=${APP_KEY}`)
+        .then((response) => {
+            return response.json()
+        })
+        .then((json) => {
+            json.list.forEach((update) => {
+                printForecast (update)
+            })
+        })
+}
+//Prints information in the header
+const printHeaderInfo = (json) => {
+  sunriseData = json.sys.sunrise
+  const sunriseHour = new Date(sunriseData * 1000).getHours()
+  const sunriseMinute = new Date(sunriseData * 1000).getMinutes()
+  const sunriseTime = `${sunriseHour}:${sunriseMinute}`
+  sunsetData = json.sys.sunset
+  const sunsetHour = new Date(sunsetData * 1000).getHours()
+  const sunsetMinute = new Date(sunsetData * 1000).getMinutes()
+  const sunsetTime = `${sunsetHour}:${sunsetMinute}`
+  header.innerHTML = `
+    <p>${weatherType} | ${temperatureNow}°</p>
+    <p>sunrise ${sunriseTime}</p>
+    <p>sunset ${sunsetTime}</p>
+    `
+}
+//Changes the main info box depending on weather type
+const printMainInfo = (json) => {
     city = json.name
     weatherType = json.weather[0].main
     temperatureNow = Math.round(json.main.temp)
@@ -56,7 +86,7 @@ const printWeatherInfo = (json) => {
             `
             break
         case "Rain":
-            body.setAttribute("class", "blue-rainy");
+            body.setAttribute("class", "blue-rainy")
             main.innerHTML = `
             <img src="./design/design2/icons/noun_Umbrella_2030530.svg" alt="Icon showing an umbrella">
             <h1>Don't forget your umbrella. It's wet in ${city} today.</h1>
@@ -69,36 +99,7 @@ const printWeatherInfo = (json) => {
             `
     }
 }
-
-const printSunInfo = (json) => {
-    sunriseData = json.sys.sunrise
-    const sunriseHour = new Date(sunriseData*1000).getHours()
-    const sunriseMinute = new Date(sunriseData*1000).getMinutes();
-    const sunriseTime = `${sunriseHour}:${sunriseMinute}`
-    sunsetData = json.sys.sunset
-    const sunsetHour = new Date(sunsetData * 1000).getHours()
-    const sunsetMinute = new Date(sunsetData * 1000).getMinutes();
-    const sunsetTime = `${sunsetHour}:${sunsetMinute}`;
-    header.innerHTML = `
-    <p>${weatherType} | ${temperatureNow}°</p>
-    <p>sunrise ${sunriseTime}</p>
-    <p>sunset ${sunsetTime}</p>
-    `;
-}
-
-const fetchForecast = () => {
-    fetch (`https://api.openweathermap.org/data/2.5/forecast?q=Stockholm,Sweden&units=metric&APPID=${APP_KEY}`)
-        .then((response) => {
-            return response.json()
-        })
-        .then((json) => {
-            json.list.forEach((update) => {
-                printForecast (update)
-            })
-            console.log (json.list)
-        })
-}
-
+//Prints the day of the week correctly
 const createDay = (day) => {    
     switch (day) {
         case 0:
@@ -126,9 +127,9 @@ const createDay = (day) => {
             weekday = "day"
     }    
 }
-
+//Prints the weather forecast
 const printForecast = (update) => {
-    dayForecast = new Date(update.dt * 1000).getDay();
+    dayForecast = new Date(update.dt * 1000).getDay()
     createDay(dayForecast)
     const today = new Date().getDay()
     temperatureForecast = update.main.temp.toFixed(1)
@@ -137,9 +138,7 @@ const printForecast = (update) => {
         container.innerHTML += `
         <p>${weekday} ${temperatureForecast}°</p>
         `
-    } else {
-        console.log('test')
-    }
+    } else {}
 }
 
 fetchData ()
