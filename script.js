a5b81f9a3bb27d9c5a666339283b0636305dff7e
Nathalie <nathae28@hotmail.com>
2024-03-02 22:01:41 +0100

    added a dropdown button for different cities

--- script.js
+++ script.js
@@ -1,35 +1,30 @@
 // DOM selectors
 const currentWeather = document.getElementById("weather-container");
-// const displayTemp = document.getElementById("temp");
-// const displayLocation = document.getElementById("location");
-// const displayCondition = document.getElementById("condition");
+const cityDropdown = document.getElementById("city-dropdown");
 const displayForecast = document.getElementById("weather-forecast");
 
 // API
 const API_BASE_URL = "https://api.openweathermap.org/data/2.5";
 const API_KEY = "a1f68c4f65b632802ca0dd3405694457";
-let place = "tokyo";
-const URL = `${API_BASE_URL}/weather?q=${place}&units=metric&appid=${API_KEY}`;
-const FORECAST = `${API_BASE_URL}/forecast?q=${place}&units=metric&appid=${API_KEY}`;
 
-//UNIX TIMESTAMP CONVERTER
+// UNIX TIMESTAMP CONVERTER
 const convertDateTime = unixTimestamp => {
   const date = new Date(unixTimestamp * 1000);
-  const hours = String(date.getHours()).padStart(2, "0"); // 2 is length of digits and "0" to lead with 0
+  const hours = String(date.getHours()).padStart(2, "0");
   const minutes = String(date.getMinutes()).padStart(2, "0");
   return `${hours}:${minutes}`;
 };
 
-//WEEKDAY DISPLAY FUNCTION
+// WEEKDAY DISPLAY FUNCTION
 const getWeekdays = date => {
   const weekdays = ["Sun", "Mon", "Tue", "Wed", "Thu", "Fri", "Sat"];
   return weekdays[date.getDay()];
 };
 
 // TODAY'S WEATHER
-const printWeather = async () => {
+const printWeather = async weatherURL => {
   try {
-    const weatherNow = await fetch(URL);
+    const weatherNow = await fetch(weatherURL);
     const data = await weatherNow.json();
     const currentTemp = data.main.temp.toFixed(1);
     const currentLocation = data.name;
@@ -56,7 +51,7 @@ const printWeather = async () => {
     currentWeather.className = backgroundClass;
 
     currentWeather.innerHTML = `
-    <div id="weather-container">
+    <div id="today-weather-backgroundr">
     <p class="temp">${currentTemp}<span>&deg;C</span></p>
     <h1 class="location">${currentLocation}</h1>
     <p class="condition">${currentCondition} <img class="icon" src="${ICON_URL}"></p>
@@ -65,36 +60,28 @@ const printWeather = async () => {
     <p class="sunset">Sunset ${todaySunset}</p>
     </div>
     </div>`;
-
-    console.log("Data:", data); //NOT FORGET TO DELETE!!
   } catch (error) {
     console.error(error); //in case of an error
   }
 };
-printWeather();
 
 // 4-DAY WEATHER FORECAST
-const printForecast = async () => {
+const printForecast = async forecastURL => {
   try {
-    const fourDayWeather = await fetch(FORECAST);
+    const fourDayWeather = await fetch(forecastURL);
     const json = await fourDayWeather.json();
     const forecastData = json.list
       .filter((item, index) => (index - 1) % 8 === 0)
-      .slice(0, 4); // Filter for every 8th item, which represents once every day
+      .slice(0, 4);
 
-    console.log("FORECAST JSON DATA", json); //NOT TO FORGET TO DELETE
-
-    //To get the correct temperature for each of the day in the forecast
     forecastData.forEach((forecast, index) => {
-      const startIdx = index * 8; // Starting index of the forecast day
-      const endIdx = startIdx + 8; // Ending index of the forecast day
+      const startIdx = index * 8;
+      const endIdx = startIdx + 8;
 
-      // Extract temperatures for the current forecast day
       const temperatures = json.list
         .slice(startIdx, endIdx)
         .map(item => item.main.temp);
 
-      // Calculate lowest and highest temperatures for the current forecast day
       const lowestTemp = Math.min(...temperatures).toFixed(0);
       const highestTemp = Math.max(...temperatures).toFixed(0);
 
@@ -115,4 +102,23 @@ const printForecast = async () => {
     console.error(error);
   }
 };
-printForecast();
+
+// EVENTLISTENER
+cityDropdown.addEventListener("change", () => {
+  const newCity = cityDropdown.value;
+  const newWeatherURL = `${API_BASE_URL}/weather?q=${newCity}&units=metric&appid=${API_KEY}`;
+  const newForecastURL = `${API_BASE_URL}/forecast?q=${newCity}&units=metric&appid=${API_KEY}`;
+
+  currentWeather.innerHTML = "";
+  displayForecast.innerHTML = "";
+
+  printWeather(newWeatherURL);
+  printForecast(newForecastURL);
+});
+
+// Initial call
+const initialWeatherURL = `${API_BASE_URL}/weather?q=bern&units=metric&appid=${API_KEY}`;
+const initialForecastURL = `${API_BASE_URL}/forecast?q=bern&units=metric&appid=${API_KEY}`;
+
+printWeather(initialWeatherURL);
+printForecast(initialForecastURL);
