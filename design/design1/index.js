3af0021692fd8218bd02446ff8c308f9e01a16ac
Citronskal <yiaswift@gmail.com>
2024-03-02 23:36:47 +0100

    V3. Search result style off

--- design/design1/index.js
+++ design/design1/index.js
@@ -7,6 +7,7 @@ const searchButton = document.getElementById("search-btn");
 // API
 const apiKey = "69822732ac1661b79f6670b175a16816";
 const defaultCity = "Malmö";
+let firstClick = true;
 
 // Event listener for weather form submission
 weatherForm.addEventListener("submit", async (event) => {
@@ -18,8 +19,8 @@ weatherForm.addEventListener("submit", async (event) => {
       // Fetch current weather and forecast data
       const { currentWeather, forecast } = await getWeather(city);
 
-      displayWeatherInfo(currentWeather); // This line is correct
-      displayWeatherForecast(forecast); // Check this line
+      displayWeatherInfo(currentWeather);
+      displayWeatherForecast(forecast);
     } catch (error) {
       console.error(error);
       displayError("Failed to fetch weather data. Please try again later.");
@@ -41,6 +42,10 @@ window.addEventListener("load", async () => {
 });
 
 searchButton.addEventListener("click", function () {
+  if (firstClick) {
+    firstClick = false; // Update flag to indicate the first click has occurred
+    return; // Exit the function without displaying the error message
+  }
   // If the input field is empty and currently visible, hide it
   if (cityInput.value.trim() === "" && cityInput.style.display !== "none") {
     cityInput.style.display = "none";
@@ -59,26 +64,72 @@ async function getWeather(city) {
   const currentWeatherUrl = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}`;
   const forecastUrl = `https://api.openweathermap.org/data/2.5/forecast?q=${city}&appid=${apiKey}`;
 
-  const currentWeatherResponse = await fetch(currentWeatherUrl);
-  const forecastResponse = await fetch(forecastUrl);
+  const [currentWeatherResponse, forecastResponse] = await Promise.all([
+    fetch(currentWeatherUrl),
+    fetch(forecastUrl),
+  ]);
 
   if (!currentWeatherResponse.ok || !forecastResponse.ok) {
     throw new Error("Failed to fetch weather data");
   }
 
-  const currentWeatherData = await currentWeatherResponse.json();
-  const forecastData = await forecastResponse.json();
+  const [currentWeatherData, forecastData] = await Promise.all([
+    currentWeatherResponse.json(),
+    forecastResponse.json(),
+  ]);
 
   return { currentWeather: currentWeatherData, forecast: forecastData };
 }
 
+// Function to adjust sunrise and sunset times based on timezone
+function adjustTimezone(sunrise, sunset, timezone) {
+  const sunriseTime = new Date((sunrise + timezone) * 1000);
+  const sunsetTime = new Date((sunset + timezone) * 1000);
+  const options = {
+    hour: "2-digit",
+    minute: "2-digit",
+    hour12: false,
+    timeZone: "UTC",
+  };
+
+  const sunriseFormatted = sunriseTime.toLocaleTimeString(undefined, options);
+  const sunsetFormatted = sunsetTime.toLocaleTimeString(undefined, options);
+
+  return { sunrise: sunriseFormatted, sunset: sunsetFormatted };
+}
+
+// Modify displayWeatherInfo function to use adjusted sunrise and sunset times
 function displayWeatherInfo(data) {
   const {
     name: city,
     main: { temp },
     weather: [{ main: description, id }],
     sys: { sunrise, sunset },
+    timezone,
   } = data;
+
+  const { sunrise: sunriseTime, sunset: sunsetTime } = adjustTimezone(
+    sunrise,
+    sunset,
+    timezone
+  );
+  const now = new Date().getTime() / 1000;
+  const isNight = now < sunrise || now > sunset;
+
+  // Get the body element
+  const body = document.body;
+  const iconElement = document.getElementById("current-icon");
+  iconElement.src = "";
+
+  // Apply different background based on whether it's night or day
+  if (isNight) {
+    body.style.background = "linear-gradient(to right, #333333, #000000)";
+    body.style.color = "#ffffff";
+    iconElement.src = `assets/moon.png`;
+  } else {
+    body.style.background = "linear-gradient(#8589ff, #e8e9ff)";
+    body.style.color = "#ffffff";
+  }
 
   weatherBackground.innerHTML = ""; // Clear previous content
 
@@ -86,10 +137,14 @@ function displayWeatherInfo(data) {
   const cityDisplay = document.createElement("h2");
   const desDisplay = document.createElement("p");
   const sunDisplay = document.createElement("p");
-  const iconDisplay = document.createElement("img");
+  const iconDisplay = document.createElement("img"); // Declare and initialize iconDisplay here
+  const celsiusDisplay = document.createElement("span");
 
-  tempDisplay.textContent = `${(temp - 273.15).toFixed(0)}°C`;
+  tempDisplay.textContent = `${(temp - 273.15).toFixed(0)}`;
   tempDisplay.classList.add("current-temp");
+
+  celsiusDisplay.textContent = "°C"; // Display the Celsius symbol
+  celsiusDisplay.classList.add("celsius");
 
   cityDisplay.textContent = city;
   cityDisplay.classList.add("current-city");
@@ -97,32 +152,33 @@ function displayWeatherInfo(data) {
   desDisplay.textContent = description;
   desDisplay.classList.add("current-sky");
 
+  iconDisplay.classList.add("current-icon");
+
   // Set the src attribute of the iconDisplay based on the weather condition ID
-  switch (true) {
-    case id === 800:
-      iconDisplay.src = "assets/clear.png";
-      break;
-    case id <= 804:
-      iconDisplay.src = "assets/clouds.png";
-      break;
-    case id <= 504:
-      iconDisplay.src = "assets/rain.png";
-      break;
-    default:
-      iconDisplay.src = "assets/clear.png";
+  if (isNight) {
+    iconDisplay.src = "assets/moon.png";
+  } else {
+    switch (true) {
+      case id === 800:
+        iconDisplay.src = "assets/clear.png";
+        break;
+      case id <= 804:
+        iconDisplay.src = "assets/clouds.png";
+        break;
+      case id <= 504:
+        iconDisplay.src = "assets/rain.png";
+        break;
+      default:
+        iconDisplay.src = "assets/clear.png";
+    }
   }
 
-  const sunriseTime = new Date(sunrise * 1000);
-  const sunsetTime = new Date(sunset * 1000);
-  const options = { hour: "2-digit", minute: "2-digit", hour12: false };
-  const sunriseFormatted = sunriseTime.toLocaleTimeString(undefined, options);
-  const sunsetFormatted = sunsetTime.toLocaleTimeString(undefined, options);
-
-  sunDisplay.textContent = `Sunrise: ${sunriseFormatted} Sunset: ${sunsetFormatted}`;
+  sunDisplay.textContent = `Sunrise: ${sunriseTime} Sunset: ${sunsetTime}`;
   sunDisplay.classList.add("weather-detail");
 
-  weatherBackground.appendChild(tempDisplay);
   weatherBackground.appendChild(iconDisplay);
+  weatherBackground.appendChild(tempDisplay);
+  weatherBackground.appendChild(celsiusDisplay);
   weatherBackground.appendChild(cityDisplay);
   weatherBackground.appendChild(desDisplay);
   weatherBackground.appendChild(sunDisplay);
@@ -134,61 +190,56 @@ async function displayWeatherForecast(forecast) {
     weekDaysContainer.innerHTML = "";
 
     const weekDays = ["Sun", "Mon", "Tue", "Wed", "Thu", "Fri", "Sat"];
-
-    // Keep track of the days already displayed
     const displayedDays = new Set();
+    const timezoneOffset = forecast.city.timezone; // Get the timezone offset of the city
 
-    // Iterate over each entry in the forecast list
     forecast.list.forEach((dayForecast) => {
-      // Extract the date from the forecast data
-      const date = new Date(dayForecast.dt * 1000);
-      // Get the day index (0 for Sunday, 1 for Monday, ..., 6 for Saturday)
+      const date = new Date((dayForecast.dt + timezoneOffset) * 1000);
       const dayIndex = date.getDay();
 
-      // Check if the day index is within the valid range
-      if (dayIndex >= 0 && dayIndex < weekDays.length) {
-        // Check if this day has already been displayed
-        if (!displayedDays.has(dayIndex)) {
-          const tempMin = (dayForecast.main.temp_min - 273.15).toFixed(0);
-          const tempMax = (dayForecast.main.temp_max - 273.15).toFixed(0);
-          const weatherId = dayForecast.weather[0].id; // Get weather ID
+      if (
+        dayIndex >= 0 &&
+        dayIndex < weekDays.length &&
+        !displayedDays.has(dayIndex)
+      ) {
+        const tempMin = (dayForecast.main.temp_min - 273.15).toFixed(0);
+        const tempMax = (dayForecast.main.temp_max - 273.15).toFixed(0);
+        const weatherId = dayForecast.weather[0].id;
 
-          // Create a div element for the day forecast
-          const dayElement = document.createElement("div");
-          dayElement.classList.add("week-day");
+        const dayElement = document.createElement("div");
+        dayElement.classList.add("week-days");
 
-          // Create elements for the day name, temperature, and weather icon
-          const dayName = document.createElement("div");
-          dayName.textContent = weekDays[dayIndex];
-          const tempElement = document.createElement("div");
-          tempElement.textContent = `${tempMin}°C / ${tempMax}°C`;
-          const iconElement = document.createElement("img");
+        const dayName = document.createElement("div");
+        dayName.textContent = weekDays[dayIndex];
+        dayName.classList.add("day");
 
-          // Set the appropriate weather icon based on weather ID
-          switch (true) {
-            case weatherId === 800:
-              iconElement.src = "assets/clear.png";
-              break;
-            case weatherId <= 804:
-              iconElement.src = "assets/clouds.png";
-              break;
-            case weatherId <= 504:
-              iconElement.src = "assets/rain.png";
-              break;
-            default:
-              iconElement.src = "assets/clear.png";
-          }
+        const tempElement = document.createElement("div");
+        tempElement.textContent = `${tempMin}°C / ${tempMax}°C`;
+        tempElement.classList.add("week-temp");
 
-          dayElement.appendChild(dayName);
-          dayElement.appendChild(tempElement);
-          dayElement.appendChild(iconElement);
+        const iconElement = document.createElement("img");
+        iconElement.classList.add("weather-emoji");
 
-          // Append the day element to the week-days container
-          weekDaysContainer.appendChild(dayElement);
-
-          // Add the day index to the set of displayed days
-          displayedDays.add(dayIndex);
+        switch (true) {
+          case weatherId === 800:
+            iconElement.src = "assets/clear.png";
+            break;
+          case weatherId <= 804:
+            iconElement.src = "assets/clouds.png";
+            break;
+          case weatherId <= 504:
+            iconElement.src = "assets/rain.png";
+            break;
+          default:
+            iconElement.src = "assets/clear.png";
         }
+
+        dayElement.appendChild(dayName);
+        dayElement.appendChild(iconElement);
+        dayElement.appendChild(tempElement);
+
+        weekDaysContainer.appendChild(dayElement);
+        displayedDays.add(dayIndex);
       }
     });
   } catch (error) {
@@ -205,10 +256,10 @@ function displayError(message) {
 
   setTimeout(() => {
     errorDisplay.remove();
-  }, 5000);
+  }, 1000);
 }
 
 function hideErrorMessage() {
-  errorDisplay.textContent = ""; // Clear the error message
+  errorDisplay.textContent = "";
   errorDisplay.classList.remove("error");
 }
