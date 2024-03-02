31834cde8f533df6424bff974ee83494b5cdc04d
Paulajungaker <paula010514@hotmail.com>
2024-03-02 18:47:41 +0100

    Adding features

--- script.js
+++ script.js
@@ -3,12 +3,15 @@
  const searchBar = document.getElementById("searchPlace");
  const searchForCityText = document.getElementById("searchForCityText");
  const weatherContainer = document.getElementById("weatherContainer");
- const weatherForecastContainer = document.getElementById("weatherForecastDocument");
+ const weatherForecastContainer = document.getElementById("weatherForecastContainer");
+ const sunContainer = document.getElementById("sunContainer");
 
-
+const city = "Palma de Mallorca, Spain";
 const base_URL = "https://api.openweathermap.org/data/2.5/weather?";
 const API_Key = "a27f07476fdacc3ee7dc03fc0b7264ed";
 const units = "metric";
+const baseForecast_URL = "https://api.openweathermap.org/data/2.5/forecast?"
+const forecast_URL = `${baseForecast_URL}q=${city}&units=${units}&APPID=${API_Key}`;
 
 // Fetch the API
 const fetchWeatherData = (city) => {
@@ -24,14 +27,36 @@ fetch(URL)
    })
    .then((json) => {
     const cityName = json.name;
-    console.log(cityName);
-    const degrees = json.main.temp;
-    console.log(degrees);
+    const degrees = Math.round(json.main.temp); 
     const weatherDescription = json.weather[0].description;
-    console.log(weatherDescription);
+
+    // Turn sunset and sunrise to a readable format like 13:00
+
+   function convertTime(timestamp) {
+     const date = new Date(timestamp * 1000);
+
+   const hours = date.getHours().toString().padStart(2, "0");
+
+   const minutes = date.getMinutes().toString().padStart(2, "0");
+
+   return `${hours}:${minutes}`;
+
+   }
+
+    const sunriseTime = convertTime(json.sys.sunrise);
+    console.log("Sunrise time: ", sunriseTime);
+
+    const sunsetTime = convertTime(json.sys.sunset);
+    console.log("Sunset time: ", sunsetTime);
+
 
 // Fill the content in the HTML elements
 weatherContainer.textContent = `${degrees}°C ${cityName} ${weatherDescription}`;
+
+  // Fill in the sunset and sunrise in the HTML element. 
+
+sunContainer.textContent = `Sunrise ${sunriseTime} Sunset ${sunsetTime}`;
+
    })
 
    .catch((error) => {
@@ -40,10 +65,67 @@ weatherContainer.textContent = `${degrees}°C ${cityName} ${weatherDescription}`
   };
 
 // Example of city
-  const city = "Palma de Mallorca, Spain";
+
   fetchWeatherData(city);
 
+// Fetch for the forecast data
+
+const fetchForecast = (city) => {
+  fetch(forecast_URL)
+  .then((response) => response.json())
+  .then((json) => {
+    console.log(json);
+  
+  // Check that time is 12:00
+  const filteredTime = json.list.filter((daytimeWeather)=> 
+  daytimeWeather.dt_txt.includes("12:00")
+  );
+
+  let forecastHTML = " ";
+
+  filteredTime.forEach((day) => {
+    const date = new Date(day.dt * 1000);
+    nameOfDay = date.toLocaleDateString("en-US", {weekday: "short"});
+    const tempMin = Math.round(day.main.temp_min);
+    const tempMax = Math.round(day.main.temp_max);
+
+    /*
+    const today = new Date();
+    const todaysForecast = date.getDay() === today.getDay();
+    */
+
+    console.log(nameOfDay, tempMin, tempMax);
+
+    forecastHTML += `${nameOfDay} ${tempMin}°C / ${tempMax}°C<br>`;
+
+  });
+
+  weatherForecastContainer.innerHTML = forecastHTML;
+
+  })
+  .catch((error) => {
+    console.error("Error fetching forecast: ", error);
+  });
+};
+
+  fetchForecast(city);
+  console.log(forecast_URL);
+
+
+
+
+
+
+
+
+
+
+  
+
  
+
+
+
 
 
  
