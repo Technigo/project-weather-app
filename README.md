a1d488ae53a5b72b09f1e8642e27ecb40227aa48
Kathinka Sewell <4355167+kathinka@users.noreply.github.com>
2024-03-02 23:22:58 +0100

    added removed styles

--- README.md
+++ README.md
@@ -1,12 +1,11 @@
 # Weather App
 
-Replace this readme with your own information about your project.
-
-Start by briefly describing the assignment in a sentence or two. Keep it short and to the point.
+Made a minimalistic weather app showing the weather for Oslo, Norway
 
 ## The problem
 
-Describe how you approached to problem, and what tools and techniques you used to solve it. How did you plan? What technologies did you use? If you had more time, what would be next?
+Solved the task according to the brief.
+Would have added search, dynamic night/day styles and geolocation of user if i had more time.
 
 ## View it live
 
--- script.js
+++ script.js
@@ -5,6 +5,7 @@ const sunset = document.getElementById("sunset")
 const suggestion = document.getElementById("suggestion")
 const icon = document.getElementById("icon")
 const forcast = document.querySelector("forcast")
+const main = document.querySelector('main')
 
 
 const API_URL = 'https://api.openweathermap.org/data/2.5/'
@@ -16,7 +17,6 @@ const country = 'Norway'
 const units = 'metric'
 const dailyForcast = `${API_URL}${today}?q=${city},${country}&units=${units}&APPID=${API_KEY}`
 const fiveDayForcast = `${API_URL}${fiveDays}?q=${city},${country}&units=${units}&APPID=${API_KEY}`
-const main = document.querySelector('main')
 
 let riseHours
 let riseMinutes
@@ -26,10 +26,43 @@ let currentStyle
 let updateStyle
 let suggestionTxt
 let timestamp
-let timezone
 let weekday
 let hours
 let minutes
+
+const getStyles = (currentStyle) => {
+	switch (currentStyle) {
+		case 'Rain':
+			updateStyle = 'rainy';
+			suggestionTxt = `Don't forget your umbrella. It's wet in ${city} today.`
+			break;
+		case 'Drizzle':
+			updateStyle = 'rainy';
+			suggestionTxt = `You might concider bringing your umbrella.We might get some light rain in ${city} today.`
+			break;
+		case 'Thunderstorm':
+			updateStyle = 'rainy';
+			suggestionTxt = `Uhu looks like a thunder in ${city} today.`
+			break;
+		case 'Snow':
+			updateStyle = 'rainy';
+			suggestionTxt = `Light a fire and get cosy.${city} is looks like snow today.`
+			break;
+		case 'Clear':
+			updateStyle = 'sunny';
+			suggestionTxt = `Get your sunnies on.${city} is looking rather great today.`
+			break;
+		case 'Clouds':
+			updateStyle = 'cloudy';
+			suggestionTxt = `Light a fire and get cosy.${city} is looking grey today.`
+			break;
+		case 'Sun':
+			updateStyle = 'sunny';
+			suggestionTxt = `Get your sunnies on.${city} is looking rather great today.`
+			break;
+		default:
+	}
+}
 
 const fetchFiveDayForcast = () => {
 	fetch(fiveDayForcast).then(response => {
@@ -39,18 +72,21 @@ const fetchFiveDayForcast = () => {
 		return response.json();
 	}
 	).then(weekData => {
-		const uniqueDays = weekData.list.filter((time) => time.dt_txt.includes("00:00:00"));
-		uniqueDays.forEach(d => {
+		const firstFilter = weekData.list.filter((time) => time.dt_txt.includes("12:00"));
+		let now = new Date()
+		now = now.toString()
+		now = now.slice(0, 3)
+		const filterOutToday = firstFilter.filter((time) => !time.dt_txt.includes(now));
 
+		filterOutToday.forEach(d => {
 			const timestamp = d.dt
 			const forcast = document.querySelector(".forcast")
 			forcast.innerHTML += `
-			<div class ="lines">
+			<div class ="lines ${updateStyle}">
 			<div class="day">${convertDay(weekday)}</div>
-			<div class="temp">${parseInt(d.main.temp).toFixed(1)}°</div>
-				<div/>`
+			<div class="temp">${d.weather[0].description} | ${parseInt(d.main.temp).toFixed(1)}° | Feels like: ${parseInt(d.main.feels_like).toFixed(1)}°</div>
+			</div>`
 			convertTime(timestamp)
-			console.log(d.dt_txt)
 		});
 	}
 	).catch(error => {
@@ -67,15 +103,15 @@ const fetchForcast = () => {
 		return response.json();
 	}
 	).then(data => {
-		const rise = convertTime(data.sys.sunrise)
-		const set = convertTime(data.sys.sunset)
 		currentStyle = data.weather[0].main
 		getStyles(currentStyle)
+		const rise = convertTime(data.sys.sunrise)
+		const set = convertTime(data.sys.sunset)
 		main.classList.add(updateStyle)
 		icon.classList.add(updateStyle)
-		weather.innerText = `${data.weather[0].main} | ${parseInt(data.main.temp).toFixed(1)}°`
-		sunrise.innerText = `sunrise ${rise}`
-		sunset.innerText = `sunset ${set}`
+		weather.innerText = `${data.weather[0].description} | ${parseInt(data.main.temp).toFixed(1)}°`
+		sunrise.innerText = `sunrise ${rise} `
+		sunset.innerText = `sunset ${set} `
 		suggestion.innerText = suggestionTxt
 	}
 	).catch(error => {
@@ -86,12 +122,10 @@ const fetchForcast = () => {
 const convertTime = (time) => {
 	let unix_timestamp = time
 	let date = new Date(unix_timestamp * 1000)
-	let hours = date.getHours()
-	let minutes = date.getMinutes()
+	let hours = (date.getHours() < 10 ? '0' : '') + date.getHours()
+	let minutes = (date.getMinutes() < 10 ? '0' : '') + date.getMinutes()
 	weekday = date.getDay()
-
-	console.log("day: " + convertDay(weekday) + " hours: " + hours + " minutes: " + minutes)
-	return (`${hours}:${minutes}`)
+	return (`${hours}.${minutes} `)
 }
 
 
@@ -116,50 +150,6 @@ const convertDay = (weekday) => {
 	}
 }
 
-const getStyles = (currentStyle) => {
-	switch (currentStyle) {
-		case 'Rain':
-			console.log("its rain")
-			updateStyle = 'rainy';
-			suggestionTxt = `Don't forget your umbrella. It's wet in ${city} today.`
-			break;
-		case 'Drizzle':
-			console.log("its drizzle")
-			updateStyle = 'rainy';
-			suggestionTxt = `You might concider bringing your umbrella. We might get some light rain in ${city} today.`
-			break;
-		case 'Thunderstorm':
-			console.log("thundertime")
-			updateStyle = 'rainy';
-			suggestionTxt = `Uhu looks like a thunder in ${city} today.`
-			break;
-		case 'Snow':
-			console.log("its snowing")
-			updateStyle = 'rainy';
-			suggestionTxt = `Light a fire and get cosy. ${city} is looks like snow today.`
-			break;
-		case 'Clear':
-			console.log("its clear skies")
-			updateStyle = 'sunny';
-			suggestionTxt = `Get your sunnies on. ${city} is looking rather great today.`
-			break;
-		case 'Clouds':
-			console.log("its cloudy")
-			updateStyle = 'cloudy';
-			suggestionTxt = `Light a fire and get cosy. ${city} is looking grey today.`
-			console.log(updateStyle)
-			break;
-		case 'Sun':
-			console.log("its sunny")
-			updateStyle = 'sunny';
-			suggestionTxt = `Get your sunnies on. ${city} is looking rather great today.`
-			break;
-		default:
-			console.log("could not find weathertype")
-			console.log(data.weather[0].main)
-	}
-}
 
 
 fetchForcast();
-
