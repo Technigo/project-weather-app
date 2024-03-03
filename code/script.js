1eea56c4f9e2aa0c8527ff5713cc8c8315497975
SofieFerrari <sofiestrahl@gmail.com>
2024-03-03 16:18:02 +0100

    I'm lost

--- code/script.js
+++ code/script.js
@@ -1,22 +1,16 @@
-//Breaking down URL - not working
-const BASE_URL = 'https://api.openweathermap.org/data/2.5/'
-const API_KEY = '1e48fdf267ccc8ee33c1c78150dcbab1'
-const city = 'Umea, Sweden'
-
-const URL = `${BASE_URL}weather?q=${city}&units=metric&APPID=${API_KEY}`
-
 //DOM selectors
 const backgroundColor = document.getElementById('background-color')
 const weatherContainer = document.getElementById('weather-container')
 const sun = document.getElementById('sun')
-const imgContainer = document.getElementById('img-container')
+const icons = document.getElementById('icon-container')
 const cityContainer = document.getElementById('city-container')
 const forecastContainer = document.getElementById('forecast-container')
 
 const errorDiv = document.getElementById('error')
 const handleForecast = document.getElementById('forecast-list')
 
-//global scope
+//Variabel for weekdays
+const dayNames = ['sun', 'mon', 'tue', 'wed', 'thu', 'fri', 'sat']
 
 //Fetching the API
 const fetchWeather = () => {
@@ -31,20 +25,6 @@ const fetchWeather = () => {
 		.catch((error) => console.log(error))
 }
 fetchWeather()
-
-//Fetching the API FORECAST
-const fetchForecast = () => {
-	fetch(
-		'https://api.openweathermap.org/data/2.5/forecast?q=Umea,Sweden&units=metric&APPID=1e48fdf267ccc8ee33c1c78150dcbab1'
-	)
-		.then((response) => response.json())
-		.then((forecast) => {
-			console.log(forecast)
-			updateForcast(forecast)
-		})
-		.catch((error) => console.log(error))
-}
-fetchForecast()
 
 //Function to get data to HTML
 const updateHtml = (data) => {
@@ -61,49 +41,61 @@ const updateHtml = (data) => {
 	})
 	weatherContainer.innerHTML = `<p>${weather} |</p><p> ${temp}°C</p>`
 	sun.innerHTML = `
-    sunrise ${rise}
-    sunset ${set}`
-	console.log(sun.innerHTML)
+    <p>sunrise ${rise}</p>
+    <p>sunset ${set}</p>`
+	console.log()
+
+	weatherStyle(weather)
+	filterTheTime(displayForecast)
 }
-//call function with updateHtml() ?
 
-//Variabel for weekdays
-const dateNames = ['sun', 'mon', 'tue', 'wed', 'thu', 'fri', 'sat']
-
-//Function to display forecast
-const updateForecast = (forecast) => {}
+//Fetching the API FORECAST
+const fetchForecast = () => {
+	fetch(
+		'https://api.openweathermap.org/data/2.5/forecast?q=Umea,Sweden&units=metric&APPID=1e48fdf267ccc8ee33c1c78150dcbab1'
+	)
+		.then((response) => response.json())
+		.then((forecast) => {
+			console.log('forecast', forecast)
+			updateForcast(forecast)
+		})
+		.catch((error) => console.log(error))
+}
+fetchForecast()
 
 //Function that changes background/text-color depending on weather
 const weatherStyle = (weather) => {
 	if (weather === 'Clear') {
-		backgroundColor.style.backgroundImage =
-			'/images/noun_Sunglasses_2055147.svg'
-	} else if (weather === 'Clouds' || 'Mist') {
-		backgroundColor.style.backgroundImage = '/images/noun_Cloud_1188486.svg'
+		backgroundColor.classList.add('sunny')
+		cityContainer.innerHTML = `<p>Get your sunnies on. Umeå is looking rather great today.</p>`
+	} else if (weather === 'Clouds' || weather === 'Mist') {
+		backgroundColor.classList.add('cloud')
+		cityContainer.innerText = `Light a fire and get cozy. Umeå is looking grey today.`
 	} else if (weather === 'Rain') {
-		backgroundColor.style.backgroundImage = '/images/noun_Umbrella_2030530.svg'
+		backgroundColor.classList.add('rain')
+		cityContainer.innerText = `Don't forget your umbrella. It's wet in Umeå today.`
 	}
 }
-weatherStyle()
 
-const displayForecast = (filterForecast) => {
-	filterForecast.forEach((day) => {
-		const weekDay = new Date(day.dt * 1000)
-		const dayIndex = weekDay.getDay()
-		const date = dateNames[dayIndex]
-		const weekTemp = Math.round(day.main.temp)
-		forecastContainer.innerHTML += `<h4>${weekDay}</h4></div>
-    <h4>${weekTemp} °C</h4>`
-	})
-	
-}
-displayForecast()
-
-//funktion för att fitrera
+//funktion för att fitrera fram 12.00
 const filterTheTime = (forecast) => {
 	const filterForecast = forecast.list.filter((day) =>
 		day.dt_txt.includes('12:00')
 	)
-	console.log(filterTheTime)
-	displayForecast()
+	displayForecast(filterForecast)
 }
+
+const displayForecast = (filterForecast) => {
+	let forecastHTML = ''
+	filterForecast.forEach((day) => {
+		const showDay = new Date(day.dt * 1000)
+		const dayIndex = showDay.getDay()
+		const weekDay = dayNames[dayIndex]
+		const weekTemp = Math.round(day.main.temp)
+		forecastHTML += `<div class ="weekday-list"><p>${weekDay}</p></div> <div class ="temp-list"><p>${weekTemp} °C</p></div>`
+	})
+
+	// Set forecastContainer innerHTML
+	forecastContainer.innerHTML = forecastHTML
+}
+
