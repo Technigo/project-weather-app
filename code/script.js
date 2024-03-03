b9b1f21cd67ac4212caaf112c5b00a88580e6b45
SofieFerrari <sofiestrahl@gmail.com>
2024-03-03 10:53:00 +0100

    working on displaying forecast

--- code/script.js
+++ code/script.js
@@ -1,4 +1,3 @@
-
 //Breaking down URL - not working
 const BASE_URL = 'https://api.openweathermap.org/data/2.5/'
 const API_KEY = '1e48fdf267ccc8ee33c1c78150dcbab1'
@@ -7,34 +6,29 @@ const city = 'Umea, Sweden'
 const URL = `${BASE_URL}weather?q=${city}&units=metric&APPID=${API_KEY}`
 
 //DOM selectors
+const backgroundColor = document.getElementById('background-color')
 const weatherContainer = document.getElementById('weather-container')
+const sun = document.getElementById('sun')
 const imgContainer = document.getElementById('img-container')
 const cityContainer = document.getElementById('city-container')
 const forecastContainer = document.getElementById('forecast-container')
 
-const sunrise = document.getElementById('sunrise')
-const sunset = document.getElementById('sunset')
-
 const errorDiv = document.getElementById('error')
-const handleTemp = document.getElementById('temperature')
-const handleName = document.getElementById('city-name')
 const handleForecast = document.getElementById('forecast-list')
-const weatherDescription = document.getElementById('weather-description')
 
 //global scope
-
 
 //Fetching the API
 const fetchWeather = () => {
 	fetch(
 		'https://api.openweathermap.org/data/2.5/weather?q=Umea,Sweden&units=metric&APPID=1e48fdf267ccc8ee33c1c78150dcbab1'
 	)
-	.then((response) => response.json())
-	.then((data) => {
-		console.log(data)
-		updateHtml(data)	
-	})
-	.catch((error) => console.log(error))
+		.then((response) => response.json())
+		.then((data) => {
+			console.log(data)
+			updateHtml(data)
+		})
+		.catch((error) => console.log(error))
 }
 fetchWeather()
 
@@ -43,101 +37,73 @@ const fetchForecast = () => {
 	fetch(
 		'https://api.openweathermap.org/data/2.5/forecast?q=Umea,Sweden&units=metric&APPID=1e48fdf267ccc8ee33c1c78150dcbab1'
 	)
-	.then((response) => response.json())
-	.then((forecast) => {
-		let filteredForecast = forecast.list.filter((day) =>
-			day.dt_txt.includes('12:00')
-	)})
-	.catch((error) => console.log(error))
+		.then((response) => response.json())
+		.then((forecast) => {
+			console.log(forecast)
+			updateForcast(forecast)
+		})
+		.catch((error) => console.log(error))
 }
 fetchForecast()
-
 
 //Function to get data to HTML
 const updateHtml = (data) => {
 	const weather = data.weather[0].main
 	const city = data.name
-  	const temp = Math.trunc(data.main.temp)
+	const temp = Math.trunc(data.main.temp)
 	const rise = new Date(data.sys.sunrise * 1000).toLocaleTimeString([], {
-    	hour: '2-digit',
-    	minute: '2-digit',
-  	})
-  	const set = new Date(data.city.sunset * 1000).toLocaleTimeString([], {
-    	hour: '2-digit',
-    	minute: '2-digit',
-  	})
-  
-  	weatherContainer.innerHTML = `<p>${weather} |</p><p>${temp}</p>
-  	<p>${rise}</p>
-  	<p>${set}</p>
-  	`
+		hour: '2-digit',
+		minute: '2-digit',
+	})
+	const set = new Date(data.sys.sunset * 1000).toLocaleTimeString([], {
+		hour: '2-digit',
+		minute: '2-digit',
+	})
+	weatherContainer.innerHTML = `<p>${weather} |</p><p> ${temp}°C</p>`
+	sun.innerHTML = `
+    sunrise ${rise}
+    sunset ${set}`
+	console.log(sun.innerHTML)
 }
-const dateNames = ['sun', 'mon', 'tue', 'wed', 'thu', 'fri', 'sat',]
+//call function with updateHtml() ?
+
+//Variabel for weekdays
+const dateNames = ['sun', 'mon', 'tue', 'wed', 'thu', 'fri', 'sat']
+
 //Function to display forecast
-const displayForecast = (filteredForecast) => {
-  filteredForecast.forEach((day) => {
-    const weekDay = new Date(day.dt * 1000)
-    const dayIndex = weekDay.getDay()
-    const date = dateNames[dayIndex]
-    const weekTemp = Math.round(day.main.temp)
-    forecastContainer.innerHTML += `<h4>${weekDay}</h4></div>
-    <h4>${weekTemp} °C</h4>`
-  })
+const updateForecast = (forecast) => {}
+
+//Function that changes background/text-color depending on weather
+const weatherStyle = (weather) => {
+	if (weather === 'Clear') {
+		backgroundColor.style.backgroundImage =
+			'/images/noun_Sunglasses_2055147.svg'
+	} else if (weather === 'Clouds' || 'Mist') {
+		backgroundColor.style.backgroundImage = '/images/noun_Cloud_1188486.svg'
+	} else if (weather === 'Rain') {
+		backgroundColor.style.backgroundImage = '/images/noun_Umbrella_2030530.svg'
+	}
 }
+weatherStyle()
 
+const displayForecast = (filterForecast) => {
+	filterForecast.forEach((day) => {
+		const weekDay = new Date(day.dt * 1000)
+		const dayIndex = weekDay.getDay()
+		const date = dateNames[dayIndex]
+		const weekTemp = Math.round(day.main.temp)
+		forecastContainer.innerHTML += `<h4>${weekDay}</h4></div>
+    <h4>${weekTemp} °C</h4>`
+	})
+	
+}
+displayForecast()
 
-
-
-
-
-//const weatherIcon = day.weather[0].icon
-			//handleForecast.innerHTML = ''
-
-			//filteredForecast.forEach((day) => {
-			//const weekTemp = day.main.temp.toFixed(0)
-				
-
-				//let weekday = new Date()
-				//let today = weekday.getDay()
-				//let tomorrow = today + 1
-
-                //insert picture for cloud/sun/rain?
-				/*
-				handleForecast.innerHTML += `
-                <div id="forecast-list">
-                <p>${new Date(day.dt_txt.replace(/-/g, '/')).toLocaleDateString(
-									'en-US',
-									{
-										weekday: 'short',
-									}
-								)} 
-                </p>
-                <p id="weekTemp">${weekTemp}ºC</p>
-                ${[day.weather[0].main]}
-                </div>
-                `
-				*/
-
-		
-				//skapa lista över fyra dagar fram. temp kl 12.00
-				//skapa funktion som filtrerar ut värden från object i array
-
-//const nameChoice = data.name
-//const tempChoice = Math.trunc(data.main.temp)
-//const weatherDescr = data.weather[0].main
-//handleName.innerText = city
-//handleTemp.innerText = `${temp} °C`
-//weatherDescription.innerText = `${weather}`
-
-/*
-const rise = new Date(data.sys.sunrise * 1000).toLocaleTimeString([],{
-	hour: '2-digit',
-	minute: '2-digit',
-})
-
-
-const set = new Date(data.sys.sunset * 1000).toLocaleTimeString([],{
-	hour: '2-digit',
-	minute: '2-digit',
-})
-*/
+//funktion för att fitrera
+const filterTheTime = (forecast) => {
+	const filterForecast = forecast.list.filter((day) =>
+		day.dt_txt.includes('12:00')
+	)
+	console.log(filterTheTime)
+	displayForecast()
+}
