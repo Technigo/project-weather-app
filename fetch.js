e07eb781bb975688abda20ef21669fe635cef0b4
Anton Tallstig <macbook@MacBook-Air-som-tillhor-Anton.local>
2024-03-04 19:39:23 +0100

    finished everything forecast related

--- fetch.js
+++ fetch.js
@@ -2,8 +2,10 @@ const todayBaseURL = "https://api.openweathermap.org/data/2.5/weather?";
 const api_key = "13effacd4851e7429104b607caf74ebc";
 const city = "Stockholm,Sweden";
 const todayURL = `${todayBaseURL}q=${city}&units=metric&APPID=${api_key}`;
+const forecastBaseURL = "https://api.openweathermap.org/data/2.5/forecast?";
+const forecastURL = `${forecastBaseURL}q=${city}&units=metric&APPID=${api_key}`;
 
-// DOM Selectors
+// DOM Selectors for current weather container
 const cityName = document.getElementById("city");
 const temperature = document.getElementById("temp");
 const weatherType = document.getElementById("we_type");
@@ -11,13 +13,12 @@ const sunriseTime = document.getElementById("sunrise");
 const sunsetTime = document.getElementById("sunset");
 const currentWeatherIcon = document.getElementById("weather-icon");
 
-// Initiate network request
+// Initiate network request for current weather data
 fetch(todayURL)
     // Convert response to JSON
     .then(response => response.json())
     // Take JSON data and execute the code inside the callback function
     .then(data => {
-        console.log(data);
         const currentCity = data.name;
         const todayTemp = data.main.temp;
         const currentType = data.weather[0].description;
@@ -45,4 +46,44 @@ fetch(todayURL)
         weatherType.innerText = currentType;
         sunriseTime.innerText = `Sunrise: ${sunriseFormatted}`;
         sunsetTime.innerText = `Sunset: ${sunsetFormatted}`;
+    });
+
+// DOM Selector for the forecast containers
+const forecastContainers = document.querySelectorAll('.day-container');
+
+// Function to update the HTML
+function updateForecastData(container, weekdayString, forecastIconURL, middayTemp) {
+    container.querySelector('.weekday').innerText = weekdayString;
+    container.querySelector('.forecast-icon').src = forecastIconURL;
+    container.querySelector('.midday_temp').innerText = `${middayTemp}°C`;
+}
+
+// Initiate network request for forecast data
+fetch(forecastURL)
+    // Convert response to JSON
+    .then(response => response.json())
+    // Take JSON data and execute the code inside the callback function
+    .then(data => {
+        // Filter array for 12:00 data
+        const filterArray = data.list.filter(item => item.dt_txt.includes("12:00"));
+
+        // Loop through forecast containers and update data
+        forecastContainers.forEach((container, index) => {
+            const item = filterArray[index];
+
+            // Convert unix to weekdays
+            const unix = item.dt;
+            const date = new Date(unix * 1000);
+            const weekday = date.getDay();
+            const weekdayArray = ["Sun", "Mon", "Tue", "Wed", "Thu", "Fri", "Sat"];
+            const weekdayString = weekdayArray[weekday];
+
+            // Forecast weather icon and temperature
+            const weatherIcon = item.weather[0].icon;
+            const forecastIconURL = `http://openweathermap.org/img/w/${weatherIcon}.png`;
+            const middayTemp = item.main.temp;
+
+            // Call function to update the HTML
+            updateForecastData(container, weekdayString, forecastIconURL, middayTemp);
+        });
     });
