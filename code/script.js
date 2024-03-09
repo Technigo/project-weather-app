cb5f9504a393c6f0ffbe2f37958a7a102e34b683
SofieFerrari <sofiestrahl@gmail.com>
2024-03-09 21:41:59 +0100

    fixed colors changing depending on weather description, also added icon

--- code/script.js
+++ code/script.js
@@ -23,8 +23,8 @@ fetchWeather()
 //Function to get data to HTML
 const updateHtml = (data) => {
 	const weather = data.weather[0].main
-	const city = data.name
-	const temp = Math.trunc(data.main.temp)
+	const cityName = data.name
+	const temp = data.main.temp.toFixed(1)
 	const rise = new Date(data.sys.sunrise * 1000).toLocaleTimeString([], {
 		hour: '2-digit',
 		minute: '2-digit',
@@ -39,77 +39,23 @@ const updateHtml = (data) => {
     <p>sunset ${set}</p>`
 	console.log(sun)
 
-	weatherStyle(weather)
+	weatherStyle(weather, cityName)
 	filterTheTime(updateForecast)
 }
 
 //Function that changes background/text-color depending on weather. ${city} not working.
-const weatherStyle = (weather) => {
-	// Hide all images
-	document.querySelectorAll('.img-container img').forEach((img) => {
-		img.style.display = 'none'
-	})
-	// Remove all weather-related classes
-    backgroundColor.classList.remove('sunny', 'cloud', 'rain');
-
+const weatherStyle = (weather, cityName) => {
 	if (weather === 'Clear') {
-		backgroundColor.classList.add('sunny')
-		cityContainer.innerHTML = `<h1>Get your sunnies on. Umeå is looking rather great today.</h1>`
-		document.getElementById('sunny').style.display = 'block'
+		cityContainer.innerHTML += `<img src="/images/noun_Sunglasses_2055147.svg" alt="sunglasses"/><h1>Get your sunnies on! ${cityName} is looking rather great today!</h1>`
+		document.body.style.backgroundColor = '#f7e9b9'
+		document.body.style.color = '#2a5510'
 	} else if (weather === 'Clouds' || weather === 'Mist') {
-		backgroundColor.classList.add('cloud')
-		cityContainer.innerHTML = `<h1>Light a fire and get cozy. Umeå is looking grey today.</h1>`
-		document.getElementById('cloud').style.display = 'block'
+		cityContainer.innerHTML = `<img src="/images/noun_Cloud_1188486.svg" alt="cloud"/><h1>Light a fire and get cozy. ${cityName} is looking grey today.</h1>`
+		document.body.style.backgroundColor = '#f4f7f8'
+		document.body.style.color = '#f47775'
 	} else if (weather === 'Rain') {
-		backgroundColor.classList.add('rain')
-		cityContainer.innerHTML = `<h1>Don't forget your umbrella. It's wet in Umeå today.</h1>`
-		document.getElementById('rain').style.display = 'block'
+		cityContainer.innerHTML = `<img src="/images/noun_Umbrella_2030530.svg" alt="umbrella"/><h1>Don't forget your umbrella. It's wet in ${cityName} today.</h1>`
+		document.body.style.backgroundColor = '#a3def7'
+		document.body.style.color = '#164a68'
 	}
 }
-
-//Variabel for weekdays
-
-
-const updateForecast = (filteredForecast) => {
-	console.log('filterForecast:', data)
-
-	let forecastHTML = ''
-	// Check if filterForecast is an array before attempting to use forEach
-	if (!Array.isArray(data)) {
-		console.error('data is not an array')
-		return
-	}
-	data.forEach((day) => {
-		const showDay = new Date(day.dt * 1000)
-		const dayIndex = showDay.getDay()
-		const weekDay = dayNames[dayIndex]
-		const weekTemp = Math.round(day.main.temp)
-		const dayNames = ['sun', 'mon', 'tue', 'wed', 'thu', 'fri', 'sat']
-		forecastHTML += `<div class ="weekday-list"><p>${weekDay}</p></div> <div class ="temp-list"><p>${weekTemp} °C</p></div>`
-	})
-
-	// Set forecastContainer innerHTML
-	forecastContainer.innerHTML = forecastHTML
-}
-
-//funktion för att filtrera fram 12.00
-const filterTheTime = (data) => {
-        const filteredForecast = data.list.filter((day) =>
-            day.dt_txt.includes('12:00')
-        )
-        updateForecast()
-}
-
-//Fetching the API FORECAST
-const fetchForecast = () => {
-    fetch(
-        'https://api.openweathermap.org/data/2.5/forecast?q=Umea,Sweden&units=metric&APPID=1e48fdf267ccc8ee33c1c78150dcbab1'
-    )
-        .then((response) => response.json())
-        .then((data) => {
-            console.log('forecast', data)
-            updateForecast(data)
-        })
-        .catch((error) => console.log(error))
-}
-fetchForecast()
