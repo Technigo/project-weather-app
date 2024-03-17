abd5f409c46d8085ddb2a89f3fed76344d4d8336
Alexandra Franzen <alexandra.s.franzen@gmail.com>
2024-03-17 17:40:23 +0100

    Final hand in for review.

--- script.js
+++ script.js
@@ -1,6 +1,6 @@
 const BASE_URL =
   "https://api.openweathermap.org/data/2.5/weather?q=Stockholm,Sweden&units=metric&APPID=343b966846b558ccc45becaa3d348154";
-//const API_KEY = "343b966846b558ccc45becaa3d348154"
+
 const FORECAST_URL =
   "https://api.openweathermap.org/data/2.5/forecast?q=Stockholm,Sweden&units=metric&APPID=343b966846b558ccc45becaa3d348154";
 
@@ -45,16 +45,20 @@ const getWeatherData = () => {
         body.style.background = "#F7E9B9";
         body.style.color = "#2A5510";
         container.innerHTML = `
-        <img src="noun_Sunglasses_2055147.svg">
+        <img src="http://127.0.0.1:5500/icons-images-new/noun_Sunglasses_2055147.svg">
         <h1>Get your sunnies on. ${data.name} is looking rather great today.</h1>`;
       } else if (todaysWeather === "Rain") {
         body.style.background = "#A3DEF7";
         body.style.color = "#164A68";
-        container.innerHTML = `<h1>Don't forget your umbrella. It's wet in ${data.name} today.</h1>`;
+        container.innerHTML = `
+        <img src="http://127.0.0.1:5500/icons-images-new/noun_Umbrella_2030530.svg">
+        <h1>Don't forget your umbrella. It's wet in ${data.name} today.</h1>`;
       } else if (todaysWeather === "Clouds") {
         body.style.background = "##F4F7F8";
         fiveDays.style.color = "#F47775";
-        container.innerHTML = `<h1>Light a fire and get cosy. ${data.name} is looking grey today.</h1>
+        container.innerHTML = `
+        <img src="http://127.0.0.1:5500/icons-images-new/noun_Cloud_1188486.svg">
+        <h1>Light a fire and get cosy. ${data.name} is looking grey today.</h1>
         `;
       }
     });
@@ -83,9 +87,10 @@ const getWeatherForecast = () => {
         const temperatureFiveDays = data.main.temp.toFixed(0);
 
         fiveDays.innerHTML += `
+        <div id="forecast">
         <p>${fiveDaysList}</p>
         <p>${temperatureFiveDays}°</p>
-        `;
+        </div>`;
       });
     });
 };
