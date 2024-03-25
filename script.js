2f02a46432bb8314a6fb06f16843127deed7bf3b
Paulajungaker <paula010514@hotmail.com>
2024-03-25 10:56:10 +0100

    Fixing the requirements

--- script.js
+++ script.js
@@ -1,23 +1,19 @@
-
-
- const searchBar = document.getElementById("searchPlace");
- const searchForCityText = document.getElementById("searchForCityText");
- const weatherContainer = document.getElementById("weatherContainer");
- const weatherForecastContainer = document.getElementById("weatherForecastContainer");
- const fullContainer = document.getElementById("fullContainer");
- const textContainer = document.getElementById("textContainer");
- const icon = document.getElementById("icon");
- const sunrise = document.getElementById("sunrise");
- const sunset = document.getElementById("sunset");
-
+const weatherContainer = document.getElementById("weatherContainer");
+const weatherForecastContainer = document.getElementById(
+  "weatherForecastContainer"
+);
+const fullContainer = document.getElementById("fullContainer");
+const textContainer = document.getElementById("textContainer");
+const icon = document.getElementById("icon");
+const sunrise = document.getElementById("sunrise");
+const sunset = document.getElementById("sunset");
 
 const city = "Palma";
 const base_URL = "https://api.openweathermap.org/data/2.5/weather?";
 const API_Key = "a27f07476fdacc3ee7dc03fc0b7264ed";
 const units = "metric";
-const baseForecast_URL = "https://api.openweathermap.org/data/2.5/forecast?"
+const baseForecast_URL = "https://api.openweathermap.org/data/2.5/forecast?";
 const forecast_URL = `${baseForecast_URL}q=${city}&units=${units}&APPID=${API_Key}`;
-
 
 // Update text and image depending on weather
 const sunny = () => {
@@ -25,155 +21,126 @@ const sunny = () => {
   document.documentElement.style.backgroundColor = "#F7E9B9";
   textContainer.innerHTML = `Get your sunnies on. ${city} is looking rather great today.`;
   fullContainer.style.color = "#2A5510";
-}
+};
 
 const rainy = () => {
   icon.src = "./design/design2/icons/noun_Umbrella_2030530.svg";
   document.documentElement.style.backgroundColor = "#BDE8FA";
-  textContainer.innerHTML = `Don't forget your umbrella. It's wet in ${city} today.`
+  textContainer.innerHTML = `Don't forget your umbrella. It's wet in ${city} today.`;
   fullContainer.style.color = "#164A68";
-}
+};
 
 const cloudy = () => {
   icon.src = "./design/design2/icons/noun_Cloud_1188486.svg";
   document.documentElement.style.backgroundColor = "#F4F7F8";
-  textContainer.innerHTML = `Light a fire and get cozy. ${city} is looking grey today.`
+  textContainer.innerHTML = `Light a fire and get cozy. ${city} is looking grey today.`;
   fullContainer.style.color = "#F47775";
-}
+};
 
 // Fetch the API
 const fetchWeatherData = (city) => {
   const URL = `${base_URL}q=${city}&units=${units}&APPID=${API_Key}`;
   console.log(URL);
 
-fetch(URL)
-   .then((response) => {
-    if (!response.ok) {
-      throw new Error("Network response was not ok");
-    }
-    return response.json();
-   })
-   .then((json) => {
-    const cityName = json.name;
-    const degrees = Math.round(json.main.temp);
-    const weatherDescription = json.weather[0].description;
-    
-    // Turn sunset and sunrise to a readable format like 13:00
+  fetch(URL)
+    .then((response) => {
+      if (!response.ok) {
+        throw new Error("Network response was not ok");
+      }
+      return response.json();
+    })
+    .then((json) => {
+      const degrees = json.main.temp.toFixed(1);
+      const weatherDescription = json.weather[0].description;
 
-   function convertTime(timestamp) {
-     const date = new Date(timestamp * 1000);
+      // Turn sunset and sunrise to a readable format like 13:00
 
-   const hours = date.getHours().toString().padStart(2, "0");
+      function convertTime(timestamp) {
+        const date = new Date(timestamp * 1000);
 
-   const minutes = date.getMinutes().toString().padStart(2, "0");
+        const hours = date.getHours().toString().padStart(2, "0");
 
-   return `${hours}:${minutes}`;
+        const minutes = date.getMinutes().toString().padStart(2, "0");
 
-   }
+        return `${hours}:${minutes}`;
+      }
 
-    const sunriseTime = convertTime(json.sys.sunrise);
-    console.log("Sunrise time: ", sunriseTime);
+      const sunriseTime = convertTime(json.sys.sunrise);
+      console.log("Sunrise time: ", sunriseTime);
 
-    const sunsetTime = convertTime(json.sys.sunset);
-    console.log("Sunset time: ", sunsetTime);
+      const sunsetTime = convertTime(json.sys.sunset);
+      console.log("Sunset time: ", sunsetTime);
 
+      // Fill the content in the HTML elements
+      weatherContainer.textContent = `${weatherDescription} | ${degrees}°C`;
 
-// Fill the content in the HTML elements
-weatherContainer.textContent = `${weatherDescription} | ${degrees}°C`;
+      // Fill in the sunset and sunrise in the HTML element.
 
-// Fill in the sunset and sunrise in the HTML element. 
+      sunrise.textContent = `Sunrise ${sunriseTime}`;
+      sunset.textContent = `Sunset ${sunsetTime}`;
 
-sunrise.textContent = `Sunrise ${sunriseTime}`;
-sunset.textContent =`Sunset ${sunsetTime}`;
+      // Choose function for display looks based on description
 
-// Choose function for display looks based on description
-
-if (weatherDescription.includes("clear sky")) {
-  sunny();
-  } else if (weatherDescription.includes("broken clouds") || weatherDescription.includes("scattered clouds") || weatherDescription.includes("few clouds") || weatherDescription.includes("mist")) {
-  cloudy();
-  } else {
-  rainy();
-  }
-
-  })
-   .catch((error) => {
-    console.error("Error fetching data:", error);
-   });
-  };
+      if (weatherDescription.includes("clear sky")) {
+        sunny();
+      } else if (
+        weatherDescription.includes("broken clouds") ||
+        weatherDescription.includes("scattered clouds") ||
+        weatherDescription.includes("few clouds") ||
+        weatherDescription.includes("mist")
+      ) {
+        cloudy();
+      } else {
+        rainy();
+      }
+    })
+    .catch((error) => {
+      console.error("Error fetching data:", error);
+    });
+};
 
 // Example of city
-  
-  fetchWeatherData(city);
+
+fetchWeatherData(city);
 
 // Fetch for the forecast data
 
 const fetchForecast = (city) => {
   fetch(forecast_URL)
-  .then((response) => response.json())
-  .then((json) => {
-    console.log(json);
-  
-  // Check that time is 12:00
-  const filteredTime = json.list.filter((daytimeWeather)=> 
-  daytimeWeather.dt_txt.includes("12:00")
-  );
+    .then((response) => response.json())
+    .then((json) => {
+      console.log(json);
 
-  let number = 1
+      // Check that time is 12:00
+      const filteredTime = json.list.filter((daytimeWeather) =>
+        daytimeWeather.dt_txt.includes("12:00")
+      );
 
-  filteredTime.forEach((day) => {
-    const date = new Date(day.dt * 1000);
-    nameOfDay = date.toLocaleDateString("en-US", {weekday: "short"});
-    const tempMin = Math.round(day.main.temp_min);
-    const tempMax = Math.round(day.main.temp_max);
+      let number = 1;
 
-    console.log(nameOfDay, tempMin, tempMax);
+      for (let i = 0; i < 4; i++) {
+        const day = filteredTime[i];
+        const date = new Date(day.dt * 1000);
+        const nameOfDay = date.toLocaleDateString("en-US", {
+          weekday: "short",
+        });
+        const tempMin = Math.round(day.main.temp_min);
+        const tempMax = Math.round(day.main.temp_min);
 
-    document.getElementById(`day${number}`).innerHTML = `${nameOfDay}`;
-    document.getElementById(`dayTemp${number}`).innerHTML = `${tempMin}°C / ${tempMax}°C\n`;
-    number++;
-    console.log(`day${number}`)
+        console.log(nameOfDay, tempMin, tempMax);
 
-  });
- 
-  })
+        document.getElementById(`day${number}`).innerHTML = `${nameOfDay}`;
+        document.getElementById(
+          `dayTemp${number}`
+        ).innerHTML = `${tempMax}°C / ${tempMin}°C\n`;
+        number++;
+      }
+    })
 
-  .catch((error) => {
-    console.error("Error fetching forecast: ", error);
-  });
+    .catch((error) => {
+      console.error("Error fetching forecast: ", error);
+    });
 };
 
-  fetchForecast(city);
-  console.log(forecast_URL);
-
-
-
-
-
-
-
-
-
-
-  
-
- 
-
-
- 
-
-
- 
-
-
-
-
-
-
-
-
-
-
-
-
-
+fetchForecast(city);
+console.log(forecast_URL);
