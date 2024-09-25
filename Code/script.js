f2c771420e6ea4765000cca27e77b15332f65470
Elina Eriksson Hult <elina@MacBook-Pro-som-tillhor-Elina.local>
2024-09-25 12:31:17 +0200

    City, temperature and what type of weather displayed on web app

--- Code/script.js
+++ Code/script.js
@@ -11,12 +11,40 @@ let city = "Stockholm";
 const URL = `${BASE_URL}?q=${city}&units=metric&APPID=${API_KEY}`;
 console.log(URL)
 
+// DOM selectors
+// const place = document.getElementById("temperature");
+
 fetch(URL)
   .then(response => response.json())
   .then(data => {
-    console.log(data.weather)
+
+    cityName = data.name;
+    //Log to console - remove later
+    console.log(cityName)
+
+    const temp = data.main.temp.toFixed(1);
+    //Log to console - remove later
+    console.log(temp)
+
+    const typeOfWeather = data.weather[0].description;
+    //Log to console - remove later
+    console.log(typeOfWeather)
+
+    const sunriseTime = data.sys.sunrise;
+    //Log to console - remove later
+    console.log(sunriseTime)
+
+    const sunsetTime = data.sys.sunset;
+    //Log to console - remove later
+    console.log(sunsetTime)
+
+    document.getElementById("location").innerText = `${cityName}`;
+    document.getElementById("temperature").innerText = `${temp}°C`;
+    document.getElementById("weather").innerText = `${typeOfWeather}`;
   })
 
+// - The app should have: city name, current temperature, weather description,
+//  sunrise/sunset time, 4-day forecast
 
 
 
