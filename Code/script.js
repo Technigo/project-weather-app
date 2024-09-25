7f623c808565caa8f4e25edfb6621200aaffb633
Elina Eriksson Hult <elina@MacBook-Pro-som-tillhor-Elina.local>
2024-09-25 10:38:49 +0200

    First commit - added URL and API key

---
+++ Code/script.js
@@ -0,0 +1,12 @@
+//Stockholm current weather API
+//URL current weather Stockholm + city + API key
+// https://api.openweathermap.org/data/2.5/weather?q=Stockholm,Sweden&units=metric&APPID=959cb256f265cbbe5b4051e4a40be3af
+
+// True constants
+const API_KEY = "959cb256f265cbbe5b4051e4a40be3af";
+const BASE_URL = "https://api.openweathermap.org/data/2.5/weather";
+
+let city = "Stockholm";
+
+const URL = `${BASE_URL}?q=${city}&units=metric&APPID=${API_KEY}`;
+console.log(URL)
