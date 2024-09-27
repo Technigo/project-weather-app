2b1c3a9fe483b3cd37327853fd6d1459c0e41ac7
Gitte Beckmann <gittebeckmann@hotmail.com>
2024-09-27 21:00:08 +0200

    added sunrise & sunset with time converted

--- script.js
+++ script.js
@@ -1,4 +1,4 @@
-const BASE_URL = "https://api.openweathermap.org/data/2.5/weather?"
+const BASE_URL = "https://api.openweathermap.org/data/2.5/"
 const API_KEY = "72e635da2875c352d9f726550253e2db"
 
 const menu = document.getElementById('hamburgerMenu')
@@ -12,10 +12,30 @@ const cityName = document.getElementById('cityName')
 const sunrise = document.getElementById('sunrise')
 const sunset = document.getElementById('sunset')
 
-// function to create new URL based on the users input
-const createURL = (cityName) => {
-    return `${BASE_URL}q=${cityName}&units=metric&APPID=${API_KEY}`
+
+// function to create new URL for forecast based on the users input
+const createForecastURL = (cityName) => {
+    return `${BASE_URL}forecast?q=${cityName}&units=metric&APPID=${API_KEY}`
 }
+
+// function to create new URL for current weather based on the users input
+const createURL = (cityName) => {
+    return `${BASE_URL}weather?q=${cityName}&units=metric&APPID=${API_KEY}`
+}
+
+
+// function to convert Unix-time into hours:minutes
+const convertUnixToTime = (unixTime, timeZone) => {
+    const time = unixTime * 1000
+    const date = new Date(time)
+    const localTime = new Date(date.getTime() + timeZone * 1000)
+
+    const hours = localTime.getUTCHours()
+    const minutes = "0" + localTime.getUTCMinutes()
+    return `${hours}:${minutes.substr(-2)}`
+}
+
+// function to convert the
 
 // update HTML
 const updateHTML = (data) => {
@@ -24,16 +44,10 @@ const updateHTML = (data) => {
     // method to round the temperatures
     const roundedTemp = Math.round(data.main.temp)
 
-    // function to convert Unix-time into hours:minutes
-    const convertUnixToTime = (unixTime) => {
-        const date = new Date(unixTime * 1000)  // Unix-times works in sec JavaScript works in mill.sec.
-        const hours = date.getHours()
-        const minutes = "0" + date.getMinutes()
-        return `${hours}:${minutes.substr(-2)}`
-    }
-
-    const sunriseTime = convertUnixToTime(data.sys.sunrise)
-    const sunsetTime = convertUnixToTime(data.sys.sunset)
+    // the local time for the specific city
+    // const localOffset = getLocalOffset(data.coord.lat, data.coord.lon)
+    const sunriseTime = convertUnixToTime(data.sys.sunrise, data.timezone)
+    const sunsetTime = convertUnixToTime(data.sys.sunset, data.timezone)
 
     temperature.innerText = `${roundedTemp}°C`
     weather.innerText = data.weather[0].description
@@ -58,6 +72,30 @@ const fetchWeatherData = async (cityName) => {
         console.log(error)
     }
 }
+
+// Update forecast HTML
+const updateForecastHTML = (dataForecast) => {
+
+
+
+}
+
+// to get the forecast weather data
+const fetchForecastData = async (cityName) => {
+    const URLForecast = createForecastURL(cityName)
+    try {
+        const response = await fetch(URLForecast)
+        if (!response.ok) {
+            throw new Error('Failed to fetch forecast weather data')
+        }
+        const dataForecast = await response.json()
+        console.log(dataForecast)
+
+    } catch (error) {
+        console.log(error)
+    }
+}
+
 
 // Event Listener to open the search window
 menu.addEventListener("click", () => {
@@ -85,6 +123,7 @@ searchButton.addEventListener("click", () => {
     console.log(city)
     if (city) {
         fetchWeatherData(city)
+        fetchForecastData(city)
         searchWindow.style.display = "none"
     } else (
         console.log("please enter a city name")
