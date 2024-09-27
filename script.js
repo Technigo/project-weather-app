e167abf960c2872b318c3c2a3645eeb17dc6965d
xingyin2024 <xingyin.thu@gmail.com>
2024-09-27 11:39:35 +0200

    add favButton

--- script.js
+++ script.js
@@ -9,4 +9,25 @@ console.log(URL_FORECAST)
 // alert("Oops, city not found! Check your spelling please.")
 
 // DOM selectors
-// const 
+const searchIcon = document.getElementById("searchIcon")
+const sideMenu = document.getElementById("sideMenu")
+const closeButton = document.getElementById("closeButton")
+const inputLocation = document.getElementById("inputLocation")
+const searchButton = document.getElementById("searchButton")
+const mainIcon = document.getElementById("mainIcon")
+const weatherData = document.getElementById("weatherData")
+const currentTemp = document.getElementById("currentTemp")
+const currentCity = document.getElementById("currentCity")
+const typeOfWeather = document.getElementById("typeOfWeather")
+const time = document.getElementById("time")
+const sunTimes = document.getElementById("sunTimes")
+const forecast = document.getElementById("forecast")
+const weekIcon = document.getElementById("weekIcon")
+const weekTemp = document.getElementById("weekTemp")
+const weekWindSpeed = document.getElementById("weekWindSpeed")
+const breathingButton = document.getElementById("breathingButton")
+
+let favCities = ["Stockholm", "Paris", "London", "Dubai", "Beijing", "Tokyo"]
+
+// Function to fetch weather data for favorite city
+
