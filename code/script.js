9d178178a3e72f9b7d3900c9bcfc9ee3b3195649
Johanna Eriksson <johanna.eriksson@schibsted.com>
2024-09-27 13:03:15 +0200

    Fetch data for City name, weather description, sunrise, sunset and temperatur. Update DOM selectors and HTML with correct ID and names.

--- code/script.js
+++ code/script.js
@@ -10,6 +10,27 @@ const forecastURL = `${forecastBaseURL}q=${city}&units=metric&APPID=${api_key}`
 
 // DOM Selectors
 const cityName = document.getElementById("city")
-const temperature = document.getElementById("temp")
+const description = document.getElementById("description")
+const temperature = document.getElementById("temperature")
 const sunriseTime = document.getElementById("sunrise")
 const sunsetTime = document.getElementById("sunset")
+
+//Fetch todays weather
+const fetchTodaysWeatherAsync = async () => {
+    try {
+        const response = await fetch(`${todayURL}`)
+        //convert response to JSON
+        const data = await response.json()
+        console.log("Data is shown in JSON format: ", data)
+        
+        cityName.innerHTML = data.name
+        temperature.innerHTML = `${data.main.temp} °C`
+        description.innerHTML = data.weather[0].description
+        sunriseTime.innerHTML = data.sys.sunrise
+        sunsetTime.innerHTML = data.sys.sunset
+    } catch (error) {
+     //Handle any errors 
+    console.error("Error when fetching Today's weather", error)
+     }
+}
+fetchTodaysWeatherAsync()
