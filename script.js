a2ee000751b2f0d4145395157d2932a485eaa8ad
Gitte Beckmann <gittebeckmann@hotmail.com>
2024-09-27 14:38:00 +0200

    added sunrise and sunset

--- script.js
+++ script.js
@@ -1,7 +1,5 @@
 const BASE_URL = "https://api.openweathermap.org/data/2.5/weather?"
 const API_KEY = "72e635da2875c352d9f726550253e2db"
-// const city = "q=city name"
-// const URL = `${BASE_URL}${city}&units=metric&APPID=${API_KEY}`
 
 const menu = document.getElementById('hamburgerMenu')
 const searchWindow = document.getElementById('searchLocationWindow')
@@ -11,6 +9,8 @@ const cityInput = document.getElementById('searchCity')
 const temperature = document.getElementById('temp')
 const weather = document.getElementById('weather')
 const cityName = document.getElementById('cityName')
+const sunrise = document.getElementById('sunrise')
+const sunset = document.getElementById('sunset')
 
 // function to create new URL based on the users input
 const createURL = (cityName) => {
@@ -24,9 +24,22 @@ const updateHTML = (data) => {
     // method to round the temperatures
     const roundedTemp = Math.round(data.main.temp)
 
+    // function to convert Unix-time into hours:minutes
+    const convertUnixToTime = (unixTime) => {
+        const date = new Date(unixTime * 1000)  // Unix-times works in sec JavaScript works in mill.sec.
+        const hours = date.getHours()
+        const minutes = "0" + date.getMinutes()
+        return `${hours}:${minutes.substr(-2)}`
+    }
+
+    const sunriseTime = convertUnixToTime(data.sys.sunrise)
+    const sunsetTime = convertUnixToTime(data.sys.sunset)
+
     temperature.innerText = `${roundedTemp}°C`
     weather.innerText = data.weather[0].description
     cityName.innerText = data.name
+    sunrise.innerText = sunriseTime
+    sunset.innerText = sunsetTime
 }
 
 // to get the weather data for the current weather
