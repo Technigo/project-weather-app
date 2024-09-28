73b2cefb70893b9c902d960106e154f3b085ece4
Elina Eriksson Hult <elina@MacBook-Pro-som-tillhor-Elina.local>
2024-09-29 01:10:40 +0200

    Added media queries

--- Code/script.js
+++ Code/script.js
@@ -1,7 +1,3 @@
-//Stockholm current weather API
-//URL current weather Stockholm + city + API key
-// https://api.openweathermap.org/data/2.5/weather?q=Stockholm,Sweden&units=metric&APPID=959cb256f265cbbe5b4051e4a40be3af
-
 // True constants
 const API_KEY = "959cb256f265cbbe5b4051e4a40be3af";
 const BASE_URL = "https://api.openweathermap.org/data/2.5/weather";
