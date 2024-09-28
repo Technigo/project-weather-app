ebdcebc7f9cfc11f36df4036541fce5b7ebf9181
ErikaOlsson <erika__olsson@hotmail.com>
2024-09-28 22:00:13 +0200

    Update: CSS btn and input

--- code/script.js
+++ code/script.js
@@ -2,7 +2,7 @@
 /* JSON file - Weather API */
 const API_KEY = "3cc790b1653416aade4ba256c95e0c67"
 const BASE_URL = "https://api.openweathermap.org/data/2.5/weather"
-let cityWeather = "Berlin";
+let cityWeather = "katrineholm";
 
 const URL = `${BASE_URL}?q=${cityWeather}&units=metric&APPID=${API_KEY}`
 console.log(URL)
@@ -41,32 +41,41 @@ const showNewHTML = (data) => {
     const sunriseDate = new Date(sunrise * 1000);
     const sunsetDate = new Date(sunset * 1000);
     // Format the time (HH:MM:SS)
-    const sunriseFormatted = sunriseDate.toLocaleTimeString();
-    const sunsetFormatted = sunsetDate.toLocaleTimeString();
+    const sunriseFormatted = sunriseDate.toLocaleTimeString([], { hour: '2-digit', minute: '2-digit' });
+    const sunsetFormatted = sunsetDate.toLocaleTimeString([], { hour: '2-digit', minute: '2-digit' });
 
     timeSunrise.innerText = sunriseFormatted
     timeSunset.innerText = sunsetFormatted
     
+    /* Layout to what weather is shown */
     if (description.includes("clear")) {
-        document.body.classList.remove("weather-card-clear", "weather-card-rain", "weather-card-cloudy");  // Rensa tidigare klasser
+        document.body.classList.remove("weather-card-clear", "weather-card-rain", "weather-card-cloudy", "btn-city-input-cloudy", "city-input-cloudy", "btn-city-input-rain", "city-input-rain");  // Rensa tidigare klasser
         weatherTitle.innerText = `Get your sunnies on. ${cityWeather} is looking rather great today.`;
         weatherIcon.src = "../assets/design-2/noun_Sunglasses_2055147.svg";  // Bild för soligt väder
         document.body.classList.add("weather-card-clear");
+        document.getElementById("city-input").className = "city-input-clear";
+        document.getElementById("btn-city-input").className = "btn-city-input-clear";
     } else if (description.includes("rain")) {
-        document.body.classList.remove("weather-card-clear", "weather-card-rain", "weather-card-cloudy");  // Rensa tidigare klasser
+        document.body.classList.remove("weather-card-clear", "weather-card-rain", "weather-card-cloudy", "btn-city-input-cloudy", "city-input-cloudy", "btn-city-input-clear", "city-input-clear", "btn-city-input-rain", "city-input-rain");  // Rensa tidigare klasser
         weatherTitle.innerText = `Don’t forget your umbrella. It’s wet in ${cityWeather} today.`;
         weatherIcon.src = "../assets/design-2/noun_Umbrella_2030530.svg";  // Bild för regnigt väder
         document.body.classList.add("weather-card-rain");
+        document.getElementById("city-input").className = "city-input-rain";
+        document.getElementById("btn-city-input").className = "btn-city-input-rain";
     } else if (description.includes("cloud")) {
-        document.body.classList.remove("weather-card-clear", "weather-card-rain", "weather-card-cloudy");  // Rensa tidigare klasser
+        document.body.classList.remove("weather-card-clear", "weather-card-rain", "weather-card-cloudy", "btn-city-input-cloudy", "city-input-cloudy", "btn-city-input-clear", "city-input-clear", "btn-city-input-rain", "city-input-rain");  // Rensa tidigare klasser
         weatherTitle.innerText = `Light a fire and get cosy. ${cityWeather} is looking grey today. `;
         weatherIcon.src = "../assets/design-2/noun_Cloud_1188486.svg";  // Bild för molnigt väder
         document.body.classList.add("weather-card-cloudy");
+        document.getElementById("city-input").className = "city-input-cloudy";
+        document.getElementById("btn-city-input").className = "btn-city-input-cloudy";
     } else {
-        document.body.classList.remove("weather-card-clear", "weather-card-rain", "weather-card-cloudy");  // Rensa tidigare klasser
+        document.body.classList.remove("weather-card-clear", "weather-card-rain", "weather-card-cloudy", "btn-city-input-cloudy", "city-input-cloudy", "btn-city-input-clear", "city-input-clear", "btn-city-input-rain", "city-input-rain");  // Rensa tidigare klasser
         weatherTitle.innerText = `${cityWeather} is offering a weather today thats not in my vocabulary.`;
         weatherIcon.src = "../assets/design-2/noun_Cloud_1188486.svg";  // Bild för molnigt väder
         document.body.classList.add("weather-card-cloudy");
+        document.getElementById("city-input").className = "city-input-cloudy";
+        document.getElementById("btn-city-input").className = "btn-city-input-cloudy";
     }
 }
 
