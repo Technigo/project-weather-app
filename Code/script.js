78488edd1adcfae8a3e63ae3ad6ef2a8fe21de7f
Elina Eriksson Hult <elina@MacBook-Pro-som-tillhor-Elina.local>
2024-09-29 16:49:33 +0200

    Clean up, removed console log and edited styling elements

--- Code/script.js
+++ Code/script.js
@@ -3,19 +3,20 @@ const API_KEY = "959cb256f265cbbe5b4051e4a40be3af";
 const BASE_URL = "https://api.openweathermap.org/data/2.5/weather";
 const FORECAST_BASE_URL = "https://api.openweathermap.org/data/2.5/forecast";
 
-let city = "Stockholm";
+const city = "Stockholm";
 
+// Create URL for current weather
 const URL = `${BASE_URL}?q=${city}&units=metric&APPID=${API_KEY}`;
 console.log(URL)
 
-// DOM selectors, take away this later
+const errorDiv = document.getElementById("error");
 
 //Fetch current weather data
 fetch(URL)
   .then(response => response.json())
   .then(data => {
 
-    cityName = data.name;
+    const cityName = data.name;
     const temp = data.main.temp.toFixed(1);
     const typeOfWeather = data.weather[0].description;
     //Convert first letter of weather description to uppercase
@@ -24,9 +25,7 @@ fetch(URL)
     const sunriseTime = new Date(data.sys.sunrise * 1000).toLocaleTimeString([], { hour: "2-digit", minute: "2-digit" });
     const sunsetTime = new Date(data.sys.sunset * 1000).toLocaleTimeString([], { hour: "2-digit", minute: "2-digit" });
 
-    // Remove consol log later
-    console.log(cityName, temp, typeOfWeather, sunriseTime, sunsetTime);
-
+    // Display weather data on page
     document.getElementById("location").innerText = `${cityName}`;
     document.getElementById("temperature").innerText = `${temp}°`;
     document.getElementById("weather").innerText = `${capitalizedWeather}`; // Use capitalized description
@@ -34,16 +33,20 @@ fetch(URL)
     document.getElementById("sunset").innerText = `${sunsetTime}`;
   })
 
-// 4 - day forecast
+  // Error message
+  .catch(error => {
+    errorDiv.innerText = "Something went wrong.";
+  });
+
+// Fetch 4-day forecast data
 const forecastURL = `${FORECAST_BASE_URL}?q=${city}&units=metric&APPID=${API_KEY}`;
-console.log(forecastURL);
 
 fetch(forecastURL)
   .then(response => response.json())
   .then(data => {
     const forecastList = document.getElementById("forecast-list");
 
-    //Grouping forecast date and calculate the average temp
+    //Group forecast date by days and calculate average temperature
     const dailyTemps = {};
 
     data.list.forEach(forecast => {
@@ -60,25 +63,23 @@ fetch(forecastURL)
         dailyTemps[dayOfWeek].totalTemp += temp;
         dailyTemps[dayOfWeek].count += 1;
       }
-    })
+    });
 
     // Clear any existing forecast data
     forecastList.innerText = "";
 
-    // Calculate the average temp and only take the first 4 days of forecast data
-    Object.keys(dailyTemps).slice(0, 4).forEach(dayOfWeek => {
+    // Display 4-day forecast
+    Object.keys(dailyTemps).slice(1, 5).forEach(dayOfWeek => {
       const averageTemp = (dailyTemps[dayOfWeek].totalTemp / dailyTemps[dayOfWeek].count).toFixed(1);
 
       const forecastItem = document.createElement("p");
       forecastItem.innerHTML = `${dayOfWeek} ${averageTemp}°`;
 
-      // Append the forecast item to the UL
       forecastList.appendChild(forecastItem);
     });
   })
 
-  // Error message applied to both fetch functions
+  // Error message
   .catch(error => {
-    console.log("Error fetching forecast data:", error);
-    // Add innertext with id in html for error message shown on site
+    errorDiv.innerText = "Something went wrong."
   });
